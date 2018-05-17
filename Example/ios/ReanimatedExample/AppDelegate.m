3571bbdd1c8b07c07c15f4cf5312003c62344ff3
Krzysztof Magiera <krzys@swmansion.com>
2018-05-17 10:53:15 +0200

    Add finger tracing

--- Example/ios/ReanimatedExample/AppDelegate.m
+++ Example/ios/ReanimatedExample/AppDelegate.m
@@ -10,6 +10,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import "MBFingerTipWindow.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -24,7 +26,9 @@
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  MBFingerTipWindow *window = [[MBFingerTipWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  window.alwaysShowTouches = YES;
+  self.window = window;
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
