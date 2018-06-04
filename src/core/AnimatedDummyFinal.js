05b3f7cdaf552efba376263b7f350adc29550a6a
osdnk <micosa97@gmail.com>
2018-06-04 10:17:50 +0200

    Well, it might be the issue

--- src/core/AnimatedDummyFinal.js
+++ src/core/AnimatedDummyFinal.js
@@ -1,25 +1,15 @@
 import AnimatedNode from './AnimatedNode';
 import { val } from '../utils';
 
-function sanitizeValue(value) {
-  return value === null || value === undefined ? value : Number(value);
-}
+export default class AnimatedDummyFinal extends AnimatedNode {
+  _what;
 
-export default class AnimatedValue extends AnimatedNode {
-  constructor(what, value) {
-    super({
-      type: 'dummyFinal',
-      value: sanitizeValue(value),
-      what: what.__nodeID,
-    });
+  constructor(what) {
+    super({ type: 'dummyFinal', what: what.__nodeID }, [what]);
+    this._what = what;
   }
 
   __onEvaluate() {
-    if (this.__inputNodes && this.__inputNodes.length) {
-      this.__inputNodes.forEach(val);
-    }
     return 0;
   }
-
-  _updateValue(value) {}
 }
