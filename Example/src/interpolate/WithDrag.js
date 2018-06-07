fb6859cb3cbded04dbf8bdbfe8c0567c6c53ed8b
Dylan Vann <dylan@dylanvann.com>
2018-06-07 10:51:25 -0400

    Remove dead code from examples.

--- Example/src/interpolate/WithDrag.js
+++ Example/src/interpolate/WithDrag.js
@@ -10,18 +10,12 @@ const {
   cond,
   sub,
   eq,
-  and,
   add,
-  call,
   multiply,
   lessThan,
   startClock,
   stopClock,
   clockRunning,
-  block,
-  timing,
-  debug,
-  spring,
   Value,
   Clock,
   event,
@@ -58,41 +52,6 @@ function runSpring(clock, value, velocity, dest) {
     cond(state.finished, stopClock(clock)),
     state.position,
   ];
-}
-
-function runTiming(clock, value, dest) {
-  const state = {
-    finished: new Value(1),
-    position: new Value(value),
-    time: new Value(0),
-    frameTime: new Value(0),
-  };
-
-  const config = {
-    duration: 500,
-    toValue: new Value(0),
-    easing: Easing.inOut(Easing.ease),
-  };
-
-  const reset = [
-    set(state.finished, 0),
-    set(state.time, 0),
-    set(state.frameTime, 0),
-  ];
-
-  return block([
-    cond(and(state.finished, eq(state.position, value)), [
-      ...reset,
-      set(config.toValue, dest),
-    ]),
-    cond(and(state.finished, eq(state.position, dest)), [
-      ...reset,
-      set(config.toValue, value),
-    ]),
-    cond(clockRunning(clock), 0, startClock(clock)),
-    timing(clock, state, config),
-    state.position,
-  ]);
 }
 
 export default class WithDrag extends Component {
