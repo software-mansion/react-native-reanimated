0f3d3609c9788df7112b88364676d9c3eb756e4e
Dylan Vann <dylan@dylanvann.com>
2018-06-07 10:32:25 -0400

    Add color node from derived.js.

--- src/derived/index.js
+++ src/derived/index.js
@@ -1,5 +1,6 @@
 export { default as abs } from './abs';
 export { default as acc } from './acc';
+export { default as color } from './color';
 export { default as diff } from './diff';
 export { default as diffClamp } from './diffClamp';
 export { default as interpolate, Extrapolate } from './interpolate';
