1861983e387a7a37b1c949546ed35851f7121df0
osdnk <micosa97@gmail.com>
2018-06-28 19:59:41 +0200

    Testss

--- src/Animated.test.js
+++ src/Animated.test.js
@@ -7,97 +7,92 @@ jest.mock('./ReanimatedEventEmitter');
 jest.mock('./ReanimatedModule');
 
 const { Value, timing, spring, decay } = Animated;
-expect.extend({
-  toAttachNodesProperly(animation) {
-    const transX = new Value(0);
+function checkIfAttachAndDetachNodesProperly(animation) {
+  const transX = new Value(0);
 
-    const initial = getNumberOfNodes();
-    const v = new AnimatedNode({ type: 'sampleView', value: 0 }, [transX]);
-    v.__addChild(transX);
-    const before = getNumberOfNodes();
-    const anim = animation.node(transX, animation.config);
-    anim.start();
-    const during = getNumberOfNodes();
-    anim.__detach_testOnly();
-    const after = getNumberOfNodes();
-    v.__removeChild(transX);
-    const final = getNumberOfNodes();
+  const initial = getNumberOfNodes();
+  const v = new AnimatedNode({ type: 'sampleView', value: 0 }, [transX]);
+  v.__addChild(transX);
+  const before = getNumberOfNodes();
+  const anim = animation.node(transX, animation.config);
+  anim.start();
+  const during = getNumberOfNodes();
+  anim.__detach_testOnly();
+  const after = getNumberOfNodes();
+  v.__removeChild(transX);
+  const final = getNumberOfNodes();
 
-    console.log(initial, before, during, after, final);
+  console.log(initial, before, during, after, final);
 
-    const pass =
-      initial === final &&
-      after === before &&
-      during > after &&
-      initial === 0 &&
-      before === 2;
-    if (pass) {
-      return {
-        pass,
-      };
-    } else {
-      return {
-        message: () =>
-          `expected ${
-            animation.name
-          }'s nodes not to be attached and detached correctly`,
-        pass: false,
-      };
-    }
-  },
-});
+  return (
+    initial === final &&
+    after === before &&
+    during > after &&
+    initial === 0 &&
+    before === 2
+  );
+}
+
 it('fails if timing does not attach nodes correctly', () => {
-  expect({
-    node: timing,
-    name: 'timing',
-    config: {
-      duration: 5000,
-      toValue: 120,
-      easing: Easing.inOut(Easing.ease),
-    },
-  }).toAttachNodesProperly();
+  expect(
+    checkIfAttachAndDetachNodesProperly({
+      node: timing,
+      name: 'timing',
+      config: {
+        duration: 5000,
+        toValue: 120,
+        easing: Easing.inOut(Easing.ease),
+      },
+    })
+  ).toBeTruthy();
 });
 
 it('fails if decay does not attach nodes correctly', () => {
-  expect({
-    node: decay,
-    name: 'decay',
-    config: {
-      deceleration: 0.997,
-    },
-  }).toAttachNodesProperly();
+  expect(
+    checkIfAttachAndDetachNodesProperly({
+      node: decay,
+      name: 'decay',
+      config: {
+        deceleration: 0.997,
+      },
+    })
+  ).toBeTruthy();
 });
 
 it('fails if spring does not attach nodes correctly', () => {
-  expect({
-    node: spring,
-    name: 'spring',
-    config: {
-      toValue: 0,
-      damping: 7,
-      mass: 1,
-      stiffness: 121.6,
-      overshootClamping: false,
-      restSpeedThreshold: 0.001,
-      restDisplacementThreshold: 0.001,
-    },
-  }).toAttachNodesProperly();
+  expect(
+    checkIfAttachAndDetachNodesProperly({
+      node: spring,
+      name: 'spring',
+      config: {
+        toValue: 0,
+        damping: 7,
+        mass: 1,
+        stiffness: 121.6,
+        overshootClamping: false,
+        restSpeedThreshold: 0.001,
+        restDisplacementThreshold: 0.001,
+      },
+    })
+  ).toBeTruthy();
 });
 
 it('fails if clock while animation does not behave correctly', () => {
-  expect({
-    node: spring,
-    name: 'spring',
-    config: {
-      toValue: 0,
-      damping: 7,
-      mass: 1,
-      stiffness: 121.6,
-      overshootClamping: false,
-      restSpeedThreshold: 0.001,
-      restDisplacementThreshold: 0.001,
-    },
-  }).toAttachNodesProperly();
+  expect(
+    checkIfAttachAndDetachNodesProperly({
+      node: spring,
+      name: 'spring',
+      config: {
+        toValue: 0,
+        damping: 7,
+        mass: 1,
+        stiffness: 121.6,
+        overshootClamping: false,
+        restSpeedThreshold: 0.001,
+        restDisplacementThreshold: 0.001,
+      },
+    })
+  ).toBeTruthy();
 });
 
 it('fails if animation related nodes are still attached after detaching of value', () => {
