6c1da9b24b39fb076662c4df0ad0cb746eeeb1c9
osdnk <micosa97@gmail.com>
2018-06-28 18:37:09 +0200

    rev tim

--- src/animations/timing.js
+++ src/animations/timing.js
@@ -13,17 +13,16 @@ export default function timing(clock, state, config) {
   const lastTime = cond(state.time, state.time, clock);
   const frameTime = add(state.frameTime, sub(clock, lastTime));
 
-  const duration = add(config.duration);
-  const progress = config.easing(divide(state.frameTime, duration));
+  const progress = config.easing(divide(state.frameTime, config.duration));
   const distanceLeft = sub(config.toValue, state.position);
   const fullDistance = divide(distanceLeft, sub(1, progress));
   const startPosition = sub(config.toValue, fullDistance);
-  const nextProgress = config.easing(divide(frameTime, duration));
+  const nextProgress = config.easing(divide(frameTime, config.duration));
   const nextPosition = add(startPosition, multiply(fullDistance, nextProgress));
 
   return block([
     cond(
-      greaterOrEq(frameTime, duration),
+      greaterOrEq(frameTime, config.duration),
       [set(state.position, config.toValue), set(state.finished, 1)],
       set(state.position, nextPosition)
     ),
