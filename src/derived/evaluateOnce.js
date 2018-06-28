86ff328eef601486f99750a687908490d7522e76
osdnk <micosa97@gmail.com>
2018-06-28 12:01:37 +0200

    rename

--- src/derived/evaluateOnce.js
+++ src/derived/evaluateOnce.js
@@ -3,11 +3,8 @@ import { block, call, always, cond, set } from '../base';
 
 // node is a node to be evaluated and children are nodes to be marked as updated
 export function evaluateOnce(node, children = []) {
-  let _children;
-  if (Array.isArray(children)) {
-    _children = children;
-  } else {
-    _children = [children];
+  if (!Array.isArray(children)) {
+    children = [children];
   }
   const done = new AnimatedValue(0);
   const evalNode = cond(
@@ -16,15 +13,15 @@ export function evaluateOnce(node, children = []) {
     block([
       node,
       call([], () => {
-        for (let i = 0; i < _children.length; i++) {
-          alwaysNode.__removeChild(_children[i]);
+        for (let i = 0; i < children.length; i++) {
+          alwaysNode.__removeChild(children[i]);
         }
       }),
     ]),
     set(done, 1)
   );
   const alwaysNode = always(evalNode);
-  for (let i = 0; i < _children.length; i++) {
-    alwaysNode.__addChild(_children[i]);
+  for (let i = 0; i < children.length; i++) {
+    alwaysNode.__addChild(children[i]);
   }
 }
