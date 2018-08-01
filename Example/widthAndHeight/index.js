533605ceb50e1c604f82e30c90f27127e81a0958
osdnk <micosa97@gmail.com>
2018-08-01 13:20:28 +0200

    example

--- Example/widthAndHeight/index.js
+++ Example/widthAndHeight/index.js
@@ -50,7 +50,7 @@ export default class Example extends Component {
   constructor(props) {
     super(props);
     const clock = new Clock();
-    this._trans = runTiming(clock, 10, 150);
+    this._trans = runTiming(clock, 10, 120);
   }
   componentDidMount() {}
   render() {
@@ -63,6 +63,7 @@ export default class Example extends Component {
               width: this._trans,
               height: this._trans,
               top: multiply(this._trans, -0.5),
+              left: multiply(this._trans, -0.5),
             },
           ]}>
           sample text is getting bigger and bigger more and moar staph staph
