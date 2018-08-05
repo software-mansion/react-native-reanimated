59565db04cdff59bac38fe25d52cd5240180a4b5
osdnk <micosa97@gmail.com>
2018-08-05 17:34:10 +0200

    Add files

---
+++ android/src/main/java/com/swmansion/reanimated/EvaluationContext.java
@@ -0,0 +1,19 @@
+package com.swmansion.reanimated;
+
+import android.util.SparseArray;
+import android.util.SparseLongArray;
+
+import com.swmansion.reanimated.nodes.Node;
+import com.swmansion.reanimated.nodes.ProceduralNode;
+
+public class EvaluationContext {
+  private static int nextContextID = 0;
+  public int contextID = EvaluationContext.nextContextID++;
+  public SparseArray<Object> memoizedValues = new SparseArray<>();
+  public final SparseArray<Node> updatedNodes = new SparseArray<>();
+  public final SparseArray<Long> lastLoopsIDs = new SparseArray<>();
+  public final ProceduralNode.PerformNode parent;
+  public EvaluationContext(ProceduralNode.PerformNode parent) {
+    this.parent = parent;
+  }
+}
---
+++ android/src/main/java/com/swmansion/reanimated/nodes/ProceduralNode.java
@@ -0,0 +1,150 @@
+package com.swmansion.reanimated.nodes;
+
+import android.util.SparseArray;
+
+import com.facebook.react.bridge.ReadableMap;
+import com.swmansion.reanimated.EvaluationContext;
+import com.swmansion.reanimated.NodesManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+public class ProceduralNode extends Node {
+
+  static public class PerformNode extends Node {
+
+    private final int mProceduralNode;
+    private final int[] mArgumentsInputs;
+    EvaluationContext mOldContext = null;
+    EvaluationContext mEvaluationContext = new EvaluationContext(this);
+
+    public PerformNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
+      super(nodeID, config, nodesManager);
+
+      mProceduralNode = config.getInt("proceduralNode");
+      ArrayList proceduralInput = (config.getArray("args").toArrayList());
+      ArrayList<Integer> inputListConverted = new ArrayList<>();
+      for (Object ids : proceduralInput) {
+        if (ids instanceof Double) {
+          inputListConverted.add(((Double) ids).intValue());
+        }
+      }
+      mArgumentsInputs = new int[inputListConverted.size()];
+      int i = 0;
+      for (Integer id : inputListConverted) {
+        mArgumentsInputs[i] = id;
+        i++;
+      }
+    }
+
+    @Override
+    protected Object evaluate(EvaluationContext oldEvaluationContext) {
+      if (mOldContext == null) {
+        mOldContext = oldEvaluationContext;
+        for (int i = 0; i < mArgumentsInputs.length; i++) {
+          int argumentID =
+                  mNodesManager.findNodeById(mProceduralNode, ProceduralNode.class)
+                          .mProceduralArguments[i];
+          ArgumentNode arg = mNodesManager.findNodeById(argumentID, ArgumentNode.class);
+          Node inputNode = mNodesManager.findNodeById(mArgumentsInputs[i], Node.class);
+          arg.matchContextWithValue(
+                  mEvaluationContext,
+                  inputNode
+          );
+          arg.matchValueWithOldContext(
+                  inputNode,
+                  oldEvaluationContext
+          );
+        }
+      } else if (mOldContext != oldEvaluationContext) {
+        throw new IllegalArgumentException("Tried to evaluate perform node in more than one contexts");
+      }
+
+      ProceduralNode proceduralNode = mNodesManager.findNodeById(mProceduralNode, ProceduralNode.class);
+      return proceduralNode.value(mEvaluationContext);
+    }
+  }
+
+  static public class ArgumentNode extends ValueNode {
+
+    private SparseArray<Node> mValuesByContext = new SparseArray<>();
+    private SparseArray<EvaluationContext> mContextsByValue = new SparseArray<>();
+    private SparseArray<EvaluationContext> mOldContextByValue = new SparseArray<>();
+
+    public ArgumentNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
+      super(nodeID, config, nodesManager);
+    }
+
+    public void matchContextWithValue(EvaluationContext context, Node node) {
+      mValuesByContext.put(context.contextID, node);
+      mContextsByValue.put(node.mNodeID, context);
+    }
+
+    public void matchValueWithOldContext(Node node, EvaluationContext context) {
+      mOldContextByValue.put(node.mNodeID, context);
+    }
+
+    @Override
+    public EvaluationContext switchContextWhileUpdatingIfNeeded(EvaluationContext context, Node lastVisited) {
+      if (lastVisited == null) {
+        return context;
+      }
+      return mContextsByValue.get(lastVisited.mNodeID);
+    }
+
+    @Override
+    public void setValue(Double value, EvaluationContext context) {
+      ((ValueNode)mValuesByContext.get(context.contextID)).setValue(value, mNodesManager.mGlobalEvaluationContext);
+    }
+
+    @Override
+    protected Double evaluate(EvaluationContext evaluationContext) {
+      if (evaluationContext == mNodesManager.mGlobalEvaluationContext) {
+        throw new IllegalArgumentException("Tried to evaluate argumentNode in global context");
+      }
+      Node value = mValuesByContext.get(evaluationContext.contextID);
+      return value.doubleValue(mOldContextByValue.get(value.mNodeID));
+    }
+  }
+
+  private final int mResultNode;
+  private final int[] mProceduralArguments;
+
+  public ProceduralNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
+    super(nodeID, config, nodesManager);
+    mResultNode = config.getInt("result");
+    ArrayList anchorInput = (config.getArray("arguments").toArrayList());
+    ArrayList<Integer> proceduralListConverted = new ArrayList<>();
+    for (Object ids : anchorInput) {
+      if (ids instanceof Double) {
+        proceduralListConverted.add(((Double) ids).intValue());
+      }
+    }
+    mProceduralArguments = new int[proceduralListConverted.size()];
+    int i = 0;
+    for (Integer id : proceduralListConverted) {
+      mProceduralArguments[i] = id;
+      i++;
+    }
+  }
+
+  @Override
+  public @Nullable List<Node<?>> getChildrenInContext(EvaluationContext context) {
+    if (context.parent != null) {
+      List<Node<?>> result = new ArrayList<>();
+      result.add(context.parent);
+      return result;
+    }
+
+    return mChildren;
+  }
+
+
+
+  @Override
+  protected Object evaluate(EvaluationContext evaluationContext) {
+    return mNodesManager.findNodeById(mResultNode, Node.class).value(evaluationContext);
+  }
+}
