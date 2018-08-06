657a39969edf39dcfa334970ec73818e8f908ce5
Krzysztof Magiera <krzys@swmansion.com>
2018-08-06 16:14:51 +0200

    Update UIManagerUtils.java

--- android/src/main/java/com/facebook/react/uimanager/UIManagerUtils.java
+++ android/src/main/java/com/facebook/react/uimanager/UIManagerUtils.java
@@ -3,7 +3,6 @@ package com.facebook.react.uimanager;
 /**
  * An class that is made in order to export package method of UIImplementation
  */
-
 public class UIManagerUtils {
   public static UIViewOperationQueue getUIViewOperationQueue(UIImplementation uiImplementation) {
     return uiImplementation.getUIViewOperationQueue();
