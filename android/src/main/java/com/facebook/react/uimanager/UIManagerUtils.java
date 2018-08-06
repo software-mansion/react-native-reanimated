249b60dc7620e262d242eb730eaa6dd4bc1b6292
Krzysztof Magiera <krzys@swmansion.com>
2018-08-06 16:18:54 +0200

    Update UIManagerUtils.java

--- android/src/main/java/com/facebook/react/uimanager/UIManagerUtils.java
+++ android/src/main/java/com/facebook/react/uimanager/UIManagerUtils.java
@@ -1,7 +1,12 @@
 package com.facebook.react.uimanager;
 
 /**
- * An class that is made in order to export package method of UIImplementation
+ * This class provides a workaround that makes it possible to access UIViewOperationQueue.
+ * In react-native core UIViewOperationQueue#getUIViewOperationQueue has limited visibility
+ * to package only. We rely on that method to check if queue is empty or not. If the queue is
+ * empty we want to trigger "onBatchComplete" in the code responsible for updating native props.
+ * Otherwise, if it is not empty it means that there is an active batch and so we can rely on
+ * "onBatchComplete" being triggered once the active batch is finished.
  */
 public class UIManagerUtils {
   public static UIViewOperationQueue getUIViewOperationQueue(UIImplementation uiImplementation) {
