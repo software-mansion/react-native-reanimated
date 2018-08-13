9d4c39a8396e990822505462a67a5b6d8f33e123
osdnk <micosa97@gmail.com>
2018-08-13 13:35:26 +0200

    Add

--- ios/Nodes/REANode.m
+++ ios/Nodes/REANode.m
@@ -1,30 +1,9 @@
 #import "REANode.h"
 #import "REANodesManager.h"
 #import "REAProceduralNode.h"
+#import "REAEvalContext.h"
 
 #import <React/RCTDefines.h>
-
-@interface REAEvalContext()
-@property (nonatomic, nonnull) NSMutableArray< REANode * >  *updatedNodes;
-@end
-
-
-@implementation REAEvalContext
-static int _nextContextID = 0;
-
-- (instancetype)initWithParent:(REAPerformNode *) parent
-{
-  if ((self = [super init])) {
-    _parent = _parent;
-    _updatedNodes = [NSMutableArray new];
-    _memoizedValues = [NSMutableDictionary new];
-    _lastLoopIDs = [NSMutableDictionary new];
-    _contextID = [NSNumber numberWithInt:_nextContextID++];
-  }
-  return self;
-}
-
-@end
 
 
 @implementation REANode
--- ios/REAEvalContext.h
+++ ios/REAEvalContext.h
@@ -2,11 +2,13 @@
 #import <UIKit/UIKit.h>
 
 @class REAPerformNode;
+@class REANode;
 
 @interface REAEvalContext : NSObject
 
 - (instancetype)initWithParent:(REAPerformNode *) parent;
 
+@property (nonatomic, nonnull) NSMutableArray< REANode * >  *updatedNodes;
 @property (nonatomic) NSMutableDictionary<NSNumber *, id> *__strong memoizedValues;
 @property (nonatomic) NSMutableDictionary<NSNumber *, NSNumber *> *__strong lastLoopIDs;
 @property (nonatomic, nonnull) NSNumber *contextID;
