1bdd41523d0bddf7283469a1469b991424b9f614
Michał Osadnik <micosa97@gmail.com>
2018-08-14 16:13:04 +0200

    Update REAConcatNode.m

--- ios/Nodes/REAConcatNode.m
+++ ios/Nodes/REAConcatNode.m
@@ -18,7 +18,7 @@
 {
   NSMutableString *result = [NSMutableString new];
   for (int i = 0; i < _input.count; i++) {
-    NSObject * val = [[self.nodesManager findNodeByID:_input[i]] value];
+    NSObject *val = [[self.nodesManager findNodeByID:_input[i]] value];
     if ([val isKindOfClass:[NSNumber class]]) {
       [result appendString:[(NSNumber *)val stringValue]];
     }
