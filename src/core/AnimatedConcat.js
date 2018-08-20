6f3ff5b3f8103856d66db72c863925530f485250
Krzysztof Magiera <krzys.magiera@gmail.com>
2018-08-20 15:36:27 +0200

    Update AnimatedConcat.js

--- src/core/AnimatedConcat.js
+++ src/core/AnimatedConcat.js
@@ -2,7 +2,6 @@ import AnimatedNode from './AnimatedNode';
 
 export default class AnimatedConcat extends AnimatedNode {
   constructor(input) {
-    console.log(input);
     super({ type: 'concat', input: input.map(n => n.__nodeID) }, input);
   }
 }
