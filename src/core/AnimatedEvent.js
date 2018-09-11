32b231171e546b7cd174f312e08ce795e8dcccd3
Krzysztof Magiera <krzys@swmansion.com>
2018-09-11 14:17:14 +0200

    Update AnimatedEvent.js

--- src/core/AnimatedEvent.js
+++ src/core/AnimatedEvent.js
@@ -38,8 +38,8 @@ export default class AnimatedEvent extends AnimatedNode {
   }
   
   // The below field is a temporary workaround to make AnimatedEvent object be recognized
-  // and filtered out by gesture handler library that relies on it being available when
-  // Animated.event is used instead of a callback for processing events.
+  // as Animated.event event callback and therefore filtered out from being send over the
+  // bridge which was causing the object to be frozen in JS.
   __isNative = true;
 
   attachEvent(viewRef, eventName) {
