c3bc5bb891d14122208608a7b39ddbb3a865337d
Michał Osadnik <micosa97@gmail.com>
2018-09-20 13:20:23 +0200

    Update delay.js

--- src/derived/delay.js
+++ src/derived/delay.js
@@ -1,7 +1,6 @@
 import { add, block, cond, defined, greaterOrEq, set } from '../base';
 import { default as Value } from '../core/AnimatedValue';
 export default function delay(clock, time, node, nodeBefore = 0) {
-  console.log(clock, time, node, nodeBefore);
   const when = new Value();
   return block([
     cond(defined(when), 0, [set(when, add(clock, time))]),
