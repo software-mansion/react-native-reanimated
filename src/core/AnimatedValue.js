bff5c8bc2f58a5a531de0035fc8ddf3278d6a132
Michał Osadnik <micosa97@gmail.com>
2018-09-20 15:05:02 +0200

    Update AnimatedValue.js

--- src/core/AnimatedValue.js
+++ src/core/AnimatedValue.js
@@ -19,10 +19,6 @@ export default class AnimatedValue extends AnimatedNode {
     this._animation = null;
   }
 
-  __attach() {
-    super.__attach();
-  }
-
   __detach() {
     ReanimatedModule.getValue(
       this.__nodeID,
