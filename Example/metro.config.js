736f8ac711b950438f1dc75a3954042ccd6be018
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-10-09 12:07:52 +0100

    fix for metro config

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -2,15 +2,12 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 const path = require('path');
 const cwd = path.resolve(__dirname);
 
-const pak = require('../package.json');
 const glob = require('glob-to-regexp');
-
-const dependencies = Object.keys(pak.dependencies);
-const peerDependencies = Object.keys(pak.peerDependencies);
 
 function getBlacklist() {
   const nodeModuleDirs = [
     glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
   ];
   return blacklist(nodeModuleDirs);
 }
@@ -18,9 +15,7 @@ function getBlacklist() {
 module.exports = {
   resolver: {
     blacklistRE: getBlacklist(),
-    extraNodeModules() {
-      return [...dependencies, ...peerDependencies];
-    },
+    providesModuleNodeModules: ['react-native', 'react', 'fbjs'],
   },
   watchFolders: [path.resolve(__dirname, '..')],
 };
