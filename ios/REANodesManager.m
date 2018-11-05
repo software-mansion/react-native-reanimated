7c5444b959e7113bf342b4ae0c33807c8e41d55f
Michał Osadnik <micosa97@gmail.com>
2018-11-05 14:11:47 +0100

    Add missing semicolon (#129)

--- ios/REANodesManager.m
+++ ios/REANodesManager.m
@@ -193,7 +193,7 @@
 {
   id val = _nodes[nodeID].value;
   if (val) {
-    callback(@[val])
+    callback(@[val]);
   } else {
     // NULL is not an object and it's not possible to pass it as callback's argument
     callback(@[[NSNull null]]);
