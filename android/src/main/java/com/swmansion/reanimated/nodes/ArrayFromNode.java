f4689b03b4f6abeca9935d48ef934119a7931214
Michał Mokijewski <hello@naf.gl>
2018-11-12 15:33:49 +0100

    add handling for other types than double in arrayFrom node

--- android/src/main/java/com/swmansion/reanimated/nodes/ArrayFromNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/ArrayFromNode.java
@@ -2,6 +2,7 @@ package com.swmansion.reanimated.nodes;
 
 import com.facebook.react.bridge.JavaOnlyArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.swmansion.reanimated.NodesManager;
 import com.swmansion.reanimated.Utils;
 
@@ -18,7 +19,22 @@ public class ArrayFromNode extends Node {
     protected Object evaluate() {
         JavaOnlyArray res = new JavaOnlyArray();
         for (int el: mArrayFrom) {
-            res.pushDouble((Double) mNodesManager.findNodeById(el, Node.class).value());
+            Object value = mNodesManager.findNodeById(el, Node.class).value();
+
+            // it's here to match objc implementation that can handle any type
+            if (value == null) {
+                res.pushNull();
+            } else if (value instanceof Number) {
+                res.pushDouble((Double) value);
+            } else if (value instanceof String) {
+                res.pushString((String) value);
+            } else if (value instanceof Boolean) {
+                res.pushBoolean((Boolean) value);
+            } else if (value.getClass().isArray()) {
+                res.pushArray((WritableArray) value);
+            } else {
+                throw new IllegalArgumentException("Unexpected type " + value.getClass().getName() + " inside arrayFrom node");
+            }
         }
         return res;
     }
