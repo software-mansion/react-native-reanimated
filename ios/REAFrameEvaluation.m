568bbdc4f276d42a04010de5c06160adaa68ffe2
osdnk <micosa97@gmail.com>
2018-12-02 14:27:51 +0100

    Extract classes

--- ios/REAFrameEvaluation.m
+++ ios/REAFrameEvaluation.m
@@ -5,84 +5,81 @@
 
 @implementation REAFrameEvaluation {
   REANodesManager *_nodesManager;
+  NSMutableArray<id<REAFinalNode>> *_finalNodes;
+  NSMutableArray<REAEvalContext *> *_contexts;
+  NSMutableDictionary<NSNumber*, NSMutableSet<REANode*>*> *_visitedNodes;
 }
 -(instancetype)initWithNodesManager:(REANodesManager *)nodesNamanger
 {
   if (self = [super init]) {
     _nodesManager = nodesNamanger;
+    _visitedNodes = [NSMutableDictionary new];
+    _finalNodes = [NSMutableArray new];
+    _contexts = [NSMutableArray new];
   }
   return self;
 }
 
 - (void)findAndUpdateNodes:(nonnull REANode *)node
-            withVisitedSet:(NSMutableDictionary<NSNumber*, NSMutableSet<REANode*>*> *)visitedNodes
-            withFinalNodes:(NSMutableArray<id<REAFinalNode>> *)finalNodes
-        withStackedContext:(NSMutableArray<REAEvalContext *> *)contexts
        withLastVisitedNode:(REANode *)lastVisited
 {
-  REAEvalContext *currentContext = contexts.lastObject;
-  if (visitedNodes[currentContext.contextID]) {
-    if ([visitedNodes[currentContext.contextID] containsObject:node]) {
+  REAEvalContext *currentContext = _contexts.lastObject;
+  if (_visitedNodes[currentContext.contextID]) {
+    if ([_visitedNodes[currentContext.contextID] containsObject:node]) {
       return;
     }
   } else {
-    visitedNodes[currentContext.contextID] = [NSMutableSet new];
+    _visitedNodes[currentContext.contextID] = [NSMutableSet new];
   }
-  [visitedNodes[currentContext.contextID] addObject:node];
+  [_visitedNodes[currentContext.contextID] addObject:node];
   NSMutableArray* children = node.childNodes;
   REAEvalContext *newContext = [node switchContextWhileUpdatingIfNeeded:currentContext withLastVisitedNode:lastVisited];
   BOOL pushedNewContext = false;
   REAEvalContext *__nullable contextPopped = NULL;
   
   if (newContext != currentContext && currentContext) {
-    [contexts addObject:newContext];
+    [_contexts addObject:newContext];
     pushedNewContext = true;
   }
   
-  if ([node isKindOfClass:[REAPerformNode class]] && contexts.count > 1) {
-    contextPopped = [contexts lastObject];
-    [contexts removeLastObject];
+  if ([node isKindOfClass:[REAPerformNode class]] && _contexts.count > 1) {
+    contextPopped = [_contexts lastObject];
+    [_contexts removeLastObject];
   }
   if ([node isKindOfClass:[REAProceduralNode class]] && currentContext.parent) {
-    [self findAndUpdateNodes:currentContext.parent withVisitedSet:visitedNodes withFinalNodes:finalNodes withStackedContext:contexts withLastVisitedNode:node];
+    [self findAndUpdateNodes:currentContext.parent withLastVisitedNode:node];
   } else if (children) {
     for (REANode *child in children) {
-      [self findAndUpdateNodes:child withVisitedSet:visitedNodes withFinalNodes:finalNodes withStackedContext:contexts withLastVisitedNode:node];
+      [self findAndUpdateNodes:child withLastVisitedNode:node];
     }
   }
   
   if ([node conformsToProtocol:@protocol(REAFinalNode)]) {
-    [finalNodes addObject:(id<REAFinalNode>)node];
+    [_finalNodes addObject:(id<REAFinalNode>)node];
   }
   
   if (pushedNewContext) {
-    [contexts removeLastObject];
+    [_contexts removeLastObject];
   }
   
   if (contextPopped) {
-    [contexts addObject:contextPopped];
+    [_contexts addObject:contextPopped];
   }
 }
 
 - (void)runPropUpdates
 {
-  NSMutableDictionary<NSNumber*, NSMutableSet<REANode*>*> *visitedNodes = [NSMutableDictionary new];
-  NSMutableArray<id<REAFinalNode>> *finalNodes = [NSMutableArray new];
-  NSMutableArray<REAEvalContext *> *contexts = [NSMutableArray new];
-  [contexts addObject:_nodesManager.globalEvalContext];
+  [_contexts addObject:_nodesManager.globalEvalContext];
   for (NSUInteger i = 0; i < _nodesManager.globalEvalContext.updatedNodes.count; i++) {
     [self findAndUpdateNodes:_nodesManager.globalEvalContext.updatedNodes[i]
-              withVisitedSet:visitedNodes
-              withFinalNodes:finalNodes
-          withStackedContext:contexts
          withLastVisitedNode:NULL
      ];
-    RCTAssert(contexts.count == 1, @"Stacking of contexts was not performed correctly");
+    RCTAssert(_contexts.count == 1, @"Stacking of contexts was not performed correctly");
     if (i == _nodesManager.globalEvalContext.updatedNodes.count - 1) {
-      while (finalNodes.count > 0) {
+      while (_finalNodes.count > 0) {
         // NSMutableArray used for stack implementation
-        [[finalNodes lastObject] update];
-        [finalNodes removeLastObject];
+        [[_finalNodes lastObject] update];
+        [_finalNodes removeLastObject];
       }
     }
   }
