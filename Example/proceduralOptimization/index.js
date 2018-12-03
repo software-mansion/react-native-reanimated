5163cbe92d9ddcd09f59ba53503fe5de50a661ae
osdnk <micosa97@gmail.com>
2018-12-03 01:36:12 +0100

    Add solar system

--- Example/proceduralOptimization/index.js
+++ Example/proceduralOptimization/index.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Animated, { Easing } from 'react-native-reanimated';
 
 const {
   set,
   cond,
-  sub,
-  pow,
-  add,
   multiply,
   startClock,
   stopClock,
   debug,
-  sqrt,
   sin,
   cos,
   clockRunning,
@@ -54,102 +50,265 @@ function runTiming(clock, value, dest, time) {
 }
 
 export default class Example extends Component {
+  static navigationOptions = {
+    title: 'Native solar system',
+    headerTitleStyle: {
+      color: 'white',
+    },
+    headerStyle: {
+      backgroundColor: 'black',
+    },
+  };
   constructor(props) {
     super(props);
-    const trans = runTiming(new Clock(), new Value(0), 120, 5000);
-    const xCircle = proc((progress, speed, radius) =>
-      multiply(add(sin(multiply(progress, speed)), 1), radius)
+    this.trans = runTiming(new Clock(), new Value(0), 100, 50000);
+    this.ellipsis = proc((progress, speed, radius, ratio, x, y) =>
+      block([
+        set(x, multiply(sin(multiply(progress, speed)), radius)),
+        set(y, multiply(cos(multiply(progress, speed)), radius, ratio)),
+      ])
     );
-    const yCircle = proc((progress, speed, radius) =>
-      multiply(add(cos(multiply(progress, speed)), 1), radius)
-    );
-    const asCircle = (progress, speed, radius) => [
-      xCircle(progress, speed, radius),
-      yCircle(progress, speed, radius),
-    ];
-    const [XA, YA] = asCircle(trans, 2, 10);
-    this.XA = XA;
-    this.YA = YA;
-    const [XB, YB] = asCircle(trans, 1.2, 20);
-    this.XB = XB;
-    this.YB = YB;
-    const [XC, YC] = asCircle(trans, 0.3, 40);
-    this.XC = XC;
-    this.YC = YC;
+    this.traversePlanets = planets =>
+      planets.map(p => ({
+        ...p,
+        x: new Value(0),
+        y: new Value(0),
+        satellites: p.satellites && this.traversePlanets(p.satellites),
+      }));
+    this.planets = this.traversePlanets(PLANETS);
   }
+  renderPlanets = planets => (
+    <React.Fragment>
+      {planets.map((p, k) => (
+        <React.Fragment key={`planet ${k}`}>
+          <Animated.Code
+            exec={this.ellipsis(
+              this.trans,
+              p.speed,
+              p.radius,
+              p.ratio,
+              p.x,
+              p.y
+            )}
+          />
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                width: p.size,
+                backgroundColor: p.color,
+                height: p.size,
+                borderRadius: p.size,
+                transform: [
+                  {
+                    translateX: p.x,
+                  },
+                  {
+                    translateY: p.y,
+                  },
+                ],
+              },
+            ]}>
+            {!!p.satellites && this.renderPlanets(p.satellites)}
+          </Animated.View>
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  );
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <Animated.View
-          style={[
-            styles.box,
-            {
-              transform: [
-                {
-                  translateX: this.XA,
-                  translateY: this.YA,
-                },
-              ],
-            },
-          ]}
-        />
-        <Animated.View
-          style={[
-            styles.box,
-            {
-              transform: [
-                {
-                  translateX: this.XB,
-                  translateY: this.YB,
-                },
-              ],
-            },
-          ]}
-        />
-        <Animated.View
-          style={[
-            styles.box,
-            {
-              transform: [
-                {
-                  translateX: this.XC,
-                  translateY: this.YC,
-                },
-              ],
-            },
-          ]}
-        />
-        <Animated.View
-          style={[
-            styles.box,
-            {
-              transform: [
-                {
-                  translateX: add(this.XC, this.XB, this.XC),
-                  translateY: add(this.YC, this.YB, this.Y),
-                },
-              ],
-            },
-          ]}
-        />
-      </ScrollView>
+      <View style={styles.container}>{this.renderPlanets(this.planets)}</View>
     );
   }
 }
 
-const BOX_SIZE = 20;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
   },
   box: {
-    width: BOX_SIZE,
-    height: BOX_SIZE,
+    position: 'absolute',
     borderColor: '#F5FCFF',
     backgroundColor: 'plum',
-    margin: BOX_SIZE / 2,
-    borderRadius: BOX_SIZE / 2,
   },
 });
+
+const PLANETS = [
+  {
+    radius: 2,
+    size: 20,
+    speed: 0.1,
+    ratio: 1,
+    rotation: '0deg',
+    color: '#c7b755',
+  },
+  {
+    radius: 25,
+    size: 5,
+    speed: 2,
+    ratio: 1,
+    rotation: '0deg',
+    color: '#c74e43',
+  },
+  {
+    radius: 35,
+    size: 5,
+    speed: 2.2,
+    ratio: 1.2,
+    rotation: '12deg',
+    color: '#4e76c7',
+  },
+  {
+    radius: 45,
+    size: 5,
+    speed: 1.7,
+    ratio: 1.1,
+    rotation: '50deg',
+    color: '#0ec700',
+  },
+  {
+    radius: 43,
+    size: 5,
+    speed: 1,
+    ratio: 3,
+    rotation: '120deg',
+    color: '#c76c03',
+  },
+  {
+    radius: 30,
+    size: 12,
+    speed: 0.6,
+    ratio: 1.4,
+    rotation: '40deg',
+    color: '#c741a4',
+  },
+  {
+    radius: 50,
+    size: 32,
+    speed: 1.2,
+    ratio: 2,
+    rotation: '120deg',
+    color: '#c78213',
+    satellites: [
+      {
+        radius: 22,
+        size: 8,
+        speed: 1,
+        ratio: 1.3,
+        rotation: '15deg',
+        color: '#c70035',
+      },
+      {
+        radius: 35,
+        size: 12,
+        speed: 1.4,
+        ratio: 0.8,
+        rotation: '112deg',
+        color: '#c6c79b',
+      },
+    ],
+  },
+  {
+    radius: 100,
+    size: 42,
+    speed: 1,
+    ratio: 1.1,
+    rotation: '40deg',
+    color: '#c7b342',
+  },
+  {
+    radius: 200,
+    size: 82,
+    speed: 1.5,
+    ratio: 1.1,
+    rotation: '120deg',
+    color: '#001ac7',
+    satellites: [
+      {
+        radius: 42,
+        size: 20,
+        speed: 1,
+        ratio: 1,
+        rotation: '0deg',
+        color: '#c7c7e5',
+      },
+      {
+        radius: 35,
+        size: 12,
+        speed: 1.4,
+        ratio: 0.8,
+        rotation: '152deg',
+        color: '#c6c79b',
+      },
+      {
+        radius: 42,
+        size: 8,
+        speed: 1.1,
+        ratio: 1.3,
+        rotation: '55deg',
+        color: '#c7be6f',
+      },
+      {
+        radius: 35,
+        size: 15,
+        speed: 1.8,
+        ratio: 0.8,
+        rotation: '132deg',
+        color: '#73304a',
+      },
+      {
+        radius: 42,
+        size: 4,
+        speed: 1,
+        ratio: 1,
+        rotation: '0deg',
+        color: '#dbdbff',
+      },
+      {
+        radius: 35,
+        size: 3,
+        speed: 1.4,
+        ratio: 0.8,
+        rotation: '152deg',
+        color: '#c6c79b',
+      },
+      {
+        radius: 42,
+        size: 7,
+        speed: 1.1,
+        ratio: 1.3,
+        rotation: '55deg',
+        color: '#c7be6f',
+        satellites: [
+          {
+            radius: 40,
+            size: 8,
+            speed: 1,
+            ratio: 1,
+            rotation: '0deg',
+            color: '#c70035',
+          },
+          {
+            radius: 50,
+            size: 12,
+            speed: 2,
+            ratio: 1,
+            rotation: '0deg',
+            color: '#c6c79b',
+          },
+        ],
+      },
+      {
+        radius: 35,
+        size: 5,
+        speed: 1.8,
+        ratio: 0.8,
+        rotation: '132deg',
+        color: '#5b730d',
+      },
+    ],
+  },
+];
