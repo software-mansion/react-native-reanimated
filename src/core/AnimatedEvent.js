7562f48c33844e09fb6f35a774eef99d773899dc
Christian Bach <christianbach@users.noreply.github.com>
2018-12-06 20:40:25 +0100

    Check if typeof Proxy is a function

--- src/core/AnimatedEvent.js
+++ src/core/AnimatedEvent.js
@@ -1,4 +1,4 @@
-import { findNodeHandle, Platform } from 'react-native';
+import { findNodeHandle } from 'react-native';
 import ReanimatedModule from '../ReanimatedModule';
 
 import AnimatedNode from './AnimatedNode';
@@ -58,9 +58,9 @@ function sanitizeArgMapping(argMapping) {
     };
 
     const proxy =
-      Platform.OS === 'android'
-        ? createEventObjectProxyPolyfill()
-        : new Proxy({}, proxyHandler);
+      typeof Proxy === 'function'
+          ? new Proxy({}, proxyHandler)
+          : createEventObjectProxyPolyfill();
     alwaysNodes.push(new AnimatedAlways(ev(proxy)));
     traverse(proxy, []);
   }
