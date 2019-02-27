83b322421fcd0f288de1476caa4ba73ce25f0754
osdnk <micosa97@gmail.com>
2019-02-27 15:06:16 +0100

    Handle if call's input node is string

--- android/src/main/java/com/swmansion/reanimated/nodes/JSCallNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/JSCallNode.java
@@ -19,12 +19,17 @@ public class JSCallNode extends Node {
   @Override
   protected Double evaluate() {
     WritableArray args = Arguments.createArray();
-    for (int i = 0; i < mInputIDs.length; i++) {
-      Node node = mNodesManager.findNodeById(mInputIDs[i], Node.class);
+    for (int mInputID : mInputIDs) {
+      Node node = mNodesManager.findNodeById(mInputID, Node.class);
       if (node.value() == null) {
         args.pushNull();
       } else {
-        args.pushDouble(node.doubleValue());
+        Object value = node.value();
+        if (value instanceof String) {
+          args.pushString((String) value);
+        } else {
+          args.pushDouble(node.doubleValue());
+        }
       }
     }
     WritableMap eventData = Arguments.createMap();
