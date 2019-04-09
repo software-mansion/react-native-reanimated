4bf40fb429af40363068ee2125cce6fcbe5c8475
Mateo Hrastnik <hrastnikmateo@gmail.com>
2019-04-09 23:17:14 +0200

    Improve Animated.Code errors Add improved error messages to Animated.Code when wrong props are passed to the component. Also, unified the types both the exec and children can accept. Both exec and children can now accept a node or a function returning a node.

--- src/core/AnimatedCode.js
+++ src/core/AnimatedCode.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import AnimatedAlways from './AnimatedAlways';
+import AnimatedNode from './AnimatedNode';
 
 class Code extends React.Component {
+  static resolveNode = maybeNode => {
+    if (typeof maybeNode === 'function') {
+      return Code.resolveNode(maybeNode());
+    }
+
+    if (maybeNode instanceof AnimatedNode) {
+      return maybeNode;
+    }
+
+    return null;
+  };
+
   componentDidMount() {
-    this.always = new AnimatedAlways(
-      this.props.exec ? this.props.exec : this.props.children()
-    );
+    const { children, exec } = this.props;
+    const nodeChildren = Code.resolveNode(children);
+    const nodeExec = Code.resolveNode(exec);
+
+    const cantResolveNode = nodeChildren === null && nodeExec === null;
+
+    if (cantResolveNode) {
+      const error =
+        nodeChildren === null
+          ? `Got "${typeof children}" type passed to children`
+          : `Got "${typeof exec}" type passed to exec`;
+
+      throw new Error(
+        `<Animated.Code /> expects the 'exec' prop or children to be an animated node or a function returning an animated node. ${error}`
+      );
+    }
+
+    this.always = new AnimatedAlways(nodeExec || nodeChildren);
     this.always.__attach();
   }
 
