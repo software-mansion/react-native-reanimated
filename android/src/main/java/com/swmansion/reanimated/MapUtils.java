50a91de0736043ffd16912030d460ec0f00db8dc
Jakub Adamczyk <jakub.adamczyk@swmansion.com>
2019-04-19 14:02:25 +0200

    delete future proofed code not being used

--- android/src/main/java/com/swmansion/reanimated/MapUtils.java
+++ android/src/main/java/com/swmansion/reanimated/MapUtils.java
@@ -1,31 +1,13 @@
 package com.swmansion.reanimated;
 
-import com.facebook.react.bridge.Dynamic;
 import com.facebook.react.bridge.JSApplicationCausedNativeException;
 import com.facebook.react.bridge.NoSuchKeyException;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class MapUtils {
-  public static boolean getBoolean(ReadableMap map,  @Nonnull String name, String errorMsg) {
-    try {
-      return map.getBoolean(name);
-    } catch (NoSuchKeyException e) {
-      throw new JSApplicationCausedNativeException(errorMsg);
-    }
-  }
-
-  public static double getDouble(ReadableMap map,  @Nonnull String name, String errorMsg) {
-    try {
-      return map.getDouble(name);
-    } catch (NoSuchKeyException e) {
-      throw new JSApplicationCausedNativeException(errorMsg);
-    }
-  }
-
   public static int getInt(ReadableMap map,  @Nonnull String name, String errorMsg) {
     try {
       return map.getInt(name);
@@ -40,33 +22,6 @@ public class MapUtils {
       return map.getString(name);
     } catch (NoSuchKeyException e) {
       throw new JSApplicationCausedNativeException("Reanimated: " + errorMsg);
-    }
-  }
-
-  @Nullable
-  public static ReadableArray getArray(ReadableMap map,  @Nonnull String name, String errorMsg) {
-    try {
-      return map.getArray(name);
-    } catch (NoSuchKeyException e) {
-      throw new JSApplicationCausedNativeException(errorMsg);
-    }
-  }
-
-  @Nullable
-  public static ReadableMap getMap(ReadableMap map,  @Nonnull String name, String errorMsg) {
-    try {
-      return map.getMap(name);
-    } catch (NoSuchKeyException e) {
-      throw new JSApplicationCausedNativeException(errorMsg);
-    }
-  }
-
-  @Nonnull
-  public static Dynamic getDynamic(ReadableMap map,  @Nonnull String name, String errorMsg) {
-    try {
-      return map.getDynamic(name);
-    } catch (NoSuchKeyException e) {
-      throw new JSApplicationCausedNativeException(errorMsg);
     }
   }
 }
