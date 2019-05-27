430c97259a5b0a5a5940edecee107923fb714cd4
Michał Osadnik <micosa97@gmail.com>
2019-05-27 17:46:08 +0200

    Get rid of useless cond

--- ios/REAUtils.h
+++ ios/REAUtils.h
@@ -1,7 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#ifndef REA_LOG_ERROR_IF_NIL
 #define REA_LOG_ERROR_IF_NIL(value, errorMsg, nodeID) ({\
   if (value == nil) RCTLogError(errorMsg, nodeID);\
 })
-#endif
