4283e333fa2435e4eac608c2b780c8c360216115
Simon Buerger <simon@bluegrassdigital.com>
2019-07-05 12:22:29 +0200

    Fix Double whole numbers being stringified as eg 34.0

--- android/src/main/java/com/swmansion/reanimated/nodes/ConcatNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/ConcatNode.java
@@ -3,8 +3,12 @@ package com.swmansion.reanimated.nodes;
 import com.facebook.react.bridge.ReadableMap;
 import com.swmansion.reanimated.NodesManager;
 import com.swmansion.reanimated.Utils;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
 
 public class ConcatNode extends Node {
+  private NumberFormat nf = new DecimalFormat("##.###");
+
   private final int[] mInputIDs;
 
   public ConcatNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
@@ -17,7 +21,12 @@ public class ConcatNode extends Node {
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < mInputIDs.length; i++) {
       Node inputNodes = mNodesManager.findNodeById(mInputIDs[i], Node.class);
-      builder.append(inputNodes.value());
+      Object value = inputNodes.value();
+      if (value instanceof Double) {
+        builder.append(nf.format(value));
+      } else {
+        builder.append(value);
+      }
     }
     return builder.toString();
   }
