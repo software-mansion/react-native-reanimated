13334cd23084092ea1ab6eb898fd015af62faca5
Christian Falch <christian.falch@gmail.com>
2019-07-27 18:47:41 +0200

    Added setvalue support for ParamNode.

--- android/src/main/java/com/swmansion/reanimated/nodes/ParamNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/ParamNode.java
@@ -6,7 +6,7 @@ import com.swmansion.reanimated.NodesManager;
 
 import java.util.Stack;
 
-public class ParamNode extends Node {
+public class ParamNode extends ValueNode {
 
     private int mRefNode;
     private String mName;
@@ -17,6 +17,17 @@ public class ParamNode extends Node {
         mRefNode = 0;
         mName = config.getString("name");
         mStack = new Stack<Object>();
+    }
+
+    @Override
+    public void setValue(Object value) {
+        Node node = mNodesManager.findNodeById(mRefNode, Node.class);
+        if(node != null) {
+            // Check instance
+            if(node instanceof ValueNode) {
+                ((ValueNode)node).setValue(value);
+            }
+        }
     }
 
     public void beginContext(int ref) {
