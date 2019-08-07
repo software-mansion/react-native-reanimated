aa5b2f5c63d2140981d8332998dc2258a0f34724
William Candillon <wcandillon@gmail.com>
2019-08-07 09:46:09 +0200

    fixes #316

--- src/derived/floor.js
+++ src/derived/floor.js
@@ -1,5 +1,6 @@
 import { sub, round } from '../base';
 
 export default function floor(a) {
-  return round(sub(a, 0.5));
+  const b = round(a);
+  return cond(lessThan(a, b), round(sub(a, 0.5)), b);
 }
