ad1a6e9ed05f13fa61a5ba55c519ca6a1a39a25a
Dulmandakh Sukhbaatar <dulmandakh@gmail.com>
2019-09-20 23:00:53 +0800

    bump react-native in Example to 0.60.5, and AndroidX

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,21 +1,17 @@
-const blacklist = require('metro-config/src/defaults/blacklist');
-const path = require('path');
-const cwd = path.resolve(__dirname);
-
-const glob = require('glob-to-regexp');
-
-function getBlacklist() {
-  const nodeModuleDirs = [
-    glob(`${process.platform === 'win32' ? path.resolve(__dirname, '..').replace(/\\/g, '/') : path.resolve(__dirname, '..')}/node_modules/*`),
-    glob(`${process.platform === 'win32' ? path.resolve(__dirname).replace(/\\/g, '/') : path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
-  ];
-  return blacklist(nodeModuleDirs);
-}
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
 module.exports = {
-  resolver: {
-    blacklistRE: getBlacklist(),
-    providesModuleNodeModules: ['react-native', 'react', 'fbjs'],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
-  watchFolders: [path.resolve(__dirname, '..')],
 };
