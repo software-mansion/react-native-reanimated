4295232af6c883bde293c80fd011b3fa4cad214b
Dulmandakh <dulmandakh@gmail.com>
2019-09-24 11:07:21 +0800

    revert metro.config.js

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,17 +1,21 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+const cwd = path.resolve(__dirname);
+
+const glob = require('glob-to-regexp');
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${process.platform === 'win32' ? path.resolve(__dirname, '..').replace(/\\/g, '/') : path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(`${process.platform === 'win32' ? path.resolve(__dirname).replace(/\\/g, '/') : path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
+  ];
+  return blacklist(nodeModuleDirs);
+}
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+  resolver: {
+    blacklistRE: getBlacklist(),
+    providesModuleNodeModules: ['react-native', 'react', 'fbjs'],
   },
+  watchFolders: [path.resolve(__dirname, '..')],
 };
