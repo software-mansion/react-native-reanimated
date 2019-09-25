cb9939328b2999a5821c7ebbe52c65c4a43cf041
Krzysztof Magiera <krzys@swmansion.com>
2019-09-25 09:52:08 +0200

    Trying to update to RN 0.61

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,13 +1,18 @@
 const blacklist = require('metro-config/src/defaults/blacklist');
 const path = require('path');
-const cwd = path.resolve(__dirname);
 
 const glob = require('glob-to-regexp');
 
 function getBlacklist() {
   const nodeModuleDirs = [
-    glob(`${process.platform === 'win32' ? path.resolve(__dirname, '..').replace(/\\/g, '/') : path.resolve(__dirname, '..')}/node_modules/*`),
-    glob(`${process.platform === 'win32' ? path.resolve(__dirname).replace(/\\/g, '/') : path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
+    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(`${path.resolve(__dirname, '..')}/e2e/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
+    glob(
+      `${path.resolve(
+        __dirname
+      )}/node_modules/*/node_modules/hoist-non-react-statics/*`
+    ),
   ];
   return blacklist(nodeModuleDirs);
 }
@@ -15,7 +20,6 @@ function getBlacklist() {
 module.exports = {
   resolver: {
     blacklistRE: getBlacklist(),
-    providesModuleNodeModules: ['react-native', 'react', 'fbjs'],
   },
   watchFolders: [path.resolve(__dirname, '..')],
 };
