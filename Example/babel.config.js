63d3701da99f8ef799500986bc88fccab1026215
osdnk <micosa97@gmail.com>
2019-09-25 11:09:38 +0200

    fix

--- Example/babel.config.js
+++ Example/babel.config.js
@@ -3,11 +3,18 @@ module.exports = api => {
   return {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
+      "@babel/plugin-transform-modules-commonjs",
       [
         'module-resolver',
         {
           alias: {
-            'react-native-reanimated': '../src/Animated',
+            "react-native-reanimated": "../src/Animated",
+            "react": "./node_modules/react",
+            "react-native": "./node_modules/react-native",
+            "fbjs": "./node_modules/fbjs",
+            "hoist-non-react-statics": "./node_modules/hoist-non-react-statics",
+            "invariant": "./node_modules/invariant",
+            "prop-types": "./node_modules/prop-types"
           },
         },
       ],
