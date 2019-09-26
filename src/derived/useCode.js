a60d4d89cb695cf8dd0410fca46d21b4a1339eac
William Candillon <wcandillon@gmail.com>
2019-09-26 17:35:33 +0200

    💚

--- src/derived/useCode.js
+++ src/derived/useCode.js
@@ -4,7 +4,7 @@ import { always } from '../base';
 function useCode(exec, deps) {
   if (typeof React.useEffect === 'function') {
     React.useEffect(() => {
-      const animatedAlways = always(typeof React.useEffect === 'function' ? exec() : exec);
+      const animatedAlways = always(typeof exec === 'function' ? exec() : exec);
       animatedAlways.__attach();
       return () => {
         animatedAlways.__detach();
