357de8415aacf871727518ccb07420f285e91a67
William Candillon <wcandillon@gmail.com>
2019-09-26 22:05:02 +0200

    Update floor.js

--- src/derived/floor.js
+++ src/derived/floor.js
@@ -1,6 +1,5 @@
 import { sub, round } from '../base';
 
 export default function floor(a) {
-  const b = round(a);
-  return cond(lessThan(a, b), round(sub(a, 0.5)), b);
+  return round(sub(a, 0.5 - Number.MIN_VALUE));
 }
