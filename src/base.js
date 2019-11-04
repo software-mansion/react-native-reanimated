07a4b1fd59f3a871c2f4ef5128c53780f81efdc3
ShaMan123 <shacharnen@gmail.com>
2019-11-04 10:23:32 +0200

    Update base.js

--- src/base.js
+++ src/base.js
@@ -14,4 +14,5 @@ export { createAnimatedAlways as always } from './core/AnimatedAlways';
 export { createAnimatedConcat as concat } from './core/AnimatedConcat';
 export { createAnimatedBlock as block, adapt } from './core/AnimatedBlock';
 export { createAnimatedFunction as proc } from './core/AnimatedFunction';
+export { createAnimatedInvoke as invoke } from './core/AnimatedInvoke';
 export * from './operators';
