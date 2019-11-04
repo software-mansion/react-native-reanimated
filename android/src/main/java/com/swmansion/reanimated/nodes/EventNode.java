0d43fc5386916fa79c34414bf245f685bcfacba0
ShaMan123 <shacharnen@gmail.com>
2019-11-04 10:21:57 +0200

    extract to & extend MapNode

--- android/src/main/java/com/swmansion/reanimated/nodes/EventNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/EventNode.java
@@ -12,49 +12,9 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-public class EventNode extends Node implements RCTEventEmitter {
-
-  private static class EventMap {
-    private final int nodeID;
-    private final String[] path;
-
-    public EventMap(ReadableArray eventPath) {
-      int size = eventPath.size();
-      path = new String[size - 1];
-      for (int i = 0; i < size - 1; i++) {
-        path[i] = eventPath.getString(i);
-      }
-      nodeID = eventPath.getInt(size - 1);
-    }
-
-    public Double lookupValue(ReadableMap event) {
-      ReadableMap map = event;
-      for (int i = 0; map != null && i < path.length - 1; i++) {
-        String key = path[i];
-        map = map.hasKey(key) ? map.getMap(key) : null;
-      }
-      if (map != null) {
-        String key = path[path.length - 1];
-        return map.hasKey(key) ? map.getDouble(key) : null;
-      }
-      return null;
-    }
-  }
-
-  private static List<EventMap> processMapping(ReadableArray mapping) {
-    int size = mapping.size();
-    List<EventMap> res = new ArrayList<>(size);
-    for (int i = 0; i < size; i++) {
-      res.add(new EventMap(mapping.getArray(i)));
-    }
-    return res;
-  }
-
-  private final List<EventMap> mMapping;
-
+public class EventNode extends MapNode implements RCTEventEmitter {
   public EventNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
     super(nodeID, config, nodesManager);
-    mMapping = processMapping(config.getArray("argMapping"));
   }
 
   @Override
@@ -63,13 +23,7 @@ public class EventNode extends Node implements RCTEventEmitter {
       throw new IllegalArgumentException("Animated events must have event data.");
     }
 
-    for (int i = 0; i < mMapping.size(); i++) {
-      EventMap eventMap = mMapping.get(i);
-      Double value = eventMap.lookupValue(event);
-      if (value != null) {
-        mNodesManager.findNodeById(eventMap.nodeID, ValueNode.class).setValue(value);
-      }
-    }
+    setValue(event);
   }
 
   @Override
@@ -77,8 +31,4 @@ public class EventNode extends Node implements RCTEventEmitter {
     throw new RuntimeException("receiveTouches is not support by animated events");
   }
 
-  @Override
-  protected Double evaluate() {
-    return ZERO;
-  }
 }
