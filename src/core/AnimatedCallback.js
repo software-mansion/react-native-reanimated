cc97df223f4dba25add6516e3183d1600422bbd7
ShaMan123 <shacharnen@gmail.com>
2019-11-05 10:59:32 +0200

    Update AnimatedCallback.js

--- src/core/AnimatedCallback.js
+++ src/core/AnimatedCallback.js
@@ -2,13 +2,13 @@
 import AnimatedMap, { sanitizeArgMapping } from './AnimatedMap';
 
 export default class AnimatedCallback extends AnimatedMap {
-  constructor(...argMapping, config = {}) {
+  constructor(argMapping, config = {}) {
     const { objectMappings, alwaysNodes } = sanitizeArgMapping(argMapping);
     super({ type: 'callback', argMapping: objectMappings });
     this._alwaysNodes = alwaysNodes;
   }
 }
 
-export function createAnimatedCallback(...argMapping, config) {
+export function createAnimatedCallback(argMapping, config) {
   return new AnimatedCallback(argMapping, config);
 }
