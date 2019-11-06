c437d7e1e6c7e3b9a26c94312023677fed1f8113
ShaMan123 <shacharnen@gmail.com>
2019-11-06 23:16:24 +0200

    Create ScrollViewTest.js

---
+++ Example/invoke/ScrollViewTest.js
@@ -0,0 +1,146 @@
+import React, { useMemo, useEffect } from 'react';
+import { StyleSheet, Dimensions, findNodeHandle, Image, NativeModules, UIManager, processColor, Sound } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { PanGestureHandler, State, RectButton, TapGestureHandler } from 'react-native-gesture-handler';
+import AnimatedTimePicker from './TimePicker';
+
+
+const { interpolate, cond, eq, add, call, set, Value, event, concat, sub, color, invoke, dispatch,useCode, or,Code, map, callback, neq, createAnimatedComponent, View, ScrollView, and, proc, Clock, multiply, onChange, not, defined, clockRunning, block, startClock, stopClock, spring } = Animated;
+
+//const pipper = invoke((a, b, c)=>)
+const P = createAnimatedComponent(PanGestureHandler);
+const Button = createAnimatedComponent(RectButton);
+
+//const measureLayout = proc((tag, parentTag, x, error) => cond(defined(tag, -1), invoke('UIManager', 'measureLayout', tag, parentTag, [error], { x })));
+const scrollTo = proc((tag, scrollX, scrollY, animated) => cond(defined(tag, -1), dispatch('RCTScrollView', 'scrollTo', tag, scrollX, scrollY, animated)));
+const scrollBy = proc((tag, scrollX, scrollY, scrollByX, scrollByY, animated) => scrollTo(tag, add(scrollX, scrollByX), add(scrollY, scrollByY), animated))
+const cb = proc((x) => callback({ x }));
+const scrollEvent = proc((x, y) => event([{ nativeEvent: { contentOffset: { x, y } } }]));
+
+export default function E() {
+  //const ref = React.useRef();
+  const [h, setH] = React.useState();
+  const [q, setQ] = React.useState();
+  const x = useMemo(() => new Value(0), []);
+  const scroll = useMemo(() => new Value(0), []);
+  const scrollX = useMemo(() => new Value(0), []);
+  const scrollY = useMemo(() => new Value(0), []);
+  const clock = useMemo(() => new Clock(), []);
+  const dest = useMemo(() => new Value(1), []);
+  const finalScroll = useMemo(() => interpolate(dest, {
+    inputRange: [0, 1],
+    outputRange: [50, 200],
+  }), []);
+  const state = useMemo(() => new Value(State.UNDETERMINED), []);
+  const appState = useMemo(() => new Value("initialAppState"), []);
+  const isScrolling = useMemo(() => new Value(0), []);
+  const error = useMemo(() => new Value(0), []);
+
+  const onScroll = useMemo(() => event([{ nativeEvent: { contentOffset: { x: scrollX, y: scrollY } } }]), [scrollX, scrollY]);
+  const onScrollStateChange = useMemo(() => event([{ nativeEvent: { state: s => set(isScrolling, or(eq(s, State.BEGAN), eq(s, State.ACTIVE))) } }]), [isScrolling]);
+  //const onScroll = useMemo(() => scrollEvent(scrollX, scrollY), [scrollX, scrollY]);
+  const onButtonPress = useMemo(() => event([{ nativeEvent: { oldState: state } }]), [state]);
+
+
+  const translationX = useMemo(() => new Value(0), []);
+  const translationY = useMemo(() => new Value(0), []);
+  const finalScrollX = useMemo(() => new Value(0), []);
+  const finalScrollY = useMemo(() => new Value(0), []);
+  const panState = useMemo(() => new Value(State.UNDETERMINED), []);
+  const onPan = useMemo(() => event([{
+    nativeEvent: {
+      translationX,
+      translationY,
+      oldState: panState
+    }
+  }]), [translationX, translationY, panState]);
+
+  useCode(
+    block([
+      cond(
+        eq(panState, State.ACTIVE),
+        [
+          set(translationX, 0),
+          set(translationY, 0),
+          set(finalScrollX, scrollX),
+          set(finalScrollY, scrollY),
+        ]
+      ),
+      call([scrollY, panState], console.log),
+      scrollTo(h, sub(finalScrollX, translationX), sub(finalScrollY, translationY), 0)
+    ]),
+    [h, translationX, scrollX, translationY, scrollY, panState, finalScrollX, scrollX, finalScrollY, scrollY]
+  );
+
+  const vibrate = useMemo(() => new Value(0), []);
+  useCode(
+    block([
+      onChange(
+        vibrate,
+        cond(
+          vibrate,
+          [
+            invoke('Vibration', 'vibrate', 500),
+            set(vibrate, 0)
+          ]
+        )
+      ),
+    ]),
+    [vibrate]
+  );
+
+  useCode(
+    block([
+      //set(scroll, runSpring(clock, scroll, 0, finalScroll)),
+      //scrollTo(h, 50, scroll, 0),
+      //onChange(state, cond(eq(state, State.ACTIVE), scrollBy(h, scrollX, scrollY, 50, scroll, 1)))
+      //cond(and(defined(q), defined(h)), invoke('UIManager', 'measureLayout', q, h, [error], [new Value(), new Value(), x])),
+      //call([x], a=>console.log('x?', a))
+      //invoke('ToastAndroid', 'showWithGravity', concat('hello from reanimated invoke', scroll), 200, 200)
+      invoke('PermissionsAndroid', 'requestPermission', 'android.permission.VIBRATE', {title: 'hello', message: 'prompting'}),
+      
+      invoke('SoundManager', 'playTouchSound'),
+      
+      invoke('ImageStoreManager', 'getBase64ForTag', "https://media.mnn.com/assets/images/2018/04/sunset_through_oak_tree.jpg.653x0_q80_crop-smart.jpg", [x], [error]),
+      //invoke('ShareModule', 'share', { title: 'Reanimated Share', message: 'hello from reanimated invoke' }, 'pica boo', {action: x}),
+      invoke('Clipboard', 'setString', concat('hello from reanimated invoke ', x)),
+    ]),
+    [h, q, scroll, dest, translationX, clock, finalScroll, state, scrollX, scrollY, x, error]
+  );
+  
+  useCode(call([state], a => console.log('state', a)), [state])
+  //useCode(call([scrollY], a => console.log('scroller', a)), [scrollY])
+  
+  /*
+  return (
+    <P
+      onHandlerStateChange={event([{ nativeEvent: { translationX } }])}
+    >
+      <Image source={require('../imageViewer/grid.png')} ref={(r) => setH(findNodeHandle(r))} />
+    </P>
+  );
+  */
+  const isInside = useMemo(() => new Value(0));
+  useCode(call([isInside], a => console.log('isInside', a)), [isInside])
+
+  //return <AnimatedTimePicker />;
+
+
+  return (
+    <PanGestureHandler
+      onGestureEvent={onPan}
+      onHandlerStateChange={onPan}
+    >
+      <ScrollView
+        style={{ flex: 1 }}
+        collapsable={false}
+        ref={(r) => setH(findNodeHandle(r))}
+        onScroll={onScroll}
+        scrollEnabled={false}
+      >
+        <View style={{flex:1, minHeight: 200}} />
+        <Image source={require('../imageViewer/grid.png')} collapsable={false} ref={(r) => setQ(findNodeHandle(r))} />
+      </ScrollView>
+    </PanGestureHandler>
+  );
+}
