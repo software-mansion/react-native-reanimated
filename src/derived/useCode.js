a7af4ed78f38eb79783cb8564ff83e18d796e64b
Yair <yairopro@gmail.com>
2019-11-07 17:45:27 +0200

    Allows useCode's 1st parameter to return undefined, null or an array.
    And makes the code clearer (it seems clearer to me 😜)

--- src/derived/useCode.js
+++ src/derived/useCode.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { always } from '../base';
+import {always, block} from '../base';
 
-function useCode(exec, deps) {
-  if (typeof React.useEffect === 'function') {
-    React.useEffect(() => {
-      if (typeof exec !== 'function') {
-        console.warn(
-          'useCode() first argument should be a function that returns an animation node.'
-        );
-      }
-      const animatedAlways = always(typeof exec === 'function' ? exec() : exec);
-      animatedAlways.__attach();
-      return () => {
-        animatedAlways.__detach();
-      };
-    }, deps);
-  }
+/**
+ * React hook to run a node.
+ * @param nodeFactory Function to build the node to run.
+ * @param dependencies Array of dependencies. Refresh the node on changes.
+ */
+export default function useCode(nodeFactory, dependencies) {
+  // check hook support first
+  if (!(React.useEffect instanceof Function))
+    return;
+
+  // --- run ----
+  React.useEffect(() => {
+        // --- check and correct 1st parameter ---
+        if (!(nodeFactory instanceof Function)) {
+          console.warn('useCode() first argument should be a function that returns an animation node.');
+
+          // correct parameter
+          const node = nodeFactory;
+          nodeFactory = () => node;
+        }
+
+        // --- get and execute node (if defined) ---
+        let node = nodeFactory();
+        if (node) {
+          // allow factory to return array
+          if (node instanceof Array)
+            node = block(node);
+
+          const animatedAlways = always(node);
+          animatedAlways.__attach();
+
+          // return undo function
+          return () => animatedAlways.__detach();
+        }
+      },
+      dependencies
+  );
 }
-export default useCode;
