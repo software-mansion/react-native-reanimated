b6f2ba6d0416d794113e51fe82d8c80e5a690435
ShaMan123 <shacharnen@gmail.com>
2019-11-09 23:54:38 +0200

    fix error to throw in js

--- android/src/main/java/com/swmansion/reanimated/nodes/EventNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/EventNode.java
@@ -1,14 +1,11 @@
 package com.swmansion.reanimated.nodes;
 
-import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.swmansion.reanimated.NodesManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.annotation.Nullable;
 
@@ -20,7 +17,7 @@ public class EventNode extends MapNode implements RCTEventEmitter {
   @Override
   public void receiveEvent(int targetTag, String eventName, @Nullable WritableMap event) {
     if (event == null) {
-      throw new IllegalArgumentException("Animated events must have event data.");
+      throw new JSApplicationIllegalArgumentException("Animated events must have event data.");
     }
 
     setValue(event);
