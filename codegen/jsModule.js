ff4f56668a08a21005f24fdc137dcf25472ea93d
osdnk <micosa97@gmail.com>
2019-11-09 04:11:07 +0100

    Make it buildable on android

--- codegen/jsModule.js
+++ codegen/jsModule.js
@@ -1,12 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import AnimatedNode from "../src/core/AnimatedNode";
+import { Platform } from "react-native";
 
 // NODES
 
 // REGISTRY
 
 export default function codegen(name) {
-  return function (...args) {
+  return function (realNode) {
+    if (Platform.OS !== 'ios') {
+      return realNode
+    }
     return function (...args) {
       // eslint-disable-next-line no-undef
       return new registry[name](args.map(a => a.__nodeID))
