76beb7dc40b9820fb546351cfb4c124e65eac141
ShaMan123 <shacharnen@gmail.com>
2019-11-10 13:46:27 +0200

    Update ControlledScrollView.js

--- Example/invoke/ControlledScrollView.js
+++ Example/invoke/ControlledScrollView.js
@@ -1,11 +1,11 @@
 import React, { useMemo, useRef, useState } from 'react';
-import { findNodeHandle, Image, StyleSheet, UIManager } from 'react-native';
+import { findNodeHandle, Image, StyleSheet, UIManager, processColor } from 'react-native';
 import { PanGestureHandler, State, RectButton } from 'react-native-gesture-handler';
 import Animated, { Easing } from 'react-native-reanimated';
 
 const { interpolate, cond, eq, timing, add, call, max, set, Text, Value, event, decay, concat, divide, abs, sub, color, invoke, dispatch, useCode, or, Code, map, callback, neq, createAnimatedComponent, View, ScrollView, and, proc, Clock, multiply, onChange, not, defined, clockRunning, block, startClock, stopClock, spring } = Animated;
 
-const decelerationRate = 0.995;
+const decelerationRate = 0.9955;
 
 const Button = createAnimatedComponent(RectButton);
 
@@ -47,14 +47,14 @@ function resolveScrollViewNS(horizontal = false) {
   })
 }
 
-const scrollTo = proc((tag, scrollX, scrollY, animated) => cond(defined(tag, -1), dispatch(resolveScrollViewNS(), 'scrollTo', tag, scrollX, scrollY, animated)));
+const scrollTo = proc((tag, scrollX, scrollY, animated) => cond(neq(tag, -1), dispatch(resolveScrollViewNS(), 'scrollTo', tag, scrollX, scrollY, animated)));
 const scrollBy = proc((tag, scrollX, scrollY, scrollByX, scrollByY, animated) => scrollTo(tag, add(scrollX, scrollByX), add(scrollY, scrollByY), animated))
 const cb = proc((x) => callback({ x }));
 const sign = proc((x) => divide(x, abs(x)));
 const decayVelocity = proc((v) => cond(eq(v, 0), 1, multiply(v, -1)));
 const scrollEvent = proc((x, y) => event([{ nativeEvent: { contentOffset: { x, y } } }]));
 
-export default function ControlledScrollView(props) {
+function ControlledScrollView(props) {
   const [handle, setHandle] = useState(-1);
 
   const scrollX = useMemo(() => new Value(0), []);
@@ -63,9 +63,7 @@ export default function ControlledScrollView(props) {
   const scrollYBegin = useMemo(() => new Value(0), []);
   const clockX = useMemo(() => new Clock(), []);
   const clockY = useMemo(() => new Clock(), []);
-  const state = useMemo(() => new Value(State.UNDETERMINED), []);
-  const isScrolling = useMemo(() => new Value(0), []);
-  
+
   const translationX = useMemo(() => new Value(0), []);
   const translationY = useMemo(() => new Value(0), []);
   const velocityX = useMemo(() => new Value(0), []);
@@ -98,17 +96,17 @@ export default function ControlledScrollView(props) {
   const onScroll = useMemo(() =>
     event([{
       nativeEvent: ({ contentOffset: { x, y } }) => cond(
-          neq(panOldState, State.ACTIVE),
-          [
-            set(scrollX, x),
-            set(scrollY, y),
-          ]
-        )
-      
+        neq(panOldState, State.ACTIVE),
+        [
+          set(scrollX, x),
+          set(scrollY, y),
+        ]
+      )
+
     }]),
     [panOldState, scrollX, scrollY]
   );
-  
+
 
   useCode(
     cond(
@@ -131,82 +129,123 @@ export default function ControlledScrollView(props) {
         scrollBy(handle, scrollXBegin, scrollYBegin, multiply(translationX, -1), multiply(translationY, -1), 0)
       ]
     ),
-    [handle, panOldState, translationX, translationY, finalScrollX,finalScrollY, scrollX, scrollY, clockX, clockY, velocityX, velocityY]
+    [handle, panOldState, translationX, translationY, finalScrollX, finalScrollY, scrollX, scrollY, clockX, clockY, velocityX, velocityY]
   );
 
+  return (
+    <PanGestureHandler
+      onGestureEvent={onPan}
+      onHandlerStateChange={onPan}
+      {...props}
+    >
+      <ScrollView
+        style={styles.scrollView}
+        collapsable={false}
+        ref={(r) => setHandle(findNodeHandle(r))}
+        onScroll={onScroll}
+        scrollEnabled={false}
+        decelerationRate={decelerationRate}
+      >
+        <Image source={require('../imageViewer/grid.png')} collapsable={false} />
+      </ScrollView>
+    </PanGestureHandler>
+  );
+}
+
+export default function ScrollViewMock(props) {
+  const [i, forceRefresh] = useState(0);
   const vibrate = useMemo(() => new Value(0), []);
+  const correct = useMemo(() => new Value(0), []);
+
   useCode(
-    block([
-      call([vibrate], console.log),
-      cond(
-        vibrate,
-        [
-          invoke('Vibration', 'vibrate', 500),
-          set(vibrate, 0)
-        ]
-      )
-    ]),
+    cond(
+      vibrate,
+      [
+        invoke('Vibration', 'vibrate', 500),
+        set(vibrate, 0),
+        //set(correct, 1)
+      ]
+    ),
     [vibrate]
   );
-
+  /*
+  useCode(
+    onChange(
+      panState,
+      cond(
+        eq(panState, State.BEGAN),
+        [
+          set(correct, 0)
+        ]
+      )
+    ),
+    [panState, correct]
+  );
+  */
   const refA = useRef();
   const refB = useRef();
 
+  const vibrateEvent = useMemo(() =>
+    event([{
+      nativeEvent: ({ state }) =>
+        set(vibrate, eq(state, State.ACTIVE))
+    }]),
+    [vibrate]
+  );
+
   const baseScrollComponent = (
-    <Button
+    <RectButton
       key="______ScrollView1"
       style={styles.default}
       ref={refA}
-      //onPress={() => vibrate.setValue(1)}
-      onHandlerStateChange={event([{ nativeEvent: () => set(vibrate, 1) }])}
+      onHandlerStateChange={(e) => e.nativeEvent.state === State.ACTIVE && vibrate.setValue(1)}
     >
       <View style={styles.default} collapsable={false}>
         <ScrollView
           style={styles.scrollView}
           collapsable={false}
-          decelerationRate={decelerationRate}
+          //decelerationRate={decelerationRate}
           waitFor={refA}
         >
           <Image source={require('../imageViewer/grid.png')} collapsable={false} />
         </ScrollView>
       </View>
-    </Button>
+    </RectButton>
   );
 
   const animatedScrollComponent = (
     <RectButton
       key="______ScrollView2"
       style={styles.default}
+      onHandlerStateChange={(e) => {
+        e.nativeEvent.state === State.BEGAN && correct.setValue(0);
+        e.nativeEvent.state === State.ACTIVE && correct.setValue(1);
+      }}
       ref={refB}
-      onPress={() => vibrate.setValue(1)}
     >
       <View style={styles.default} collapsable={false}>
-        <PanGestureHandler
-          onGestureEvent={onPan}
-          onHandlerStateChange={onPan}
-          waitFor={refB}
-          enabled={false}
+        <ControlledScrollView waitFor={refB} />
+        <View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: cond(correct, processColor('red'), processColor('transparent')),
+              opacity: cond(correct, 0.5, 0),
+            }
+          ]}
         >
-          <ScrollView
-            style={styles.scrollView}
-            collapsable={false}
-            ref={(r) => setHandle(findNodeHandle(r))}
-            onScroll={onScroll}
-            scrollEnabled={false}
-            decelerationRate={decelerationRate}
-          >
-            <Image source={require('../imageViewer/grid.png')} collapsable={false} />
-          </ScrollView>
-        </PanGestureHandler>
+          <Text style={styles.text}>I'm the fake one</Text>
+        </View>
       </View>
     </RectButton>
   );
 
-  const randomizer = (Math.random() * 10) % 2 === 0;
+  const randomizer = useMemo(() => Math.round(Math.random() * 10) % 2 === 0, [i]);
 
   return (
     <View style={styles.default}>
-      <Text>One ScrollView is a regular one, the other is actually controlled by a PanGestureHandler utilizing dispatch</Text>
+      <Text>One ScrollView is a regular one, the other is actually controlled by a PanGestureHandler utilizing dispatch.</Text>
       <Text>Tap on the one you think is the real one</Text>
       {randomizer ? baseScrollComponent : animatedScrollComponent}
       {randomizer ? animatedScrollComponent : baseScrollComponent}
@@ -223,5 +262,14 @@ const styles = StyleSheet.create({
   },
   default: {
     flex:1
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    alignSelf: 'center',
+    color: 'black',
+    backgroundColor: 'white'
   }
 })
