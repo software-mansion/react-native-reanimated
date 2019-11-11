658d1ac8233d04fb4989ce50008bfd82c41bfe34
ShaMan123 <shacharnen@gmail.com>
2019-11-11 23:40:55 +0200

    rename folder

--- Example/App.js
+++ Example/App.js
@@ -14,7 +14,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import ChatHeads from './chatHeads';
 import Code from './code';
-import Invoke, { SCREENS as INVOKE_SCREENS } from './invoke';
+import Invoke, { SCREENS as INVOKE_SCREENS } from './directManipulation';
 import Colors from './colors';
 import DifferentSpringConfigs from './differentSpringConfigs';
 import ImageViewer from './imageViewer';
