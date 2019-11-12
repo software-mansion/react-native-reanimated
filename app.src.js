d68a7dd75f9bfe669710540ecf5f49318e984d91
osdnk <micosa97@gmail.com>
2019-11-12 12:10:54 +0100

    Updates

---
+++ app.src.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RedBox from 'redbox-react';
+import App from '/Users/osdnk/work/react-native-reanimated/docs/node_modules/component-docs/dist/templates/App.js';
+import data from './app.data';
+import '/Users/osdnk/work/react-native-reanimated/docs/node_modules/component-docs/dist/styles/reset.css';
+import '/Users/osdnk/work/react-native-reanimated/docs/node_modules/component-docs/dist/styles/globals.css';
+
+import '/Users/osdnk/work/react-native-reanimated/docs/assets/styles.css';
+
+const root = document.getElementById('root');
+const render = () => {
+  try {
+    ReactDOM.hydrate(
+      <App
+        name={window.__INITIAL_PATH__}
+        data={data}
+        github={"https://github.com/kmagiera/react-native-reanimated/edit/master/"}
+        logo={undefined}
+      />,
+      root
+    );
+  } catch (e) {
+    ReactDOM.render(
+      <RedBox error={e} />,
+      root
+    );
+  }
+};
+
+if (module.hot) {
+  module.hot.accept(() => {
+    render();
+  });
+}
+
+render();
