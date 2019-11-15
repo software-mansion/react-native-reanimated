2f7a05ae632c13e967464e9653e6a858c8c2adad
Jakub Gonet <jakub.gonet@swmansion.com>
2019-11-15 13:33:37 +0100

    Updates

--- app.src.js
+++ app.src.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RedBox from 'redbox-react';
-import App from '/Users/osdnk/work/RNR-copy/react-native-reanimated/docs/node_modules/component-docs/dist/templates/App.js';
+import App from '/Users/jgonet/Documents/Projects/RN/react-native-reanimated/docs/node_modules/component-docs/dist/templates/App.js';
 import data from './app.data';
-import '/Users/osdnk/work/RNR-copy/react-native-reanimated/docs/node_modules/component-docs/dist/styles/reset.css';
-import '/Users/osdnk/work/RNR-copy/react-native-reanimated/docs/node_modules/component-docs/dist/styles/globals.css';
+import '/Users/jgonet/Documents/Projects/RN/react-native-reanimated/docs/node_modules/component-docs/dist/styles/reset.css';
+import '/Users/jgonet/Documents/Projects/RN/react-native-reanimated/docs/node_modules/component-docs/dist/styles/globals.css';
 
-import '/Users/osdnk/work/RNR-copy/react-native-reanimated/docs/assets/styles.css';
+import '/Users/jgonet/Documents/Projects/RN/react-native-reanimated/docs/assets/styles.css';
 
 const root = document.getElementById('root');
 const render = () => {
