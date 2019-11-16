9516941f886b6a74153e0c9db6278c8da5459018
ShaMan123 <shacharnen@gmail.com>
2019-11-16 19:51:04 +0200

    organize

--- android/src/main/java/com/swmansion/reanimated/nodes/CallMapNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/CallMapNode.java
@@ -3,33 +3,26 @@ package com.swmansion.reanimated.nodes;
 import com.facebook.react.bridge.ReadableMap;
 import com.swmansion.reanimated.NodesManager;
 
-public class CallMapNode extends CallFuncNode implements ContextNode {
+public class CallMapNode extends CallFuncNode implements ValueManagerNode {
+
+    private final int mMapNodeID;
 
     public CallMapNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
         super(nodeID, config, nodesManager);
-        MapNode mapNode = mNodesManager.findNodeById(mWhatNodeID, Node.class).source(MapNode.class);
+        mMapNodeID = config.getInt("map");
+
+        MapNode mapNode = mNodesManager.findNodeById(mMapNodeID, MapNode.class);
         for (int i = 0; i < mapNode.mMapping.size(); i++) {
             int paramId = mapNode.mMapping.get(i).nodeID;
             mParams[i] = paramId;
         }
     }
 
-    @Override
-    public void beginContext(Integer ref, String prevCallID) {
-        //  noop
-    }
-
-    @Override
-    public void endContext() {
-        super.endContext();
-        //  noop
-        //  we should wait for callbacks to resolve
-    }
-
     public void setValue(Object value) {
         beginContext();
-        MapNode whatNode = mNodesManager.findNodeById(mWhatNodeID, MapNode.class);
-        whatNode.setValue(value);
-        endContext();//super.endContext();
+        Node whatNode = mNodesManager.findNodeById(mWhatNodeID, Node.class);
+        ((ValueManagerNode) whatNode).setValue(value);
+        endContext();
     }
+
 }
