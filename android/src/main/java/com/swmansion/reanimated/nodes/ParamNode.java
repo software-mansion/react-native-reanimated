5d23b28537de5e04c28714ab6363e0fbc561a34e
ShaMan123 <shacharnen@gmail.com>
2019-11-18 15:12:24 +0200

    better error

--- android/src/main/java/com/swmansion/reanimated/nodes/ParamNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/ParamNode.java
@@ -52,7 +52,9 @@ public class ParamNode extends ValueNode implements ContextNode {
       mUpdateContext.callID = callID;
       return val;
     } catch (EmptyStackException e) {
-      throw new JSApplicationCausedNativeException(getClass().getSimpleName() + " is trying to evaluate with no context.", e);
+      throw new JSApplicationCausedNativeException(getClass().getSimpleName() + " is trying to evaluate with no context.\n" +
+              "It seems you are trying to use `callback` inside `proc`. This is not yet supported.",
+              e);
     }
   }
 }
