9309e43f664d70f6cd9d1e6b6486214e060eac77
ShaMan123 <shacharnen@gmail.com>
2019-12-01 14:16:52 +0200

    Update SyncedScrollViews.js

--- Example/directManipulation/SyncedScrollViews.js
+++ Example/directManipulation/SyncedScrollViews.js
@@ -1,22 +1,76 @@
-import React, { useMemo, useEffect } from 'react';
-import { findNodeHandle, Image, StyleSheet } from 'react-native';
+import React, { useMemo, useRef } from 'react';
+import { Image, StyleSheet, ScrollView as RNScrollView } from 'react-native';
 import Animated from 'react-native-reanimated';
+import { State, NativeViewGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
 
-
-const { interpolate, cond, eq, add, call, set, Value, event, concat, sub, color, invoke, dispatch, useCode, or, Code, callback, neq, createAnimatedComponent, View, ScrollView, and, proc, Clock, multiply, onChange, not, defined, clockRunning, block, startClock, stopClock, spring } = Animated;
-
-const scrollTo = proc((scrollX, scrollY, animated) => dispatch('RCTScrollView', 'scrollTo', scrollX, scrollY, animated));
+const { Value, event, dispatch, useCode, createAnimatedComponent, block, cond, not, and, eq, set, View, or, debug } = Animated;
+const ScrollView = createAnimatedComponent(RNScrollView);
 
 export default function SyncedScrollViews() {
   //const [handleA, setHandleA] = React.useState();
   //const [handleB, setHandleB] = React.useState();
   const scrollX = useMemo(() => new Value(0), []);
   const scrollY = useMemo(() => new Value(0), []);
+  const stateA = useMemo(() => new Value(State.UNDETERMINED), []);
+  const stateB = useMemo(() => new Value(State.UNDETERMINED), []);
+  const state = useMemo(() => debug('scroll state', new Value(0)), []);
 
   const scrollToA = useMemo(() => dispatch('RCTScrollView', 'scrollTo', scrollX, scrollY, 0), [scrollX, scrollY]);
   const scrollToB = useMemo(() => dispatch('RCTScrollView', 'scrollTo', scrollX, scrollY, 0), [scrollX, scrollY]);
 
-  const onScroll = useMemo(() => event([{ nativeEvent: { contentOffset: { x: scrollX, y: scrollY } } }]), [scrollX, scrollY]);
+  const panRef = useRef();
+  const scrollARef = useRef();
+  const scrollBRef = useRef();
+
+  const beginDragA = useMemo(() =>
+    event([{
+      nativeEvent: ({ contentOffset: { x, y } }) => block([
+        set(state, 1),
+        set(scrollX, x),
+        set(scrollY, y)
+      ])
+    }]),
+    [state]
+  );
+
+  const beginDragB = useMemo(() =>
+    event([{
+      nativeEvent: ({ contentOffset: { x, y } }) => block([
+        set(state, 2),
+        set(scrollX, x),
+        set(scrollY, y)
+      ])
+    }]),
+    [state]
+  );
+
+  const onScrollA = useMemo(() =>
+    event([{
+      nativeEvent: ({ contentOffset: { x, y } }) =>
+        cond(
+          or(eq(state, 1), eq(state, 0)),
+          [
+            set(scrollX, x),
+            set(scrollY, y)
+          ]
+        )
+    }]),
+    [state]
+  );
+
+  const onScrollB = useMemo(() =>
+    event([{
+      nativeEvent: ({ contentOffset: { x, y } }) =>
+        cond(
+          or(eq(state, 2), eq(state, 0)),
+          [
+            set(scrollX, x),
+            set(scrollY, y),
+          ]
+        )
+    }]),
+    [state]
+  );
 
   // const otherScrollY = useMemo(() => new Value(0), []);
   // const otherOnScroll = useMemo(() => event([{ nativeEvent: { contentOffset: { y: otherScrollY } } }]), [otherScrollY]);
@@ -24,7 +78,7 @@ export default function SyncedScrollViews() {
   useCode(() =>
     block([
       scrollToA,
-      scrollToB
+      scrollToB,
     ]),
     [scrollToA, scrollToB]
   );
@@ -33,18 +87,42 @@ export default function SyncedScrollViews() {
     <ScrollView
       style={styles.scrollView}
       collapsable={false}
-      onScroll={onScroll}
       scrollEventThrottle={1}
+      simultaneousHandlers={panRef}
     >
       <Image source={require('../imageViewer/grid.png')} collapsable={false} />
     </ScrollView>
   );
 
   return (
-    <>
-      {React.cloneElement(baseScrollComponent, { ref: (ref) => scrollToA.setNativeView(ref) })}
-      {React.cloneElement(baseScrollComponent, { ref: (ref) => scrollToB.setNativeView(ref) })}
-    </>
+    <PanGestureHandler
+      ref={panRef}
+      simultaneousHandlers={[scrollARef, scrollBRef]}
+      enabled={false}
+    >
+      <View style={styles.default} collapsable={false}>
+        {React.cloneElement(baseScrollComponent, {
+          ref: (ref) => {
+            scrollToA.setNativeView(ref);
+            scrollARef.current = ref;
+          },
+          onScroll: onScrollA,
+          onScrollBeginDrag: beginDragA,
+          onMomentumScrollBegin: beginDragA,
+          onMomentumScrollEnd: onScrollA
+        })}
+        {React.cloneElement(baseScrollComponent, {
+          ref: (ref) => {
+            scrollToB.setNativeView(ref);
+            scrollBRef.current = ref;
+          },
+          onScroll: onScrollB,
+          onScrollBeginDrag: beginDragB,
+          onMomentumScrollBegin: beginDragB,
+          onMomentumScrollEnd: onScrollB
+        })}
+      </View>
+    </PanGestureHandler>
   );
 }
 
