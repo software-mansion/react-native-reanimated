2a2e578f6dba744aaa978b9b56a72773a58bed5a
Jakub Gonet <jakub.gonet@swmansion.com>
2020-01-03 17:01:08 +0100

    Add install of NVM

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
 NODE_V=${NODE_VERSION:-'13.3.0'}
-
-echo "installing node"
+echo 'Installing nvm'
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+echo 'installing node'
 nvm install $NODE_V
 nvm use $NODE_V
 nvm alias default $NODE_V
