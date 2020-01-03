a37ae7093677a225d6729a5d1e8cdf3645bfb829
Jakub Gonet <jakub.gonet@swmansion.com>
2020-01-03 17:25:41 +0100

    test

--- .travis/e2e/install.android.sh
+++ .travis/e2e/install.android.sh
@@ -5,6 +5,7 @@ source $(dirname "$0")/../install.sh
 cd e2e
 
 echo 'Installing packages...'
+echo "Node version: $(node -v)"
 yarn
 echo 'Building Android app'
 cd ./android && ./gradlew assembleDebug
--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ NODE_V=${NODE_VERSION:-'13.3.0'}
 echo 'Installing nvm'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-echo 'installing node'
+echo 'Installing node'
 nvm install $NODE_V
 nvm use $NODE_V
 nvm alias default $NODE_V
