2e02d2d133463615dc5492f4a155c4da8a593667
Jakub Gonet <jakub.gonet@swmansion.com>
2020-01-03 15:19:26 +0100

    Install node and remove npm install >/dev/null

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 
+NODE_VERSION="13.3.0"
+
+echo "installing node, version $NODE_VERSION"
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+nvm alias default $NODE_VERSION
+node --version
 echo 'Installing react-native-cli, detox-cli, yarn'
-npm install -g react-native-cli >/dev/null 2>&1
-npm install -g detox-cli >/dev/null 2>&1
-npm install -g yarn >/dev/null 2>&1
+npm install -g react-native-cli
+npm install -g detox-cli
+npm install -g yarn
 echo 'Installing packages...'
 yarn
