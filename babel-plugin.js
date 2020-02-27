932214f4160a5d0b41fc046df2977979c166f6f6
karolbisztyga-swmansion <karol.bisztyga@swmansion.com>
2020-02-27 13:34:32 +0100

    arrow functions abandoned, double quotes replacement

--- babel-plugin.js
+++ babel-plugin.js
@@ -7,30 +7,28 @@ module.exports = function ({ types: t }) {
       DirectiveLiteral(path) {
         const value = path.node.value;
         if (value === 'worklet') {
-          console.log("ok " + value);
           const fun = path.getFunctionParent();
           let str = fun.toString()
           str = str.replace(
             /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '');
           str = str.replace(/\n/ig, '');
+          str = str.replace(/"/ig, '`');
           let newCode = ""
-          let type = fun.node.type
-          if (type === 'ArrowFunctionExpression') {
-            str = ('function' + str).replace('=> {', '{')
-            type = 'FunctionDeclaration'
-          }
           
-          if (type === 'FunctionExpression') {
+          if (fun.node.type === 'FunctionExpression') {
             newCode = codeForExpresion.replace(/FUNCTION_CODE/g, str);
             fun.replaceWithSourceString(newCode);
             fun.skip();
-          } else if (type === 'FunctionDeclaration') {
+          } else if (fun.node.type === 'FunctionDeclaration') {
             newCode = codeForDeclaration;
             newCode = newCode.replace(/NAME/g, fun.node.id.name);
             newCode = newCode.replace(/FUNCTION_CODE/g, str);
             fun.insertAfter(t.StringLiteral(""));
             const nextInd = fun.key+1;
             fun.getSibling(nextInd).replaceWithSourceString(newCode);
+          } else if (fun.node.type === 'ArrowFunctionExpression') {
+            // arrow function is unreachable as babel code which is run before this plugin
+            // converts all arrow functions into Function Expressions
           }
         }
       }
