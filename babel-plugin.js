3b19d96e36aa880e69e3fa5a713fa74f7545ab63
karolbisztyga-swmansion <karol.bisztyga@swmansion.com>
2020-02-27 14:46:58 +0100

    quotes handled properly

--- babel-plugin.js
+++ babel-plugin.js
@@ -1,6 +1,6 @@
 "use strict"
-const codeForExpresion = '(function(){const t = FUNCTION_CODE; t.asString = "FUNCTION_CODE"; return t;})()'
-const codeForDeclaration = 'NAME.asString = "FUNCTION_CODE"'
+const codeForExpresion = "(function(){const t = FUNCTION_CODE; t.asString = 'FUNCTION_CODE'; return t;})()"
+const codeForDeclaration = "NAME.asString = 'FUNCTION_CODE'"
 module.exports = function ({ types: t }) {
   return {
     visitor: {
@@ -12,17 +12,19 @@ module.exports = function ({ types: t }) {
           str = str.replace(
             /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '');
           str = str.replace(/\n/ig, '');
-          str = str.replace(/"/ig, '`');
           let newCode = ""
           
           if (fun.node.type === 'FunctionExpression') {
-            newCode = codeForExpresion.replace(/FUNCTION_CODE/g, str);
+            newCode = codeForExpresion.replace('FUNCTION_CODE', str);
+            str = str.replace(/'/ig, "\\'");
+            newCode = newCode.replace('FUNCTION_CODE', str);
             fun.replaceWithSourceString(newCode);
             fun.skip();
           } else if (fun.node.type === 'FunctionDeclaration') {
             newCode = codeForDeclaration;
-            newCode = newCode.replace(/NAME/g, fun.node.id.name);
-            newCode = newCode.replace(/FUNCTION_CODE/g, str);
+            newCode = newCode.replace('NAME', fun.node.id.name);
+            str = str.replace(/'/ig, "\\'");
+            newCode = newCode.replace('FUNCTION_CODE', str);
             fun.insertAfter(t.StringLiteral(""));
             const nextInd = fun.key+1;
             fun.getSibling(nextInd).replaceWithSourceString(newCode);
