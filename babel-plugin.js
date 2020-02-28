175888dc273c7b087e4dd2168ac7a2ba45e7f720
karolbisztyga-swmansion <59833830+karolbisztyga-swmansion@users.noreply.github.com>
2020-02-28 15:24:27 +0100

    Turbo version babel plugin (#624)
    * arrow functions
    
    * revert App.js
    
    * arrow functions abandoned, double quotes replacement
    
    * quotes handled properly

--- babel-plugin.js
+++ babel-plugin.js
@@ -1,32 +1,36 @@
 "use strict"
-const codeForExpresion = '(function(){const t = FUNCTION_CODE; t.asString = "FUNCTION_CODE"; return t;})()'
-const codeForDeclaration = 'NAME.asString = "FUNCTION_CODE"'
+const codeForExpresion = "(function(){const t = FUNCTION_CODE; t.asString = 'FUNCTION_CODE'; return t;})()"
+const codeForDeclaration = "NAME.asString = 'FUNCTION_CODE'"
 module.exports = function ({ types: t }) {
   return {
     visitor: {
       DirectiveLiteral(path) {
         const value = path.node.value;
         if (value === 'worklet') {
-          console.log("ok " + value);
           const fun = path.getFunctionParent();
           let str = fun.toString()
           str = str.replace(
             /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '');
           str = str.replace(/\n/ig, '');
           let newCode = ""
+          
           if (fun.node.type === 'FunctionExpression') {
-            newCode = codeForExpresion.replace(/FUNCTION_CODE/g, str);
+            newCode = codeForExpresion.replace('FUNCTION_CODE', str);
+            str = str.replace(/'/ig, "\\'");
+            newCode = newCode.replace('FUNCTION_CODE', str);
             fun.replaceWithSourceString(newCode);
             fun.skip();
           } else if (fun.node.type === 'FunctionDeclaration') {
             newCode = codeForDeclaration;
-            newCode = newCode.replace(/NAME/g, fun.node.id.name);
-            newCode = newCode.replace(/FUNCTION_CODE/g, str);
+            newCode = newCode.replace('NAME', fun.node.id.name);
+            str = str.replace(/'/ig, "\\'");
+            newCode = newCode.replace('FUNCTION_CODE', str);
             fun.insertAfter(t.StringLiteral(""));
             const nextInd = fun.key+1;
             fun.getSibling(nextInd).replaceWithSourceString(newCode);
-          } else {
-            // arrow functions not supported yet
+          } else if (fun.node.type === 'ArrowFunctionExpression') {
+            // arrow function is unreachable as babel code which is run before this plugin
+            // converts all arrow functions into Function Expressions
           }
         }
       }
