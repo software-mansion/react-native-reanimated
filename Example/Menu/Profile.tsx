38dcb63e4057d0260cbc0088fb8deca0714319dd
Krzysztof Magiera <krzys@swmansion.com>
2020-03-13 15:26:29 +0100

    Some comments

--- Example/Menu/Profile.tsx
+++ Example/Menu/Profile.tsx
@@ -25,7 +25,7 @@ export default ({ hideTarget, hideWorklet, scale, rotateY, translateX }) => {
       const sign = translationX < 0 ? -1 : 1;
 
       const x = translationX;
-      const dt = 1 / 60;
+      const dt = 1 / 60; // would be nice to be provided with dt when worklet is used as a looper
       const tension = 300;
       const damping = 12;
       const acc = -tension * x;
@@ -67,12 +67,16 @@ export default ({ hideTarget, hideWorklet, scale, rotateY, translateX }) => {
       'worklet';
       let translationX = this.event.translationX + totalTranslation.value;
       if (this.event.state === 2) {
+        // for animation it'd be convinient to have a way to define a logic that runs
+        // when it gets interrupted (ended or cancelled).
         this.stop(animateWorklet);
       } else if (this.event.state === 5) {
         totalTranslation.set(translationX);
         if (translationX < -30) {
           this.log('FINAL TRANSLATION ' + translationX);
           // we need to paste hide logic here, would be nice to be able to use function
+          // maybe "worklet" should just be a "function" and when we want to use
+          // it to drive animation we can use some "looper" abstraction?
           hideTarget.set(0);
           this.start(hideWorklet);
         }
@@ -84,6 +88,9 @@ export default ({ hideTarget, hideWorklet, scale, rotateY, translateX }) => {
       const extraNorm = Math.min(translationX * sign, 100) / 100;
       const extraProgress = Math.sqrt(extraNorm) * sign;
 
+      // the logic of updating rotation and translate needs to be copied in two
+      // places, we could've used Anmated.interpolate to avoid that and only update
+      // "progress"
       extraRotate.set(extraProgress * -30 + 'deg');
       extraTranslate.set(extraProgress * 24);
     },
