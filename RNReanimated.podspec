a9688ff608d6462bdf615ff8f03d62770b04cd13
karol-bisztyga <59833830+karol-bisztyga@users.noreply.github.com>
2020-03-25 08:37:17 +0100

    Turbo version refactor (#678)
    * android refactor works
    
    * moved sws, sa, so
    
    * android path simplified
    
    * refactoring on ios works
    
    * refactored mapper, simplified podspec, native proxy fix

--- RNReanimated.podspec
+++ RNReanimated.podspec
@@ -20,7 +20,10 @@ Pod::Spec.new do |s|
   s.platforms    = { :ios => "9.0", :tvos => "9.0" }
   s.source       = { :git => "https://github.com/software-mansion/react-native-reanimated.git", :tag => "#{s.version}" }
 
-  s.source_files = "ios/**/*.{mm,h,m}", "Common/cpp/*.cpp", "Common/cpp/headers/*.h"
+  s.source_files =
+      "ios/**/*.{mm,h,m}",
+      "Common/cpp/**/*.cpp",
+      "Common/cpp/headers/**/*.h"
   
   s.pod_target_xcconfig    = {
     "USE_HEADERMAP" => "YES",
--- android/Android.mk
+++ android/Android.mk
@@ -12,12 +12,21 @@ LOCAL_MODULE := reanimated
 
 PROJECT_FILES := $(wildcard $(LOCAL_PATH)/src/main/cpp/*.cpp)
 PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/**/*.cpp)
 
 PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := $(PROJECT_FILES)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/src/main/cpp/headers $(LOCAL_PATH)/../Common/cpp/headers $(HERMES_ENGINE)/android/include 
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/src/main/cpp/headers \
+	$(LOCAL_PATH)/../Common/cpp/headers \
+	$(LOCAL_PATH)/../Common/cpp/headers/NativeModules \
+	$(LOCAL_PATH)/../Common/cpp/headers/Registries \
+	$(LOCAL_PATH)/../Common/cpp/headers/SharedItems \
+	$(LOCAL_PATH)/../Common/cpp/headers/SpecTools \
+	$(LOCAL_PATH)/../Common/cpp/headers/Tools \
+	$(HERMES_ENGINE)/android/include 
 
 LOCAL_CFLAGS += -DONANDROID -fvisibility=hidden -fexceptions -frtti
 
