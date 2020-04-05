88456ea228f95754c1aa15549dd1a6b9a15623a9
Jakub Gonet <jakub.gonet@swmansion.com>
2020-04-05 19:55:00 +0200

    Add config for babel and metro

--- ReanimatedExample/babel.config.js
+++ ReanimatedExample/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-tab-view': '../src/index',
+        },
+      },
+    ],
+  ],
 };
--- ReanimatedExample/metro.config.js
+++ ReanimatedExample/metro.config.js
@@ -1,4 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const glob = require('glob-to-regexp');
+
+const root = path.resolve(__dirname, '..');
+
+const packages = JSON.parse(
+  fs.readFileSync(path.join(root, 'package.json'), 'utf8'),
+);
+
+const modules = [
+  '@babel/runtime',
+  ...Object.keys({
+    ...packages.dependencies,
+    ...packages.peerDependencies,
+  }),
+];
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${root}/node_modules/*`),
+    glob(`${root}/docs/*`),
+    glob(`${root}/Example/*`),
+  ];
+  return blacklist(nodeModuleDirs);
+}
+
 module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [root],
+  resolver: {
+    blacklistRE: getBlacklist(),
+    extraNodeModules: modules.reduce((cfg, name) => {
+      cfg[name] = path.join(__dirname, 'node_modules', name);
+      return cfg;
+    }, {}),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
