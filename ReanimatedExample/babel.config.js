954aa2081df61fae388f605901fb730872d7e7a0
Jakub Gonet <jakub.gonet@swmansion.com>
2020-04-06 11:41:34 +0200

    Use yarn relative path to include REA

--- ReanimatedExample/babel.config.js
+++ ReanimatedExample/babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          'react-native-tab-view': '../src/index',
-        },
-      },
-    ],
-  ],
 };
