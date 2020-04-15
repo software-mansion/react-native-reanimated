81fc4050537a4d006deb228ce9c80970544f773a
ShaMan123 <shacharnen@gmail.com>
2020-04-15 15:18:16 +0300

    fix merge

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,4 +1,24 @@
+const path = require('path');
+const modules = [
+  'react',
+  'react-native',
+  '@babel/runtime',
+  'fbjs',
+  'hoist-non-react-statics',
+  'invariant',
+  'prop-types'
+];
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      'react-native-reanimated': path.resolve(__dirname, '..'),
+      ...modules.reduce((a, name) => {
+        a[name] = path.resolve(__dirname, 'node_modules', name);
+        return a;
+      }, {})
+    }
+  },
+  watchFolders: [path.resolve(__dirname, '..')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
