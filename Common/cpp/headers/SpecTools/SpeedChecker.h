cff07c2c56f58cf5b7d2c14498f00e6d039aee99
szymon <szymon.kapala@swmansion.com>
2020-04-23 09:11:58 +0200

    add a way to turn off speedcheck logs

--- Common/cpp/headers/SpecTools/SpeedChecker.h
+++ Common/cpp/headers/SpecTools/SpeedChecker.h
@@ -8,17 +8,23 @@
 #ifndef SpeedChecker_h
 #define SpeedChecker_h
 
+#define CHECK_SPEED 0
+
 #include "Logger.h"
 
 class SpeedChecker {
 public:
   static void checkSpeed(std::string tag, std::function<void()> fun) {
-  //  auto start = std::chrono::system_clock::now();
+#if CHECK_SPEED
+    auto start = std::chrono::system_clock::now();
+#endif
     fun();
-  //  auto end = std::chrono::system_clock::now();
-  //  std::chrono::duration<double> elapsed_seconds = end-start;
-    //tag += " " + std::to_string(elapsed_seconds.count()) + "s";
-   // Logger::log(tag.c_str());
+#if CHECK_SPEED
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    tag += " " + std::to_string(elapsed_seconds.count()) + "s";
+    Logger::log(tag.c_str());
+#endif
   }
 };
 
