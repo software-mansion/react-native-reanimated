53c9f9993c6919eb8f8cf271ff937d86d4b3bd94
Oleg Repin <iamolegga@gmail.com>
2020-05-12 13:15:34 +0300

    Instantiate Value only once in useValue hook

--- src/useValue.js
+++ src/useValue.js
@@ -2,5 +2,9 @@ import React from 'react';
 import AnimatedValue from './core/AnimatedValue';
 
 export default function useValue(initialValue) {
-  return React.useRef(new AnimatedValue(initialValue)).current;
+  const ref = React.useRef(null);
+  if (ref.current === null) {
+    ref.current = new AnimatedValue(initialValue);
+  }
+  return ref.current;
 }
