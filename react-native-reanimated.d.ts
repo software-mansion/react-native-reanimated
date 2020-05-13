da1948afe20085eef32c998af02f4c1274b57f11
Aleksandr Klyavlin <hastomit@gmail.com>
2020-05-13 19:51:51 +0300

    Fix strict mode typings (#722)
    Partially fixes #719
    My bad 😞 Forgot that transform property on TransformsStyle['transform'] is optional so in strict mode inference must check for undefined
    
    Co-authored-by: hastom <aklyavlin@onvis.ru>

--- react-native-reanimated.d.ts
+++ react-native-reanimated.d.ts
@@ -147,8 +147,8 @@ declare module 'react-native-reanimated' {
 
     export const SpringUtils: SpringUtils
 
-    export type TransformStyleTypes = TransformsStyle['transform'] extends readonly (infer T)[] ? T : never
-    export type AdaptTransforms<T> = { [P in keyof T]: Animated.Adaptable<T[P]> }
+    export type TransformStyleTypes = TransformsStyle['transform'] extends (readonly (infer T)[] | undefined) ? T : never
+    export type AdaptTransforms<T> = { [P in keyof T]: Adaptable<T[P] extends string ? number | string : T[P]> }
     export type AnimatedTransform = (AdaptTransforms<TransformStyleTypes>)[]
 
     export type AnimateStyle<S extends object> = {
