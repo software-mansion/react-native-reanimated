9094df23d41536193d4fed52a93bd3938cbeeb10
Jakub <jakub.gonet@swmansion.com>
2020-06-01 16:12:57 +0200

    Add mocks for Reanimated 2 (#838)
    ## Description
    
    Adds mocks for Reanimated 2 for file `ReanimatedModule.js`

--- src/core/Core.test.js
+++ src/core/Core.test.js
@@ -5,6 +5,7 @@ import renderer from 'react-test-renderer';
 
 jest.mock('../ReanimatedEventEmitter');
 jest.mock('../ReanimatedModule');
+jest.mock('../reanimated2/NativeReanimated.js');
 
 describe('Core Animated components', () => {
   xit('fails if something other then a node or function that returns a node is passed to Animated.Code exec prop', () => {
---
+++ src/reanimated2/__mocks__/NativeReanimated.js
@@ -0,0 +1,10 @@
+export default {
+  installCoreFunctions: () => {},
+  makeShareable: () => {},
+  makeMutable: () => {},
+  makeRemote: () => {},
+  startMapper: () => {},
+  stopMapper: () => {},
+  registerEventHandler: () => {},
+  unregisterEventHandler: () => {},
+};
