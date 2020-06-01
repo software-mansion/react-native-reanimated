e25c97e2aa6637d917c8616b68e3952f943b0e41
szymon <szymon.kapala@swmansion.com>
2020-06-01 15:15:12 +0200

    stage 1

--- Common/cpp/Registries/WorkletsCache.cpp
+++ Common/cpp/Registries/WorkletsCache.cpp
@@ -18,8 +18,8 @@ jsi::Function function(jsi::Runtime &rt, const std::string& code) {
   return eval(rt, ("(" + code + ")").c_str()).getObject(rt).getFunction(rt);
 }
 
-std::shared_ptr<jsi::Function> WorkletsCache::getFunction(jsi::Runtime & rt, std::shared_ptr<FrozenObject> frozenObj) {
-  int workletId = frozenObj->map["__workletID"]->numberValue;
+std::shared_ptr<jsi::Function> WorkletsCache::getFunction(jsi::Runtime &rt, std::shared_ptr<FrozenObject> frozenObj) {
+  long long workletId = frozenObj->map["__workletID"]->numberValue;
   if (worklets.count(workletId) == 0) {
     jsi::Function fun = function(rt, frozenObj->map["asString"]->stringValue);
     std::shared_ptr<jsi::Function> funPtr(new jsi::Function(std::move(fun)));
--- plugin.js
+++ plugin.js
@@ -149,11 +149,11 @@ function processWorkletFunction(t, fun) {
   // if we don't clone other modules won't process parts of newFun defined below
   // this is weird but couldn't find a better way to force transform helper to
   // process the function
+  const workletID = Math.random() * 1e18;
   const clone = t.cloneNode(fun.node);
   const funExpression = t.functionExpression(null, clone.params, clone.body);
 
   const funString = buildWorkletString(t, fun, variables);
-  const workletID = fun.start;
 
   const newFun = t.functionExpression(
     fun.id,
