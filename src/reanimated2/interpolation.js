c36f1132915e359127139facf4a524edd2f091d3
Michał Osadnik <micosa97@gmail.com>
2020-06-04 00:49:41 +0200

    Update interpolation.js

--- src/reanimated2/interpolation.js
+++ src/reanimated2/interpolation.js
@@ -30,6 +30,9 @@ function internalInterpolate(x, l, r, ll, rr, type) {
 
 export function interpolate(x, input, output, type) {
   'worklet';
+  if (x && x.__nodeID) {
+    throw new Error('Reanimated: interpolate from V1 has been renamed to interpolateNode.')
+  }
   const length = input.length;
   let narrowedInput = [];
   if (x < input[0]) {
