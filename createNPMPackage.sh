a1d1576c5e8124489c19d0736d7902b62aba9094
szymon <szymon.kapala@swmansion.com>
2020-06-16 13:13:47 +0200

    stage 0.01
    stage 0.1
    
    stage 0.2
    
    stage 0.2
    
    stage 0.3
    
    stage 0.3
    
    stage 0.31
    
    stage 0.5
    
    stage 0.6
    
    stage 1
    
    cleanup
    
    get rid of jsc
    
    tigger building every time
    
    cleanup

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -3,62 +3,30 @@ set -e
 
 ROOT=$(pwd)
 
+# part I - I (clear old aar)
+
+rm -rf android/build/outputs/aar/*.aar
+cd android 
+gradle clean
+
 # part I (prepare android-npm)
 
-cd Example/android/
-./gradlew assembleRelease
-cd ../node_modules/react-native/ReactAndroid/build/react-ndk/all
-
-for d in *
-do
-  echo "processing $d"
-  mkdir -p  "${ROOT}/android/src/main/JNILibs/${d}"
-  cp "${d}/libreanimated.so" "${ROOT}/android/src/main/JNILibs/${d}/."
-done
-
-cd "${ROOT}"
-rm -r android/build/outputs/aar/*.aar
-
-cd  Example/android/
-
-
- ./gradlew clean
- ./gradlew aR
-
-# clean so
-
-
-cd ../node_modules/react-native/ReactAndroid/build/react-ndk/all
-
-rm -rf "${ROOT}/android/src/main/JNILibs/"
-
+gradle :assembleDebug
 cd $ROOT
 
-rm -f android-npm/react-native-reanimated.aar
+rm -rf android-npm/react-native-reanimated.aar
 cp android/build/outputs/aar/*.aar android-npm/react-native-reanimated.aar
-
-# PART II (create package)
-
 
 mv android android-temp
 mv android-npm android
 
+# PART II (create package)
+
 npm pack
 
-#mv android android-npm
-#mv android-temp android
+# PART III (clean)
 
+mv android android-npm
+mv android-temp android
 
 echo "Done!"
-
-# How to generate a keystore: https://coronalabs.com/blog/2014/08/26/tutorial-understanding-android-app-signing/
-
-
-: "
-This script is extremely slow right now, so it's better to do the following steps:
-- build Example project
-- copy so files to android/src/main/JNILibs
-- click on react-native-reaniamted module in AS and then build->make module
-- copy aar from android/build/outputs/aar/ to android-npm/react-native-reanimated.aar
-- execute the second part of the script
-"
