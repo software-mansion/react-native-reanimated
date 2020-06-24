b472a6575ea0f46d300ae699b2fe6c45b7787f18
Szymon20000 <szymon.kapala@swmansion.com>
2020-06-24 09:44:31 +0200

    [Android] Improve building. (#900)
    WHY:
    To speed up building.
    
    HOW:
    1 Added necessary tasks to Android/build.gradle similar to tasks from react-native/ReactAndroid/build.gradle.
    2 Created Application.mk
    3 Created Android.mk
    4 Add important lines from ReactAndroid/src/main/jni/react/jni/Android.mk (I've tried to import/include this module but it didn't work)
    5 update createNPMPackage.sh

--- android/src/main/JNI/Android.mk
+++ android/src/main/JNI/Android.mk
@@ -2,15 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(warning $(LOCAL_PATH))
-HERMES_ENGINE := $(LOCAL_PATH)/../Example/node_modules/hermes-engine
-$(warning $(Hermes-engine))
-$(warning $(Hermes-engine)/android/include)
-
-
 LOCAL_MODULE := reanimated
 
-PROJECT_FILES := $(wildcard $(LOCAL_PATH)/src/main/cpp/*.cpp)
+PROJECT_FILES := $(wildcard $(LOCAL_PATH)/../cpp/*.cpp)
 PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/*.cpp)
 PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/**/*.cpp)
 
@@ -18,19 +12,21 @@ PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := $(PROJECT_FILES)
 
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/src/main/cpp/headers \
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	$(LOCAL_PATH)/../cpp/headers \
 	$(LOCAL_PATH)/../Common/cpp/headers \
 	$(LOCAL_PATH)/../Common/cpp/headers/NativeModules \
 	$(LOCAL_PATH)/../Common/cpp/headers/Registries \
 	$(LOCAL_PATH)/../Common/cpp/headers/SharedItems \
 	$(LOCAL_PATH)/../Common/cpp/headers/SpecTools \
 	$(LOCAL_PATH)/../Common/cpp/headers/Tools \
-	$(HERMES_ENGINE)/android/include 
+	$(HERMES_ENGINE)/android/include \
 
-LOCAL_CFLAGS += -DONANDROID -fvisibility=hidden -fexceptions -frtti
+LOCAL_CFLAGS += -DONANDROID -fexceptions -frtti
 
-LOCAL_STATIC_LIBRARIES := libjsi libjsireact libcallinvoker callinvokerholder
-LOCAL_SHARED_LIBRARIES := libfolly_json libfb libreactnativejni libhermes libfbjni
+LOCAL_STATIC_LIBRARIES := libjsi
+LOCAL_SHARED_LIBRARIES := libfolly_json libreactnativejni libhermes libfbjni
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(LOCAL_PATH)/react/Android.mk
