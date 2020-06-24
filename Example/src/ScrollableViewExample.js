233d932d8e2bafae7ab429ba2b07f8541a32902b
Terry Sahaidak <tasssik@gmail.com>
2020-06-24 10:35:05 +0300

    Make sure to use single onGestureEvent
    Co-authored-by: Szymon20000 <szymon20000@gmail.com>

--- Example/src/ScrollableViewExample.js
+++ Example/src/ScrollableViewExample.js
@@ -92,8 +92,7 @@ function ScrollableView({ children }) {
   return (
     <View style={{ flex: 1 }}>
       <PanGestureHandler
-        onGestureEvent={handler}
-        onHandlerStateChange={handler}>
+        onGestureEvent={handler}>
         <Animated.View style={styles}>
           <View onLayout={onLayout}>{children}</View>
         </Animated.View>
