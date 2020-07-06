823db00f5c513af3cf510498e2b97447a55b3e82
szymon <szymon.kapala@swmansion.com>
2020-07-06 14:16:51 +0200

    cleanup

--- ios/native/RENativeMethods.h
+++ ios/native/RENativeMethods.h
@@ -1,11 +1,8 @@
 #import <Foundation/Foundation.h>
-#include <vector>
-#include <string>
+#import <vector>
+#import <string>
 #import <React/RCTUIManager.h>
-
-NS_ASSUME_NONNULL_BEGIN
 
 std::vector<std::pair<std::string,double>> measure(int viewTag, RCTUIManager *uiManager);
 void scrollTo(int scrollViewTag, RCTUIManager *uiManager, double x, double y, bool animated);
 
-NS_ASSUME_NONNULL_END
