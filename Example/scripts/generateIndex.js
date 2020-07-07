000399299f825bf60a5b4b62b18720478cd15710
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-07-07 15:30:10 +0200

    tests init

---
+++ Example/scripts/generateIndex.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path')
+
+const cwd = process.cwd()
+
+// check if in Example directory
+const cwdSplit = cwd.split(path.sep)
+if (cwdSplit[cwdSplit.length - 1] !== 'Example') {
+    console.log('invalid path, please enter the Example directory')
+    process.exit(1)
+}
+
+const inputPath = cwd + path.sep + 'index-template.js'
+const outputPath = cwd + path.sep + 'index.js'
+// read command line arguments
+const args = []
+process.argv.forEach((value, index) => {
+    if (index <= 1) return;
+    args.push(value)
+})
+
+console.log(`reading file ${ inputPath }`)
+let content = fs.readFileSync(inputPath).toString()
+
+// replace
+const replaces = {
+    'component': ['App', 'TestApp'],
+    'path': ['./src/App', './test/TestApp'],
+}
+
+const replaceIndex = (args.indexOf('--test') !== -1) ? 1 : 0
+Object.keys(replaces).forEach((key) => {
+    content = content.split('${' + key + '}').join(replaces[key][replaceIndex])
+})
+
+console.log(`writing to ${outputPath}`)
+fs.writeFileSync(outputPath, content, function(err) {
+    if(err) {
+        console.log(err);
+        process.exit(1)
+    }
+});
+
+process.exit(0);
