109a6bc1fda102988a59b1888c1d11c2d12cf8b4
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-07-07 15:30:10 +0200

    tests init

---
+++ Example/test/TestApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const TestApp = () => {
+    return (
+        <View>
+            <Text>working TEST</Text>
+        </View>
+    )
+}
+
+export default TestApp;
