fedbe54e114d5c40afff79d0c889fcce09e88d23
Jakub Gonet <jakub.gonet@swmansion.com>
2020-07-08 13:58:34 +0200

    Revert "Add the converting code of deg at animated value"
    This reverts commit 87717fc52acfc197f3c7a5235c74ada8bf30f402.

--- src/core/AnimatedValue.js
+++ src/core/AnimatedValue.js
@@ -28,17 +28,6 @@ export default class AnimatedValue extends InternalAnimatedValue {
   }
 
   interpolate(config) {
-    _convertToRadians(config);
     return interpolate(this, config);
-  }
-
-  // FIXME: Where to put this code???
-  _convertToRadians(config) {
-    if (typeof config.outputRange[0] == 'string' && config.outputRange.some(v => v.includes('deg'))) {
-      config.outputRange = config.outputRange
-        .map(v => v.slice(0, v.indexOf('deg')))
-        .map(Number)
-        .map(v => v * Math.PI / 180)
-    }
   }
 }
