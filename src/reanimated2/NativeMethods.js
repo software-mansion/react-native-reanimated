53c388d36cef17646015abc860806c7ff3bb9a0d
szymon <szymon.kapala@swmansion.com>
2020-07-13 09:14:35 +0200

    export our 'findNodeHandle' method

--- src/reanimated2/NativeMethods.js
+++ src/reanimated2/NativeMethods.js
@@ -1,11 +1,17 @@
+import { findNodeHandle } from 'react-native';
+
+export function getTag(view) {
+  return findNodeHandle(view);
+}
+
 export function measure(viewTag) {
   'worklet';
   if (!_WORKLET) {
-    throw '(measure) method cannot be used on RN side!';
+    throw new Error('(measure) method cannot be used on RN side!');
   }
   const result = _measure(viewTag);
   if (result.x === -1234567) {
-    throw `The view with tag ${viewTag} could not be measured`;
+    throw new Error(`The view with tag ${viewTag} could not be measured`);
   }
   return result;
 }
@@ -13,7 +19,7 @@ export function measure(viewTag) {
 export function scrollTo(viewTag, x, y, animated) {
   'worklet';
   if (!_WORKLET) {
-    throw '(scrollTo) method cannot be used on RN side!';
+    throw new Error('(scrollTo) method cannot be used on RN side!');
   }
   _scrollTo(viewTag, x, y, animated);
 }
