ad22b70f4b315e99806fb10ad424fa9bf534b140
Jakub Gonet <jakub.gonet@swmansion.com>
2020-07-22 16:40:28 +0200

    Fix passing nodes directly to the Animated.Code

--- src/core/AnimatedCode.js
+++ src/core/AnimatedCode.js
@@ -1,8 +1,30 @@
 import useCode from '../derived/useCode';
 
-function Code({ exec, children, dependencies = [] }) {
-  useCode(children || exec, dependencies);
+function assertNodesNotNull(code, children, exec) {
+  if (!code) {
+    const error = !children
+      ? `Got "${typeof children}" type passed to children`
+      : `Got "${typeof exec}" type passed to exec`;
 
+    throw new Error(
+      `<Animated.Code /> expects the 'exec' prop or children to be an animated node or a function returning an animated node. ${error}`
+    );
+  }
+}
+
+function Code({ exec, children, dependencies = [] }) {
+  const nodes = children || exec;
+
+  let code = null;
+  if (nodes instanceof AnimatedNode) {
+    code = () => nodes;
+  } else if (typeof nodes === 'function') {
+    code = nodes;
+  }
+
+  assertNodesNotNull(code, children, exec);
+
+  useCode(code, dependencies);
   return null;
 }
 
