software-mansion/react-native-reanimated

commit 35e28d4143c829a180901f2a009fe2558078e7da

Fix flickering on mount (#1027)
## Description

Fixes #219 
Fixes #762

Previously components were flickering when mounted due to asynchronously updating component styles.

In `createAnimatedComponent()` we filtered out styles with Animated nodes, then component rendered with default values,
then nodes were initialized on the native side and applied their styles.
This issue was mostly observed when layout styles were changed, for example when setting height like in the example below.

This fix relies on updating the value of `Value` when nodes are detached (`__detach()` in `InternalAnimatedValue`) because it updates its value on the JS side. Without that this fix would only work on the first mount (when initial values are used).

## Example

```js
import * as React from 'react';
import {Button, Text, View} from 'react-native';
import Animated from 'react-native-reanimated';

export default function Flickering() {
  const value = Animated.useValue(80);
  const [i, setI] = React.useState(0);
  const triggerRemount = () => {
    setI(i => i + 1);
  };
  return (
    <View
      key={i}
      style={{
        height: '100%',
        backgroundColor: 'white',
        position: 'relative',
        paddingTop: 100,
      }}>
      <Animated.View
        style={{
          height: value,
          position: 'absolute',
          width: '100%',
          backgroundColor: 'green',
        }}>
        <Text style={{color: 'white', fontSize: 15}}>Some footer</Text>
      </Animated.View>
      <Button
        onPress={() => {
          triggerRemount();
        }}
        style={{marginTop: 100}}
        title="Trigger remount!"
      />
    </View>
  );
}
```

src/createAnimatedComponent.js
@@ -197,8 +197,12 @@ export default function createAnimatedComponent(Component) {
       const style = {};
       for (const key in inputStyle) {
         const value = inputStyle[key];
-        if (!(value instanceof AnimatedNode) && key !== 'transform') {
-          style[key] = value;
+        if (key !== 'transform') {
+          if (value instanceof AnimatedNode) {
+            style[key] = value.__getValue();
+          } else {
+            style[key] = value;
+          }
         }
       }
       return style;
@@ -216,7 +220,9 @@ export default function createAnimatedComponent(Component) {
           // alltogether. Therefore we provide a dummy callback here to allow
           // native event dispatcher to hijack events.
           props[key] = dummyListener;
-        } else if (!(value instanceof AnimatedNode)) {
+        } else if (value instanceof AnimatedNode) {
+          props[key] = value.__getValue();
+        } else {
           props[key] = value;
         }
       }

commit abb597967bcc02c8b3f5509d698693bbba49fa5b

Fix initial value for props
This fixes 35e28d4 which introduced regression causing Malformed call from JS error in RN.

src/createAnimatedComponent.js
@@ -4,6 +4,7 @@ import ReanimatedEventEmitter from './ReanimatedEventEmitter';
 
 import AnimatedEvent from './core/AnimatedEvent';
 import AnimatedNode from './core/AnimatedNode';
+import AnimatedValue from './core/AnimatedValue';
 import { createOrReusePropsNode } from './core/AnimatedProps';
 
 import invariant from 'fbjs/lib/invariant';
@@ -198,9 +199,9 @@ export default function createAnimatedComponent(Component) {
       for (const key in inputStyle) {
         const value = inputStyle[key];
         if (key !== 'transform') {
-          if (value instanceof AnimatedNode) {
-            style[key] = value.__getValue();
-          } else {
+          if (value instanceof AnimatedValue) {
+            style[key] = value._startingValue;
+          } else if (!(value instanceof AnimatedNode)) {
             style[key] = value;
           }
         }
@@ -220,9 +221,9 @@ export default function createAnimatedComponent(Component) {
           // alltogether. Therefore we provide a dummy callback here to allow
           // native event dispatcher to hijack events.
           props[key] = dummyListener;
-        } else if (value instanceof AnimatedNode) {
-          props[key] = value.__getValue();
-        } else {
+        } else if (value instanceof AnimatedValue) {
+          props[key] = value._startingValue;
+        } else if (!(value instanceof AnimatedNode)) {
           props[key] = value;
         }
       }
@@ -247,7 +248,9 @@ export default function createAnimatedComponent(Component) {
     }
   }
 
-  AnimatedComponent.displayName = `AnimatedComponent(${Component.displayName || Component.name || 'Component'})`
+  AnimatedComponent.displayName = `AnimatedComponent(${Component.displayName ||
+    Component.name ||
+    'Component'})`;
 
   return AnimatedComponent;
 }