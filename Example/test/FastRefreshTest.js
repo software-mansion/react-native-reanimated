99553f1d2fbd40b4b091fd988ce93daca24ef00d
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-08-06 13:29:26 +0200

    update tests

--- Example/test/FastRefreshTest.js
+++ Example/test/FastRefreshTest.js
@@ -4,9 +4,7 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   useDerivedValue,
-  useEvent,
 } from 'react-native-reanimated';
-import { PanGestureHandler } from 'react-native-gesture-handler';
 
 const Child = ({ expanded }) => {
   const uas = useAnimatedStyle(() => {
@@ -26,14 +24,17 @@ const FastRefreshTest = () => {
   const [expanded, setExpanded] = useState(true);
 
   const shared = useSharedValue(state);
+  const testResult = useSharedValue(0);
 
-  // test useDerivedValue
   const derived = useDerivedValue(() => {
     return state;
-  }, [state]);
+  });
 
-  const derivedNotListening = useDerivedValue(() => {
-    return state;
+  const resultUas = useAnimatedStyle(() => {
+    const color = testResult.value === 1 ? 'green' : 'red';
+    return {
+      backgroundColor: color,
+    };
   });
 
   // test useAnimatedStyle
@@ -43,39 +44,29 @@ const FastRefreshTest = () => {
     };
   });
 
-  const uasListening = useAnimatedStyle(() => {
+  const uasListeningOne = useAnimatedStyle(() => {
     return {
       width: state,
     };
   }, [state]);
 
-  const uasNotListening = useAnimatedStyle(() => {
+  const uasListeningAll = useAnimatedStyle(() => {
     return {
       width: state,
     };
   });
 
+  const uasNotListening = useAnimatedStyle(() => {
+    return {
+      width: state,
+    };
+  }, []);
+
   const update = () => {
+    testResult.value = 0;
     setState(Math.floor(Math.random() * 300));
     setExpanded(!expanded);
   };
-
-  // test useEvent
-  const flag = useSharedValue(1);
-  const eventHandler = useEvent(
-    (event) => {
-      if (flag.value === 1) {
-        setState(Math.floor(Math.random() * 300));
-        flag.value = 0;
-        setTimeout(() => {
-          flag.value = 1;
-        }, 100);
-      }
-    },
-    ['onGestureHandlerStateChange', 'onGestureHandlerEvent']
-  );
-
-  let bgColor;
 
   // sometimes we have to wait for shared value to be set and then read properly
   const waitingTimeout = 100;
@@ -84,43 +75,36 @@ const FastRefreshTest = () => {
     const delay = Date.now() - startCounting;
     if (delay > waitingTimeout) {
       console.log('timeout exceeded waiting for shared value: ' + delay);
-      bgColor = 'red';
       break;
     }
   }
 
-  if (bgColor === undefined) {
-    bgColor =
-      state === derived.value &&
-      state === shared.value &&
-      state !== derivedNotListening.value
-        ? 'green'
-        : 'red';
-  }
-
   return (
     <View>
-      <Text>
-        Press the button or try to drag the box on the bottom to do testing
-      </Text>
+      <Text>Press the button to do testing</Text>
       <Button title="change state" onPress={update} />
-      <Text style={{ padding: 10, backgroundColor: bgColor }}>
-        Those should be the same: {state}/{derived.value}/{shared.value} but not
-        this one: {derivedNotListening.value}
-      </Text>
+      <Button
+        title="check derived values"
+        onPress={() => {
+          if (derived.value === state) {
+            testResult.value = 1;
+          } else {
+            testResult.value = 0;
+          }
+        }}
+      />
+      <Text>This box should be green after pressing checking button</Text>
+      <Animated.View
+        style={[{ width: 100, height: 20, backgroundColor: 'gray' }, resultUas]}
+      />
       <Text>The lengths of the following containers should be the same</Text>
       <Animated.View style={[styles.box, uasShared]} />
-      <Animated.View style={[styles.box, uasListening]} />
+      <Animated.View style={[styles.box, uasListeningOne]} />
+      <Animated.View style={[styles.box, uasListeningAll]} />
       <Text>This should alternate(long/short) on button press</Text>
       <Child expanded={expanded} />
       <Text>But this should not update</Text>
       <Animated.View style={[styles.box, uasNotListening]} />
-      <Text>Event test, try to drag the box below to trigger changes</Text>
-      <PanGestureHandler onGestureEvent={eventHandler}>
-        <Animated.View
-          style={[{ backgroundColor: 'green', width: 100, height: 100 }]}
-        />
-      </PanGestureHandler>
     </View>
   );
 };
