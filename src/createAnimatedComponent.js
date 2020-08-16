ff5c9942cbc15c4a9ae4d5822327bb48df87ff02
Tsang Chin Lung <hello@cltsang.com>
2020-08-16 11:39:34 +0000

    Defend against nullish value (#1113)
    ## Description
    
    `hasAnimatedNodes(value)` object property check was crashing because `null` is considered as an object.
    
    ## Example
    
    Any `null` value in style object will crash.
    
    ``` js
    import Animated from 'react-native-reanimated';
    import {TextInput} from 'react-native';
    import React from 'react';
    
    const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
    
    export default function App(props) {
      return (
        <AnimatedTextInput
          style={{ backgroundColor: null }}
        />
      );
    };
    ```
    
    Co-authored-by: Jakub <jakub.gonet@swmansion.com>

--- src/createAnimatedComponent.js
+++ src/createAnimatedComponent.js
@@ -31,7 +31,7 @@ function hasAnimatedNodes(value) {
   if (Array.isArray(value)) {
     return value.some((item) => hasAnimatedNodes(item));
   }
-  if (typeof value === 'object') {
+  if (value && typeof value === 'object') {
     return Object.keys(value).some((key) => hasAnimatedNodes(value[key]));
   }
   return false;
