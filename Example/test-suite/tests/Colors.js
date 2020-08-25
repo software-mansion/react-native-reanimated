9e804bf06379717b4182c4a86a97c1b791182ef2
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-08-25 09:17:32 +0200

    update colors test

--- Example/test-suite/tests/Colors.js
+++ Example/test-suite/tests/Colors.js
@@ -5,29 +5,23 @@ export const name = 'Colors';
 
 export function test(t) {
   t.describe('colors', () => {
-    // test basic colors
-    t.it('process red color', () => {
-      const padding = 2;
-      // test red
-      const redStr = processColor('red')
-        .toString(16)
-        .substring(padding + 0, padding + 2);
-      const redNum = Number.parseInt(redStr, 16);
-      t.expect(redNum).not.toBe(0);
+    t.it('process basic colors', () => {
+      const testData = [
+        ['red', 'ff0000'],
+        ['green', '008000'],
+        ['blue', '0000ff'],
+      ];
 
-      // test green
-      const greenStr = processColor('green')
-        .toString(16)
-        .substring(padding + 2, padding + 4);
-      const greenNum = Number.parseInt(greenStr, 16);
-      t.expect(greenNum).not.toBe(0);
+      const obtainColorString = (color) => {
+        const padding = 2;
+        return processColor(color)
+          .toString(16)
+          .substring(padding);
+      };
 
-      // test blue
-      const blueStr = processColor('blue')
-        .toString(16)
-        .substring(padding + 4, padding + 6);
-      const blueNum = Number.parseInt(blueStr, 16);
-      t.expect(blueNum).not.toBe(0);
+      testData.forEach(([color, expectedString]) => {
+        t.expect(obtainColorString(color)).toBe(expectedString);
+      });
     });
   });
 }
