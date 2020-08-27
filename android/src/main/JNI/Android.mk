e45f558d115933b92d8fe5368fe5269f4ab66733
Szymon20000 <szymon.kapala@swmansion.com>
2020-08-27 10:23:30 +0200

    Use CallInvoker to fix Promises in callbacks. (#1160)
    ## WHY
    fixes: https://github.com/software-mansion/react-native-reanimated/issues/1131
    
    ## HOW
    Pass CallInvokerHolder to the NativeProxy just like we do with jsContext. The only hack used here is creating an empty shared library `libturbomodulejsijni` that's loaded by `CallInovokerHolderImpl.java`. It's not directly used by The `CallInovokerHolderImpl` so it's safe.

--- android/src/main/JNI/Android.mk
+++ android/src/main/JNI/Android.mk
@@ -24,9 +24,16 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 
 LOCAL_CFLAGS += -DONANDROID -fexceptions -frtti
 
-LOCAL_STATIC_LIBRARIES := libjsi 
+LOCAL_STATIC_LIBRARIES := libjsi callinvokerholder
 LOCAL_SHARED_LIBRARIES := libhermes libfolly_json libfbjni libreactnativejni
 
 include $(BUILD_SHARED_LIBRARY)
+
+# start | build empty library which is needed by CallInvokerHolderImpl.java
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := turbomodulejsijni
+include $(BUILD_SHARED_LIBRARY)
+# end
 
 include $(LOCAL_PATH)/react/Android.mk
