bffa62b5441ac9712ec0ba3f78d4cd1aa31d1c74
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-09-10 09:44:53 +0200

    add example screens to the web

--- Example/src/WebApp.js
+++ Example/src/WebApp.js
@@ -1,44 +1,164 @@
+import { createBrowserApp } from '@react-navigation/web';
 import React from 'react';
-import { View, Button } from 'react-native';
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from 'react-native-reanimated';
+import {
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  YellowBox,
+} from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function AnimatedStyleUpdateExample() {
-  const randomWidth = useSharedValue(10);
+import AnimatedStyleUpdateExample from './AnimatedStyleUpdateExample';
+import WobbleExample from './WobbleExample';
+import DragAndSnapExample from './DragAndSnapExample';
+import ScrollEventExample from './ScrollEventExample';
+import ChatHeadsExample from './ChatHeadsExample';
+import MeasureExample from './MeasureExample';
+import SwipeableListExample from './SwipeableListExample';
+import ScrollableViewExample from './ScrollableViewExample';
+import ScrollToExample from './ScrollToExample';
+/* font awesome does not work * /
+import AnimatedTabBarExample from './AnimatedTabBarExample';
+/**/
+import LightboxExample from './LightboxExample';
+/* masked view does not work * /
+import LiquidSwipe from './LiquidSwipe';
+/**/
+YellowBox.ignoreWarnings(['Calling `getNode()`']);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+const SCREENS = {
+  AnimatedStyleUpdate: {
+    screen: AnimatedStyleUpdateExample,
+    title: '🆕 Animated Style Update',
+  },
+  WobbleExample: {
+    screen: WobbleExample,
+    title: '🆕 Animation Modifiers (Wobble Effect)',
+  },
+  DragAndSnapExample: {
+    screen: DragAndSnapExample,
+    title: '🆕 Drag and Snap',
+  },
+  MeasureExample: {
+    screen: MeasureExample,
+    title: '🆕 Synchronous Measure',
+  },
+  ScrollEventExample: {
+    screen: ScrollEventExample,
+    title: '🆕 Scroll Events',
+  },
+  ChatHeadsExample: {
+    screen: ChatHeadsExample,
+    title: '🆕 Chat Heads',
+  },
+  ScrollableToExample: {
+    screen: ScrollToExample,
+    title: '🆕 scrollTo',
+  },
+  SwipeableListExample: {
+    screen: SwipeableListExample,
+    title: '🆕 (advanced) Swipeable List',
+  },
+  LightboxExample: {
+    screen: LightboxExample,
+    title: '🆕 (advanced) Lightbox',
+  },
+  ScrollableViewExample: {
+    screen: ScrollableViewExample,
+    title: '🆕 (advanced) ScrollView imitation',
+  },
+  /** /
+  AnimatedTabBarExample: {
+    screen: AnimatedTabBarExample,
+    title: '🆕 (advanced) Tab Bar Example',
+  },
+  /** /
+  LiquidSwipe: {
+    screen: LiquidSwipe,
+    title: '🆕 Liquid Swipe Example',
+  },
+  /**/
+};
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: randomWidth.value,
-    };
-  });
-
+function MainScreen({ navigation }) {
+  const data = Object.keys(SCREENS).map((key) => ({ key }));
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = withTiming(Math.random() * 350, config);
-        }}
-      />
-    </View>
+    <FlatList
+      style={styles.list}
+      data={data}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={(props) => (
+        <MainScreenItem
+          {...props}
+          screens={SCREENS}
+          onPressItem={({ key }) => navigation.navigate(key)}
+        />
+      )}
+      renderScrollComponent={(props) => <ScrollView {...props} />}
+    />
   );
 }
+
+MainScreen.navigationOptions = {
+  title: '🎬 Reanimated 2.x Examples',
+};
+
+export function ItemSeparator() {
+  return <View style={styles.separator} />;
+}
+
+export function MainScreenItem({ item, onPressItem, screens }) {
+  const { key } = item;
+  return (
+    <RectButton style={styles.button} onPress={() => onPressItem(item)}>
+      <Text style={styles.buttonText}>{screens[key].title || key}</Text>
+    </RectButton>
+  );
+}
+
+const Reanimated2App = createStackNavigator(
+  {
+    Main: { screen: MainScreen },
+    ...SCREENS,
+  },
+  {
+    initialRouteName: 'Main',
+    headerMode: 'screen',
+  }
+);
+
+const ExampleApp = createSwitchNavigator({
+  Reanimated2App,
+});
+
+export const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#EFEFF4',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DBDBE0',
+  },
+  buttonText: {
+    backgroundColor: 'transparent',
+  },
+  button: {
+    flex: 1,
+    height: 60,
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+const createApp = Platform.select({
+  web: (input) => createBrowserApp(input, { history: 'hash' }),
+  default: (input) => createAppContainer(input),
+});
+
+export default createApp(ExampleApp);
