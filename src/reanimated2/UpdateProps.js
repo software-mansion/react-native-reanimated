38418d3b4e4f0a91c633727f9728fabc8819c054
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-09-11 13:15:01 +0200

    fix colors & ref check

--- src/reanimated2/UpdateProps.js
+++ src/reanimated2/UpdateProps.js
@@ -1,6 +1,7 @@
 /* global _updateProps _updatePropsJS */
 import processColor from './Colors';
 import { makeShareable } from './core';
+import { Platform } from 'react-native';
 
 // copied from react-native/Libraries/Components/View/ReactNativeStyleAttributes
 const colorProps = [
@@ -24,11 +25,14 @@ const ColorProperties = makeShareable(colorProps);
 
 export default function updateProps(viewTag, updates, maybeViewRef) {
   'worklet';
-  Object.keys(updates).forEach((key) => {
-    if (ColorProperties.indexOf(key) !== -1) {
-      updates[key] = processColor(updates[key]);
-    }
-  });
+
+  if (Platform.OS !== 'web') {
+    Object.keys(updates).forEach((key) => {
+      if (ColorProperties.indexOf(key) !== -1) {
+        updates[key] = processColor(updates[key]);
+      }
+    });
+  }
 
   const updatePropsInternal =
     typeof _updateProps === 'undefined' ? _updatePropsJS : _updateProps;
