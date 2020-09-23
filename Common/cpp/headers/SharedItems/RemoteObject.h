3d0694e756c418a3231fd188bb4a200299d5553a
Szymon20000 <szymon.kapala@swmansion.com>
2020-09-23 13:32:48 +0200

    revert (#1268)
    In useAnimatedStyle we hardly rely on RemoteObject ability to return the same object everytime we call ToJSValue on UI thread. The pr brings back the ability by reverting all remote object changes introduced by #1236.

--- Common/cpp/headers/SharedItems/RemoteObject.h
+++ Common/cpp/headers/SharedItems/RemoteObject.h
@@ -8,13 +8,18 @@ using namespace facebook;
 
 namespace reanimated {
 
-class RemoteObject: public jsi::HostObject {
+class RemoteObject: public jsi::HostObject, public StoreUser {
 private:
+  NativeReanimatedModule *module;
+  std::weak_ptr<jsi::Value> backing;
   std::unique_ptr<FrozenObject> initializer;
 public:
-  jsi::Value returnObject(jsi::Runtime &rt);
+  void maybeInitializeOnUIRuntime(jsi::Runtime &rt);
   RemoteObject(jsi::Runtime &rt, jsi::Object &object, NativeReanimatedModule *module):
-    initializer(new FrozenObject(rt, object, module)) {}
+    module(module), initializer(new FrozenObject(rt, object, module)) {}
+  void set(jsi::Runtime &rt, const jsi::PropNameID &name, const jsi::Value &value);
+  jsi::Value get(jsi::Runtime &rt, const jsi::PropNameID &name);
+  std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime &rt);
 };
 
 }
