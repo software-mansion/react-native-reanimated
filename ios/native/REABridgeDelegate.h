ee7eee723ed72e218aa03565b8ec7d132d44a9a4
szymon <szymon.kapala@swmansion.com>
2020-10-08 10:35:10 +0200

    improve

--- ios/native/REABridgeDelegate.h
+++ ios/native/REABridgeDelegate.h
@@ -6,11 +6,11 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <React/RCTBridgeDelegate.h>
+#import <React/RCTCxxBridgeDelegate.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface REABridgeDelegate : NSObject
+@interface REABridgeDelegate : NSObject <RCTCxxBridgeDelegate>
 
 - (instancetype)initWithDelegate:(id<RCTBridgeDelegate>)delegate;
 
