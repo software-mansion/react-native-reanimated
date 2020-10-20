6c4efc1efe051d2ccbdab0c61d6ad76f1fcd97b4
karol-bisztyga <59833830+karol-bisztyga@users.noreply.github.com>
2020-10-20 09:34:25 +0200

    Shared value rerender upgrade (#1351)
    ## Description
    
    This PR makes `useSharedValue` more consistent with other hooks when needed. Now it would change on every rerender when the value passed to the hook changes.
    So in the following scenario
    ```
    const [state, setState] = useState(0);
    const sv = useSharedValue(state);
    ```
    every time `state` changes(and component rerenders) `sv`'s value is updated to what's in `state`.
    
    This is inconsistent with for instance `useState`, for which the state has to be set explicitly to get modified. However the default behavior persisted for backward compatibility.
    
    
    Fixes https://github.com/software-mansion/react-native-reanimated/issues/1293

--- react-native-reanimated.d.ts
+++ react-native-reanimated.d.ts
@@ -452,7 +452,8 @@ declare module 'react-native-reanimated' {
 
     // reanimated2 hooks
     export function useSharedValue<T>(
-      initialValue: T
+      initialValue: T,
+      shouldRebuild?: boolean
     ): T extends SharedValueType ? SharedValue<T> : never;
 
     export function useDerivedValue<T extends SharedValueType>(
