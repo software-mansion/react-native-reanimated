062fdd68915993815138351d781365b0c4d16008
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-11-10 12:30:47 +0100

    cleanup 1

--- Example/src/repro/libt/types.ts
+++
@@ -1,6 +0,0 @@
-export type GalleryItemType = {
-  id: string;
-  width: number;
-  height: number;
-  uri: string;
-};
--- Example/src/repro/libt/types/react-native-reanimated.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-native-reanimated' {
-  function makeRemote<T>(value: T): T;
-
-  function useEvent<T>(
-    handler: T,
-    events: string[],
-    rebuild: boolean,
-  ): T;
-}
