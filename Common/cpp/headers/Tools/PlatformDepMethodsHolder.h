64b61db0c89356f6d1abf50ab2d3a44f12b4e736
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-11-13 13:06:25 +0100

    fix rebase

--- Common/cpp/headers/Tools/PlatformDepMethodsHolder.h
+++ Common/cpp/headers/Tools/PlatformDepMethodsHolder.h
@@ -9,7 +9,7 @@ namespace reanimated
 {
 
 using UpdaterFunction = std::function<void(jsi::Runtime &rt, int viewTag, const jsi::Value &viewName, const jsi::Object &object)>;
-using RequestRender = std::function<void(std::function<void(double)>)>;
+using RequestRender = std::function<void(std::function<void(double)>, jsi::Runtime &rt)>;
 using ScrollToFunction = std::function<void(int, double, double, bool)>;
 using MeasuringFunction = std::function<std::vector<std::pair<std::string, double>>(int)>;
 using TimeProviderFunction = std::function<double(void)>;
