6f19c070b3734a26b194ed4f45ec189f1b4437d4
ArMANIAK <reklama.alatyr@gmail.com>
2020-12-19 13:36:52 +0200

    feat:JSDocs for colors

--- src/reanimated2/platform-specific/android/Colors.js
+++ src/reanimated2/platform-specific/android/Colors.js
@@ -1,7 +1,22 @@
+/**@module android/colors */
+
+/**
+ * on Android color is represented as signed 32 bit int
+ * @param {int} c color in RGB
+ * @return {int} 32bit int
+ */
 export const colorTo32bitInt = c => {
     return c < (1 << 31) >>> 0 ? c : c - Math.pow(2, 32);
 }
 
+/**
+ * Android use 32 bit *signed* integer to represent the color
+ * We utilize the fact that bitwise operations in JS also operates on
+ * signed 32 bit integers, so that we can use those to convert from
+ * *unsigned* to *signed* 32bit int that way.
+ * @param {int} unsigned32bit 
+ * @return {int} signed 32bit int
+ */
 export const convertToSigned = unsigned32bit => {
     return unsigned32bit | 0x0;
 }
--- src/reanimated2/platform-specific/web/Colors.js
+++ src/reanimated2/platform-specific/web/Colors.js
@@ -1,3 +1,13 @@
+/**@module web/colors */
+
+/**
+ * 
+ * @param {int} r value of red channel in range 0 to 255
+ * @param {int} g value of green channel in range 0 to 255
+ * @param {int} b value of blue channel in range 0 to 255
+ * @param {float} alpha value of alpha-channel (transperancy) in range 0 to 1
+ * @return {string} css specific property of color in rgba(r, g, b, alpha, format)
+ */
 export const webRGB = (r, g, b, alpha) => {
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
