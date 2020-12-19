b730c5e1d3ec89467cd55c8f03064345a5d38318
Rostyslav Broslavskyi <r.broslavskyi@omo.systems>
2020-12-19 16:35:03 +0200

    [fix] Prettier

--- react-native-reanimated.d.ts
+++ react-native-reanimated.d.ts
@@ -2,13 +2,7 @@
 // TypeScript Version: 2.8
 
 declare module 'react-native-reanimated' {
-  import {
-    ComponentClass,
-    ReactNode,
-    Component,
-    RefObject,
-    ComponentType,
-  } from 'react';
+  import { ComponentClass, ReactNode, Component, RefObject, ComponentType } from 'react';
   import {
     ViewProps,
     TextProps,
@@ -58,10 +52,7 @@ declare module 'react-native-reanimated' {
       IDENTITY = 'identity',
     }
 
-    type ExtrapolateParameter =
-      | Extrapolate
-      | { extrapolateLeft?: Extrapolate; extrapolateRight?: Extrapolate }
-      | undefined;
+    type ExtrapolateParameter = Extrapolate | { extrapolateLeft?: Extrapolate, extrapolateRight?: Extrapolate } | undefined 
 
     interface InterpolationConfig {
       inputRange: ReadonlyArray<Adaptable<number>>;
@@ -253,10 +244,7 @@ declare module 'react-native-reanimated' {
       getNode(): ReactNativeScrollView;
     }
     export class Code extends Component<CodeProps> {}
-    export function createAnimatedComponent<
-      S extends object,
-      P extends { style?: StyleProp<S> }
-    >(component: ComponentType<P>): ComponentType<AnimateProps<S, P>>;
+    export function createAnimatedComponent<S extends object, P extends { style?: StyleProp<S>; }>(component: ComponentType<P>): ComponentType<AnimateProps<S, P>>;
 
     // classes
     export {
@@ -435,10 +423,7 @@ declare module 'react-native-reanimated' {
     export function cancelAnimation<T extends SharedValue<SharedValueType>>(
       sharedValue: T
     ): void;
-    export function withDelay(
-      delayMS: number,
-      delayedAnimation: number
-    ): number;
+    export function withDelay(delayMS: number, delayedAnimation: number): number;
     export function withRepeat(
       animation: number,
       numberOfReps?: number,
@@ -499,10 +484,8 @@ declare module 'react-native-reanimated' {
       effects: (dependencies: D) => void,
       deps?: DependencyList
     );
-
-    export type AnimatedStyleProp<T extends object> =
-      | AnimateStyle<T>
-      | RegisteredStyle<AnimateStyle<T>>;
+                        
+    export type AnimatedStyleProp<T extends object> = AnimateStyle<T> | RegisteredStyle<AnimateStyle<T>>;
     export function useAnimatedStyle<
       T extends AnimatedStyleProp<ViewStyle | ImageStyle | TextStyle>
     >(updater: () => T, deps?: DependencyList): T;
