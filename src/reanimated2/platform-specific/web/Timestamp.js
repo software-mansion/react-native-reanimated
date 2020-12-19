ec79b6371d28364421b2a18e7ebb03534a76da15
ArMANIAK <reklama.alatyr@gmail.com>
2020-12-19 14:42:49 +0200

    feat: JSDocs for Timestamp

--- src/reanimated2/platform-specific/web/Timestamp.js
+++ src/reanimated2/platform-specific/web/Timestamp.js
@@ -1,5 +1,9 @@
+/**@module web/Timestamp */
 
-
+/**
+ * Receiving timestamp from browser
+ * @return {int} timestamp of a moment of time function was called 
+ */
 export const getWebTimestamp = () => {
     return window.performance.now();
 }
