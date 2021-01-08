f8fd8d4ee360ac914c24ae39b28760b6cc8b8298
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-01-08 17:32:35 +0100

    make built ins worklets

--- src/reanimated2/PropAdapters.js
+++ src/reanimated2/PropAdapters.js
@@ -1,11 +1,13 @@
 import { useAnimatedPropAdapter } from './Hooks';
 
 export const SVGAdapter = useAnimatedPropAdapter((props) => {
+  'worklet';
   // todo
 });
 
 export const TextInputAdapter = useAnimatedPropAdapter(
   (props) => {
+    'worklet';
     const keys = Object.keys(props);
     // convert text to value like RN does here: https://github.com/facebook/react-native/blob/master/Libraries/Components/TextInput/TextInput.js#L878
     if (keys.includes('value')) {
