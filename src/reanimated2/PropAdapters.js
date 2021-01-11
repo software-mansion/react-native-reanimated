0626d31cc01931d59f98d206c11cead2e9c1fb18
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-01-11 15:47:30 +0100

    handle transform string case

--- src/reanimated2/PropAdapters.js
+++ src/reanimated2/PropAdapters.js
@@ -3,23 +3,33 @@ import { createAnimatedPropAdapter } from './core';
 export const SVGAdapter = createAnimatedPropAdapter((props) => {
   'worklet';
   const keys = Object.keys(props);
-  // transform - taken from https://github.com/react-native-svg/react-native-svg/blob/develop/src/elements/Shape.tsx#L200
-  if (keys.includes('transform') && Array.isArray(props.transform)) {
-    // should be an array of 6 elements
-    if (props.transform.length !== 6) {
-      throw new Error(
-        `invalid transform length of ${props.transform.length}, should be 6`
-      );
+  // transform
+  if (keys.includes('transform')) {
+    if (Array.isArray(props.transform)) {
+      // case of array with 6 values => https://github.com/react-native-svg/react-native-svg/blob/develop/src/elements/Shape.tsx#L200
+      if (props.transform.length !== 6) {
+        throw new Error(
+          `invalid transform length of ${props.transform.length}, should be 6`
+        );
+      }
+      const transform = props.transform;
+      const x = props.x || 0;
+      const y = props.y || 0;
+      props.transform = [
+        { translateX: transform[0] * x + transform[2] * y + transform[4] },
+        { translateX: transform[1] * x + transform[3] * y + transform[5] },
+      ];
+    } else if (typeof props.transform === 'string') {
+      // case of string 'translate(translateX translateY)'
+      // todo: handle other cases of transform string like here https://github.com/react-native-svg/react-native-svg/blob/develop/src/lib/extract/extractTransform.ts#L184
+      const arr = props.transform
+        .replace('translate(', '')
+        .replace(')', '')
+        .split(' ');
+      props.transform = [{ translateX: arr[0] }, { translateX: arr[1] }];
     }
-    const transform = props.transform;
-    const x = props.x || 0;
-    const y = props.y || 0;
-    props.transform = [
-      { translateX: transform[0] * x + transform[2] * y + transform[4] },
-      { translateX: transform[1] * x + transform[3] * y + transform[5] },
-    ];
   }
-  // todo: other cases
+  // todo: other props
 });
 
 export const TextInputAdapter = createAnimatedPropAdapter(
