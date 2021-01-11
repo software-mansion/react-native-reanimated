1135624836c76186d26f166bc71156cea90de5f9
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-01-11 12:10:15 +0100

    add transform case to SVG

--- src/reanimated2/PropAdapters.js
+++ src/reanimated2/PropAdapters.js
@@ -2,7 +2,24 @@ import { useAnimatedPropAdapter } from './core';
 
 export const SVGAdapter = useAnimatedPropAdapter((props) => {
   'worklet';
-  // todo
+  const keys = Object.keys(props);
+  // transform - taken from https://github.com/react-native-svg/react-native-svg/blob/develop/src/elements/Shape.tsx#L200
+  if (keys.includes('transform') && Array.isArray(props.transform)) {
+    // should be an array of 6 elements
+    if (props.transform.length !== 6) {
+      throw new Error(
+        `invalid transform length of ${props.transform.length}, should be 6`
+      );
+    }
+    const transform = props.transform;
+    const x = props.x || 0;
+    const y = props.y || 0;
+    props.transform = [
+      { translateX: transform[0] * x + transform[2] * y + transform[4] },
+      { translateX: transform[1] * x + transform[3] * y + transform[5] },
+    ];
+  }
+  // todo: other cases
 });
 
 export const TextInputAdapter = useAnimatedPropAdapter(
