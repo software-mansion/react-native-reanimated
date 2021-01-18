a3d561e7ace81574459afed2eeb90895b952c6f8
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-01-18 10:29:44 +0100

    mend

--- Example/src/AnimatedStyleUpdateExample.js
+++ Example/src/AnimatedStyleUpdateExample.js
@@ -9,14 +9,6 @@ import React from 'react';
 
 export default function AnimatedStyleUpdateExample(props) {
   const randomWidth = useSharedValue(10);
-  const v1 = useSharedValue('tmp');
-  const v2 = useSharedValue(true);
-  const v3 = useSharedValue(5);
-  const v4 = useSharedValue({ v1: 'v1', v2: 1 });
-  const v5 = useSharedValue([1, 2, 3]);
-  const v6 = useSharedValue([1, '', () => {}, {}]);
-
-  console.log(v1, v2, v3, v4, v5, v6);
 
   const config = {
     duration: 500,
@@ -44,7 +36,6 @@ export default function AnimatedStyleUpdateExample(props) {
       <Button
         title="toggle"
         onPress={() => {
-          console.log(randomWidth);
           randomWidth.value = Math.random() * 350;
         }}
       />
