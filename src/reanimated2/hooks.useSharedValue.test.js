abc0bccf3c51ce6e8d4806af64c4c9f95eeb901f
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-01-25 14:07:37 +0100

    update unit tests

--- src/reanimated2/hooks.useSharedValue.test.js
+++ src/reanimated2/hooks.useSharedValue.test.js
@@ -7,38 +7,13 @@ jest.mock('../ReanimatedModule');
 jest.mock('./NativeReanimated');
 
 describe('useSharedValue', () => {
-  it('update value when shouldRebuild = true', () => {
+  it('retains value on rerender', () => {
     // Given
-    const shouldRebuild = true;
     const initialValue = 0;
     const updatedValue = 1;
 
     function TestComponent(props) {
-      const opacity = useSharedValue(props.value, shouldRebuild);
-      return <Animated.View style={{ opacity: opacity.value }} />;
-    }
-
-    // When rendering with initial value
-    const wrapper = renderer.create(
-      <TestComponent key="box" value={initialValue} />
-    );
-
-    expect(wrapper.root.children[0].props.style.opacity).toBe(initialValue);
-
-    // When rendering with updated value
-    wrapper.update(<TestComponent key="box" value={updatedValue} />);
-
-    expect(wrapper.root.children[0].props.style.opacity).toBe(updatedValue);
-  });
-
-  it('retains value when shouldRebuild = false', () => {
-    // Given
-    const shouldRebuild = false;
-    const initialValue = 0;
-    const updatedValue = 1;
-
-    function TestComponent(props) {
-      const opacity = useSharedValue(props.value, shouldRebuild);
+      const opacity = useSharedValue(props.value);
       return <Animated.View style={{ opacity: opacity.value }} />;
     }
 
