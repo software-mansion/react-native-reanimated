4a8adb83c1fa028560e076f57c9ab393b8a2dec9
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-02-02 18:18:33 +0100

    Revert example file

--- Example/src/AnimatedStyleUpdateExample.js
+++ Example/src/AnimatedStyleUpdateExample.js
@@ -1,96 +1,44 @@
-// import Animated, {
-//   useSharedValue,
-//   useAnimatedReaction,
-//   withTiming,
-//   withDecay,
-//   withRepeat,
-//   withSequence,
-//   withSpring,
-//   withDelay,
-//   useAnimatedRef,
-//   useAnimatedGestureHandler,
-//   measure,
-//   useAnimatedStyle,
-// } from 'react-native-reanimated';
-// import {
-//   PanGestureHandler,
-//   TouchableWithoutFeedback,
-// } from 'react-native-gesture-handler';
-// import { View, Button, Text, StyleSheet } from 'react-native';
-// import React, { useLayoutEffect, useEffect, useState } from 'react';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button } from 'react-native';
+import React from 'react';
 
-// function ProgressBarControl() {
-//   const progress = useSharedValue(0);
-//   const boxRef = useAnimatedRef();
+export default function AnimatedStyleUpdateExample(props) {
+  const randomWidth = useSharedValue(10);
 
-//   const makeProgress = () => {
-//     progress.value = withTiming(Math.min(progress.value + 0.2, 1));
-//   };
-//   const reset = () => {
-//     progress.value = withTiming(0);
-//   };
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
 
-//   const barStyle = useAnimatedStyle(() => {
-//     return {
-//       width: `${progress.value * 100}%`,
-//     };
-//   });
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
 
-//   const gestureHandler = useAnimatedGestureHandler({
-//     onStart: (_, ctx) => {
-//       ctx.startingProgress = progress.value;
-//     },
-//     onActive: (e, ctx) => {
-//       const { width } = measure(boxRef);
-//       const progressTranslation = e.translationX / width;
-//       progress.value = ctx.startingProgress + progressTranslation;
-//     },
-//     onEnd: (e) => {
-//       const { width } = measure(boxRef);
-//       progress.value = withDecay({
-//         velocity: e.velocityX / width,
-//         clamp: [0, 1],
-//       });
-//     },
-//   });
-
-//   return (
-//     <>
-//       <View ref={boxRef} style={styles.barContainer}>
-//         <PanGestureHandler onGestureEvent={gestureHandler}>
-//           <Animated.View style={[styles.bar, barStyle]} />
-//         </PanGestureHandler>
-//       </View>
-//       <Button onPress={makeProgress} title="make progress" />
-//       <Button onPress={reset} title="start over" />
-//     </>
-//   );
-// }
-
-// export default function Main(props) {
-//   return (
-//     <View style={styles.container}>
-//       <ProgressBarControl />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     marginTop: 150,
-//     marginHorizontal: 20,
-//     height: 120,
-//     justifyContent: 'space-between',
-//   },
-//   barContainer: {
-//     height: 50,
-//     borderColor: '#001A72',
-//     borderWidth: 1,
-//     borderRadius: 5,
-//     overflow: 'hidden',
-//   },
-//   bar: {
-//     backgroundColor: '#001A72',
-//     height: 50,
-//   },
-// });
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+      }}>
+      <Animated.View
+        style={[
+          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
+          style,
+        ]}
+      />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
+    </View>
+  );
+}
