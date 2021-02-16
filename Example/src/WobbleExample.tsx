b15baeed68eaea7176c6288140140fd58eb44073
Jakub Gonet <jakub.gonet@swmansion.com>
2021-02-16 15:49:53 +0100

    Add types to WobbleExample

--- Example/src/WobbleExample.tsx
+++ Example/src/WobbleExample.tsx
@@ -7,13 +7,13 @@ import Animated, {
   withSequence,
 } from 'react-native-reanimated';
 import { View, Button, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { FC } from 'react';
 
 const ANGLE = 9;
 const TIME = 100;
 const EASING = Easing.elastic(1.5);
 
-export default function WobbleExample(props) {
+const WobbleExample: FC = () => {
   const rotation = useSharedValue(1);
 
   const style = useAnimatedStyle(() => {
@@ -51,7 +51,7 @@ export default function WobbleExample(props) {
       />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   box: {
@@ -63,3 +63,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#001a72',
   },
 });
+
+export default WobbleExample;
