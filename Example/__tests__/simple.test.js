7053a40c423fe8a4e066afae77f1f279a0257be7
szymon <szymon.kapala@swmansion.com>
2021-02-16 13:54:22 +0100

    stage 0.1

--- Example/__tests__/simple.test.js
+++ Example/__tests__/simple.test.js
@@ -1,14 +1,68 @@
-import React, { Text, View } from 'react';
+import React from 'react';
+import { Text, View } from 'react-native';
 import {
     render,
     fireEvent
 } from '@testing-library/react-native';
+//import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+
+/*function AnimatedBox() {
+    const sv = useSharedValue(50);
+    const style = useAnimatedStyle(() => {
+        return {
+            width: sv.value,
+        }
+    });
+
+    return (
+        <View>
+            <Animated.View testID="id" style={{}} handleClick={() => {
+                sv.value = (sv.value + 50) % 200;
+            }} />
+        </View>
+    )
+}*/
+
+/*describe("aniamted", () => {
+    test('UAS', () => {
+        const { getByTestId } = render(<AnimatedBox/>);
+        const animatedView = getByTestId("id");
+
+        expect(animatedView).toHaveAnimatedStyle({
+            width: 50,
+        });
+
+        reAct(() => {
+            fireEvent(animatedView, "handleClick");
+        })
+
+        expect(animatedView).toHaveAnimatedStyle({
+            width: 100,
+        });
+
+        reAct(() => {
+            fireEvent(animatedView, "handleClick");
+        })
+
+        expect(animatedView.props.style.width).
+    })
+})*/
+
+function PropText({text}) {
+    return (
+        <View style={{backgroundColor: 'red'}}>
+            <Text>
+                {text}
+            </Text>
+        </View>
+    );
+}
 
 function Comp() {
     return (
         <View>
-            <Text>ooos</Text>
-            <Text>Janusz</Text>
+            <Text testID="text">ooos</Text>
+            <PropText text="Janusz"/>
         </View>
     )
 }
@@ -27,7 +81,20 @@ describe("comp test", () => {
     test('mount', async () => {
 
         const w = (<Comp/>)
-        const {findByText, findAllByText} = render(w)
-        expect(5).toBe(5);
+        const {getByTestId, findAllByText} = render(w)
+        const object = getByTestId("text");
+        expect(object.type).toBe("Text");
+       // console.log(object.parent);
+        expect(object.parent.parent.type).toBe("View")
+
+        const propText = object.parent.parent.children[1];
+
+        propText.animatedProps = { width: 300 };
+
+        expect(JSON.stringify(propText.animatedProps)).toBe(JSON.stringify({width: 300}))
+
+        console.log(propText)
+        
+        expect(object.parent.parent.children[1].children[0].props.style.backgroundColor).toBe("green")
     })
 })
---
+++ Example/jest-setup.js
@@ -0,0 +1,6 @@
+
+/*jest.mock('react-native-reanimated', () => {
+    const Reanimated = require('../src/reanimated2/__mocks__');
+
+    return Reanimated;
+});*/
