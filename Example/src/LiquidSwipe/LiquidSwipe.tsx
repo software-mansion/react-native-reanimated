46311f4ea9d22cb1f897c0158d97155e1ed18f4c
Jakub Gonet <jakub.gonet@swmansion.com>
2021-02-22 15:48:35 +0100

    Remove unused import

--- Example/src/LiquidSwipe/LiquidSwipe.tsx
+++ Example/src/LiquidSwipe/LiquidSwipe.tsx
@@ -1,4 +1,4 @@
-import React, { ReactComponentElement } from 'react';
+import React from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
 import Animated, {
   useSharedValue,
