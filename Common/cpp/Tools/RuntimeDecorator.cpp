software-mansion/react-native-reanimated

commit 424bb2e29235844ab9721aec08c77991eef57d34

Resolve pod conflict (#1775)
## Description

The problem very well described [here](https://github.com/CocoaPods/CocoaPods/issues/9248) (Freeform description section)

## Possible solution
- turn off USE_HEADERMAPS
- move header file to `preserved_paths`

Common/cpp/Tools/RuntimeDecorator.cpp
@@ -1,5 +1,5 @@
 #include "RuntimeDecorator.h"
-#include "Logger.h"
+#include "Common/cpp/hidden_headers/Logger.h"
 #include <unordered_map>
 #include <memory>
 

Common/cpp/hidden_headers/SpeedChecker.h
@@ -2,7 +2,7 @@
 
 #define CHECK_SPEED 0
 
-#include "Logger.h"
+#include "./Logger.h"
 
 namespace reanimated
 {

ios/native/REAIOSLogger.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "LoggerInterface.h"
+#include "Common/cpp/hidden_headers/LoggerInterface.h"
 #include <stdio.h>
 
 namespace reanimated {

ios/native/REAIOSLogger.mm
@@ -1,5 +1,5 @@
 #include "REAIOSLogger.h"
-#include "Logger.h"
+#include "Common/cpp/hidden_headers/Logger.h"
 #import <Foundation/Foundation.h>
 
 namespace reanimated {

Common/cpp/NativeModules/NativeReanimatedModule.cpp
@@ -1,6 +1,4 @@
 #include "NativeReanimatedModule.h"
-#include "Logger.h"
-#include "SpeedChecker.h"
 #include "ShareableValue.h"
 #include "MapperRegistry.h"
 #include "Mapper.h"

Common/cpp/SharedItems/ShareableValue.cpp
@@ -1,7 +1,6 @@
 #include "ShareableValue.h"
 #include "SharedParent.h"
 #include "NativeReanimatedModule.h"
-#include "Logger.h"
 #include "MutableValue.h"
 #include "MutableValueSetterProxy.h"
 #include "RemoteObject.h"

Common/cpp/headers/SharedItems/ShareableValue.h
@@ -2,7 +2,6 @@
 
 #include "WorkletsCache.h"
 #include "SharedParent.h"
-#include "Logger.h"
 #include "ValueWrapper.h"
 #include "HostFunctionHandler.h"
 #include "JSIStoreValueUser.h"

Common/cpp/hidden_headers/Logger.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "LoggerInterface.h"
+#include "./LoggerInterface.h"
 #include <memory>
 
 namespace reanimated

commit 040b04430dcb77e4fae77750943610b7b1b96c87

fix headers (#1779)
fixing headers problem on Android which was introduced with https://github.com/software-mansion/react-native-reanimated/pull/1775/files

Common/cpp/Tools/RuntimeDecorator.cpp
@@ -1,5 +1,5 @@
 #include "RuntimeDecorator.h"
-#include "Common/cpp/hidden_headers/Logger.h"
+#include "ReanimatedHiddenHeaders.h"
 #include <unordered_map>
 #include <memory>
 

Common/cpp/headers/Tools/ReanimatedHiddenHeaders.h
@@ -1,0 +1,15 @@
+
+#ifndef REANIMATEDEXAMPLE_REANIMATEDHIDDEN_H
+#define REANIMATEDEXAMPLE_REANIMATEDHIDDEN_H
+
+#if defined(ONANDROID)
+    #include "Logger.h"
+    #include "LoggerInterface.h"
+    #include "SpeedChecker.h"
+#else
+    #include "../../hidden_headers/Logger.h"
+    #include "../../hidden_headers/LoggerInterface.h"
+    #include "../../hidden_headers/SpeedChecker.h"
+#endif
+
+#endif //REANIMATEDEXAMPLE_REANIMATEDHIDDEN_H

Common/cpp/hidden_headers/SpeedChecker.h
@@ -3,6 +3,9 @@
 #define CHECK_SPEED 0
 
 #include "./Logger.h"
+#include <memory>
+#include <functional>
+#include <string>
 
 namespace reanimated
 {

android/src/main/JNI/Android.mk
@@ -12,15 +12,16 @@ PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := $(PROJECT_FILES)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-	$(LOCAL_PATH)/../cpp/headers \
-	$(LOCAL_PATH)/../Common/cpp/headers \
-	$(LOCAL_PATH)/../Common/cpp/headers/NativeModules \
-	$(LOCAL_PATH)/../Common/cpp/headers/Registries \
-	$(LOCAL_PATH)/../Common/cpp/headers/SharedItems \
-	$(LOCAL_PATH)/../Common/cpp/headers/SpecTools \
-	$(LOCAL_PATH)/../Common/cpp/headers/Tools \
-	$(HERMES_ENGINE)/android/include \
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/hidden_headers
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../cpp/headers
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers/NativeModules
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers/Registries
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers/SharedItems
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers/SpecTools
+LOCAL_C_INCLUDES +=	$(LOCAL_PATH)/../Common/cpp/headers/Tools
+LOCAL_C_INCLUDES +=	$(HERMES_ENGINE)/android/include
 
 LOCAL_CFLAGS += -DONANDROID -fexceptions -frtti
 

ios/native/REAIOSLogger.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Common/cpp/hidden_headers/LoggerInterface.h"
+#include "ReanimatedHiddenHeaders.h"
 #include <stdio.h>
 
 namespace reanimated {

ios/native/REAIOSLogger.mm
@@ -1,5 +1,4 @@
 #include "REAIOSLogger.h"
-#include "Common/cpp/hidden_headers/Logger.h"
 #import <Foundation/Foundation.h>
 
 namespace reanimated {