02758ce91f5e767230b8063f61505f67e9eb60ea
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-03-05 11:42:14 +0100

    Remove old versions

--- docs/src/theme/Navbar/index.js
+++ docs/src/theme/Navbar/index.js
@@ -226,7 +226,6 @@ function Navbar() {
                   [styles.hideLogoText]: isSearchBarExpanded,
                 })}>
                 {title}
-                <span className="badge badge--info">V2 stable</span>
               </strong>
             )}
           </Link>
