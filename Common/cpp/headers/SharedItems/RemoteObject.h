7fc83c725e0c2cf4cd448dc2711b316616ed9926
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-08 14:05:38 +0100

    Remove unused RuntimeManager*

--- Common/cpp/headers/SharedItems/RemoteObject.h
+++ Common/cpp/headers/SharedItems/RemoteObject.h
@@ -3,7 +3,6 @@
 #include "SharedParent.h"
 #include "FrozenObject.h"
 #include "JSIStoreValueUser.h"
-#include "RuntimeManager.h"
 
 using namespace facebook;
 
@@ -11,13 +10,12 @@ namespace reanimated {
 
 class RemoteObject: public jsi::HostObject, public StoreUser {
 private:
-  RuntimeManager *runtimeManager;
   std::weak_ptr<jsi::Value> backing;
   std::unique_ptr<FrozenObject> initializer;
 public:
   void maybeInitializeOnUIRuntime(jsi::Runtime &rt);
   RemoteObject(jsi::Runtime &rt, jsi::Object &object, RuntimeManager *runtimeManager, std::shared_ptr<Scheduler> s):
-     StoreUser(s), runtimeManager(runtimeManager), initializer(new FrozenObject(rt, object, runtimeManager)) {}
+     StoreUser(s), initializer(new FrozenObject(rt, object, runtimeManager)) {}
   void set(jsi::Runtime &rt, const jsi::PropNameID &name, const jsi::Value &value);
   jsi::Value get(jsi::Runtime &rt, const jsi::PropNameID &name);
   std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime &rt);
