0867beb77d553527bf5569e9eaec5f005d850110
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-09 23:49:13 +0100

    Make worklets from WorkletsCache static

--- Common/cpp/Registries/WorkletsCache.cpp
+++ Common/cpp/Registries/WorkletsCache.cpp
@@ -7,7 +7,7 @@ using namespace facebook;
 namespace reanimated
 {
 
-std::unordered_map<long long, std::shared_ptr<jsi::Function>> worklets;
+static std::unordered_map<long long, std::shared_ptr<jsi::Function>> worklets;
 
 jsi::Value eval(jsi::Runtime &rt, const char *code) {
   return rt.global().getPropertyAsFunction(rt, "eval").call(rt, code);
