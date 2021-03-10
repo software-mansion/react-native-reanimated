66fee65877653fc7dec8f4c73cf4833e801fcf71
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-03-10 18:37:08 +0100

    Clean up

--- src/reanimated2/UpdateProps.js
+++ src/reanimated2/UpdateProps.js
@@ -24,6 +24,30 @@ const colorProps = [
 
 const ColorProperties = makeShareable(colorProps);
 
+const updateSingleProps = (viewDescriptor, updates, maybeViewRef, adapters) => {
+  'worklet';
+  const viewName = viewDescriptor?.name || 'RCTView';
+
+  if (adapters) {
+    adapters.forEach((adapter) => {
+      adapter(updates);
+    });
+  }
+
+  if (Platform.OS !== 'web') {
+    Object.keys(updates).forEach((key) => {
+      if (ColorProperties.includes(key)) {
+        updates[key] = processColor(updates[key]);
+      }
+    });
+  }
+
+  const updatePropsInternal =
+    typeof _updateProps === 'undefined' ? _updatePropsJS : _updateProps;
+
+  updatePropsInternal(viewDescriptor?.tag, viewName, updates, maybeViewRef);
+};
+
 export const updateProps = (
   viewDescriptors,
   updates,
@@ -32,44 +56,13 @@ export const updateProps = (
 ) => {
   'worklet';
 
-  const updatePropsInternal =
-    typeof _updateProps === 'undefined' ? _updatePropsJS : _updateProps;
-
-  const updateSingleProps = (
-    _viewDescriptorsValue,
-    _updates,
-    _maybeViewRef
-  ) => {
-    const viewName = _viewDescriptorsValue?.name || 'RCTView';
-
-    if (adapters) {
-      adapters.forEach((adapter) => {
-        adapter(updates);
-      });
-    }
-
-    if (Platform.OS !== 'web') {
-      Object.keys(updates).forEach((key) => {
-        if (ColorProperties.includes(key)) {
-          updates[key] = processColor(updates[key]);
-        }
-      });
-    }
-
-    updatePropsInternal(
-      _viewDescriptorsValue?.tag,
-      viewName,
-      _updates,
-      _maybeViewRef
-    );
-  };
   if (Platform.OS !== 'web') {
     viewDescriptors.value.forEach((item, _) => {
-      updateSingleProps(item, updates, null);
+      updateSingleProps(item, updates, null, adapters);
     });
   } else {
     maybeViewRef.items.forEach((item, _) => {
-      updateSingleProps(null, updates, item);
+      updateSingleProps(null, updates, item, adapters);
     });
   }
 };
--- src/reanimated2/ViewDescriptorsSet.js
+++ src/reanimated2/ViewDescriptorsSet.js
@@ -5,32 +5,30 @@ export function makeViewDescriptorsSet() {
   const ref = useRef(null);
   if (ref.current === null) {
     const data = {
-      batchToInsert: [],
       batchToRemove: new Set(),
-      existsTags: new Set(),
+      tags: new Set(),
       waitForInsertSync: false,
       waitForRemoveSync: false,
       workletViewDescriptors: makeMutable([]),
       items: [],
 
       add: (item) => {
-        if (data.existsTags.has(item.tag)) {
+        if (data.tags.has(item.tag)) {
           return;
         }
-        data.existsTags.add(item.tag);
-        data.batchToInsert.push(item);
+        data.tags.add(item.tag);
+        data.items.push(item);
 
         if (!data.waitForInsertSync) {
           data.waitForInsertSync = true;
 
           setImmediate(() => {
-            data.items = data.items.concat(data.batchToInsert);
             data.workletViewDescriptors.value = data.items;
-            data.batchToInsert = [];
             data.waitForInsertSync = false;
           });
         }
       },
+
       remove: (viewTag) => {
         data.batchToRemove.add(viewTag);
 
@@ -41,7 +39,7 @@ export function makeViewDescriptorsSet() {
             const items = [];
             for (const item of data.items) {
               if (data.batchToRemove.has(item.tag)) {
-                data.existsTags.delete(item.tag);
+                data.tags.delete(item.tag);
               } else {
                 items.push(item);
               }
@@ -53,6 +51,7 @@ export function makeViewDescriptorsSet() {
           });
         }
       },
+
       rebuildWorkletViewDescriptors: (workletViewDescriptors) => {
         data.workletViewDescriptors = workletViewDescriptors;
       },
@@ -73,6 +72,7 @@ export function makeViewsRefSet() {
         if (data.items.has(item)) return;
         data.items.add(item);
       },
+
       remove: (item) => {
         data.items.delete(item);
       },
