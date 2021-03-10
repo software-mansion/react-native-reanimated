def0998df7dc6e124f8f9b78f1a26ec2c21fd147
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-10 12:35:24 +0100

    Make runtime unique_ptr

--- Common/cpp/headers/SharedItems/RuntimeManager.h
+++ Common/cpp/headers/SharedItems/RuntimeManager.h
@@ -23,9 +23,9 @@ using namespace facebook;
  */
 class RuntimeManager {
 public:
-  RuntimeManager(std::shared_ptr<jsi::Runtime> runtime,
+  RuntimeManager(std::unique_ptr<jsi::Runtime>&& runtime,
                  std::shared_ptr<ErrorHandler> errorHandler,
-                 std::shared_ptr<Scheduler> scheduler): runtime(runtime), errorHandler(errorHandler), scheduler(scheduler) { }
+                 std::shared_ptr<Scheduler> scheduler): runtime(std::move(runtime)), errorHandler(errorHandler), scheduler(scheduler) { }
 public:
   /**
    Holds the jsi::Function worklet that is responsible for updating values in JS.
@@ -35,7 +35,7 @@ public:
   /**
    Holds the jsi::Runtime this RuntimeManager is managing.
    */
-  std::shared_ptr<jsi::Runtime> runtime;
+  std::unique_ptr<jsi::Runtime> runtime;
   /**
    Holds the error handler that will be invoked when any kind of error occurs.
    */
