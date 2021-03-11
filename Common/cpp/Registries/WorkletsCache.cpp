2438aa60d9f7d3512e32c905cf5b8d499656ab3d
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-11 12:56:06 +0100

    Update WorkletsCache.cpp

--- Common/cpp/Registries/WorkletsCache.cpp
+++ Common/cpp/Registries/WorkletsCache.cpp
@@ -7,6 +7,7 @@ using namespace facebook;
 namespace reanimated
 {
 
+// TODO: Those functions never get released, memory leak?
 static std::unordered_map<long long, std::shared_ptr<jsi::Function>> worklets;
 
 jsi::Value eval(jsi::Runtime &rt, const char *code) {
