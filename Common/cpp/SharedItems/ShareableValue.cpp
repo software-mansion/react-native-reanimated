d5685e2966d99b096673c717d4f767837fea893c
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-03-23 10:45:55 +0100

    Fix collision name (#1856)
    ## Description
    
    Method `std::shared_ptr<jsi::Function> get()` from `struct HostFunctionHandler : jsi::HostObject` had name conflict wtih `virtual Value get(Runtime&, const PropNameID& name)` from `jsi::HostObject` and this crashed CI and compilation for android.
    Related with: https://github.com/software-mansion/react-native-reanimated/pull/1844
    
    ## Checklist
    
    - [x] Ensured that CI passes

--- Common/cpp/SharedItems/ShareableValue.cpp
+++ Common/cpp/SharedItems/ShareableValue.cpp
@@ -254,7 +254,7 @@ jsi::Value ShareableValue::toJSValue(jsi::Runtime &rt) {
       if (hostRuntime == &rt) {
         // function is accessed from the same runtime it was crated, we just return same function obj
         
-        return jsi::Value(rt, *hostFunctionWrapper->value->get());
+        return jsi::Value(rt, *hostFunctionWrapper->value->getPureFunction().get());
       } else {
         // function is accessed from a different runtime, we wrap function in host func that'd enqueue
         // call on an appropriate thread
@@ -306,7 +306,7 @@ jsi::Value ShareableValue::toJSValue(jsi::Runtime &rt) {
               args[i] = params[i]->getValue(*hostRuntime);
             }
              
-            jsi::Value returnedValue = hostFunction->get()->call(*hostRuntime,
+            jsi::Value returnedValue = hostFunction->getPureFunction().get()->call(*hostRuntime,
                                                 static_cast<const jsi::Value*>(args),
                                                 (size_t)params.size());
              
