8df5cfe934ef44d1abf25a26a1356dd633429a07
szymon <szymon.kapala@swmansion.com>
2021-03-24 08:50:03 +0100

    plan even more JS logic

--- src/reanimated2/layoutReanimation/AnimatedRoot.js
+++ src/reanimated2/layoutReanimation/AnimatedRoot.js
@@ -40,6 +40,42 @@ export class AnimatedRoot extends React.Component {
 
 }
 
+// Register LayoutAnimationRepository
+
+runOnUI(
+    () => {
+        'worklet';
+
+        const configs = {};
+
+        global.LayoutAnimationRepository = {
+            configs,
+            registerConfig(tag, config) {
+                configs[tag] = config;
+            },
+            removeConfig(tag) {
+                configs[tag].sv.value = 0;
+                delete configs[tag];
+            },
+            startAnimationForTag(tag) { //TODO use previous animation values like velocity
+                if (typeof configs[tag] != 'function') {
+                    console.error(`Animation for a tag: ${tag} it not a function!`);
+                }
+                const animation = configs[tag](); // it should be an animation factory as it has been created on RN side
+                animation.callback = (finished) => {
+                    if (finished) {
+                        sv.value = 0;
+                        _stopObservingProgress(tag);
+                    }
+                }
+                _startObservingProgress(tag, sv);
+                sv.value = animation;
+                
+            },
+        };  
+    }
+)();
+
 /*
 
     <AnimatedRoot animation={withTiming(1)} >
