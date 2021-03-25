3af576aa24b627a2e026b652670e75f163b9afbc
szymon <szymon.kapala@swmansion.com>
2021-03-25 10:34:06 +0100

    almost done version 1

--- ios/LayoutReanimation/REAAnimationRootViewManager.h
+++ ios/LayoutReanimation/REAAnimationRootViewManager.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface REAAnimationRootViewManager : RCTViewManager
-
+ //TODO let view Manager handle animation updates (this way we can let view animated almost any property)
 @end
 
 NS_ASSUME_NONNULL_END
--- ios/LayoutReanimation/REAAnimationsManager.m
+++ ios/LayoutReanimation/REAAnimationsManager.m
@@ -15,12 +15,16 @@
 
 @implementation REAAnimationsManager {
   RCTUIManager* _uiManager;
+  NSMutableDictionary<NSNumber*, REASnapshooter*>* _firstSnapshots;
+  NSMutableDictionary<NSNumber*, REASnapshooter*>* _secondSnapshots;
 }
 
 - (instancetype)initWithUIManager:(RCTUIManager *)uiManager
 {
   if (self = [super init]) {
     _uiManager = uiManager;
+    _firstSnapshots = [NSMutableDictionary new];
+    _secondSnapshots = [NSMutableDictionary new];
   }
   return self;
 }
@@ -32,12 +36,15 @@
 
 - (void)startAnimationWithFirstSnapshot:(REASnapshooter*)snapshooter
 {
-  //TODO
+  _firstSnapshots[snapshooter.tag] = nil;
+  _secondSnapshots[snapshooter.tag] = nil;
+  _firstSnapshots[snapshooter.tag] = snapshooter;
+  _startAnimationForTag(snapshooter.tag);
 }
 
 - (void)addSecondSnapshot:(REASnapshooter*)snapshooter
 {
-  //TODO
+  _secondSnapshots[snapshooter.tag] = snapshooter;
 }
 
 - (void)setAnimationStartingBlock:(void (^)(NSNumber *tag))startAnimation
@@ -45,9 +52,53 @@
   _startAnimationForTag = startAnimation;
 }
 
-- (void)notifyAboutProgress:(NSNumber*)progress tag:(NSNumber*)tag
+- (void)notifyAboutProgress:(NSNumber*)progressNumber tag:(NSNumber*)tag
 {
-  //TODO
+  double progress = [progressNumber doubleValue];
+  REASnapshooter* first = _firstSnapshots[tag];
+  REASnapshooter* second = _secondSnapshots[tag];
+  if (first == nil || second == nil) { // animation is not ready
+    return;
+  }
+  
+  NSMutableSet<UIView *>* allViews = [NSMutableSet new];
+  for (UIView *view in first.capturedValues.allKeys) {
+    [allViews addObject:view];
+  }
+  for (UIView *view in second.capturedValues.allKeys) {
+    [allViews addObject:view];
+  }
+  
+  for (UIView *view in allViews) {
+    NSDictionary<NSString*, NSNumber*>* startValues = _firstSnapshots[view];
+    NSDictionary<NSString*, NSNumber*>* targetValues = _secondSnapshots[view];
+    
+    // TODO let ViewManager handle animation progress based on view snapshots
+    // TODO calculate properly (right now we assume there is not active translation matrix)
+    if (startValues != nil && targetValues != nil) { //interpolate
+      // TODO make it more flexiable
+      if (progress == 1.0) {
+        view.transform = CGAffineTransformIdentity;
+        continue;
+      }
+      
+      double currentX = [targetValues[@"originX"] doubleValue] * progress + [startValues[@"originX"] doubleValue] * (1.0 - progress);
+      double currentY = [targetValues[@"originY"] doubleValue] * progress + [startValues[@"originY"] doubleValue] * (1.0 - progress);
+      double currentWidth = [targetValues[@"width"] doubleValue] * progress + [startValues[@"width"] doubleValue] * (1.0 - progress);
+      double currentHeight = [targetValues[@"height"] doubleValue] * progress + [startValues[@"height"] doubleValue] * (1.0 - progress);
+      
+      double scaleWidth = currentWidth/[targetValues[@"width"] doubleValue];
+      double scaleHeight = currentHeight/[targetValues[@"height"] doubleValue];
+      
+      double translateX = currentX - [targetValues[@"originX"] doubleValue];
+      double translateY = currentY - [targetValues[@"originY"] doubleValue];
+      
+      CGAffineTransform scaleMatrix = CGAffineTransformScale(CGAffineTransformIdentity, scaleWidth, scaleHeight);
+      view.transform = CGAffineTransformTranslate(scaleMatrix, translateX, translateY);
+    }
+    
+    //TODO appearing and disappearing
+  }
 }
 
 @end
