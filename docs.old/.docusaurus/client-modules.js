668142da275e35c373dc0741e493f104d4e8befb
Jakub Gonet <jakub.gonet@swmansion.com>
2021-03-25 17:33:47 +0100

    WIP

---
+++ docs.old/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/jgonet/Projects/react-native-reanimated/docs/src/css/custom.css"),
+];
