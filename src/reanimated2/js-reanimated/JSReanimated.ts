software-mansion/react-native-reanimated

commit 5a449ec21457bf7754fd9c618d0d89695b742fa0

fix: ssr support (#1890) (#1891)
## Description

Fix #1890

## Changes

Don't call `window` in the constructor of JS Reanimated, since this isn't available on the server. 

## Test code and steps to reproduce

One-line change, which reverts back to what was already there in `2.0.0-rc.0`. Let me know if this is needed, but given that it's simply a regression patch, I figure it isn't.

src/reanimated2/js-reanimated/JSReanimated.ts
@@ -10,12 +10,15 @@ export default class JSReanimated {
   _renderRequested = false;
   _mapperRegistry = new MapperRegistry(this);
   _frames = [];
-  timeProvider = null;
+  timeProvider = {};
 
   constructor() {
-    this.timeProvider = process.env.JEST_WORKER_ID
-      ? { now: () => Date.now() }
-      : window.performance;
+    if(process.env.JEST_WORKER_ID) {
+      this.timeProvider.now = () => Date.now();
+    }
+    else {
+      this.timeProvider.now = () => window.performance.now();
+    }
   }
 
   pushFrame(frame) {

commit 3ba7ab9230bb499bf0cbcfdb198bf4add0954ac4

Fix indent (#1910)
Fixes prettier issue introduced in #1891.

¯\\_(ツ)_/¯

src/reanimated2/js-reanimated/JSReanimated.ts
@@ -13,10 +13,9 @@ export default class JSReanimated {
   timeProvider = {};
 
   constructor() {
-    if(process.env.JEST_WORKER_ID) {
+    if (process.env.JEST_WORKER_ID) {
       this.timeProvider.now = () => Date.now();
-    }
-    else {
+    } else {
       this.timeProvider.now = () => window.performance.now();
     }
   }