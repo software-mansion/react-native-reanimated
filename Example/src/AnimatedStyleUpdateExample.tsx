853e726fba32116080941cba2a13d02dfe5cfcbb
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-04-29 15:15:32 +0200

    Revert changes

---
+++ Example/src/AnimatedStyleUpdateExample.tsx
@@ -0,0 +1,46 @@
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button } from 'react-native';
+import React from 'react';
+
+function AnimatedStyleUpdateExample(): React.ReactElement {
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+      }}>
+      <Animated.View
+        style={[
+          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
+          style,
+        ]}
+      />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
+    </View>
+  );
+}
+
+export default AnimatedStyleUpdateExample;
