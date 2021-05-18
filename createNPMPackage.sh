762ed27aabc4bb4d8c53dc19cca32072f7eddd59
szymon <szymon.kapala@swmansion.com>
2021-05-18 16:09:52 +0200

    choose proper aar

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -12,18 +12,19 @@ version_name=("64" "63" "62")
 for index in {0..2}
 do
   yarn add react-native@"${versions[$index]}"
-  for for_hermes in ["True", "False"]
+  for for_hermes in "True" "False"
   do
+    engine="jsc"
+    if [ "$for_hermes" == "True" ]; then
+      engine="hermes"
+    fi
+    echo "engine=${engine}"
+
     cd android 
     gradle clean
 
     FOR_HERMES=${for_hermes} gradle :assembleDebug
     cd $ROOT
-
-    engine="jsc"
-    if [[for_hermes == "True"]]; then
-      engine="hermes"
-    fi
 
     rm -rf android-npm/react-native-reanimated-"${version_name[$index]}-${engine}".aar
     cp android/build/outputs/aar/*.aar android-npm/react-native-reanimated-"${version_name[$index]}-${engine}".aar
