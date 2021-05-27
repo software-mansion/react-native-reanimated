c1161abe8e20bf45e0fd5d9362b3e6e82f2c95a2
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-05-27 19:20:34 +0200

    Added new examples to main screen

--- Example/src/App.tsx
+++ Example/src/App.tsx
@@ -1,53 +1,231 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Home, SpringLayoutAnimation, MountingUnmounting, SwipeableList, Menu, Modal, Carousel, ModalNewAPI } from './LayoutReanimation';
+import { FlatList, StyleSheet, Text, View, LogBox } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { SpringLayoutAnimation, MountingUnmounting, SwipeableList, Menu, Modal, Carousel, ModalNewAPI } from './LayoutReanimation';
+
+import Reanimated1 from '../reanimated1/App';
+
+import ExtrapolationExample from './ExtrapolationExample';
+import AnimatedStyleUpdateExample from './AnimatedStyleUpdateExample';
+import WobbleExample from './WobbleExample';
+import DragAndSnapExample from './DragAndSnapExample';
+import ScrollEventExample from './ScrollEventExample';
+import ChatHeadsExample from './ChatHeadsExample';
+import MeasureExample from './MeasureExample';
+import SwipeableListExample from './SwipeableListExample';
+import ScrollableViewExample from './ScrollableViewExample';
+import ScrollToExample from './ScrollToExample';
+import AnimatedTabBarExample from './AnimatedTabBarExample';
+import LightboxExample from './LightboxExample';
+import LiquidSwipe from './LiquidSwipe';
+import ScrollExample from './AnimatedScrollExample';
+LogBox.ignoreLogs(['Calling `getNode()`']);
+
+type Screens = Record<string, { screen: React.ComponentType; title?: string }>;
+
+const SCREENS: Screens = {
+  AnimatedStyleUpdate: {
+    screen: AnimatedStyleUpdateExample,
+    title: 'Animated Style Update',
+  },
+  WobbleExample: {
+    screen: WobbleExample,
+    title: 'Animation Modifiers (Wobble Effect)',
+  },
+  DragAndSnapExample: {
+    screen: DragAndSnapExample,
+    title: 'Drag and Snap',
+  },
+  MeasureExample: {
+    screen: MeasureExample,
+    title: 'Synchronous Measure',
+  },
+  ScrollEventExample: {
+    screen: ScrollEventExample,
+    title: 'Scroll Events',
+  },
+  ChatHeadsExample: {
+    screen: ChatHeadsExample,
+    title: 'Chat Heads',
+  },
+  ScrollableToExample: {
+    screen: ScrollToExample,
+    title: 'scrollTo',
+  },
+  SwipeableListExample: {
+    screen: SwipeableListExample,
+    title: '(advanced) Swipeable List',
+  },
+  LightboxExample: {
+    screen: LightboxExample,
+    title: '(advanced) Lightbox',
+  },
+  ScrollableViewExample: {
+    screen: ScrollableViewExample,
+    title: '(advanced) ScrollView imitation',
+  },
+  AnimatedTabBarExample: {
+    screen: AnimatedTabBarExample,
+    title: '(advanced) Tab Bar Example',
+  },
+  LiquidSwipe: {
+    screen: LiquidSwipe,
+    title: 'Liquid Swipe Example',
+  },
+  ExtrapolationExample: {
+    screen: ExtrapolationExample,
+    title: 'Extrapolation Example',
+  },
+  ScrollExample: {
+    screen: ScrollExample,
+    title: 'Scroll Example',
+  },
+
+  ModalNewAPI: {
+    title: '🆕 ModalNewAPI',
+    screen: ModalNewAPI,
+  }, 
+  SpringLayoutAnimation: {
+    title: '🆕 Spring Layout Animation',
+    screen: SpringLayoutAnimation,
+  },
+  MountingUnmounting: {
+    title: '🆕 Mounting Unmounting',
+    screen: MountingUnmounting,
+  },
+  SwipeableList: {
+    title: '🆕 Swipeable list',
+    screen: SwipeableList,
+  },
+  Menu: {
+    title: '🆕 Menu',
+    screen: Menu,
+  },
+  Modal: {
+    title: '🆕 Modal',
+    screen: Modal,
+  },
+  Carousel: {
+    title: '🆕 Carousel',
+    screen: Carousel,
+  }
+};
+
+type RootStackParams = { Home: undefined } & { [key: string]: undefined };
+type MainScreenProps = {
+  navigation: StackNavigationProp<RootStackParams, 'Home'>;
+  setUseRea2: (useRea2: boolean) => void;
+};
+
+function MainScreen({ navigation, setUseRea2 }: MainScreenProps) {
+  const data = Object.keys(SCREENS).map((key) => ({ key }));
+  return (
+    <FlatList
+      style={styles.list}
+      data={data}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={(props) => (
+        <MainScreenItem
+          {...props}
+          screens={SCREENS}
+          onPressItem={({ key }) => navigation.navigate(key)}
+        />
+      )}
+      renderScrollComponent={(props) => <ScrollView {...props} />}
+      ListFooterComponent={() => <LaunchReanimated1 setUseRea2={setUseRea2} />}
+    />
+  );
+}
+
+export function ItemSeparator(): React.ReactElement {
+  return <View style={styles.separator} />;
+}
+
+type Item = { key: string };
+type MainScreenItemProps = {
+  item: Item;
+  onPressItem: ({ key }: Item) => void;
+  screens: Screens;
+};
+export function MainScreenItem({
+  item,
+  onPressItem,
+  screens,
+}: MainScreenItemProps): React.ReactElement {
+  const { key } = item;
+  return (
+    <RectButton style={styles.button} onPress={() => onPressItem(item)}>
+      <Text style={styles.buttonText}>{screens[key].title || key}</Text>
+    </RectButton>
+  );
+}
+
+function LaunchReanimated1({
+  setUseRea2,
+}: {
+  setUseRea2: (useRea2: boolean) => void;
+}) {
+  return (
+    <>
+      <ItemSeparator />
+      <RectButton style={styles.button} onPress={() => setUseRea2?.(false)}>
+        <Text style={styles.buttonText}>👵 Reanimated 1.x Examples</Text>
+      </RectButton>
+    </>
+  );
+}
 
 const Stack = createStackNavigator();
 
-const Screens = [
-  {
-    name: "ModalNewAPI",
-    screen: ModalNewAPI,
-  }, 
-  {
-    name: 'Spring Layout Animation',
-    screen: SpringLayoutAnimation,
-  },
-  {
-    name: 'Mounting Unmounting',
-    screen: MountingUnmounting,
-  },
-  {
-    name: 'Swipeable list',
-    screen: SwipeableList,
-  },
-  {
-    name: "Menu",
-    screen: Menu,
-  },
-  {
-    name: "Modal",
-    screen: Modal,
-  },
-  {
-    name: "Carousel",
-    screen: Carousel,
-  }
-];
+const Reanimated2 = (setUseRea2: (useRea2: boolean) => void) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      options={{ title: '🎬 Reanimated 2.x Examples' }}
+      children={(props) => <MainScreen {...props} setUseRea2={setUseRea2} />}
+    />
+    {Object.keys(SCREENS).map((name) => (
+      <Stack.Screen
+        key={name}
+        name={name}
+        getComponent={() => SCREENS[name].screen}
+        options={{ title: SCREENS[name].title || name }}
+      />
+    ))}
+  </Stack.Navigator>
+);
 
-export default function App(): React.ReactElement {
+function App(): React.ReactElement {
+  const [useRea2, setUseRea2] = React.useState(true);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" >
-          {props => <Home {...props} screens={Screens}/>}
-        </Stack.Screen>
-         { Screens.map(screen => (
-           <Stack.Screen name={screen.name} component={screen.screen}/>
-         )) }
-      </Stack.Navigator>
+      {useRea2 ? Reanimated2(setUseRea2) : Reanimated1(setUseRea2)}
     </NavigationContainer>
   );
 }
+
+export const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#EFEFF4',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DBDBE0',
+  },
+  buttonText: {
+    backgroundColor: 'transparent',
+  },
+  button: {
+    flex: 1,
+    height: 60,
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
