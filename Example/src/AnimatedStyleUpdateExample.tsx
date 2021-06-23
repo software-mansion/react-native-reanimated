eeb8c884037ba6d6b290c6c6c1ad5badf4e635f6
Szymon <szymon.kapala@swmansion.com>
2021-06-23 09:36:23 +0200

    recover original example

--- Example/src/AnimatedStyleUpdateExample.tsx
+++ Example/src/AnimatedStyleUpdateExample.tsx
@@ -1,53 +1,46 @@
-import React, {useEffect} from 'react';
-import {NavigationContainer, NavigationProp} from '@react-navigation/native';
-import {enableScreens} from 'react-native-screens';
-import {CardStyleInterpolators, createStackNavigator} from '@react-navigation/stack';
-import Animated, {AnimatedLayout, SlideInLeft, SlideInDown, SlideOutDown, FadeIn, FadeOut} from 'react-native-reanimated';
-import { View, Text, Button } from 'react-native';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button } from 'react-native';
+import React from 'react';
 
-enableScreens(true);
-const Stack = createStackNavigator();
+function AnimatedStyleUpdateExample(): React.ReactElement {
+  const randomWidth = useSharedValue(10);
 
-const Screen1 = ({navigation}: {navigation: NavigationProp<any>}) => {
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
   return (
-    <View>
-      <Text> Screen 1 </Text>
-      <Button title="open modal" onPress={() => navigation.navigate('Screen2')}/> 
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+      }}>
+      <Animated.View
+        style={[
+          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
+          style,
+        ]}
+      />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
     </View>
   );
-};
+}
 
-const Screen2 = () => {
-  return (
-    <AnimatedLayout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Animated.View style={{position: 'absolute', height: "100%", width: '100%'}} entering={FadeIn.springify()} exiting={FadeOut.duration(2000)}>
-        <View style={{backgroundColor: 'black', opacity: 0.5, flex: 1}}/>
-      </Animated.View>
-      <Animated.View entering={SlideInLeft.springify()} exiting={SlideOutDown.duration(2000)} style={{height: 100, width:100, backgroundColor: 'red'}}>
-          <Animated.Text entering={SlideInLeft.duration(1000)}> 
-            screen 2 
-          </Animated.Text>
-      </Animated.View>
-    </AnimatedLayout>
-  );
-};
-
-const App = () => {
-  return (
-      <Stack.Navigator
-        detachInactiveScreens={false}
-        mode="modal"
-        screenOptions={{
-          animationEnabled: false,
-          cardStyle: { backgroundColor: 'transparent' },
-          headerStyle: {backgroundColor: 'red'},
-          gestureEnabled: true,
-          // cardOverlayEnabled: true,
-      }}>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-      </Stack.Navigator>
-  );
-};
-
-export default App;
+export default AnimatedStyleUpdateExample;
