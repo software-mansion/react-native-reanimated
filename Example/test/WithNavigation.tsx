af6e1f360c4df605c49f86edcf482c3afdef6c0e
Szymon <szymon.kapala@swmansion.com>
2021-06-23 09:26:42 +0200

    add layout animation example

--- Example/test/WithNavigation.tsx
+++ Example/test/WithNavigation.tsx
@@ -1,53 +1,79 @@
-import React, {useEffect} from 'react';
-import {NavigationContainer, NavigationProp} from '@react-navigation/native';
-import {enableScreens} from 'react-native-screens';
-import {CardStyleInterpolators, createStackNavigator} from '@react-navigation/stack';
-import Animated, {AnimatedLayout, SlideInLeft, SlideInDown, SlideOutDown, FadeIn, FadeOut} from 'react-native-reanimated';
+import React, { useEffect } from 'react';
+import { NavigationContainer, NavigationProp } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import Animated, { AnimatedLayout, SlideInLeft, SlideInDown, SlideOutDown, FadeIn, FadeOut } from 'react-native-reanimated';
 import { View, Text, Button } from 'react-native';
+import { ModalNewAPI, Modal, Carousel, MountingUnmounting, SpringLayoutAnimation } from '../src/LayoutReanimation';
 
 enableScreens(true);
 const Stack = createStackNavigator();
 
-const Screen1 = ({navigation}: {navigation: NavigationProp<any>}) => {
-  return (
-    <View>
-      <Text> Screen 1 </Text>
-      <Button title="open modal" onPress={() => navigation.navigate('Screen2')}/> 
-    </View>
-  );
+const Screen1 = ({ navigation }: { navigation: NavigationProp<any> }) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text> Screen 1 </Text>
+            <ModalNewAPI/>
+            <Button title="next" onPress={() => navigation.navigate('Screen2')} />
+        </View>
+    );
 };
 
-const Screen2 = () => {
-  return (
-    <AnimatedLayout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Animated.View style={{position: 'absolute', height: "100%", width: '100%'}} entering={FadeIn.springify()} exiting={FadeOut.duration(2000)}>
-        <View style={{backgroundColor: 'black', opacity: 0.5, flex: 1}}/>
-      </Animated.View>
-      <Animated.View entering={SlideInLeft.springify()} exiting={SlideOutDown.duration(2000)} style={{height: 100, width:100, backgroundColor: 'red'}}>
-          <Animated.Text entering={SlideInLeft.duration(1000)}> 
-            screen 2 
-          </Animated.Text>
-      </Animated.View>
-    </AnimatedLayout>
-  );
+const Screen2 = ({ navigation }: { navigation: NavigationProp<any> }) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text> Screen 2 </Text>
+            <Modal/>
+            <Button title="next" onPress={() => navigation.navigate('Screen3')} />
+        </View>
+    );
+};
+const Screen3 = ({ navigation }: { navigation: NavigationProp<any> }) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text> Screen 3 </Text>
+            <SpringLayoutAnimation/>
+            <Button title="next" onPress={() => navigation.navigate('Screen4')} />
+        </View>
+    );
+};
+const Screen4 = ({ navigation }: { navigation: NavigationProp<any> }) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text> Screen 4 </Text>
+            <Carousel/>
+            <Button title="next" onPress={() => navigation.navigate('Screen5')} />
+        </View>
+    );
+};
+const Screen5 = ({ navigation }: { navigation: NavigationProp<any> }) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text> Screen 5 </Text>
+            <MountingUnmounting/>
+            <Button title="next" onPress={() => navigation.navigate('Screen1')} />
+        </View>
+    );
 };
 
 const App = () => {
-  return (
-      <Stack.Navigator
-        detachInactiveScreens={true}
-        mode="modal"
-        screenOptions={{
-          animationEnabled: false,
-          cardStyle: { backgroundColor: 'transparent' },
-          headerStyle: {backgroundColor: 'red'},
-          gestureEnabled: true,
-          // cardOverlayEnabled: true,
-      }}>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-      </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            detachInactiveScreens={false}
+            mode="modal"
+            screenOptions={{
+                animationEnabled: false,
+                headerStyle: { backgroundColor: 'red' },
+                gestureEnabled: true,
+                // cardOverlayEnabled: true,
+            }}>
+            <Stack.Screen component={Screen1} name="Screen1"/>
+            <Stack.Screen component={Screen2} name="Screen2"/>
+            <Stack.Screen component={Screen3} name="Screen3"/>
+            <Stack.Screen component={Screen4} name="Screen4"/>
+            <Stack.Screen component={Screen5} name="Screen5"/>
+        </Stack.Navigator>
+    );
 };
 
 export default App;
