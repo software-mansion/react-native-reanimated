2ee1429585464fa4bea52ac31c4bb21b6e7df996
Szymon20000 <szymon.kapala@swmansion.com>
2021-06-23 14:47:29 +0200

    Fix ios crash (#2147)
    fixes: https://github.com/software-mansion/react-native-reanimated/issues/2092
    As you can see in the stack trace from the issue static object are deallocated before StoreUser destructor is called. 
    It seems like the problem only occurs when a user closes an app. However, it's still a serious problem as crashing apps are treated differently on iOS. In order to get rid of the problem, I store old static members as one object which is kept as shared_ptr instance field.

--- Common/cpp/headers/Tools/JSIStoreValueUser.h
+++ Common/cpp/headers/Tools/JSIStoreValueUser.h
@@ -13,11 +13,16 @@ using namespace facebook;
 
 namespace reanimated {
 
+struct StaticStoreUser {
+    std::atomic<int> ctr;
+    std::unordered_map<int, std::vector<std::shared_ptr<jsi::Value>>> store;
+    std::recursive_mutex storeMutex;
+};
+
 class StoreUser {
   int identifier = 0;
-  static std::atomic<int> ctr;
-  static std::unordered_map<int, std::vector<std::shared_ptr<jsi::Value>>> store;
-  static std::recursive_mutex storeMutex;
+  static std::shared_ptr<StaticStoreUser> staticStoreUserData;
+  std::shared_ptr<StaticStoreUser> storeUserData;
   std::weak_ptr<Scheduler> scheduler;
   
 public:
