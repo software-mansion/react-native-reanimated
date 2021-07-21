fc72ab38afc57f14e7e1bb4ff8cf3e094e95df62
Jakub Myśliwiec <jakub.mysliwiec@swmansion.com>
2021-07-21 15:44:39 +0200

    Added withCallback modifier to keyframe

--- src/reanimated2/layoutReanimation/animationBuilder/Keyframe.ts
+++ src/reanimated2/layoutReanimation/animationBuilder/Keyframe.ts
@@ -20,6 +20,7 @@ export interface ParsedKeyframesDefinition {
 export class Keyframe implements IEntryExitAnimationBuilder {
   durationV?: number;
   delayV?: number;
+  callbackV?: (finished: boolean) => void;
   definitions: Record<string, KeyframeProps>;
 
   /*
@@ -178,6 +179,11 @@ export class Keyframe implements IEntryExitAnimationBuilder {
     return this;
   }
 
+  withCallback(callback: (finsihed: boolean) => void): Keyframe {
+    this.callbackV = callback;
+    return this;
+  }
+
   private getDelayFunction(): AnimationFunction {
     const delay = this.delayV;
     return delay
@@ -192,6 +198,7 @@ export class Keyframe implements IEntryExitAnimationBuilder {
     const delay = this.delayV;
     const delayFunction = this.getDelayFunction();
     const { keyframes, initialValues } = this.parseDefinitions();
+    const callback = this.callbackV;
 
     return (_targetValues) => {
       'worklet';
@@ -251,6 +258,7 @@ export class Keyframe implements IEntryExitAnimationBuilder {
       return {
         animations: animations,
         initialValues: initialValues,
+        callback: callback,
       };
     };
   };
