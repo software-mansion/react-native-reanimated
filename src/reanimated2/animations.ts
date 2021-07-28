9ba229d1b1781706c98afb9e8eaf53a23ff652dd
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-07-28 22:04:44 +0200

    Fix export in test app

--- src/reanimated2/animations.ts
+++ src/reanimated2/animations.ts
@@ -302,6 +302,7 @@ export function withStyleAnimation(styleAnimations) {
             previousAnimation.styleAnimations &&
             previousAnimation.styleAnimations.transform
           ) {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             prevAnimation = previousAnimation.styleAnimations.transform;
           }
 
