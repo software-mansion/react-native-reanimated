7e430544defd1de1b74f7f7340f83d127cffc112
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-08-18 14:33:45 +0200

    Override uiBlockWithLayoutUpdateForRootView

--- ios/LayoutReanimation/REAUIManager.h
+++ ios/LayoutReanimation/REAUIManager.h
@@ -7,9 +7,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface REAUIManager : RCTUIManager
 @property BOOL blockSetter;
 - (void)setBridge:(RCTBridge *)bridge;
-- (void)removeDisappearing:(UIView*) view;
-- (void)notifyAboutRemoval:(UIView*) view removeAtIndices:(NSArray<NSNumber *> *)removeAtIndices;
-- (void)addDisappearing:(UIView*) view;
 @end
 
 NS_ASSUME_NONNULL_END
--- ios/LayoutReanimation/REAUIManager.m
+++ ios/LayoutReanimation/REAUIManager.m
@@ -2,6 +2,10 @@
 #import <Foundation/Foundation.h>
 #import "RCTComponentData.h"
 #import "RCTUIManagerObserverCoordinator.h"
+#import "RCTRootShadowView.h"
+#import "RCTRootViewInternal.h"
+#import "RCTLayoutAnimation.h"
+#import "RCTLayoutAnimationGroup.h"
 
 @interface RCTUIManager(REA)
 - (void)_manageChildren:(NSNumber *)containerTag
@@ -50,9 +54,6 @@ BOOL blockSetter = false;
         removeAtIndices:(NSArray<NSNumber *> *)removeAtIndices
                registry:(NSMutableDictionary<NSNumber *, id<RCTComponent>> *)registry
 {
-  UIView* view = (UIView*)registry[containerTag];
-  [self removeDisappearing:view];
-  [self notifyAboutRemoval:view removeAtIndices:removeAtIndices];
   [super _manageChildren:containerTag
          moveFromIndices:moveFromIndices
            moveToIndices:moveToIndices
@@ -60,20 +61,170 @@ BOOL blockSetter = false;
             addAtIndices:addAtIndices
          removeAtIndices:removeAtIndices
                 registry:registry];
-  [self addDisappearing: view];
 }
 
-- (void)removeDisappearing:(UIView*) view
+- (RCTViewManagerUIBlock)uiBlockWithLayoutUpdateForRootView:(RCTRootShadowView *)rootShadowView
 {
-  
-}
-- (void)notifyAboutRemoval:(UIView*) view removeAtIndices:(NSArray<NSNumber *> *)removeAtIndices
-{
-  
-}
-- (void)addDisappearing:(UIView*) view
-{
-  
+//  RCTAssertUIManagerQueue();
+
+  NSHashTable<RCTShadowView *> *affectedShadowViews = [NSHashTable weakObjectsHashTable];
+  [rootShadowView layoutWithAffectedShadowViews:affectedShadowViews];
+
+  if (!affectedShadowViews.count) {
+    // no frame change results in no UI update block
+    return nil;
+  }
+
+  typedef struct {
+    CGRect frame;
+    UIUserInterfaceLayoutDirection layoutDirection;
+    BOOL isNew;
+    BOOL parentIsNew;
+    RCTDisplayType displayType;
+  } RCTFrameData;
+
+  // Construct arrays then hand off to main thread
+  NSUInteger count = affectedShadowViews.count;
+  NSMutableArray *reactTags = [[NSMutableArray alloc] initWithCapacity:count];
+  NSMutableData *framesData = [[NSMutableData alloc] initWithLength:sizeof(RCTFrameData) * count];
+  {
+    NSUInteger index = 0;
+    RCTFrameData *frameDataArray = (RCTFrameData *)framesData.mutableBytes;
+    for (RCTShadowView *shadowView in affectedShadowViews) {
+      reactTags[index] = shadowView.reactTag;
+      RCTLayoutMetrics layoutMetrics = shadowView.layoutMetrics;
+      frameDataArray[index++] = (RCTFrameData){layoutMetrics.frame,
+                                               layoutMetrics.layoutDirection,
+                                               shadowView.isNewView,
+                                               shadowView.superview.isNewView,
+                                               layoutMetrics.displayType};
+    }
+  }
+
+  for (RCTShadowView *shadowView in affectedShadowViews) {
+    // We have to do this after we build the parentsAreNew array.
+    shadowView.newView = NO;
+
+    NSNumber *reactTag = shadowView.reactTag;
+
+    if (shadowView.onLayout) {
+      CGRect frame = shadowView.layoutMetrics.frame;
+      shadowView.onLayout(@{
+        @"layout" : @{
+          @"x" : @(frame.origin.x),
+          @"y" : @(frame.origin.y),
+          @"width" : @(frame.size.width),
+          @"height" : @(frame.size.height),
+        },
+      });
+    }
+
+    if (RCTIsReactRootView(reactTag) && [shadowView isKindOfClass:[RCTRootShadowView class]]) {
+      CGSize contentSize = shadowView.layoutMetrics.frame.size;
+
+      RCTExecuteOnMainQueue(^{
+        NSMutableDictionary<NSNumber *, UIView *>* viewRegistry = [self valueForKey:@"_viewRegistry"];
+        UIView *view = viewRegistry[reactTag];
+        RCTAssert(view != nil, @"view (for ID %@) not found", reactTag);
+
+        RCTRootView *rootView = (RCTRootView *)[view superview];
+        if ([rootView isKindOfClass:[RCTRootView class]]) {
+          rootView.intrinsicContentSize = contentSize;
+        }
+      });
+    }
+  }
+
+  // Perform layout (possibly animated)
+  return ^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+    const RCTFrameData *frameDataArray = (const RCTFrameData *)framesData.bytes;
+    RCTLayoutAnimationGroup *layoutAnimationGroup = [uiManager valueForKey:@"_layoutAnimationGroup"];
+
+    __block NSUInteger completionsCalled = 0;
+
+    NSInteger index = 0;
+    for (NSNumber *reactTag in reactTags) {
+      RCTFrameData frameData = frameDataArray[index++];
+
+      UIView *view = viewRegistry[reactTag];
+      CGRect frame = frameData.frame;
+
+      UIUserInterfaceLayoutDirection layoutDirection = frameData.layoutDirection;
+      BOOL isNew = frameData.isNew;
+      RCTLayoutAnimation *updatingLayoutAnimation = isNew ? nil : layoutAnimationGroup.updatingLayoutAnimation;
+      BOOL shouldAnimateCreation = isNew && !frameData.parentIsNew;
+      RCTLayoutAnimation *creatingLayoutAnimation =
+          shouldAnimateCreation ? layoutAnimationGroup.creatingLayoutAnimation : nil;
+      BOOL isHidden = frameData.displayType == RCTDisplayTypeNone;
+
+      void (^completion)(BOOL) = ^(BOOL finished) {
+        completionsCalled++;
+        if (layoutAnimationGroup.callback && completionsCalled == count) {
+          layoutAnimationGroup.callback(@[ @(finished) ]);
+
+          // It's unsafe to call this callback more than once, so we nil it out here
+          // to make sure that doesn't happen.
+          layoutAnimationGroup.callback = nil;
+        }
+      };
+
+      if (view.reactLayoutDirection != layoutDirection) {
+        view.reactLayoutDirection = layoutDirection;
+      }
+
+      if (view.isHidden != isHidden) {
+        view.hidden = isHidden;
+      }
+
+      if (creatingLayoutAnimation) {
+        // Animate view creation
+        [view reactSetFrame:frame];
+
+        CATransform3D finalTransform = view.layer.transform;
+        CGFloat finalOpacity = view.layer.opacity;
+
+        NSString *property = creatingLayoutAnimation.property;
+        if ([property isEqualToString:@"scaleXY"]) {
+          view.layer.transform = CATransform3DMakeScale(0, 0, 0);
+        } else if ([property isEqualToString:@"scaleX"]) {
+          view.layer.transform = CATransform3DMakeScale(0, 1, 0);
+        } else if ([property isEqualToString:@"scaleY"]) {
+          view.layer.transform = CATransform3DMakeScale(1, 0, 0);
+        } else if ([property isEqualToString:@"opacity"]) {
+          view.layer.opacity = 0.0;
+        } else {
+          RCTLogError(@"Unsupported layout animation createConfig property %@", creatingLayoutAnimation.property);
+        }
+
+        [creatingLayoutAnimation
+              performAnimations:^{
+                if ([property isEqualToString:@"scaleX"] || [property isEqualToString:@"scaleY"] ||
+                    [property isEqualToString:@"scaleXY"]) {
+                  view.layer.transform = finalTransform;
+                } else if ([property isEqualToString:@"opacity"]) {
+                  view.layer.opacity = finalOpacity;
+                }
+              }
+            withCompletionBlock:completion];
+
+      } else if (updatingLayoutAnimation) {
+        // Animate view update
+        [updatingLayoutAnimation
+              performAnimations:^{
+                [view reactSetFrame:frame];
+              }
+            withCompletionBlock:completion];
+
+      } else {
+        // Update without animation
+        [view reactSetFrame:frame];
+        completion(YES);
+      }
+    }
+
+    // Clean up
+    [uiManager setValue:nil forKey:@"_layoutAnimationGroup"];
+  };
 }
 
 - (Class)class {
