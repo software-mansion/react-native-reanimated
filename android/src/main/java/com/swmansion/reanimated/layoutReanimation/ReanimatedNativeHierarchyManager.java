01baa95edacb68a7adedf089120f873e022dda58
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-09-14 12:01:04 +0200

    Add check for layout

--- android/src/main/java/com/swmansion/reanimated/layoutReanimation/ReanimatedNativeHierarchyManager.java
+++ android/src/main/java/com/swmansion/reanimated/layoutReanimation/ReanimatedNativeHierarchyManager.java
@@ -73,7 +73,9 @@ class ReaLayoutAnimator extends LayoutAnimationController {
         // for recently created views.
         if (view.getWidth() == 0 || view.getHeight() == 0) {
             view.layout(x, y, x + width, y + height);
-            mAnimationsManager.onViewCreate(view, (ViewGroup) view.getParent(), new Snapshot(view,mWeakNativeViewHierarchyManage.get()));
+            if(view.getId() != -1) {
+                mAnimationsManager.onViewCreate(view, (ViewGroup) view.getParent(), new Snapshot(view,mWeakNativeViewHierarchyManage.get()));
+            }
         } else {
             Snapshot before = new Snapshot(view,mWeakNativeViewHierarchyManage.get());
             view.layout(x, y, x + width, y + height);
