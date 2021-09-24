7cdd1544b6a10ad3d25c1de704325b94952f30f0
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-09-24 09:40:36 +0200

    Simultaneous entering&layout (#2450)
    ## Description
    
    I merge entering animation with layout animation. I want to make smooth appearing if a component started layout animation during entering animation. 
    
    https://user-images.githubusercontent.com/36106620/134588531-19af5a25-6d89-4ca3-930d-8323ced85b57.mov

--- src/reanimated2/layoutReanimation/LayoutAnimationRepository.tsx
+++ src/reanimated2/layoutReanimation/LayoutAnimationRepository.tsx
@@ -6,27 +6,44 @@ runOnUI(() => {
   'worklet';
 
   const configs: Record<string, any> = {};
+  const enteringAnimationForTag: Record<string, any> = {};
 
   global.LayoutAnimationRepository = {
     configs,
     registerConfig(tag, config) {
       configs[tag] = config;
+      enteringAnimationForTag[tag] = null;
     },
     removeConfig(tag) {
       delete configs[tag];
+      delete enteringAnimationForTag[tag];
     },
     startAnimationForTag(tag, type, yogaValues) {
       if (configs[tag] == null) {
         return; // :(
       }
-
       const style = configs[tag][type](yogaValues);
+      let currentAnimation = style.animations;
+      if (type === 'entering') {
+        enteringAnimationForTag[tag] = style;
+      } else if (type === 'layout') {
+        const entryAniamtion = enteringAnimationForTag[tag].animations;
+        const layoutAnimation = style.animations;
+        currentAnimation = {};
+        for (const key in entryAniamtion) {
+          currentAnimation[key] = entryAniamtion[key];
+        }
+        for (const key in layoutAnimation) {
+          currentAnimation[key] = layoutAnimation[key];
+        }
+      }
+
       const sv: { value: boolean; _value: boolean } = configs[tag].sv;
       _stopObservingProgress(tag, false);
       _startObservingProgress(tag, sv);
       sv._value = Object.assign({}, sv._value, style.initialValues);
       _stopObservingProgress(tag, false);
-      const animation = withStyleAnimation(style.animations);
+      const animation = withStyleAnimation(currentAnimation);
 
       animation.callback = (finished?: boolean) => {
         if (finished) {
