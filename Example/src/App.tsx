5d8b74aff48a42b7b6904e817ea981fbd43a5326
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2021-10-27 12:48:06 +0200

    Add nested component example

--- Example/src/App.tsx
+++ Example/src/App.tsx
@@ -2,8 +2,9 @@ import ChessboardFastExample from './Fabric/ChessboardFastExample';
 import ChessboardSlowExample from './Fabric/ChessboardSlowExample';
 import FlexExample from './Fabric/FlexExample';
 import NativePropsExample from './Fabric/NativePropsExample';
+import NestedComponentExample from './Fabric/NestedComponentExample';
 import ParentAndChildExample from './Fabric/ParentAndChildExample';
 import ScrollViewExample from './Fabric/ScrollViewExample';
 import UIPropsExample from './Fabric/UIPropsExample';
 
-export default ChessboardFastExample;
+export default NestedComponentExample;
