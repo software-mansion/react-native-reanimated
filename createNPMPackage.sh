e68ca0401453edbd58df56087cdcf5bb6771e43b
Kacper Kafara <kacper.kafara@swmansion.com>
2021-11-04 20:56:09 +0100

    Enable build from source on client-side

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -6,10 +6,10 @@ ROOT=$(pwd)
 
 unset CI
 
-versions=("0.67.0-rc.4" "0.66.3" "0.65.1" "0.64.3" "0.63.3")
-version_name=("67" "66" "65" "64" "63")
+versions=("0.67.0-rc.2")
+version_name=("67")
 
-for index in {0..4}
+for index in {0..0}
 do
   yarn add react-native@"${versions[$index]}"
   for for_hermes in "True" "False"
@@ -43,7 +43,7 @@ do
 
     ./gradlew clean
 
-    FOR_HERMES=${for_hermes} ./gradlew :assembleDebug
+    CLIENT_SIDE_BUILD="False" FOR_HERMES=${for_hermes} ./gradlew :assembleDebug
 
     cd ./rnVersionPatch/$versionNumber
     if [ $(find . | grep 'java') ];
