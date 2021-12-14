df873f0090acb14bbd993577a4bfb096f5ff8a49
Kudo Chien <kudo@expo.dev>
2021-12-15 00:29:37 +0800

    [ios] import react class from formal react module (#2720)
    ## Description
    
    react classes should be imported by `#import <React/RCTSomeClass.h>` which clang can convert as `@import React.RCTSomeClass;` and find correct clang modules.
    
    ## Changes
    
    `#import "RCTSomeClass.h"` -> `#import <React/RCTSomeClass.h>`
    
    ## Test code and steps to reproduce
    
    build passing:
    
    ```
    yarn
    cd Example && yarn
    cd Example/ios && pod install
    npx react-native run-ios
    ```
    
    ## Checklist
    
    - [x] Ensured that CI passes

--- ios/LayoutReanimation/REAUIManager.mm
+++ ios/LayoutReanimation/REAUIManager.mm
@@ -1,15 +1,16 @@
 #import "REAUIManager.h"
 #import <Foundation/Foundation.h>
 #include "FeaturesConfig.h"
-#import "RCTComponentData.h"
-#import "RCTLayoutAnimation.h"
-#import "RCTLayoutAnimationGroup.h"
-#import "RCTModalHostView.h"
-#import "RCTRootShadowView.h"
-#import "RCTRootViewInternal.h"
-#import "RCTUIManagerObserverCoordinator.h"
 #import "REAIOSScheduler.h"
 #include "Scheduler.h"
+
+#import <React/RCTComponentData.h>
+#import <React/RCTLayoutAnimation.h>
+#import <React/RCTLayoutAnimationGroup.h>
+#import <React/RCTModalHostView.h>
+#import <React/RCTRootShadowView.h>
+#import <React/RCTRootViewInternal.h>
+#import <React/RCTUIManagerObserverCoordinator.h>
 
 #if __has_include(<RNScreens/RNSScreen.h>)
 #import <RNScreens/RNSScreen.h>
