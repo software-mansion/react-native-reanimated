d0fcf5e0b57cd909511031c7cab440cd08349131
Kacper Kafara <kacperkafara@gmail.com>
2022-01-31 14:50:16 +0100

    Temporary create package only for RN 0.67 [tests]

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -6,10 +6,10 @@ ROOT=$(pwd)
 
 unset CI
 
-versions=("0.67.0-rc.4" "0.66.3" "0.65.1" "0.64.3" "0.63.3")
-version_name=("67" "66" "65" "64" "63")
+versions=("0.67.0-rc.4")
+version_name=("67")
 
-for index in {0..4}
+for index in {0..0}
 do
   yarn add react-native@"${versions[$index]}"
   for for_hermes in "True" "False"
