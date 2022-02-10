53ac86007aa86b7443c4aeb16d907b7a2427464a
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-02-10 12:44:18 +0100

    Update building script

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -12,7 +12,7 @@ version_name=("68" "67" "66" "65" "64")
 for index in {0..4}
 do
   yarn add react-native@"${versions[$index]}"
-  for for_hermes in "False"
+  for for_hermes in "True" "False"
   do
     engine="jsc"
     if [ "$for_hermes" == "True" ]; then
