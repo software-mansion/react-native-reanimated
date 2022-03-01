e26378abcf157a8dbf88b168ad674d8fae300694
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-03-01 14:38:10 +0100

    Add configs

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -9,10 +9,10 @@ unset CI
 versions=("0.68.0-rc.2" "0.67.3" "0.66.3" "0.65.1" "0.64.3")
 version_name=("68" "67" "66" "65" "64")
 
-for index in {0..4}
+for index in {0..0}
 do
   yarn add react-native@"${versions[$index]}"
-  for for_hermes in "True" "False"
+  for for_hermes in "False"
   do
     engine="jsc"
     if [ "$for_hermes" == "True" ]; then
@@ -80,7 +80,6 @@ yarn add react-native@"${versions[0]}" --dev
 
 cp -R android-npm/expo android/
 yarn run type:generate
-cd android && ./gradlew clean && cd ..
 npm pack
 rm -rf android/expo
 
