f6ff31042677392a356b88ff313268a7cd73442d
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-03-04 16:13:07 +0100

    Revert patch

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -21,9 +21,42 @@ do
     echo "engine=${engine}"
 
     cd android 
+
+    echo "APPLY PATCH"
+    versionNumber=${version_name[$index]}
+    cd ./rnVersionPatch/$versionNumber
+    rm -rf ../backup/*
+    cp -r . ../backup
+    if [ "$(find . | grep 'java')" ];
+    then 
+      fileList=$(find . | grep -i 'java')
+      for file in $fileList; do
+        echo "COPY: $file"
+        cp ../../src/main/java/com/swmansion/reanimated/$file ../backup/$file
+        cp $file ../../src/main/java/com/swmansion/reanimated/$file
+      done
+    else
+    pwd
+      echo "NO PATCH";
+    fi
+    cd ../..
+
     ./gradlew clean
 
     CLIENT_SIDE_BUILD="False" FOR_HERMES=${for_hermes} ./gradlew :assembleDebug --no-build-cache --rerun-tasks
+
+    cd ./rnVersionPatch/$versionNumber
+    if [ $(find . | grep 'java') ];
+    then 
+      echo "RESTORE BACKUP"
+      for file in $fileList; do
+        echo "BACKUP: $file"
+        cp ../backup/$file ../../src/main/java/com/swmansion/reanimated/$file
+      done
+      echo "CLEAR BACKUP"
+      rm -rf ../backup/*
+    fi
+    cd ../..
 
     cd $ROOT
 
