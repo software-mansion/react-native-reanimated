90b85e0df07f50362a56191b69a64fddeacbd679
Krzysztof Piaskowy <krzychu2956@gmail.com>
2022-03-21 08:43:21 +0100

    Fix Java linter

--- android/src/main/java/com/swmansion/reanimated/sensor/ReanimatedSensorListener.java
+++ android/src/main/java/com/swmansion/reanimated/sensor/ReanimatedSensorListener.java
@@ -3,31 +3,29 @@ package com.swmansion.reanimated.sensor;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-
 import com.swmansion.reanimated.NativeProxy;
 
 public class ReanimatedSensorListener implements SensorEventListener {
 
-    private NativeProxy.SensorSetter setter;
-    private double lastRead = (double) System.currentTimeMillis();
-    private final double interval;
+  private NativeProxy.SensorSetter setter;
+  private double lastRead = (double) System.currentTimeMillis();
+  private final double interval;
 
-    ReanimatedSensorListener(NativeProxy.SensorSetter setter, double interval) {
-        this.setter = setter;
-        this.interval = interval;
+  ReanimatedSensorListener(NativeProxy.SensorSetter setter, double interval) {
+    this.setter = setter;
+    this.interval = interval;
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    double current = (double) System.currentTimeMillis();
+    if (current - lastRead < interval) {
+      return;
     }
+    lastRead = current;
+    setter.sensorSetter(event.values);
+  }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        double current = (double) System.currentTimeMillis();
-        if (current - lastRead < interval) {
-            return;
-        }
-        lastRead = current;
-        setter.sensorSetter(event.values);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 }
