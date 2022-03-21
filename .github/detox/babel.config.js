709e4d34417c97fb8256f6fe092189d45c35f338
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-03-21 10:39:25 +0100

    Test of detox setup

---
+++ .github/detox/babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['react-native-reanimated/plugin']
+};
+  
---
+++ .github/detox/e2e/firstTest.e2e.js
@@ -0,0 +1,13 @@
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.launchApp();
+  });
+
+  it('should have "Step One" section', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
+  });
+
+  it('should have "See Your Changes" section', async () => {
+    await expect(element(by.text('See Your Changes'))).toBeVisible();
+  });
+});
