600a69d63d7ee7c3a50a6b96022d684dc4cc51d7
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-04-28 22:36:25 +0200

    Update `ScreenStackHeaderConfigExample`

--- FabricExample/src/ScreenStackHeaderConfigBackgroundColorExample.tsx
+++ FabricExample/src/ScreenStackHeaderConfigBackgroundColorExample.tsx
@@ -5,12 +5,17 @@ import {
   ScreenStackHeaderConfig,
 } from 'react-native-screens';
 import Animated, {
-  Easing,
   useAnimatedProps,
+  useAnimatedStyle,
   useSharedValue,
-  withRepeat,
-  withTiming,
 } from 'react-native-reanimated';
+import { StyleSheet, View } from 'react-native';
+import {
+  Gesture,
+  GestureEvent,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 
 declare const performance: {
   now: () => number;
@@ -20,32 +25,76 @@ const AnimatedScreenStackHeaderConfig = Animated.createAnimatedComponent(
   ScreenStackHeaderConfig
 );
 
-export default function ScreenStackHeaderConfigBackgroundColorExample() {
-  const sv = useSharedValue(0);
+export default function EverythingExample() {
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({ x: 0, y: 0 });
 
-  React.useEffect(() => {
-    sv.value = withRepeat(
-      withTiming(1, { easing: Easing.linear, duration: 1000 }),
-      -1
-    );
-    return () => {
-      sv.value = 0;
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      'worklet';
+      isPressed.value = true;
+    })
+    .onChange((e: GestureEvent) => {
+      'worklet';
+      offset.value = {
+        x: e.changeX + offset.value.x,
+        y: e.changeY + offset.value.y,
+      };
+    })
+    .onFinalize(() => {
+      'worklet';
+      isPressed.value = false;
+    });
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: offset.value.x },
+        { translateY: offset.value.y },
+      ],
+      backgroundColor: isPressed.value ? 'gray' : 'black',
     };
-  }, [sv]);
+  });
 
   const animatedProps = useAnimatedProps(() => {
-    const color = `hsl(${Math.round(sv.value * 360)}, 100%, 50%)`;
+    const h = Math.max(0, Math.min(360, Math.round(180 + offset.value.x)));
+    const l = Math.max(0, Math.min(100, Math.round(50 - offset.value.y / 5)));
+    const color = `hsl(${h}, 100%, ${l}%)`;
     return {
       backgroundColor: color,
       title: color,
     };
-  }, []);
+  }, [offset]);
 
   return (
-    <ScreenStack>
-      <Screen>
-        <AnimatedScreenStackHeaderConfig animatedProps={animatedProps} />
-      </Screen>
-    </ScreenStack>
+    <GestureHandlerRootView style={styles.root}>
+      <ScreenStack>
+        <Screen>
+          <AnimatedScreenStackHeaderConfig animatedProps={animatedProps} />
+        </Screen>
+      </ScreenStack>
+      <View style={styles.container}>
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[styles.ball, animatedStyles]} />
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ball: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: 'black',
+  },
+});
