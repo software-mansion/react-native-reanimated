e49e1abea60badb0bfb7666389adfa29093bf697
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-05-07 13:44:14 +0200

    Update CMake

--- android/src/fabric/java/com/swmansion/reanimated/ReaCompatibility.java
+++ android/src/fabric/java/com/swmansion/reanimated/ReaCompatibility.java
@@ -1,14 +1,16 @@
 package com.swmansion.reanimated;
 
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.swmansion.reanimated.NodesManager;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.uimanager.UIManagerHelper;
+import com.facebook.react.uimanager.common.UIManagerType;
 import com.facebook.react.fabric.FabricUIManager;
 
 class ReaCompatibility {
     private FabricUIManager fabricUIManager;
 
     public ReaCompatibility(ReactApplicationContext reactApplicationContext) {
-        fabricUIManager = (FabricUIManager) UIManagerHelper.getUIManager(mReactApplicationContext, UIManagerType.FABRIC);
+        fabricUIManager = (FabricUIManager) UIManagerHelper.getUIManager(reactApplicationContext, UIManagerType.FABRIC);
     }
 
     public void registerFabricEventListener(NodesManager nodeManager) {
