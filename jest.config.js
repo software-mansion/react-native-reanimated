cc155bb4ec8294bdb6a7a00f28ccee15b64c66ec
Angelika Serwa <angelika.serwa@gmail.com>
2022-05-10 15:40:28 +0200

    Fix jest tests (#3228)
    ## Description
    
    Fixes jest tests failures when using node 18.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   preset: 'react-native',
   modulePathIgnorePatterns: ['Example', 'docs', 'lib'],
   setupFiles: ['./jest-setup.js'],
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
