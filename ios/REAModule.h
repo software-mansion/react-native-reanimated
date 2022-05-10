5b377904e9f02af1fe60e5fd111ffdf1e74b8f5c
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-05-10 14:15:12 +0200

    Move import

--- ios/REAModule.h
+++ ios/REAModule.h
@@ -3,13 +3,14 @@
 #import <RNReanimated/ReanimatedUIManagerBinding.h>
 #endif
 
-#import <RNReanimated/REANodesManager.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTUIManager.h>
 #import <React/RCTUIManagerObserverCoordinator.h>
 #import <React/RCTUIManagerUtils.h>
+
+#import <RNReanimated/REANodesManager.h>
 
 @interface REAModule : RCTEventEmitter <RCTBridgeModule, RCTEventDispatcherObserver, RCTUIManagerObserver>
 
