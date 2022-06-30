4c7f3a20a6972819b037dfc5365716e168a73e85
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-06-30 15:50:10 +0200

    Update commands

--- createNPMPackage.sh
+++ createNPMPackage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yarn run type:generate
-yarn --cwd ./plugin-swc/ build:plugin
+yarn --cwd ./plugin-swc/ build:plugin --release
 if [ $1 = "nightly" ];
 then
   node scripts/set-nightly-version.js
