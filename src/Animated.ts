fba06eb1e6c40a3a729f595ceb28efbb7762cea0
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-06-30 15:27:01 +0200

    Fix building

--- src/Animated.ts
+++ src/Animated.ts
@@ -9,4 +9,5 @@ export { default as View } from './reanimated2/component/View';
 export { default as ScrollView } from './reanimated2/component/ScrollView';
 export { default as Image } from './reanimated2/component/Image';
 export { default as FlatList } from './reanimated2/component/FlatList';
+// @ts-ignore backward compatibility with treeshaking
 export * from './reanimated1';
