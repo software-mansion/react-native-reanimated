c0f8412fa8b0e268fb999f67672950b02855fa2f
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2022-07-08 09:34:45 +0200

    Remove obsolete `cloneTree` implementation

--- Common/cpp/Fabric/ReanimatedCommitHook.cpp
+++ Common/cpp/Fabric/ReanimatedCommitHook.cpp
@@ -10,58 +10,6 @@ using namespace facebook::react;
 
 namespace reanimated {
 
-static inline ShadowNode::Unshared cloneTree(
-    ShadowNode::Shared oldRootNode,
-    const ShadowNodeFamily &family,
-    const folly::dynamic &dynProps,
-    const PropsParserContext &propsParserContext,
-    const std::shared_ptr<PropsRegistry> propsRegistry,
-    std::set<ShadowNode *> &yogaChildrenUpdates) {
-  auto ancestors = family.getAncestors(*oldRootNode);
-
-  if (ancestors.empty()) {
-    return ShadowNode::Unshared{nullptr};
-  }
-
-  auto &parent = ancestors.back();
-  auto &oldShadowNode = parent.first.get().getChildren().at(parent.second);
-
-  const auto props = oldShadowNode->getComponentDescriptor().cloneProps(
-      propsParserContext, oldShadowNode->getProps(), RawProps(dynProps));
-
-  auto newShadowNode = oldShadowNode->clone({/* .props = */ props});
-
-  auto childNode = newShadowNode;
-
-  for (auto it = ancestors.rbegin(); it != ancestors.rend(); ++it) {
-    auto &parentNode = it->first.get();
-    auto childIndex = it->second;
-
-    auto children = parentNode.getChildren();
-    react_native_assert(
-        ShadowNode::sameFamily(*children.at(childIndex), *childNode));
-
-    auto isSealed = parentNode.getSealed();
-    if (!isSealed) {
-      // optimization
-      auto &parentNodeNonConst = const_cast<ShadowNode &>(parentNode);
-      parentNodeNonConst.replaceChild(
-          *children.at(childIndex), childNode, childIndex);
-      yogaChildrenUpdates.insert(&parentNodeNonConst);
-      return std::const_pointer_cast<ShadowNode>(oldRootNode);
-    }
-
-    children[childIndex] = childNode;
-
-    childNode = parentNode.clone({
-        ShadowNodeFragment::propsPlaceholder(),
-        std::make_shared<SharedShadowNodeList>(children),
-    });
-  }
-
-  return std::const_pointer_cast<ShadowNode>(childNode);
-}
-
 RootShadowNode::Unshared ReanimatedCommitHook::shadowTreeWillCommit(
     ShadowTree const &shadowTree,
     RootShadowNode::Shared const &oldRootShadowNode,
