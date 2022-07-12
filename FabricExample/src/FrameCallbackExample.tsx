28148e9bb98468c285858ae8c43cbb2ef67a8005
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-12 12:45:46 +0200

    Improved FrameCallbackExample

--- FabricExample/src/FrameCallbackExample.tsx
+++ FabricExample/src/FrameCallbackExample.tsx
@@ -1,25 +1,110 @@
 import Animated, {
   useAnimatedStyle,
   useFrameCallback,
+  useSharedValue,
 } from 'react-native-reanimated';
-import { StyleSheet, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, View } from 'react-native';
 
 import React from 'react';
 
 export default function FrameCallbackExample() {
-  const frameCallback = useFrameCallback(() => {
-    console.log('callback');
+  const x1 = useSharedValue(0);
+  const y1 = useSharedValue(0);
+
+  let windowX = Math.min(
+    Dimensions.get('window').width,
+    Dimensions.get('window').height
+  );
+  windowX -= styles.box1.height;
+  windowX -= windowX % 2;
+
+  const frameCallback1 = useFrameCallback(() => {
+    if (x1.value === windowX && y1.value !== 0) {
+      y1.value -= 1;
+    }
+    if (x1.value === 0 && y1.value !== windowX) {
+      y1.value += 1;
+    }
+    if (y1.value === windowX && x1.value !== windowX) {
+      x1.value += 1;
+    }
+    if (y1.value === 0 && x1.value !== 0) {
+      x1.value -= 1;
+    }
   }, false);
 
-  frameCallback.start();
+  const animatedStyle1 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: x1.value,
+        },
+        {
+          translateY: y1.value,
+        },
+      ],
+    };
+  });
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {};
+  const x2 = useSharedValue(0);
+  const y2 = useSharedValue(0);
+
+  const frameCallback2 = useFrameCallback(() => {
+    if (x2.value === windowX && y2.value !== 0) {
+      y2.value -= 2;
+    }
+    if (x2.value === 0 && y2.value !== windowX) {
+      y2.value += 2;
+    }
+    if (y2.value === windowX && x2.value !== windowX) {
+      x2.value += 2;
+    }
+    if (y2.value === 0 && x2.value !== 0) {
+      x2.value -= 2;
+    }
+  }, false);
+
+  const animatedStyle2 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: x2.value,
+        },
+        {
+          translateY: y2.value,
+        },
+      ],
+    };
   });
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.box, animatedStyle]} />
+      <View style={styles.container}>
+        <Animated.View style={[styles.box1, animatedStyle1]} />
+        <Animated.View style={[styles.box2, animatedStyle2]} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={'Start/stop square1 animation'}
+          onPress={() => {
+            if (frameCallback1.state) {
+              frameCallback1.stop();
+            } else {
+              frameCallback1.start();
+            }
+          }}
+        />
+        <Button
+          title={'Start/stop square2 animation'}
+          onPress={() => {
+            if (frameCallback2.state) {
+              frameCallback2.stop();
+            } else {
+              frameCallback2.start();
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -27,12 +112,20 @@ export default function FrameCallbackExample() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 150,
-    height: 150,
+  buttonContainer: {
+    paddingBottom: 50,
+  },
+  box1: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
     backgroundColor: 'navy',
+  },
+  box2: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
   },
 });
