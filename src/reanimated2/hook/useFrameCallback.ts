13490afd76d518bdf2a6a3e9a6b644d0b6ca42d7
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-12 09:45:57 +0200

    Added typescript code for useFrameCallback()

--- src/reanimated2/hook/useFrameCallback.ts
+++ src/reanimated2/hook/useFrameCallback.ts
@@ -2,31 +2,42 @@ import { useEffect, useRef } from 'react';
 import NativeReanimated from '../NativeReanimated';
 
 export type FrameCallback = {
-  start: () => void,
-  stop: () => void,
-  state: boolean,
-  callbackId: number,
-}
+  start: () => void;
+  stop: () => void;
+  state: boolean;
+  callbackId: number;
+};
 
-export function useFrameCallback(callback: () => void): FrameCallback {
+export function useFrameCallback(
+  callback: () => void,
+  autostart = true
+): FrameCallback {
   const ref = useRef<FrameCallback>({
-    start: () => {},
-    stop: () => {},
+    start: () => {
+      NativeReanimated.manageStateFrameCallback(ref.current.callbackId, true);
+    },
+    stop: () => {
+      NativeReanimated.manageStateFrameCallback(ref.current.callbackId, false);
+    },
     state: false,
     callbackId: -1,
   });
 
-  if (ref.current.callbackId = -1) {
-    
+  if (ref.current.callbackId === -1) {
+    ref.current.callbackId = NativeReanimated.registerFrameCallback(callback);
+
+    if (autostart) ref.current.start.apply({});
   }
 
   useEffect(() => {
-    // todo
-    ref.current.callbackId = NativeReanimated.registerFrameCallback(callback);
+    if (ref.current.callbackId === -1)
+      ref.current.callbackId = NativeReanimated.registerFrameCallback(callback);
 
     return () => {
       NativeReanimated.unregisterFrameCallback(ref.current.callbackId);
-    }
+      ref.current.state = false;
+      ref.current.callbackId = -1;
+    };
   });
 
   return ref.current;
