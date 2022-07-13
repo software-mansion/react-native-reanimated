1821328b6212d643018c987300b87847dd661c6c
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-07-13 09:54:22 +0200

    Improve demo

--- FabricExample/src/CoreAnimationSpringExample.tsx
+++ FabricExample/src/CoreAnimationSpringExample.tsx
@@ -8,25 +8,44 @@ import Animated, {
 } from 'react-native-reanimated';
 
 export default function CoreAnimationSpringExample() {
-  const sv = useSharedValue(0);
+  const sv1 = useSharedValue(0);
+  const sv2 = useSharedValue(0);
+  const sv3 = useSharedValue(0);
 
   const ref = React.useRef(1);
 
-  const animatedStyle = useAnimatedStyle(() => {
+  const animatedStyle1 = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: sv.value }],
+      transform: [{ translateX: sv1.value }],
+    };
+  });
+
+  const animatedStyle2 = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: sv2.value }],
+    };
+  });
+
+  const animatedStyle3 = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: sv3.value }],
     };
   });
 
   const handlePress = () => {
-    sv.value = withSpring(ref.current * 90);
+    sv1.value = withSpring(ref.current * 60);
+    sv2.value = withSpring(ref.current * 80);
+    sv3.value = withSpring(ref.current * 100);
     ref.current = -ref.current;
   };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.box, animatedStyle]} />
       <Button title="Animate" onPress={handlePress} />
+      <View style={styles.vspace} />
+      <Animated.View style={[styles.box, styles.box1, animatedStyle1]} />
+      <Animated.View style={[styles.box, styles.box2, animatedStyle2]} />
+      <Animated.View style={[styles.box, styles.box3, animatedStyle3]} />
     </View>
   );
 }
@@ -37,10 +56,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  vspace: {
+    height: 40,
+  },
   box: {
-    width: 130,
-    height: 130,
-    marginVertical: 30,
-    backgroundColor: 'navy',
+    width: 80,
+    height: 80,
+    marginVertical: 8,
+  },
+  box1: {
+    backgroundColor: 'tomato',
+  },
+  box2: {
+    backgroundColor: 'gold',
+  },
+  box3: {
+    backgroundColor: 'lime',
   },
 });
