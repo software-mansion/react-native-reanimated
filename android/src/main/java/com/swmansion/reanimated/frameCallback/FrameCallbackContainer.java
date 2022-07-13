c54620920fd6cc58e7956787601d556a95b4df2b
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-13 14:01:52 +0200

    Formatted java with yarn format:java

--- android/src/main/java/com/swmansion/reanimated/frameCallback/FrameCallbackContainer.java
+++ android/src/main/java/com/swmansion/reanimated/frameCallback/FrameCallbackContainer.java
@@ -2,7 +2,6 @@ package com.swmansion.reanimated.frameCallback;
 
 import android.util.Log;
 import android.view.Choreographer;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
