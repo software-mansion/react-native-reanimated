2513eb78988d932cf8c025a49338864b9f144cfe
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-25 09:56:33 +0200

    Fixed useFrameCallbackHook not running on UI thread

--- src/reanimated2/frameCallback/FrameCallbackRegistry.ts
+++ src/reanimated2/frameCallback/FrameCallbackRegistry.ts
@@ -1,54 +1,101 @@
+import { runOnUI } from '../';
+
+export interface FrameCallbackRegistryUI {
+  frameCallbackRegistry: Map<number, () => void>;
+  frameCallbackActive: Set<number>;
+  isFrameCallbackRunning: boolean;
+  runAnimation: () => void;
+  registerFrameCallback: (callback: () => void, callbackId: number) => void;
+  unregisterFrameCallback: (frameCallbackId: number) => void;
+  manageStateFrameCallback: (frameCallbackId: number, state: boolean) => void;
+}
+
 export default class FrameCallbackRegistry {
   private nextCallbackId = 0;
-  private frameCallbackRegistry = new Map<number, () => void>();
-  private frameCallbackActive = new Set<number>();
-  private isFrameCallbackRunning = false;
 
-  private runAnimation() {
-    const loop = () => {
-      this.frameCallbackActive.forEach((key) => {
-        const callback = this.frameCallbackRegistry.get(key);
-        callback!();
-      });
+  constructor() {
+    runOnUI(() => {
+      'worklet';
 
-      if (this.frameCallbackActive.size > 0) {
-        requestAnimationFrame(loop);
-      } else {
-        this.isFrameCallbackRunning = false;
-      }
-    };
+      global._frameCallbackRegistry = {
+        frameCallbackRegistry: new Map<number, () => void>(),
+        frameCallbackActive: new Set<number>(),
+        isFrameCallbackRunning: false,
 
-    if (!this.isFrameCallbackRunning) {
-      requestAnimationFrame(loop);
-      this.isFrameCallbackRunning = true;
-    }
+        runAnimation() {
+          const loop = () => {
+            this.frameCallbackActive.forEach((key: number) => {
+              const callback = this.frameCallbackRegistry.get(key);
+              callback!();
+            });
+
+            if (this.frameCallbackActive.size > 0) {
+              requestAnimationFrame(loop);
+            } else {
+              this.isFrameCallbackRunning = false;
+            }
+          };
+
+          if (!this.isFrameCallbackRunning) {
+            requestAnimationFrame(loop);
+            this.isFrameCallbackRunning = true;
+          }
+        },
+
+        registerFrameCallback(callback: () => void, callbackId: number) {
+          this.frameCallbackRegistry.set(callbackId, callback);
+        },
+
+        unregisterFrameCallback(frameCallbackId: number) {
+          this.manageStateFrameCallback(frameCallbackId, false);
+          this.frameCallbackRegistry.delete(frameCallbackId);
+        },
+
+        manageStateFrameCallback(frameCallbackId: number, state: boolean) {
+          if (frameCallbackId === -1) {
+            return;
+          }
+          if (state) {
+            this.frameCallbackActive.add(frameCallbackId);
+            this.runAnimation();
+          } else {
+            this.frameCallbackActive.delete(frameCallbackId);
+          }
+        },
+      };
+    })();
   }
 
   registerFrameCallback(callback: () => void): number {
     if (!callback) {
       return -1;
     }
+
     const callbackId = this.nextCallbackId;
     this.nextCallbackId++;
-    this.frameCallbackRegistry.set(callbackId, callback);
+
+    runOnUI(() => {
+      'worklet';
+      global._frameCallbackRegistry.registerFrameCallback(callback, callbackId);
+    })();
 
     return callbackId;
   }
 
   unregisterFrameCallback(frameCallbackId: number): void {
-    this.manageStateFrameCallback(frameCallbackId, false);
-    this.frameCallbackRegistry.delete(frameCallbackId);
+    runOnUI(() => {
+      'worklet';
+      global._frameCallbackRegistry.unregisterFrameCallback(frameCallbackId);
+    })();
   }
 
   manageStateFrameCallback(frameCallbackId: number, state: boolean): void {
-    if (frameCallbackId === -1) {
-      return;
-    }
-    if (state) {
-      this.frameCallbackActive.add(frameCallbackId);
-      this.runAnimation();
-    } else {
-      this.frameCallbackActive.delete(frameCallbackId);
-    }
+    runOnUI(() => {
+      'worklet';
+      global._frameCallbackRegistry.manageStateFrameCallback(
+        frameCallbackId,
+        state
+      );
+    })();
   }
 }
