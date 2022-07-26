75f6be75bff57d87f024e101a413442b2fac9d51
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-26 08:12:17 +0200

    Improvementes after review

--- src/reanimated2/frameCallback/FrameCallbackRegistry.ts
+++ src/reanimated2/frameCallback/FrameCallbackRegistry.ts
@@ -4,66 +4,72 @@ export interface FrameCallbackRegistryUI {
   frameCallbackRegistry: Map<number, () => void>;
   frameCallbackActive: Set<number>;
   isFrameCallbackRunning: boolean;
-  runAnimation: () => void;
+  runCallbacks: () => void;
   registerFrameCallback: (callback: () => void, callbackId: number) => void;
   unregisterFrameCallback: (frameCallbackId: number) => void;
   manageStateFrameCallback: (frameCallbackId: number, state: boolean) => void;
 }
 
-export default class FrameCallbackRegistry {
+const prepareUIRegistry = runOnUI(() => {
+  'worklet';
+
+  const frameCallbackRegistry: FrameCallbackRegistryUI = {
+    frameCallbackRegistry: new Map<number, () => void>(),
+    frameCallbackActive: new Set<number>(),
+    isFrameCallbackRunning: false,
+
+    runCallbacks() {
+      const loop = () => {
+        this.frameCallbackActive.forEach((key: number) => {
+          const callback = this.frameCallbackRegistry.get(key);
+          callback!();
+        });
+
+        if (this.frameCallbackActive.size > 0) {
+          requestAnimationFrame(loop);
+        } else {
+          this.isFrameCallbackRunning = false;
+        }
+      };
+
+      if (!this.isFrameCallbackRunning) {
+        requestAnimationFrame(loop);
+        this.isFrameCallbackRunning = true;
+      }
+    },
+
+    registerFrameCallback(callback: () => void, callbackId: number) {
+      this.frameCallbackRegistry.set(callbackId, callback);
+    },
+
+    unregisterFrameCallback(frameCallbackId: number) {
+      this.manageStateFrameCallback(frameCallbackId, false);
+      this.frameCallbackRegistry.delete(frameCallbackId);
+    },
+
+    manageStateFrameCallback(frameCallbackId: number, state: boolean) {
+      if (frameCallbackId === -1) {
+        return;
+      }
+      if (state) {
+        this.frameCallbackActive.add(frameCallbackId);
+        this.runCallbacks();
+      } else {
+        this.frameCallbackActive.delete(frameCallbackId);
+      }
+    },
+  };
+
+  global._frameCallbackRegistry = frameCallbackRegistry;
+});
+
+export default class FrameCallbackRegistryJS {
   private nextCallbackId = 0;
 
   constructor() {
-    runOnUI(() => {
-      'worklet';
-
-      global._frameCallbackRegistry = {
-        frameCallbackRegistry: new Map<number, () => void>(),
-        frameCallbackActive: new Set<number>(),
-        isFrameCallbackRunning: false,
-
-        runAnimation() {
-          const loop = () => {
-            this.frameCallbackActive.forEach((key: number) => {
-              const callback = this.frameCallbackRegistry.get(key);
-              callback!();
-            });
-
-            if (this.frameCallbackActive.size > 0) {
-              requestAnimationFrame(loop);
-            } else {
-              this.isFrameCallbackRunning = false;
-            }
-          };
-
-          if (!this.isFrameCallbackRunning) {
-            requestAnimationFrame(loop);
-            this.isFrameCallbackRunning = true;
-          }
-        },
-
-        registerFrameCallback(callback: () => void, callbackId: number) {
-          this.frameCallbackRegistry.set(callbackId, callback);
-        },
-
-        unregisterFrameCallback(frameCallbackId: number) {
-          this.manageStateFrameCallback(frameCallbackId, false);
-          this.frameCallbackRegistry.delete(frameCallbackId);
-        },
-
-        manageStateFrameCallback(frameCallbackId: number, state: boolean) {
-          if (frameCallbackId === -1) {
-            return;
-          }
-          if (state) {
-            this.frameCallbackActive.add(frameCallbackId);
-            this.runAnimation();
-          } else {
-            this.frameCallbackActive.delete(frameCallbackId);
-          }
-        },
-      };
-    })();
+    if (global._frameCallbackRegistry === undefined) {
+      prepareUIRegistry();
+    }
   }
 
   registerFrameCallback(callback: () => void): number {
