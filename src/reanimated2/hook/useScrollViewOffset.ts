5f604cda8f5b686f76a09d3a2ac60520d0b8b488
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-07-29 16:02:05 +0200

    Removed debugging console.log

--- src/reanimated2/hook/useScrollViewOffset.ts
+++ src/reanimated2/hook/useScrollViewOffset.ts
@@ -31,7 +31,6 @@ export function useScrollViewOffset(
 
   useEffect(() => {
     const viewTag = findNodeHandle(aref.current);
-    console.log(aref.current);
     const horizontal = _IS_FABRIC
       ? aref.current?.currentProps.horizontal
       : aref.current?._internalFiberInstanceHandleDEV._debugOwner.memoizedProps
