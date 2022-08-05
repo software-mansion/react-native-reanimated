f8783406092591908cfe92e0bde2ee510ac92d96
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-08-05 13:55:21 +0200

    Add coment

--- src/reanimated2/js-reanimated/global.ts
+++ src/reanimated2/js-reanimated/global.ts
@@ -39,4 +39,11 @@ const initializeGlobalsForWeb = () => {
   return true;
 };
 
+/*
+  If a file doesn't export anything, tree shaking doesn't pack 
+  them into the JS bundle. In effect, the code inside of this 
+  file will never execute. That is why we wrapped initialization 
+  code into a function, and we called in during creating module export.
+*/
+
 export default initializeGlobalsForWeb();
