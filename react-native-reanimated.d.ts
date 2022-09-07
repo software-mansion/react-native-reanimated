0c6d41203f5159396ddab056b1f9755073f3eae8
Naftali Beder <naftali@naftalibeder.com>
2022-09-07 06:13:48 -0500

    Refactor `measure()` to handle `try` internally (#3453)
    ## Description
    
    Allows `measure()` to handle exceptions internally, and return `null` if
    the measurement cannot be made.
    
    The impetus for this is occasional hard crashes when calling `measure`,
    seemingly due to a view not yet having loaded, but not fixable in the
    enclosing app with a null check on the ref, a `try` block, etc. - once
    the JS passes a value to the native method `_measure`, an invalid
    argument will cause a crash.
    
    This way, the caller:
    - is guaranteed that calling `measure` will not cause the enclosing app
    to crash (even in a `try` block)
    - does not need to consider the various reasons why measurement might
    fail (uninitialized ref, component out of view, etc.)
    - can cleanly check the return object, instead of needing to evaluate an
    arbitrary property on it (e.g. `if (isNaN(measured.x))`
    
    Note: I still occasionally get a crash that reports that the native
    `_measure` method is not defined. I haven't been able to fix this,
    because it's impossible to do a falsy check - if it doesn't exist, even
    a conditional or `console.log` referencing `_measure` (even in a `try`)
    causes a hard crash. Any ideas for how to check this?
    
    ## Changes
    
    - Return early if `viewTag` is `-1` (invalid)
    - Make early returns return `null` instead of throwing
    - Fix incorrect `RefObjectFunction` import path
    - Fix example assuming non-null `measure` response
    - Remove unneeded ignore comments
    - Fix "indefined" misspelling
    
    ## Test code and steps to reproduce
    
    Simply run `FabricExample/src/MeasureExample.tsx`.
    
    ## Checklist
    
    - [X] Included code example that can be used to test this change
    - [X] Updated TS types
    - [ ] Added TS types tests
    - [ ] Added unit / integration tests
    - [X] Updated documentation
    - [X] Ensured that CI passes

--- react-native-reanimated.d.ts
+++ react-native-reanimated.d.ts
@@ -54,6 +54,8 @@ declare module 'react-native-reanimated' {
     import('./lib/types/lib/reanimated2/animation/index').StyleLayoutAnimation;
   export type Animation<T> =
     import('./lib/types/lib/reanimated2/commonTypes').Animation<T>;
+  export type MeasuredDimensions =
+    import('./lib/types/lib/reanimated2/commonTypes').MeasuredDimensions;
 
   namespace Animated {
     type Nullable<T> = T | null | undefined;
@@ -809,14 +811,7 @@ declare module 'react-native-reanimated' {
   export function defineAnimation<T>(starting: any, factory: () => T): number;
   export function measure<T extends Component>(
     ref: RefObject<T>
-  ): {
-    width: number;
-    height: number;
-    x: number;
-    y: number;
-    pageX: number;
-    pageY: number;
-  };
+  ): MeasuredDimensions | null;
 
   export function getRelativeCoords(
     ref: RefObject<Component>,
