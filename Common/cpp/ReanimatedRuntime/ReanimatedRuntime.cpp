105b7ca5e33862bdad584fbce064cfbaffa37df5
Karol Wąsowski <karol.wasowski@swmansion.com>
2022-09-15 10:42:16 +0200

    Added documentation to code

--- Common/cpp/ReanimatedRuntime/ReanimatedRuntime.cpp
+++ Common/cpp/ReanimatedRuntime/ReanimatedRuntime.cpp
@@ -1,3 +1,10 @@
+// JS_RUNTIME_HERMES is only set on Android so we have to check __has_include
+// on iOS.
+#if (__has_include( \
+         <reacthermes/HermesExecutorFactory.h>) || __has_include(<hermes/hermes.h>) || JS_RUNTIME_HERMES)
+#define JS_RUNTIME_HERMES 1
+#endif
+
 #include "ReanimatedRuntime.h"
 
 #include <cxxreact/MessageQueueThread.h>
@@ -6,8 +13,7 @@
 #include <memory>
 #include <utility>
 
-#if (__has_include( \
-         <reacthermes/HermesExecutorFactory.h>) || __has_include(<hermes/hermes.h>) || JS_RUNTIME_HERMES)
+#if JS_RUNTIME_HERMES
 #include "ReanimatedHermesRuntime.h"
 #elif JS_RUNTIME_V8
 #include <v8runtime/V8RuntimeFactory.h>
@@ -20,51 +26,26 @@ namespace reanimated {
 using namespace facebook;
 using namespace react;
 
-#if (__has_include( \
-         <reacthermes/HermesExecutorFactory.h>) || __has_include(<hermes/hermes.h>) || JS_RUNTIME_HERMES)
-
-std::shared_ptr<jsi::Runtime> makeHermesReanimatedRuntime(
+std::shared_ptr<jsi::Runtime> ReanimatedRuntime::make(
     std::shared_ptr<MessageQueueThread> jsQueue) {
+#if JS_RUNTIME_HERMES
   std::unique_ptr<facebook::hermes::HermesRuntime> runtime =
       facebook::hermes::makeHermesRuntime();
   facebook::hermes::HermesRuntime &hermesRuntime = *runtime;
 
   return std::make_shared<ReanimatedHermesRuntime>(
       std::move(runtime), hermesRuntime, jsQueue);
-}
-
 #elif JS_RUNTIME_V8
-
-std::shared_ptr<jsi::Runtime> makeV8ReanimatedRuntime(
-    std::shared_ptr<MessageQueueThread> jsQueue) {
   jsQueue->quitSynchronous();
 
   auto config = std::make_unique<rnv8::V8RuntimeConfig>();
   config->enableInspector = false;
   config->appName = "reanimated";
   return rnv8::createSharedV8Runtime(runtime_, std::move(config));
-}
-
 #else
-
-std::shared_ptr<jsi::Runtime> makeJSCReanimatedRuntime(
-    std::shared_ptr<MessageQueueThread> jsQueue) {
   jsQueue->quitSynchronous();
 
   return facebook::jsc::makeJSCRuntime();
-}
-
-#endif
-
-std::shared_ptr<jsi::Runtime> ReanimatedRuntime::make(
-    std::shared_ptr<MessageQueueThread> jsQueue) {
-#if (__has_include( \
-         <reacthermes/HermesExecutorFactory.h>) || __has_include(<hermes/hermes.h>) || JS_RUNTIME_HERMES)
-  return makeHermesReanimatedRuntime(jsQueue);
-#elif JS_RUNTIME_V8
-  return makeV8ReanimatedRuntime(jsQueue);
-#else
-  return makeJSCReanimatedRuntime(jsQueue);
 #endif
 }
 
