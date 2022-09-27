a453c777b75facc0cc0d839e8b21596ffc7f7f59
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-09-27 16:51:41 +0200

    Export SharedValue type from Animated

--- src/Animated.ts
+++ src/Animated.ts
@@ -9,3 +9,4 @@ export { AnimatedView as View } from './reanimated2/component/View';
 export { AnimatedScrollView as ScrollView } from './reanimated2/component/ScrollView';
 export { AnimatedImage as Image } from './reanimated2/component/Image';
 export { AnimatedFlatList as FlatList } from './reanimated2/component/FlatList';
+export { SharedValue } from './reanimated2';
