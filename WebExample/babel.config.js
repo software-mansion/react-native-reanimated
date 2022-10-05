e4c7326b710bce4d788c5d3569938276671cd71c
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-10-05 12:58:36 +0200

    WebExample works with link

--- WebExample/babel.config.js
+++ WebExample/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
+      '../plugin',
     ],
   };
 };
