f326fac9fe604b38d3aa8a91f3cf0836a8b44946
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-10-13 12:40:11 +0200

    Fast reload works

--- WebExample/babel.config.js
+++ WebExample/babel.config.js
@@ -6,12 +6,16 @@ module.exports = function (api) {
       [
         'module-resolver',
         {
+          extensions: ['.js', '.ts', '.tsx'],
           alias: {
             react: './node_modules/react',
+            'react-native': './node_modules/react-native-web',
+            'react-native-reanimated': '../src/index',
           },
         },
       ],
-      '../plugin',
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
   };
 };
