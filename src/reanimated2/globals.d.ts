fb89601fa915db7fa69a3e0228348e7009344f87
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2022-10-14 16:22:08 +0200

    Improve globals.d.ts (#3626)
    This PR eliminates differences between two sections in `globals.d.ts`.
    Also, two types needed to be fixed.

--- src/reanimated2/globals.d.ts
+++ src/reanimated2/globals.d.ts
@@ -1,13 +1,14 @@
-import { AnimatedStyle, StyleProps } from './commonTypes';
+import { AnimatedStyle, StyleProps, MeasuredDimensions } from './commonTypes';
 import { ReanimatedConsole } from './core';
-import FrameCallbackRegistryUI from './frameCallback/FrameCallbackRegistryUI';
-import { MeasuredDimensions } from './NativeMethods';
 import { NativeReanimated } from './NativeReanimated/NativeReanimated';
+
 declare global {
   const _WORKLET: boolean;
-  const _frameTimestamp: number;
+  const _frameTimestamp: number | null;
   const _eventTimestamp: number;
+  const __reanimatedModuleProxy: NativeReanimated;
   const _setGlobalConsole: (console?: ReanimatedConsole) => void;
+  const _log: (s: string) => void;
   const _getCurrentTime: () => number;
   const _stopObservingProgress: (tag: number, flag: boolean) => void;
   const _startObservingProgress: (
@@ -20,7 +21,7 @@ declare global {
     name: string,
     updates: StyleProps | AnimatedStyle
   ) => void;
-  const _measure: (viewTag: number) => MeasuredDimensions | null;
+  const _measure: (viewTag: number) => MeasuredDimensions;
   const _scrollTo: (
     viewTag: number,
     x: number,
@@ -28,20 +29,40 @@ declare global {
     animated: boolean
   ) => void;
   const _chronoNow: () => number;
+  const performance: { now: () => number };
+  const LayoutAnimationRepository: {
+    configs: Record<string, unknown>;
+    registerConfig(tag: number, config: Record<string, unknown>): void;
+    removeConfig(tag: number): void;
+    startAnimationForTag(tag: number, type: string, yogaValues: unknown): void;
+  };
   const ReanimatedDataMock: {
     now: () => number;
   };
-  const _frameCallbackRegistry: FrameCallbackRegistryUI;
+
   namespace NodeJS {
     interface Global {
+      _WORKLET: boolean;
+      _IS_FABRIC: boolean;
+      _frameTimestamp: number | null;
+      _eventTimestamp: number;
+      __reanimatedModuleProxy: NativeReanimated;
       _setGlobalConsole: (console?: ReanimatedConsole) => void;
       _log: (s: string) => void;
-      _setGestureState: () => void;
-      _WORKLET: boolean;
-      __reanimatedModuleProxy: NativeReanimated;
-      _frameTimestamp: number | null;
-      _measure: () => MeasuredDimensions | null;
-      _scrollTo: () => void;
+      _getCurrentTime: () => number;
+      _stopObservingProgress: (tag: number, flag: boolean) => void;
+      _startObservingProgress: (
+        tag: number,
+        flag: { value: boolean; _value: boolean }
+      ) => void;
+      _setGestureState: (handlerTag: number, newState: number) => void;
+      _measure: (viewTag: number) => MeasuredDimensions;
+      _scrollTo: (
+        viewTag: number,
+        x: number,
+        y: number,
+        animated: boolean
+      ) => void;
       _chronoNow: () => number;
       performance: { now: () => number };
       LayoutAnimationRepository: {
@@ -57,7 +78,6 @@ declare global {
       ReanimatedDataMock: {
         now: () => number;
       };
-      _frameCallbackRegistry: FrameCallbackRegistryUI;
     }
   }
 }
