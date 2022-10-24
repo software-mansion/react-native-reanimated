5e15541729ea28a0f042fbec38521fc32340aa4f
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-10-24 12:11:24 +0200

    Update NativeMethods.ts

--- src/reanimated2/NativeMethods.ts
+++ src/reanimated2/NativeMethods.ts
@@ -13,11 +13,12 @@ export function getTag(
 
 const isNative = !shouldBeUseWeb();
 
-export function measure(
+export let measure: (
   animatedRef: RefObjectFunction<Component>
-): MeasuredDimensions | null {
-  'worklet';
-  if (isWeb()) {
+) => MeasuredDimensions | null;
+
+if (isWeb()) {
+  measure = (animatedRef: RefObjectFunction<Component>) => {
     const element = animatedRef() as unknown as HTMLElement; // TODO: fix typing of animated refs on web
     const viewportOffset = element.getBoundingClientRect();
     return {
@@ -28,52 +29,55 @@ export function measure(
       pageX: viewportOffset.left,
       pageY: viewportOffset.top,
     };
-  }
-
-  if (isChromeDebugger()) {
+  };
+} else if (isChromeDebugger()) {
+  measure = (_animatedRef: RefObjectFunction<Component>) => {
     console.warn('[Reanimated] measure() cannot be used with Chrome Debugger');
     return null;
-  }
+  };
+} else {
+  measure = (animatedRef: RefObjectFunction<Component>) => {
+    'worklet';
+    if (!_WORKLET) {
+      console.warn(
+        '[Reanimated] measure() was called from the main JS context. Measure is ' +
+          'only available in the UI runtime. This may also happen if measure() ' +
+          'was called by a worklet in the useAnimatedStyle hook, because useAnimatedStyle ' +
+          'calls the given worklet on the JS runtime during render. If you want to ' +
+          'prevent this warning then wrap the call with `if (_WORKLET)`. Then it will ' +
+          'only be called on the UI runtime after the render has been completed.'
+      );
+      return null;
+    }
 
-  if (!_WORKLET) {
-    console.warn(
-      '[Reanimated] measure() was called from the main JS context. Measure is ' +
-        'only available in the UI runtime. This may also happen if measure() ' +
-        'was called by a worklet in the useAnimatedStyle hook, because useAnimatedStyle ' +
-        'calls the given worklet on the JS runtime during render. If you want to ' +
-        'prevent this warning then wrap the call with `if (_WORKLET)`. Then it will ' +
-        'only be called on the UI runtime after the render has been completed.'
-    );
-    return null;
-  }
+    const viewTag = animatedRef();
+    if (viewTag === -1) {
+      console.warn(
+        `[Reanimated] The view with tag ${viewTag} is not a valid argument for measure(). This may be because the view is not currently rendered, which may not be a bug (e.g. an off-screen FlatList item).`
+      );
+      return null;
+    }
 
-  const viewTag = animatedRef();
-  if (viewTag === -1) {
-    console.warn(
-      `[Reanimated] The view with tag ${viewTag} is not a valid argument for measure(). This may be because the view is not currently rendered, which may not be a bug (e.g. an off-screen FlatList item).`
-    );
-    return null;
-  }
-
-  const measured = _measure(viewTag);
-  if (measured === null) {
-    console.warn(
-      `[Reanimated] The view with tag ${viewTag} has some undefined, not-yet-computed or meaningless value of \`LayoutMetrics\` type. This may be because the view is not currently rendered, which may not be a bug (e.g. an off-screen FlatList item).`
-    );
-    return null;
-  } else if (measured.x === -1234567) {
-    console.warn(
-      `[Reanimated] The view with tag ${viewTag} returned an invalid measurement response`
-    );
-    return null;
-  } else if (isNaN(measured.x)) {
-    console.warn(
-      `[Reanimated] The view with tag ${viewTag} gets view-flattened on Android. To disable view-flattening, set \`collapsable={false}\` on this component.`
-    );
-    return null;
-  } else {
-    return measured;
-  }
+    const measured = _measure(viewTag);
+    if (measured === null) {
+      console.warn(
+        `[Reanimated] The view with tag ${viewTag} has some undefined, not-yet-computed or meaningless value of \`LayoutMetrics\` type. This may be because the view is not currently rendered, which may not be a bug (e.g. an off-screen FlatList item).`
+      );
+      return null;
+    } else if (measured.x === -1234567) {
+      console.warn(
+        `[Reanimated] The view with tag ${viewTag} returned an invalid measurement response`
+      );
+      return null;
+    } else if (isNaN(measured.x)) {
+      console.warn(
+        `[Reanimated] The view with tag ${viewTag} gets view-flattened on Android. To disable view-flattening, set \`collapsable={false}\` on this component.`
+      );
+      return null;
+    } else {
+      return measured;
+    }
+  };
 }
 
 export function dispatchCommand(
