719242f3a577b19547c661da2b5ab8c32dc47863
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2022-10-27 16:48:53 +0200

    Lint iOS on CI (#3728)

--- ios/REAModule.mm
+++ ios/REAModule.mm
@@ -195,19 +195,21 @@ RCT_EXPORT_METHOD(triggerRender)
   [_nodesManager dispatchEvent:event];
 }
 
-- (void)startObserving {
-    hasListeners = YES;
+- (void)startObserving
+{
+  hasListeners = YES;
 }
 
-- (void)stopObserving {
-    hasListeners = NO;
+- (void)stopObserving
+{
+  hasListeners = NO;
 }
 
 - (void)sendEventWithName:(NSString *)eventName body:(id)body
 {
-    if (hasListeners) {
-        [super sendEventWithName:eventName body:body];
-    }
+  if (hasListeners) {
+    [super sendEventWithName:eventName body:body];
+  }
 }
 
 @end
