80306bb4daa963f0f585faea4fa264bde4c40e48
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-11-04 11:51:21 +0100

    CI test v8

--- .github/workflows/helper/configureV8.js
+++ .github/workflows/helper/configureV8.js
@@ -4,30 +4,48 @@ const buildGradlePath = 'app/android/app/build.gradle';
 const mainApplicationPath =
   'app/android/app/src/main/java/com/app/MainApplication.java';
 
-const { exec } = require('child_process');
-
-exec('ls -la', (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
-
 fs.readFile(buildGradlePath, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+  data = data.replace('enableHermes: true,', 'enableHermes: false,');
+
+  fs.writeFile(buildGradlePath, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
 
 fs.readFile(mainApplicationPath, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+  const imports = `
+  import java.util.List;
+  import com.facebook.react.bridge.JavaScriptExecutorFactory;
+  import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
+  import io.csie.kudo.reactnative.v8.executor.V8ExecutorFactory;
+  `;
+
+  const getJSMainModuleName = 'protected String getJSMainModuleName() {';
+  const getJavaScriptExecutorFactory = `
+  @Override
+  protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
+    return new V8ExecutorFactory(
+        getApplicationContext(),
+        getPackageName(),
+        AndroidInfoHelpers.getFriendlyDeviceName(),
+        getUseDeveloperSupport());
+  }
+  
+  protected String getJSMainModuleName() {`;
+
+  data = data.replace(getJSMainModuleName, getJavaScriptExecutorFactory);
+
+  fs.writeFile(mainApplicationPath, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
