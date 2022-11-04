7ebeea6bc5c5189b5598799be6fa999d93816577
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-11-04 11:20:55 +0100

    CI test v6

--- .github/workflows/helper/configureV8.js
+++ .github/workflows/helper/configureV8.js
@@ -5,6 +5,20 @@ const buildGradlePath =
 const mainApplicationPath =
   '../../../../app/node_modules/react-native-reanimated/android/app/src/main/java/com/app/MainApplication.java';
 
+const { exec } = require('child_process');
+
+exec('ls -la', (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
 fs.readFile(buildGradlePath, 'utf8', function (err, data) {
   console.log(__dirname);
   if (err) {
