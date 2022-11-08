ef8082804ca1de1d91cb8fe0ae7927dfb6156885
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-11-08 12:02:42 +0100

    Update download_aar.sh

--- download_aar.sh
+++ download_aar.sh
@@ -1,9 +1,5 @@
-mkdir -p node_modules/react-native/android && \
-    cd node_modules/react-native/android && \
-    rm -rf react-native-0.71.0-rc.0-debug.aar && \
-    wget https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
+mkdir -p node_modules/react-native/android
+wget -P node_modules/react-native/android -nc https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
 
-mkdir -p Example/node_modules/react-native/android && \
-    cd Example/node_modules/react-native/android && \
-    rm -rf react-native-0.71.0-rc.0-debug.aar && \
-    wget https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
+mkdir -p Example/node_modules/react-native/android
+wget -P Example/node_modules/react-native/android -nc https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
