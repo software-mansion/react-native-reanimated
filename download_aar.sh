f5144734a52ee4af05b31ff2eefc10a0d6ccc0a6
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-11-10 13:22:49 +0100

    Run Example app with Hermes enabled

--- download_aar.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p node_modules/react-native/android
-wget -P node_modules/react-native/android -nc https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
-
-mkdir -p Example/node_modules/react-native/android
-wget -P Example/node_modules/react-native/android -nc https://repo1.maven.org/maven2/com/facebook/react/react-native/0.71.0-rc.0/react-native-0.71.0-rc.0-debug.aar
