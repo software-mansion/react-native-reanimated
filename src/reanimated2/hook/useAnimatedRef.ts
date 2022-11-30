software-mansion/react-native-reanimated

commit 99573c396ca6b7e05ec28b5f2165ee7ad06c6dc5

Shareable value rewrite (#3722)
## Description

This PR contains a new implementation of the "shareable value" concept – one of the lowest level key concepts behind shared values/worklets etc.

There were many reasons for attempting this rewrite, but the main objective was to address memory-related issues rooted in that core part and difficult to fix due to an inherent complexity of that bit of the codebase. Below, I list a couple of reasons providing some more details description:

1. The aforementioned memory issues can be best noted on #2824 – the root cause of that and similar issue lies in the way we reference objects between the two runtimes. Due to the fact we use JSI's host objects and direct references it turns out that a lot of objects can outlast garbage collection because, for example they are referenced on the secondary runtime where garbage collection didn't run yet. Moreover, JSI objects have their own, simplified version of GC that has to run as well. As a result, for some objects to be cleaned up properly, we needed to run GC on the main JS runtime, then on the UI runtime and once again on the main JS runtime.
2. Secondary objective for the rewrite was to address the maintainability of the codebase part being rewritten. After investigating the issue mentioned above we concluded that it is too difficult and risky to try to fix it given the architectural choices we made in the past.

src/reanimated2/hook/useAnimatedRef.ts
@@ -4,30 +4,39 @@ import { RefObjectFunction } from './commonTypes';
 import { ShadowNodeWrapper } from '../commonTypes';
 import { getTag } from '../NativeMethods';
 import { getShadowNodeWrapperFromHostInstance } from '../fabricUtils';
+import {
+  makeShareableCloneRecursive,
+  registerShareableMapping,
+} from '../shareables';
+
+const getTagValueFunction = global._IS_FABRIC
+  ? getShadowNodeWrapperFromHostInstance
+  : getTag;
 
 export function useAnimatedRef<T extends Component>(): RefObjectFunction<T> {
   const tag = useSharedValue<number | ShadowNodeWrapper | null>(-1);
   const ref = useRef<RefObjectFunction<T>>();
-  const isFabric = global._IS_FABRIC;
 
   if (!ref.current) {
     const fun: RefObjectFunction<T> = <RefObjectFunction<T>>((component) => {
-      'worklet';
       // enters when ref is set by attaching to a component
       if (component) {
-        tag.value = isFabric
-          ? getShadowNodeWrapperFromHostInstance(component)
-          : getTag(component);
+        tag.value = getTagValueFunction(component);
         fun.current = component;
       }
       return tag.value;
     });
 
-    Object.defineProperty(fun, 'current', {
-      value: null,
-      writable: true,
-      enumerable: false,
+    fun.current = null;
+
+    const remoteRef = makeShareableCloneRecursive({
+      __init: () => {
+        'worklet';
+        return () => tag.value;
+      },
     });
+    registerShareableMapping(fun, remoteRef);
+
     ref.current = fun;
   }
 

commit ef6e0f222ccd9d87447fede810daf544cc20bd16

Fix animated refs on Fabric (#3822)
## Summary

This PR fixes crash on Fabric when using synchronous measure/scrollTo
etc – this error was a regression introduced in #3722.

The issue was that did not transform shadow node wrapper ref into
shareable ref. Shareable ref was necessary for the UI side to recognize
and extract the shadow node wrapper that was needed for the sync calls
to execute.

## Test plan

Run FabricExample, use "Measure on UI" on the "Measure Example" screen
and "ScrollTo on UI" button on the "ScrollTo Example" screen.

src/reanimated2/hook/useAnimatedRef.ts
@@ -6,11 +6,20 @@ import { getTag } from '../NativeMethods';
 import { getShadowNodeWrapperFromHostInstance } from '../fabricUtils';
 import {
   makeShareableCloneRecursive,
+  makeShareableShadowNodeWrapper,
   registerShareableMapping,
 } from '../shareables';
 
+function getShareableShadowNodeFromComponent(
+  component: Component
+): ShadowNodeWrapper {
+  return makeShareableShadowNodeWrapper(
+    getShadowNodeWrapperFromHostInstance(component)
+  );
+}
+
 const getTagValueFunction = global._IS_FABRIC
-  ? getShadowNodeWrapperFromHostInstance
+  ? getShareableShadowNodeFromComponent
   : getTag;
 
 export function useAnimatedRef<T extends Component>(): RefObjectFunction<T> {