0ab53d929aa7d5132a3de09c1ee9837fb55cbb56
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2022-12-13 11:36:58 +0100

    Revert old example

--- Example/src/AnimatedStyleUpdateExample.tsx
+++ Example/src/AnimatedStyleUpdateExample.tsx
@@ -1,128 +1,46 @@
-import * as React from 'react';
-import { View, Image, TouchableNativeFeedback } from 'react-native';
-import { NavigationContainer, ParamListBase } from '@react-navigation/native';
-import { createNativeStackNavigator } from 'react-native-screens/native-stack';
-import Animated from 'react-native-reanimated';
-import { StackScreenProps } from '@react-navigation/stack';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button } from 'react-native';
+import React from 'react';
 
-const photo = require('./image.jpg');
-const Stack = createNativeStackNavigator();
-const AnimatedImage = Animated.createAnimatedComponent(Image);
+function AnimatedStyleUpdateExample(): React.ReactElement {
+  const randomWidth = useSharedValue(10);
 
-function Card({ navigation, title, transitionTag, isOpen = false }: any) {
-  const goNext = (screenName: string) => {
-    navigation.navigate(screenName, {
-      title: title,
-      sharedTransitionTag: transitionTag,
-    });
+  const config = {
+    duration: 500,
+    easing: Easing.bezierFn(0.5, 0.01, 0, 1),
   };
 
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
   return (
-    <TouchableNativeFeedback
-      onPress={() => {
-        goNext(isOpen ? 'Screen1' : 'Screen2');
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
       }}>
       <Animated.View
-        style={
-          isOpen
-            ? { height: 500, marginTop: 50, backgroundColor: 'green' }
-            : { height: 120, marginTop: 20, backgroundColor: 'red' }
-        }
-        sharedTransitionTag={transitionTag + '1'}
-        >
-        <Animated.Text
-          sharedTransitionTag={transitionTag + '2'}
-          style={{ width: '100%', height: 20 }}>
-          {title}
-        </Animated.Text>
-        <AnimatedImage
-          sharedTransitionTag={transitionTag + '3'}
-          source={photo}
-          style={{ width: '100%', height: isOpen ? 300 : 100 }}
-        />
-        {/* <Animated.View
-        sharedTransitionTag={transitionTag + "3"}
-        style={{ width: '100%', borderWidth: 5, backgroundColor: isOpen ? 'olive' : 'purple', height: isOpen ? 200 : 100 }}
-      /> */}
-        <Animated.Text
-          sharedTransitionTag={transitionTag + '4'}
-          style={{ width: '100%', height: isOpen ? 100 : 0 }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas aliquid,
-          earum non, dignissimos fugit rerum exercitationem ab consequatur,
-          error animi veritatis delectus. Nostrum sapiente distinctio possimus
-          vel nam facilis ut?
-        </Animated.Text>
-      </Animated.View>
-    </TouchableNativeFeedback>
-  );
-}
-
-function Screen1({ navigation }: StackScreenProps<ParamListBase>) {
-  return (
-    <Animated.ScrollView style={{ flex: 1 /* marginTop: 200 */ }}>
-      {[...Array(6)].map((_, i) => (
-        <Card
-          key={i}
-          navigation={navigation}
-          title={'Mleko' + i}
-          transitionTag={'mleko1' + i}
-        />
-      ))}
-    </Animated.ScrollView>
-  );
-}
-
-function Screen3({ route, navigation }: StackScreenProps<ParamListBase>) {
-  const { title, sharedTransitionTag } = route.params as any;
-
-  return (
-    <View style={{ flex: 1 }}>
-      <Card
-        navigation={navigation}
-        title={title}
-        transitionTag={sharedTransitionTag}
-        isOpen={true}
+        style={[
+          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
+          style,
+        ]}
+      />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
       />
     </View>
   );
 }
 
-function Screen2({ route, navigation }: StackScreenProps<ParamListBase>) {
-  const { title, sharedTransitionTag } = route.params as any;
-
-  return (
-    <View style={{ flex: 1 }}>
-      <Card
-        navigation={navigation}
-        title={title}
-        transitionTag={sharedTransitionTag}
-        isOpen={true}
-      />
-    </View>
-  );
-}
-
-export default function SimpleSharedElementTransition() {
-  return (
-    // <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          // stackAnimation: 'fade_from_bottom',
-          // stackAnimation: 'slide_from_right',
-          // stackAnimation: 'fade',
-          // stackAnimation: 'none',
-        }}>
-        <Stack.Screen
-          name="Screen1"
-          component={Screen1}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Screen2"
-          component={Screen2}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
-    // </NavigationContainer>
-  );
-}
+export default AnimatedStyleUpdateExample;
