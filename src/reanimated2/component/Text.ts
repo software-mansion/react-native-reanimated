ac58a1b0ba82ed26ccef3810b34443eda7732475
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-01-03 13:48:25 +0100

    Don't cast text to any

--- src/reanimated2/component/Text.ts
+++ src/reanimated2/component/Text.ts
@@ -1,6 +1,6 @@
 import { Text } from 'react-native';
 import createAnimatedComponent from '../../createAnimatedComponent';
 
-export const AnimatedText = createAnimatedComponent(Text as any);
+export const AnimatedText = createAnimatedComponent(Text);
 
 export type AnimatedText = typeof AnimatedText & Text;
