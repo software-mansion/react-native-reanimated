7e72beb99bbf3c541c3781d5bb45c44057581bd5
Angelika Serwa <angelika.serwa@swmansion.com>
2023-01-03 18:21:16 +0100

    Add more tests

--- __tests__/InterpolateColor.test.js
+++ __tests__/InterpolateColor.test.js
@@ -11,16 +11,33 @@ import Animated, {
 describe('colors interpolation', () => {
   it('interpolates rgb without gamma correction', () => {
     const colors = ['#105060', '#609020'];
-    const interpolatedColor = interpolateColor(0.5, [0, 1], colors, 'RGB', {
+    let interpolatedColor = interpolateColor(0.5, [0, 1], colors, 'RGB', {
       gamma: 1,
     });
     expect(interpolatedColor).toBe('rgba(56, 112, 64, 1)');
+
+    interpolatedColor = interpolateColor(0, [0, 1], colors, 'RGB', {
+      gamma: 1,
+    });
+    expect(interpolatedColor).toBe('rgba(16, 80, 96, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors, 'RGB', {
+      gamma: 1,
+    });
+    expect(interpolatedColor).toBe('rgba(96, 144, 32, 1)');
   });
 
   it('interpolates rgb with gamma correction', () => {
     const colors = ['#105060', '#609020'];
-    const interpolatedColor = interpolateColor(0.5, [0, 1], colors);
+
+    let interpolatedColor = interpolateColor(0.5, [0, 1], colors);
     expect(interpolatedColor).toBe('rgba(71, 117, 73, 1)');
+
+    interpolatedColor = interpolateColor(0, [0, 1], colors);
+    expect(interpolatedColor).toBe('rgba(16, 80, 96, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors);
+    expect(interpolatedColor).toBe('rgba(96, 144, 32, 1)');
   });
 
   it('interpolates hsv', () => {
@@ -30,11 +47,31 @@ describe('colors interpolation', () => {
     });
     expect(interpolatedColor).toBe('rgba(23, 120, 54, 1)');
 
+    interpolatedColor = interpolateColor(0, [0, 1], colors, 'HSV', {
+      useCorrectedHSVInterpolation: false,
+    });
+    expect(interpolatedColor).toBe('rgba(16, 80, 96, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors, 'HSV', {
+      useCorrectedHSVInterpolation: false,
+    });
+    expect(interpolatedColor).toBe('rgba(96, 144, 32, 1)');
+
     colors = ['#ff4800', '#ff00fb'];
     interpolatedColor = interpolateColor(0.5, [0, 1], colors, 'HSV', {
       useCorrectedHSVInterpolation: false,
     });
     expect(interpolatedColor).toBe('rgba(0, 255, 166, 1)');
+
+    interpolatedColor = interpolateColor(0, [0, 1], colors, 'HSV', {
+      useCorrectedHSVInterpolation: false,
+    });
+    expect(interpolatedColor).toBe('rgba(255, 72, 0, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors, 'HSV', {
+      useCorrectedHSVInterpolation: false,
+    });
+    expect(interpolatedColor).toBe('rgba(255, 0, 251, 1)');
   });
 
   it('interpolates corrected hsv', () => {
@@ -42,9 +79,21 @@ describe('colors interpolation', () => {
     let interpolatedColor = interpolateColor(0.5, [0, 1], colors, 'HSV');
     expect(interpolatedColor).toBe('rgba(23, 120, 54, 1)');
 
+    interpolatedColor = interpolateColor(0, [0, 1], colors, 'HSV');
+    expect(interpolatedColor).toBe('rgba(16, 80, 96, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors, 'HSV');
+    expect(interpolatedColor).toBe('rgba(96, 144, 32, 1)');
+
     colors = ['#ff4800', '#ff00fb'];
     interpolatedColor = interpolateColor(0.5, [0, 1], colors, 'HSV');
     expect(interpolatedColor).toBe('rgba(255, 0, 90, 1)');
+
+    interpolatedColor = interpolateColor(0, [0, 1], colors, 'HSV');
+    expect(interpolatedColor).toBe('rgba(255, 72, 0, 1)');
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors, 'HSV');
+    expect(interpolatedColor).toBe('rgba(255, 0, 251, 1)');
   });
 
   it('interpolates css colors', () => {
@@ -55,8 +104,14 @@ describe('colors interpolation', () => {
 
   it('interpolates semi-transparent colors', () => {
     const colors = ['#10506050', '#60902070'];
-    const interpolatedColor = interpolateColor(0.5, [0, 1], colors);
+    let interpolatedColor = interpolateColor(0.5, [0, 1], colors);
     expect(interpolatedColor).toBe(`rgba(71, 117, 73, ${96 / 255})`);
+
+    interpolatedColor = interpolateColor(0, [0, 1], colors);
+    expect(interpolatedColor).toBe(`rgba(16, 80, 96, ${80 / 255})`);
+
+    interpolatedColor = interpolateColor(1, [0, 1], colors);
+    expect(interpolatedColor).toBe(`rgba(96, 144, 32, ${112 / 255})`);
   });
 
   function TestComponent() {
