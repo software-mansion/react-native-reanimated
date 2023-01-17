c1becc98cd099c4a4564e16a99c2905917e36c52
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-01-17 13:53:48 +0100

    Raise error when `cppVersion` is undefined (#3944)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please follow the template so that
    the reviewers can easily understand what the code changes affect. -->
    
    ## Summary
    
    This PR provides a better error message in cases like #3943.
    
    <!-- Explain the motivation for this PR. Include "Fixes #<number>" if
    applicable. -->
    
    ## Test plan
    
    <!-- Provide a minimal but complete code snippet that can be used to
    test out this change along with instructions how to run it and a
    description of the expected behavior. -->

--- src/reanimated2/platform-specific/checkVersion.ts
+++ src/reanimated2/platform-specific/checkVersion.ts
@@ -5,6 +5,12 @@ import { version as jsVersion } from '../../../package.json';
  */
 export function checkVersion(): void {
   const cppVersion = global._REANIMATED_VERSION_CPP;
+  if (cppVersion === undefined) {
+    console.error(
+      `[Reanimated] Couldn't determine the version of the native part of Reanimated. Did you forget to re-build the app after upgrading react-native-reanimated? If you use Expo Go, you must use the exact version which is bundled into Expo SDK.`
+    );
+    return;
+  }
   const ok = (() => {
     if (
       jsVersion.match(/^\d+\.\d+\.\d+$/) &&
