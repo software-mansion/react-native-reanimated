09968788f703088f7a908f2ffde1054ebf5b3342
Juliusz Wajgelt <jwajgelt@gmail.com>
2023-02-24 12:19:11 +0100

    add a comment in `checkVersion`

--- src/reanimated2/platform-specific/checkVersion.ts
+++ src/reanimated2/platform-specific/checkVersion.ts
@@ -1,3 +1,8 @@
+/**
+ * We hardcode the version of Reanimated here in order to compare it
+ * with the version used to build the native part of the library in runtime.
+ * Remember to keep this in sync with the version declared in `package.json`
+ */
 const jsVersion = '3.0.0-rc.10';
 
 /**
