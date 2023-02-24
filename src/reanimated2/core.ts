software-mansion/react-native-reanimated

commit 535b5d64c0720da3a9a39d99a67e878fe09b5154

Add setImmediate implementation to the UI runtime (#3970)
## Summary

This PR adds an implementation of web's setImmediate API to the
reanimated's UI runtime. We utilize this implementation in mappers and
event handlers for more accurate animation/gesture tracking as well as
unblock further optimizations.

There are several things that this PR changes:
1) We add event timestamp metadata to touch/scroll/sensor/etc events.
This change is in preparation of providing a true system timestamps for
such events (we don't do that yet but instead call system timer in order
to pass down the current time)
2) We update performance.now API to use the same reference point for the
provided time as requestAnimationFrame API. Before performance.now would
use a different reference point which made it impossible to use it as a
starting point for animations.
3) We update event handler callbacks such that they perform a flush for
rAF and immediates queues
4) We populate global.__frameTimestamp for all the code that executes
within requestAnimationFrame and event callbacks. We then use it in
useAnimatedStyle as a reference point for starting animations and in
case it wasn't set, we use performance.now
5) We add some code in initializers.ts to define setImmediate method and
to register method for flushing immediates queue
6) We introduce set-immediate-based batching for runOnUI calls on the
main RN runtime. We then flush all "immediates" after the whole batch is
processed on the UI runtime.

## Test plan

Check a bunch of examples from the example app: "Use animated style",
"Bokeh", "Drag and snap".
For touch based interaction verify that movement doesn't lag behind the
event. The way this can be done is by screen recording drag and snap
example with touch indicators on, then checking on the recorded clip in
between frames whether there is a correct movement aligned with the
updates of the touch indicator.
The above needs to be done for all configurations:
Android/iOS/Fabric/Paper

src/reanimated2/core.ts
@@ -20,7 +20,6 @@ import { initializeUIRuntime } from './initializers';
 
 export { stopMapper } from './mappers';
 export { runOnJS, runOnUI } from './threads';
-export { getTimestamp } from './time';
 
 export type ReanimatedConsole = Pick<
   Console,
@@ -115,9 +114,16 @@ export function registerEventHandler<T>(
   eventHash: string,
   eventHandler: (event: T) => void
 ): string {
+  function handleAndFlushImmediates(eventTimestamp: number, event: T) {
+    'worklet';
+    global.__frameTimestamp = eventTimestamp;
+    eventHandler(event);
+    global.__flushAnimationFrame(eventTimestamp);
+    global.__frameTimestamp = undefined;
+  }
   return NativeReanimatedModule.registerEventHandler(
     eventHash,
-    makeShareableCloneRecursive(eventHandler)
+    makeShareableCloneRecursive(handleAndFlushImmediates)
   );
 }
 

.github/workflows/static-root-checks.yml
@@ -16,10 +16,10 @@ jobs:
     steps:
       - name: checkout
         uses: actions/checkout@v2
-      - name: Use Node.js 14
+      - name: Use Node.js 16
         uses: actions/setup-node@v2
         with:
-          node-version: 14
+          node-version: 16
           cache: 'yarn'
       - name: Install node dependencies
         run: yarn

Common/cpp/AnimatedSensor/AnimatedSensorModule.cpp
@@ -42,10 +42,12 @@ jsi::Value AnimatedSensorModule::registerSensor(
         }
 
         auto &rt = *runtimeHelper->uiRuntime();
-        auto handler =
-            shareableHandler->getJSValue(rt).asObject(rt).asFunction(rt);
+        auto handler = shareableHandler->getJSValue(rt);
         if (sensorType == SensorType::ROTATION_VECTOR) {
           jsi::Object value(rt);
+          // TODO: timestamp should be provided by the platform implementation
+          // such that the native side has a chance of providing a true event
+          // timestamp
           value.setProperty(rt, "qx", newValues[0]);
           value.setProperty(rt, "qy", newValues[1]);
           value.setProperty(rt, "qz", newValues[2]);
@@ -54,14 +56,14 @@ jsi::Value AnimatedSensorModule::registerSensor(
           value.setProperty(rt, "pitch", newValues[5]);
           value.setProperty(rt, "roll", newValues[6]);
           value.setProperty(rt, "interfaceOrientation", orientationDegrees);
-          handler.call(rt, value);
+          runtimeHelper->runOnUIGuarded(handler, value);
         } else {
           jsi::Object value(rt);
           value.setProperty(rt, "x", newValues[0]);
           value.setProperty(rt, "y", newValues[1]);
           value.setProperty(rt, "z", newValues[2]);
           value.setProperty(rt, "interfaceOrientation", orientationDegrees);
-          handler.call(rt, value);
+          runtimeHelper->runOnUIGuarded(handler, value);
         }
       });
   if (sensorId != -1) {

Common/cpp/NativeModules/NativeReanimatedModule.cpp
@@ -309,11 +309,13 @@ jsi::Value NativeReanimatedModule::registerEventHandler(
 
   scheduler->scheduleOnUI([=] {
     jsi::Runtime &rt = *runtimeHelper->uiRuntime();
-    auto handlerFunction =
-        handlerShareable->getJSValue(rt).asObject(rt).asFunction(rt);
+    auto handlerFunction = handlerShareable->getJSValue(rt);
     auto handler = std::make_shared<WorkletEventHandler>(
-        newRegistrationId, eventName, std::move(handlerFunction));
-    eventHandlerRegistry->registerEventHandler(handler);
+        runtimeHelper,
+        newRegistrationId,
+        eventName,
+        std::move(handlerFunction));
+    eventHandlerRegistry->registerEventHandler(std::move(handler));
   });
 
   return jsi::Value(static_cast<double>(newRegistrationId));
@@ -393,19 +395,11 @@ jsi::Value NativeReanimatedModule::configureLayoutAnimation(
 }
 
 void NativeReanimatedModule::onEvent(
+    double eventTimestamp,
     const std::string &eventName,
     const jsi::Value &payload) {
-  try {
-    eventHandlerRegistry->processEvent(*runtime, eventName, payload);
-  } catch (std::exception &e) {
-    std::string str = e.what();
-    this->errorHandler->setError(str);
-    this->errorHandler->raise();
-  } catch (...) {
-    std::string str = "OnEvent error";
-    this->errorHandler->setError(str);
-    this->errorHandler->raise();
-  }
+  eventHandlerRegistry->processEvent(
+      *runtime, eventTimestamp, eventName, payload);
 }
 
 bool NativeReanimatedModule::isAnyHandlerWaitingForEvent(
@@ -421,20 +415,10 @@ void NativeReanimatedModule::maybeRequestRender() {
 }
 
 void NativeReanimatedModule::onRender(double timestampMs) {
-  try {
-    std::vector<FrameCallback> callbacks = frameCallbacks;
-    frameCallbacks.clear();
-    for (auto &callback : callbacks) {
-      callback(timestampMs);
-    }
-  } catch (std::exception &e) {
-    std::string str = e.what();
-    this->errorHandler->setError(str);
-    this->errorHandler->raise();
-  } catch (...) {
-    std::string str = "OnRender error";
-    this->errorHandler->setError(str);
-    this->errorHandler->raise();
+  std::vector<FrameCallback> callbacks = frameCallbacks;
+  frameCallbacks.clear();
+  for (auto &callback : callbacks) {
+    callback(timestampMs);
   }
 }
 
@@ -485,13 +469,7 @@ bool NativeReanimatedModule::handleEvent(
     const std::string &eventName,
     const jsi::Value &payload,
     double currentTime) {
-  jsi::Runtime &rt = *runtime.get();
-  jsi::Object global = rt.global();
-  jsi::String eventTimestampName =
-      jsi::String::createFromAscii(rt, "_eventTimestamp");
-  global.setProperty(rt, eventTimestampName, currentTime);
-  onEvent(eventName, payload);
-  global.setProperty(rt, eventTimestampName, jsi::Value::undefined());
+  onEvent(currentTime, eventName, payload);
 
   // TODO: return true if Reanimated successfully handled the event
   // to avoid sending it to JavaScript
@@ -674,13 +652,12 @@ jsi::Value NativeReanimatedModule::subscribeForKeyboardEvents(
     const jsi::Value &handlerWorklet,
     const jsi::Value &isStatusBarTranslucent) {
   auto shareableHandler = extractShareableOrThrow(rt, handlerWorklet);
-  auto uiRuntime = runtimeHelper->uiRuntime();
   return subscribeForKeyboardEventsFunction(
       [=](int keyboardState, int height) {
-        jsi::Runtime &rt = *uiRuntime;
+        jsi::Runtime &rt = *runtimeHelper->uiRuntime();
         auto handler = shareableHandler->getJSValue(rt);
-        handler.asObject(rt).asFunction(rt).call(
-            rt, jsi::Value(keyboardState), jsi::Value(height));
+        runtimeHelper->runOnUIGuarded(
+            handler, jsi::Value(keyboardState), jsi::Value(height));
       },
       isStatusBarTranslucent.getBool());
 }

Common/cpp/NativeModules/NativeReanimatedModule.h
@@ -99,7 +99,10 @@ class NativeReanimatedModule : public NativeReanimatedModuleSpec,
 
   void onRender(double timestampMs);
 
-  void onEvent(const std::string &eventName, const jsi::Value &payload);
+  void onEvent(
+      double eventTimestamp,
+      const std::string &eventName,
+      const jsi::Value &payload);
 
   bool isAnyHandlerWaitingForEvent(std::string eventName);
 

Common/cpp/Registries/EventHandlerRegistry.cpp
@@ -24,6 +24,7 @@ void EventHandlerRegistry::unregisterEventHandler(uint64_t id) {
 
 void EventHandlerRegistry::processEvent(
     jsi::Runtime &rt,
+    double eventTimestamp,
     const std::string &eventName,
     const jsi::Value &eventPayload) {
   std::vector<std::shared_ptr<WorkletEventHandler>> handlersForEvent;
@@ -40,7 +41,7 @@ void EventHandlerRegistry::processEvent(
   eventPayload.asObject(rt).setProperty(
       rt, "eventName", jsi::String::createFromUtf8(rt, eventName));
   for (auto handler : handlersForEvent) {
-    handler->process(rt, eventPayload);
+    handler->process(eventTimestamp, eventPayload);
   }
 }
 

Common/cpp/Registries/EventHandlerRegistry.h
@@ -29,6 +29,7 @@ class EventHandlerRegistry {
 
   void processEvent(
       jsi::Runtime &rt,
+      double eventTimestamp,
       const std::string &eventName,
       const jsi::Value &eventPayload);
 

Common/cpp/Tools/RuntimeDecorator.cpp
@@ -71,28 +71,6 @@ void RuntimeDecorator::decorateRuntime(
 #endif // DEBUG
 
   jsi_utils::installJsiFunction(rt, "_log", logValue);
-
-  auto chronoNow = [](jsi::Runtime &rt,
-                      const jsi::Value &thisValue,
-                      const jsi::Value *args,
-                      size_t count) -> jsi::Value {
-    double now = std::chrono::system_clock::now().time_since_epoch() /
-        std::chrono::milliseconds(1);
-    return jsi::Value(now);
-  };
-
-  rt.global().setProperty(
-      rt,
-      "_chronoNow",
-      jsi::Function::createFromHostFunction(
-          rt, jsi::PropNameID::forAscii(rt, "_chronoNow"), 0, chronoNow));
-  jsi::Object performance(rt);
-  performance.setProperty(
-      rt,
-      "now",
-      jsi::Function::createFromHostFunction(
-          rt, jsi::PropNameID::forAscii(rt, "now"), 0, chronoNow));
-  rt.global().setProperty(rt, "performance", performance);
 }
 
 void RuntimeDecorator::decorateUIRuntime(
@@ -144,9 +122,20 @@ void RuntimeDecorator::decorateUIRuntime(
   jsi_utils::installJsiFunction(
       rt, "_updateDataSynchronously", updateDataSynchronously);
 
-  jsi_utils::installJsiFunction(rt, "_getCurrentTime", getCurrentTime);
-  rt.global().setProperty(rt, "_frameTimestamp", jsi::Value::undefined());
-  rt.global().setProperty(rt, "_eventTimestamp", jsi::Value::undefined());
+  auto performanceNow = [getCurrentTime](
+                            jsi::Runtime &rt,
+                            const jsi::Value &thisValue,
+                            const jsi::Value *args,
+                            size_t count) -> jsi::Value {
+    return jsi::Value(getCurrentTime());
+  };
+  jsi::Object performance(rt);
+  performance.setProperty(
+      rt,
+      "now",
+      jsi::Function::createFromHostFunction(
+          rt, jsi::PropNameID::forAscii(rt, "now"), 0, performanceNow));
+  rt.global().setProperty(rt, "performance", performance);
 
   // layout animation
   jsi_utils::installJsiFunction(

commit 2fd0da8818b63727a311eeb45817d949021d37b9

Flush animation-frame and immediates on keyboard event to follow same flow as other types of events (#4098)
## Summary

This diff fixes an issue with animated views not updating when animating
along keyboard. This problem regressed after #3970 where we changed the
logic of when enqueued callbacks for animation frame and immediates are
flushed. The logic has been moved from c++ to js and we added some code
to execute animation frame and immediates as a part of event handling
process. However, keyboard events have a separate flow of providing
event data (we should unify this at some point), and we missed updating
it in that place.

This PR adds similar logic to [what we use for regular
events](https://github.com/software-mansion/react-native-reanimated/blob/535b5d64c0720da3a9a39d99a67e878fe09b5154/src/reanimated2/core.ts#L117)
into the code responsible for handling keyboard events.

## Test plan

Run animated keyboard example on iOS and Android.

src/reanimated2/core.ts
@@ -138,8 +138,18 @@ export function subscribeForKeyboardEvents(
   eventHandler: (state: number, height: number) => void,
   options: AnimatedKeyboardOptions
 ): number {
+  // TODO: this should really go with the same code path as other events, that is
+  // via registerEventHandler. For now we are copying the code from there.
+  function handleAndFlushImmediates(state: number, height: number) {
+    'worklet';
+    const now = performance.now();
+    global.__frameTimestamp = now;
+    eventHandler(state, height);
+    global.__flushAnimationFrame(now);
+    global.__frameTimestamp = undefined;
+  }
   return NativeReanimatedModule.subscribeForKeyboardEvents(
-    makeShareableCloneRecursive(eventHandler),
+    makeShareableCloneRecursive(handleAndFlushImmediates),
     options.isStatusBarTranslucentAndroid ?? false
   );
 }