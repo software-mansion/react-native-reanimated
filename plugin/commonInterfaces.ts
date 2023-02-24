077ea394d25db8268a9d848d5b06ee7d872f0d2c
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-02-24 09:24:10 +0100

    removed deleted files

--- plugin/commonInterfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface BabelMapType {
-  version: number;
-  sources: string[];
-  names: string[];
-  sourceRoot?: string | undefined;
-  sourcesContent?: string[] | undefined;
-  mappings: string;
-  file: string;
-}
