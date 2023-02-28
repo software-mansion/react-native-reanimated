5e94779bb42908ddfac53c156b05d1a8f838576b
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-02-28 13:21:05 +0100

    chore: add flatlists

--- Example/src/SharedElementTransition/Profiles.tsx
+++ Example/src/SharedElementTransition/Profiles.tsx
@@ -7,6 +7,7 @@ import {
   Dimensions,
   ImageBackground,
   Image,
+  FlatList,
 } from 'react-native';
 import {
   createNativeStackNavigator,
@@ -22,6 +23,18 @@ import Animated, {
 // const countryside = require('./assets/countryside.jpg');
 // const dawn = require('./assets/dawn.jpg');
 const nature = require('./assets/nature.jpg');
+
+const lake1 = require('./assets/lake-1.jpg');
+const lake2 = require('./assets/lake-2.jpg');
+const lake3 = require('./assets/lake-3.jpg');
+const lake4 = require('./assets/lake-4.jpg');
+const lake5 = require('./assets/lake-5.jpg');
+
+const forrest1 = require('./assets/forrest-1.jpg');
+const forrest2 = require('./assets/forrest-2.jpg');
+const forrest3 = require('./assets/forrest-3.jpg');
+const forrest4 = require('./assets/forrest-4.jpg');
+const forrest5 = require('./assets/forrest-5.jpg');
 
 const dogge = require('./assets/dogge-avatar.png');
 const desert = require('./assets/desert-avatar.png');
@@ -143,6 +156,89 @@ function ProfilesScreen({
   );
 }
 
+// const lakes = {
+//   lake1: {
+//     image: lake1,
+//     title: 'Lake 1',
+//   },
+//   lake2: {
+//     image: lake2,
+//     title: 'Lake 2',
+//   },
+//   lake3: {
+//     image: lake3,
+//     title: 'Lake 3',
+//   },
+//   lake4: {
+//     image: lake4,
+//     title: 'Lake 4',
+//   },
+//   lake5: {
+//     image: lake5,
+//     title: 'Lake 5',
+//   },
+// };
+
+// I totally made these names up, im sorry
+// French names in the US Top 200 for girls include Annabelle, Charlotte, Claire, Josephine, and Sophie.
+const lakes = [
+  {
+    image: lake3,
+    title: 'Lake Annabelle',
+    id: 'lake-1',
+  },
+  {
+    image: lake2,
+    title: 'Lake Charlotte',
+    id: 'lake-2',
+  },
+  {
+    image: lake1,
+    title: 'Lake Claire',
+    id: 'lake-3',
+  },
+  {
+    image: lake4,
+    title: 'Lake Claire',
+    id: 'lake-4',
+  },
+  {
+    image: lake5,
+    title: 'Lake Sophie',
+    id: 'lake-5',
+  },
+];
+
+// I made that up too
+// https://en.natmus.dk/historical-knowledge/denmark/prehistoric-period-until-1050-ad/the-viking-age/the-people/names/
+const forrests = [
+  {
+    image: forrest2,
+    title: 'Arne Forrest',
+    id: 'forrest-1',
+  },
+  {
+    image: forrest4,
+    title: 'Birger Forrest',
+    id: 'forrest-2',
+  },
+  {
+    image: forrest1,
+    title: 'Bjørn Forrest',
+    id: 'forrest-3',
+  },
+  {
+    image: forrest3,
+    title: 'Halfdan Forrest',
+    id: 'forrest-4',
+  },
+  {
+    image: forrest5,
+    title: 'Astrid Forrest',
+    id: 'forrest-5',
+  },
+];
+
 function HomeScreen({
   route,
   navigation,
@@ -173,7 +269,8 @@ function HomeScreen({
               ...styles.header,
               fontSize: 40,
               flex: 1,
-              color: '#064e3b',
+              color: '#1e293b',
+              fontFamily: 'Poppins-Medium',
             }}>
             Home
           </Text>
@@ -192,6 +289,93 @@ function HomeScreen({
           />
         </Pressable>
       </View>
+      <Text
+        style={{
+          fontSize: 24,
+          fontFamily: 'Poppins-Medium',
+          color: '#1e40af',
+          marginBottom: 15,
+          marginLeft: 20,
+        }}>
+        Lakes
+      </Text>
+      <FlatList
+        data={lakes}
+        style={{ marginLeft: 10 }}
+        renderItem={(item: any) => {
+          return (
+            <View style={{ marginHorizontal: 10, height: 150 }}>
+              <Animated.Image
+                sharedTransitionTag={item.item.id}
+                sharedTransitionStyle={transition}
+                source={item.item.image}
+                style={{
+                  height: 200,
+                  width: 150,
+                  marginBottom: 8,
+                  borderRadius: 10,
+                }}
+              />
+              <Animated.Text
+                sharedTransitionTag={`${item.item.id}-text`}
+                sharedTransitionStyle={transition}
+                style={{
+                  fontFamily: 'Poppins-Regular',
+                  fontSize: 16,
+                  color: '#1e40af',
+                }}>
+                {item.item.title}
+              </Animated.Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+      />
+
+      <Text
+        style={{
+          fontSize: 24,
+          fontFamily: 'Poppins-Medium',
+          color: '#166534',
+          marginBottom: 15,
+          marginLeft: 20,
+        }}>
+        Forrests
+      </Text>
+      <FlatList
+        data={forrests}
+        style={{ marginLeft: 10 }}
+        renderItem={(item: any) => {
+          return (
+            <View style={{ marginHorizontal: 10 }}>
+              <Animated.Image
+                sharedTransitionTag={item.item.id}
+                sharedTransitionStyle={transition}
+                source={item.item.image}
+                style={{
+                  height: 200,
+                  width: 150,
+                  marginBottom: 8,
+                  borderRadius: 10,
+                }}
+              />
+              <Animated.Text
+                sharedTransitionTag={`${item.item.id}-text`}
+                sharedTransitionStyle={transition}
+                style={{
+                  fontFamily: 'Poppins-Regular',
+                  fontSize: 16,
+                  color: '#064e3b',
+                }}>
+                {item.item.title}
+              </Animated.Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+      />
     </View>
   );
 }
