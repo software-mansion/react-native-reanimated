30e29e63cd14d45bda4309e7924e9d62707d2fca
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-03-03 14:34:17 +0100

    Refactor to single condition

--- src/reanimated2/threads.ts
+++ src/reanimated2/threads.ts
@@ -50,10 +50,8 @@ export const flushImmediates = shouldBeUseWeb()
 export function runOnUI<A extends any[], R>(
   worklet: ComplexWorkletFunction<A, R>
 ): (...args: A) => void {
-  if (__DEV__ && !shouldBeUseWeb()) {
-    if (worklet.__workletHash === undefined) {
-      throw new Error('runOnUI() can only be used on worklets');
-    }
+  if (__DEV__ && !shouldBeUseWeb() && worklet.__workletHash === undefined) {
+    throw new Error('runOnUI() can only be used on worklets');
   }
   return (...args) => {
     if (IS_JEST) {
