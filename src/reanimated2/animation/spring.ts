d6a6326b00b40ccfd5de81c9406c0f19bccbd2a3
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro.local>
2023-03-16 12:44:46 +0100

    Code review

--- src/reanimated2/animation/spring.ts
+++ src/reanimated2/animation/spring.ts
@@ -14,9 +14,9 @@ type SpringConfig = {
   velocity?: number;
 } & /** When duration is provided damping is set to some calculated value unless damping value is specified in config. If damping is specified mass is calculated. If both mass and damping are provided typescript error is thrown.
 Also initial velocity must be zero if duration is specified. */ (
-  | { velocity?: never; mass?: never; damping?: number; duration?: number }
-  | { velocity?: never; mass?: number; damping?: never; duration?: number }
-  | { velocity?: number; mass?: number; damping?: number; duration?: never }
+  | { mass?: never; damping?: number; duration?: number }
+  | { mass?: number; damping?: never; duration?: number }
+  | { mass?: number; damping?: number; duration?: never }
 );
 
 export interface SpringAnimation extends Animation<SpringAnimation> {
@@ -60,7 +60,7 @@ export function withSpring(
       config.velocity = 0;
     }
 
-    function criticallyDampedSpring(
+    function criticallyDampedSpringCalculations(
       animation: InnerSpringAnimation,
       precalculatedValues: {
         v0: number;
@@ -87,7 +87,7 @@ export function withSpring(
       };
     }
 
-    function underDampedSpring(
+    function underDampedSpringCalculations(
       animation: InnerSpringAnimation,
       precalculatedValues: {
         zeta: number;
@@ -141,14 +141,8 @@ export function withSpring(
       const deltaTime = Math.min(now - lastTimestamp, 64);
       animation.lastTimestamp = now;
 
-      const c =
-        config.duration && !userConfig?.damping
-          ? animation.newDamping
-          : config.damping;
-      const m =
-        config.duration && userConfig?.damping
-          ? animation.newMass
-          : config.mass;
+      const c = config.damping;
+      const m = config.mass;
       const k = config.stiffness;
 
       const v0 = -velocity;
@@ -163,7 +157,7 @@ export function withSpring(
       const { position: newPosition, velocity: newVelocity } =
         // always use underDamped motion if duration was provided
         zeta < 1 || userConfig?.duration
-          ? underDampedSpring(animation, {
+          ? underDampedSpringCalculations(animation, {
               zeta,
               v0,
               x0,
@@ -171,7 +165,12 @@ export function withSpring(
               omega1,
               t,
             })
-          : criticallyDampedSpring(animation, { v0, x0, omega0, t });
+          : criticallyDampedSpringCalculations(animation, {
+              v0,
+              x0,
+              omega0,
+              t,
+            });
 
       const isOvershooting = () => {
         if (config.overshootClamping && config.stiffness !== 0) {
@@ -217,32 +216,48 @@ export function withSpring(
 
       if (userConfig?.duration && userConfig?.damping && userConfig?.mass) {
         console.warn(
-          "You've specified to all the parameters: damping, mass and duration. At least one of them must be calculated depending on the two others"
+          "You've specified all the parameters: damping, mass and duration. At least one of them must be calculated depending on the two others"
         );
       }
 
-      if (userConfig?.duration && userConfig?.velocity) {
-        console.warn('Passing both velocity and duration is not allowed');
-      }
-
       if (userConfig?.duration) {
-        const amplitude = Number(animation.toValue) - value;
+        const x0 = Number(animation.toValue) - value;
+        const v0 = config.velocity;
+        const k = config.stiffness;
+        const m = config.mass;
+
+        let amplitude = Math.sqrt((m * v0 * v0 + k * x0 * x0) / k);
 
         if (!userConfig?.damping) {
           // If damping is not provided calculate new damping
 
-          /** Use this formulae https://phys.libretexts.org/Bookshelves/University_Physics/Book%3A_University_Physics_(OpenStax)/Book%3A_University_Physics_I_-_Mechanics_Sound_Oscillations_and_Waves_(OpenStax)/15%3A_Oscillations/15.06%3A_Damped_Oscillations
+          /** Use this formula: https://phys.libretexts.org/Bookshelves/University_Physics/Book%3A_University_Physics_(OpenStax)/Book%3A_University_Physics_I_-_Mechanics_Sound_Oscillations_and_Waves_(OpenStax)/15%3A_Oscillations/15.06%3A_Damped_Oscillations
            * to find the asympotote and esitmate the damping that gives us the expected duration */
           const m = config.mass;
           newDamping =
             -((2 * m) / (0.001 * config.duration)) *
             Math.log(config.restDisplacementThreshold / Math.abs(amplitude));
+
+          config.damping = newDamping;
         } else {
+          /** As you can notice our formula for amplitude contains mass
+           * Still we can calculate mass solving this equation (https://www.wolframalpha.com/input?i=solve%28ln%28t+*+sqrt%28x0*x0+%2B+v0*v0*m%29%29+%3D+%28m*c%29%2F%282000*d%29%2Cm%29)
+           * However the solution contain Lambert W function (https://en.wikipedia.org/wiki/Lambert_W_function)
+           * And this function is difficult to implement.
+           *
+           * Howeve if initial velocity is zero the kinetic energy is zero too and all the calculations get much simpler,
+           */
+          config.velocity = 0;
+          amplitude = x0;
+
           // calculate new mass
           const d = config.damping;
           newMass =
-            -(0.002 * d * config.duration) /
-            Math.log(config.restDisplacementThreshold / Math.abs(amplitude));
+            (-0.002 * (d * config.duration)) /
+            Math.log(
+              (0.01 * config.restDisplacementThreshold) / Math.abs(amplitude)
+            );
+          config.mass = newMass;
         }
       }
 
@@ -250,15 +265,17 @@ export function withSpring(
         animation.velocity =
           previousAnimation.velocity || animation.velocity || 0;
         animation.lastTimestamp = previousAnimation.lastTimestamp || now;
-        animation.firstTimestamp = previousAnimation.firstTimestamp || now;
         animation.newDamping = previousAnimation.newDamping || newDamping;
         animation.newMass = previousAnimation.newMass || newMass;
+        animation.firstTimestamp = now;
       } else {
         animation.lastTimestamp = now;
         animation.firstTimestamp = now;
         animation.newDamping = newDamping;
         animation.newMass = newMass;
       }
+
+      animation.firstTimestamp = now;
     }
 
     return {
