60958342232f4976f5c53fff09777a5bb54658a9
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2023-03-28 13:06:00 +0200

    [Plugin] [0/9] Split plugin into separate files and add bundling option. (#4209)
    ## Summary
    
    More work on plugin refactorization. This is simply splitting the code
    into files without any changes to it and adding `esbuild` bundler to
    generate a single file.
    
    ## Test plan
    
    Run `yarn jest` and build Example app.
    
    ---------
    
    Co-authored-by: Tomek Zawadzki <tomasz.zawadzki@swmansion.com>

---
+++ plugin/src/injectVersion.ts
@@ -0,0 +1,34 @@
+import * as BabelCore from '@babel/core';
+import * as BabelTypes from '@babel/types';
+
+export function injectVersion(
+  path: BabelCore.NodePath<BabelTypes.DirectiveLiteral>
+) {
+  // We want to inject plugin's version only once,
+  // hence we have a Directive Literal line in Reanimated code.
+  // See src/reanimated2/platform-specific/checkPluginVersion.ts
+  // to see the details of this implementation.
+  if (path.node.value !== 'inject Reanimated Babel plugin version') {
+    return;
+  }
+  const injectedName = '_REANIMATED_VERSION_BABEL_PLUGIN';
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const versionString = require('../../package.json').version;
+  const pluginVersionNode = BabelTypes.expressionStatement(
+    BabelTypes.assignmentExpression(
+      '=',
+      BabelTypes.memberExpression(
+        BabelTypes.identifier('global'),
+        BabelTypes.identifier(injectedName)
+      ),
+      BabelTypes.stringLiteral(versionString)
+    )
+  );
+
+  const functionParent = (
+    path.getFunctionParent() as BabelCore.NodePath<BabelTypes.FunctionDeclaration>
+  ).node;
+  // DirectiveLiteral is in property of its function parent 'directives' hence we cannot just replace it.
+  functionParent.body.directives = [];
+  functionParent.body.body.unshift(pluginVersionNode);
+}
