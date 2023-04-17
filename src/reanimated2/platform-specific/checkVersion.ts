736f1857f6a0a60f656f471398fa70721ce795aa
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-04-17 09:00:24 +0200

    remove unnecessary export

--- src/reanimated2/platform-specific/checkVersion.ts
+++ src/reanimated2/platform-specific/checkVersion.ts
@@ -20,7 +20,7 @@ function checkCppVersion() {
   }
 }
 
-export function matchVersion(version1: string, version2: string) {
+function matchVersion(version1: string, version2: string) {
   if (version1.match(/^\d+\.\d+\.\d+$/) && version2.match(/^\d+\.\d+\.\d+$/)) {
     // x.y.z, compare only major and minor, skip patch
     const [major1, minor1] = version1.split('.');
