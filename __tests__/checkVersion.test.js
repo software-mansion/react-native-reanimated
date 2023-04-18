6a331fda4a8f8559f120a1e869f5fd36982a113d
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-04-18 17:31:37 +0200

    test changes

---
+++ __tests__/checkVersion.test.js
@@ -0,0 +1,51 @@
+const {
+  checkCppVersion,
+  matchVersion,
+} = require('../src/reanimated2/platform-specific/checkCppVersion');
+const { version: packageVersion } = require('../package.json');
+
+describe('checkCppVersion', () => {
+  beforeEach(() => {
+    global._REANIMATED_VERSION_CPP = packageVersion;
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    delete global._REANIMATED_VERSION_CPP;
+    console.error.mockRestore();
+  });
+
+  it('checks version successfully', () => {
+    checkCppVersion();
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it('throws error when version is undefined', () => {
+    delete global._REANIMATED_VERSION_CPP;
+    checkCppVersion();
+    expect(console.error).toHaveBeenCalled();
+  });
+});
+
+describe('matchVersion', () => {
+  it('matches versions', () => {
+    expect(matchVersion('1.2.3', '1.2.3')).toBe(true);
+  });
+
+  it('validates patch mismatch', () => {
+    expect(matchVersion('1.2.3', '1.2.4')).toBe(true);
+  });
+
+  it('invalidates minor mismatch', () => {
+    expect(matchVersion('1.2.3', '1.3.3')).toBe(false);
+  });
+
+  it('invalidates major mismatch', () => {
+    expect(matchVersion('1.2.3', '2.2.3')).toBe(false);
+  });
+
+  it('validates versions with post string', () => {
+    expect(matchVersion('1.2.3-post', '1.2.3-post')).toBe(true);
+  });
+});
--- src/reanimated2/platform-specific/checkCppVersion.ts
+++ src/reanimated2/platform-specific/checkCppVersion.ts
@@ -16,7 +16,7 @@ export function checkCppVersion() {
   }
 }
 
-function matchVersion(version1: string, version2: string) {
+export function matchVersion(version1: string, version2: string) {
   if (version1.match(/^\d+\.\d+\.\d+$/) && version2.match(/^\d+\.\d+\.\d+$/)) {
     // x.y.z, compare only major and minor, skip patch
     const [major1, minor1] = version1.split('.');
