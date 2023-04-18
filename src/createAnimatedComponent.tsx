5abbeed05562f7de0cd26425f1c250ae6ad5a710
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-04-18 17:50:53 +0200

    Update createAnimatedComponent warning

--- src/createAnimatedComponent.tsx
+++ src/createAnimatedComponent.tsx
@@ -246,8 +246,7 @@ export default function createAnimatedComponent(
   invariant(
     typeof Component !== 'function' ||
       (Component.prototype && Component.prototype.isReactComponent),
-    '`createAnimatedComponent` does not support stateless functional components; ' +
-      'use a class component instead.'
+    `Looks like you're passing a ${Component.name} functional component to 'createAnimatedComponent'.\n\n'createAnimatedComponent' relies on passing refs to work correctly. Wrap your functional component with React.forwardRef() or use a class component instead.`
   );
 
   class AnimatedComponent extends React.Component<
