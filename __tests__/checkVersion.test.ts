3c95c2d286d13d3214c31bb6211f4371658da47c
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-05-15 11:21:11 +0200

    Replace console.error with throw new Error (#4452)
    ## Summary
    
    This PR changes the behavior of the app when version mismatch is
    detected. Previously, we called `console.error` in such case, but the
    app still continued to execute. This PR replaces `console.error` with
    `throw new Error` which stops the execution and shows full-screen
    LogBox.
    
    | Before | After |
    |:-:|:-:|
    | <img width="459" alt="before"
    src="https://github.com/software-mansion/react-native-reanimated/assets/20516055/4b58c8f8-350a-4d17-8c3a-51a9a3cf1fc9">
    | <img width="459" alt="after"
    src="https://github.com/software-mansion/react-native-reanimated/assets/20516055/53cd425f-4da1-4ff4-86c8-776fdcbfe4a0">
    |

--- __tests__/checkVersion.test.ts
+++ __tests__/checkVersion.test.ts
@@ -5,30 +5,21 @@ import {
 const { version: packageVersion } = require('../package.json');
 
 describe('checkCppVersion', () => {
-  let spyOnConsoleError: jest.SpyInstance;
-
   beforeEach(() => {
     global._REANIMATED_VERSION_CPP = packageVersion;
-    spyOnConsoleError = jest
-      .spyOn(console, 'error')
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      .mockImplementation(() => {});
   });
 
   afterEach(() => {
     delete global._REANIMATED_VERSION_CPP;
-    spyOnConsoleError.mockRestore();
   });
 
   it('checks version successfully', () => {
-    checkCppVersion();
-    expect(console.error).not.toHaveBeenCalled();
+    expect(checkCppVersion).not.toThrow();
   });
 
   it('throws error when version is undefined', () => {
     delete global._REANIMATED_VERSION_CPP;
-    checkCppVersion();
-    expect(console.error).toHaveBeenCalled();
+    expect(checkCppVersion).toThrow();
   });
 });
 
