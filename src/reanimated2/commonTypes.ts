0d612a852399120f282c7dad87e7f364e59ce865
Jakub Piasecki <jakubpiasecki67@gmail.com>
2023-05-22 09:03:09 +0200

    Update `AnimatedStyle` and `useAnimatedStyle` types (#3773)
    ## Description
    
    This PR is part of an ongoing effort to get rid of the `.d.ts` file and
    switch to types generated based on the source code.
    
    ## Changes
    
    - Updated `AnimatedStyle` to allow primitives for certain props
    - Updated `useAnimatedStyle` to not accept functions returning arrays
    
    ## Checklist
    
    - [ ] Included code example that can be used to test this change
    - [ ] Updated TS types
    - [ ] Added TS types tests
    - [ ] Added unit / integration tests
    - [ ] Updated documentation
    - [ ] Ensured that CI passes

--- src/reanimated2/commonTypes.ts
+++ src/reanimated2/commonTypes.ts
@@ -37,11 +37,31 @@ export interface StyleProps extends ViewStyle, TextStyle {
   [key: string]: any;
 }
 
-export interface AnimatedStyle
-  extends Record<string, Animation<AnimationObject>> {
+export interface AnimatedStyle extends Record<string, AnimationObject> {
   [key: string]: any;
-  transform?: Array<Record<string, Animation<AnimationObject>>>;
+  transform?: Array<
+    | Record<'matrix', number[] | AnimationObject>
+    | Partial<
+        Record<
+          | 'perspective'
+          | 'scale'
+          | 'scaleX'
+          | 'scaleY'
+          | 'translateX'
+          | 'translateY',
+          number | AnimationObject
+        >
+      >
+    | Partial<
+        Record<
+          'rotate' | 'rotateX' | 'rotateY' | 'rotateZ' | 'skewX' | 'skewY',
+          string | AnimationObject
+        >
+      >
+    | Record<string, AnimationObject>
+  >;
 }
+
 export interface SharedValue<T> {
   value: T;
   addListener: (listenerID: number, listener: (value: T) => void) => void;
