c0e26e805c5e87fb1f7d4b1e9dc1400399a31b8d
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-06-02 19:16:49 +0200

    Remove unnecessary import

--- ios/native/UIResponder+Reanimated.mm
+++ ios/native/UIResponder+Reanimated.mm
@@ -1,7 +1,6 @@
 #if REACT_NATIVE_MINOR_VERSION <= 71 && !defined(RCT_NEW_ARCH_ENABLED) && !defined(DONT_AUTOINSTALL_REANIMATED)
 
 #import <RNReanimated/REAInitializer.h>
-#import <RNReanimated/REAUIManager.h>
 #import <RNReanimated/UIResponder+Reanimated.h>
 
 #if __has_include(<reacthermes/HermesExecutorFactory.h>)
