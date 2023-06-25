4c64e92da19f0f4ba37ce361b47af1d6e747746c
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-06-25 19:09:55 +0200

    Update shareables.ts

--- src/reanimated2/shareables.ts
+++ src/reanimated2/shareables.ts
@@ -213,9 +213,7 @@ export function makeShareableCloneOnUIRecursive<T>(value: T): ShareableRef<T> {
   function cloneRecursive<T>(value: T): ShareableRef<T> {
     const type = typeof value;
     if ((type === 'object' || type === 'function') && value !== null) {
-      // @ts-ignore trust me
       if (value.__remoteFunction) {
-        // @ts-ignore trust me
         return value.__remoteFunction;
       }
       let toAdapt: any;
