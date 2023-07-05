361d1b93954f2e917bba3906a4e9150b9c9ca26e
Aleksandra Cynk <aleksandracynk@swmansion.com>
2023-07-05 11:22:17 +0200

    Fix eslint in tests

--- __tests__/.eslintrc.js
+++ __tests__/.eslintrc.js
@@ -6,4 +6,23 @@ module.exports = {
     'prettier',
     'plugin:import/typescript',
   ],
+  plugins: ['react', 'react-native', 'import', 'jest', '@typescript-eslint'],
+  env: {
+    'react-native/react-native': true,
+    'jest/globals': true,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 };
