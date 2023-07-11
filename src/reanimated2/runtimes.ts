53f7465cbafcb7660ea4358d7fe950c3692cb9fd
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-07-11 18:25:46 +0200

    Setup console in Reanimated

--- src/reanimated2/runtimes.ts
+++ src/reanimated2/runtimes.ts
@@ -1,28 +1,61 @@
+import { isChromeDebugger } from './PlatformChecker';
 import type { ComplexWorkletFunction } from './commonTypes';
+import { reportFatalErrorOnJS } from './errors';
 import { valueUnpacker } from './initializers';
 import { makeShareableCloneRecursive } from './shareables';
+import { runOnJS } from './threads';
 
 export type WorkletRuntime = {
   __hostObjectWorkletRuntime: true;
 };
+
+const IS_CHROME_DEBUGGER = isChromeDebugger();
 
 export function createWorkletRuntime(name: string) {
   // @ts-ignore valueUnpacker is a worklet
   const valueUnpackerCode = valueUnpacker.__initData.code;
   const runtime = global._createWorkletRuntime(name, valueUnpackerCode);
 
-  // we need to use different names because `_scheduleOnJS` is whitelisted
+  // we need to use different names because `_scheduleOnJS` and `_makeShareableClone` are whitelisted
   const {
     _makeShareableClone: makeShareableClone,
     _scheduleOnJS: scheduleOnJS,
   } = global;
+
+  // TODO: unify with initializers.ts
+  const capturableConsole = { ...console };
+
   runOnRuntimeSync(runtime, () => {
     'worklet';
+
     global._scheduleOnJS = scheduleOnJS;
     global._makeShareableClone = makeShareableClone;
-  });
 
-  // TODO: call user-defined initializer worklet
+    // setup error handler
+    global.__ErrorUtils = {
+      reportFatalError: (error: Error) => {
+        runOnJS(reportFatalErrorOnJS)({
+          message: error.message,
+          stack: error.stack,
+        });
+      },
+    };
+
+    if (!IS_CHROME_DEBUGGER) {
+      // setup console
+      // @ts-ignore TypeScript doesn't like that there are missing methods in console object, but we don't provide all the methods for the UI runtime console version
+      global.console = {
+        assert: runOnJS(capturableConsole.assert),
+        debug: runOnJS(capturableConsole.debug),
+        log: runOnJS(capturableConsole.log),
+        warn: runOnJS(capturableConsole.warn),
+        error: runOnJS(capturableConsole.error),
+        info: runOnJS(capturableConsole.info),
+      };
+    }
+
+    // TODO: call user-defined initializer worklet
+  });
 
   return runtime;
 }
