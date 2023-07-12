2cffa17060a49fb76f874bc7f71ab70965f69122
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-07-12 14:58:16 +0200

    init

--- plugin/.eslintrc.js
+++ plugin/.eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   root: true,
   extends: '../.eslintrc.js',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   rules: {
     'curly': 'error',
   },
   ignorePatterns: ['**/*.d.ts','jestUtils.ts']};
+  
