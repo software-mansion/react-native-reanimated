6b066fa6902bc4420d691dca46c93f618a57eb8b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-07-17 16:24:18 +0200

    Fix modal SET progress animation on Android

--- src/reanimated2/layoutReanimation/sharedTransitions/ProgressTransitionManager.ts
+++ src/reanimated2/layoutReanimation/sharedTransitions/ProgressTransitionManager.ts
@@ -72,8 +72,19 @@ export class ProgressTransitionManager {
           global.ProgressTransitionRegister.onTransitionEnd();
         }
       );
-      if (Platform.OS === 'ios') {
-        // required by modals
+
+      if (Platform.OS === 'android') {
+        // onFinishTransitioning event is available only on Android and
+        // is used to handle closing modals
+        eventHandler.onDisappear = registerEventHandler(
+          'onFinishTransitioning',
+          () => {
+            'worklet';
+            global.ProgressTransitionRegister.onAndroidFinishTransitioning();
+          }
+        );
+      } else if (Platform.OS === 'ios') {
+        // topDisappear event is required to handle closing modals on iOS
         eventHandler.onDisappear = registerEventHandler('topDisappear', () => {
           'worklet';
           global.ProgressTransitionRegister.onTransitionEnd(true);
@@ -148,6 +159,13 @@ function createProgressTransitionRegister() {
         const snapshot = snapshots.get(viewTag);
         progressAnimation!(viewTag, snapshot, progress);
       }
+    },
+    onAndroidFinishTransitioning: () => {
+      if (toRemove.size === 0) {
+        // it should be ran only on modal closing
+        return;
+      }
+      progressTransitionManager.onTransitionEnd();
     },
     onTransitionEnd: (removeViews = false) => {
       for (const viewTag of currentTransitions) {
