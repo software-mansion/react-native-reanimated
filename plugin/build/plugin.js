27a40c59b7f34f36d9fa22e4a6ad88819b10f5ee
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-07-18 09:54:22 +0200

    Improve detecting production bundle in Babel plugin

--- plugin/build/plugin.js
+++ plugin/build/plugin.js
@@ -104,7 +104,8 @@ var require_utils = __commonJS({
     Object.defineProperty(exports2, "__esModule", { value: true });
     exports2.isRelease = void 0;
     function isRelease() {
-      return !!process.env.BABEL_ENV && ["production", "release"].includes(process.env.BABEL_ENV);
+      var _a;
+      return !!process.env.BABEL_ENV && ["prod", "release", "stag"].some((_a = process.env.BABEL_ENV) === null || _a === void 0 ? void 0 : _a.toLowerCase().includes);
     }
     exports2.isRelease = isRelease;
   }
--- plugin/src/utils.ts
+++ plugin/src/utils.ts
@@ -1,6 +1,8 @@
 export function isRelease() {
   return (
     !!process.env.BABEL_ENV &&
-    ['production', 'release'].includes(process.env.BABEL_ENV)
+    ['prod', 'release', 'stag'].some(
+      process.env.BABEL_ENV?.toLowerCase().includes
+    )
   );
 }
