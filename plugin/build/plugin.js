226a1a3da351a016ca915e8f7585c43bfdf16758
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-07-18 10:25:56 +0200

    Improve detecting production bundle in Babel plugin (#4747)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please follow the template so that
    the reviewers can easily understand what the code changes affect. -->
    
    ## Summary
    
    This PR changes the way how Reanimated Babel plugin detects if dev or
    prod bundle should generated. Previously, we would only support
    `BABEL_ENV=production` and `BABEL_ENV=release`; all other options would
    fallback to dev bundle. Thanks to this change we will also support other
    configurations e.g. `productionRelease` or `staging`. See #4690 for
    details.
    
    ## Test plan
    
    <!-- Provide a minimal but complete code snippet that can be used to
    test out this change along with instructions how to run it and a
    description of the expected behavior. -->

--- plugin/build/plugin.js
+++ plugin/build/plugin.js
@@ -104,7 +104,8 @@ var require_utils = __commonJS({
     Object.defineProperty(exports2, "__esModule", { value: true });
     exports2.isRelease = void 0;
     function isRelease() {
-      return !!process.env.BABEL_ENV && ["production", "release"].includes(process.env.BABEL_ENV);
+      var _a;
+      return !!((_a = process.env.BABEL_ENV) === null || _a === void 0 ? void 0 : _a.match(/(prod|release|stag[ei])/i));
     }
     exports2.isRelease = isRelease;
   }
