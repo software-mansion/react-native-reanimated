e6251f3a5063c9dc4411363ce44a864d23e8dcaf
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-07-20 11:15:52 +0200

    exportele

--- src/reanimated2/NativeReanimated/index.ts
+++ src/reanimated2/NativeReanimated/index.ts
@@ -2,8 +2,6 @@ import reanimatedJS from '../js-reanimated';
 import { shouldBeUseWeb } from '../PlatformChecker';
 import { NativeReanimated } from './NativeReanimated';
 
-const NativeReanimatedModule = (
-  shouldBeUseWeb() ? reanimatedJS : new NativeReanimated()
-) as NativeReanimated;
-
-export default NativeReanimatedModule;
+export default (shouldBeUseWeb()
+  ? reanimatedJS
+  : new NativeReanimated()) as NativeReanimated;
