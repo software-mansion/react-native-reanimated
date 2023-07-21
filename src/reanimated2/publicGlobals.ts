f231dab999bd2196617ae9cc31acd90e3fe8c0f1
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-07-21 13:09:01 +0200

    use TSDoc

--- src/reanimated2/publicGlobals.ts
+++ src/reanimated2/publicGlobals.ts
@@ -3,15 +3,17 @@ export {};
 
 declare global {
   /** This global variable is a diagnostic/development tool.
-  It used to be necessary in the past for some of the
-  functionalities of react-native-reanimated to work
-  properly but it's no longer the case. Your code
-  shouldn't depend on it, we keep it here
-  mainly for backward compatibility reasons for our users. */
+   * It used to be necessary in the past for some of the
+   * functionalities of react-native-reanimated to work
+   * properly but it's no longer the case. Your code
+   * shouldn't depend on it, we keep it here
+   * mainly for backward compatibility reasons for our users.
+   */
   var _WORKLET: boolean | undefined;
 
   /** This is simply an address of UI Runtime. Its type stems from
-  the need to be able to pass it from the native side to JS
-  when it's a 64-bit address value. */
+   * the need to be able to pass it from the native side to JS
+   * when it's a 64-bit address value.
+   */
   var _WORKLET_RUNTIME: ArrayBuffer;
 }
