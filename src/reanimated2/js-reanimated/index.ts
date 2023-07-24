software-mansion/react-native-reanimated

commit 6652c790b46baa486ec068b10f9821af68786cd6

Properly type `__reanimatedModuleProxy` (and other stuff) (#4780)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

The motivation behind this PR is that `__reanimatedModuleProxy` (which
is injected via JSI) has the same type as `NativeReanimated` class which
is a JS-only adapter that wraps JSI binding. While this allows us to
save a few lines of code, it breaks the mental model of adapters having
a potentially different API than wrapped instances.

This PR adds an explicit type `NativeReanimatedModule` type which is
used for `__reanimatedModuleProxy`. Apart from this, I've decoupled
`JSReanimated` from `NativeReanimated` by eliminating inheritance as
well as refactored the constructor of `NativeReanimated` to simplify the
logic behind it.

Finally, I've removed `updateDataSynchronously` method which not only
was unused but also didn't work at all (it's used only from the UI
runtime as `_updateDataSynchronously`).

## Test plan

<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->

src/reanimated2/js-reanimated/index.ts
@@ -24,7 +24,12 @@ if (isWeb()) {
 
 const reanimatedJS = new JSReanimated();
 
-global._makeShareableClone = (c) => c;
+global._makeShareableClone = () => {
+  throw new Error(
+    '[Reanimated] _makeShareableClone should never be called in JSReanimated.'
+  );
+};
+
 global._scheduleOnJS = (func, args) => {
   if (args) {
     queueMicrotask(() => func(...args));

src/reanimated2/NativeReanimated/index.ts
@@ -2,6 +2,4 @@ import reanimatedJS from '../js-reanimated';
 import { shouldBeUseWeb } from '../PlatformChecker';
 import { NativeReanimated } from './NativeReanimated';
 
-export default (shouldBeUseWeb()
-  ? reanimatedJS
-  : new NativeReanimated()) as NativeReanimated;
+export default shouldBeUseWeb() ? reanimatedJS : new NativeReanimated();

src/reanimated2/NativeReanimated/NativeReanimated.ts
@@ -11,79 +11,114 @@ import type {
 } from '../layoutReanimation';
 import { checkCppVersion } from '../platform-specific/checkCppVersion';
 
-export class NativeReanimated {
-  native: boolean;
-  private InnerNativeModule: any;
-
-  constructor(native = true) {
-    if (global.__reanimatedModuleProxy === undefined && native) {
-      const { ReanimatedModule } = NativeModules;
-      ReanimatedModule?.installTurboModule();
-    }
-    this.InnerNativeModule = global.__reanimatedModuleProxy;
-    this.native = native;
-    if (native) {
-      if (this.InnerNativeModule === undefined) {
-        throw new Error(
-          `[Reanimated] The native part of Reanimated doesn't seem to be initialized. This could be caused by\n\
-  - not rebuilding the app after installing or upgrading Reanimated\n\
-  - trying to run Reanimated on an unsupported platform\n\
-  - running in a brownfield app without manually initializing the native library`
-        );
-      }
-      checkCppVersion();
-    }
-  }
-
+// this is the type of `__reanimatedModuleProxy` which is injected using JSI
+export interface NativeReanimatedModule {
   installCoreFunctions(
-    callGuard: <T extends Array<any>, U>(
+    callGuard: <T extends Array<unknown>, U>(
       fn: (...args: T) => U,
       ...args: T
     ) => void,
     valueUnpacker: <T>(value: T) => T
-  ): void {
+  ): void;
+  makeShareableClone<T>(
+    value: T,
+    shouldPersistRemote: boolean
+  ): ShareableRef<T>;
+  makeSynchronizedDataHolder<T>(
+    valueRef: ShareableRef<T>
+  ): ShareableSyncDataHolderRef<T>;
+  getDataSynchronously<T>(ref: ShareableSyncDataHolderRef<T>): T;
+  scheduleOnUI<T>(shareable: ShareableRef<T>): void;
+  registerEventHandler<T>(
+    eventHash: string,
+    eventHandler: ShareableRef<T>
+  ): number;
+  unregisterEventHandler(id: number): void;
+  getViewProp<T>(
+    viewTag: string,
+    propName: string,
+    callback?: (result: T) => void
+  ): Promise<T>;
+  enableLayoutAnimations(flag: boolean): void;
+  registerSensor(
+    sensorType: number,
+    interval: number,
+    iosReferenceFrame: number,
+    handler: ShareableRef<(data: Value3D | ValueRotation) => void>
+  ): number;
+  unregisterSensor(sensorId: number): void;
+  configureProps(uiProps: string[], nativeProps: string[]): void;
+  subscribeForKeyboardEvents(
+    handler: ShareableRef<number>,
+    isStatusBarTranslucent: boolean
+  ): number;
+  unsubscribeFromKeyboardEvents(listenerId: number): void;
+  configureLayoutAnimation(
+    viewTag: number,
+    type: LayoutAnimationType,
+    sharedTransitionTag: string,
+    config: ShareableRef<Keyframe | LayoutAnimationFunction>
+  ): void;
+}
+
+export class NativeReanimated {
+  native = true;
+  private InnerNativeModule: NativeReanimatedModule;
+
+  constructor() {
+    if (global.__reanimatedModuleProxy === undefined) {
+      const { ReanimatedModule } = NativeModules;
+      ReanimatedModule?.installTurboModule();
+    }
+    if (global.__reanimatedModuleProxy === undefined) {
+      throw new Error(
+        `[Reanimated] The native part of Reanimated doesn't seem to be initialized. This could be caused by\n\
+- not rebuilding the app after installing or upgrading Reanimated\n\
+- trying to run Reanimated on an unsupported platform\n\
+- running in a brownfield app without manually initializing the native library`
+      );
+    }
+    checkCppVersion();
+    this.InnerNativeModule = global.__reanimatedModuleProxy;
+  }
+
+  installCoreFunctions(
+    callGuard: <T extends Array<unknown>, U>(
+      fn: (...args: T) => U,
+      ...args: T
+    ) => void,
+    valueUnpacker: <T>(value: T) => T
+  ) {
     return this.InnerNativeModule.installCoreFunctions(
       callGuard,
       valueUnpacker
     );
   }
 
-  makeShareableClone<T>(
-    value: T,
-    shouldPersistRemote: boolean
-  ): ShareableRef<T> {
+  makeShareableClone<T>(value: T, shouldPersistRemote: boolean) {
     return this.InnerNativeModule.makeShareableClone(
       value,
       shouldPersistRemote
     );
   }
 
-  makeSynchronizedDataHolder<T>(
-    valueRef: ShareableRef<T>
-  ): ShareableSyncDataHolderRef<T> {
+  makeSynchronizedDataHolder<T>(valueRef: ShareableRef<T>) {
     return this.InnerNativeModule.makeSynchronizedDataHolder(valueRef);
   }
 
-  getDataSynchronously<T>(ref: ShareableSyncDataHolderRef<T>): T {
+  getDataSynchronously<T>(ref: ShareableSyncDataHolderRef<T>) {
     return this.InnerNativeModule.getDataSynchronously(ref);
-  }
-
-  updateDataSynchronously<T>(
-    ref: ShareableSyncDataHolderRef<T>,
-    value: T
-  ): void {
-    this.InnerNativeModule.updateDataSynchronously(ref, value);
   }
 
   scheduleOnUI<T>(shareable: ShareableRef<T>) {
     return this.InnerNativeModule.scheduleOnUI(shareable);
   }
 
-  registerSensor<T>(
+  registerSensor(
     sensorType: number,
     interval: number,
     iosReferenceFrame: number,
-    handler: ShareableRef<T> | ((data: Value3D | ValueRotation) => void)
+    handler: ShareableRef<(data: Value3D | ValueRotation) => void>
   ) {
     return this.InnerNativeModule.registerSensor(
       sensorType,
@@ -97,14 +132,11 @@ export class NativeReanimated {
     return this.InnerNativeModule.unregisterSensor(sensorId);
   }
 
-  registerEventHandler<T>(
-    eventHash: string,
-    eventHandler: ShareableRef<T>
-  ): number {
+  registerEventHandler<T>(eventHash: string, eventHandler: ShareableRef<T>) {
     return this.InnerNativeModule.registerEventHandler(eventHash, eventHandler);
   }
 
-  unregisterEventHandler(id: number): void {
+  unregisterEventHandler(id: number) {
     return this.InnerNativeModule.unregisterEventHandler(id);
   }
 
@@ -112,7 +144,7 @@ export class NativeReanimated {
     viewTag: string,
     propName: string,
     callback?: (result: T) => void
-  ): Promise<T> {
+  ) {
     return this.InnerNativeModule.getViewProp(viewTag, propName, callback);
   }
 
@@ -130,25 +162,25 @@ export class NativeReanimated {
     );
   }
 
-  enableLayoutAnimations(flag: boolean): void {
+  enableLayoutAnimations(flag: boolean) {
     this.InnerNativeModule.enableLayoutAnimations(flag);
   }
 
-  configureProps(uiProps: string[], nativeProps: string[]): void {
+  configureProps(uiProps: string[], nativeProps: string[]) {
     this.InnerNativeModule.configureProps(uiProps, nativeProps);
   }
 
   subscribeForKeyboardEvents(
     handler: ShareableRef<number>,
     isStatusBarTranslucent: boolean
-  ): number {
+  ) {
     return this.InnerNativeModule.subscribeForKeyboardEvents(
       handler,
       isStatusBarTranslucent
     );
   }
 
-  unsubscribeFromKeyboardEvents(listenerId: number): void {
+  unsubscribeFromKeyboardEvents(listenerId: number) {
     this.InnerNativeModule.unsubscribeFromKeyboardEvents(listenerId);
   }
 }

src/reanimated2/Sensor.ts
@@ -33,7 +33,7 @@ function initSensorData(
   }
 }
 
-export default class Sensor<T> {
+export default class Sensor {
   public listenersNumber = 0;
   private sensorId: number | null = null;
   private sensorType: SensorType;
@@ -47,7 +47,7 @@ export default class Sensor<T> {
   }
 
   register(
-    eventHandler: ShareableRef<T> | ((data: Value3D | ValueRotation) => void)
+    eventHandler: ShareableRef<(data: Value3D | ValueRotation) => void>
   ) {
     const config = this.config;
     const sensorType = this.sensorType;

src/reanimated2/SensorContainer.ts
@@ -9,7 +9,7 @@ import type {
 import Sensor from './Sensor';
 
 export class SensorContainer {
-  private nativeSensors: Map<number, Sensor<any>> = new Map();
+  private nativeSensors: Map<number, Sensor> = new Map();
 
   getSensorId(sensorType: SensorType, config: SensorConfig) {
     return (
@@ -34,10 +34,10 @@ export class SensorContainer {
     return sensor!.getSharedValue();
   }
 
-  registerSensor<T>(
+  registerSensor(
     sensorType: SensorType,
     config: SensorConfig,
-    handler: ShareableRef<T> | ((data: Value3D | ValueRotation) => void)
+    handler: ShareableRef<(data: Value3D | ValueRotation) => void>
   ): number {
     const sensorId = this.getSensorId(sensorType, config);
 

src/reanimated2/commonTypes.ts
@@ -69,8 +69,8 @@ export interface SharedValue<T> {
   modify: (modifier: (value: T) => T) => void;
 }
 
-// The below type is used for HostObjects retured by the JSI API that don't have
-// any accessable fields or methods but can carry data that is accessed from the
+// The below type is used for HostObjects returned by the JSI API that don't have
+// any accessible fields or methods but can carry data that is accessed from the
 // c++ side. We add a field to the type to make it possible for typescript to recognize
 // which JSI methods accept those types as arguments and to be able to correctly type
 // check other methods that may use them. However, this field is not actually defined

src/reanimated2/globals.d.ts
@@ -11,7 +11,7 @@ import type {
   ComplexWorkletFunction,
 } from './commonTypes';
 import type { FrameCallbackRegistryUI } from './frameCallback/FrameCallbackRegistryUI';
-import type { NativeReanimated } from './NativeReanimated/NativeReanimated';
+import type { NativeReanimatedModule } from './NativeReanimated/NativeReanimated';
 import type { SensorContainer } from './SensorContainer';
 import type { LayoutAnimationsManager } from './layoutReanimation/animationsManager';
 import type { ProgressTransitionRegister } from './layoutReanimation/sharedTransitions';
@@ -22,7 +22,7 @@ declare global {
   var _IS_FABRIC: boolean | undefined;
   var _REANIMATED_VERSION_CPP: string | undefined;
   var _REANIMATED_VERSION_BABEL_PLUGIN: string | undefined;
-  var __reanimatedModuleProxy: NativeReanimated | undefined;
+  var __reanimatedModuleProxy: NativeReanimatedModule | undefined;
   var evalWithSourceMap:
     | ((js: string, sourceURL: string, sourceMap: string) => any)
     | undefined;
@@ -35,7 +35,7 @@ declare global {
   ) => void;
   var _notifyAboutEnd: (tag: number, removeView: boolean) => void;
   var _setGestureState: (handlerTag: number, newState: number) => void;
-  var _makeShareableClone: (value: any) => any;
+  var _makeShareableClone: <T>(value: T) => ShareableRef<T>;
   var _updateDataSynchronously: (
     dataHolder: ShareableSyncDataHolderRef<any>,
     data: ShareableRef<any>

src/reanimated2/initializers.ts
@@ -9,7 +9,7 @@ import {
 } from './threads';
 
 // callGuard is only used with debug builds
-function callGuardDEV<T extends Array<any>, U>(
+function callGuardDEV<T extends Array<unknown>, U>(
   fn: (...args: T) => U,
   ...args: T
 ): void {

commit 3f8392a8002e9855c1ade78e5f566b07e5103ece

Fix `runOnJS` calling `_makeShareableClone` when already on the JS thread (#4799)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

<!-- Explain the motivation for this PR. Include "Fixes #<number>" if
applicable. -->

This PR addresses a regression introduced by me in #4780.

<img width="300" alt="bad error"
src="https://github.com/software-mansion/react-native-reanimated/assets/20516055/3dcd1aa6-5e13-4082-9449-367355e752e3">

After long consideration, it occurred to me that both
`_makeShareableClone` and `_scheduleOnJS` were originally meant to
represent JSI bindings (i.e. JS functions implemented in C++ and
injected via JSI), thus none of them should ever be used when in
JSReanimated mode.

This PR moves the implementation of `_scheduleOnJS` to the only place
where it's called which enables us to generalize it to cover more cases,
in particular calling `runOnJS` on the main React Native runtime.

Kudos for @tjzel for investigating the issue and submitting a hotfix
(#4790).

## Test plan

<details>
<summary>App.tsx</summary>

```tsx
import {StyleSheet, View, Button} from 'react-native';

import React from 'react';
import {runOnJS} from 'react-native-reanimated';

export default function App() {
  const handlePress = () => {
    runOnJS(() => {
      'worklet';
      console.log(_WORKLET, 'Hello from the JS runtime!');
    })();
  };

  return (
    <View style={styles.container}>
      <Button onPress={handlePress} title="Click me" />
    </View>
  );
}

const styles = StyleSheet.create({
  container: {
    flex: 1,
    alignItems: 'center',
    justifyContent: 'center',
  },
});
```

</details>

src/reanimated2/js-reanimated/index.ts
@@ -30,12 +30,10 @@ global._makeShareableClone = () => {
   );
 };
 
-global._scheduleOnJS = (func, args) => {
-  if (args) {
-    queueMicrotask(() => func(...args));
-  } else {
-    queueMicrotask(func);
-  }
+global._scheduleOnJS = () => {
+  throw new Error(
+    '[Reanimated] _scheduleOnJS should never be called in JSReanimated.'
+  );
 };
 
 interface JSReanimatedComponent {

src/reanimated2/threads.ts
@@ -162,6 +162,10 @@ export function runOnJS<A extends any[], R>(
   fun: ComplexWorkletFunction<A, R>
 ): (...args: A) => void {
   'worklet';
+  if (!IS_NATIVE || !_WORKLET) {
+    // if we are already on the JS thread, we just schedule the worklet on the JS queue
+    return (...args) => queueMicrotask(args.length ? () => fun(...args) : fun);
+  }
   if (fun.__workletHash) {
     // if `fun` is a worklet, we schedule a call of a remote function `runWorkletOnJS`
     // and pass the worklet as a first argument followed by original arguments