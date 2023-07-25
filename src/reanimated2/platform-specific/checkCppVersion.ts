cd48d150c371cf7e785edbc1c3c13b5dced1e8a2
Aleksandra Cynk <aleksandracynk@swmansion.com>
2023-07-25 10:35:57 +0200

    -||-

--- src/reanimated2/platform-specific/checkCppVersion.ts
+++ src/reanimated2/platform-specific/checkCppVersion.ts
@@ -15,7 +15,8 @@ export function checkCppVersion() {
   }
 }
 
-function matchVersion(version1: string, version2: string) {
+// ts-prune-ignore-next
+export function matchVersion(version1: string, version2: string) {
   if (version1.match(/^\d+\.\d+\.\d+$/) && version2.match(/^\d+\.\d+\.\d+$/)) {
     // x.y.z, compare only major and minor, skip patch
     const [major1, minor1] = version1.split('.');
