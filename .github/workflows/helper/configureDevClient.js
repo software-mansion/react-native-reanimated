6243719de7da4ae53f0a48ac409fb0e7aeb89365
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-07-26 17:46:13 +0200

    Update CI script

---
+++ .github/workflows/helper/configureDevClient.js
@@ -0,0 +1,35 @@
+fs = require('fs');
+
+function patchFile(path, find, replace) {
+  let data = fs.readFileSync(path, 'utf8');
+  data = data.replace(find, replace);
+  fs.writeFileSync(path, data);
+}
+
+const command = process.argv[2];
+
+if (command === 'setBundleIdentifier') {
+  patchFile(
+    'app.json',
+    '"ios": {',
+    '"ios": {"bundleIdentifier":"com.swmansion.app",'
+  );
+
+  patchFile(
+    'app.json',
+    '"android": {',
+    '"android": {"package": "com.swmansion.app",'
+  );
+}
+
+if (command === 'setupFabricIOS') {
+  patchFile('ios/Podfile.properties.json', '{', '{"newArchEnabled":"true",');
+}
+
+if (command === 'setupFabricAndroid') {
+  patchFile(
+    'android/gradle.properties',
+    'newArchEnabled=false',
+    'newArchEnabled=true'
+  );
+}
