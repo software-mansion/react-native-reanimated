40c12e45e9ce9c605d1777e055ff4df48deb5914
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-07-31 18:19:40 +0200

    remove prune

--- src/reanimated2/helperTypes.ts
+++ src/reanimated2/helperTypes.ts
@@ -134,7 +134,6 @@ export type AnimateProps<P extends object> = NonStyleAnimatedProps<P> &
     animatedProps?: Partial<AnimatedPropsProp<P>>;
   };
 
-// ts-prune-ignore-next This will be used soon
 export type AnimatedProps<P extends object> = AnimateProps<P>;
 
 export type AnimatedPropsAdapterFunction = (
