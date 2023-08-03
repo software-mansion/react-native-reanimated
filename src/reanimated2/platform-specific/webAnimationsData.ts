fb1e8e5264fd2e3e7893513839d340ecabda00cc
Michał Bert <michal.bert@swmansion.com>
2023-08-03 11:11:46 +0200

    Remove back easing

--- src/reanimated2/platform-specific/webAnimationsData.ts
+++ src/reanimated2/platform-specific/webAnimationsData.ts
@@ -1288,5 +1288,4 @@ export const WebEasings: Record<string, number[]> = {
   sin: [0.12, 0, 0.39, 0],
   circle: [0.55, 0, 1, 0.45],
   exp: [0.7, 0, 0.84, 0],
-  back: [0.44, -0.03, 0.79, -1], // Kacper <3
 };
