1658f60dc09ae18545160c3c38d4593772c531bd
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-08-08 16:06:56 +0200

    Remove comments

--- ios/native/REAIOSUIScheduler.mm
+++ ios/native/REAIOSUIScheduler.mm
@@ -7,8 +7,6 @@ using namespace react;
 
 void REAIOSUIScheduler::scheduleOnUI(std::function<void()> job)
 {
-  // TODO: check if runtime is alive
-
   if ([NSThread isMainThread]) {
     job();
     return;
@@ -18,8 +16,6 @@ void REAIOSUIScheduler::scheduleOnUI(std::function<void()> job)
 
   if (!scheduledOnUI_) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      // TODO: check if runtime is alive
-
       triggerUI();
     });
   }
