1781267cd66f5b882812d791500fa37fe6814de1
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-08-09 17:49:49 +0200

    Update src/reanimated2/shareables.ts

--- src/reanimated2/shareables.ts
+++ src/reanimated2/shareables.ts
@@ -21,7 +21,6 @@ const _shareableFlag = Symbol('shareable flag');
 const MAGIC_KEY = 'REANIMATED_MAGIC_KEY';
 
 function isHostObject(value: NonNullable<object>) {
-  'worklet';
   // We could use JSI to determine whether an object is a host object, however
   // the below workaround works well and is way faster than an additional JSI call.
   // We use the fact that host objects have broken implementation of `hasOwnProperty`
