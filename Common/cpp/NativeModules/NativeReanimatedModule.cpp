48f5ca46c7725bae938d2d24c943b65517ba4382
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-08-23 10:22:28 +0200

    Remove const

--- Common/cpp/NativeModules/NativeReanimatedModule.cpp
+++ Common/cpp/NativeModules/NativeReanimatedModule.cpp
@@ -530,7 +530,7 @@ bool NativeReanimatedModule::handleRawEvent(
   }
 
   int tag = eventTarget->getTag();
-  const auto &eventType = rawEvent.type;
+  auto eventType = rawEvent.type;
   if (eventType.rfind("top", 0) == 0) {
     eventType = "on" + eventType.substr(3);
   }
