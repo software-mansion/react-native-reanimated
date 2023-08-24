0e5b62bfa5aeb8bed9a75f6aff5fedcf73f1a864
Jakub Fedak <jakubfedak1030@gmail.com>
2023-08-24 16:41:19 +0200

    Delete App.macos file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import {
   FlatList,
   Linking,
   Platform,
+  Pressable,
   ScrollView,
   StyleSheet,
   Text,
@@ -28,6 +29,10 @@ import {
   PathConfigMap,
   useNavigation,
 } from '@react-navigation/native';
+import {
+  StackNavigationProp,
+  createStackNavigator,
+} from '@react-navigation/stack';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { EXAMPLES } from './examples';
@@ -39,6 +44,10 @@ type RootStackParamList = { [P in keyof typeof EXAMPLES]: undefined } & {
 };
 
 interface HomeScreenProps {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+}
+
+interface HomeScreenNativeProps {
   navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
 }
 
@@ -55,7 +64,7 @@ function findExamples(search: string) {
   );
 }
 
-function HomeScreen({ navigation }: HomeScreenProps) {
+function HomeScreen({ navigation }: HomeScreenProps | HomeScreenNativeProps) {
   const [search, setSearch] = React.useState('');
 
   React.useLayoutEffect(() => {
@@ -100,6 +109,15 @@ interface ItemProps {
 }
 
 function Item({ icon, title, onPress }: ItemProps) {
+  if (Platform.OS === 'macos') {
+    return (
+      <Pressable style={styles.button} onPress={onPress}>
+        {icon && <Text style={styles.title}>{icon + '  '}</Text>}
+        <Text style={styles.title}>{title}</Text>
+      </Pressable>
+    );
+  }
+
   return (
     <RectButton style={styles.button} onPress={onPress}>
       {icon && <Text style={styles.title}>{icon + '  '}</Text>}
@@ -112,7 +130,10 @@ function ItemSeparator() {
   return <View style={styles.separator} />;
 }
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack =
+  Platform.OS === 'macos'
+    ? createStackNavigator<RootStackParamList>()
+    : createNativeStackNavigator<RootStackParamList>();
 
 const linking = {
   prefixes: [],
@@ -147,7 +168,11 @@ export default function App() {
       try {
         const initialUrl = await Linking.getInitialURL();
 
-        if (Platform.OS !== 'web' && initialUrl == null) {
+        if (
+          Platform.OS !== 'web' &&
+          Platform.OS !== 'macos' &&
+          initialUrl == null
+        ) {
           // Only restore state if there's no deep link and we're not on web
           const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
           const state = savedStateString
