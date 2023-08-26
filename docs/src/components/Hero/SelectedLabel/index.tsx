d9a1aaf69bbcc9cbb8644f718424e6a287e09b1c
tboba <tymoteusz.boba@gmail.com>
2023-08-26 19:04:25 +0200

    Make 'Reanimated' label draggable

--- docs/src/components/Hero/SelectedLabel/index.tsx
+++ docs/src/components/Hero/SelectedLabel/index.tsx
@@ -1,40 +1,44 @@
 import clsx from 'clsx';
 import React from 'react';
 import styles from './styles.module.css';
+import Draggable from 'react-draggable';
+import { useWindowSize } from '@docusaurus/theme-common';
 
 const SelectedLabel: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <span className={clsx(styles.headingLabel, styles.selection)}>
-      <div className={styles.selectionContainer}>
-        <div
-          className={clsx(
-            styles.selectionBox,
-            styles.boxUpper,
-            styles.boxLeft
-          )}></div>
-        <div
-          className={clsx(
-            styles.selectionBox,
-            styles.boxUpper,
-            styles.boxRight
-          )}></div>
-        <div
-          className={clsx(
-            styles.selectionBox,
-            styles.boxLower,
-            styles.boxLeft
-          )}></div>
-        <div
-          className={clsx(
-            styles.selectionBox,
-            styles.boxLower,
-            styles.boxRight
-          )}></div>
-        {children}
-      </div>
-    </span>
+    <Draggable bounds='[class*="hero"]'>
+      <span className={clsx(styles.headingLabel, styles.selection)}>
+        <div className={styles.selectionContainer}>
+          <div
+            className={clsx(
+              styles.selectionBox,
+              styles.boxUpper,
+              styles.boxLeft
+            )}></div>
+          <div
+            className={clsx(
+              styles.selectionBox,
+              styles.boxUpper,
+              styles.boxRight
+            )}></div>
+          <div
+            className={clsx(
+              styles.selectionBox,
+              styles.boxLower,
+              styles.boxLeft
+            )}></div>
+          <div
+            className={clsx(
+              styles.selectionBox,
+              styles.boxLower,
+              styles.boxRight
+            )}></div>
+          {children}
+        </div>
+      </span>
+    </Draggable>
   );
 };
 
