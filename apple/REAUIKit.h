d2ce3683f77e0cde962546de628dacd9fa0185ac
Jakub Fedak <jakubfedak1030@gmail.com>
2023-09-01 14:37:18 +0200

    Clean code

--- apple/REAUIKit.h
+++ apple/REAUIKit.h
@@ -6,15 +6,7 @@ typedef UIView REAUIView;
 
 #else // TARGET_OS_OSX [
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #import <RNReanimated/RCTUIView+Reanimated.h>
-
-#ifdef __cplusplus
-}
-#endif
 
 typedef RCTUIView REAUIView;
 
