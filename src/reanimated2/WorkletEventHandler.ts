238174ad86aa82507f20b3c61679f13a6ce7e386
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-09-14 09:09:12 +0200

    init

--- src/reanimated2/WorkletEventHandler.ts
+++ src/reanimated2/WorkletEventHandler.ts
@@ -1,25 +1,39 @@
 'use strict';
-import type { __NativeEvent } from './commonTypes';
+import type { NativeSyntheticEvent } from 'react-native';
 import NativeReanimatedModule from './NativeReanimated';
 import { registerEventHandler, unregisterEventHandler } from './core';
+import type { EventPayload, ReanimatedEvent } from './hook/commonTypes';
 
-function jsListener<T extends __NativeEvent<T>>(
+type AssertNativeEvent<Event extends object> = NativeSyntheticEvent<
+  EventPayload<Event>
+>;
+
+// In JS implementation (e.g. for web) we don't use Reanimated's
+// event emitter therefore we have to handle here
+// the event that came from React Native and convert it.
+function jsListener<Event extends object>(
   eventName: string,
-  handler: (event: T) => void
+  handler: (event: ReanimatedEvent<Event>) => void
 ) {
-  return (evt: T) => {
-    handler({ ...evt.nativeEvent, eventName });
+  return (evt: AssertNativeEvent<Event>) => {
+    handler({ ...evt.nativeEvent, eventName } as ReanimatedEvent<Event>);
   };
 }
 
-export default class WorkletEventHandler<T extends __NativeEvent<T>> {
-  worklet: (event: T) => void;
+export default class WorkletEventHandler<Event extends object> {
+  worklet: (event: ReanimatedEvent<Event>) => void;
   eventNames: string[];
   reattachNeeded: boolean;
-  listeners: Record<string, (event: T) => void>;
+  listeners:
+    | Record<string, (event: ReanimatedEvent<ReanimatedEvent<Event>>) => void>
+    | Record<string, (event: AssertNativeEvent<Event>) => void>;
+
   viewTag: number | undefined;
   registrations: number[];
-  constructor(worklet: (event: T) => void, eventNames: string[] = []) {
+  constructor(
+    worklet: (event: ReanimatedEvent<Event>) => void,
+    eventNames: string[] = []
+  ) {
     this.worklet = worklet;
     this.eventNames = eventNames;
     this.reattachNeeded = false;
@@ -29,7 +43,10 @@ export default class WorkletEventHandler<T extends __NativeEvent<T>> {
 
     if (!NativeReanimatedModule.native) {
       this.listeners = eventNames.reduce(
-        (acc: Record<string, (event: T) => void>, eventName: string) => {
+        (
+          acc: Record<string, (event: AssertNativeEvent<Event>) => void>,
+          eventName: string
+        ) => {
           acc[eventName] = jsListener(eventName, worklet);
           return acc;
         },
@@ -38,7 +55,7 @@ export default class WorkletEventHandler<T extends __NativeEvent<T>> {
     }
   }
 
-  updateWorklet(newWorklet: (event: T) => void): void {
+  updateWorklet(newWorklet: (event: ReanimatedEvent<Event>) => void): void {
     this.worklet = newWorklet;
     this.reattachNeeded = true;
   }
