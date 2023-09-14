a56db7daa83291af7dae64ebf9ccf9984d5f1073
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-09-14 11:33:19 +0200

    switcheroo

--- src/reanimated2/hook/useScrollViewOffset.ts
+++ src/reanimated2/hook/useScrollViewOffset.ts
@@ -6,8 +6,8 @@ import type Animated from '../../index'; // TODO: fixme?
 import type { ScrollEvent } from './useAnimatedScrollHandler';
 import type { SharedValue } from '../commonTypes';
 import { findNodeHandle } from 'react-native';
-import { useSharedValue } from './useSharedValue';
 import { useEvent } from './useEvent';
+import { useSharedValue } from './useSharedValue';
 
 const scrollEventNames = [
   'onScroll',
