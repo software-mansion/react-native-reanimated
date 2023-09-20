81dce303a4f4890f4d427565d265b5d8f13a58a3
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-09-20 18:21:54 +0200

    fix PlatformChecker

--- src/reanimated2/PlatformChecker.ts
+++ src/reanimated2/PlatformChecker.ts
@@ -1,11 +1,13 @@
 'use strict';
 import { Platform } from 'react-native';
 
+// This type is necessary since some libraries tend to do a lib check
+// and this file causes type errors on `global` access.
+type localGlobal = typeof globalThis & Record<string, unknown>;
+
 export function isJest(): boolean {
   return !!process.env.JEST_WORKER_ID;
 }
-
-type localGlobal = typeof globalThis & Record<string, unknown>;
 
 export function isChromeDebugger(): boolean {
   return (
