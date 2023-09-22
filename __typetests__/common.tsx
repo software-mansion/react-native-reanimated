0562a90a84f1d4459ba3eed04e71e0db57380aaa
Alex Cynk <ola.zn123@wp.pl>
2023-09-22 12:28:40 +0200

    Type fixes and refactor of decay (#5088)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please follow the template so that
    the reviewers can easily understand what the code changes affect. -->
    
    ## Summary
    
    First part of this change is
    [HERE](https://github.com/software-mansion/react-native-reanimated/pull/5090)
    
    
    1. $\large \textcolor{teal}{\textsf{Why do we need this?}}$
    * In the future we expect further modification of our decay function, we
    want to provide smoother transition when some big additional velocity is
    added. Smaller files are easier to modify safely.
    * We have two completely different formulas for movement depending on
    `rubberBandEffect` prop. Therefore it would be nice to separate these
    formulas instead of having them intersecting with each other.
    * Some type fixes and assertions were added. It's generally a bad
    pattern to use `!` as no-null-assertion, and definitely not the safest
    one.
    1. $\large \textcolor{teal}{\textsf{What was done?}}$
       * Move `rubberBandDecay` and `rigidDecay` to separate files
       * add type-check to get rid of no-null-assertion
    1. $\large \textcolor{teal}{\textsf{New file structure}}$
    Imports are as follows:
    
    ```mermaid
    flowchart BT
    
    A --> Z(Index)
    B(RigidDecay) -->  A(Decay)
    D(Utils) --> A
    C(RubberBandDecay) -->  A(Decay)
    
    D --> B
    D --> C
    ``` 
    
    <!-- Explain the motivation for this PR. Include "Fixes #<number>" if
    applicable. -->
    
    ## Test plan
    Make sure everything works just like it was working before
    
    <table>
    <tr><td>Rubber Band Effect</td><td>Rigid Decay</td></tr>
    <tr><td>
    
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/56199675/77b87311-0d61-4756-a426-bde53a701e41
    
    
    
    </td><td>
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/56199675/162f208d-b914-49fa-ae2c-46bcca5ad8f5
    
    </td></tr>
    
    </table>
    
    ---------
    
    Co-authored-by: Aleksandra Cynk <aleksandra.cynk@swmansion.com>
    Co-authored-by: Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>

--- __typetests__/common.tsx
+++ __typetests__/common.tsx
@@ -881,7 +881,45 @@ function WithSequenceTest() {
 }
 
 // withDecay
-function WithDecayTest() {
+function withDecayTest() {
+  // @ts-expect-error `rubberBandEffect=true` makes `clamp` required.
+  const a = withDecay({ rubberBandEffect: true });
+
+  const b = withDecay({ rubberBandEffect: false });
+
+  const c = withDecay({ rubberBandEffect: true, clamp: [0, 1] });
+
+  // @ts-expect-error `clamp` too short.
+  const d = withDecay({ rubberBandEffect: true, clamp: [0] });
+
+  // @ts-expect-error `clamp` too long.
+  const e = withDecay({ rubberBandEffect: true, clamp: [0, 1, 2] });
+
+  // @ts-expect-error When `rubberBandEffect` is false then `rubberBandFactor` should not be provided.
+  const f = withDecay({ rubberBandEffect: false, rubberBandFactor: 3 });
+
+  // @ts-expect-error When `rubberBandEffect` isn't provided then `rubberBandFactor` should not be provided.
+  const f2 = withDecay({ rubberBandFactor: 3 });
+
+  const g = withDecay({
+    rubberBandEffect: true,
+    clamp: [0, 1],
+    rubberBandFactor: 3,
+  });
+
+  const rubberBandOn: boolean = Math.random() < 0.5;
+  // @ts-expect-error  When `rubberBandEffect` is an unknown boolean user still has to pass clamp.
+  const h = withDecay({ rubberBandEffect: rubberBandOn });
+
+  const i = withDecay({ rubberBandEffect: rubberBandOn, clamp: [0, 1] });
+
+  // @ts-expect-error Config is required
+  const j = withDecay();
+
+  const k = withDecay({});
+}
+
+function WithDecayComponentTest() {
   const x = useSharedValue(0);
 
   const gestureHandler = useAnimatedGestureHandler({
