2993ce60e179c03c05c50645a98c211b16a564bb
Michał Bert <michal.bert@swmansion.com>
2023-09-22 12:39:58 +0200

    Change method of comparing rects

--- src/reanimated2/layoutReanimation/web/animationsManager.ts
+++ src/reanimated2/layoutReanimation/web/animationsManager.ts
@@ -285,7 +285,13 @@ export function getRandomDelay(maxDelay = 1000) {
 }
 
 export function areDOMRectsEqual(r1: DOMRect, r2: DOMRect) {
-  return JSON.stringify(r1) === JSON.stringify(r2);
+  // There are 4 more fields, but checking these should suffice
+  return (
+    r1.x === r2.x &&
+    r1.y === r2.y &&
+    r1.width === r2.width &&
+    r1.height === r2.height
+  );
 }
 
 export function setElementAnimation(
