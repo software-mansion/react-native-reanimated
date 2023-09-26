ace7af4d7c4497c17c5a55ce1817f09af0068f68
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2023-09-26 16:46:02 +0200

    Add comment to `LayoutAnimationConfig`

--- src/reanimated2/component/LayoutAnimationConfig.tsx
+++ src/reanimated2/component/LayoutAnimationConfig.tsx
@@ -7,6 +7,8 @@ import { findNodeHandle } from 'react-native';
 export const SkipEnteringContext =
   createContext<React.MutableRefObject<boolean> | null>(null);
 
+// skipEntering - don't animate entering of children on wrapper mount
+// skipExiting - don't animate exiting of children on wrapper unmount
 interface LayoutAnimationConfigProps {
   skipEntering?: boolean;
   skipExiting?: boolean;
@@ -27,6 +29,13 @@ function SkipEntering(props: { shouldSkip: boolean; children: ReactNode }) {
   );
 }
 
+// skipExiting (unlike skipEntering) cannot be done by conditionally
+// configuring the animation in `createAnimatedComponent`, since at this stage
+// we don't know if the wrapper is going to be unmounted or not.
+// That's why we need to pass the skipExiting flag to the native side
+// when the wrapper is unmounted to prevent the animation.
+// Since `ReactNode` can be a list of nodes, we wrap every child with our wrapper
+// so we are able to access its tag with `findNodeHandle`.
 export class LayoutAnimationConfig extends Component<LayoutAnimationConfigProps> {
   componentWillUnmount(): void {
     if (
