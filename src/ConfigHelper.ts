software-mansion/react-native-reanimated

commit 46b523a1f7c09d8ac5144e32859b725a94c42900

Fix `useAnimatedProps` on web (#4977)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

This PR changes behavior of `useAnimatedProps` so that props get updated
properly on web.

Fixes #4909

<!-- Explain the motivation for this PR. Include "Fixes #<number>" if
applicable. -->

## Test plan

<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->

Tested on [example from
issue](https://github.com/apatozi/issue-reanimated-useanimatedprops-web).

src/reanimated2/js-reanimated/index.ts
@@ -56,7 +56,8 @@ interface JSReanimatedComponent {
 
 export const _updatePropsJS = (
   updates: StyleProps | AnimatedStyle<any>,
-  viewRef: { _component?: JSReanimatedComponent }
+  viewRef: { _component?: JSReanimatedComponent },
+  isAnimatedProps?: boolean
 ): void => {
   if (viewRef._component) {
     const component = viewRef._component;
@@ -74,14 +75,14 @@ export const _updatePropsJS = (
       // This is the legacy way to update props on React Native Web <= 0.18.
       // Also, some components (e.g. from react-native-svg) don't have styles
       // and always provide setNativeProps function instead (even on React Native Web 0.19+).
-      setNativeProps(component, rawStyles);
+      setNativeProps(component, rawStyles, isAnimatedProps);
     } else if (
       createReactDOMStyle !== undefined &&
       component.style !== undefined
     ) {
       // React Native Web 0.19+ no longer provides setNativeProps function,
       // so we need to update DOM nodes directly.
-      updatePropsDOM(component, rawStyles);
+      updatePropsDOM(component, rawStyles, isAnimatedProps);
     } else if (Object.keys(component.props).length > 0) {
       Object.keys(component.props).forEach((key) => {
         if (!rawStyles[key]) {
@@ -98,21 +99,31 @@ export const _updatePropsJS = (
 
 const setNativeProps = (
   component: JSReanimatedComponent,
-  style: StyleProps
+  newProps: StyleProps,
+  isAnimatedProps?: boolean
 ): void => {
+  if (isAnimatedProps) {
+    component.setNativeProps?.(newProps);
+    return;
+  }
+
   const previousStyle = component.previousStyle ? component.previousStyle : {};
-  const currentStyle = { ...previousStyle, ...style };
+  const currentStyle = { ...previousStyle, ...newProps };
   component.previousStyle = currentStyle;
+
   component.setNativeProps?.({ style: currentStyle });
 };
 
 const updatePropsDOM = (
-  component: JSReanimatedComponent,
-  style: StyleProps
+  component: JSReanimatedComponent | HTMLElement,
+  style: StyleProps,
+  isAnimatedProps?: boolean
 ): void => {
-  const previousStyle = component.previousStyle ? component.previousStyle : {};
+  const previousStyle = (component as JSReanimatedComponent).previousStyle
+    ? (component as JSReanimatedComponent).previousStyle
+    : {};
   const currentStyle = { ...previousStyle, ...style };
-  component.previousStyle = currentStyle;
+  (component as JSReanimatedComponent).previousStyle = currentStyle;
 
   const domStyle = createReactDOMStyle(currentStyle);
   if (Array.isArray(domStyle.transform) && createTransformValue !== undefined) {
@@ -133,7 +144,11 @@ const updatePropsDOM = (
   }
 
   for (const key in domStyle) {
-    (component.style as StyleProps)[key] = domStyle[key];
+    if (isAnimatedProps) {
+      (component as HTMLElement).setAttribute(key, domStyle[key]);
+    } else {
+      (component.style as StyleProps)[key] = domStyle[key];
+    }
   }
 };
 

src/reanimated2/UpdateProps.ts
@@ -11,15 +11,16 @@ import { runOnUIImmediately } from './threads';
 let updateProps: (
   viewDescriptor: SharedValue<Descriptor[]>,
   updates: StyleProps | AnimatedStyle<any>,
-  maybeViewRef: ViewRefSet<any> | undefined
+  maybeViewRef: ViewRefSet<any> | undefined,
+  isAnimatedProps?: boolean
 ) => void;
 
 if (shouldBeUseWeb()) {
-  updateProps = (_, updates, maybeViewRef) => {
+  updateProps = (_, updates, maybeViewRef, isAnimatedProps) => {
     'worklet';
     if (maybeViewRef) {
       maybeViewRef.items.forEach((item, _) => {
-        _updatePropsJS(updates, item);
+        _updatePropsJS(updates, item, isAnimatedProps);
       });
     }
   };

src/reanimated2/helperTypes.ts
@@ -163,5 +163,6 @@ export type useAnimatedPropsType = <T extends object>(
   adapters?:
     | AnimatedPropsAdapterFunction
     | AnimatedPropsAdapterFunction[]
-    | null
+    | null,
+  isAnimatedProps?: boolean
 ) => Partial<T>;

src/reanimated2/hook/Hooks.ts
@@ -1,12 +1,37 @@
 import { useCallback } from 'react';
 import { useAnimatedStyle } from './useAnimatedStyle';
 import type { DependencyList } from './commonTypes';
-import type { useAnimatedPropsType } from '../helperTypes';
+import type {
+  AnimatedPropsAdapterFunction,
+  useAnimatedPropsType,
+} from '../helperTypes';
+import { shouldBeUseWeb } from '../PlatformChecker';
 
 // TODO: we should make sure that when useAP is used we are not assigning styles
 // when you need styles to animated you should always use useAS
 // TODO TYPESCRIPT This is a temporary cast to get rid of .d.ts file.
-export const useAnimatedProps = useAnimatedStyle as useAnimatedPropsType;
+
+export let useAnimatedProps: useAnimatedPropsType;
+
+if (shouldBeUseWeb()) {
+  useAnimatedProps = function <T extends object>(
+    updater: () => Partial<T>,
+    deps?: DependencyList | null,
+    adapters?:
+      | AnimatedPropsAdapterFunction
+      | AnimatedPropsAdapterFunction[]
+      | null
+  ) {
+    return (useAnimatedStyle as useAnimatedPropsType)(
+      updater,
+      deps,
+      adapters,
+      true
+    );
+  };
+} else {
+  useAnimatedProps = useAnimatedStyle as useAnimatedPropsType;
+}
 
 export function useWorkletCallback<A extends unknown[], R>(
   fun: (...args: A) => R,

src/reanimated2/hook/useAnimatedStyle.ts
@@ -183,7 +183,8 @@ function styleUpdater(
   updater: BasicWorkletFunction<AnimatedStyle<any>>,
   state: AnimatedState,
   maybeViewRef: ViewRefSet<any> | undefined,
-  animationsActive: SharedValue<boolean>
+  animationsActive: SharedValue<boolean>,
+  isAnimatedProps = false
 ): void {
   'worklet';
   const animations = state.animations ?? {};
@@ -260,7 +261,7 @@ function styleUpdater(
     state.animations = [];
 
     if (!shallowEqual(oldValues, newValues)) {
-      updateProps(viewDescriptors, newValues, maybeViewRef);
+      updateProps(viewDescriptors, newValues, maybeViewRef, isAnimatedProps);
     }
   }
   state.last = newValues;
@@ -416,7 +417,8 @@ export const useAnimatedStyle = function <T extends AnimatedStyle<any>>(
   // animated style cannot be an array
   updater: BasicWorkletFunction<T extends Array<unknown> ? never : T>,
   dependencies?: DependencyList,
-  adapters?: AdapterWorkletFunction | AdapterWorkletFunction[]
+  adapters?: AdapterWorkletFunction | AdapterWorkletFunction[],
+  isAnimatedProps = false
 ): AnimatedStyleResult {
   const viewsRef: ViewRefSet<any> = makeViewsRefSet();
   const initRef = useRef<AnimationRef>();
@@ -512,7 +514,8 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
           updaterFn,
           remoteState,
           maybeViewRef,
-          animationsActive
+          animationsActive,
+          isAnimatedProps
         );
       };
     }

commit 66be81331d924604478457a5e407d5207e038611

Fix animated UI Props on Web (#5169)
## Summary

This PR fixes regression introduced in
https://github.com/software-mansion/react-native-reanimated/pull/4977.
Some animated props (for example UI props) required to be updated
directly as component prop - `setNativeProps(props)`, but all the rest
of them still required to be updated as a component style -
`setNativeProps({style: props})`.

| before | after |
| --- | --- |
| <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/60eb728e-1724-4826-9174-0d7b603595b5"
/> | <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/9ac9aa45-6827-4572-b268-f2510c2e5503"
/> |

## Test plan

<details>
<summary>AnimatedProps - UIProps - #4909</summary>

```js
import { Button, Text, View } from 'react-native';
import Animated, {
  useSharedValue,
  useAnimatedProps,
} from 'react-native-reanimated';
Animated.addWhitelistedNativeProps({
  accessibilityRole: true,
});
Animated.addWhitelistedUIProps({
  accessibilityRole: true,
});

export default function App() {
  const sharedRole = useSharedValue('initial');
  const animatedProps = useAnimatedProps(() => {
    console.debug('this gets trigered correctly', sharedRole.value);
    return {
      accessibilityRole: sharedRole.value
    };
  }, [sharedRole]);

  return (
    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
      <Animated.View
        animatedProps={animatedProps}
        nativeID="find-me-in-dev-tools">
        <Text>some lorem ipsum</Text>
      </Animated.View>
      <Button
        title="ToggleRole"
        onPress={() => {
          if (sharedRole.value == 'New Value') {
            sharedRole.value = 'New Value2';
          } else {
            sharedRole.value = 'New Value';
          }
        }}
      />
    </View>
  );
}


```

</details>


<details>
<summary>AnimatedProps</summary>

```js
import { TextInput } from 'react-native';

src/ConfigHelper.ts
@@ -1,150 +1,41 @@
 'use strict';
+import { PropsAllowlists } from './propsAllowlists';
 import { configureProps as jsiConfigureProps } from './reanimated2/core';
-
-/**
- * Styles allowed to be direcly updated in UI thread
- */
-let UI_THREAD_PROPS_WHITELIST: Record<string, boolean> = {
-  opacity: true,
-  transform: true,
-  /* colors */
-  backgroundColor: true,
-  borderRightColor: true,
-  borderBottomColor: true,
-  borderColor: true,
-  borderEndColor: true,
-  borderLeftColor: true,
-  borderStartColor: true,
-  borderTopColor: true,
-  /* ios styles */
-  shadowOpacity: true,
-  shadowRadius: true,
-  /* legacy android transform properties */
-  scaleX: true,
-  scaleY: true,
-  translateX: true,
-  translateY: true,
-};
-
-/**
- * Whitelist of view props that can be updated in native thread via UIManagerModule
- */
-let NATIVE_THREAD_PROPS_WHITELIST: Record<string, boolean> = {
-  borderBottomWidth: true,
-  borderEndWidth: true,
-  borderLeftWidth: true,
-  borderRightWidth: true,
-  borderStartWidth: true,
-  borderTopWidth: true,
-  borderWidth: true,
-  bottom: true,
-  flex: true,
-  flexGrow: true,
-  flexShrink: true,
-  height: true,
-  left: true,
-  margin: true,
-  marginBottom: true,
-  marginEnd: true,
-  marginHorizontal: true,
-  marginLeft: true,
-  marginRight: true,
-  marginStart: true,
-  marginTop: true,
-  marginVertical: true,
-  maxHeight: true,
-  maxWidth: true,
-  minHeight: true,
-  minWidth: true,
-  padding: true,
-  paddingBottom: true,
-  paddingEnd: true,
-  paddingHorizontal: true,
-  paddingLeft: true,
-  paddingRight: true,
-  paddingStart: true,
-  paddingTop: true,
-  paddingVertical: true,
-  right: true,
-  start: true,
-  top: true,
-  width: true,
-  zIndex: true,
-  borderBottomEndRadius: true,
-  borderBottomLeftRadius: true,
-  borderBottomRightRadius: true,
-  borderBottomStartRadius: true,
-  borderRadius: true,
-  borderTopEndRadius: true,
-  borderTopLeftRadius: true,
-  borderTopRightRadius: true,
-  borderTopStartRadius: true,
-  elevation: true,
-  fontSize: true,
-  lineHeight: true,
-  textShadowRadius: true,
-  textShadowOffset: true,
-  letterSpacing: true,
-  aspectRatio: true,
-  columnGap: true, // iOS only
-  end: true, // number or string
-  flexBasis: true, // number or string
-  gap: true,
-  rowGap: true,
-  /* strings */
-  display: true,
-  backfaceVisibility: true,
-  overflow: true,
-  resizeMode: true,
-  fontStyle: true,
-  fontWeight: true,
-  textAlign: true,
-  textDecorationLine: true,
-  fontFamily: true,
-  textAlignVertical: true,
-  fontVariant: true,
-  textDecorationStyle: true,
-  textTransform: true,
-  writingDirection: true,
-  alignContent: true,
-  alignItems: true,
-  alignSelf: true,
-  direction: true, // iOS only
-  flexDirection: true,
-  flexWrap: true,
-  justifyContent: true,
-  position: true,
-  /* text color */
-  color: true,
-  tintColor: true,
-  shadowColor: true,
-  placeholderTextColor: true,
-};
 
 function configureProps(): void {
   jsiConfigureProps(
-    Object.keys(UI_THREAD_PROPS_WHITELIST),
-    Object.keys(NATIVE_THREAD_PROPS_WHITELIST)
+    Object.keys(PropsAllowlists.UI_THREAD_PROPS_WHITELIST),
+    Object.keys(PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST)
   );
 }
 
 export function addWhitelistedNativeProps(
   props: Record<string, boolean>
 ): void {
-  const oldSize = Object.keys(NATIVE_THREAD_PROPS_WHITELIST).length;
-  NATIVE_THREAD_PROPS_WHITELIST = {
-    ...NATIVE_THREAD_PROPS_WHITELIST,
+  const oldSize = Object.keys(
+    PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST
+  ).length;
+  PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST = {
+    ...PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST,
     ...props,
   };
-  if (oldSize !== Object.keys(NATIVE_THREAD_PROPS_WHITELIST).length) {
+  if (
+    oldSize !==
+    Object.keys(PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST).length
+  ) {
     configureProps();
   }
 }
 
 export function addWhitelistedUIProps(props: Record<string, boolean>): void {
-  const oldSize = Object.keys(UI_THREAD_PROPS_WHITELIST).length;
-  UI_THREAD_PROPS_WHITELIST = { ...UI_THREAD_PROPS_WHITELIST, ...props };
-  if (oldSize !== Object.keys(UI_THREAD_PROPS_WHITELIST).length) {
+  const oldSize = Object.keys(PropsAllowlists.UI_THREAD_PROPS_WHITELIST).length;
+  PropsAllowlists.UI_THREAD_PROPS_WHITELIST = {
+    ...PropsAllowlists.UI_THREAD_PROPS_WHITELIST,
+    ...props,
+  };
+  if (
+    oldSize !== Object.keys(PropsAllowlists.UI_THREAD_PROPS_WHITELIST).length
+  ) {
     configureProps();
   }
 }
@@ -171,8 +62,8 @@ export function adaptViewConfig(viewConfig: ViewConfig): void {
       // we don't want to add native props as they affect layout
       // we also skip props which repeat here
       if (
-        !(key in NATIVE_THREAD_PROPS_WHITELIST) &&
-        !(key in UI_THREAD_PROPS_WHITELIST)
+        !(key in PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST) &&
+        !(key in PropsAllowlists.UI_THREAD_PROPS_WHITELIST)
       ) {
         propsToAdd[key] = true;
       }

src/propsAllowlists.ts
@@ -1,0 +1,125 @@
+type AllowlistsHolder = {
+  UI_THREAD_PROPS_WHITELIST: Record<string, boolean>;
+  NATIVE_THREAD_PROPS_WHITELIST: Record<string, boolean>;
+};
+
+export const PropsAllowlists: AllowlistsHolder = {
+  /**
+   * Styles allowed to be direcly updated in UI thread
+   */
+  UI_THREAD_PROPS_WHITELIST: {
+    opacity: true,
+    transform: true,
+    /* colors */
+    backgroundColor: true,
+    borderRightColor: true,
+    borderBottomColor: true,
+    borderColor: true,
+    borderEndColor: true,
+    borderLeftColor: true,
+    borderStartColor: true,
+    borderTopColor: true,
+    /* ios styles */
+    shadowOpacity: true,
+    shadowRadius: true,
+    /* legacy android transform properties */
+    scaleX: true,
+    scaleY: true,
+    translateX: true,
+    translateY: true,
+  },
+  /**
+   * Whitelist of view props that can be updated in native thread via UIManagerModule
+   */
+  NATIVE_THREAD_PROPS_WHITELIST: {
+    borderBottomWidth: true,
+    borderEndWidth: true,
+    borderLeftWidth: true,
+    borderRightWidth: true,
+    borderStartWidth: true,
+    borderTopWidth: true,
+    borderWidth: true,
+    bottom: true,
+    flex: true,
+    flexGrow: true,
+    flexShrink: true,
+    height: true,
+    left: true,
+    margin: true,
+    marginBottom: true,
+    marginEnd: true,
+    marginHorizontal: true,
+    marginLeft: true,
+    marginRight: true,
+    marginStart: true,
+    marginTop: true,
+    marginVertical: true,
+    maxHeight: true,
+    maxWidth: true,
+    minHeight: true,
+    minWidth: true,
+    padding: true,
+    paddingBottom: true,
+    paddingEnd: true,
+    paddingHorizontal: true,
+    paddingLeft: true,
+    paddingRight: true,
+    paddingStart: true,
+    paddingTop: true,
+    paddingVertical: true,
+    right: true,
+    start: true,
+    top: true,
+    width: true,
+    zIndex: true,
+    borderBottomEndRadius: true,
+    borderBottomLeftRadius: true,
+    borderBottomRightRadius: true,
+    borderBottomStartRadius: true,
+    borderRadius: true,
+    borderTopEndRadius: true,
+    borderTopLeftRadius: true,
+    borderTopRightRadius: true,
+    borderTopStartRadius: true,
+    elevation: true,
+    fontSize: true,
+    lineHeight: true,
+    textShadowRadius: true,
+    textShadowOffset: true,
+    letterSpacing: true,
+    aspectRatio: true,
+    columnGap: true, // iOS only
+    end: true, // number or string
+    flexBasis: true, // number or string
+    gap: true,
+    rowGap: true,
+    /* strings */
+    display: true,
+    backfaceVisibility: true,
+    overflow: true,
+    resizeMode: true,
+    fontStyle: true,
+    fontWeight: true,
+    textAlign: true,
+    textDecorationLine: true,
+    fontFamily: true,
+    textAlignVertical: true,
+    fontVariant: true,
+    textDecorationStyle: true,
+    textTransform: true,
+    writingDirection: true,
+    alignContent: true,
+    alignItems: true,
+    alignSelf: true,
+    direction: true, // iOS only
+    flexDirection: true,
+    flexWrap: true,
+    justifyContent: true,
+    position: true,
+    /* text color */
+    color: true,
+    tintColor: true,
+    shadowColor: true,
+    placeholderTextColor: true,
+  },
+};

src/reanimated2/js-reanimated/index.ts
@@ -3,6 +3,7 @@ import JSReanimated from './JSReanimated';
 import type { StyleProps } from '../commonTypes';
 import type { AnimatedStyle } from '../helperTypes';
 import { isWeb } from '../PlatformChecker';
+import { PropsAllowlists } from '../../propsAllowlists';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let createReactDOMStyle: (style: any) => any;
@@ -113,8 +114,15 @@ const setNativeProps = (
   isAnimatedProps?: boolean
 ): void => {
   if (isAnimatedProps) {
-    component.setNativeProps?.(newProps);
-    return;
+    const uiProps: Record<string, unknown> = {};
+    for (const key in newProps) {
+      if (isNativeProp(key)) {
+        uiProps[key] = newProps[key];
+      }
+    }
+    // Only update UI props directly on the component,
+    // other props can be updated as standard style props.
+    component.setNativeProps?.(uiProps);
   }
 
   const previousStyle = component.previousStyle ? component.previousStyle : {};
@@ -161,5 +169,9 @@ const updatePropsDOM = (
     }
   }
 };
+
+function isNativeProp(propName: string): boolean {
+  return !!PropsAllowlists.NATIVE_THREAD_PROPS_WHITELIST[propName];
+}
 
 export default reanimatedJS;