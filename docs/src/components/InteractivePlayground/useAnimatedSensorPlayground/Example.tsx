2a79a325ae6ff00e988d5baf3c0f29dd175377b6
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-10-05 15:28:28 +0200

    Attach texture

--- docs/src/components/InteractivePlayground/useAnimatedSensorPlayground/Example.tsx
+++ docs/src/components/InteractivePlayground/useAnimatedSensorPlayground/Example.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 interface Props {
   options: any;
@@ -8,6 +10,10 @@ interface Props {
 function Phone(props) {
   const { x, y, z } = props;
   const meshRef = useRef();
+  const colorMap = useLoader(
+    TextureLoader,
+    useBaseUrl('img/reanimated-texture.png')
+  );
 
   useEffect(() => {
     meshRef.current.rotation.x = x;
@@ -18,7 +24,13 @@ function Phone(props) {
   return (
     <mesh position={[0, 0, 2.5]} ref={meshRef}>
       <boxGeometry args={[1, 2, 0.1]} />
-      <meshStandardMaterial color={'#782aeb'} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
+      <meshStandardMaterial attachArray="material" map={colorMap} />
+      <meshStandardMaterial attachArray="material" color={'#001a72'} />
     </mesh>
   );
 }
@@ -26,10 +38,18 @@ function Phone(props) {
 export default function App({ options }: Props) {
   return (
     <Canvas style={{ height: 300 }}>
-      <ambientLight intensity={10} />
-
-      <pointLight position={[1, 1, 1]} intensity={5} />
-      <Phone {...options} />
+      <React.Suspense fallback={null}>
+        <ambientLight intensity={50} />
+        <spotLight
+          position={[1, 1, 1]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={50}
+        />
+        <pointLight position={[-1, -1, -1]} decay={0} intensity={50} />
+        <Phone {...options} />
+      </React.Suspense>
     </Canvas>
   );
 }
