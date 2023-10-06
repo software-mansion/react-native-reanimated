0a807177b864e91d2eb5a73ac240b82d9ba84fb1
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-10-06 16:18:42 +0200

    Implement rotation

--- docs/src/components/InteractivePlayground/useAnimatedSensorPlayground/index.tsx
+++ docs/src/components/InteractivePlayground/useAnimatedSensorPlayground/index.tsx
@@ -6,23 +6,100 @@ import { Range, SelectOption, formatReduceMotion } from '..';
 import { ReduceMotion } from 'react-native-reanimated';
 import useScreenSize from '@site/src/hooks/useScreenSize';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-import { SensorType } from 'react-native-reanimated';
+import { SensorType, ValueRotation } from 'react-native-reanimated';
 import * as THREE from 'three';
 
 const g_0 = 9.80665;
 
 const initialState = {
-  x: 180,
-  y: 180,
-  z: 180,
+  x: 0,
+  y: 0,
+  z: 0,
   sensorType: 'GRAVITY',
 };
 
-export default function useAnimatedSensorPlayground() {
-  const { windowWidth } =
-    ExecutionEnvironment.canUseViewport && useScreenSize();
-  const isMobile = windowWidth < 768;
+function degreeToRadian(x: number, y: number, z: number) {
+  const radX = THREE.MathUtils.degToRad(x);
+  const radY = THREE.MathUtils.degToRad(y);
+  const radZ = THREE.MathUtils.degToRad(z);
+  return { radX, radY, radZ };
+}
 
+function getGravityRotationVector(x: number, y: number, z: number) {
+  const { radX, radY, radZ } = degreeToRadian(x, y, z);
+
+  const euler = new THREE.Euler(radX, radY, radZ, 'XYZ');
+  const rotationMatrix = new THREE.Matrix4().makeRotationFromEuler(euler);
+  const gravityVector = new THREE.Vector3(0, -g_0, 0);
+
+  const rotationVector = gravityVector.applyMatrix4(rotationMatrix);
+
+  return rotationVector;
+}
+
+function formatGravityCode(rotationVector: THREE.Vector3) {
+  return `
+  { 
+    "interfaceOrientation": 0,
+    "x": ${rotationVector.x.toFixed(2)},
+    "y": ${rotationVector.y.toFixed(2)},
+    "z": ${rotationVector.z.toFixed(2)}
+  }
+  `;
+}
+
+function getRotation(
+  x: number,
+  y: number,
+  z: number
+): Omit<ValueRotation, 'interfaceOrientation'> {
+  const { radX, radY, radZ } = degreeToRadian(x, y, z);
+  const euler = new THREE.Euler(radX, radY, radZ, 'XYZ');
+  const quaternion = new THREE.Quaternion();
+  quaternion.setFromEuler(euler);
+
+  return {
+    pitch: euler.x,
+    roll: euler.y,
+    yaw: euler.z,
+    qw: quaternion.w,
+    qx: quaternion.x,
+    qy: quaternion.y,
+    qz: quaternion.z,
+  };
+}
+
+function formatRotationCode({
+  pitch,
+  roll,
+  yaw,
+  qw,
+  qx,
+  qy,
+  qz,
+}: Omit<ValueRotation, 'interfaceOrientation'>) {
+  return `
+  { 
+    "interfaceOrientation": 0,
+    "pitch": ${(-pitch).toFixed(2)},
+    "roll": ${roll.toFixed(2)},
+    "yaw": ${yaw.toFixed(2)},
+
+    "qw": ${qw.toFixed(2)},
+    "qx": ${qx.toFixed(2)},
+    "qy": ${qy.toFixed(2)},
+    "qz": ${qz.toFixed(2)}
+  }
+  `;
+}
+
+function getGyroscope(x: number, y: number, z: number, dt: number) {
+  const { radX, radY, radZ } = degreeToRadian(x, y, z);
+
+  console.log(radY / dt);
+}
+
+export default function useAnimatedSensorPlayground() {
   const [x, setX] = useState(initialState.x);
   const [y, setY] = useState(initialState.y);
   const [z, setZ] = useState(initialState.z);
@@ -32,28 +109,7 @@ export default function useAnimatedSensorPlayground() {
     setX(initialState.x);
     setY(initialState.y);
     setZ(initialState.z);
-    setSensorType(initialState.sensorType);
   };
-
-  const angleX = THREE.MathUtils.degToRad(x);
-  const angleY = THREE.MathUtils.degToRad(y);
-  const angleZ = THREE.MathUtils.degToRad(z);
-
-  var euler = new THREE.Euler(angleX, angleY, angleZ, 'XYZ');
-
-  var rotationMatrix = new THREE.Matrix4().makeRotationFromEuler(euler);
-
-  var vector = new THREE.Vector3(0, -g_0, 0);
-  const rotationVector = vector.applyMatrix4(rotationMatrix);
-
-  const gravityCode = `
-  { 
-    "interfaceOrientation": 0,
-    "x": ${rotationVector.x.toFixed(2)},
-    "y": ${rotationVector.y.toFixed(2)},
-    "z": ${rotationVector.z.toFixed(2)}
-  }
-  `;
 
   const controls = (
     <>
@@ -69,24 +125,49 @@ export default function useAnimatedSensorPlayground() {
           'ROTATION',
         ]}
       />
-      <Range label="X" min={0} max={359} step={1} value={x} onChange={setX} />
-      <Range label="Y" min={0} max={359} step={1} value={y} onChange={setY} />
-      <Range label="Z" min={0} max={359} step={1} value={z} onChange={setZ} />
+      <Range
+        label="X"
+        min={-180}
+        max={180}
+        step={1}
+        value={x}
+        onChange={setX}
+      />
+      <Range
+        label="Y"
+        min={-180}
+        max={180}
+        step={1}
+        value={y}
+        onChange={setY}
+      />
+      <Range
+        label="Z"
+        min={-180}
+        max={180}
+        step={1}
+        value={z}
+        onChange={setZ}
+      />
     </>
   );
 
-  const example = (
-    <Example
-      options={{
-        x,
-        y,
-        z,
-      }}
-    />
-  );
+  const example = <Example options={{ x, y, z }} />;
+
+  const gravityCode = formatGravityCode(getGravityRotationVector(x, y, z));
+  const rotationCode = formatRotationCode(getRotation(x, y, z));
+
+  const formatCode = (sensorType: string) => {
+    if (sensorType === 'GRAVITY') {
+      return gravityCode;
+    }
+    if (sensorType === 'ROTATION') {
+      return rotationCode;
+    }
+  };
 
   return {
-    code: gravityCode,
+    code: formatCode(sensorType),
     controls,
     example,
     resetOptions,
