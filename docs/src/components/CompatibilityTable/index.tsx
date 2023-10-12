2cf86e2b1230ecc26c0ae7269a5e5e6d28874f70
Aleksandra Cynk <aleksandra.cynk@swmansion.com>
2023-10-12 11:42:27 +0200

    Final fix

--- docs/src/components/CompatibilityTable/index.tsx
+++ docs/src/components/CompatibilityTable/index.tsx
@@ -84,7 +84,7 @@ export const CompatibilityTablePaper: React.FC = () => {
       </tr>
       <tr>
         <td>
-          from <b>2.10.x</b> to <b>2.13.x</b>
+          from <b>2.11.x</b> to <b>2.13.x</b>
         </td>
         <td></td>
         <td style={styles.supported}></td>
@@ -95,6 +95,22 @@ export const CompatibilityTablePaper: React.FC = () => {
         <td style={styles.supported}></td>
         <td style={styles.supported}></td>
         <td style={styles.supported}></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>
+          <b>2.10.x</b>
+        </td>
+        <td></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td style={styles.supported}></td>
+        <td></td>
         <td></td>
         <td></td>
       </tr>
