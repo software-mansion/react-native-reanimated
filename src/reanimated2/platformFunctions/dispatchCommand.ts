c02e5933aab9ba3df6c90d71f5ea08d6edeae0fa
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2023-10-16 14:13:20 +0200

    Refactor `platformFunctions`

--- src/reanimated2/platformFunctions/dispatchCommand.ts
+++ src/reanimated2/platformFunctions/dispatchCommand.ts
@@ -6,47 +6,65 @@ import type { Component } from 'react';
 
 const IS_NATIVE = !shouldBeUseWeb();
 
+function dispatchCommandFabric<T extends Component>(
+  animatedRef: AnimatedRef<T>,
+  commandName: string,
+  args: Array<unknown> = []
+) {
+  'worklet';
+  if (!_WORKLET) {
+    return;
+  }
+
+  const shadowNodeWrapper = animatedRef() as ShadowNodeWrapper;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  _dispatchCommandFabric!(shadowNodeWrapper, commandName, args);
+}
+
+function dispatchCommandPaper<T extends Component>(
+  animatedRef: AnimatedRef<T>,
+  commandName: string,
+  args: Array<unknown> = []
+) {
+  'worklet';
+  if (!_WORKLET) {
+    return;
+  }
+
+  const viewTag = animatedRef() as number;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  _dispatchCommandPaper!(viewTag, commandName, args);
+}
+
+function dispatchCommandJest() {
+  console.warn('[Reanimated] dispatchCommand() is not supported with Jest.');
+}
+
+function dispatchCommandChromeDebugger() {
+  console.warn(
+    '[Reanimated] dispatchCommand() is not supported with Chrome Debugger.'
+  );
+}
+
+function dispatchCommandDefault() {
+  console.warn(
+    '[Reanimated] dispatchCommand() is not supported on this configuration.'
+  );
+}
+
 export let dispatchCommand: <T extends Component>(
   animatedRef: AnimatedRef<T>,
   commandName: string,
   args?: Array<unknown>
 ) => void;
 if (IS_NATIVE && global._IS_FABRIC) {
-  dispatchCommand = (animatedRef, commandName, args = []) => {
-    'worklet';
-    if (!_WORKLET) {
-      return;
-    }
-
-    const shadowNodeWrapper = animatedRef() as ShadowNodeWrapper;
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    _dispatchCommandFabric!(shadowNodeWrapper, commandName, args);
-  };
+  dispatchCommand = dispatchCommandFabric;
 } else if (IS_NATIVE) {
-  dispatchCommand = (animatedRef, commandName, args = []) => {
-    'worklet';
-    if (!_WORKLET) {
-      return;
-    }
-
-    const viewTag = animatedRef() as number;
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    _dispatchCommandPaper!(viewTag, commandName, args);
-  };
+  dispatchCommand = dispatchCommandPaper;
 } else if (isJest()) {
-  dispatchCommand = () => {
-    console.warn('[Reanimated] dispatchCommand() is not supported with Jest.');
-  };
+  dispatchCommand = dispatchCommandJest;
 } else if (isChromeDebugger()) {
-  dispatchCommand = () => {
-    console.warn(
-      '[Reanimated] dispatchCommand() is not supported with Chrome Debugger.'
-    );
-  };
+  dispatchCommand = dispatchCommandChromeDebugger;
 } else {
-  dispatchCommand = () => {
-    console.warn(
-      '[Reanimated] dispatchCommand() is not supported on this configuration.'
-    );
-  };
+  dispatchCommand = dispatchCommandDefault;
 }
--- src/reanimated2/platformFunctions/dispatchCommand.web.ts
+++ src/reanimated2/platformFunctions/dispatchCommand.web.ts
@@ -1,11 +1,5 @@
 'use strict';
-import type { AnimatedRef } from '../hook/commonTypes';
-import type { Component } from 'react';
 
-export const dispatchCommand: <T extends Component>(
-  animatedRef: AnimatedRef<T>,
-  commandName: string,
-  args?: Array<unknown>
-) => void = () => {
+export function dispatchCommand() {
   console.warn('[Reanimated] dispatchCommand() is not supported on web.');
-};
+}
