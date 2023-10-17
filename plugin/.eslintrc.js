1bc1f807e9d9776891cbf09c33742b4e0fff19bd
Aleksandra Cynk <aleksandra.cynk@swmansion.com>
2023-10-17 16:15:11 +0200

    Fix plugin eslint

--- plugin/.eslintrc.js
+++ plugin/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '../.eslintrc.js',
-  ignorePatterns: ['**/*.d.ts','jestUtils.ts']};
+  rules:{
+    '@typescript-eslint/no-use-before-define':'off'
+  },
+  ignorePatterns: ['**/*.d.ts','jestUtils.ts']
+};
