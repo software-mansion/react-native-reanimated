38a55bce761f9ec7fec47aaca848b3007a352bed
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-10-17 23:19:45 +0200

    Move `setNativeProps` to `platformFunctions`

--- src/reanimated2/platformFunctions/index.ts
+++ src/reanimated2/platformFunctions/index.ts
@@ -1,4 +1,6 @@
+'use strict';
 export { dispatchCommand } from './dispatchCommand';
 export { measure } from './measure';
 export { scrollTo } from './scrollTo';
 export { setGestureState } from './setGestureState';
+export { setNativeProps } from './setNativeProps';
