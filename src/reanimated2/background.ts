16c31faa7a7ec2561932218d8070682bfab7ddbf
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-10-19 16:41:15 +0200

    Add error handling

--- src/reanimated2/background.ts
+++ src/reanimated2/background.ts
@@ -1,10 +1,14 @@
 'use strict';
 import NativeReanimatedModule from './NativeReanimated';
+import { shouldBeUseWeb } from './PlatformChecker';
+import type { WorkletFunction } from './commonTypes';
 import type { WorkletRuntime } from './runtimes';
 import {
   makeShareableCloneOnUIRecursive,
   makeShareableCloneRecursive,
 } from './shareables';
+
+const SHOULD_BE_USE_WEB = shouldBeUseWeb();
 
 export type BackgroundQueue = {
   __hostObjectBackgroundQueue: never;
@@ -17,10 +21,14 @@ export function createBackgroundQueue(name: string) {
 export function runOnBackgroundQueue(
   backgroundQueue: BackgroundQueue,
   workletRuntime: WorkletRuntime,
-  worklet: () => void
+  worklet: WorkletFunction<[], void>
 ) {
   'worklet';
-  // TODO: proper error handling
+  if (__DEV__ && !SHOULD_BE_USE_WEB && worklet.__workletHash === undefined) {
+    throw new Error(
+      '[Reanimated] The function passed to `runOnBackgroundQueue` is not a worklet.'
+    );
+  }
   if (_WORKLET) {
     _scheduleOnBackgroundQueue(
       backgroundQueue,
