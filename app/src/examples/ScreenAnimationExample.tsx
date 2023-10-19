97ec16b684cff46985e43b3eb411bfb25b0e2e18
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-10-19 12:39:35 +0200

    Add example

---
+++ app/src/examples/ScreenAnimationExample.tsx
@@ -0,0 +1,54 @@
+import { ParamListBase } from '@react-navigation/native';
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from 'react-native-screens/native-stack';
+
+const ScreenA = ({ navigation }: NativeStackScreenProps<ParamListBase>) => (
+  <View style={[styles.container, styles.screenA]}>
+    <Button
+      title="Go to detail"
+      onPress={() => navigation.navigate('ScreenB')}
+    />
+    <Button onPress={() => navigation.pop()} title="🔙 Back to ScreenA" />
+  </View>
+);
+
+const ScreenB = ({ navigation }: NativeStackScreenProps<ParamListBase>) => (
+  <View style={[styles.container, styles.screenB]}>
+    <Button title="Go back" onPress={() => navigation.goBack()} />
+  </View>
+);
+
+const Stack = createNativeStackNavigator();
+
+const App = () => (
+  <Stack.Navigator
+    screenOptions={{
+      stackAnimation: 'none',
+    }}>
+    <Stack.Screen
+      name="ScreenA"
+      component={ScreenA}
+      options={{ title: 'Screen Transition Animation' }}
+    />
+    <Stack.Screen name="ScreenB" component={ScreenB} />
+  </Stack.Navigator>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 10,
+  },
+  screenA: {
+    backgroundColor: 'moccasin',
+  },
+  screenB: {
+    backgroundColor: 'thistle',
+  },
+});
+
+export default App;
