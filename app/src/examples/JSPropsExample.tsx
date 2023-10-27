90e8a6d593b5aace9fb47ab9c371018862e628fe
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-10-27 09:05:57 +0200

    Major improvements

--- app/src/examples/JSPropsExample.tsx
+++ app/src/examples/JSPropsExample.tsx
@@ -1,4 +1,4 @@
-import { TextInput } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
 import Svg, { Path, Circle, G } from 'react-native-svg';
 import Animated, {
@@ -11,74 +11,62 @@ import Animated, {
 } from 'react-native-reanimated';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
-const BORDER_WIDTH = 25;
-const DIAL_RADIUS = 22.5;
-
-export const { PI } = Math;
-export const TAU = 2 * PI;
-
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 const AnimatedG = Animated.createAnimatedComponent(G);
 const AnimatedInput = Animated.createAnimatedComponent(TextInput);
 
-const polarToCartesian = (
-  angle: number,
-  radius: number,
-  { x, y }: { x: number; y: number }
-) => {
+interface Point {
+  x: number;
+  y: number;
+}
+
+function polarToCartesian(angle: number, radius: number, { x, y }: Point) {
   'worklet';
   const a = ((angle - 90) * Math.PI) / 180.0;
   return { x: x + radius * Math.cos(a), y: y + radius * Math.sin(a) };
-};
+}
 
-const cartesianToPolar = (
+function cartesianToPolar(
   x: number,
   y: number,
-  { x: cx, y: cy }: { x: number; y: number },
+  { x: cx, y: cy }: Point,
   step = 1
-) => {
+) {
   'worklet';
-
   const value =
     Math.atan((y - cy) / (x - cx)) / (Math.PI / 180) + (x > cx ? 90 : 270);
-
   return Math.round(value * (1 / step)) / (1 / step);
-};
+}
 
-const unMix = (value: number, x: number, y: number) => {
+function unMix(value: number, x: number, y: number) {
   'worklet';
   return (value - x) / (y - x);
-};
+}
 
-type Props = {
+type CircleSliderProps = {
   width: number;
   height: number;
-  fillColor: string[];
   value: number;
-  meterColor: string;
   min?: number;
   max?: number;
-  onValueChange: (value: any) => void;
-  children: (
-    props: Partial<{ defaultValue: string; text: string }>
-  ) => React.ReactNode;
   step?: number;
   decimals?: number;
+  onValueChange?: (value: number) => void;
 };
 
-const CircleSlider = (props: Props) => {
+function CircleSlider(props: CircleSliderProps) {
   const {
     width,
     height,
     value,
-    meterColor,
-    children,
-    min,
-    max,
+    min = 0,
+    max = 359,
     step = 1,
     decimals,
   } = props;
   const smallestSide = Math.min(width, height);
+
+  const BALL_RADIUS = 20;
 
   const cx = width / 2;
   const cy = height / 2;
@@ -112,12 +100,12 @@ const CircleSlider = (props: Props) => {
   const animatedCircle = useAnimatedProps(() => {
     const p2 = endPos.value;
     return {
-      x: p2.x - 7.5,
-      y: p2.y - 7.5,
+      x: p2.x - BALL_RADIUS,
+      y: p2.y - BALL_RADIUS,
     };
   });
 
-  const animatedChildrenProps = useAnimatedProps(() => {
+  const animatedInputProps = useAnimatedProps(() => {
     const decimalCount = (num: number) => {
       if (decimals) {
         return decimals;
@@ -135,14 +123,13 @@ const CircleSlider = (props: Props) => {
       [min! / 360, max! / 360]
     );
 
-    return {
-      defaultValue: `${value.toFixed(decimalCount(step))}`,
-      text: `${value.toFixed(decimalCount(step))}`,
-    };
+    const text = `${value.toFixed(decimalCount(step))}`;
+
+    return { text, defaultValue: text };
   });
 
   const gestureHandler = useAnimatedGestureHandler({
-    onActive: ({ x, y }: { x: number; y: number }, ctx: any) => {
+    onActive: ({ x, y }, ctx: { value: number }) => {
       const value = cartesianToPolar(x, y, { x: cx, y: cy }, step);
 
       ctx.value = interpolate(
@@ -153,68 +140,72 @@ const CircleSlider = (props: Props) => {
       end.value = value;
     },
     onFinish: (_, ctx) => {
-      runOnJS(props.onValueChange)(ctx.value);
+      if (props.onValueChange) {
+        runOnJS(props.onValueChange)(ctx.value);
+      }
     },
   });
 
   return (
-    <PanGestureHandler onGestureEvent={gestureHandler}>
-      <Animated.View>
-        <Svg width={width} height={height}>
-          <Circle cx={cx} cy={cy} r={r + BORDER_WIDTH / 2 - 1} fill="blue" />
-          <Circle
-            cx={cx}
-            cy={cy}
-            r={r}
-            strokeWidth={BORDER_WIDTH}
-            fill="url(#fill)"
-            stroke="rgba(255, 255, 525, 0.2)"
-          />
-          <AnimatedPath
-            stroke={meterColor}
-            strokeWidth={BORDER_WIDTH}
-            fill="none"
-            animatedProps={animatedPath}
-          />
-          <AnimatedG animatedProps={animatedCircle} onPress={() => {}}>
-            <Circle cx={7.5} cy={7.5} r={DIAL_RADIUS} fill={meterColor} />
-          </AnimatedG>
-        </Svg>
-        {children && children(animatedChildrenProps)}
-      </Animated.View>
-    </PanGestureHandler>
-  );
-};
-
-CircleSlider.defaultProps = {
-  width: 300,
-  height: 300,
-  fillColor: ['#fff'],
-  meterColor: '#fff',
-  min: 0,
-  max: 359,
-  step: 1,
-  onValueChange: (x: any) => x,
-};
-
-export default function JSPropsExample() {
-  const handleChange = (value) => console.log(value);
-
-  return (
-    <CircleSlider
-      width={325}
-      height={325}
-      value={0}
-      meterColor={'#ffffff'}
-      onValueChange={handleChange}>
-      {(animatedProps) => (
-        <AnimatedInput
-          keyboardType="numeric"
-          maxLength={3}
-          selectTextOnFocus={false}
-          animatedProps={animatedProps}
-        />
-      )}
-    </CircleSlider>
+    <>
+      <PanGestureHandler onGestureEvent={gestureHandler} minDist={0}>
+        <Animated.View>
+          <Svg width={width} height={height}>
+            <Circle cx={cx} cy={cy} r={r + 20 / 2 - 1} fill="lightgray" />
+            <Circle
+              cx={cx}
+              cy={cy}
+              r={r}
+              strokeWidth={20}
+              fill="url(#fill)"
+              stroke="rgba(255, 255, 255, 0.2)"
+            />
+            <AnimatedPath
+              stroke="white"
+              strokeWidth={20}
+              fill="none"
+              animatedProps={animatedPath}
+            />
+            <AnimatedG animatedProps={animatedCircle} onPress={() => {}}>
+              <Circle
+                cx={BALL_RADIUS}
+                cy={BALL_RADIUS}
+                r={BALL_RADIUS}
+                fill="white"
+              />
+            </AnimatedG>
+          </Svg>
+        </Animated.View>
+      </PanGestureHandler>
+      <AnimatedInput
+        editable={false}
+        animatedProps={animatedInputProps}
+        style={styles.input}
+      />
+    </>
   );
 }
+
+export default function JSPropsExample() {
+  return (
+    <View style={styles.container}>
+      <CircleSlider
+        width={300}
+        height={300}
+        value={45}
+        onValueChange={(value) => console.log(value)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    fontVariant: ['tabular-nums'],
+  },
+});
