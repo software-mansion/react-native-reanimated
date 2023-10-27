f6e58b3b7182001f783691a96e9c66a97361961b
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-10-27 10:41:59 +0200

    Improvements again

--- app/src/examples/JSPropsExample.tsx
+++ app/src/examples/JSPropsExample.tsx
@@ -1,5 +1,5 @@
-import { TextInput, View, StyleSheet } from 'react-native';
-import React, { useEffect } from 'react';
+import { TextInput, View, StyleSheet, ColorValue } from 'react-native';
+import React from 'react';
 import Svg, { Path, Circle, G } from 'react-native-svg';
 import Animated, {
   runOnJS,
@@ -7,6 +7,7 @@ import Animated, {
   useDerivedValue,
   useAnimatedProps,
   useAnimatedGestureHandler,
+  interpolate,
 } from 'react-native-reanimated';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
@@ -37,121 +38,121 @@ function cartesianToPolar(
   return Math.round(value * (1 / step)) / (1 / step);
 }
 
-function unMix(value: number, x: number, y: number) {
+function valueToProgress(value: number, min: number, max: number) {
   'worklet';
-  return (value - x) / (y - x);
+  return interpolate(value, [min, max], [0, 360]);
 }
 
-type CircleSliderProps = {
+function progressToValue(progress: number, min: number, max: number) {
+  'worklet';
+  return interpolate(progress, [0, 1], [min, max]);
+}
+
+type CircularSliderProps = {
   size: number;
+  circleRadius: number;
+  knobRadius: number;
+  knobColor: ColorValue;
+  progressLineWidth: number;
+  progressLineColor: ColorValue;
+  trackLineWidth: number;
+  trackLineColor: ColorValue;
+
   value: number;
-  min?: number;
-  max?: number;
+  min: number;
+  max: number;
   step?: number;
-  decimals?: number;
   onValueChange?: (value: number) => void;
 };
 
-function CircleSlider(props: CircleSliderProps) {
-  const { size, value, min = 0, max = 359, step = 1, decimals } = props;
-  const CIRCLE_RADIUS = 150;
-  const KNOB_RADIUS = 15;
-  const PROGRESS_WIDTH = 20;
+function CircularSlider(props: CircularSliderProps) {
+  const {
+    size,
+    circleRadius,
+    knobRadius,
+    knobColor,
+    progressLineWidth,
+    progressLineColor,
+    trackLineWidth,
+    trackLineColor,
 
-  const cx = CIRCLE_RADIUS;
-  const cy = CIRCLE_RADIUS;
-  const r = CIRCLE_RADIUS * 0.85;
+    value,
+    min,
+    max,
+    step = 1,
+  } = props;
 
-  const start = useSharedValue(0);
-  const end = useSharedValue(unMix(value, min! / 360, max! / 360));
+  const CENTER = { x: size / 2, y: size / 2 };
 
-  useEffect(() => {
-    end.value = unMix(value, min! / 360, max! / 360);
-  }, [value, end, max, min]);
+  const angle = useSharedValue(valueToProgress(value, min, max));
 
-  const startPos = useDerivedValue(() =>
-    polarToCartesian(start.value, r, { x: cx, y: cy })
+  const currentValue = useDerivedValue(() =>
+    Math.floor(progressToValue(angle.value / 360, min, max))
   );
 
-  const endPos = useDerivedValue(() =>
-    polarToCartesian(end.value, r, { x: cx, y: cy })
+  const knobPosition = useDerivedValue(() =>
+    polarToCartesian(angle.value, circleRadius, CENTER)
   );
 
   const animatedPath = useAnimatedProps(() => {
-    const p1 = startPos.value;
-    const p2 = endPos.value;
+    const p1 = polarToCartesian(0, circleRadius, CENTER);
+    const p2 = knobPosition.value;
     return {
-      d: `M${p1.x} ${p1.y} A ${r} ${r} 0 ${end.value > 180 ? 1 : 0} 1 ${p2.x} ${
-        p2.y
-      }`,
+      d: `M${p1.x} ${p1.y} A ${circleRadius} ${circleRadius} 0 ${
+        angle.value > 180 ? 1 : 0
+      } 1 ${p2.x} ${p2.y}`,
     };
   });
 
   const animatedCircle = useAnimatedProps(() => {
-    const p2 = endPos.value;
+    const p = knobPosition.value;
     return {
-      x: p2.x - KNOB_RADIUS,
-      y: p2.y - KNOB_RADIUS,
+      x: p.x - knobRadius,
+      y: p.y - knobRadius,
     };
   });
 
   const animatedInputProps = useAnimatedProps(() => {
-    const decimalCount = (num: number) => {
-      if (decimals) {
-        return decimals;
-      }
-      const numStr = String(num);
-      if (numStr.includes('.')) {
-        return numStr.split('.')[1].length;
-      }
-      return 0;
-    };
-
-    const value = end.value;
-
-    const text = `${value.toFixed(decimalCount(step))}`;
-
+    const text = String(currentValue.value);
     return { text, defaultValue: text };
   });
 
   const gestureHandler = useAnimatedGestureHandler({
-    onActive: ({ x, y }, ctx: { value: number }) => {
-      const value = cartesianToPolar(x, y, { x: cx, y: cy }, step);
-      ctx.value = value;
-      end.value = value;
+    onActive: ({ x, y }) => {
+      angle.value = cartesianToPolar(x, y, CENTER, step);
     },
-    onFinish: (_, ctx) => {
+    onFinish: () => {
       if (props.onValueChange) {
-        runOnJS(props.onValueChange)(ctx.value);
+        runOnJS(props.onValueChange)(currentValue.value);
       }
     },
   });
 
   return (
     <>
-      <PanGestureHandler onGestureEvent={gestureHandler} minDist={0}>
+      <PanGestureHandler onGestureEvent={gestureHandler} minDist={10}>
         <Animated.View>
           <Svg width={size} height={size}>
             <Circle
-              cx={cx}
-              cy={cy}
-              r={r}
-              strokeWidth={PROGRESS_WIDTH}
+              cx={CENTER.x}
+              cy={CENTER.y}
+              r={circleRadius}
+              stroke={trackLineColor}
+              strokeWidth={trackLineWidth}
               fill="none"
-              stroke="lightgray"
             />
             <AnimatedPath
-              stroke="lime"
-              strokeWidth={PROGRESS_WIDTH}
+              stroke={progressLineColor}
+              strokeWidth={progressLineWidth}
               fill="none"
               animatedProps={animatedPath}
             />
             <AnimatedG animatedProps={animatedCircle}>
               <Circle
-                cx={KNOB_RADIUS}
-                cy={KNOB_RADIUS}
-                r={KNOB_RADIUS}
-                fill="blue"
+                cx={knobRadius}
+                cy={knobRadius}
+                r={knobRadius}
+                fill={knobColor}
               />
             </AnimatedG>
           </Svg>
@@ -169,10 +170,19 @@ function CircleSlider(props: CircleSliderProps) {
 export default function JSPropsExample() {
   return (
     <View style={styles.container}>
-      <CircleSlider
+      <CircularSlider
         size={300}
-        value={45}
-        onValueChange={(value) => console.log(value)}
+        circleRadius={120}
+        knobRadius={15}
+        knobColor="green"
+        progressLineWidth={20}
+        progressLineColor="lime"
+        trackLineWidth={10}
+        trackLineColor="lightgray"
+        value={120}
+        min={0}
+        max={359}
+        onValueChange={console.log}
       />
     </View>
   );
