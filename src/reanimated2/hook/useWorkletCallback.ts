b1180f942cf36f06fc92423fcc31b342f2159d9c
szydlovsky <9szydlowski9@gmail.com>
2023-10-31 11:09:27 +0100

    Make useWorkletCallback properly infer dependencies

--- src/reanimated2/hook/useWorkletCallback.ts
+++ src/reanimated2/hook/useWorkletCallback.ts
@@ -1,10 +1,14 @@
 'use strict';
 import { useCallback } from 'react';
 import type { DependencyList } from './commonTypes';
+import type { WorkletFunction } from '../commonTypes';
 
 export function useWorkletCallback<Args extends unknown[], ReturnValue>(
   worklet: (...args: Args) => ReturnValue,
   deps?: DependencyList
 ) {
-  return useCallback(worklet, deps ?? []);
+  return useCallback(
+    worklet,
+    deps ?? Object.values((worklet as WorkletFunction).__closure)
+  );
 }
