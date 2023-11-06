984afad504a39689c6f70f2feae1f8dae26c9cef
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-11-06 11:20:44 +0100

    Web mock

--- src/reanimated2/js-reanimated/index.ts
+++ src/reanimated2/js-reanimated/index.ts
@@ -47,6 +47,12 @@ global._scheduleOnJS = () => {
   );
 };
 
+global._manageScreenTransition = () => {
+  throw new Error(
+    '[Reanimated] _manageScreenTransition should never be called in JSReanimated.'
+  );
+};
+
 interface JSReanimatedComponent {
   previousStyle: StyleProps;
   setNativeProps?: (style: StyleProps) => void;
