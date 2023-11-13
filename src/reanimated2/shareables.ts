software-mansion/react-native-reanimated

commit cb04295c9b574342bcc3e68db42a83742acbdadd

Expose APIs for integrating with worklets (#4944)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

This PR implements and exposes a new API for creating worklet runtimes
as well as running and scheduling worklets both from JS and C++ code.
The motivation of this PR was to allow for easier integration of other
third-party libraries with Reanimated worklets.

The following changes were required but they were merged as separate PRs
for the ease of code review:

* https://github.com/software-mansion/react-native-reanimated/pull/4608
* https://github.com/software-mansion/react-native-reanimated/pull/4612
* https://github.com/software-mansion/react-native-reanimated/pull/4621
* https://github.com/software-mansion/react-native-reanimated/pull/4722
* https://github.com/software-mansion/react-native-reanimated/pull/4723
* https://github.com/software-mansion/react-native-reanimated/pull/4731
* https://github.com/software-mansion/react-native-reanimated/pull/4765
* https://github.com/software-mansion/react-native-reanimated/pull/4768
* https://github.com/software-mansion/react-native-reanimated/pull/4773
* https://github.com/software-mansion/react-native-reanimated/pull/4775
* https://github.com/software-mansion/react-native-reanimated/pull/4779
* https://github.com/software-mansion/react-native-reanimated/pull/4780
* https://github.com/software-mansion/react-native-reanimated/pull/4785
* https://github.com/software-mansion/react-native-reanimated/pull/4800
* https://github.com/software-mansion/react-native-reanimated/pull/4869
* https://github.com/software-mansion/react-native-reanimated/pull/4871
* https://github.com/software-mansion/react-native-reanimated/pull/4929

## Changes

1. Removed `RuntimeManager` and `JSRuntimeHelper` in favor of
`WorkletRuntime` class

src/reanimated2/shareables.ts
@@ -180,7 +180,6 @@ See \`https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshoo
             true,
             depth + 1
           );
-          delete value.__initData;
         }
 
         for (const [key, element] of Object.entries(value)) {

commit b7fccb906797f7e9fb7f071337448481d2d72c3c

Avoid double serialization of `__initData` (#5186)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

When serializing worklets, first we call `makeShareableCloneRecursive`
for `value.__initData` (see #3930) and then we process the rest of the
props. Currently, we don't skip `__initData` in the second phase which
leads to double conversion of this field and contradicts the whole idea.
The regression was introduced by me in #4944 which removed `delete
value.__initData;` in order to allow worklets to be copied to multiple
runtimes and not only one. This PR skips serializing `__initData` if it
has already been serialized.

## Test plan

<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->

src/reanimated2/shareables.ts
@@ -203,6 +203,9 @@ See \`https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshoo
         }
 
         for (const [key, element] of Object.entries(value)) {
+          if (key === '__initData' && toAdapt.__initData !== undefined) {
+            continue;
+          }
           toAdapt[key] = makeShareableCloneRecursive(
             element,
             shouldPersistRemote,