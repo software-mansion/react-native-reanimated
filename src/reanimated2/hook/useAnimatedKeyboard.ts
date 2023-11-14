532bb025cbf82e80702a344c73b58af8d92241d4
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-11-14 15:48:49 +0100

    TSDoc useAnimatedKeyboard

--- src/reanimated2/hook/useAnimatedKeyboard.ts
+++ src/reanimated2/hook/useAnimatedKeyboard.ts
@@ -11,6 +11,13 @@ import type {
 } from '../commonTypes';
 import { KeyboardState } from '../commonTypes';
 
+/**
+ * Lets you synchronously get the position and state of the keyboard.
+ *
+ * @param options an additional keyboard configuration options
+ * @returns an object with the current keyboard `height` and `state` as [shared values](https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/glossary#shared-value)
+ * @see https://docs.swmansion.com/react-native-reanimated/docs/device/useAnimatedKeyboard
+ */
 export function useAnimatedKeyboard(
   options: AnimatedKeyboardOptions = { isStatusBarTranslucentAndroid: false }
 ): AnimatedKeyboardInfo {
