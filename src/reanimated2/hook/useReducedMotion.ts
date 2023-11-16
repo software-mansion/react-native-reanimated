34f44641c4b36705c246b2418404c5bac32d7be9
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-11-16 11:30:38 +0100

    TSDoc device hooks

--- src/reanimated2/hook/useReducedMotion.ts
+++ src/reanimated2/hook/useReducedMotion.ts
@@ -3,6 +3,12 @@ import { isReducedMotion } from '../PlatformChecker';
 
 const IS_REDUCED_MOTION = isReducedMotion();
 
+/**
+ * Lets you query the reduced motion system setting.
+ *
+ * @returns a boolean indicating whether the reduced motion setting was enabled when the app started
+ * @see https://docs.swmansion.com/react-native-reanimated/docs/device/useReducedMotion
+ */
 export function useReducedMotion() {
   return IS_REDUCED_MOTION;
 }
