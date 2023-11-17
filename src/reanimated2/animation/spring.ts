300c7f3fab373e213d9cf0d6ebf95903b2d460f4
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-11-17 17:20:41 +0100

    TSDoc spring

--- src/reanimated2/animation/spring.ts
+++ src/reanimated2/animation/spring.ts
@@ -32,9 +32,9 @@ type withSpringType = <T extends AnimatableValue>(
 /**
  * Lets you create spring-based animations.
  *
- * @param toValue - the value at which the animation will come to rest
- * @param config - the spring animation configuration
- * @param callback - a function called on animation complete
+ * @param toValue - the value at which the animation will come to rest - {@link AnimatableValue}
+ * @param config - the spring animation configuration - {@link SpringConfig}
+ * @param callback - a function called on animation complete - {@link AnimationCallback}
  * @returns an [animation object](https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/glossary#animation-object) which holds the current state of the animation
  * @see https://docs.swmansion.com/react-native-reanimated/docs/animations/withSpring
  */
