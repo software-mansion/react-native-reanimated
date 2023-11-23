35ae2ff9821cae022377726ef904042dc24c7af0
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2023-11-23 16:33:36 +0100

    [Web] Fix hiding component with `entering` on every render (#5278)
    Requires #5282.
    
    ## Summary
    
    Some TS shenanigans as a necessary bonus.
    
    <table>
    <tr><td>BEFORE</td><td>AFTER</td></tr>
    <tr><td>
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/40713406/d13c9186-f1e5-4e12-bbb1-704b1eac0c08
    
    </td><td>
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/40713406/804fbcfb-f0f0-4b3f-9af3-c46617ffed18
    
    </td></tr>
    </table>
    
    
    
    
    ## Test plan
    
    <details>
    
    ```tsx
    import { StyleSheet, View, Button } from 'react-native';
    
    import Animated, { FadeIn } from 'react-native-reanimated';
    
    import React from 'react';
    
    export default function EmptyExample() {
      const [shouldBeUseEntering, setShouldBeUseEntering] = React.useState(false);
    
      function handlePress() {
        setShouldBeUseEntering(!shouldBeUseEntering);
      }
    
      return (
        <View style={styles.container}>
          <Button title="ShouldBeUseEntering" onPress={handlePress} />
          <Animated.View
            style={styles.box}
            entering={shouldBeUseEntering ? FadeIn : undefined}
          />
        </View>
      );
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'center',
      },
      box: {
        width: 100,
        height: 100,
        backgroundColor: 'blue',
      },
    });
    ```
    
    </details>
    
    ---------
    
    Co-authored-by: Michał Bert <michal.bert@swmansion.com>

--- src/reanimated2/layoutReanimation/web/animationsManager.ts
+++ src/reanimated2/layoutReanimation/web/animationsManager.ts
@@ -36,18 +36,12 @@ function chooseConfig<ComponentProps extends Record<string, unknown>>(
 
 function checkUndefinedAnimationFail(
   initialAnimationName: string,
-  isLayoutTransition: boolean,
-  hasEnteringAnimation: boolean,
-  element: HTMLElement
+  isLayoutTransition: boolean
 ) {
   // This prevents crashes if we try to set animations that are not defined.
   // We don't care about layout transitions since they're created dynamically
   if (initialAnimationName in Animations || isLayoutTransition) {
     return false;
-  }
-
-  if (hasEnteringAnimation) {
-    makeElementVisible(element);
   }
 
   console.warn(
@@ -57,17 +51,9 @@ function checkUndefinedAnimationFail(
   return true;
 }
 
-function checkReduceMotionFail(
-  animationConfig: AnimationConfig,
-  hasEnteringAnimation: boolean,
-  element: HTMLElement
-) {
+function checkReduceMotionFail(animationConfig: AnimationConfig) {
   if (!animationConfig.reduceMotion) {
     return false;
-  }
-
-  if (hasEnteringAnimation) {
-    makeElementVisible(element);
   }
 
   return true;
@@ -100,33 +86,28 @@ function chooseAction(
   }
 }
 
-export function startWebLayoutAnimation<
+function tryGetAnimationConfigWithTransform<
   ComponentProps extends Record<string, unknown>
 >(
   props: Readonly<AnimatedComponentProps<ComponentProps>>,
-  element: HTMLElement,
-  animationType: LayoutAnimationType,
-  transitionData?: TransitionData
+  animationType: LayoutAnimationType
 ) {
   const config = chooseConfig(animationType, props);
   if (!config) {
-    return;
+    return null;
   }
 
-  const hasEnteringAnimation = props.entering !== undefined;
   const isLayoutTransition = animationType === LayoutAnimationType.LAYOUT;
   const initialAnimationName =
     typeof config === 'function' ? config.name : config.constructor.name;
 
   const shouldFail = checkUndefinedAnimationFail(
     initialAnimationName,
-    isLayoutTransition,
-    hasEnteringAnimation,
-    element
+    isLayoutTransition
   );
 
   if (shouldFail) {
-    return;
+    return null;
   }
 
   const transform = extractTransformFromStyle(props.style as StyleProps);
@@ -142,17 +123,46 @@ export function startWebLayoutAnimation<
     initialAnimationName as AnimationNames
   );
 
-  if (checkReduceMotionFail(animationConfig, hasEnteringAnimation, element)) {
-    return;
+  if (checkReduceMotionFail(animationConfig)) {
+    return null;
   }
 
-  chooseAction(
-    animationType,
-    animationConfig,
-    element,
-    transitionData as TransitionData,
-    transform
+  return { animationConfig, transform };
+}
+
+export function startWebLayoutAnimation<
+  ComponentProps extends Record<string, unknown>
+>(
+  props: Readonly<AnimatedComponentProps<ComponentProps>>,
+  element: HTMLElement,
+  animationType: LayoutAnimationType,
+  shouldMakeVisible = false,
+  transitionData?: TransitionData
+) {
+  let visibilityDelay = 0;
+
+  const maybeAnimationConfigWithTransform = tryGetAnimationConfigWithTransform(
+    props,
+    animationType
   );
+
+  if (maybeAnimationConfigWithTransform) {
+    const { animationConfig, transform } = maybeAnimationConfigWithTransform;
+
+    visibilityDelay = animationConfig.delay;
+
+    chooseAction(
+      animationType,
+      animationConfig,
+      element,
+      transitionData as TransitionData,
+      transform
+    );
+  }
+
+  if (shouldMakeVisible) {
+    makeElementVisible(element, visibilityDelay);
+  }
 }
 
 export function tryActivateLayoutTransition<
@@ -184,6 +194,7 @@ export function tryActivateLayoutTransition<
     props,
     element,
     LayoutAnimationType.LAYOUT,
+    false,
     transitionData
   );
 }
