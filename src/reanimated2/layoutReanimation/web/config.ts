34f0e47b6bd09d3f543b159ac88f29a1738992cc
Michał Bert <63123542+m-bert@users.noreply.github.com>
2023-11-23 16:17:58 +0100

    Fix `entering` in web LA (#5401)
    ## Summary
    
    This PR changes where we set component visibility at the beginning on
    entering animation. Moving it to `onanimationstart` prevents flickering.
    
    ## Test plan
    
    Tested on example app and provided repro:
    
    <details>
    <summary> Repro code </summary>
    
    ```jsx
    import React from 'react';
    import { StyleSheet, View, Text } from 'react-native';
    import Animated, {
      FadeIn,
      FadeInLeft,
      FadeInRight,
      FadeInUp,
      FadeInDown,
      FadeOut,
      FadeOutLeft,
      FadeOutRight,
      FadeOutUp,
      FadeOutDown,
    } from 'react-native-reanimated';
    
    const DELAY = 500;
    
    const fadeAnimation = [
      {
        enteringName: 'FadeIn',
        enteringAnimation: FadeIn,
        exitingName: 'FadeOut',
        exitingAnimation: FadeOut,
      },
      {
        enteringName: 'FadeInLeft',
        enteringAnimation: FadeInLeft,
        exitingName: 'FadeOutLeft',
        exitingAnimation: FadeOutLeft,
      },
      {
        enteringName: 'FadeInRight',
        enteringAnimation: FadeInRight,
        exitingName: 'FadeOutRight',
        exitingAnimation: FadeOutRight,
      },
      {
        enteringName: 'FadeInUp',
        enteringAnimation: FadeInUp,
        exitingName: 'FadeOutUp',
        exitingAnimation: FadeOutUp,
      },
      {
        enteringName: 'FadeInDown',
        enteringAnimation: FadeInDown,
        exitingName: 'FadeOutDown',
        exitingAnimation: FadeOutDown,
      },
    ];
    
    export default function App() {
      const [showExiting, setShowExiting] = React.useState(false);
    
      React.useEffect(() => {
        const timeout = setTimeout(() => {
          setShowExiting(true);
        }, DELAY * fadeAnimation.length);
        return () => clearTimeout(timeout);
      }, []);
    
      return (
        <View style={styles.container}>
          {showExiting
            ? fadeAnimation.map((animation) => (
                <View
                  style={[styles.box, styles.exitingBox]}
                  key={animation.exitingName}>
                  <Text style={styles.exitingText}>{animation.exitingName}</Text>
                </View>
              ))
            : fadeAnimation.map((animation, i) => (
                <Animated.View
                  entering={animation.enteringAnimation.delay(DELAY * i)}
                  exiting={animation.exitingAnimation.delay(500 * i)}
                  key={animation.enteringName}
                  style={[styles.box, styles.enteringBox]}>
                  <Text style={styles.enteringText}>{animation.enteringName}</Text>
                </Animated.View>
              ))}
        </View>
      );
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'center',
      },
      box: {
        height: 50,
        width: 250,
        margin: 4,
        alignItems: 'center',
        justifyContent: 'center',
      },
      enteringBox: {
        backgroundColor: '#b58df1',
      },
      enteringText: {
        fontSize: 16,
        color: 'white',
      },
      exitingBox: {
        borderColor: '#b58df1',
        borderStyle: 'dashed',
        borderWidth: 1,
      },
      exitingText: {
        fontSize: 16,
        color: '#b58df1',
      },
    });
    ```
    </details>

--- src/reanimated2/layoutReanimation/web/config.ts
+++ src/reanimated2/layoutReanimation/web/config.ts
@@ -1,5 +1,6 @@
 'use strict';
 import type { ReduceMotion } from '../../commonTypes';
+import type { LayoutAnimationType } from '../animationBuilder/commonTypes';
 import {
   BounceIn,
   BounceInData,
@@ -45,6 +46,7 @@ export type AnimationCallback = ((finished: boolean) => void) | null;
 
 export interface AnimationConfig {
   animationName: string;
+  animationType: LayoutAnimationType;
   duration: number;
   delay: number;
   easing: string;
