fffc5de6e1b7da4d856a6f882fc4c13d59522fa6
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-11-24 14:13:20 +0100

    Add link to shared value glossary

--- src/reanimated2/hook/useSharedValue.ts
+++ src/reanimated2/hook/useSharedValue.ts
@@ -5,7 +5,7 @@ import type { SharedValue } from '../commonTypes';
 import { makeMutable } from '../core';
 
 /**
- * Lets you define shared values in your components.
+ * Lets you define [shared values](https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/glossary#shared-value) in your components.
  *
  * @param initialValue - The value you want to be initially stored to a `.value` property.
  * @returns A shared value with a single `.value` property initially set to the `initialValue`.
