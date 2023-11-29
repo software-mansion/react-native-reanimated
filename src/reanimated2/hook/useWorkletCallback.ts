292b6291b3e082a00b9cc8aec7cc8156272ed435
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2023-11-29 14:50:03 +0100

    Add TSDoc to public API  (#5377)
    This PR aims to cover the entirety of Reanimated public API with short,
    descriptive TSDoc comments. This will allow developers to stay longer in
    their editors and save some time searching and googling.
    
    Under each function there's a link to the documentation.

--- src/reanimated2/hook/useWorkletCallback.ts
+++ src/reanimated2/hook/useWorkletCallback.ts
@@ -2,6 +2,9 @@
 import { useCallback } from 'react';
 import type { DependencyList } from './commonTypes';
 
+/**
+ * @deprecated don't use
+ */
 export function useWorkletCallback<Args extends unknown[], ReturnValue>(
   worklet: (...args: Args) => ReturnValue,
   deps?: DependencyList
