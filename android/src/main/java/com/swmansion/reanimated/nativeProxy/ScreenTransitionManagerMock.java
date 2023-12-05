7513d1e876933bf52be7ac9d9bd7af382219b13b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-12-05 09:55:21 +0100

    Java lint

--- android/src/main/java/com/swmansion/reanimated/nativeProxy/ScreenTransitionManagerMock.java
+++ android/src/main/java/com/swmansion/reanimated/nativeProxy/ScreenTransitionManagerMock.java
@@ -4,14 +4,14 @@ import com.swmansion.common.ScreenTransitionManager;
 
 public class ScreenTransitionManagerMock implements ScreenTransitionManager {
 
-    @Override
-    public int[] startTransition(int stackTag) {
-        return new int[] {-1, -1};
-    }
+  @Override
+  public int[] startTransition(int stackTag) {
+    return new int[] {-1, -1};
+  }
 
-    @Override
-    public void updateTransition(int stackTag, double progress) {}
+  @Override
+  public void updateTransition(int stackTag, double progress) {}
 
-    @Override
-    public void finishTransition(int stackTag, boolean canceled) {}
+  @Override
+  public void finishTransition(int stackTag, boolean canceled) {}
 }
