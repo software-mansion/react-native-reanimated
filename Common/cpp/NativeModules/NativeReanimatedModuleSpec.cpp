b9b56ee25ae2c26495fb063df3650c2420887867
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2023-12-06 16:18:07 +0100

    Width & opacity for Fabric getViewProp

--- Common/cpp/NativeModules/NativeReanimatedModuleSpec.cpp
+++ Common/cpp/NativeModules/NativeReanimatedModuleSpec.cpp
@@ -95,7 +95,11 @@ static jsi::Value SPEC_PREFIX(getViewProp)(
     size_t) {
   static_cast<NativeReanimatedModuleSpec *>(&turboModule)
       ->getViewProp(
-          rt, std::move(args[0]), std::move(args[1]), std::move(args[2]), std::move(args[3]));
+          rt,
+          std::move(args[0]),
+          std::move(args[1]),
+          std::move(args[2]),
+          std::move(args[3]));
   return jsi::Value::undefined();
 }
 
