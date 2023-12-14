04df11644c44872f91600a4b78a07bd6d7583f7b
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2023-12-14 14:10:23 +0100

    init

--- src/reanimated2/NativeReanimated/NativeReanimated.ts
+++ src/reanimated2/NativeReanimated/NativeReanimated.ts
@@ -79,7 +79,6 @@ function assertSingleReanimatedInstance() {
 See \`https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooting#another-instance-of-reanimated-was-detected\` for more details. Previous: ${global._REANIMATED_VERSION_JS}, current: ${jsVersion}.`
     );
   }
-  global._REANIMATED_VERSION_JS = jsVersion;
 }
 
 export class NativeReanimated {
@@ -90,6 +89,7 @@ export class NativeReanimated {
     if (__DEV__) {
       assertSingleReanimatedInstance();
     }
+    global._REANIMATED_VERSION_JS = jsVersion;
     if (global.__reanimatedModuleProxy === undefined) {
       const { ReanimatedModule } = NativeModules;
       ReanimatedModule?.installTurboModule();
