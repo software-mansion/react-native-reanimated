cd64e771e82d1d0ac1dc7894bfb208c0abb5e553
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2023-12-21 15:31:03 +0100

    More tests

--- app/src/examples/RuntimeTests/tests/withTiming.test.tsx
+++ app/src/examples/RuntimeTests/tests/withTiming.test.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -68,35 +68,29 @@ const AnimatedComponent = ({
   );
 };
 
-const styles = StyleSheet.create({
-  container: { flex: 1, flexDirection: 'column', backgroundColor: 'beige' },
-  animatedBox: {
-    width: 0,
-    opacity: 0,
-    height: 80,
-    margin: 30,
-  },
-});
-
-describe('With timing animation works with COLORS 🎨', () => {
+describe('withTiming animation of COLOR 🎨', () => {
   (
     [
-      [
-        'color as hex number 0x6495ed\n\t\t⚠️ This is not a valid color format, this behavior may be broken in the future\n\t',
-        EXAMPLE_COLORS.cornflowerblue[0],
-      ],
-      [
-        'color as rgb string "rgb(100,149,237)"',
-        EXAMPLE_COLORS.cornflowerblue[1],
-      ],
-      ['color as hex string "0x6495ed"', EXAMPLE_COLORS.cornflowerblue[2]],
-      [
-        'color as color string "cornflowerblue"',
-        EXAMPLE_COLORS.cornflowerblue[3],
-      ],
+      {
+        description:
+          'color as hex number 0x6495ed\n\t\t⚠️ This is not a valid color format, this behavior may be broken in the future\n\t',
+        color: EXAMPLE_COLORS.cornflowerblue[0],
+      },
+      {
+        description: 'color as rgb string "rgb(100,149,237)"',
+        color: EXAMPLE_COLORS.cornflowerblue[1],
+      },
+      {
+        description: 'color as hex string "0x6495ed"',
+        color: EXAMPLE_COLORS.cornflowerblue[2],
+      },
+      {
+        description: 'color as color string "cornflowerblue"',
+        color: EXAMPLE_COLORS.cornflowerblue[3],
+      },
     ] as const
-  ).forEach((entry) => {
-    const [description, color] = entry;
+  ).forEach((testCase) => {
+    const { description, color } = testCase;
     test(description, async () => {
       await render(<AnimatedComponent color2={color} />);
       const component = getTestComponent('AnimatedComponent');
@@ -104,7 +98,7 @@ describe('With timing animation works with COLORS 🎨', () => {
         EXAMPLE_COLORS.coral[2],
         ComparisonMode.COLOR
       );
-      await wait(1000);
+      await wait(600);
 
       expect(await component.getAnimatedStyle('backgroundColor')).toBe(
         EXAMPLE_COLORS.cornflowerblue[2],
@@ -114,17 +108,99 @@ describe('With timing animation works with COLORS 🎨', () => {
   });
 });
 
-describe('With timing tests', () => {
-  test('test animating width', async () => {
-    await render(<AnimatedComponent />);
-    const component = getTestComponent('AnimatedComponent');
-    await wait(600);
-    expect(await component.getAnimatedStyle('width')).toBe(
-      '100',
-      ComparisonMode.DISTANCE
-    );
-  });
+describe('withTiming animation of WIDTH', () => {
+  const AnimatedWidth = ({
+    startWidth,
+    finalWidth,
+  }: {
+    startWidth: number | `${number}%` | 'auto';
+    finalWidth: number | `${number}%` | 'auto';
+  }) => {
+    const widthSV = useSharedValue(startWidth);
+    const ref = useTestRef('AnimatedWidth');
 
+    const style = useAnimatedStyle(() => {
+      return {
+        width: withTiming(widthSV.value, { duration: 200 }),
+      };
+    });
+
+    useEffect(() => {
+      widthSV.value = finalWidth;
+    }, [widthSV, finalWidth]);
+
+    return (
+      <View style={styles.container}>
+        <Animated.View ref={ref} style={[styles.animatedBox, style]} />
+      </View>
+    );
+  };
+
+  (
+    [
+      {
+        startWidth: 0,
+        finalWidth: 100,
+        finalWidthInPixels: 100,
+        description: 'width in pixels',
+      },
+      {
+        startWidth: '0%',
+        finalWidth: '100%',
+        finalWidthInPixels: Dimensions.get('window').width,
+        description: 'width in percents',
+      },
+      {
+        startWidth: '0%',
+        finalWidth: '75%',
+        finalWidthInPixels: Dimensions.get('window').width * 0.75,
+        description: 'width in percents',
+      },
+      {
+        startWidth: '20%',
+        finalWidth: 100,
+        finalWidthInPixels: 100,
+        description: 'width from percents to pixels',
+      },
+      {
+        startWidth: 20,
+        finalWidth: '40%',
+        finalWidthInPixels: Dimensions.get('window').width * 0.4,
+        description: 'width from pixels to percents',
+      },
+      {
+        startWidth: 'auto',
+        finalWidth: '20%',
+        finalWidthInPixels: Dimensions.get('window').width * 0.2,
+        description: 'width from auto to percents',
+      },
+      {
+        startWidth: 'auto',
+        finalWidth: 20,
+        finalWidthInPixels: 20,
+        description: 'width from auto to pixels',
+      },
+    ] as const
+  ).forEach((testCase) => {
+    const { startWidth, finalWidth, finalWidthInPixels, description } =
+      testCase;
+
+    const fullDescription = `${description}, from ${startWidth} to ${finalWidth}`;
+    test(fullDescription, async () => {
+      await render(
+        <AnimatedWidth startWidth={startWidth} finalWidth={finalWidth} />
+      );
+      const component = getTestComponent('AnimatedWidth');
+      await wait(600);
+      expect(await component.getAnimatedStyle('width')).toBe(
+        finalWidthInPixels,
+        ComparisonMode.DISTANCE
+      );
+    });
+  });
+});
+
+describe('withTiming, test CALLBACKS', () => {
   test('withTiming - test callback of independent withTiming animations', async () => {
     await render(<AnimatedComponent />);
     await wait(600);
@@ -137,4 +213,14 @@ describe('With timing tests', () => {
     expect(getTrackerCallCount('color')).toBeCalledUI(1);
     expect(getTrackerCallCount('color')).toBeCalledJS(0);
   });
+});
+
+const styles = StyleSheet.create({
+  container: { flex: 1, flexDirection: 'column', backgroundColor: 'beige' },
+  animatedBox: {
+    width: 0,
+    opacity: 0,
+    height: 80,
+    margin: 30,
+  },
 });
