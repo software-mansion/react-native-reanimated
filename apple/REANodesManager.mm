9338e96ba3cfa3f96c06c422e3bec768bb1c1db0
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2024-01-03 13:41:47 +0100

    Fix build on macos

--- apple/REANodesManager.mm
+++ apple/REANodesManager.mm
@@ -519,12 +519,16 @@ using namespace facebook::react;
 
 - (NSString *)stringFromColor:(UIColor *)color
 {
-  const size_t totalComponents = CGColorGetNumberOfComponents(color.CGColor);
-  const CGFloat *components = CGColorGetComponents(color.CGColor);
+  if
+#!TARGET_OS_OSX const size_t totalComponents = CGColorGetNumberOfComponents(color.CGColor);
+    const CGFloat *components = CGColorGetComponents(color.CGColor);
   return [NSString stringWithFormat:@"#%02x%02x%02x",
                                     (int)(255 * components[MIN(0, totalComponents - 2)]),
                                     (int)(255 * components[MIN(1, totalComponents - 2)]),
                                     (int)(255 * components[MIN(2, totalComponents - 2)])];
+#else
+return "Cant read color on macos"
+#endif
 }
 
 - (NSString *)obtainProp:(nonnull NSNumber *)viewTag propName:(nonnull NSString *)propName
