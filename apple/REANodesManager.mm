3e7720dbdac2ee5de8eb92ec1eb3c2256f0c53a2
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2024-01-03 14:20:20 +0100

    Fix if clause

--- apple/REANodesManager.mm
+++ apple/REANodesManager.mm
@@ -519,15 +519,15 @@ using namespace facebook::react;
 
 - (NSString *)stringFromColor:(UIColor *)color
 {
-  if
-#!TARGET_OS_OSX const size_t totalComponents = CGColorGetNumberOfComponents(color.CGColor);
-    const CGFloat *components = CGColorGetComponents(color.CGColor);
+#if !TARGET_OS_OSX
+  const size_t totalComponents = CGColorGetNumberOfComponents(color.CGColor);
+  const CGFloat *components = CGColorGetComponents(color.CGColor);
   return [NSString stringWithFormat:@"#%02x%02x%02x",
                                     (int)(255 * components[MIN(0, totalComponents - 2)]),
                                     (int)(255 * components[MIN(1, totalComponents - 2)]),
                                     (int)(255 * components[MIN(2, totalComponents - 2)])];
 #else
-return "Cant read color on macos"
+  return "Cant read color on macos"
 #endif
 }
 
