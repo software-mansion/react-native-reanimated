5a05bcce054214cd51247e6bc82aa22404c76e7e
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2024-01-04 13:52:47 +0100

    Improve TS in src [4/6] - Improve mapper types (#5485)
    <!-- Thanks for submitting a pull request! We appreciate you spending
    the time to work on these changes. Please follow the template so that
    the reviewers can easily understand what the code changes affect. -->
    
    ## Summary
    
    This PR fixes types of Mappers.
    
    ## Test plan
    
    CI
    
    Co-authored by: @marmor157.

--- src/reanimated2/layoutReanimation/animationBuilder/Keyframe.ts
+++ src/reanimated2/layoutReanimation/animationBuilder/Keyframe.ts
@@ -163,9 +163,7 @@ class InnerKeyframe implements IEntryExitAnimationBuilder {
               Object.keys(transformStyle).forEach((transformProp: string) => {
                 addKeyPointWith(
                   makeKeyframeKey(index, transformProp),
-                  transformStyle[
-                    transformProp as keyof typeof transformStyle
-                  ] as number | string
+                  transformStyle[transformProp as keyof typeof transformStyle]
                 );
               });
             });
