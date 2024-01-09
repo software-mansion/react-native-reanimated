552f98c806134153a25f62956a17ea5a1dd05f41
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2024-01-09 18:57:27 +0100

    Change rule

--- eslintPlugin/funcPrefixMatching.js
+++ eslintPlugin/funcPrefixMatching.js
@@ -1,27 +1,51 @@
-const rulePrefix = ['is', 'pre', 'on', 'post', 'get', 'set'];
-const isValidName = (name, { prefix, exclude }) => {
-  const isValid = (prefix) => name.indexOf(prefix) === 0;
-  return exclude.some(isValid) || prefix.some(isValid);
-};
 const onFuncPrefixMatchingCreate = (context) => {
-  const { options } = context;
-  const { include = [], exclude = [] } = options[0] || {};
   return {
-    Identifier: (node) => {
-      if (
-        node.parent.init &&
-        node.parent.init.type === 'ArrowFunctionExpression'
-        // You can add more checks here
-      ) {
-        const { name } = node;
-        const allPrefix = [...include, ...rulePrefix].sort();
-        // Sorting is optional
+    // JSXElement:
+    JSXOpeningElement: (node) => {
+      if (node.name.type === 'JSXMemberExpression') {
+        const expressionIdentifier = node.name.object.name;
+        if (expressionIdentifier === 'Animated') {
+          let containAnimatedProp = false;
+          for (let attribute of node.attributes) {
+            const attributeName = attribute.name.name;
+            if (['entering', 'exiting', 'layout'].includes(attributeName)) {
+              containAnimatedProp = true;
+            }
 
-        if (!isValidName(name, { prefix: allPrefix, exclude })) {
-          context.report(
-            node,
-            `${name} should start with ${allPrefix.join(', ')}.`
-          );
+            if (attributeName === 'style') {
+              const valueExpression = attribute.value.expression;
+              console.log('HERE', valueExpression.type);
+
+              if (valueExpression.type === 'ObjectExpression') {
+                const properties = valueExpression.properties;
+                properties.forEach((property) => {});
+                //inline Props
+              }
+
+              if (valueExpression.type === 'MemberExpression') {
+                console.log(valueExpression);
+                if (valueExpression.object === 'Identifier') {
+                  // Assume that any style, that isn't accessed from stylesheet like styles.container
+                  // is enough to assume that particular element has some animation
+                  containAnimatedProp = true;
+                }
+              }
+              if (valueExpression.type === 'ArrayExpression') {
+                const elements = valueExpression.elements;
+                elements.forEach((element) => {
+                  if (element.type == 'Identifier') {
+                    containAnimatedProp = true;
+                  }
+                });
+              }
+            }
+          }
+          if (!containAnimatedProp) {
+            context.report(
+              node,
+              "This element is animated but doesn't have any actual animation"
+            );
+          }
         }
       }
     },
