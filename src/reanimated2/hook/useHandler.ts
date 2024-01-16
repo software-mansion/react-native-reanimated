software-mansion/react-native-reanimated

commit ddca5694a2440eb3c2c15a66b7f29daf6e9ffcaf

Remove `makeRemote` (#5518)
## Summary

`makeRemote` function seems to be no longer necessary.
Prettifying `useAnimatedStyle` as a bonus.

### Why can `makeRemote` be removed

`makeRemote` utilizes the `ShareableHandle` idea. Let's look at its
code:

```ts
export function makeRemote(initial) {
  const handle = makeShareableCloneRecursive({
    __init: () => {
      'worklet';
      return initial;
    },
  });
  registerShareableMapping(initial, handle);
  return initial;
}
```

The object `handle` defined inside this function has `__init` worklet
property. This worklet has the object `initial` in its closure. When
`makeShareableCloneRecursive` is called with this worklet, `initial's`
value will be copied to the `handle`'s closure. Therefore, calling
`__init` on UI runtime will return the copied value of initial.

So at this point we have a `ShareableObject` `handle` with a worklet
property `__init`. Both this object and its worklet were saved in
`_shareableCache` when `makeShareableCloneRecursive` was called. Current
mappings are:

`_shareableCache[handle]` - `handle`
`_shareableCache[handle.__init]` - `handle.__init`

Now `registerShareableMapping` is called. This function sets a new
mapping for `_shareableCache`. It maps `initial` to `handle`. Therefore
any worklet that has `initial` in its closure will take the mapping for
`initial` instead, which is the `handle`.

Then on the UI thread, when we evaluate worklet that uses `initial`, we
run it through `valueUnpacker`. `valueUnpacker` checks if the object has
`__init` property, if yes, then it return the result of `__init`
worklet. So it returns the copied value of `initial` that was copied
during evaluation of `__init`.

To sum it up:

`makeRemote` is a function that maps a given JS reference to a function
that on UI thread will return the same value. With current
implementation of Shareables this hardly serves any purpose, since we
could just copy the value like a plain JS value, using its copy.

src/reanimated2/hook/useHandler.ts
@@ -1,7 +1,6 @@
 'use strict';
 import { useEffect, useRef } from 'react';
 import type { WorkletFunction } from '../commonTypes';
-import { makeRemote } from '../core';
 import { isWeb, isJest } from '../PlatformChecker';
 import type { DependencyList, ReanimatedEvent } from './commonTypes';
 import { areDependenciesEqual, buildDependencies } from './utils';
@@ -66,7 +65,7 @@ export function useHandler<
   const initRef = useRef<ContextWithDependencies<Context> | null>(null);
   if (initRef.current === null) {
     initRef.current = {
-      context: makeRemote<Context>({} as Context),
+      context: {} as Context,
       savedDependencies: [],
     };
   }

src/createAnimatedComponent/PropsFilter.tsx
@@ -37,7 +37,7 @@ export class PropsFilter implements IPropsFilter {
         const processedStyle: StyleProps = styles.map((style) => {
           if (style && style.viewDescriptors) {
             // this is how we recognize styles returned by useAnimatedStyle
-            style.viewsRef.add(component);
+            style.viewsRef?.add(component);
             if (component._isFirstRender) {
               this._initialStyle = {
                 ...style.initial.value,

src/createAnimatedComponent/createAnimatedComponent.tsx
@@ -244,9 +244,7 @@ export function createAnimatedComponent(
     _detachStyles() {
       if (IS_WEB && this._styles !== null) {
         for (const style of this._styles) {
-          if (style?.viewsRef) {
-            style.viewsRef.remove(this);
-          }
+          style.viewsRef.delete(this);
         }
       } else if (this._viewTag !== -1 && this._styles !== null) {
         for (const style of this._styles) {

src/reanimated2/ViewDescriptorsSet.ts
@@ -3,6 +3,7 @@ import { useRef } from 'react';
 import { makeMutable } from './core';
 import type { SharedValue } from './commonTypes';
 import type { Descriptor } from './hook/commonTypes';
+import { shouldBeUseWeb } from './PlatformChecker';
 
 export interface ViewRefSet<T> {
   items: Set<T>;
@@ -51,7 +52,18 @@ export function makeViewDescriptorsSet(): ViewDescriptorsSet {
   return data;
 }
 
-export function useViewRefSet<T>(): ViewRefSet<T> {
+const SHOULD_BE_USE_WEB = shouldBeUseWeb();
+
+export const useViewRefSet = SHOULD_BE_USE_WEB
+  ? useViewRefSetJS
+  : useViewRefSetNative;
+
+function useViewRefSetNative() {
+  // Stub native implementation.
+  return undefined;
+}
+
+function useViewRefSetJS<T>(): ViewRefSet<T> {
   const ref = useRef<ViewRefSet<T> | null>(null);
   if (ref.current === null) {
     const data: ViewRefSet<T> = {

src/reanimated2/core.ts
@@ -31,7 +31,7 @@ export { runOnJS, runOnUI } from './threads';
 export { createWorkletRuntime, runOnRuntime } from './runtimes';
 export type { WorkletRuntime } from './runtimes';
 export { makeShareable, makeShareableCloneRecursive } from './shareables';
-export { makeMutable, makeRemote } from './mutables';
+export { makeMutable } from './mutables';
 
 const IS_FABRIC = isFabric();
 

src/reanimated2/hook/commonTypes.ts
@@ -81,7 +81,10 @@ export interface AnimatedStyleHandle<
     value: AnimatedStyle<Style>;
     updater: () => AnimatedStyle<Style>;
   };
-  viewsRef: ViewRefSet<unknown>;
+  /**
+   * @remarks `viewsRef` is only defined in Web implementation.
+   */
+  viewsRef: ViewRefSet<unknown> | undefined;
 }
 
 export interface JestAnimatedStyleHandle<

src/reanimated2/hook/useAnimatedStyle.ts
@@ -2,7 +2,7 @@
 import type { MutableRefObject } from 'react';
 import { useEffect, useRef } from 'react';
 
-import { startMapper, stopMapper, makeRemote } from '../core';
+import { startMapper, stopMapper } from '../core';
 import updateProps, { updatePropsJestWrapper } from '../UpdateProps';
 import { initialUpdaterRun } from '../animation';
 import { useSharedValue } from './useSharedValue';
@@ -43,7 +43,7 @@ interface AnimatedState {
   isAnimationCancelled: boolean;
 }
 
-interface AnimationRef {
+interface AnimatedUpdaterData {
   initial: {
     value: AnimatedStyle<any>;
     updater: () => AnimatedStyle<any>;
@@ -418,17 +418,17 @@ export function useAnimatedStyle<Style extends DefaultStyle>(
   adapters?: WorkletFunction | WorkletFunction[],
   isAnimatedProps = false
 ): AnimatedStyleHandle<Style> | JestAnimatedStyleHandle<Style> {
-  const viewsRef: ViewRefSet<unknown> = useViewRefSet();
-  const initRef = useRef<AnimationRef>();
+  const viewsRef: ViewRefSet<unknown> | undefined = useViewRefSet();
+  const animatedUpdaterData = useRef<AnimatedUpdaterData>();
   let inputs = Object.values(updater.__closure ?? {});
   if (SHOULD_BE_USE_WEB) {
     if (!inputs.length && dependencies?.length) {
-      // let web work without a Babel/SWC plugin
+      // let web work without a Babel plugin
       inputs = dependencies;
     }
     if (__DEV__ && !inputs.length && !dependencies && !updater.__workletHash) {
       throw new Error(
-        `[Reanimated] \`useAnimatedStyle\` was used without a dependency array or Babel plugin. Please explicitly pass a dependency array, or enable the Babel/SWC plugin.
+        `[Reanimated] \`useAnimatedStyle\` was used without a dependency array or Babel plugin. Please explicitly pass a dependency array, or enable the Babel plugin.
 For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/docs/guides/web-support#web-without-the-babel-plugin\`.`
       );
     }
@@ -439,8 +439,8 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
       : [adapters]
     : [];
   const adaptersHash = adapters ? buildWorkletsHash(adaptersArray) : null;
-  const animationsActive = useSharedValue<boolean>(true);
-  const jestAnimatedStyle: MutableRefObject<Style> = useRef<Style>({} as Style);
+  const areAnimationsActive = useSharedValue<boolean>(true);
+  const jestAnimatedStyle = useRef<Style>({} as Style);
 
   // build dependencies
   if (!dependencies) {
@@ -450,27 +450,28 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
   }
   adaptersHash && dependencies.push(adaptersHash);
 
-  if (!initRef.current) {
+  if (!animatedUpdaterData.current) {
     const initialStyle = initialUpdaterRun(updater);
-    validateAnimatedStyles(initialStyle);
-    initRef.current = {
+    if (__DEV__) {
+      validateAnimatedStyles(initialStyle);
+    }
+    animatedUpdaterData.current = {
       initial: {
         value: initialStyle,
-        updater: updater,
+        updater,
       },
-      remoteState: makeRemote<AnimatedState>({
+      remoteState: {
         last: initialStyle,
         animations: {},
         isAnimationCancelled: false,
         isAnimationRunning: false,
-      }),
+      },
       viewDescriptors: makeViewDescriptorsSet(),
     };
   }
 
-  const { initial, remoteState, viewDescriptors } = initRef.current;
+  const { initial, remoteState, viewDescriptors } = animatedUpdaterData.current;
   const shareableViewDescriptors = viewDescriptors.shareableViewDescriptors;
-  const maybeViewRef = SHOULD_BE_USE_WEB ? viewsRef : undefined;
 
   dependencies.push(shareableViewDescriptors);
 
@@ -495,8 +496,8 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
           shareableViewDescriptors,
           updater,
           remoteState,
-          maybeViewRef,
-          animationsActive,
+          viewsRef,
+          areAnimationsActive,
           jestAnimatedStyle,
           adaptersArray
         );
@@ -508,8 +509,8 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
           shareableViewDescriptors,
           updaterFn,
           remoteState,
-          maybeViewRef,
-          animationsActive,
+          viewsRef,
+          areAnimationsActive,
           isAnimatedProps
         );
       };
@@ -518,14 +519,15 @@ For more, see the docs: \`https://docs.swmansion.com/react-native-reanimated/doc
     return () => {
       stopMapper(mapperId);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, dependencies);
 
   useEffect(() => {
-    animationsActive.value = true;
+    areAnimationsActive.value = true;
     return () => {
-      animationsActive.value = false;
+      areAnimationsActive.value = false;
     };
-  }, []);
+  }, [areAnimationsActive]);
 
   checkSharedValueUsage(initial.value);
 

src/reanimated2/mutables.ts
@@ -163,14 +163,3 @@ export function makeMutable<Value>(
   shareableMappingCache.set(mutable, handle);
   return mutable;
 }
-
-export function makeRemote<T extends object>(initial: T = {} as T): T {
-  const handle = makeShareableCloneRecursive({
-    __init: () => {
-      'worklet';
-      return initial;
-    },
-  });
-  shareableMappingCache.set(initial, handle);
-  return initial;
-}

commit 44d0286be35b09c69bb5def9f11fd5a4209dc0d7

Fix `useHandler` context issue (#5586)
## Summary

This PR fixes a regression introduced in #5518. `useHandler` works by
maintaining a `context` object that can be accessed and modified in
event handlers (e.g. within `useAnimatedGestureHandler`). Removing
`makeRemote` call there made it so that any changes to the `context`
object were not visible in event handlers, rendering the `context`
object useless. In this PR I wrapped the context object with
`makeShareable`, which works basically in the same way as `makeRemote`
did.

## Test plan

Check whether the `[SET] Modals` example behaves properly (i.e. if the
modal closing gestures are captured correctly).

src/reanimated2/hook/useHandler.ts
@@ -4,6 +4,7 @@ import type { WorkletFunction } from '../commonTypes';
 import { isWeb, isJest } from '../PlatformChecker';
 import type { DependencyList, ReanimatedEvent } from './commonTypes';
 import { areDependenciesEqual, buildDependencies } from './utils';
+import { makeShareable } from '../shareables';
 
 interface GeneralHandler<
   Event extends object,
@@ -64,8 +65,9 @@ export function useHandler<
 ): UseHandlerContext<Context> {
   const initRef = useRef<ContextWithDependencies<Context> | null>(null);
   if (initRef.current === null) {
+    const context = makeShareable({} as Context);
     initRef.current = {
-      context: {} as Context,
+      context,
       savedDependencies: [],
     };
   }