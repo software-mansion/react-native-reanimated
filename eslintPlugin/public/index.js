ec548605a3c7f30b1a130057727d29af91380cab
Aleksandra Cynk <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2024-02-02 10:22:59 +0100

    Use esbuild

--- eslintPlugin/public/index.js
+++ eslintPlugin/public/index.js
@@ -1,7 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.rules = void 0;
-var noAnimatedStyleToNonAnimatedComponent_1 = require("./noAnimatedStyleToNonAnimatedComponent");
+const noAnimatedStyleToNonAnimatedComponent_1 = __importDefault(
+  require('./noAnimatedStyleToNonAnimatedComponent')
+);
 exports.rules = {
-    'animated-style-non-animated-component': noAnimatedStyleToNonAnimatedComponent_1.default,
+  'animated-style-non-animated-component':
+    noAnimatedStyleToNonAnimatedComponent_1.default,
 };
--- eslintPlugin/public/noAnimatedStyleToNonAnimatedComponent.js
+++ eslintPlugin/public/noAnimatedStyleToNonAnimatedComponent.js
@@ -1,30 +1,25 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-var utils_1 = require('@typescript-eslint/utils');
-var rule = {
+const utils_1 = require('@typescript-eslint/utils');
+const rule = {
   create: function (context) {
     return {
-      JSXOpeningElement: function (node) {
-        var _a;
+      JSXOpeningElement(node) {
         if (node.name.type === utils_1.AST_NODE_TYPES.JSXMemberExpression)
           return; // Property-like namespace syntax <Animated.View>
         if (node.name.type === utils_1.AST_NODE_TYPES.JSXNamespacedName) return; // XML-based namespace syntax: <Animated:View>
-        var sourceCode = context.getSourceCode();
-        var tokensBefore = sourceCode.getTokensBefore(node);
-        var componentName =
-          (_a = node === null || node === void 0 ? void 0 : node.name) ===
-            null || _a === void 0
-            ? void 0
-            : _a.name;
+        const sourceCode = context.getSourceCode();
+        const tokensBefore = sourceCode.getTokensBefore(node);
+        const componentName = node?.name?.name;
         function isVariableDefinedAs(variableName, expectedToken) {
-          var isAnimated = false;
-          var variableNameTokenIds = [];
-          tokensBefore.forEach(function (token, idx) {
+          let isAnimated = false;
+          const variableNameTokenIds = [];
+          tokensBefore.forEach((token, idx) => {
             if (token.value === variableName) {
               variableNameTokenIds.push(idx);
             }
           });
-          variableNameTokenIds.forEach(function (idx) {
+          variableNameTokenIds.forEach((idx) => {
             if (tokensBefore[idx + 2].value === expectedToken) {
               isAnimated = true;
             }
@@ -32,62 +27,47 @@ var rule = {
           return isAnimated;
         }
         function checkIdentifierNode(styleExpression) {
-          var variableName = styleExpression.name;
-          var isAnimatedStyle = isVariableDefinedAs(
+          const variableName = styleExpression.name;
+          const isAnimatedStyle = isVariableDefinedAs(
             variableName,
             'useAnimatedStyle'
           );
           if (isAnimatedStyle) {
             context.report({
-              node: node,
+              node,
               messageId: 'animatedStyle',
-              data: {
-                componentName: componentName,
-                variableName: variableName,
-              },
+              data: { componentName, variableName },
             });
           }
         }
         function checkObjectNode(styleExpression) {
-          var properties = styleExpression.properties;
-          properties.forEach(function (property) {
+          const properties = styleExpression.properties;
+          properties.forEach((property) => {
             if (property.type === utils_1.AST_NODE_TYPES.SpreadElement) {
               return; //Ignore spread elements
             }
             if (property.value.type === 'Identifier') {
-              var variableName = property.value.name;
+              const variableName = property.value.name;
               if (isVariableDefinedAs(variableName, 'useSharedValue')) {
-                var propertyName =
+                const propertyName =
                   'name' in property.key ? property.key.name : variableName;
                 context.report({
-                  node: node,
+                  node,
                   messageId: 'sharedValue',
-                  data: {
-                    propertyName: propertyName,
-                    propertyValue: variableName,
-                  },
+                  data: { propertyName, propertyValue: variableName },
                 });
               }
             }
           });
         }
         function checkArrayNode(styleExpression) {
-          var arrayNodes = styleExpression.elements;
-          arrayNodes.forEach(function (node) {
-            if (
-              (node === null || node === void 0 ? void 0 : node.type) ===
-              'Identifier'
-            ) {
+          const arrayNodes = styleExpression.elements;
+          arrayNodes.forEach((node) => {
+            if (node?.type === 'Identifier') {
               checkIdentifierNode(node);
-            } else if (
-              (node === null || node === void 0 ? void 0 : node.type) ===
-              'ArrayExpression'
-            ) {
+            } else if (node?.type === 'ArrayExpression') {
               checkArrayNode(node);
-            } else if (
-              (node === null || node === void 0 ? void 0 : node.type) ===
-              'ObjectExpression'
-            ) {
+            } else if (node?.type === 'ObjectExpression') {
               checkObjectNode(node);
             }
           });
@@ -98,8 +78,8 @@ var rule = {
         ) {
           return;
         }
-        var styleAttribute = node.attributes
-          .map(function (attribute) {
+        const styleAttribute = node.attributes
+          .map((attribute) => {
             return attribute.type === utils_1.AST_NODE_TYPES.JSXAttribute &&
               attribute.name.name === 'style'
               ? [attribute]
@@ -109,7 +89,7 @@ var rule = {
         if (styleAttribute.length == 0) {
           return;
         }
-        var styleValue = styleAttribute[0].value; // assume no duplicate props
+        const styleValue = styleAttribute[0].value; // assume no duplicate props
         if (
           styleValue === null ||
           styleValue.type === utils_1.AST_NODE_TYPES.Literal
@@ -119,7 +99,7 @@ var rule = {
         if (styleValue.type === utils_1.AST_NODE_TYPES.JSXSpreadChild) {
           return; //TODO Ignore this for now
         }
-        var styleExpression = styleValue.expression;
+        const styleExpression = styleValue.expression;
         switch (styleExpression.type) {
           case utils_1.AST_NODE_TYPES.Identifier: // style={myVariable}
             checkIdentifierNode(styleExpression);
@@ -139,7 +119,7 @@ var rule = {
   },
   meta: {
     docs: {
-      recommended: 'error',
+      recommended: 'recommended',
       description: 'Avoid looping over enums.',
     },
     messages: {
