bf822bdaa30fb19c514c0adca0bbaa4e77e9f075
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-02-08 11:34:54 +0100

    feat: add visionos support (#5627)
    ## Summary
    
    This PR adds `visionos` support. I've refactored call sites to
    `UIApplication.sharedApplication.keyWindow` (it's unavailable on
    visionOS) for `RCTKeyWindow()` its a React Native core utility for
    retrieving key window. (In one of my recent PRs it got support for
    multi-window scenarios:
    https://github.com/facebook/react-native/pull/42036)
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/52801365/8b535f78-c91c-4a7c-b956-29c6985d157a
    
    ## Test plan
    
    1. Init new visionOS app `npx @callstack/react-native-visionos@latest
    init MyApp`
    2. Install Reanimated from GitHub commit or link it locally: 
    
    `"react-native-reanimated":
    "git@github.com:okwasniewski/react-native-reanimated.git#feat/visionos",`
    
    ---------
    
    Co-authored-by: Tomek Zawadzki <tomekzawadzki98@gmail.com>
    Co-authored-by: Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>

--- apple/LayoutReanimation/REAAnimationsManager.m
+++ apple/LayoutReanimation/REAAnimationsManager.m
@@ -250,7 +250,11 @@ BOOL REANodeFind(id<RCTComponent> view, int (^block)(id<RCTComponent>))
   if (needsViewPositionUpdate) {
     CGPoint newCenter = CGPointMake(centerX, centerY);
     if (convertFromAbsolute) {
+#if TARGET_OS_OSX
       REAUIView *window = UIApplication.sharedApplication.keyWindow;
+#else
+      REAUIView *window = RCTKeyWindow();
+#endif
       CGPoint convertedCenter = [window convertPoint:newCenter toView:view.superview];
       view.center = convertedCenter;
     } else {
