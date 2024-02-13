d318158f0b0b28f252e13e73a8edc7f5314e98a3
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-02-13 19:58:06 +0100

    init

--- app/src/examples/WithoutBabelPluginExample.tsx
+++ app/src/examples/WithoutBabelPluginExample.tsx
@@ -14,9 +14,10 @@ import { StyleSheet, Text, View } from 'react-native';
 enableExperimentalWebImplementation(true);
 
 function isBabelPluginEnabled() {
-  'inject Reanimated Babel plugin version';
-  // @ts-ignore
-  return global._REANIMATED_VERSION_BABEL_PLUGIN !== undefined;
+  function worklet() {
+    'worklet';
+  }
+  return '__workletHash' in worklet;
 }
 
 export default function WithoutBabelPluginExample() {
