e1712e50d577bae95329f58d06a886270a3c0636
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-02-14 10:08:42 +0100

    Refactor keyboard

--- android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardAnimationCallback.java
+++ android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardAnimationCallback.java
@@ -3,61 +3,42 @@ package com.swmansion.reanimated.keyboard;
 import androidx.annotation.NonNull;
 import androidx.core.view.WindowInsetsAnimationCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import java.util.List;
 
 public class KeyboardAnimationCallback extends WindowInsetsAnimationCompat.Callback {
-  private final Keyboard mKeyboard;
-  private final NotifyAboutKeyboardChangeFunction mNotifyAboutKeyboardChange;
-  private static final int CONTENT_TYPE_MASK = WindowInsetsCompat.Type.ime();
+    private final Keyboard keyboard;
+    private final NotifyAboutKeyboardChangeFunction notifyAboutKeyboardChange;
 
-  public KeyboardAnimationCallback(
-      Keyboard keyboard, NotifyAboutKeyboardChangeFunction notifyAboutKeyboardChange) {
-    super(WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE);
-    mNotifyAboutKeyboardChange = notifyAboutKeyboardChange;
-    mKeyboard = keyboard;
-  }
-
-  @NonNull
-  @Override
-  public WindowInsetsAnimationCompat.BoundsCompat onStart(
-      @NonNull WindowInsetsAnimationCompat animation,
-      @NonNull WindowInsetsAnimationCompat.BoundsCompat bounds) {
-    if (!isKeyboardAnimation(animation)) {
-      return bounds;
+    public KeyboardAnimationCallback(Keyboard keyboard, NotifyAboutKeyboardChangeFunction notifyAboutKeyboardChange) {
+        super(WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE);
+        this.notifyAboutKeyboardChange = notifyAboutKeyboardChange;
+        this.keyboard = keyboard;
     }
-    mKeyboard.onAnimationStart();
-    mNotifyAboutKeyboardChange.call();
-    return super.onStart(animation, bounds);
-  }
 
-  @NonNull
-  @Override
-  public WindowInsetsCompat onProgress(
-      @NonNull WindowInsetsCompat insets,
-      @NonNull List<WindowInsetsAnimationCompat> runningAnimations) {
-    boolean isAnyKeyboardAnimationRunning = false;
-    for (WindowInsetsAnimationCompat animation : runningAnimations) {
-      if (isKeyboardAnimation(animation)) {
-        isAnyKeyboardAnimationRunning = true;
-        break;
-      }
+    @NonNull
+    @Override
+    public WindowInsetsAnimationCompat.BoundsCompat onStart(
+            @NonNull WindowInsetsAnimationCompat animation,
+            @NonNull WindowInsetsAnimationCompat.BoundsCompat bounds) {
+        keyboard.onAnimationStart();
+        notifyAboutKeyboardChange.call();
+        return super.onStart(animation, bounds);
     }
-    if (isAnyKeyboardAnimationRunning) {
-      mKeyboard.updateHeight(insets);
-      mNotifyAboutKeyboardChange.call();
-    }
-    return insets;
-  }
 
-  @Override
-  public void onEnd(@NonNull WindowInsetsAnimationCompat animation) {
-    if (isKeyboardAnimation(animation)) {
-      mKeyboard.onAnimationEnd();
-      mNotifyAboutKeyboardChange.call();
+    @NonNull
+    @Override
+    public WindowInsetsCompat onProgress(
+            @NonNull WindowInsetsCompat insets,
+            @NonNull List<WindowInsetsAnimationCompat> runningAnimations) {
+        keyboard.updateHeight(insets);
+        notifyAboutKeyboardChange.call();
+        return insets;
     }
-  }
 
-  private static boolean isKeyboardAnimation(@NonNull WindowInsetsAnimationCompat animation) {
-    return (animation.getTypeMask() & CONTENT_TYPE_MASK) != 0;
-  }
+    @Override
+    public void onEnd(@NonNull WindowInsetsAnimationCompat animation) {
+        keyboard.onAnimationEnd();
+        notifyAboutKeyboardChange.call();
+    }
 }
