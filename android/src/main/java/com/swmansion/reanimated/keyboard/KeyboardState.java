e78ad2f97da38d3b1e9d27b52d572d1ca50f7d98
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-02-14 10:25:16 +0100

    Java lint

--- android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardState.java
+++ android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardState.java
@@ -1,19 +1,19 @@
 package com.swmansion.reanimated.keyboard;
 
 public enum KeyboardState {
-    UNKNOWN(0),
-    OPENING(1),
-    OPEN(2),
-    CLOSING(3),
-    CLOSED(4);
+  UNKNOWN(0),
+  OPENING(1),
+  OPEN(2),
+  CLOSING(3),
+  CLOSED(4);
 
-    private final int value;
+  private final int value;
 
-    KeyboardState(int value) {
-        this.value = value;
-    }
+  KeyboardState(int value) {
+    this.value = value;
+  }
 
-    public int asInt() {
-        return value;
-    }
+  public int asInt() {
+    return value;
+  }
 }
