395e1a933e72f952bd1ad6fccf763fa6daf08791
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-02-20 16:44:59 +0100

    init

--- app/src/examples/LettersExample.tsx
+++ app/src/examples/LettersExample.tsx
@@ -18,16 +18,19 @@ interface LettersProps {
 function Letters({ text }: LettersProps) {
   return (
     <Animated.View style={styles.line}>
-      {splitLetters(text).map(({ char, key }, index) => (
-        <Animated.Text
-          key={key}
-          style={styles.text}
-          entering={FadeIn.duration(500).delay(index)}
-          exiting={FadeOut.duration(500).delay(index)}
-          layout={Layout.duration(Math.random() * 700 + 200).delay(index)}>
-          {char}
-        </Animated.Text>
-      ))}
+      {splitLetters(text).map(({ char, key }, index) => {
+        console.log(index);
+        return (
+          <Animated.Text
+            key={key}
+            style={styles.text}
+            entering={FadeIn.duration(500).delay(index)}
+            exiting={FadeOut.duration(500).delay(index)}
+            layout={Layout.duration(Math.random() * 700 + 200).delay(index)}>
+            {char}
+          </Animated.Text>
+        );
+      })}
     </Animated.View>
   );
 }
@@ -44,7 +47,7 @@ export default function LettersExample() {
   useEffect(() => {
     const id = setInterval(() => {
       setState((s) => !s);
-    }, 2000);
+    }, 3500);
     return () => clearInterval(id);
   }, []);
 
