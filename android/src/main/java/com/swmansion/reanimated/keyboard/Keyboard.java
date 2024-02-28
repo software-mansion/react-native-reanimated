16093e713f961d4eac804bf13e7ab7177f88d3c0
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-02-28 17:18:39 +0100

    Add m prefix

--- android/src/main/java/com/swmansion/reanimated/keyboard/Keyboard.java
+++ android/src/main/java/com/swmansion/reanimated/keyboard/Keyboard.java
@@ -4,31 +4,31 @@ import androidx.core.view.WindowInsetsCompat;
 import com.facebook.react.uimanager.PixelUtil;
 
 public class Keyboard {
-  private KeyboardState state;
-  private int height = 0;
+  private KeyboardState mState;
+  private int mHeight = 0;
   private static final int CONTENT_TYPE_MASK = WindowInsetsCompat.Type.ime();
   private static final int SYSTEM_BAR_TYPE_MASK = WindowInsetsCompat.Type.systemBars();
 
   public KeyboardState getState() {
-    return state;
+    return mState;
   }
 
   public int getHeight() {
-    return height;
+    return mHeight;
   }
 
   public void updateHeight(WindowInsetsCompat insets) {
     int contentBottomInset = insets.getInsets(CONTENT_TYPE_MASK).bottom;
     int systemBarBottomInset = insets.getInsets(SYSTEM_BAR_TYPE_MASK).bottom;
     int keyboardHeightDip = contentBottomInset - systemBarBottomInset;
-    height = (int) PixelUtil.toDIPFromPixel(Math.max(0, keyboardHeightDip));
+    mHeight = (int) PixelUtil.toDIPFromPixel(Math.max(0, keyboardHeightDip));
   }
 
   public void onAnimationStart() {
-    state = height == 0 ? KeyboardState.OPENING : KeyboardState.CLOSING;
+    mState = mHeight == 0 ? KeyboardState.OPENING : KeyboardState.CLOSING;
   }
 
   public void onAnimationEnd() {
-    state = height == 0 ? KeyboardState.CLOSED : KeyboardState.OPEN;
+    mState = mHeight == 0 ? KeyboardState.CLOSED : KeyboardState.OPEN;
   }
 }
--- android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardAnimationManager.java
+++ android/src/main/java/com/swmansion/reanimated/keyboard/KeyboardAnimationManager.java
@@ -10,38 +10,38 @@ interface NotifyAboutKeyboardChangeFunction {
 }
 
 public class KeyboardAnimationManager {
-  private int nextListenerId = 0;
-  private final HashMap<Integer, KeyboardWorkletWrapper> listeners = new HashMap<>();
-  private final Keyboard keyboard = new Keyboard();
+  private int mNextListenerId = 0;
+  private final HashMap<Integer, KeyboardWorkletWrapper> mListeners = new HashMap<>();
+  private final Keyboard mKeyboard = new Keyboard();
   private final WindowsInsetsManager windowsInsetsManager;
 
   public KeyboardAnimationManager(WeakReference<ReactApplicationContext> reactContext) {
     windowsInsetsManager =
-        new WindowsInsetsManager(reactContext, keyboard, this::notifyAboutKeyboardChange);
+        new WindowsInsetsManager(reactContext, mKeyboard, this::notifyAboutKeyboardChange);
   }
 
   public int subscribeForKeyboardUpdates(
       KeyboardWorkletWrapper callback, boolean isStatusBarTranslucent) {
-    int listenerId = nextListenerId++;
-    if (listeners.isEmpty()) {
+    int listenerId = mNextListenerId++;
+    if (mListeners.isEmpty()) {
       KeyboardAnimationCallback keyboardAnimationCallback =
-          new KeyboardAnimationCallback(keyboard, this::notifyAboutKeyboardChange);
+          new KeyboardAnimationCallback(mKeyboard, this::notifyAboutKeyboardChange);
       windowsInsetsManager.startObservingChanges(keyboardAnimationCallback, isStatusBarTranslucent);
     }
-    listeners.put(listenerId, callback);
+    mListeners.put(listenerId, callback);
     return listenerId;
   }
 
   public void unsubscribeFromKeyboardUpdates(int listenerId) {
-    listeners.remove(listenerId);
-    if (listeners.isEmpty()) {
+    mListeners.remove(listenerId);
+    if (mListeners.isEmpty()) {
       windowsInsetsManager.stopObservingChanges();
     }
   }
 
   public void notifyAboutKeyboardChange() {
-    for (KeyboardWorkletWrapper listener : listeners.values()) {
-      listener.invoke(keyboard.getState().asInt(), keyboard.getHeight());
+    for (KeyboardWorkletWrapper listener : mListeners.values()) {
+      listener.invoke(mKeyboard.getState().asInt(), mKeyboard.getHeight());
     }
   }
 }
