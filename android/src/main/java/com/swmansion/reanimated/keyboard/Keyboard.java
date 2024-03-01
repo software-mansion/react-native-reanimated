7b508cd60ecbf9e96ca7bd8c9e60f7a4737034e2
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-03-01 11:58:28 +0100

    [useAnimatedKeyboard][Android] Fix state after animation dismiss (#5741)
    ## Summary
    
    Split of
    https://github.com/software-mansion/react-native-reanimated/pull/5666
    
    It fixes the issue where the state was incorrect if a keyboard
    transition was canceled.

--- android/src/main/java/com/swmansion/reanimated/keyboard/Keyboard.java
+++ android/src/main/java/com/swmansion/reanimated/keyboard/Keyboard.java
@@ -6,6 +6,7 @@ import com.facebook.react.uimanager.PixelUtil;
 public class Keyboard {
   private KeyboardState mState;
   private int mHeight = 0;
+  private int mActiveTransitionCounter = 0;
   private static final int CONTENT_TYPE_MASK = WindowInsetsCompat.Type.ime();
   private static final int SYSTEM_BAR_TYPE_MASK = WindowInsetsCompat.Type.systemBars();
 
@@ -20,15 +21,31 @@ public class Keyboard {
   public void updateHeight(WindowInsetsCompat insets) {
     int contentBottomInset = insets.getInsets(CONTENT_TYPE_MASK).bottom;
     int systemBarBottomInset = insets.getInsets(SYSTEM_BAR_TYPE_MASK).bottom;
-    int keyboardHeightDip = contentBottomInset - systemBarBottomInset;
-    mHeight = (int) PixelUtil.toDIPFromPixel(Math.max(0, keyboardHeightDip));
+    int keyboardHeightDip = Math.max(0, contentBottomInset - systemBarBottomInset);
+    if (keyboardHeightDip == 0 && mState == KeyboardState.OPEN) {
+      /*
+      When the keyboard is being canceling, for one frame the insets show a keyboard height of 0,
+      causing a jump of the keyboard. We can avoid it by ignoring that frame and calling
+      the listeners on the following frame.
+      */
+      return;
+    }
+    mHeight = (int) PixelUtil.toDIPFromPixel(keyboardHeightDip);
   }
 
   public void onAnimationStart() {
-    mState = mHeight == 0 ? KeyboardState.OPENING : KeyboardState.CLOSING;
+    if (mActiveTransitionCounter > 0) {
+      mState = mState == KeyboardState.OPENING ? KeyboardState.CLOSING : KeyboardState.OPENING;
+    } else {
+      mState = mHeight == 0 ? KeyboardState.OPENING : KeyboardState.CLOSING;
+    }
+    mActiveTransitionCounter++;
   }
 
   public void onAnimationEnd() {
-    mState = mHeight == 0 ? KeyboardState.CLOSED : KeyboardState.OPEN;
+    mActiveTransitionCounter--;
+    if (mActiveTransitionCounter == 0) {
+      mState = mHeight == 0 ? KeyboardState.CLOSED : KeyboardState.OPEN;
+    }
   }
 }
