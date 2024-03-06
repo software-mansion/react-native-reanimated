software-mansion/react-native-reanimated

commit d80936495f51f9884e25c3f16d0fa2983929138a

Introduce executeOnUIRuntimeSync and use it to replace Sync Data Holder (#4300)
## Summary

This PR introduces a new concept of executing worklets on the UI runtime
synchronously from the RN JS runtime.

It is implemented using runtime decorator that uses a lock to guard all
the code that's being executed on that runtime. Thanks to this locking
mechanism, we can now grab that lock on a different thread and safely
execute some more methods without worrying that the runtime object will
be accessed concurrently.

This new method makes opens up some new possibilities. The first one
being the way we handle two-way shared values. The shared value rewrite
introduced a concept of one-way shared values that are only accessed and
modified from the ui runtime. We initially thought such values can be
used in majority of the use cases, however, in practice it turned out
that a lot of existing code already relies on reading from shared values
on the RN thread. Despite the reads happening very infrequently (just a
few times per shared value runtime), we still needed to serialize all
the updates for a given shared value such that it can be read later on
from the RN thread. With this change, we can leverage synchronous
execution of the code on UI runtime and ask it to read the value on
demand rather than synchronizing the value after each single update.


## Test plan

---------

Co-authored-by: Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
Co-authored-by: Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>

Common/cpp/SharedItems/Shareables.cpp
@@ -110,23 +110,6 @@ jsi::Value makeShareableClone(
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
-void updateDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef,
-    const jsi::Value &newData) {
-  auto dataHolder = extractShareableOrThrow<ShareableSynchronizedDataHolder>(
-      rt, synchronizedDataHolderRef);
-  dataHolder->set(rt, newData);
-}
-
-jsi::Value getDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef) {
-  auto dataHolder = extractShareableOrThrow<ShareableSynchronizedDataHolder>(
-      rt, synchronizedDataHolderRef);
-  return dataHolder->get(rt);
-}
-
 std::shared_ptr<Shareable> extractShareableOrThrow(
     jsi::Runtime &rt,
     const jsi::Value &maybeShareableValue,
@@ -271,42 +254,6 @@ jsi::Value ShareableHandle::toJSValue(jsi::Runtime &rt) {
     // method should be called at most once
   }
   return jsi::Value(rt, *remoteValue_);
-}
-
-jsi::Value ShareableSynchronizedDataHolder::get(jsi::Runtime &rt) {
-  std::unique_lock<std::mutex> read_lock(dataAccessMutex_);
-  if (&rt == primaryRuntime_) {
-    if (primaryValue_ != nullptr) {
-      return jsi::Value(rt, *primaryValue_);
-    }
-    auto value = data_->getJSValue(rt);
-    primaryValue_ = std::make_unique<jsi::Value>(rt, value);
-    return value;
-  }
-  if (secondaryValue_ == nullptr) {
-    auto value = data_->getJSValue(rt);
-    secondaryValue_ = std::make_unique<jsi::Value>(rt, value);
-    secondaryRuntime_ = &rt;
-    return value;
-  }
-  if (&rt == secondaryRuntime_) {
-    return jsi::Value(rt, *secondaryValue_);
-  }
-  throw std::runtime_error(
-      "[Reanimated] ShareableSynchronizedDataHolder supports only RN or UI runtime");
-}
-
-void ShareableSynchronizedDataHolder::set(
-    jsi::Runtime &rt,
-    const jsi::Value &data) {
-  std::unique_lock<std::mutex> write_lock(dataAccessMutex_);
-  data_ = extractShareableOrThrow(rt, data);
-  primaryValue_.reset();
-  secondaryValue_.reset();
-}
-
-jsi::Value ShareableSynchronizedDataHolder::toJSValue(jsi::Runtime &rt) {
-  return ShareableJSRef::newHostObject(rt, shared_from_this());
 }
 
 jsi::Value ShareableString::toJSValue(jsi::Runtime &rt) {

Common/cpp/SharedItems/Shareables.h
@@ -20,7 +20,7 @@ jsi::Function getCallGuard(jsi::Runtime &rt);
 
 // If possible, please use `WorkletRuntime::runGuarded` instead.
 template <typename... Args>
-inline void runOnRuntimeGuarded(
+inline jsi::Value runOnRuntimeGuarded(
     jsi::Runtime &rt,
     const jsi::Value &function,
     Args &&...args) {
@@ -29,9 +29,9 @@ inline void runOnRuntimeGuarded(
   // JavaScript and propagating them to the main React Native thread such that
   // they can be presented using RN's LogBox.
 #ifndef NDEBUG
-  getCallGuard(rt).call(rt, function, args...);
+  return getCallGuard(rt).call(rt, function, args...);
 #else
-  function.asObject(rt).asFunction(rt).call(rt, args...);
+  return function.asObject(rt).asFunction(rt).call(rt, args...);
 #endif
 }
 
@@ -81,7 +81,6 @@ class Shareable {
     WorkletType,
     RemoteFunctionType,
     HandleType,
-    SynchronizedDataHolder,
     HostObjectType,
     HostFunctionType,
     ArrayBufferType,
@@ -148,15 +147,6 @@ jsi::Value makeShareableClone(
     jsi::Runtime &rt,
     const jsi::Value &value,
     const jsi::Value &shouldRetainRemote);
-
-void updateDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef,
-    const jsi::Value &newData);
-
-jsi::Value getDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef);
 
 std::shared_ptr<Shareable> extractShareableOrThrow(
     jsi::Runtime &rt,
@@ -296,37 +286,6 @@ class ShareableHandle : public Shareable {
   ~ShareableHandle() {
     cleanupIfRuntimeExists(remoteRuntime_, remoteValue_);
   }
-
-  jsi::Value toJSValue(jsi::Runtime &rt) override;
-};
-
-class ShareableSynchronizedDataHolder
-    : public Shareable,
-      public std::enable_shared_from_this<ShareableSynchronizedDataHolder> {
- private:
-  std::shared_ptr<Shareable> data_;
-  std::mutex dataAccessMutex_; // Protects `data_`.
-  jsi::Runtime *primaryRuntime_;
-  jsi::Runtime *secondaryRuntime_;
-  std::unique_ptr<jsi::Value> primaryValue_;
-  std::unique_ptr<jsi::Value> secondaryValue_;
-
- public:
-  ShareableSynchronizedDataHolder(
-      jsi::Runtime &rt,
-      const jsi::Value &initialValue)
-      : Shareable(SynchronizedDataHolder),
-        data_(extractShareableOrThrow(rt, initialValue)),
-        primaryRuntime_(&rt) {}
-
-  ~ShareableSynchronizedDataHolder() {
-    cleanupIfRuntimeExists(primaryRuntime_, primaryValue_);
-    cleanupIfRuntimeExists(secondaryRuntime_, secondaryValue_);
-  }
-
-  jsi::Value get(jsi::Runtime &rt);
-
-  void set(jsi::Runtime &rt, const jsi::Value &data);
 
   jsi::Value toJSValue(jsi::Runtime &rt) override;
 };

Common/cpp/NativeModules/NativeReanimatedModule.cpp
@@ -60,6 +60,7 @@ NativeReanimatedModule::NativeReanimatedModule(
           jsQueue,
           jsScheduler_,
           "Reanimated UI runtime",
+          true /* supportsLocking */,
           valueUnpackerCode)),
       valueUnpackerCode_(valueUnpackerCode),
       eventHandlerRegistry_(std::make_unique<EventHandlerRegistry>()),
@@ -161,6 +162,12 @@ void NativeReanimatedModule::scheduleOnUI(
   });
 }
 
+jsi::Value NativeReanimatedModule::executeOnUIRuntimeSync(
+    jsi::Runtime &rt,
+    const jsi::Value &worklet) {
+  return uiWorkletRuntime_->executeSync(rt, worklet);
+}
+
 jsi::Value NativeReanimatedModule::createWorkletRuntime(
     jsi::Runtime &rt,
     const jsi::Value &name,
@@ -170,6 +177,7 @@ jsi::Value NativeReanimatedModule::createWorkletRuntime(
       jsQueue_,
       jsScheduler_,
       name.asString(rt).utf8(rt),
+      false /* supportsLocking */,
       valueUnpackerCode_);
   auto initializerShareable = extractShareableOrThrow<ShareableWorklet>(
       rt, initializer, "[Reanimated] Initializer must be a worklet.");
@@ -183,27 +191,6 @@ jsi::Value NativeReanimatedModule::scheduleOnRuntime(
     const jsi::Value &shareableWorkletValue) {
   reanimated::scheduleOnRuntime(rt, workletRuntimeValue, shareableWorkletValue);
   return jsi::Value::undefined();
-}
-
-jsi::Value NativeReanimatedModule::makeSynchronizedDataHolder(
-    jsi::Runtime &rt,
-    const jsi::Value &initialShareable) {
-  auto dataHolder =
-      std::make_shared<ShareableSynchronizedDataHolder>(rt, initialShareable);
-  return dataHolder->getJSValue(rt);
-}
-
-void NativeReanimatedModule::updateDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef,
-    const jsi::Value &newData) {
-  reanimated::updateDataSynchronously(rt, synchronizedDataHolderRef, newData);
-}
-
-jsi::Value NativeReanimatedModule::getDataSynchronously(
-    jsi::Runtime &rt,
-    const jsi::Value &synchronizedDataHolderRef) {
-  return reanimated::getDataSynchronously(rt, synchronizedDataHolderRef);
 }
 
 jsi::Value NativeReanimatedModule::makeShareableClone(

Common/cpp/NativeModules/NativeReanimatedModule.h
@@ -46,18 +46,9 @@ class NativeReanimatedModule : public NativeReanimatedModuleSpec {
       const jsi::Value &value,
       const jsi::Value &shouldRetainRemote) override;
 
-  jsi::Value makeSynchronizedDataHolder(
-      jsi::Runtime &rt,
-      const jsi::Value &initialShareable) override;
-  jsi::Value getDataSynchronously(
-      jsi::Runtime &rt,
-      const jsi::Value &synchronizedDataHolderRef) override;
-  void updateDataSynchronously(
-      jsi::Runtime &rt,
-      const jsi::Value &synchronizedDataHolderRef,
-      const jsi::Value &newData);
-
   void scheduleOnUI(jsi::Runtime &rt, const jsi::Value &worklet) override;
+  jsi::Value executeOnUIRuntimeSync(jsi::Runtime &rt, const jsi::Value &worklet)
+      override;
 
   jsi::Value createWorkletRuntime(
       jsi::Runtime &rt,

Common/cpp/NativeModules/NativeReanimatedModuleSpec.cpp
@@ -17,26 +17,6 @@ static jsi::Value SPEC_PREFIX(makeShareableClone)(
       ->makeShareableClone(rt, std::move(args[0]), std::move(args[1]));
 }
 
-// Sync methods
-
-static jsi::Value SPEC_PREFIX(makeSynchronizedDataHolder)(
-    jsi::Runtime &rt,
-    TurboModule &turboModule,
-    const jsi::Value *args,
-    size_t) {
-  return static_cast<NativeReanimatedModuleSpec *>(&turboModule)
-      ->makeSynchronizedDataHolder(rt, std::move(args[0]));
-}
-
-static jsi::Value SPEC_PREFIX(getDataSynchronously)(
-    jsi::Runtime &rt,
-    TurboModule &turboModule,
-    const jsi::Value *args,
-    size_t) {
-  return static_cast<NativeReanimatedModuleSpec *>(&turboModule)
-      ->getDataSynchronously(rt, std::move(args[0]));
-}
-
 // scheduler
 
 static jsi::Value SPEC_PREFIX(scheduleOnUI)(
@@ -47,6 +27,15 @@ static jsi::Value SPEC_PREFIX(scheduleOnUI)(
   static_cast<NativeReanimatedModuleSpec *>(&turboModule)
       ->scheduleOnUI(rt, std::move(args[0]));
   return jsi::Value::undefined();
+}
+
+static jsi::Value SPEC_PREFIX(executeOnUIRuntimeSync)(
+    jsi::Runtime &rt,
+    TurboModule &turboModule,
+    const jsi::Value *args,
+    size_t) {
+  return static_cast<NativeReanimatedModuleSpec *>(&turboModule)
+      ->executeOnUIRuntimeSync(rt, std::move(args[0]));
 }
 
 static jsi::Value SPEC_PREFIX(createWorkletRuntime)(
@@ -200,12 +189,9 @@ NativeReanimatedModuleSpec::NativeReanimatedModuleSpec(
   methodMap_["makeShareableClone"] =
       MethodMetadata{2, SPEC_PREFIX(makeShareableClone)};
 
-  methodMap_["makeSynchronizedDataHolder"] =
-      MethodMetadata{1, SPEC_PREFIX(makeSynchronizedDataHolder)};
-  methodMap_["getDataSynchronously"] =
-      MethodMetadata{1, SPEC_PREFIX(getDataSynchronously)};
-
   methodMap_["scheduleOnUI"] = MethodMetadata{1, SPEC_PREFIX(scheduleOnUI)};
+  methodMap_["executeOnUIRuntimeSync"] =
+      MethodMetadata{1, SPEC_PREFIX(executeOnUIRuntimeSync)};
   methodMap_["createWorkletRuntime"] =
       MethodMetadata{2, SPEC_PREFIX(createWorkletRuntime)};
   methodMap_["scheduleOnRuntime"] =

Common/cpp/NativeModules/NativeReanimatedModuleSpec.h
@@ -29,16 +29,11 @@ class JSI_EXPORT NativeReanimatedModuleSpec : public TurboModule {
       const jsi::Value &value,
       const jsi::Value &shouldRetainRemote) = 0;
 
-  // Synchronized data objects
-  virtual jsi::Value makeSynchronizedDataHolder(
-      jsi::Runtime &rt,
-      const jsi::Value &initialShareable) = 0;
-  virtual jsi::Value getDataSynchronously(
-      jsi::Runtime &rt,
-      const jsi::Value &synchronizedDataHolderRef) = 0;
-
   // Scheduling
   virtual void scheduleOnUI(jsi::Runtime &rt, const jsi::Value &worklet) = 0;
+  virtual jsi::Value executeOnUIRuntimeSync(
+      jsi::Runtime &rt,
+      const jsi::Value &worklet) = 0;
 
   // Worklet runtime
   virtual jsi::Value createWorkletRuntime(

Common/cpp/ReanimatedRuntime/WorkletRuntime.cpp
@@ -4,15 +4,69 @@
 #include "WorkletRuntimeCollector.h"
 #include "WorkletRuntimeDecorator.h"
 
+#include <jsi/decorator.h>
+
 namespace reanimated {
+
+class AroundLock {
+  const std::shared_ptr<std::recursive_mutex> mutex_;
+
+ public:
+  explicit AroundLock(const std::shared_ptr<std::recursive_mutex> &mutex)
+      : mutex_(mutex) {}
+
+  void before() const {
+    mutex_->lock();
+  }
+
+  void after() const {
+    mutex_->unlock();
+  }
+};
+
+class LockableRuntime : public jsi::WithRuntimeDecorator<AroundLock> {
+  AroundLock aroundLock_;
+  std::shared_ptr<jsi::Runtime> runtime_;
+
+ public:
+  explicit LockableRuntime(
+      std::shared_ptr<jsi::Runtime> &runtime,
+      const std::shared_ptr<std::recursive_mutex> &runtimeMutex)
+      : jsi::WithRuntimeDecorator<AroundLock>(*runtime, aroundLock_),
+        aroundLock_(runtimeMutex),
+        runtime_(std::move(runtime)) {}
+};
+
+static std::shared_ptr<jsi::Runtime> makeRuntime(
+    jsi::Runtime &runtime,
+    const std::shared_ptr<MessageQueueThread> &jsQueue,
+    const std::string &name,
+    const bool supportsLocking,
+    const std::shared_ptr<std::recursive_mutex> &runtimeMutex) {
+  auto reanimatedRuntime = ReanimatedRuntime::make(runtime, jsQueue, name);
+  if (supportsLocking) {
+    return std::make_shared<LockableRuntime>(reanimatedRuntime, runtimeMutex);
+  } else {
+    return reanimatedRuntime;
+  }
+}
 
 WorkletRuntime::WorkletRuntime(
     jsi::Runtime &rnRuntime,
     const std::shared_ptr<MessageQueueThread> &jsQueue,
     const std::shared_ptr<JSScheduler> &jsScheduler,
     const std::string &name,
+    const bool supportsLocking,
     const std::string &valueUnpackerCode)
-    : runtime_(ReanimatedRuntime::make(rnRuntime, jsQueue, name)), name_(name) {
+    : runtimeMutex_(std::make_shared<std::recursive_mutex>()),
+      runtime_(makeRuntime(
+          rnRuntime,
+          jsQueue,
+          name,
+          supportsLocking,
+          runtimeMutex_)),
+      name_(name),
+      supportsLocking_(supportsLocking) {
   jsi::Runtime &rt = *runtime_;
   WorkletRuntimeCollector::install(rt);
   WorkletRuntimeDecorator::decorate(rt, name, jsScheduler);
@@ -24,6 +78,25 @@ WorkletRuntime::WorkletRuntime(
           .asObject(rt)
           .asFunction(rt);
   rt.global().setProperty(rt, "__valueUnpacker", valueUnpacker);
+}
+
+jsi::Value WorkletRuntime::executeSync(
+    jsi::Runtime &rt,
+    const jsi::Value &worklet) const {
+  assert(
+      supportsLocking_ &&
+      ("[Reanimated] Runtime \"" + name_ + "\" doesn't support locking.")
+          .c_str());
+  auto shareableWorklet = extractShareableOrThrow<ShareableWorklet>(
+      rt,
+      worklet,
+      "[Reanimated] Only worklets can be executed synchronously on UI runtime.");
+  auto lock = std::unique_lock<std::recursive_mutex>(*runtimeMutex_);
+  jsi::Runtime &uiRuntime = getJSIRuntime();
+  auto result = runGuarded(shareableWorklet);
+  auto shareableResult = extractShareableOrThrow(uiRuntime, result);
+  lock.unlock();
+  return shareableResult->getJSValue(rt);
 }
 
 jsi::Value WorkletRuntime::get(

Common/cpp/ReanimatedRuntime/WorkletRuntime.h
@@ -26,6 +26,7 @@ class WorkletRuntime : public jsi::HostObject,
       const std::shared_ptr<MessageQueueThread> &jsQueue,
       const std::shared_ptr<JSScheduler> &jsScheduler,
       const std::string &name,
+      const bool supportsLocking,
       const std::string &valueUnpackerCode);
 
   jsi::Runtime &getJSIRuntime() const {
@@ -33,11 +34,11 @@ class WorkletRuntime : public jsi::HostObject,
   }
 
   template <typename... Args>
-  inline void runGuarded(
+  inline jsi::Value runGuarded(
       const std::shared_ptr<ShareableWorklet> &shareableWorklet,
       Args &&...args) const {
     jsi::Runtime &rt = *runtime_;
-    runOnRuntimeGuarded(
+    return runOnRuntimeGuarded(
         rt, shareableWorklet->getJSValue(rt), std::forward<Args>(args)...);
   }
 
@@ -50,6 +51,8 @@ class WorkletRuntime : public jsi::HostObject,
         [=, self = shared_from_this()] { self->runGuarded(shareableWorklet); });
   }
 
+  jsi::Value executeSync(jsi::Runtime &rt, const jsi::Value &worklet) const;
+
   std::string toString() const {
     return "[WorkletRuntime \"" + name_ + "\"]";
   }
@@ -59,9 +62,11 @@ class WorkletRuntime : public jsi::HostObject,
   std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime &rt) override;
 
  private:
+  const std::shared_ptr<std::recursive_mutex> runtimeMutex_;
   const std::shared_ptr<jsi::Runtime> runtime_;
   const std::string name_;
   std::shared_ptr<AsyncQueue> queue_;
+  const bool supportsLocking_;
 };
 
 // This function needs to be non-inline to avoid problems with dynamic_cast on

commit 0f34974aada53d9c39f3d613713e783f1502e19d

Fix race condition when ShareableHandle is accessed on multiple threads (#5759)
## Summary

Fixes #5660, a regression introduced in #4300.

The aforementioned race condition happens this way:

1. An object using a `ShareableHandle` underneath (e.g. a shared value)
is created.
2. This object is accessed on UI thread.
3. The initializer of this `ShareableHandle` get called on the UI
thread.
4. At the same time, JS thread schedules an operation on this object
(e.g. setting a value of shared value).
5. Access on UI thread forces the initialization. The condition of the
if clause resolves to true and UI thread tries to access the runtime.
![Screenshot 2024-03-04 at 13 00
07](https://github.com/software-mansion/react-native-reanimated/assets/40713406/6c124599-3d60-4211-a1a9-3ccedd88f687)
6. However, access from JS thread has locked the runtime and causes the
UI runtime to wait.
7. Then, JS thread enters the same if clause body and initializes the
whole shareable.
8. After initializing and releasing the runtime, UI thread gets
unblocked.
9. However, now `initializer_` has been nulled and causes memory access
issues.

It's difficult to change the whole flow of locking to prevent such
scenarios. Therefore we won't null the `initializer_` object anymore.
However, this won't fix the problem of potential double initialization.
Luckily, the code of `valueUnpacker` already prevents that with its
shareable handle cache and the fact that runtime operations must be
sequential.

## Test plan

Run the following race condition reproduction made - you should re-run
the app several times (probably up to 20ish) since it's most likely to
happen when the app starts. It's also more likely to happen on Android
simulators (at least for me).

<details>
<summary>
Test code
</summary>

```tsx
import {useSharedValue} from 'react-native-reanimated';
import {useEffect, useState} from 'react';

const value = 666666;

const Screen = () => {
  const sv = useSharedValue(value);
  useSharedValue(1);

Common/cpp/SharedItems/Shareables.cpp
@@ -226,7 +226,8 @@ jsi::Value ShareableWorklet::toJSValue(jsi::Runtime &rt) {
           [](const auto &item) { return item.first == "__workletHash"; }) &&
       "ShareableWorklet doesn't have `__workletHash` property");
   jsi::Value obj = ShareableObject::toJSValue(rt);
-  return getValueUnpacker(rt).call(rt, obj);
+  return getValueUnpacker(rt).call(
+      rt, obj, jsi::String::createFromAscii(rt, "Worklet"));
 }
 
 jsi::Value ShareableRemoteFunction::toJSValue(jsi::Runtime &rt) {
@@ -245,13 +246,23 @@ jsi::Value ShareableRemoteFunction::toJSValue(jsi::Runtime &rt) {
 }
 
 jsi::Value ShareableHandle::toJSValue(jsi::Runtime &rt) {
-  if (initializer_ != nullptr) {
+  if (remoteValue_ == nullptr) {
     auto initObj = initializer_->getJSValue(rt);
-    remoteValue_ =
-        std::make_unique<jsi::Value>(getValueUnpacker(rt).call(rt, initObj));
-    remoteRuntime_ = &rt;
-    initializer_ = nullptr; // we can release ref to initializer as this
-    // method should be called at most once
+    auto value = std::make_unique<jsi::Value>(getValueUnpacker(rt).call(
+        rt, initObj, jsi::String::createFromAscii(rt, "Handle")));
+
+    // We are locking the initialization here since the thread that is
+    // initalizing can be pre-empted on runtime lock. E.g.
+    // UI thread can be pre-empted on initialization of a shared value and then
+    // JS thread can try to access the shared value, locking the whole runtime.
+    // If we put the lock on `getValueUnpacker` part (basically any part that
+    // requires runtime) we would get a deadlock since UI thread would never
+    // release it.
+    std::unique_lock<std::mutex> lock(initializationMutex_);
+    if (remoteValue_ == nullptr) {
+      remoteValue_ = std::move(value);
+      remoteRuntime_ = &rt;
+    }
   }
   return jsi::Value(rt, *remoteValue_);
 }

Common/cpp/SharedItems/Shareables.h
@@ -273,8 +273,13 @@ class ShareableRemoteFunction
 
 class ShareableHandle : public Shareable {
  private:
+  // We don't release the initializer since the handle can get
+  // initialized in parallel on multiple threads. However this is not a problem,
+  // since the final value is taken from a cache on the runtime which guarantees
+  // sequential access.
   std::unique_ptr<ShareableObject> initializer_;
   std::unique_ptr<jsi::Value> remoteValue_;
+  mutable std::mutex initializationMutex_;
   jsi::Runtime *remoteRuntime_;
 
  public:

src/reanimated2/valueUnpacker.ts
@@ -48,7 +48,7 @@ function valueUnpacker(objectToUnpack: any, category?: string): any {
     const functionInstance = workletFun.bind(objectToUnpack);
     objectToUnpack._recur = functionInstance;
     return functionInstance;
-  } else if (objectToUnpack.__init) {
+  } else if (objectToUnpack.__init !== undefined) {
     let value = handleCache.get(objectToUnpack);
     if (value === undefined) {
       value = objectToUnpack.__init();
@@ -63,7 +63,11 @@ See \`https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshoo
     fun.__remoteFunction = objectToUnpack;
     return fun;
   } else {
-    throw new Error('[Reanimated] Data type not recognized by value unpacker.');
+    throw new Error(
+      `[Reanimated] Data type in category "${category}" not recognized by value unpacker: "${_toString(
+        objectToUnpack
+      )}".`
+    );
   }
 }
 