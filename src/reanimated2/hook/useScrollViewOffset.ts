software-mansion/react-native-reanimated

commit 89f76a7d2a2918614b6674e0abb1e542cd7fe8a7

Make `useEvent` return a different reference if event handler should be reattached (#5743)
## Summary

Currently Reanimated relies on reattaching event handlers on render.
However, we give any notice to React that it should occur, because
`useEvent` always returns the same reference.

With these changes, the reference for a given handler will change if it
had to be rebuild and components using `memo` will get re-rendered.

Fixes #5364 

## Test plan

See added `MemoExample.tsx`.

src/reanimated2/hook/useScrollViewOffset.ts
@@ -52,17 +52,17 @@ export function useScrollViewOffset(
     // for more information about this cast.
   ) as unknown as EventHandlerInternal<ReanimatedScrollEvent>;
 
-  useEffect(() => {
-    const viewTag = IS_WEB
-      ? animatedRef.current
-      : findNodeHandle(animatedRef.current);
+  const component = animatedRef.current;
 
-    eventHandler.current?.registerForEvents(viewTag as number);
+  useEffect(() => {
+    const viewTag = IS_WEB ? component : findNodeHandle(component);
+
+    eventHandler.workletEventHandler.registerForEvents(viewTag as number);
 
     return () => {
-      eventHandler.current?.unregisterFromEvents();
+      eventHandler.workletEventHandler?.unregisterFromEvents();
     };
-  }, [animatedRef.current]);
+  }, [component, eventHandler]);
 
   return offsetRef.current;
 }

app/src/examples/MemoExample.tsx
@@ -1,0 +1,85 @@
+import React, { memo } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import type { NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+import type {
+  AnimatedStyle,
+  EventHandlerProcessed,
+} from 'react-native-reanimated';
+
+const MemoizedComponent = memo<{
+  scrollHandler: EventHandlerProcessed<NativeSyntheticEvent<NativeScrollEvent>>;
+  animatedBoxStyle: AnimatedStyle;
+}>(({ scrollHandler, animatedBoxStyle }) => (
+  <>
+    <Animated.View style={[styles.box, animatedBoxStyle]} />
+    <Animated.ScrollView
+      onScroll={scrollHandler}
+      horizontal={true}
+      style={styles.list}>
+      {[...Array(10).keys()].map((i: number) => (
+        <View
+          key={i}
+          style={[
+            styles.element,
+            {
+              backgroundColor: `hsl(${i * 10}, 50%, 50%)`,
+            },
+          ]}
+        />
+      ))}
+    </Animated.ScrollView>
+  </>
+));
+
+export default function App() {
+  const [counter, setCounter] = React.useState(0);
+
+  const width = useSharedValue(100);
+  const backgroundColor = useSharedValue('hsl(0, 50%, 50%)');
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    console.log(counter);
+    width.value = 100 + event.contentOffset.x / 5;
+    backgroundColor.value = `hsl(${event.contentOffset.x / 10}, 50%, 50%)`;
+  });
+  const animatedBoxStyle = useAnimatedStyle(() => ({
+    width: width.value,
+    backgroundColor: backgroundColor.value,
+  }));
+
+  return (
+    <View style={styles.container}>
+      <MemoizedComponent
+        scrollHandler={scrollHandler}
+        animatedBoxStyle={animatedBoxStyle}
+      />
+      <Button title="Re-render" onPress={() => setCounter((s) => s + 1)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  list: {
+    maxHeight: 120,
+  },
+  box: {
+    height: 100,
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    margin: 30,
+  },
+  element: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+});

app/src/examples/index.ts
@@ -117,6 +117,7 @@ import NestedLayoutAnimationConfig from './LayoutAnimations/NestedLayoutAnimatio
 import WithClampExample from './WithClampExample';
 import WorkletFactoryCrash from './WorkletFactoryCrashExample';
 import HabitsExample from './LayoutAnimations/HabitsExample';
+import MemoExample from './MemoExample';
 
 interface Example {
   icon?: string;
@@ -151,6 +152,11 @@ export const EXAMPLES: Record<string, Example> = {
     icon: '🟨',
     title: 'JS props',
     screen: JSPropsExample,
+  },
+  MemoExample: {
+    icon: '🧠',
+    title: 'Memo',
+    screen: MemoExample,
   },
 
   // About

src/createAnimatedComponent/PropsFilter.tsx
@@ -65,13 +65,15 @@ export class PropsFilter implements IPropsFilter {
           });
         }
       } else if (
-        has('current', value) &&
-        value.current instanceof WorkletEventHandler
+        has('workletEventHandler', value) &&
+        value.workletEventHandler instanceof WorkletEventHandler
       ) {
-        if (value.current.eventNames.length > 0) {
-          value.current.eventNames.forEach((eventName) => {
-            props[eventName] = has('listeners', value.current)
-              ? (value.current.listeners as Record<string, unknown>)[eventName]
+        if (value.workletEventHandler.eventNames.length > 0) {
+          value.workletEventHandler.eventNames.forEach((eventName) => {
+            props[eventName] = has('listeners', value.workletEventHandler)
+              ? (
+                  value.workletEventHandler.listeners as Record<string, unknown>
+                )[eventName]
               : dummyListener;
           });
         } else {

src/createAnimatedComponent/createAnimatedComponent.tsx
@@ -231,15 +231,15 @@ export function createAnimatedComponent(
       for (const key in this.props) {
         const prop = this.props[key];
         if (
-          has('current', prop) &&
-          prop.current instanceof WorkletEventHandler
+          has('workletEventHandler', prop) &&
+          prop.workletEventHandler instanceof WorkletEventHandler
         ) {
           if (viewTag === null) {
             viewTag = IS_WEB
               ? this._component
               : findNodeHandle(options?.setNativeProps ? this : node);
           }
-          prop.current.registerForEvents(viewTag as number, key);
+          prop.workletEventHandler.registerForEvents(viewTag as number, key);
         }
       }
     }
@@ -248,10 +248,10 @@ export function createAnimatedComponent(
       for (const key in this.props) {
         const prop = this.props[key];
         if (
-          has('current', prop) &&
-          prop.current instanceof WorkletEventHandler
+          has('workletEventHandler', prop) &&
+          prop.workletEventHandler instanceof WorkletEventHandler
         ) {
-          prop.current.unregisterFromEvents();
+          prop.workletEventHandler.unregisterFromEvents();
         }
       }
     }
@@ -280,11 +280,11 @@ export function createAnimatedComponent(
       for (const key in prevProps) {
         const prop = this.props[key];
         if (
-          has('current', prop) &&
-          prop.current instanceof WorkletEventHandler &&
-          prop.current.reattachNeeded
+          has('workletEventHandler', prop) &&
+          prop.workletEventHandler instanceof WorkletEventHandler &&
+          prop.workletEventHandler.reattachNeeded
         ) {
-          prop.current.unregisterFromEvents();
+          prop.workletEventHandler.unregisterFromEvents();
         }
       }
 
@@ -293,9 +293,9 @@ export function createAnimatedComponent(
       for (const key in this.props) {
         const prop = this.props[key];
         if (
-          has('current', prop) &&
-          prop.current instanceof WorkletEventHandler &&
-          prop.current.reattachNeeded
+          has('workletEventHandler', prop) &&
+          prop.workletEventHandler instanceof WorkletEventHandler &&
+          prop.workletEventHandler.reattachNeeded
         ) {
           if (viewTag === null) {
             const node = this._getEventViewRef() as AnimatedComponentRef;
@@ -304,8 +304,8 @@ export function createAnimatedComponent(
               ? this._component
               : findNodeHandle(options?.setNativeProps ? this : node);
           }
-          prop.current.registerForEvents(viewTag as number, key);
-          prop.current.reattachNeeded = false;
+          prop.workletEventHandler.registerForEvents(viewTag as number, key);
+          prop.workletEventHandler.reattachNeeded = false;
         }
       }
     }

src/reanimated2/hook/useEvent.ts
@@ -1,5 +1,4 @@
 'use strict';
-import type { MutableRefObject } from 'react';
 import { useRef } from 'react';
 import WorkletEventHandler from '../WorkletEventHandler';
 import type { ReanimatedEvent } from './commonTypes';
@@ -17,9 +16,9 @@ export type EventHandlerProcessed<
   Context extends Record<string, unknown> = never
 > = (event: Event, context?: Context) => void;
 
-export type EventHandlerInternal<Event extends object> = MutableRefObject<
-  WorkletEventHandler<Event>
->;
+export type EventHandlerInternal<Event extends object> = {
+  workletEventHandler: WorkletEventHandler<Event>;
+};
 
 /**
  * Lets you run a function whenever a specified native event occurs.
@@ -49,13 +48,18 @@ export function useEvent<Event extends object, Context = never>(
   eventNames: string[] = [],
   rebuild = false
 ): EventHandlerInternal<Event> {
-  const initRef = useRef<WorkletEventHandler<Event> | null>(null);
+  const initRef = useRef<EventHandlerInternal<Event>>(null!);
   if (initRef.current === null) {
-    initRef.current = new WorkletEventHandler<Event>(handler, eventNames);
+    const workletEventHandler = new WorkletEventHandler<Event>(
+      handler,
+      eventNames
+    );
+    initRef.current = { workletEventHandler };
   } else if (rebuild) {
-    initRef.current.updateWorklet(handler);
+    const workletEventHandler = initRef.current.workletEventHandler;
+    workletEventHandler.updateWorklet(handler);
+    initRef.current = { workletEventHandler };
   }
 
-  // We cast it since we don't want to expose initial null value outside.
-  return initRef as EventHandlerInternal<Event>;
+  return initRef.current;
 }

commit 4a15cf467b709a9365ae57a8eb7ba28397984a29

Fix `useScrollViewOffset` using animated ref before its initialization (#5764)
## Summary

Fixing regression I introduced in #5743 by making the line

`const component = animatedRef.current` 

outside of `useEfect`.

## Test plan

Check `Article progress` example.

src/reanimated2/hook/useScrollViewOffset.ts
@@ -52,9 +52,8 @@ export function useScrollViewOffset(
     // for more information about this cast.
   ) as unknown as EventHandlerInternal<ReanimatedScrollEvent>;
 
-  const component = animatedRef.current;
-
   useEffect(() => {
+    const component = animatedRef.current;
     const viewTag = IS_WEB ? component : findNodeHandle(component);
 
     eventHandler.workletEventHandler.registerForEvents(viewTag as number);
@@ -62,7 +61,11 @@ export function useScrollViewOffset(
     return () => {
       eventHandler.workletEventHandler?.unregisterFromEvents();
     };
-  }, [component, eventHandler]);
+    // React here has a problem with `animatedRef.current` since a Ref .current
+    // field shouldn't be used as a dependency. However, in this case we have
+    // to do it this way.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animatedRef, animatedRef.current, eventHandler]);
 
   return offsetRef.current;
 }