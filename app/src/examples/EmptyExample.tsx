433c93ae1207aa76be7df7c9f9421717bdb81e65
Latropos <aleksandracynk@Aleksandras-MacBook-Pro-3.local>
2024-03-06 15:50:24 +0100

    revert changes to EmptyExample.tsx

--- app/src/examples/EmptyExample.tsx
+++ app/src/examples/EmptyExample.tsx
@@ -1,32 +1,11 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  // Easing, <- this should be the correct import
-} from 'react-native-reanimated';
-import { View, Button, StyleSheet, Easing } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+
 import React from 'react';
 
-export default function AnimatedStyleUpdateExample() {
-  const randomWidth = useSharedValue(10);
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, {
-        easing: Easing.linear,
-      }),
-    };
-  });
-
+export default function EmptyExample() {
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.box, style]} />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+      <Text>Hello world!</Text>
     </View>
   );
 }
@@ -34,12 +13,7 @@ export default function AnimatedStyleUpdateExample() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-  },
-  box: {
-    width: 100,
-    height: 80,
-    backgroundColor: 'black',
-    margin: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
