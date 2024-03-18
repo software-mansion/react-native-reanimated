244dc5b4db862ede6324f2491afb3c8c0f9423ee
Ignacy Łątka <74246391+LatekVo@users.noreply.github.com>
2024-03-18 12:27:16 +0100

    Make "Reanimated" title in docs interactive. (#5766)
    ## Summary
    
    This PR makes the "Reanimated" title in docs interactive.
    ![Screenshot 2024-03-07 at 15 43
    27](https://github.com/software-mansion/react-native-reanimated/assets/74246391/16ba8f5b-a833-400b-b1ae-6253866638f1)
    
    ## Test plan
    
    To test this feature, 
    - clone this fork
    - go to `docs` folder
    - run `yarn start`
    
    ## Results
    
    
    https://github.com/software-mansion/react-native-reanimated/assets/74246391/426696e8-1bce-4349-bb76-1cdbfa84a0c1
    
    ---------
    
    Co-authored-by: Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>

---
+++ docs/src/components/Hero/SelectedLabel/utils.ts
@@ -0,0 +1,101 @@
+import { DraggableId } from './SelectionBox';
+
+export type DynamicStyles = {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+};
+
+export type StaticStyles = {
+  initialWidth: number;
+  initialHeight: number;
+  enabledTextInteractivity: boolean;
+};
+
+export type TextScaleStyles = {
+  x: number;
+  y: number;
+};
+
+export const computeSelectionStyles = (
+  position: { x: number; y: number },
+  draggableIdentifier: DraggableId,
+  dynamicStyles
+): DynamicStyles => {
+  const isLeft =
+    draggableIdentifier === DraggableId.BOTTOM_LEFT ||
+    draggableIdentifier === DraggableId.TOP_LEFT;
+  const isTop =
+    draggableIdentifier === DraggableId.TOP_LEFT ||
+    draggableIdentifier === DraggableId.TOP_RIGHT;
+  const isCenter = draggableIdentifier === DraggableId.CENTER;
+
+  // depedning on whether draggable is on left, right, top or bottom,
+  // we want to either resize, or move and resize our object
+  const positionAdjustment = {
+    x: isLeft ? position.x : 0,
+    y: isTop ? position.y : 0,
+  };
+
+  const sizeChange = {
+    x: isLeft ? -position.x : position.x,
+    y: isTop ? -position.y : position.y,
+  };
+
+  // adjust variables when dragging the center
+  if (isCenter) {
+    positionAdjustment.x = sizeChange.x;
+    positionAdjustment.y = sizeChange.y;
+    sizeChange.x = 0;
+    sizeChange.y = 0;
+  }
+
+  // stop overreduction in size
+  if (dynamicStyles.width + sizeChange.x < 0)
+    sizeChange.x = -dynamicStyles.width;
+  if (dynamicStyles.height + sizeChange.y < 0)
+    sizeChange.y = -dynamicStyles.height;
+
+  // stop dragging a minimized object
+  if (!isCenter) {
+    if (dynamicStyles.width - positionAdjustment.x < 0)
+      positionAdjustment.x = 0;
+    if (dynamicStyles.height - positionAdjustment.y < 0)
+      positionAdjustment.y = 0;
+  }
+
+  return {
+    left: dynamicStyles.left + positionAdjustment.x,
+    top: dynamicStyles.top + positionAdjustment.y,
+    width: dynamicStyles.width + sizeChange.x,
+    height: dynamicStyles.height + sizeChange.y,
+  };
+};
+
+export const computeTextStyles = (
+  dynamicStyles: DynamicStyles,
+  staticStyles: StaticStyles
+): TextScaleStyles => {
+  // these magic numbers are a result of disparity between font's apparent and actual size
+  const sizeOffsetX = 0.99;
+  const sizeOffsetY = 1.21;
+
+  // scale starts at 1 and as it gets larger approaches sizeOffset
+  const textScale = {
+    x:
+      (dynamicStyles.width / staticStyles.initialWidth) * sizeOffsetX -
+      sizeOffsetX +
+      1,
+    y:
+      (dynamicStyles.height / staticStyles.initialHeight) * sizeOffsetY -
+      sizeOffsetY +
+      1,
+  };
+
+  // prevent text overadjustment
+  if (textScale.x < 0) textScale.x = 0;
+  if (textScale.y < 0) textScale.y = 0;
+
+  return textScale;
+};
