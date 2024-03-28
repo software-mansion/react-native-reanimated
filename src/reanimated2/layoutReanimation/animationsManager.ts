a303ce028ef1a4bb06d07709e7af6e1c66214d95
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2024-03-28 18:37:08 +0100

    Cleanup 3

--- src/reanimated2/layoutReanimation/animationsManager.ts
+++ src/reanimated2/layoutReanimation/animationsManager.ts
@@ -20,30 +20,10 @@ function startObservingProgress(
   const isSharedTransition =
     animationType === LayoutAnimationType.SHARED_ELEMENT_TRANSITION;
   sharedValue.addListener(tag + TAG_OFFSET, () => {
-    console.log(sharedValue.value);
+    // console.log(sharedValue.value);
     _notifyAboutProgress(tag, sharedValue.value, isSharedTransition);
   });
 }
-
-// function startObservingProgress2(
-//   node: ShadowNodeWrapper,
-//   tag: number,
-//   sharedValue: SharedValue<Record<string, unknown>>,
-//   animationType: LayoutAnimationType
-// ): void {
-//   'worklet';
-//   const isSharedTransition =
-//     animationType === LayoutAnimationType.SHARED_ELEMENT_TRANSITION;
-//   sharedValue.addListener(tag + TAG_OFFSET, () => {
-//     // console.log(sharedValue.value);
-//     _updatePropsFabric!([
-//       {
-//         shadowNodeWrapper: node,
-//         updates: sharedValue.value,
-//       },
-//     ]);
-//   });
-// }
 
 function stopObservingProgress(
   tag: number,
@@ -76,7 +56,7 @@ function createLayoutAnimationManager() {
         global.ProgressTransitionRegister.onTransitionStart(tag, yogaValues);
         return;
       }
-      console.log('start', tag, type, yogaValues, config);
+      // console.log('start', tag, type, yogaValues, config);
 
       const style = config(yogaValues);
       // console.log(style);
@@ -116,68 +96,13 @@ function createLayoutAnimationManager() {
       startObservingProgress(tag, value, type);
       value.value = animation;
     },
-    // start2(
-    //   node: ShadowNodeWrapper,
-    //   tag: number,
-    //   type: LayoutAnimationType,
-    //   /**
-    //    * createLayoutAnimationManager creates an animation manager for both Layout animations and Shared Transition Elements animations.
-    //    */
-    //   yogaValues: Partial<SharedTransitionAnimationsValues>,
-    //   config: (
-    //     arg: Partial<SharedTransitionAnimationsValues>
-    //   ) => LayoutAnimation
-    // ) {
-    //   if (type === LayoutAnimationType.SHARED_ELEMENT_TRANSITION_PROGRESS) {
-    //     global.ProgressTransitionRegister.onTransitionStart(tag, yogaValues);
-    //     return;
-    // }
-    // console.log('start', tag, type, yogaValues, config);
-
-    // const style = config(yogaValues);
-    // // console.log(style);
-    // let currentAnimation = style.animations;
-
-    // // When layout animation is requested, but a previous one is still running, we merge
-    // // new layout animation targets into the ongoing animation
-    // const previousAnimation = currentAnimationForTag.get(tag);
-    // if (previousAnimation) {
-    //   currentAnimation = { ...previousAnimation, ...style.animations };
-    // }
-    // currentAnimationForTag.set(tag, currentAnimation);
-
-    //   let value = mutableValuesForTag.get(tag);
-    //   if (value === undefined) {
-    //     value = makeUIMutable(style.initialValues);
-    //     mutableValuesForTag.set(tag, value);
-    //   } else {
-    //     stopObservingProgress(tag, value);
-    //     value._value = style.initialValues;
-    //   }
-
-    //   // @ts-ignore The line below started failing because I added types to the method – don't have time to fix it right now
-    //   const animation = withStyleAnimation(currentAnimation);
-
-    //   animation.callback = (finished?: boolean) => {
-    //     if (finished) {
-    //       currentAnimationForTag.delete(tag);
-    //       mutableValuesForTag.delete(tag);
-    //       const shouldRemoveView = type === LayoutAnimationType.EXITING;
-    //       stopObservingProgress(tag, value, shouldRemoveView);
-    //     }
-    //     style.callback &&
-    //       style.callback(finished === undefined ? false : finished);
-    //   };
-
-    //   startObservingProgress2(node, tag, value, type);
-    //   value.value = animation;
-    // },
     stop(tag: number) {
       const value = mutableValuesForTag.get(tag);
       if (!value) {
         return;
       }
-      stopObservingProgress(tag, value);
+      value.removeListener(tag + TAG_OFFSET);
+      // stopObservingProgress(tag, value);
     },
   };
 }
