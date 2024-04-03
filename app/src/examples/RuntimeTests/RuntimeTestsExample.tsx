cdb22e87d32677c3e930978d0b26ae3a61bf4ba4
Aleksandra Cynk <ola.zn123@wp.pl>
2024-04-03 16:39:40 +0200

    Renamte test files

--- app/src/examples/RuntimeTests/RuntimeTestsExample.tsx
+++ app/src/examples/RuntimeTests/RuntimeTestsExample.tsx
@@ -4,7 +4,8 @@ import RuntimeTestsRunner from './ReanimatedRuntimeTestsRunner/RuntimeTestsRunne
 // load tests
 // import './tests/Animations.test';
 import './tests/animations/withTiming/easingSnapshots.test';
-import './tests/animations/withTiming/withTiming.test';
+import './tests/animations/withTiming/basicTests.test';
+import './tests/animations/withTiming/colors.test';
 
 export default function RuntimeTestsExample() {
   return <RuntimeTestsRunner />;
