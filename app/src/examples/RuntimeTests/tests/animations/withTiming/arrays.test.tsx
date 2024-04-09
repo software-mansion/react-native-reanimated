e6a0dd3d78fd70709c4b525f51d36c4c2de1f762
Aleksandra Cynk <ola.zn123@wp.pl>
2024-04-09 10:07:30 +0200

    More test cases

--- app/src/examples/RuntimeTests/tests/animations/withTiming/arrays.test.tsx
+++ app/src/examples/RuntimeTests/tests/animations/withTiming/arrays.test.tsx
@@ -22,9 +22,11 @@ const IndependentComponents = ({
   startWidths,
   finalWidths,
   duration,
+  scalars,
 }: {
   startWidths: [number, number, number];
   finalWidths: [number, number, number];
+  scalars: [number, number, number];
   duration: number;
 }) => {
   const widths = useSharedValue(startWidths);
@@ -33,13 +35,13 @@ const IndependentComponents = ({
   const ref2 = useTestRef('IndependentComponent2');
 
   const style0 = useAnimatedStyle(() => {
-    return { width: widths.value[0] };
+    return { width: widths.value[0] * scalars[0] };
   });
   const style1 = useAnimatedStyle(() => {
-    return { width: widths.value[1] };
+    return { width: widths.value[1] * scalars[1] };
   });
   const style2 = useAnimatedStyle(() => {
-    return { width: widths.value[2] };
+    return { width: widths.value[2] * scalars[2] };
   });
 
   useEffect(() => {
@@ -47,7 +49,7 @@ const IndependentComponents = ({
       100,
       withTiming(finalWidths, {
         duration,
-      }),
+      })
     );
   }, [widths, finalWidths, duration]);
 
@@ -73,54 +75,136 @@ describe('withTiming animation of ARRAY', () => {
 
   (
     [
-      { startWidths: [20, 20, 20], finalWidths: [300, 300, 300], speed: 'very slow' },
-      { startWidths: [20, 300, 20], finalWidths: [200, 30, 200], speed: 'slow' },
-      { startWidths: [20, 20, 20], finalWidths: [20, 200, 100], speed: 'medium' },
-      { startWidths: [20, 20, 20], finalWidths: [130, 140, 150], speed: 'medium' },
-      { startWidths: [20, 20, 20], finalWidths: [130, 140, 150], speed: 'fast' },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [300, 300, 300],
+        speed: 'very slow',
+      },
+      {
+        startWidths: [20, 300, 20],
+        finalWidths: [200, 30, 200],
+        speed: 'slow',
+      },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [20, 200, 100],
+        speed: 'medium',
+      },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [130, 140, 150],
+        speed: 'medium',
+      },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [130, 140, 150],
+        speed: 'fast',
+      },
       { startWidths: [20, 20, 20], finalWidths: [20, 140, 150], speed: 'fast' },
       { startWidths: [20, 20, 20], finalWidths: [20, 20, 150], speed: 'fast' },
-      { startWidths: [20, 20, 20], finalWidths: [130, 140, 150], speed: 'very fast' },
-      { startWidths: [200, 200, 200], finalWidths: [130, 140, 150], speed: 'very fast' },
-      { startWidths: [200, 200, 200], finalWidths: [200, 140, 150], speed: 'very fast' },
-      { startWidths: [20, 20, 20], finalWidths: [130, 140, 150], speed: 'immediately' },
-      { startWidths: [20, 20, 20], finalWidths: [130, 14.3, 150], speed: 'medium' },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [130, 140, 150],
+        speed: 'very fast',
+      },
+      {
+        startWidths: [200, 200, 200],
+        finalWidths: [130, 140, 150],
+        speed: 'very fast',
+      },
+      {
+        startWidths: [200, 200, 200],
+        finalWidths: [200, 140, 150],
+        speed: 'very fast',
+      },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [130, 140, 150],
+        speed: 'immediately',
+      },
+      {
+        startWidths: [20, 20, 20],
+        finalWidths: [130, 14.3, 150],
+        speed: 'medium',
+      },
+      {
+        startWidths: [2e-4, 20, 20],
+        finalWidths: [13.78e-4, 200, 20],
+        scalars: [1e5, 1, 1],
+        speed: 'medium',
+      },
+      {
+        startWidths: [2e-9, 20, 20],
+        finalWidths: [13.78e-9, 200, 20],
+        scalars: [1e10, 1, 1],
+        speed: 'medium',
+      },
+      {
+        startWidths: [2e-17, 20, 20],
+        finalWidths: [13.78e-17, 200, 20],
+        scalars: [1e18, 1, 1],
+        speed: 'medium',
+      },
+      {
+        startWidths: [2e-125, 1e20, 20],
+        finalWidths: [13.78e-125, 1e20, 20],
+        scalars: [1e126, 1e-19, 1],
+        speed: 'medium',
+      },
+      {
+        startWidths: [2e-130, 1e-20, 20],
+        finalWidths: [13.78e-125, 15e20, 20],
+        scalars: [1e126, 1e-19, 1],
+        speed: 'medium',
+      },
     ] as Array<{
       startWidths: [number, number, number];
       finalWidths: [number, number, number];
+      scalars?: [number, number, number];
       speed: keyof typeof TestSpeed;
     }>
-  ).forEach(({ startWidths, finalWidths, speed: speedName }) => {
-    test(`Animate independent components ${speedName} FROM ${startWidths} TO ${finalWidths}`, async () => {
-      await render(
-        <IndependentComponents
-          startWidths={startWidths}
-          finalWidths={finalWidths}
-          duration={TestSpeed[speedName]}
-        />,
-      );
-      const independentComponents = [
-        'IndependentComponent0',
-        'IndependentComponent1',
-        'IndependentComponent2',
-      ].map(refName => getTestComponent(refName));
-
-      independentComponents.forEach(async (component, index) => {
-        expect(await component.getAnimatedStyle('width')).toBe(
-          startWidths[index],
-          ComparisonMode.DISTANCE,
+  ).forEach(
+    ({
+      startWidths,
+      finalWidths,
+      scalars: passedScalars,
+      speed: speedName,
+    }) => {
+      const scalars: [number, number, number] = passedScalars
+        ? passedScalars
+        : [1, 1, 1];
+      test(`Animate independent components ${speedName} FROM ${startWidths} TO ${finalWidths}`, async () => {
+        await render(
+          <IndependentComponents
+            startWidths={startWidths}
+            finalWidths={finalWidths}
+            duration={TestSpeed[speedName]}
+            scalars={scalars}
+          />
         );
-      });
-      await wait(TestSpeed[speedName] + 200);
+        const independentComponents = [
+          'IndependentComponent0',
+          'IndependentComponent1',
+          'IndependentComponent2',
+        ].map((refName) => getTestComponent(refName));
 
-      independentComponents.forEach(async (component, index) => {
-        expect(await component.getAnimatedStyle('width')).toBe(
-          finalWidths[index],
-          ComparisonMode.DISTANCE,
-        );
+        independentComponents.forEach(async (component, index) => {
+          expect(await component.getAnimatedStyle('width')).toBe(
+            startWidths[index] * scalars[index],
+            ComparisonMode.DISTANCE
+          );
+        });
+        await wait(TestSpeed[speedName] + 200);
+
+        independentComponents.forEach(async (component, index) => {
+          expect(await component.getAnimatedStyle('width')).toBe(
+            finalWidths[index],
+            ComparisonMode.DISTANCE
+          );
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 const styles = StyleSheet.create({
