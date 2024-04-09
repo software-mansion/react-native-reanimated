1ad99c5cd6509fc68b4dab52d547c0da77269314
Aleksandra Cynk <ola.zn123@wp.pl>
2024-04-09 10:36:56 +0200

    Add array test

---
+++ app/src/examples/RuntimeTests/tests/.prettierc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100, // Increase line width to make test cases more compact
+};
