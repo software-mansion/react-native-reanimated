0624bbaee1bdc3c58d1ba907ac6b62a3bbef7c56
Marek Kotarba <markuskotarba@gmail.com>
2024-04-11 11:18:37 +0200

    test

--- docs/types.ts
+++ docs/types.ts
@@ -1,1 +1,3 @@
 declare module '*.otf';
+
+const ladabdsasdaduygasydgsa = 'sadnuysaguydgusgav hgjdvsauy ggsaf hgdsa ytasyuf gyvsa7b6 sag  ffsads tasy tthgs satugdg satdsgauttafuydgsafgavbkja;hvlib vhjabvyauibvjsajhsijema je stmarek in lubie czernyhc luydzi ';
