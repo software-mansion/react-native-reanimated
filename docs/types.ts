0f15c12ef1561109127a799e65dc676a29f915bb
Marek Kotarba <markuskotarba@gmail.com>
2024-04-11 11:20:36 +0200

    yup, that's work

--- docs/types.ts
+++ docs/types.ts
@@ -1,3 +1,1 @@
 declare module '*.otf';
-
-const ladabdsasdaduygasydgsa = 'sadnuysaguydgusgav hgjdvsauy ggsaf hgdsa ytasyuf gyvsa7b6 sag  ffsads tasy tthgs satugdg satdsgauttafuydgsafgavbkja;hvlib vhjabvyauibvjsajhsijema je stmarek in lubie czernyhc luydzi ';
