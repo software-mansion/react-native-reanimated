96e39e73208db315091a77ecc21c0ec503c5f0ec
Marek Kotarba <markuskotarba@gmail.com>
2024-04-11 10:22:56 +0200

    nestings removed

--- docs/scripts/build-og-images.jsx
+++ docs/scripts/build-og-images.jsx
@@ -5,6 +5,7 @@ import { promisify } from 'util';
 import React from 'react';
 import path from 'path';
 import fs from 'fs';
+import OGImageStream from './og-image-stream';
 
 const getMarkdownHeader = (path) => {
   const content = fs.readFileSync(path, 'utf-8');
@@ -19,7 +20,7 @@ const getMarkdownHeader = (path) => {
     }))
     .sort((a, b) => a.level - b.level || a.index - b.index);
 
-  return headers[0]?.title;
+  return headers[0]?.title || 'React Native Reanimated';
 };
 
 async function saveStreamToFile(stream, path) {
@@ -55,103 +56,21 @@ const buildOGImages = async () => {
   const imageBuffer = fs.readFileSync(imagePath);
   const base64Image = `data:image/png;base64,${imageBuffer.toString('base64')}`;
 
-  await Promise.all(
-    docs.map(async ({ dir, files }) => {
-      files.map(async (file) => {
-        const header =
-          getMarkdownHeader(path.resolve(baseDocsPath, dir, file)) ??
-          'React Native Reanimated';
+  docs.map(async ({ dir, files }) => {
+    files.map(async (file) => {
+      const header = getMarkdownHeader(path.resolve(baseDocsPath, dir, file));
 
-        await saveStreamToFile(
-          new ImageResponse(
-            (
-              <div
-                style={{
-                  display: 'flex',
-                  fontSize: 40,
-                  color: 'black',
-                  background: 'white',
-                  width: '100%',
-                  height: '100%',
-                  padding: '50px 200px',
-                  textAlign: 'center',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <img
-                  style={{
-                    width: 1200,
-                    height: 630,
-                    objectFit: 'cover',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                  }}
-                  src={base64Image}
-                  alt=""
-                />
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: 1200,
-                    gap: -20,
-                    padding: '0 201px 0 67px',
-                  }}>
-                  <p
-                    style={{
-                      fontSize: 72,
-                      fontWeight: 'bold',
-                      color: '#001A72',
-                      textAlign: 'left',
-                      fontFamily: 'Aeonik Bold',
-                      textWrap: 'wrap',
-                    }}>
-                    {header}
-                  </p>
-                  <pre
-                    style={{
-                      fontSize: 40,
-                      fontWeight: 'normal',
-                      color: '#001A72',
-                      textAlign: 'left',
-                      fontFamily: 'Aeonik Regular',
-                      textWrap: 'wrap',
-                    }}>
-                    {'Check out the React Native\nReanimated documentation.'}
-                  </pre>
-                </div>
-              </div>
-            ),
-            {
-              width: 1200,
-              height: 630,
-              fonts: [
-                {
-                  name: 'Aeonik Bold',
-                  data: fs.readFileSync(
-                    path.resolve(__dirname, '../static/font/Aeonik-Bold.otf')
-                  ),
-                  style: 'normal',
-                },
-                {
-                  name: 'Aeonik Regular',
-                  data: fs.readFileSync(
-                    path.resolve(__dirname, '../static/font/Aeonik-Regular.otf')
-                  ),
-                  style: 'normal',
-                },
-              ],
-            }
-          ).body,
-          path.resolve(
-            targetDocs,
-            `${header.replace(/ /g, '-').replace('/', '-').toLowerCase()}.png`
-          )
-        );
-      });
-    })
-  );
+      const ogImageStream = OGImageStream(header, base64Image);
+
+      await saveStreamToFile(
+        ogImageStream,
+        path.resolve(
+          targetDocs,
+          `${header.replace(/ /g, '-').replace('/', '-').toLowerCase()}.png`
+        )
+      );
+    });
+  });
 };
 
 buildOGImages();
