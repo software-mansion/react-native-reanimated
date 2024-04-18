805017603cdfb69721210275f7d4e407a5f017ce
szydlovsky <9szydlowski9@gmail.com>
2024-04-18 15:54:07 +0200

    add typetests

---
+++ __typetests__/common/useComposedEventHandlerTest.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import Animated, {
+  useAnimatedScrollHandler,
+  useComposedEventHandler,
+  useEvent,
+} from '../..';
+import { Text } from 'react-native';
+import type { ReanimatedEvent } from '../..';
+
+function useComposedEventHandlerTest() {
+  function useComposedEventHandlerTestDifferentHandlers() {
+    function useCustomScrollHandler(
+      handler: (event: ReanimatedEvent<object>) => void
+    ) {
+      return useEvent(
+        (event) => {
+          'worklet';
+          handler(event);
+        },
+        ['onScroll']
+      );
+    }
+
+    const onScrollCustomHandler = useCustomScrollHandler((e) => {
+      'worklet';
+      console.log(`custom scroll handler invoked on ${e.eventName}`);
+    });
+
+    const onScrollHandler = useAnimatedScrollHandler((e) => {
+      console.log(`scroll handler invoked on ${e.eventName}`);
+    });
+
+    const composedHandler = useComposedEventHandler([
+      onScrollHandler,
+      onScrollCustomHandler,
+    ]);
+
+    return (
+      <>
+        <Animated.ScrollView onScroll={composedHandler}>
+          {[...new Array(20)].map((_each, index) => (
+            <Text>{index}</Text>
+          ))}
+        </Animated.ScrollView>
+      </>
+    );
+  }
+
+  function useComposedEventHandlerTestDifferentProps() {
+    const onDragBeginHandler = useAnimatedScrollHandler({
+      onBeginDrag(e) {
+        'worklet';
+        console.log('Drag begin');
+      },
+    });
+
+    const onDragEndHandler = useAnimatedScrollHandler({
+      onEndDrag(e) {
+        'worklet';
+        console.log('Drag end');
+      },
+    });
+
+    const composedHandler1 = useComposedEventHandler([
+      onDragBeginHandler,
+      onDragEndHandler,
+    ]);
+
+    const composedHandler2 = useComposedEventHandler([
+      onDragBeginHandler,
+      onDragEndHandler,
+    ]);
+
+    return (
+      <>
+        <Animated.ScrollView
+          onMomentumScrollBegin={composedHandler1}
+          onMomentumScrollEnd={composedHandler2}>
+          {[...new Array(20)].map((_each, index) => (
+            <Text>{index}</Text>
+          ))}
+        </Animated.ScrollView>
+      </>
+    );
+  }
+}
