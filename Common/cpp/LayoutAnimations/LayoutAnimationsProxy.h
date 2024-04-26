1871c77764fddbc970a61f17934200248c485e36
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2024-04-26 14:10:24 +0200

    Move to utils

--- Common/cpp/LayoutAnimations/LayoutAnimationsProxy.h
+++ Common/cpp/LayoutAnimations/LayoutAnimationsProxy.h
@@ -5,52 +5,13 @@
 
 #include <react/renderer/mounting/MountingOverrideDelegate.h>
 #include <react/renderer/componentregistry/ComponentDescriptorFactory.h>
+#include "LayoutAnimationsUtils.h"
 
 namespace reanimated {
 
 class NativeReanimatedModule;
 
 using namespace facebook;
-
-struct Window{
-  double width, height;
-};
-
-struct Frame{
-  std::optional<double> x, y, width, height;
-  Frame(jsi::Runtime& runtime, const jsi::Object &newStyle){
-    if (newStyle.hasProperty(runtime, "originX")){
-      x = newStyle.getProperty(runtime, "originX").asNumber();
-    }
-    if (newStyle.hasProperty(runtime, "originY")){
-      y = newStyle.getProperty(runtime, "originY").asNumber();
-    }
-    if (newStyle.hasProperty(runtime, "width")){
-      width = newStyle.getProperty(runtime, "width").asNumber();
-    }
-    if (newStyle.hasProperty(runtime, "height")){
-      height = newStyle.getProperty(runtime, "height").asNumber();
-    }
-  }
-};
-
-struct UpdateValues{
-  Props::Shared newProps;
-  Frame frame;
-};
-
-struct Snapshot{
-  double x, y, width, height, windowWidth, windowHeight;
-  Snapshot(ShadowView& shadowView, Window window){
-    auto& frame = shadowView.layoutMetrics.frame;
-    x = frame.origin.x;
-    y = frame.origin.y;
-    width = frame.size.width;
-    height = frame.size.height;
-    windowWidth = window.width;
-    windowHeight = window.height;
-  }
-};
 
 struct RootNode;
 
@@ -125,46 +86,6 @@ struct LayoutAnimation {
   }
 };
 
-struct SurfaceManager {
-  mutable std::unordered_map<SurfaceId, std::shared_ptr<std::unordered_map<Tag, UpdateValues>>> props_;
-  mutable std::unordered_map<SurfaceId, ShadowViewMutationList> cleanups_;
-  mutable std::unordered_map<SurfaceId, Window> windows_;
-  
-  std::unordered_map<Tag, UpdateValues>& getUpdateMap(SurfaceId surfaceId){
-    auto props = props_.find(surfaceId);
-    if (props != props_.end()){
-      return *props->second;
-    }
-    
-    auto newProps = std::make_shared<std::unordered_map<Tag, UpdateValues>>();
-    props_.insert_or_assign(surfaceId, newProps);
-    return *newProps;
-  }
-  
-  void updateWindow(SurfaceId surfaceId, double windowWidth, double windowHeight){
-    windows_.insert_or_assign(surfaceId, Window{windowWidth, windowHeight});
-  }
-  
-  Window getWindow(SurfaceId surfaceId){
-    auto windowIt = windows_.find(surfaceId);
-    if (windowIt != windows_.end()){
-      return windowIt->second;
-    }
-    return Window{0,0};
-  }
-  
-  void consumeCleanupMutations(SurfaceId surfaceId, ShadowViewMutationList& mutations){
-    for (auto& mutation: cleanups_[surfaceId]){
-      mutations.push_back(mutation);
-    }
-    cleanups_.erase(surfaceId);
-  }
-  
-  ShadowViewMutationList& getCleanupMutations(SurfaceId surfaceId){
-    return cleanups_[surfaceId];
-  }
-};
-
 struct LayoutAnimationsProxy : public MountingOverrideDelegate{
   mutable std::unordered_map<Tag, std::shared_ptr<RootNode>> rootNodeForTag;
   mutable std::unordered_map<Tag, std::shared_ptr<MutationNode>> nodeForTag;
@@ -213,20 +134,5 @@ struct LayoutAnimationsProxy : public MountingOverrideDelegate{
       const TransactionTelemetry& telemetry,
       ShadowViewMutationList mutations) const override;
 };
-
-static inline void updateLayoutMetrics(LayoutMetrics& layoutMetrics, Frame& frame){
-  if (frame.width) {
-    layoutMetrics.frame.size.width = *frame.width;
-  }
-  if (frame.height) {
-    layoutMetrics.frame.size.height = *frame.height;
-  }
-  if (frame.x) {
-    layoutMetrics.frame.origin.x = *frame.x;
-  }
-  if (frame.y) {
-    layoutMetrics.frame.origin.y = *frame.y;
-  }
-}
 
 }
