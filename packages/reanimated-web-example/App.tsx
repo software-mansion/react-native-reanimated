4146ed6bb9e90b884d7558f11c63c0ff82601cab
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-05-13 12:23:03 +0200

    cleanup v2

--- packages/reanimated-web-example/App.tsx
+++ packages/reanimated-web-example/App.tsx
@@ -1,7 +1,5 @@
-import { Text } from "react-native";
 import Apps from "reanimated-app";
 
 export default function App() {
-  // return <Text>App</Text>;
   return <Apps />;
 }
--- packages/reanimated-web-example/babel.config.js
+++ packages/reanimated-web-example/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function (api) {
-  api.cache(true);
+  const disableBabelPlugin = process.env.DISABLE_BABEL_PLUGIN === "1";
+  // https://babeljs.io/docs/en/config-files#apicache
+  api.cache.invalidate(() => disableBabelPlugin);
+  if (disableBabelPlugin) {
+    console.log("Starting Web example without Babel plugin.");
+  }
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: disableBabelPlugin ? [] : ["react-native-reanimated/plugin"],
   };
 };
