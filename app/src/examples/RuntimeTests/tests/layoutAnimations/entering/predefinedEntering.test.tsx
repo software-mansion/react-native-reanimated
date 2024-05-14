42693199d781794997ee187f7a992fb14ba52666
Aleksandra Cynk <ola.zn123@wp.pl>
2024-05-14 15:07:25 +0200

    Delete only

--- app/src/examples/RuntimeTests/tests/layoutAnimations/entering/predefinedEntering.test.tsx
+++ app/src/examples/RuntimeTests/tests/layoutAnimations/entering/predefinedEntering.test.tsx
@@ -110,7 +110,7 @@ async function getSnapshotUpdates(entering: any, waitTime: number, duration: num
   return updates;
 }
 
-describe.only('Test predefined entering', () => {
+describe('Test predefined entering', () => {
   describe('Entering on mount, no modifiers', async () => {
     test.each(ENTERING_SETS)('Test suite of ${0}In', async ([_setName, enteringSet, waitTime]) => {
       for (const entering of enteringSet) {
