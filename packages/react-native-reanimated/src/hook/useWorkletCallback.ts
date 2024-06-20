9e45fc1d32bbc1cba62ba97dc845d704a67ffda3
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-06-20 10:57:38 +0200

    run lineter

--- packages/react-native-reanimated/src/hook/useWorkletCallback.ts
+++ packages/react-native-reanimated/src/hook/useWorkletCallback.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react';
 import type { DependencyList } from './commonTypes';
 
 /**
- * @deprecated: use React.useCallback instead
+ * @deprecated use React.useCallback instead
  */
 export function useWorkletCallback<Args extends unknown[], ReturnValue>(
   worklet: (...args: Args) => ReturnValue,
