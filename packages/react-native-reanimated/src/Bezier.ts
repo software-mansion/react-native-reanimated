7cca7ba5c0fc4d904bbf016749403c69ffc6faa0
Ignacy Łątka <latkaignacy@gmail.com>
2024-07-09 13:18:44 +0200

    detects if Float32Array is supported and uses it instead of Array

--- packages/react-native-reanimated/src/Bezier.ts
+++ packages/react-native-reanimated/src/Bezier.ts
@@ -105,15 +105,11 @@ export function Bezier(
     return LinearEasing;
   }
 
-  // FIXME: Float32Array is not available in Hermes right now
-  //
-  // var float32ArraySupported = typeof Float32Array === 'function';
-  // const sampleValues = float32ArraySupported
-  // ? new Float32Array(kSplineTableSize)
-  // : new Array(kSplineTableSize);
-
   // Precompute samples table
-  const sampleValues = new Array(kSplineTableSize);
+  const float32ArraySupported = typeof Float32Array === 'function';
+  const sampleValues = float32ArraySupported
+    ? new Float32Array(kSplineTableSize)
+    : new Array(kSplineTableSize);
 
   for (let i = 0; i < kSplineTableSize; ++i) {
     sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
