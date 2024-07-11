9ad587f2eab876621bca1d1c636f7ce824c0a768
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-11 18:53:58 +0200

    Start writing makeMutable docs

--- apps/common-app/src/examples/EmptyExample.tsx
+++ apps/common-app/src/examples/EmptyExample.tsx
@@ -1,12 +1,31 @@
 import { Text, StyleSheet, View } from 'react-native';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  makeMutable,
+  useAnimatedReaction,
+  withTiming,
+} from 'react-native-reanimated';
 
-export default function EmptyExample() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-    </View>
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const mv = makeMutable(counter);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [mv]);
+
+  useAnimatedReaction(
+    () => mv.value,
+    (value) => {
+      console.log(value);
+    }
   );
 }
 
