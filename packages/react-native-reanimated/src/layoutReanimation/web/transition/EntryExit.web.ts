026c08f5e422fd8a8443d20bed2d4bb8f3b2391c
Michał Bert <michal.bert@swmansion.com>
2024-07-15 12:19:38 +0200

    Move adding transform into seperate function

--- packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
+++ packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
@@ -1,9 +1,43 @@
 'use strict';
-import type { AnimationData, TransitionData } from '../animationParser';
+import type {
+  AnimationData,
+  AnimationStyle,
+  TransitionData,
+} from '../animationParser';
 import { AnimationsData } from '../config';
 
 const ExitingFinalStep = 49;
 const EnteringStartStep = 50;
+
+type TransformData = {
+  translateX: string;
+  translateY: string;
+  scale: string;
+};
+
+// Layout transitions on web work in "reverse order". It means that the element is rendered at its destination and then, at the beginning of the animation,
+// we move it back to its starting point. In `EntryExit` transition we want `exiting` animation to start from original position.
+// This function is responsible for adding transition data into beginning of each keyframe step.
+// Doing so will ensure that the element will perform exiting animation from correct position.
+function addTransformToKeepPosition(
+  keyframeStyleData: Record<number, AnimationStyle>,
+  exitingAnimationStyle: Record<number, AnimationStyle>,
+  transformData: TransformData
+) {
+  for (const [timestamp, styles] of Object.entries(exitingAnimationStyle)) {
+    if (styles.transform !== undefined) {
+      // If transform was defined, we want to put transform from transition at the beginning, hence we use `unshift`
+      styles.transform.unshift(transformData);
+    } else {
+      // If transform was undefined, we simply add transform from transition
+      styles.transform = [transformData];
+    }
+
+    // Now, we want to squeeze exiting animation from range 0-100 into range 0-49 (remaining half will be used for entering animation)
+    const newTimestamp = parseInt(timestamp) / 2;
+    keyframeStyleData[`${Math.min(newTimestamp, ExitingFinalStep)}`] = styles;
+  }
+}
 
 export function EntryExitTransition(
   name: string,
@@ -16,7 +50,7 @@ export function EntryExitTransition(
     AnimationsData[transitionData.entering]
   );
 
-  const additionalData = {
+  const additionalData: TransformData = {
     translateX: `${transitionData.translateX}px`,
     translateY: `${transitionData.translateY}px`,
     scale: `${transitionData.scaleX},${transitionData.scaleY}`,
@@ -28,19 +62,11 @@ export function EntryExitTransition(
     duration: 300,
   };
 
-  for (const [timestamp, styles] of Object.entries(
-    exitingAnimationData.style
-  )) {
-    if (styles.transform !== undefined) {
-      styles.transform.unshift(additionalData);
-    } else {
-      styles.transform = [additionalData];
-    }
-
-    const newTimestamp = parseInt(timestamp) / 2;
-
-    keyframeData.style[`${Math.min(newTimestamp, ExitingFinalStep)}`] = styles;
-  }
+  addTransformToKeepPosition(
+    keyframeData.style,
+    exitingAnimationData.style,
+    additionalData
+  );
 
   for (const [timestamp, styles] of Object.entries(
     enteringAnimationData.style
