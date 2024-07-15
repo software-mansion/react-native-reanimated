7b3201816ccbb1a35e34b3ad3e14812dcf860a64
Michał Bert <michal.bert@swmansion.com>
2024-07-15 13:23:13 +0200

    Fix moving in Fade animations

--- packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
+++ packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
@@ -16,15 +16,16 @@ type TransformData = {
 };
 
 // Layout transitions on web work in "reverse order". It means that the element is rendered at its destination and then, at the beginning of the animation,
-// we move it back to its starting point. In `EntryExit` transition we want `exiting` animation to start from original position.
+// we move it back to its starting point.
 // This function is responsible for adding transition data into beginning of each keyframe step.
-// Doing so will ensure that the element will perform exiting animation from correct position.
+// Doing so will ensure that the element will perform animation from correct position.
 function addTransformToKeepPosition(
   keyframeStyleData: Record<number, AnimationStyle>,
-  exitingAnimationStyle: Record<number, AnimationStyle>,
-  transformData: TransformData
+  animationStyle: Record<number, AnimationStyle>,
+  transformData: TransformData,
+  isExiting: boolean
 ) {
-  for (const [timestamp, styles] of Object.entries(exitingAnimationStyle)) {
+  for (const [timestamp, styles] of Object.entries(animationStyle)) {
     if (styles.transform !== undefined) {
       // If transform was defined, we want to put transform from transition at the beginning, hence we use `unshift`
       styles.transform.unshift(transformData);
@@ -33,9 +34,12 @@ function addTransformToKeepPosition(
       styles.transform = [transformData];
     }
 
-    // Now, we want to squeeze exiting animation from range 0-100 into range 0-49 (remaining half will be used for entering animation)
     const newTimestamp = parseInt(timestamp) / 2;
-    keyframeStyleData[`${Math.min(newTimestamp, ExitingFinalStep)}`] = styles;
+    const index = isExiting
+      ? Math.min(newTimestamp, ExitingFinalStep) // We want to squeeze exiting animation from range 0-100 into range 0-49
+      : newTimestamp + EnteringStartStep; // Entering animation will start from 50 and go up to 100
+
+    keyframeStyleData[`${index}`] = styles;
   }
 }
 
@@ -50,10 +54,16 @@ export function EntryExitTransition(
     AnimationsData[transitionData.entering]
   );
 
-  const additionalData: TransformData = {
+  const additionalExitingData: TransformData = {
     translateX: `${transitionData.translateX}px`,
     translateY: `${transitionData.translateY}px`,
     scale: `${transitionData.scaleX},${transitionData.scaleY}`,
+  };
+
+  const additionalEnteringData: TransformData = {
+    translateX: `0px`,
+    translateY: `0px`,
+    scale: `1,1`,
   };
 
   const keyframeData: AnimationData = {
@@ -65,14 +75,16 @@ export function EntryExitTransition(
   addTransformToKeepPosition(
     keyframeData.style,
     exitingAnimationData.style,
-    additionalData
+    additionalExitingData,
+    true
   );
 
-  for (const [timestamp, styles] of Object.entries(
-    enteringAnimationData.style
-  )) {
-    keyframeData.style[parseInt(timestamp) / 2 + EnteringStartStep] = styles;
-  }
+  addTransformToKeepPosition(
+    keyframeData.style,
+    enteringAnimationData.style,
+    additionalEnteringData,
+    false
+  );
 
   return keyframeData;
 }
