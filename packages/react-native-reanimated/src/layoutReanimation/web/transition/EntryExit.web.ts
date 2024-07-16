b93b3aeabe3edd07c6249a59613ed589b4e5b483
Michał Bert <michal.bert@swmansion.com>
2024-07-16 10:40:55 +0200

    Hide component while switching animation

--- packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
+++ packages/react-native-reanimated/src/layoutReanimation/web/transition/EntryExit.web.ts
@@ -43,6 +43,37 @@ function addTransformToKeepPosition(
   }
 }
 
+// EntryExit transition consists of two animations - exiting and entering.
+// In Keyframes one cannot simply specify animation for given frame. Switching from one animation
+// to the other one between steps 49 and 50 may lead to flickers, since browser tries to interpolate
+// one step into the other. To avoid that, we set components' `opacity` to 0 right before switching animation
+// and set it again to 1 when component is in right position. Hiding component between animations
+// prevents flickers.
+function hideComponentBetweenAnimations(
+  keyframeStyleData: Record<number, AnimationStyle>
+) {
+  // We have to take into account that some animations have already defined `opacity`.
+  // In that case, we don't want to override it.
+  const opacityInStep = new Map<number, number>();
+
+  if (keyframeStyleData[0].opacity === undefined) {
+    opacityInStep.set(48, 1);
+    opacityInStep.set(49, 0);
+  }
+
+  if (keyframeStyleData[50].opacity === undefined) {
+    opacityInStep.set(50, 0);
+    opacityInStep.set(51, 1);
+  }
+
+  for (const [step, opacity] of opacityInStep) {
+    keyframeStyleData[step] = {
+      ...keyframeStyleData[step],
+      opacity: opacity,
+    };
+  }
+}
+
 export function EntryExitTransition(
   name: string,
   transitionData: TransitionData
@@ -85,6 +116,8 @@ export function EntryExitTransition(
     additionalEnteringData,
     false
   );
+
+  hideComponentBetweenAnimations(keyframeData.style);
 
   return keyframeData;
 }
