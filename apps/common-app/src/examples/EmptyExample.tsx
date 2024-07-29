1fd869fd6199bd1a01f2711e6e2ec902af1d440a
Aleksandra Cynk <ola.zn123@wp.pl>
2024-07-29 12:20:44 +0200

    Demo bug

--- apps/common-app/src/examples/EmptyExample.tsx
+++ apps/common-app/src/examples/EmptyExample.tsx
@@ -1,11 +1,33 @@
-import { Text, StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Button } from 'react-native';
 import React from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function EmptyExample() {
+  const leftSV = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return { left: withTiming(leftSV.value) };
+  });
+
+  const animatedStyleDelayed = useAnimatedStyle(() => {
+    return { left: withDelay(0, withTiming(leftSV.value)) };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Button
+        title="toggle"
+        onPress={() => {
+          leftSV.value = leftSV.value === 200 ? 0 : 200;
+        }}
+      />
+      <Animated.View style={[styles.box, animatedStyle]} />
+      <Animated.View style={[styles.box, animatedStyleDelayed]} />
     </View>
   );
 }
@@ -13,7 +35,13 @@ export default function EmptyExample() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  box: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'navy',
+    borderRadius: 5,
+    margin: 5,
   },
 });
