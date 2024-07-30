a5e10c5fee22644b35ae59b210c0cee639a087da
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-30 12:19:52 +0200

    Ignore first render in .value read/write warnings

--- packages/react-native-reanimated/src/mutables.ts
+++ packages/react-native-reanimated/src/mutables.ts
@@ -2,13 +2,22 @@
 import { shouldBeUseWeb } from './PlatformChecker';
 import type { Mutable } from './commonTypes';
 import { ReanimatedError } from './errors';
-
+import { isFirstReactRender, isReactRendering } from './reactUtils';
 import { shareableMappingCache } from './shareableMappingCache';
 import { makeShareableCloneRecursive } from './shareables';
 import { executeOnUIRuntimeSync, runOnUI } from './threads';
 import { valueSetter } from './valueSetter';
 
 const SHOULD_BE_USE_WEB = shouldBeUseWeb();
+
+const invalidReadWarning =
+  '[Reanimated] Reading from `value` during component render. Please ensure that you do not access the `value` property while React is rendering a component.';
+const invalidWriteWarning =
+  '[Reanimated] Writing to `value` during component render. Please ensure that you do not access the `value` property while React is rendering a component.';
+
+function shouldWarnInvalidAccess() {
+  return isReactRendering() && !isFirstReactRender();
+}
 
 type Listener<Value> = (newValue: Value) => void;
 
@@ -93,12 +102,19 @@ function makeMutableNative<Value>(initial: Value): Mutable<Value> {
 
   const mutable: Mutable<Value> = {
     get value(): Value {
+      if (shouldWarnInvalidAccess()) {
+        console.warn(invalidReadWarning);
+      }
       const uiValueGetter = executeOnUIRuntimeSync((sv: Mutable<Value>) => {
         return sv.value;
       });
       return uiValueGetter(mutable);
     },
     set value(newValue) {
+      if (shouldWarnInvalidAccess()) {
+        console.log('warn 2');
+        console.warn(invalidWriteWarning);
+      }
       runOnUI(() => {
         mutable.value = newValue;
       })();
@@ -149,6 +165,9 @@ function makeMutableWeb<Value>(initial: Value): Mutable<Value> {
       return value;
     },
     set value(newValue) {
+      if (shouldWarnInvalidAccess()) {
+        console.warn(invalidWriteWarning);
+      }
       valueSetter(mutable, newValue);
     },
 
--- packages/react-native-reanimated/src/reactUtils.ts
+++ packages/react-native-reanimated/src/reactUtils.ts
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export function isReactRendering() {
+function getCurrentReactOwner() {
   // @ts-expect-error React secret internals aren't typed
-  return !!React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner.current;
+  return React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner.current;
+}
+
+export function isReactRendering() {
+  return !!getCurrentReactOwner();
+}
+
+export function isFirstReactRender() {
+  const currentOwner = getCurrentReactOwner()
+  // This is not null only after the first render and stores all the
+  // data from the previous render.
+  const alternate = currentOwner?.alternate;
+  return isReactRendering() && !alternate;
 }
