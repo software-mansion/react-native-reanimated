b71a8d8f9d2ce9c22fc46f9d6e1236e3deb14260
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-30 12:31:37 +0200

    Add separate example in the example app

--- apps/common-app/src/examples/EmptyExample.tsx
+++ apps/common-app/src/examples/EmptyExample.tsx
@@ -1,41 +1,11 @@
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
-import React, { useEffect } from 'react';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
+import React from 'react';
 
 export default function EmptyExample() {
-  const [counter, setCounter] = React.useState(0);
-  const sv = useSharedValue(0);
-
-  // logs writing to `value`... warning
-  sv.value = counter;
-  // logs reading from `value`... warning
-  console.log('shared value:', sv.value);
-
-  useEffect(() => {
-    // no warning is logged
-    sv.value = counter;
-    // no warning is logged
-    console.log('useEffect shared value:', sv.value);
-  }, [sv, counter]);
-
-  const reRender = () => {
-    setCounter((prev) => prev + 1);
-  };
-
-  const animatedBarStyle = useAnimatedStyle(() => ({
-    transform: [{ scaleX: withTiming(Math.sin((sv.value * Math.PI) / 10)) }],
-  }));
-
   return (
     <View style={styles.container}>
-      <Button title="Re-render" onPress={reRender} />
-      <Text>Counter: {counter}</Text>
-      <Animated.View style={[styles.bar, animatedBarStyle]} />
+      <Text>Hello world!</Text>
     </View>
   );
 }
@@ -45,10 +15,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  bar: {
-    height: 20,
-    backgroundColor: 'blue',
-    width: '100%',
   },
 });
