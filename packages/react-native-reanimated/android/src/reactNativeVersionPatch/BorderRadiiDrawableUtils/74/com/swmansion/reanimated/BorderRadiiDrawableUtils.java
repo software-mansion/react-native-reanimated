76532847a58105a198e39ecb40915d82dca59616
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-07-30 10:29:01 +0200

    Fix spotless config

--- packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/74/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
+++ packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/74/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
@@ -2,23 +2,21 @@ package com.swmansion.reanimated;
 
 import android.graphics.drawable.Drawable;
 import android.view.View;
-
 import com.facebook.react.views.view.ReactViewBackgroundDrawable;
-import com.swmansion.reanimated.ReactNativeUtils;
 
 public class BorderRadiiDrawableUtils {
-    public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
-        Drawable background = view.getBackground();
-        if (background instanceof ReactViewBackgroundDrawable) {
-            ReactViewBackgroundDrawable drawable = (ReactViewBackgroundDrawable) background;
-            return new ReactNativeUtils.BorderRadii(
-                    drawable.getFullBorderRadius(),
-                    drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.TOP_LEFT),
-                    drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.TOP_RIGHT),
-                    drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.BOTTOM_LEFT),
-                    drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.BOTTOM_RIGHT));
-        } else {
-            return new ReactNativeUtils.BorderRadii(0, 0, 0, 0, 0);
-        }
+  public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
+    Drawable background = view.getBackground();
+    if (background instanceof ReactViewBackgroundDrawable) {
+      ReactViewBackgroundDrawable drawable = (ReactViewBackgroundDrawable) background;
+      return new ReactNativeUtils.BorderRadii(
+          drawable.getFullBorderRadius(),
+          drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.TOP_LEFT),
+          drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.TOP_RIGHT),
+          drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.BOTTOM_LEFT),
+          drawable.getBorderRadius(ReactViewBackgroundDrawable.BorderRadiusLocation.BOTTOM_RIGHT));
+    } else {
+      return new ReactNativeUtils.BorderRadii(0, 0, 0, 0, 0);
     }
+  }
 }
--- packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
+++ packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
@@ -2,30 +2,25 @@ package com.swmansion.reanimated;
 
 import android.graphics.drawable.Drawable;
 import android.view.View;
-
-import com.facebook.react.uimanager.drawable.CSSBackgroundDrawable;
 import com.facebook.react.uimanager.LengthPercentage;
-import com.facebook.react.uimanager.style.BorderRadiusProp;
+import com.facebook.react.uimanager.drawable.CSSBackgroundDrawable;
 import com.facebook.react.uimanager.style.ComputedBorderRadius;
-import com.swmansion.reanimated.ReactNativeUtils;
-
-import java.util.Objects;
 
 public class BorderRadiiDrawableUtils {
-    public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
-        Drawable background = view.getBackground();
-        if (background instanceof CSSBackgroundDrawable drawable) {
-            LengthPercentage uniform = drawable.getBorderRadius().getUniform();
-            float full = uniform != null ? uniform.resolve(view.getWidth(), view.getHeight()) : Float.NaN;
-            ComputedBorderRadius computedBorderRadius = drawable.getComputedBorderRadius();
-            return new ReactNativeUtils.BorderRadii(
-                    full,
-                    computedBorderRadius.getTopLeft(),
-                    computedBorderRadius.getTopRight(),
-                    computedBorderRadius.getBottomLeft(),
-                    computedBorderRadius.getBottomRight());
-        } else {
-            return new ReactNativeUtils.BorderRadii(0, 0, 0, 0, 0);
-        }
+  public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
+    Drawable background = view.getBackground();
+    if (background instanceof CSSBackgroundDrawable drawable) {
+      LengthPercentage uniform = drawable.getBorderRadius().getUniform();
+      float full = uniform != null ? uniform.resolve(view.getWidth(), view.getHeight()) : Float.NaN;
+      ComputedBorderRadius computedBorderRadius = drawable.getComputedBorderRadius();
+      return new ReactNativeUtils.BorderRadii(
+          full,
+          computedBorderRadius.getTopLeft(),
+          computedBorderRadius.getTopRight(),
+          computedBorderRadius.getBottomLeft(),
+          computedBorderRadius.getBottomRight());
+    } else {
+      return new ReactNativeUtils.BorderRadii(0, 0, 0, 0, 0);
     }
+  }
 }
