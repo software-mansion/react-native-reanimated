071e5139b8a3b9fe51cb5921a1d7d13ae1437670
Bartłomiej Błoniarz <56109050+bartlomiejbloniarz@users.noreply.github.com>
2024-07-30 15:03:06 +0200

    Modify reanimated props updates (#6330)
    ## Summary
    
    This PR changes the way `performOperations` and `ReanimatedCommitHook`
    are synchronized. The current implementations faces 2 problems:
    
    1. Updates that come through `performOperations` after
    `pleaseSkipReanimatedCommit` is called in the commit hook will be
    deferred until the next commit. Usually it's fine since the next commit
    will be triggered by the next animation frame. But if there was a
    singular update scheduled through Reanimated, we might not see the
    change for a long time. This issue is more thoroughly explained in [this
    issue](https://github.com/software-mansion/react-native-reanimated/issues/6245).
    
    2. In `ReanimatedCommitMarker` there is an assumption that there can be
    at most one commit happening on a given thread (i.e. there can't be
    nested commits). This isn't true, since there can be an event listener
    that commits some changes in a reaction to a native mount/unmount (which
    is a part of the commit function). [This exact scenario was observed in
    the New Expensify App with
    `react-native-keyboard-controller`](https://github.com/Expensify/App/issues/44437#issuecomment-2250696773).
    At first I thought this is a mistake, but [this PR in
    RN](https://github.com/facebook/react-native/commit/5f0435fb852cbb8abfe6cab77784ebddf1ad38ac)
    seems to allow for scenarios like that.
    
    Applied fixes:
    
    ad 1. Now instead of resetting the skip flag in reanimated after a
    transaction is mounted (via MountHook), we reset the flag whenever a
    non-empty batch is read in `performOperations`. This ensures that we
    don't make an unnecessary commit, but never skip any updates.
    
    ad 2. Now we mark reanimated commits through
    `ReanimatedCommitShadowNode`. This is a class derived from ShadowNode
    that allows us to modify the root nodes traits_ (and add our custom
    trait). This ensures that even if the root node gets cloned it will
    retain the information. We couldn't derive from `RootShadowNode` since
    it is a final class.
    
    ## Test plan

--- packages/react-native-reanimated/Common/cpp/NativeModules/NativeReanimatedModule.cpp
+++ packages/react-native-reanimated/Common/cpp/NativeModules/NativeReanimatedModule.cpp
@@ -18,7 +18,7 @@
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #include <react/renderer/scheduler/Scheduler.h>
-#include "ReanimatedCommitMarker.h"
+#include "ReanimatedCommitShadowNode.h"
 #include "ShadowTreeCloner.h"
 #endif
 
@@ -648,6 +648,10 @@ void NativeReanimatedModule::performOperations() {
   {
     auto lock = propsRegistry_->createLock();
 
+    if (copiedOperationsQueue.size() > 0) {
+      propsRegistry_->resetReanimatedSkipCommitFlag();
+    }
+
     // remove recently unmounted ShadowNodes from PropsRegistry
     if (!tagsToRemove_.empty()) {
       for (auto tag : tagsToRemove_) {
@@ -713,10 +717,6 @@ void NativeReanimatedModule::performOperations() {
   const auto &shadowTreeRegistry = uiManager_->getShadowTreeRegistry();
 
   shadowTreeRegistry.visit(surfaceId_, [&](ShadowTree const &shadowTree) {
-    // Mark the commit as Reanimated commit so that we can distinguish it
-    // in ReanimatedCommitHook.
-    ReanimatedCommitMarker commitMarker;
-
     shadowTree.commit(
         [&](RootShadowNode const &oldRootShadowNode)
             -> RootShadowNode::Unshared {
@@ -736,7 +736,19 @@ void NativeReanimatedModule::performOperations() {
             }
 #endif
           }
-          return cloneShadowTreeWithNewProps(oldRootShadowNode, propsMap);
+
+          auto rootNode =
+              cloneShadowTreeWithNewProps(oldRootShadowNode, propsMap);
+
+          // Mark the commit as Reanimated commit so that we can distinguish it
+          // in ReanimatedCommitHook.
+
+          auto reaShadowNode =
+              std::reinterpret_pointer_cast<ReanimatedCommitShadowNode>(
+                  rootNode);
+          reaShadowNode->setReanimatedCommitTrait();
+
+          return rootNode;
         },
         { /* .enableStateReconciliation = */
           false,
@@ -834,10 +846,6 @@ void NativeReanimatedModule::initializeFabric(
 
   commitHook_ =
       std::make_shared<ReanimatedCommitHook>(propsRegistry_, uiManager_);
-#if REACT_NATIVE_MINOR_VERSION >= 73
-  mountHook_ =
-      std::make_shared<ReanimatedMountHook>(propsRegistry_, uiManager_);
-#endif
 }
 
 void NativeReanimatedModule::initializeLayoutAnimations() {
