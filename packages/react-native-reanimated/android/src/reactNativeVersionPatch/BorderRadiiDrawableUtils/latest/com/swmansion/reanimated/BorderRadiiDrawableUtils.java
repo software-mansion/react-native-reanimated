876cb36b96d78b699920fb0740d1c5a94581abfe
szydlovsky <9szydlowski9@gmail.com>
2024-08-01 14:15:31 +0200

    fix java drawable cast

--- packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
+++ packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
@@ -9,7 +9,8 @@ import com.facebook.react.uimanager.style.ComputedBorderRadius;
 public class BorderRadiiDrawableUtils {
   public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
     Drawable background = view.getBackground();
-    if (background instanceof CSSBackgroundDrawable drawable) {
+    if (background instanceof CSSBackgroundDrawable) {
+      CSSBackgroundDrawable drawable = (CSSBackgroundDrawable) background;
       LengthPercentage uniform = drawable.getBorderRadius().getUniform();
       float full = uniform != null ? uniform.resolve(view.getWidth(), view.getHeight()) : Float.NaN;
       ComputedBorderRadius computedBorderRadius = drawable.getComputedBorderRadius();
