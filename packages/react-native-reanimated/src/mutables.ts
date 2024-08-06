9bb0a71c1fbad59dcdecbb59ce5fcc355a89f165
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-08-06 12:23:23 +0200

    Clean up dev checks

--- packages/react-native-reanimated/src/mutables.ts
+++ packages/react-native-reanimated/src/mutables.ts
@@ -15,7 +15,7 @@ function shouldWarnAboutAccessDuringRender() {
 }
 
 function checkInvalidReadDuringRender() {
-  if (__DEV__ && shouldWarnAboutAccessDuringRender()) {
+  if (shouldWarnAboutAccessDuringRender()) {
     console.warn(
       '[Reanimated] Reading from `value` during component render. Please ensure that you do not access the `value` property while React is rendering a component.'
     );
@@ -23,7 +23,7 @@ function checkInvalidReadDuringRender() {
 }
 
 function checkInvalidWriteDuringRender() {
-  if (__DEV__ && shouldWarnAboutAccessDuringRender()) {
+  if (shouldWarnAboutAccessDuringRender()) {
     console.warn(
       '[Reanimated] Writing to `value` during component render. Please ensure that you do not access the `value` property while React is rendering a component.'
     );
