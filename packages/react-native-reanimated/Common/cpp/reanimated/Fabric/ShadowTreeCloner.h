f109d728e98fbabc5f1a9d53bddbe62d6af81052
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-08-07 15:21:36 +0200

    Remove formatting changes from cpp files

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.h
@@ -4,8 +4,8 @@
 #include <react/renderer/core/PropsParserContext.h>
 #include <react/renderer/uimanager/UIManager.h>
 
-#include <memory>
 #include <type_traits>
+#include <memory>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -15,10 +15,8 @@ using namespace react;
 
 namespace reanimated {
 
-using PropsMap =
-    std::unordered_map<const ShadowNodeFamily *, std::vector<RawProps>>;
-using ChildrenMap =
-    std::unordered_map<const ShadowNodeFamily *, std::unordered_set<int>>;
+using PropsMap = std::unordered_map<const ShadowNodeFamily *, std::vector<RawProps>>;
+using ChildrenMap = std::unordered_map<const ShadowNodeFamily *, std::unordered_set<int>>;
 
 RootShadowNode::Unshared cloneShadowTreeWithNewProps(
     const RootShadowNode &oldRootNode,
