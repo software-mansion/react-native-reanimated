be39c8b93323fcc30949721044328b8514a1fd82
Michał Bert <michal.bert@swmansion.com>
2024-08-11 15:33:09 +0200

    Import as type

--- packages/react-native-reanimated/src/platformFunctions/findNodeHandle.web.ts
+++ packages/react-native-reanimated/src/platformFunctions/findNodeHandle.web.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentClass } from 'react';
+import type { Component, ComponentClass } from 'react';
 
 export function findNodeHandle(
   componentOrHandle: null | number | Component<any, any> | ComponentClass<any>
