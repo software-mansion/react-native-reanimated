3edaaad7208fabe97a45e9cd5127b19a77bd8d8a
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-08-17 22:08:04 +0200

    chore: improve build pipeline

---
+++ packages/react-native-reanimated/plugin/bundle.js
@@ -0,0 +1,14 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['./lib/plugin.js'],
+  outfile: 'index.js',
+  bundle: true,
+  packages: 'external',
+  external: ['../package.json'],
+  allowOverwrite: true,
+  logLevel: 'warning',
+  platform: 'node',
+  sourcemap: 'linked',
+  minify: true,
+});
