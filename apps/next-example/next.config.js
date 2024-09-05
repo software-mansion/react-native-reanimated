d0e61069250f2f2f42194f56a83a4a8a81df9c89
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-09-05 21:13:21 +0200

    chore: fix Next types

--- apps/next-example/next.config.js
+++ apps/next-example/next.config.js
@@ -1,6 +1,5 @@
 const { withExpo } = require('@expo/next-adapter');
 
-// @ts-expect-error
 const withPlugins = require('next-compose-plugins');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -17,7 +16,6 @@ module.exports = withPlugins([withBundleAnalyzer, withExpo], {
     ignoreDuringBuilds: true,
   },
   transpilePackages: ['react-native-reanimated', 'react-native', 'expo'],
-  // @ts-expect-error
   webpack(config, _options) {
     if (disableMinification) {
       config.optimization.minimizer = [];
