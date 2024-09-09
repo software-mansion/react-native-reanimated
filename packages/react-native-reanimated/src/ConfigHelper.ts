501c892f5b4dc510e9da4ccf3cd24bcb674d1de3
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-09-09 12:49:41 +0200

    Fix linting issue

--- packages/react-native-reanimated/src/ConfigHelper.ts
+++ packages/react-native-reanimated/src/ConfigHelper.ts
@@ -58,11 +58,11 @@ export function addWhitelistedUIProps(props: Record<string, boolean>): void {
 }
 
 /**
- * Updates Reanimated logger config with the user-provided configuration.
- * Will affect Reanimated code executed after call to this function so
- * it should be called before any Reanimated code is executed to take effect.
- * Each call to this function will override the previous configuration
- * (it's recommended to call it only once).
+ * Updates Reanimated logger config with the user-provided configuration. Will
+ * affect Reanimated code executed after call to this function so it should be
+ * called before any Reanimated code is executed to take effect. Each call to
+ * this function will override the previous configuration (it's recommended to
+ * call it only once).
  *
  * @param config - The new logger configuration to apply.
  */
