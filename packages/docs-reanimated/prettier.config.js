f6345a5c4412e0a7ff17e5d376ca2412ff826824
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-09-09 14:53:50 +0200

    chore: Add context

--- packages/docs-reanimated/prettier.config.js
+++ packages/docs-reanimated/prettier.config.js
@@ -1,5 +1,8 @@
 const defaultConfig = require("../../prettier.config");
 
+// We can't bump to Prettier 3.x because current version of Docusaurus
+// crashes on build with it.
+
 module.exports = {
   ...defaultConfig,
   // Override plugins that don't work with Prettier 2.x
