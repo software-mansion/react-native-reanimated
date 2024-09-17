456d39e6cb2364b4e81a55bac025edea86fe31f8
Mikołaj Szydłowski <9szydlowski9@gmail.com>
2024-09-17 12:23:22 +0200

    Easing example (#33)
    * add easing examples
    
    * make stepEnd observable
    
    * review changes

--- apps/fabric-example/src/examples/AnimationSettings/AnimationTimingFunction.tsx
+++ apps/fabric-example/src/examples/AnimationSettings/AnimationTimingFunction.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from 'react-native';
 import type { CSSAnimationConfig } from 'react-native-reanimated';
 import { colors, radius, sizes } from '../../theme';
-import Animated from 'react-native-reanimated';
+import Animated, { cubicBezier, linear, steps } from 'react-native-reanimated';
 import { ExampleScreen } from './components';
 
 export default function AnimationTimingFunction() {
@@ -32,18 +32,80 @@ export default function AnimationTimingFunction() {
         {
           title: 'Predefined Easings',
           items: [
+            { animationTimingFunction: 'ease', label: 'ease' },
+            { animationTimingFunction: 'easeIn', label: 'easeIn' },
+            { animationTimingFunction: 'easeOut', label: 'easeOut' },
+            { animationTimingFunction: 'easeInOut', label: 'easeInOut' },
             { animationTimingFunction: 'linear', label: 'linear' },
+            { animationTimingFunction: 'stepStart', label: 'stepStart' },
             {
-              animationTimingFunction: 'ease-in-out-back',
-              label: 'ease-in-out-back',
+              animationTimingFunction: 'stepEnd',
+              animationIterationCount: 1,
+              animationFillMode: 'forwards',
+              label: 'stepEnd',
             },
           ],
         },
         {
-          title: 'Cube Bezier Easings',
-          description: 'Some examples of cubic bezier easings.',
+          title: 'Cubic Bezier Easing',
+          description:
+            'Specify a Bézier curve to shape the progress of an animation or a transition. It is defined by two control points (x1, y1, x2, y2) that mathematically describe the curve.',
           items: [
-            // TODO - add cubic bezier examples
+            {
+              animationTimingFunction: cubicBezier(0.2, 0.9, 0.8, 0.25),
+              label: 'cubicBezier(\n 0.2, 0.9, 0.8, 0.25\n)',
+            },
+            {
+              animationTimingFunction: cubicBezier(0.1, 1.5, 0.8, 1.5),
+              label: 'cubicBezier(\n 0.1, 1.5, 0.8, 1.5\n)',
+            },
+            {
+              animationTimingFunction: cubicBezier(0.3, 0, 1, 0),
+              label: 'cubicBezier(\n 0.3, 0, 1, 0\n)',
+            },
+          ],
+        },
+        {
+          title: 'Linear Easing with points',
+          description:
+            "Specify a simple polygonal chain that always starts at an x-value of 0 and ends at an x-value of 1. You can either specify the points' y and x coordinates or leave the x coordinates to be inferred.",
+          items: [
+            {
+              animationTimingFunction: linear([0, { y: 0.25, x: '75%' }, 1]),
+              label: "linear([\n 0,\n {y: 0.25, x: '75%'},\n 1\n])",
+            },
+            {
+              animationTimingFunction: linear([
+                { y: 0.6, x: '0%' },
+                { y: 0.1, x: '50%' },
+                { y: 1, x: '100%' },
+              ]),
+              label:
+                "linear([\n {y: 0.6, x: '0%'},\n {y: 0.1, x: '50%'},\n {y: 1, x: '100%'}\n])",
+            },
+          ],
+        },
+        {
+          title: 'Steps easing',
+          description:
+            'Creates an easing function that makes given number of even steps over increasing y-values. The second argument is a modifier that adds jumps before or after the steps.',
+          items: [
+            {
+              animationTimingFunction: steps(2, 'jumpStart'),
+              label: "steps(2, 'jumpStart') or\nsteps(2, 'start')",
+            },
+            {
+              animationTimingFunction: steps(4, 'jumpEnd'),
+              label: "steps(4, 'jumpEnd') or\nsteps(4, 'end')",
+            },
+            {
+              animationTimingFunction: steps(5, 'jumpNone'),
+              label: "steps(5, 'jumpNone')",
+            },
+            {
+              animationTimingFunction: steps(3, 'jumpBoth'),
+              label: "steps(3, 'jumpBoth')",
+            },
           ],
         },
       ]}
