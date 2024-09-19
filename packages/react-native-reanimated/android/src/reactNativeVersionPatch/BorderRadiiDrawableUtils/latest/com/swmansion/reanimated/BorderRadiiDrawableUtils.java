e0a6cc47a7dba2884e0c19b5f97b8864b4d6b86c
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2024-09-19 17:23:23 +0200

    refactor BorderRadiiDrawableUtils

--- packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
+++ packages/react-native-reanimated/android/src/reactNativeVersionPatch/BorderRadiiDrawableUtils/latest/com/swmansion/reanimated/BorderRadiiDrawableUtils.java
@@ -7,43 +7,26 @@ import com.facebook.react.uimanager.LengthPercentage;
 import com.facebook.react.uimanager.style.BorderRadiusProp;
 
 public class BorderRadiiDrawableUtils {
-  public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
-    Rect bounds = view.getBackground().getBounds();
-    LengthPercentage full =
-        BackgroundStyleApplicator.getBorderRadius(view, BorderRadiusProp.BORDER_RADIUS);
-    LengthPercentage topLeft =
-        BackgroundStyleApplicator.getBorderRadius(view, BorderRadiusProp.BORDER_TOP_LEFT_RADIUS);
-    LengthPercentage topRight =
-        BackgroundStyleApplicator.getBorderRadius(view, BorderRadiusProp.BORDER_TOP_RIGHT_RADIUS);
-    LengthPercentage bottomLeft =
-        BackgroundStyleApplicator.getBorderRadius(view, BorderRadiusProp.BORDER_BOTTOM_LEFT_RADIUS);
-    LengthPercentage bottomRight =
-        BackgroundStyleApplicator.getBorderRadius(
-            view, BorderRadiusProp.BORDER_BOTTOM_RIGHT_RADIUS);
-
+  private static float getRadiusForCorner(View view, BorderRadiusProp corner, float defaultValue) {
     // This logic is valid only when `LengthPercentage.getType()` equals
     // `LengthPercentageType.POINT`. However, since the `SET` logic is only applicable on paper,
     // which only supports the `POINT` type, it's safe to assume that the type will always be
     // `POINT`.
     // https://github.com/facebook/react-native/blob/6d51c5cd8ef817fe30f01a10f2af682fc02a5fb7/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.java#L164
+    LengthPercentage length = BackgroundStyleApplicator.getBorderRadius(view, corner);
+    if (length == null) {
+      return defaultValue;
+    }
+    Rect bounds = view.getBackground().getBounds();
+    return length.resolve(bounds.width(), bounds.height()).toPixelFromDIP().getHorizontal();
+  }
+
+  public static ReactNativeUtils.BorderRadii getBorderRadii(View view) {
     return new ReactNativeUtils.BorderRadii(
-        full == null
-            ? 0
-            : full.resolve(bounds.width(), bounds.height()).toPixelFromDIP().getHorizontal(),
-        topLeft == null
-            ? Float.NaN
-            : topLeft.resolve(bounds.width(), bounds.height()).toPixelFromDIP().getHorizontal(),
-        topRight == null
-            ? Float.NaN
-            : topRight.resolve(bounds.width(), bounds.height()).toPixelFromDIP().getHorizontal(),
-        bottomLeft == null
-            ? Float.NaN
-            : bottomLeft.resolve(bounds.width(), bounds.height()).toPixelFromDIP().getHorizontal(),
-        bottomRight == null
-            ? Float.NaN
-            : bottomRight
-                .resolve(bounds.width(), bounds.height())
-                .toPixelFromDIP()
-                .getHorizontal());
+        getRadiusForCorner(view, BorderRadiusProp.BORDER_RADIUS, 0),
+        getRadiusForCorner(view, BorderRadiusProp.BORDER_TOP_LEFT_RADIUS, Float.NaN),
+        getRadiusForCorner(view, BorderRadiusProp.BORDER_TOP_RIGHT_RADIUS, Float.NaN),
+        getRadiusForCorner(view, BorderRadiusProp.BORDER_BOTTOM_LEFT_RADIUS, Float.NaN),
+        getRadiusForCorner(view, BorderRadiusProp.BORDER_BOTTOM_RIGHT_RADIUS, Float.NaN));
   }
 }
