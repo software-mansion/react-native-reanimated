963eb6e801373ce5f9d3272d24fc225e72787548
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-09-23 22:53:42 +0200

    Update include validator script

--- packages/react-native-reanimated/scripts/validate-apple.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if grep -Rn apple -e '#import "' ; then
-    echo "Found #import with double quotes in \`ios/\` directory. Convert them to angle-bracket imports, e.g. \`#import <RNReanimated/REAModule.h>\`." && false
-fi
