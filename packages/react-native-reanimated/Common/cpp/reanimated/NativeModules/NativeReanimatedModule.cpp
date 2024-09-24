9b46cbfbd6c6b9537c19452ee28ebfe6e3e31cb0
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-09-24 09:38:25 +0200

    Improve ifdefs

--- packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/NativeReanimatedModule.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/NativeReanimatedModule.cpp
@@ -22,10 +22,10 @@
 #include <react/renderer/scheduler/Scheduler.h>
 #include <react/renderer/uimanager/UIManagerBinding.h>
 #include <react/renderer/uimanager/primitives.h>
-#if REACT_NATIVE_MINOR_VERSION >= 73 && defined(RCT_NEW_ARCH_ENABLED)
+#if REACT_NATIVE_MINOR_VERSION >= 73
 #include <react/utils/CoreFeatures.h>
-#endif
-#endif
+#endif // REACT_NATIVE_MINOR_VERSION
+#endif // RCT_NEW_ARCH_ENABLED
 
 #include <functional>
 #include <iomanip>
