af16d243dce12ce0deb27f991af4351fad7fbb83
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-09-24 11:29:29 +0200

    Disallow Reanimated includes in Worklets

--- packages/react-native-reanimated/scripts/validate-includes.sh
+++ packages/react-native-reanimated/scripts/validate-includes.sh
@@ -16,3 +16,6 @@ if grep -Rn Common/cpp -e '#include "' ; then
     echo "Found #include with double quotes in \`Common/cpp/\` directory. Convert them to angle-bracket imports, e.g. \`#include <reanimated/Fabric/PropsRegistry.h>\`." && false
 fi
 
+if grep -Rn Common/cpp/worklets -e '#include <reanimated/' ; then
+    echo "Found \`#include <reanimated/**/*.h>\` in \`Common/cpp/worklets/'\` directory which is not allowed." && false
+fi
