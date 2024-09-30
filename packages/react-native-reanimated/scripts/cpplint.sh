2260cabff5a92e96c9f40a9efcb42d7778601b43
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-09-30 18:31:20 +0200

    chore: Enable namespace and include rules

--- packages/react-native-reanimated/scripts/cpplint.sh
+++ packages/react-native-reanimated/scripts/cpplint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which cpplint >/dev/null; then
-  cpplint --linelength=230 --filter=-legal/copyright,-readability/todo,-build/namespaces,-whitespace/comments,-whitespace/parens,-build/c++11,-runtime/references --quiet --recursive "$@"
+  cpplint --linelength=230 --filter=-legal/copyright,-readability/todo,-build/namespaces,-whitespace/comments,-whitespace/parens,-build/c++11,-runtime/references,+build/namespaces_headers,+build/namespaces,+build/endif_comment --quiet --recursive "$@"
 else
   echo "error: cpplint not installed, download from https://github.com/cpplint/cpplint" 1>&2
   exit 1
