f9a082c93aead2915c403949d711488aac89e517
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-10-04 10:34:09 +0200

    chore: Update cpp validation scripts

--- packages/react-native-reanimated/scripts/validate-common.sh
+++ packages/react-native-reanimated/scripts/validate-common.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 if grep -Rn Common/cpp -e '^#include "'; then
-    echo 'Found `#include "..."` in `Common/cpp/` directory. Convert it to `#include <...>`, e.g. `#include <reanimated/Fabric/PropsRegistry.h>`.' && false
+    echo 'Found `#include "..."` in `Common/cpp/` directory. Convert it to `#include <...>`, e.g. `#include <reanimated/Fabric/PropsRegistry.h>`.'
+    exit 1
 fi
 
-if grep -Rn Common/cpp/worklets -e '^#include <reanimated/'; then
-    echo 'Found `#include <reanimated/...>` in `Common/cpp/worklets/` directory which is not allowed.' && false
+if grep -Rn Common/cpp/worklets -e '^#include .*reanimated\/'; then
+    echo 'Found `#include` from Reanimated in Worklets which is not allowed.'
+    exit 1
+fi
+
+if grep -Rn android/src/main/cpp/worklets -e 'reanimated::'; then
+    echo 'Found `reanimated::` usage in Worklets which is not allowed.'
+    exit 1
 fi
