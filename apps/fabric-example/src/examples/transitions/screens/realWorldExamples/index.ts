e3e30585552cd9f9f4af0ff842343e3fd84f3289
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-10-27 18:11:47 +0100

    feat: Circular popup menu example and route card implementation (#91)
    * Add circular popup menu example
    
    * Finish circular menu example, add route card
    
    * Remove console log

--- apps/fabric-example/src/examples/transitions/screens/realWorldExamples/index.ts
+++ apps/fabric-example/src/examples/transitions/screens/realWorldExamples/index.ts
@@ -1,9 +1,11 @@
 import AppSettings from './AppSettings';
+import CircularPopupMenu from './CircularPopupMenu';
 import FlexGallery from './FlexGallery';
 import HamburgerMenuButtons from './HamburgerMenuButtons';
 
 export default {
   AppSettings,
+  CircularPopupMenu,
   FlexGallery,
   HamburgerMenuButtons,
 };
