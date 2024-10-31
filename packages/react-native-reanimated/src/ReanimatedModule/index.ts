f820cba57a674bf2b1abf4673b4583ae7410a9d5
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2024-10-31 09:48:08 +0100

    chore: Add noop typescript worklets module (#6650)
    ## Summary
    
    Preparing for #6539 
    
    ## Test plan
    
    This PR has no real runtime impact

--- packages/react-native-reanimated/src/ReanimatedModule/index.ts
+++ packages/react-native-reanimated/src/ReanimatedModule/index.ts
@@ -1,8 +1,4 @@
 'use strict';
-import { createJSReanimatedModule } from './js-reanimated';
-import { shouldBeUseWeb } from '../PlatformChecker';
-import { createNativeReanimatedModule } from './NativeReanimated';
 
-export const ReanimatedModule = shouldBeUseWeb()
-  ? createJSReanimatedModule()
-  : createNativeReanimatedModule();
+export { ReanimatedModule } from './reanimatedModuleInstance';
+export type { ReanimatedModuleProxy } from './reanimatedModuleProxy';
