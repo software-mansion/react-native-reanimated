d6bcf54593a4c10e9bdab22c1baa99c1d8c774f5
Alex Duży <91994767+alduzy@users.noreply.github.com>
2024-11-14 08:58:39 +0100

    chore: migrate to screens v4 and navigation v7 (#6622)
    ## Summary
    
    This PR bumps version of the `react-native-screens` package to v4 and
    `@react-navigation` packages to v7.
    
    ## Changes
    
    - updated dependencies
    - followed [migrating from 6.x
    docs](https://reactnavigation.org/docs/7.x/upgrading-from-6.x/)
    - submitted a necessary
    [PR](https://github.com/software-mansion/react-native-screens/pull/243)
    to `react-native-screens`
    - ensured examples work well

--- apps/common-app/src/examples/SharedElementTransitions/CustomTransition.tsx
+++ apps/common-app/src/examples/SharedElementTransitions/CustomTransition.tsx
@@ -63,7 +63,7 @@ function Screen2({ navigation }: NativeStackScreenProps<ParamListBase>) {
         sharedTransitionTag="tag"
         sharedTransitionStyle={transition}
       />
-      <Button title="go back" onPress={() => navigation.navigate('Screen1')} />
+      <Button title="go back" onPress={() => navigation.popTo('Screen1')} />
     </View>
   );
 }
