c4352755cffdeacc846fd83d24e2a33367d8d445
Mathieu Acthernoene <zoontek@gmail.com>
2024-11-19 19:23:14 +0100

    feature: Detect edge-to-edge and set isStatusBarTranslucentAndroid / isNavigationBarTranslucentAndroid
    # Conflicts:
    #	packages/react-native-reanimated/src/core.ts
    
    # Conflicts:
    #	yarn.lock

--- packages/react-native-reanimated/src/core.ts
+++ packages/react-native-reanimated/src/core.ts
@@ -1,4 +1,10 @@
 'use strict';
+
+import {
+  controlEdgeToEdgeValues,
+  isEdgeToEdge,
+} from 'react-native-is-edge-to-edge';
+
 import { ReanimatedModule } from './ReanimatedModule';
 import { isWeb, shouldBeUseWeb, isFabric } from './PlatformChecker';
 import type {
@@ -23,6 +29,7 @@ export type { WorkletRuntime } from './runtimes';
 export { makeShareable, makeShareableCloneRecursive } from './shareables';
 export { makeMutable } from './mutables';
 
+const EDGE_TO_EDGE = isEdgeToEdge();
 const SHOULD_BE_USE_WEB = shouldBeUseWeb();
 
 /** @returns `true` in Reanimated 3, doesn't exist in Reanimated 2 or 1 */
@@ -112,12 +119,21 @@ export function subscribeForKeyboardEvents(
     global.__flushAnimationFrame(now);
     global.__frameTimestamp = undefined;
   }
+
+  if (__DEV__) {
+    controlEdgeToEdgeValues({
+      isStatusBarTranslucentAndroid: options.isStatusBarTranslucentAndroid,
+      isNavigationBarTranslucentAndroid:
+        options.isNavigationBarTranslucentAndroid,
+    });
+  }
+
   return ReanimatedModule.subscribeForKeyboardEvents(
     makeShareableCloneRecursive(
       handleAndFlushAnimationFrame as WorkletFunction
     ),
-    options.isStatusBarTranslucentAndroid ?? false,
-    options.isNavigationBarTranslucentAndroid ?? false
+    EDGE_TO_EDGE || (options.isStatusBarTranslucentAndroid ?? false),
+    EDGE_TO_EDGE || (options.isNavigationBarTranslucentAndroid ?? false)
   );
 }
 
