6e6e962054e71ca2211d32e6de889f622de3ece9
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2024-12-12 15:00:26 +0100

    feat(Worklets): react-native-worklets node package stub  (#6806)
    ## Summary
    
    I was doing some weird git stuff and github closed previous PR 🤷 #6790
    
    ## Test plan
    
    🚀

---
+++ packages/react-native-worklets/android/src/main/java/com/swmansion/worklets/DummyWorkletsModule.java
@@ -0,0 +1,18 @@
+package com.swmansion.worklets;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = DummyWorkletsModule.NAME)
+public class DummyWorkletsModule extends
+        NativeSulphateWorkletsSpec {
+
+  public DummyWorkletsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public boolean installTurboModule() {
+    return true;
+  }
+}
