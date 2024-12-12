9735871da835dea24f89270785fc65f5e2e77dcd
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2024-12-12 15:00:26 +0100

    feat(Worklets): react-native-worklets node package stub  (#6806)
    ## Summary
    
    I was doing some weird git stuff and github closed previous PR 🤷 #6790
    
    ## Test plan
    
    🚀

---
+++ packages/react-native-worklets/src/specs/NativeDummyWorklets.ts
@@ -0,0 +1,9 @@
+'use strict';
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+interface Spec extends TurboModule {
+  installTurboModule: () => boolean;
+}
+
+export default TurboModuleRegistry.get<Spec>('DummyWorklets');
