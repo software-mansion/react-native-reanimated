1e9cdd2fa86a84d7caa73fdd5d97027842678cad
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-12-19 11:49:47 +0100

    docs: add State of React Native 2024 banner (#6831)
    ## Description
    
    This PR adds a State of React Native 2024 banner to the top of
    Reanimated documentation.
    
    _Fill out the survey now_ button leads to
    https://survey.stateofreactnative.com/
    The banner **will automatically disappear January 8.**
    
    ### Desktop
    <img width="1728" alt="image"
    src="https://github.com/user-attachments/assets/fdb47af2-9046-40bc-b4ba-1b6ccf959ed6"
    />
    
    ### Tablet
    
    <img width="787" alt="image"
    src="https://github.com/user-attachments/assets/f3f8093c-7e05-4a92-ad25-409067805f47"
    />
    
    
    ### Mobile
    
    <img width="435" alt="image"
    src="https://github.com/user-attachments/assets/34dab9eb-fa6d-4ec0-8f52-26da4847b9bf"
    />
    
    
    ## How to test?
    
    ```
    cd packages/docs-reanimated
    yarn
    yarn start
    ```

--- packages/docs-reanimated/src/theme/AnnouncementBar/Content/index.js
+++ packages/docs-reanimated/src/theme/AnnouncementBar/Content/index.js
@@ -2,23 +2,28 @@ import React from 'react';
 import clsx from 'clsx';
 import ArrowButton from './ArrowButton';
 import styles from './styles.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function AnnouncementBarContent(props) {
   return (
     <div className={clsx(styles.content, props.className)}>
       <div className={styles.wrapper}>
-        <strong className={styles.headline}>App.js Conf 2024</strong>
+        <img
+          className={styles.logo}
+          src={useBaseUrl('/img/state-of-react-native-logo.svg')}
+          alt="State of React Native logo"
+        />
+        <strong className={styles.headline}>State of React Native 2024</strong>
         <p className={styles.subText}>
-          An Expo & React Native conference in Europe is back, May 22-24 in
-          Kraków, Poland!
+          Have a few minutes and want to shape the future of React Native?
         </p>
       </div>
       <a
         className={styles.link}
-        href="https://appjs.co/"
+        href="https://survey.stateofreactnative.com/"
         target="_blank"
         rel="noreferrer noopener">
-        <span className={styles.linkTitle}>Learn More</span>
+        <span className={styles.linkTitle}>Fill out the survey now!</span>
         <div className={styles.linkArrowContainer}>
           <ArrowButton className={styles.linkArrow} />
         </div>
