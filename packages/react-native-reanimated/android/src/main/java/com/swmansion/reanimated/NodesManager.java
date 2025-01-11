c6dc67d1aaf1f53e17d276263977453061a1e7bd
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2025-01-11 09:51:20 +0100

    fix: Unsubscribe dangling listeners (#6696)
    ## Summary
    
    Unsubscribes listeners that were not removed correctly during the app
    reload.
    
    ## Test plan
    
    I tested it with a new project using Expo (SDK 52) and with Fabric
    enabled. I reloaded the app several times and performed a heap dump to
    verify that everything was removed correctly.
    
    ---------
    
    Co-authored-by: Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>

--- packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/NodesManager.java
+++ packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/NodesManager.java
@@ -30,6 +30,7 @@ import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.UIManagerReanimatedHelper;
 import com.facebook.react.uimanager.common.UIManagerType;
 import com.facebook.react.uimanager.events.Event;
+import com.facebook.react.uimanager.events.EventDispatcher;
 import com.facebook.react.uimanager.events.EventDispatcherListener;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.swmansion.reanimated.layoutReanimation.AnimationsManager;
@@ -111,6 +112,7 @@ public class NodesManager implements EventDispatcherListener {
   public Set<String> uiProps = Collections.emptySet();
   public Set<String> nativeProps = Collections.emptySet();
   private ReaCompatibility compatibility;
+  private @Nullable Runnable mUnsubscribe = null;
 
   public NativeProxy getNativeProxy() {
     return mNativeProxy;
@@ -129,6 +131,15 @@ public class NodesManager implements EventDispatcherListener {
 
     if (mNativeProxy != null) {
       mNativeProxy = null;
+    }
+
+    if (compatibility != null) {
+      compatibility.unregisterFabricEventListener(this);
+    }
+
+    if (mUnsubscribe != null) {
+      mUnsubscribe.run();
+      mUnsubscribe = null;
     }
   }
 
@@ -183,8 +194,10 @@ public class NodesManager implements EventDispatcherListener {
     // Events are handled in the native modules thread in the `onEventDispatch()` method.
     // This method indirectly uses `mChoreographerCallback` which was created after event
     // registration, creating race condition
-    Objects.requireNonNull(UIManagerHelper.getEventDispatcher(context, uiManagerType))
-        .addListener(this);
+    EventDispatcher eventDispatcher =
+        Objects.requireNonNull(UIManagerHelper.getEventDispatcher(context, uiManagerType));
+    eventDispatcher.addListener(this);
+    mUnsubscribe = () -> eventDispatcher.removeListener(this);
 
     mAnimationManager = new AnimationsManager(mContext, mUIManager);
   }
