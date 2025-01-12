8a7d4c866c308bb6c4e0af669a6bcb79bc7df9a7
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-01-12 11:33:17 +0100

    Bump version to 3.16.7

--- packages/react-native-reanimated/src/platform-specific/jsVersion.ts
+++ packages/react-native-reanimated/src/platform-specific/jsVersion.ts
@@ -4,4 +4,4 @@
  * version used to build the native part of the library in runtime. Remember to
  * keep this in sync with the version declared in `package.json`
  */
-export const jsVersion = '3.16.6';
+export const jsVersion = '3.16.7';
