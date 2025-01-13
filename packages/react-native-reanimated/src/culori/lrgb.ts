bfc50530aedc9f64512f53b41343a8f157cb6c16
d4vidi <amit.d4vidi@gmail.com>
2025-01-13 14:08:55 +0200

    Revert "Add MIT license credit to culori"
    This reverts commit b2ff960ceb91f787a17a9fa0add099ec81adf84f.

--- packages/react-native-reanimated/src/culori/lrgb.ts
+++ packages/react-native-reanimated/src/culori/lrgb.ts
@@ -1,35 +1,14 @@
-// MIT License
-//
-// Copyright (c) 2018 Dan Burzo
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+'use strict';
 
 /*
  * The vast majority of the code exported by this module is a direct copy of the code from the culori package (see
  * https://culorijs.org/), which deserves full credit for it. In particular, code from the following path has been used:
  * - https://github.com/Evercoder/culori/tree/v4.0.1/src/lrgb
- *
- * TODO Remove once we have the option to get a workletized version of the culori package
- *  https://github.com/software-mansion/react-native-reanimated/pull/6782#pullrequestreview-2488830278
  */
 
-'use strict';
+// TODO Remove once we have the option to get a workletized version of the culori package
+//   https://github.com/software-mansion/react-native-reanimated/pull/6782#pullrequestreview-2488830278
+
 import type { RgbColor } from './Colors';
 
 const channelFromLrgb = (c = 0) => {
