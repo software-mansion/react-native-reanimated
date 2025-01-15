30e19bd33442736b47ca9fb52823b2571968a530
szydlovsky <9szydlowski9@gmail.com>
2025-01-15 15:50:26 +0100

    improve animated keyboard example

--- apps/common-app/src/examples/AnimatedKeyboardExample.tsx
+++ apps/common-app/src/examples/AnimatedKeyboardExample.tsx
@@ -1,88 +1,166 @@
+import React from 'react';
 import Animated, {
-  KeyboardState,
   useAnimatedKeyboard,
   useAnimatedStyle,
+  useAnimatedProps,
 } from 'react-native-reanimated';
 import {
-  Button,
   Keyboard,
   ScrollView,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
   View,
+  Text,
 } from 'react-native';
 
-import React from 'react';
+Animated.addWhitelistedNativeProps({ text: true });
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
-const BOX_SIZE = 50;
+const KeyboardStateToName = {
+  0: 'UNKNOWN',
+  1: 'OPENING',
+  2: 'OPEN',
+  3: 'CLOSING',
+  4: 'CLOSED',
+};
 
-function NestedView() {
-  useAnimatedKeyboard();
-  return <View style={styles.nestedView} />;
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function RandomView({ number }: { number: number }) {
+  const color = getRandomColor();
+  return (
+    <View style={[styles.randomView, { backgroundColor: color }]}>
+      <Text>Random view {number}</Text>
+    </View>
+  );
 }
 
 export default function AnimatedKeyboardExample() {
   const keyboard = useAnimatedKeyboard();
-  const OPENING = KeyboardState.OPENING;
-  const style = useAnimatedStyle(() => {
-    const color = keyboard.state.value === OPENING ? 'red' : 'blue';
 
-    return {
-      backgroundColor: color,
-    };
-  });
   const translateStyle = useAnimatedStyle(() => {
     return {
       transform: [{ translateY: -keyboard.height.value }],
     };
   });
-  const [shouldShowNestedView, setShouldShowNestedView] = React.useState(false);
+
+  const animatedHeightProps = useAnimatedProps(() => {
+    const text = `Keyboard height: ${keyboard.height.value}`;
+    return { text, defaultValue: text };
+  });
+
+  const animatedStateProps = useAnimatedProps(() => {
+    const text = `Keyboard state: ${KeyboardStateToName[keyboard.state.value]} - ${keyboard.state.value}`;
+    return { text, defaultValue: text };
+  });
 
   return (
-    <ScrollView
-      contentContainerStyle={styles.container}
-      keyboardDismissMode="interactive"
-      scrollEnabled={false}>
-      <Animated.View style={[styles.box, style]} />
-      <Button
-        title="Toggle nested view"
-        onPress={() => {
-          setShouldShowNestedView(!shouldShowNestedView);
-        }}
-      />
-      {shouldShowNestedView ? <NestedView /> : null}
-      <Animated.View style={translateStyle}>
-        <Button
-          title="Dismiss"
+    <>
+      <ScrollView
+        contentContainerStyle={styles.container}
+        keyboardDismissMode="interactive"
+        scrollEnabled={true}>
+        {Array.from({ length: 10 }).map((_, index) => {
+          return <RandomView key={index} number={index} />;
+        })}
+      </ScrollView>
+      <Animated.View style={[styles.accessoryBar, translateStyle]}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            autoCorrect
+            defaultValue="press me!!!"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.button}
           onPress={() => {
             Keyboard.dismiss();
-          }}
-        />
-        <TextInput style={styles.textInput} autoCorrect />
+          }}>
+          <Text>Dismiss</Text>
+        </TouchableOpacity>
       </Animated.View>
-    </ScrollView>
+      <View style={styles.infoContainer}>
+        <AnimatedTextInput
+          style={styles.infoText}
+          animatedProps={animatedHeightProps}
+        />
+        <AnimatedTextInput
+          style={styles.infoText}
+          animatedProps={animatedStateProps}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 70,
+    paddingVertical: 100,
   },
-  box: { width: BOX_SIZE, height: BOX_SIZE, marginBottom: 100 },
+  infoContainer: {
+    position: 'absolute',
+    padding: 15,
+    width: 300,
+    borderBottomRightRadius: 20,
+    backgroundColor: '#B0E0E6',
+  },
+  infoText: {
+    fontWeight: '700',
+    width: '100%',
+  },
+  accessoryBar: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: '#B0E0E6',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 20,
+  },
+  inputContainer: {
+    flex: 5,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginRight: 20,
+  },
   textInput: {
-    borderColor: 'blue',
+    borderColor: 'indigo',
     borderStyle: 'solid',
+    borderRadius: 20,
     borderWidth: 2,
-    height: 60,
-    width: 200,
+    height: 55,
+    width: '100%',
+    paddingLeft: 20,
+    fontWeight: '700',
   },
-  nestedView: {
-    width: 100,
-    height: 100,
-    backgroundColor: '#ffff00',
+  button: {
+    flex: 1,
+    borderColor: 'indigo',
+    borderStyle: 'solid',
+    borderRadius: 20,
+    borderWidth: 2,
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  randomView: {
+    height: 150,
+    width: 150,
+    borderRadius: 20,
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
