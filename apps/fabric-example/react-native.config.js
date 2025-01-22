aa114d3fe30f9a26b33c7c5ea63b16898c1c5080
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-22 18:11:44 +0100

    Clean up fabric example deps

--- apps/fabric-example/react-native.config.js
+++ apps/fabric-example/react-native.config.js
@@ -1,8 +1,20 @@
-/** @type {import('@react-native-community/cli-types').Config} */
+/**
+ * This file is required to properly resolve native dependencies
+ */
+const path = require('path');
+
+const commonAppDir = path.resolve(__dirname, '../common-app');
+const commonAppPkg = require(path.resolve(commonAppDir, 'package.json'));
+
+const dependencies = Object.fromEntries([
+  // Include all common-app dependencies
+  ...Object.keys(commonAppPkg.devDependencies || {}),
+  ...Object.keys(commonAppPkg.dependencies || {})
+].map(name => [name, {
+  root: path.resolve(__dirname, `../../node_modules/${name}`)
+}]));
+
 module.exports = {
-  project: {
-    ios: {},
-    android: {},
-  },
+  dependencies,
   assets: ['./assets/fonts/'],
 };
