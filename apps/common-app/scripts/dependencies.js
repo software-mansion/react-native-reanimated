db85ead520412979bc016f5c21c880172816749b
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-23 18:45:19 +0100

    Update macos-example to use shared common-app dependencies, add dependency overridding

--- apps/common-app/scripts/dependencies.js
+++ apps/common-app/scripts/dependencies.js
@@ -1,23 +1,41 @@
 const path = require('path');
 
-function resolveDependencies(dependencies = {}) {
+/**
+ * @param {Object.<string, string>} dependencies
+ * @param {string[]} exclude
+ */
+function resolveDependencies(dependencies = {}, exclude = []) {
   return Object.fromEntries(
-    Object.keys(dependencies).map((name) => [
-      name,
-      { root: path.resolve(__dirname, `../../../node_modules/${name}`) },
-    ])
+    Object.keys(dependencies)
+      .filter((name) => !exclude.includes(name))
+      .map((name) => [
+        name,
+        { root: path.resolve(__dirname, `../../../node_modules/${name}`) },
+      ])
   );
 }
 
-function getDependencies() {
-  const commonAppDir = path.resolve(__dirname, '../../common-app');
+/**
+ * This function will return the dependencies from the common-app package
+ * that aren't listed in the current app's package.json
+ * 
+ * @param {string} currentAppDir - the current app directory (e.g. __dirname)
+ */
+function getDependencies(currentAppDir = '.') {
+  const commonAppDir = path.resolve(__dirname, '..');
   const commonAppPkg = require(path.resolve(commonAppDir, 'package.json'));
 
-  // Get all common-app dependencies
-  const dependencies = resolveDependencies(commonAppPkg.dependencies);
-  const devDependencies = resolveDependencies(commonAppPkg.devDependencies);
+  const currentAppPkg = require(path.resolve(currentAppDir, 'package.json'));
+  const currentAppDeps = [
+    ...Object.keys(currentAppPkg.devDependencies),
+    ...Object.keys(currentAppPkg.dependencies),
+  ];
 
-  return { ...devDependencies, ...dependencies };
+  return {
+    // Get all common-app dependencies that aren't already in the current app
+    ...resolveDependencies(commonAppPkg.devDependencies, currentAppDeps),
+    ...resolveDependencies(commonAppPkg.dependencies, currentAppDeps),
+  };
 }
 
 module.exports = {
