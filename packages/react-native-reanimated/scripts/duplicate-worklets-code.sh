2017a8e3b009b16c5e9e2c56f8219dd6f4810cf8
Tomasz Żelawski <tzelawski@gmail.com>
2025-01-24 16:09:22 +0100

    refactor: actually copy from worklets

---
+++ packages/react-native-reanimated/scripts/duplicate-worklets-code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+copy_files_recursively() {
+  local dest_dir=$1
+  local src_dir=$2
+
+  mkdir -p "$dest_dir"
+
+  for file in "$src_dir"/*; do
+    if [ -d "$file" ]; then
+      echo "Recursing into $file"
+      copy_files_recursively "$dest_dir/$(basename "$file")" "$file"
+    else
+      ln "$file" "$dest_dir/$(basename "$file")"
+    fi
+  done
+}
+
+# Worklets iOS files
+copy_files_recursively "apple/worklets" "../react-native-worklets/apple/worklets"
+
+# Worklets Android files
+copy_files_recursively "android/src/worklets" "../react-native-worklets/android/src/worklets"
+
+# Worklets Android-specific cpp files
+copy_files_recursively "android/src/main/cpp/worklets/android" "../react-native-worklets/android/src/main/cpp/worklets/android"
+
+# Worklets Common cpp files
+copy_files_recursively "Common/cpp/worklets" "../react-native-worklets/Common/cpp/worklets"
