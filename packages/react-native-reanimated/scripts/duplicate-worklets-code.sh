8604d295a99f54949dc6a4c0b420647d6fe6eeb8
Tomasz Żelawski <tzelawski@gmail.com>
2025-01-27 16:32:23 +0100

    chore: apple

--- packages/react-native-reanimated/scripts/duplicate-worklets-code.sh
+++ packages/react-native-reanimated/scripts/duplicate-worklets-code.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Duplicating Worklets in Reanimated"
+
 copy_files_recursively() {
   local dest_dir=$1
   local src_dir=$2
@@ -8,10 +10,11 @@ copy_files_recursively() {
 
   for file in "$src_dir"/*; do
     if [ -d "$file" ]; then
-      echo "Recursing into $file"
+      # echo "Recursing into $file"
       copy_files_recursively "$dest_dir/$(basename "$file")" "$file"
     else
-      ln "$file" "$dest_dir/$(basename "$file")"
+      echo "    Linking $(basename "$file") in Reanimated"
+      ln -f "$file" "$dest_dir/$(basename "$file")"
     fi
   done
 }
@@ -20,6 +23,7 @@ copy_files_recursively() {
 copy_files_recursively "apple/worklets" "../react-native-worklets/apple/worklets"
 
 # Worklets Android files
+# Note that we don't duplicate "android/src/main"
 copy_files_recursively "android/src/worklets" "../react-native-worklets/android/src/worklets"
 
 # Worklets Android-specific cpp files
@@ -27,3 +31,5 @@ copy_files_recursively "android/src/main/cpp/worklets" "../react-native-worklets
 
 # Worklets Common cpp files
 copy_files_recursively "Common/cpp/worklets" "../react-native-worklets/Common/cpp/worklets"
+
+echo "Done duplicating Worklets in Reanimated"
