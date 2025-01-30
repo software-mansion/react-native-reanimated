32cb70a9dd8108e303d8aa01033193cd1097f5d3
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-01-30 09:24:52 +0100

    Run format

--- packages/react-native-reanimated/apple/reanimated/apple/native/PlatformDepMethodsHolderImpl.mm
+++ packages/react-native-reanimated/apple/reanimated/apple/native/PlatformDepMethodsHolderImpl.mm
@@ -93,8 +93,7 @@ RequestRenderFunction makeRequestRender(REANodesManager *nodesManager)
       // TODO macOS targetTimestamp isn't available on macOS
       auto targetTimestamp = displayLink.timestamp + displayLink.duration;
 #endif
-      const double frameTimestamp =
-        calculateTimestampWithSlowAnimations(targetTimestamp) * 1000;
+      const double frameTimestamp = calculateTimestampWithSlowAnimations(targetTimestamp) * 1000;
       onRender(frameTimestamp);
     }];
   };
