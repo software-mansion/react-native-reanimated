c082c393970eb26b7d17ff057efdcd64ab550577
Tomasz Żelawski <tzelawski@gmail.com>
2025-02-03 18:14:41 +0100

    fix: WorkletsError capturing

--- packages/react-native-worklets/src/worklets/WorkletsError.ts
+++ packages/react-native-worklets/src/worklets/WorkletsError.ts
@@ -4,7 +4,10 @@ import { createCustomError, registerCustomError } from './errors';
 
 export const WorkletsError = createCustomError('Worklets');
 
+// To capture it in a the registering worklet's closure.
+const WorkletsErrorConstructor = WorkletsError;
+
 export function registerWorkletsError() {
   'worklet';
-  registerCustomError(WorkletsError, 'Worklets');
+  registerCustomError(WorkletsErrorConstructor, 'Worklets');
 }
