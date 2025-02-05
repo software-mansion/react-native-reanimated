b3b50fd9391f449124483f5c4fba1aa141ffe9ac
Tomasz Żelawski <tzelawski@gmail.com>
2025-02-05 18:27:25 +0100

    chore: fix script

--- packages/react-native-reanimated/scripts/duplicate-worklets-code.sh
+++ packages/react-native-reanimated/scripts/duplicate-worklets-code.sh
@@ -26,23 +26,23 @@ if [ ! -d "$WORKLETS_DIR" ]; then
 fi
 
 TYPESCRIPT_DEST_DIR="src/worklets"
-rm -r "$TYPESCRIPT_DEST_DIR"
+rm -fr "$TYPESCRIPT_DEST_DIR"
 copy_files_recursively "$TYPESCRIPT_DEST_DIR" "$WORKLETS_DIR/src/worklets"
 
-IOS_SOURCE_DIR="$WORKLETS_DIR/apple/worklets"
-rm -r "$IOS_SOURCE_DIR"
-copy_files_recursively "apple/worklets" "$IOS_SOURCE_DIR"
+IOS_DEST_DIR="apple/worklets"
+rm -fr "$IOS_DEST_DIR"
+copy_files_recursively "$IOS_DEST_DIR" "$WORKLETS_DIR/apple/worklets"
 
-ANDROID_JAVA_SOURCE_DIR="$WORKLETS_DIR/android/src/worklets"
-rm -r "$ANDROID_JAVA_SOURCE_DIR"
-copy_files_recursively "android/src/worklets" "$ANDROID_JAVA_SOURCE_DIR"
+ANDROID_JAVA_DEST_DIR="android/src/worklets"
+rm -fr "$ANDROID_JAVA_DEST_DIR"
+copy_files_recursively "$ANDROID_JAVA_DEST_DIR" "$WORKLETS_DIR/android/src/worklets"
 
-ANDROID_CPP_SOURCE_DIR="$WORKLETS_DIR/android/src/main/cpp/worklets"
-rm -r "$ANDROID_CPP_SOURCE_DIR"
-copy_files_recursively "android/src/main/cpp/worklets" "$ANDROID_CPP_SOURCE_DIR"
+ANDROID_CPP_DEST_DIR="android/src/main/cpp/worklets"
+rm -fr "$ANDROID_CPP_DEST_DIR"
+copy_files_recursively "$ANDROID_CPP_DEST_DIR" "$WORKLETS_DIR/android/src/main/cpp/worklets"
 
-COMMON_CPP_SOURCE_DIR="$WORKLETS_DIR/Common/cpp/worklets"
-rm -r "$COMMON_CPP_SOURCE_DIR"
-copy_files_recursively "Common/cpp/worklets" "$COMMON_CPP_SOURCE_DIR"
+COMMON_CPP_DEST_DIR="Common/cpp/worklets"
+rm -fr "$COMMON_CPP_DEST_DIR"
+copy_files_recursively "$COMMON_CPP_DEST_DIR" "$WORKLETS_DIR/Common/cpp/worklets"
 
 echo "Done duplicating Worklets in Reanimated"
