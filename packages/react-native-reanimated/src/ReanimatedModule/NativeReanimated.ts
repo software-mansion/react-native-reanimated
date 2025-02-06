software-mansion/react-native-reanimated

commit 455d94a78112189a8a8068d8420532bfa6c28398

refactor: move Worklets JS core from Reanimated (#6973)
## Summary

Currently JS core of Worklets sits in Reanimated package. This PR moves
it to Worklets. It's a bit messy but we'll get there eventually.

- Added `createCustomError` that creates `WorkletsError` and
`ReanimatedError` used in respective packages.
- Added ESLint rule `use-worklets-error`, a copypaste of
`use-reanimated-error`.
- Moved `shareables.ts` to `react-native-worklets`.
- Moved `threads.ts` to `react-native-worklets`.
- Moved `initializers.ts` to `react-native-worklets`
- Moved `runtimes.ts` to `react-native-worklets`
- JS version checking in worklets is disabled for the time being, as it
would clutter this PR even more. We'll circle back to it once we set
version checking between Reanimated and Worklets.

## Test plan

CI, run the example app.

packages/react-native-reanimated/src/ReanimatedModule/NativeReanimated.ts
@@ -13,7 +13,7 @@ import type {
   NormalizedSingleCSSAnimationConfig,
   NormalizedSingleCSSAnimationSettings,
 } from '../css/platform/native';
-import { ReanimatedError } from '../errors';
+import { ReanimatedError, registerReanimatedError } from '../errors';
 import { getShadowNodeWrapperFromRef } from '../fabricUtils';
 import { checkCppVersion } from '../platform-specific/checkCppVersion';
 import { jsVersion } from '../platform-specific/jsVersion';
@@ -24,7 +24,7 @@ import type {
   ShareableRef,
   WorkletFunction,
 } from '../WorkletsResolver';
-import { WorkletsModule } from '../WorkletsResolver';
+import { executeOnUIRuntimeSync, WorkletsModule } from '../WorkletsResolver';
 import type {
   IReanimatedModule,
   ReanimatedModuleProxy,
@@ -81,6 +81,7 @@ See https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooti
       checkCppVersion();
     }
     this.#reanimatedModuleProxy = global.__reanimatedModuleProxy;
+    executeOnUIRuntimeSync(registerReanimatedError);
   }
 
   registerSensor(

packages/react-native-reanimated/src/errors.ts
@@ -1,95 +1,10 @@
-/* eslint-disable reanimated/use-reanimated-error */
 'use strict';
 
-import type { WorkletStackDetails } from './WorkletsResolver';
+import { createCustomError, registerCustomError } from './WorkletsResolver';
 
-type ReanimatedError = Error & 'ReanimatedError'; // signed type
+export const ReanimatedError = createCustomError('Reanimated');
 
-interface ReanimatedErrorConstructor extends Error {
-  new (message?: string): ReanimatedError;
-  (message?: string): ReanimatedError;
-  readonly prototype: ReanimatedError;
-}
-
-const ReanimatedErrorConstructor: ReanimatedErrorConstructor =
-  function ReanimatedError(message?: string) {
-    'worklet';
-    const prefix = '[Reanimated]';
-    const errorInstance = new Error(message ? `${prefix} ${message}` : prefix);
-    errorInstance.name = 'ReanimatedError';
-    return errorInstance;
-  } as ReanimatedErrorConstructor;
-
-export { ReanimatedErrorConstructor as ReanimatedError };
-
-/**
- * Registers `ReanimatedError` in global scope. Use it only for Worklet
- * runtimes.
- */
 export function registerReanimatedError() {
   'worklet';
-  if (!_WORKLET) {
-    throw new Error(
-      '[Reanimated] registerReanimatedError() must be called on Worklet runtime'
-    );
-  }
-  (global as Record<string, unknown>).ReanimatedError =
-    ReanimatedErrorConstructor;
-}
-
-const _workletStackDetails = new Map<number, WorkletStackDetails>();
-
-export function registerWorkletStackDetails(
-  hash: number,
-  stackDetails: WorkletStackDetails
-) {
-  _workletStackDetails.set(hash, stackDetails);
-}
-
-function getBundleOffset(error: Error): [string, number, number] {
-  const frame = error.stack?.split('\n')?.[0];
-  if (frame) {
-    const parsedFrame = /@([^@]+):(\d+):(\d+)/.exec(frame);
-    if (parsedFrame) {
-      const [, file, line, col] = parsedFrame;
-      return [file, Number(line), Number(col)];
-    }
-  }
-  return ['unknown', 0, 0];
-}
-
-function processStack(stack: string): string {
-  const workletStackEntries = stack.match(/worklet_(\d+):(\d+):(\d+)/g);
-  let result = stack;
-  workletStackEntries?.forEach((match) => {
-    const [, hash, origLine, origCol] = match.split(/:|_/).map(Number);
-    const errorDetails = _workletStackDetails.get(hash);
-    if (!errorDetails) {
-      return;
-    }
-    const [error, lineOffset, colOffset] = errorDetails;
-    const [bundleFile, bundleLine, bundleCol] = getBundleOffset(error);
-    const line = origLine + bundleLine + lineOffset;
-    const col = origCol + bundleCol + colOffset;
-
-    result = result.replace(match, `${bundleFile}:${line}:${col}`);
-  });
-  return result;
-}
-
-export function reportFatalErrorOnJS({
-  message,
-  stack,
-}: {
-  message: string;
-  stack?: string;
-}) {
-  const error = new Error();
-  error.message = message;
-  error.stack = stack ? processStack(stack) : undefined;
-  error.name = 'ReanimatedError';
-  // @ts-ignore React Native's ErrorUtils implementation extends the Error type with jsEngine field
-  error.jsEngine = 'reanimated';
-  // @ts-ignore the reportFatalError method is an internal method of ErrorUtils not exposed in the type definitions
-  global.ErrorUtils.reportFatalError(error);
+  registerCustomError(ReanimatedError, 'Reanimated');
 }

packages/react-native-worklets/src/worklets/initializers.ts
@@ -1,26 +1,27 @@
 'use strict';
-import { registerReanimatedError, reportFatalErrorOnJS } from './errors';
+
+import { reportFatalErrorOnJS } from './errors';
 import {
   DEFAULT_LOGGER_CONFIG,
   logToLogBoxAndConsole,
   registerLoggerConfig,
   replaceLoggerImplementation,
 } from './logger';
+import { mockedRequestAnimationFrame } from './mockedRequestAnimationFrame';
 import {
   isChromeDebugger,
   isJest,
   isWeb,
   shouldBeUseWeb,
 } from './PlatformChecker';
-import type { IReanimatedModule } from './ReanimatedModule';
 import {
   callMicrotasks,
   executeOnUIRuntimeSync,
   runOnJS,
-  runOnUIImmediately,
   setupMicrotasks,
 } from './threads';
-import { mockedRequestAnimationFrame } from './WorkletsResolver';
+import { registerWorkletsError, WorkletsError } from './WorkletsError';
+import type { IWorkletsModule } from './WorkletsModule';
 
 const IS_JEST = isJest();
 const SHOULD_BE_USE_WEB = shouldBeUseWeb();
@@ -48,10 +49,10 @@ if (SHOULD_BE_USE_WEB) {
   global._log = console.log;
   global._getAnimationTimestamp = () => performance.now();
 } else {
-  // Register ReanimatedError and logger config in the UI runtime global scope.
+  // Register WorkletsError and logger config in the UI runtime global scope.
   // (we are using `executeOnUIRuntimeSync` here to make sure that the changes
   // are applied before any async operations are executed on the UI runtime)
-  executeOnUIRuntimeSync(registerReanimatedError)();
+  executeOnUIRuntimeSync(registerWorkletsError)();
   executeOnUIRuntimeSync(registerLoggerConfig)(DEFAULT_LOGGER_CONFIG);
   executeOnUIRuntimeSync(overrideLogFunctionImplementation)();
 }
@@ -80,6 +81,7 @@ export function setupCallGuard() {
     reportFatalError: (error: Error) => {
       runOnJS(reportFatalErrorOnJS)({
         message: error.message,
+        moduleName: 'Worklets',
         stack: error.stack,
       });
     },
@@ -184,14 +186,13 @@ function setupRequestAnimationFrame() {
   };
 }
 
-export function initializeUIRuntime(ReanimatedModule: IReanimatedModule) {
+export function initializeUIRuntime(WorkletsModule: IWorkletsModule) {
   if (isWeb()) {
     return;
   }
-  if (!ReanimatedModule) {
-    // eslint-disable-next-line reanimated/use-reanimated-error
-    throw new Error(
-      '[Reanimated] Reanimated is trying to initialize the UI runtime without a valid ReanimatedModule'
+  if (!WorkletsModule) {
+    throw new WorkletsError(
+      'Worklets are trying to initialize the UI runtime without a valid WorkletsModule'
     );
   }
   if (IS_JEST) {
@@ -204,13 +205,13 @@ export function initializeUIRuntime(ReanimatedModule: IReanimatedModule) {
     globalThis.requestAnimationFrame = mockedRequestAnimationFrame;
   }
 
-  runOnUIImmediately(() => {
-    'worklet';
-    setupCallGuard();
-    setupConsole();
-    if (!SHOULD_BE_USE_WEB) {
+  if (!SHOULD_BE_USE_WEB) {
+    executeOnUIRuntimeSync(() => {
+      'worklet';
+      setupCallGuard();
+      setupConsole();
       setupMicrotasks();
       setupRequestAnimationFrame();
-    }
-  })();
+    })();
+  }
 }

packages/react-native-worklets/src/worklets/errors.ts
@@ -1,0 +1,103 @@
+'use strict';
+
+import type { WorkletStackDetails } from './workletTypes';
+
+export type CustomError<TName extends string> = Error & { name: TName }; // signed type
+
+export interface CustomErrorConstructor<TName extends string> extends Error {
+  new (message?: string): CustomError<TName>;
+  (message?: string): CustomError<TName>;
+  readonly prototype: CustomError<TName>;
+}
+
+export function createCustomError<TName extends string>(
+  name: TName
+): CustomErrorConstructor<TName> {
+  const constructor = function CustomError(message?: string) {
+    'worklet';
+    const prefix = `[${name}]`;
+    // eslint-disable-next-line reanimated/use-worklets-error
+    const errorInstance = new Error(message ? `${prefix} ${message}` : prefix);
+    errorInstance.name = `${name}Error`;
+    return errorInstance;
+  };
+
+  Object.defineProperty(constructor, 'name', { value: `${name}Error` });
+
+  return constructor as CustomErrorConstructor<TName>;
+}
+
+/** Registers custom errors in global scope. Use it only for Worklet runtimes. */
+export function registerCustomError<TName extends string>(
+  constructor: CustomErrorConstructor<TName>,
+  name: TName
+) {
+  'worklet';
+  if (!_WORKLET) {
+    // eslint-disable-next-line reanimated/use-worklets-error
+    throw new Error(
+      '[Worklets] registerCustomError() must be called on a Worklet runtime'
+    );
+  }
+  (global as Record<string, unknown>)[`${name}Error`] = constructor;
+}
+
+const _workletStackDetails = new Map<number, WorkletStackDetails>();
+
+export function registerWorkletStackDetails(
+  hash: number,
+  stackDetails: WorkletStackDetails
+) {
+  _workletStackDetails.set(hash, stackDetails);
+}
+
+function getBundleOffset(error: Error): [string, number, number] {
+  const frame = error.stack?.split('\n')?.[0];
+  if (frame) {
+    const parsedFrame = /@([^@]+):(\d+):(\d+)/.exec(frame);
+    if (parsedFrame) {
+      const [, file, line, col] = parsedFrame;
+      return [file, Number(line), Number(col)];
+    }
+  }
+  return ['unknown', 0, 0];
+}
+
+function processStack(stack: string): string {
+  const workletStackEntries = stack.match(/worklet_(\d+):(\d+):(\d+)/g);
+  let result = stack;
+  workletStackEntries?.forEach((match) => {
+    const [, hash, origLine, origCol] = match.split(/:|_/).map(Number);
+    const errorDetails = _workletStackDetails.get(hash);
+    if (!errorDetails) {
+      return;
+    }
+    const [error, lineOffset, colOffset] = errorDetails;
+    const [bundleFile, bundleLine, bundleCol] = getBundleOffset(error);
+    const line = origLine + bundleLine + lineOffset;
+    const col = origCol + bundleCol + colOffset;
+
+    result = result.replace(match, `${bundleFile}:${line}:${col}`);
+  });
+  return result;
+}
+
+export function reportFatalErrorOnJS({
+  message,
+  stack,
+  moduleName,
+}: {
+  message: string;
+  stack?: string;
+  moduleName: string;
+}) {
+  // eslint-disable-next-line reanimated/use-worklets-error
+  const error = new Error();
+  error.message = message;
+  error.stack = stack ? processStack(stack) : undefined;
+  error.name = `${moduleName}Error`;
+  // @ts-ignore React Native's ErrorUtils implementation extends the Error type with jsEngine field
+  error.jsEngine = moduleName;
+  // @ts-ignore the reportFatalError method is an internal method of ErrorUtils not exposed in the type definitions
+  global.ErrorUtils.reportFatalError(error);
+}

commit 753464bef579555d86a081aeab2de0381179dee4

fix: move requestAnimationFrame back to Reanimated (#6991)
## Summary

I introduced a regression in #6973 by moving JS implementation of
`requestAnimationFrame` to Worklets. Native version of
`requestAnimationFrame` is injected in Reanimated's
`UIRuntimeDecorator.cpp` so `requestAnimationFrame` must be overwritten
in Reanimated, not in Worklets.

Since all the native implementation is in Reanimated and it would be a
hard task to extract it right now, I decided to keep it for the time
being unless we decide where it should land eventually.

## Test plan

The only difference is the lack of batching with my regression
introduced. You can `console.log(currentCallbacks.length)` to see that
the batching is restored.

packages/react-native-reanimated/src/ReanimatedModule/NativeReanimated.ts
@@ -18,6 +18,7 @@ import { getShadowNodeWrapperFromRef } from '../fabricUtils';
 import { checkCppVersion } from '../platform-specific/checkCppVersion';
 import { jsVersion } from '../platform-specific/jsVersion';
 import { isFabric, shouldBeUseWeb } from '../PlatformChecker';
+import { setupRequestAnimationFrame } from '../requestAnimationFrame';
 import { ReanimatedTurboModule } from '../specs';
 import type {
   IWorkletsModule,
@@ -81,7 +82,11 @@ See https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooti
       checkCppVersion();
     }
     this.#reanimatedModuleProxy = global.__reanimatedModuleProxy;
-    executeOnUIRuntimeSync(registerReanimatedError);
+    executeOnUIRuntimeSync(function initializeUI() {
+      'worklet';
+      registerReanimatedError();
+      setupRequestAnimationFrame();
+    })();
   }
 
   registerSensor(

packages/react-native-reanimated/src/errors.ts
@@ -4,7 +4,9 @@ import { createCustomError, registerCustomError } from './WorkletsResolver';
 
 export const ReanimatedError = createCustomError('Reanimated');
 
+const ReanimatedErrorConstructor = ReanimatedError;
+
 export function registerReanimatedError() {
   'worklet';
-  registerCustomError(ReanimatedError, 'Reanimated');
+  registerCustomError(ReanimatedErrorConstructor, 'Reanimated');
 }

packages/react-native-reanimated/src/requestAnimationFrame.ts
@@ -1,0 +1,41 @@
+'use strict';
+
+import { callMicrotasks } from './WorkletsResolver';
+
+export function setupRequestAnimationFrame() {
+  'worklet';
+
+  // Jest mocks requestAnimationFrame API and it does not like if that mock gets overridden
+  // so we avoid doing requestAnimationFrame batching in Jest environment.
+  const nativeRequestAnimationFrame = global.requestAnimationFrame;
+
+  let animationFrameCallbacks: Array<(timestamp: number) => void> = [];
+  let flushRequested = false;
+
+  global.__flushAnimationFrame = (frameTimestamp: number) => {
+    const currentCallbacks = animationFrameCallbacks;
+    animationFrameCallbacks = [];
+    currentCallbacks.forEach((f) => f(frameTimestamp));
+    callMicrotasks();
+  };
+
+  global.requestAnimationFrame = (
+    callback: (timestamp: number) => void
+  ): number => {
+    animationFrameCallbacks.push(callback);
+    if (!flushRequested) {
+      flushRequested = true;
+      nativeRequestAnimationFrame((timestamp) => {
+        flushRequested = false;
+        global.__frameTimestamp = timestamp;
+        global.__flushAnimationFrame(timestamp);
+        global.__frameTimestamp = undefined;
+      });
+    }
+    // Reanimated currently does not support cancelling callbacks requested with
+    // requestAnimationFrame. We return -1 as identifier which isn't in line
+    // with the spec but it should give users better clue in case they actually
+    // attempt to store the value returned from rAF and use it for cancelling.
+    return -1;
+  };
+}

packages/react-native-worklets/src/worklets/initializers.ts
@@ -14,12 +14,7 @@ import {
   isWeb,
   shouldBeUseWeb,
 } from './PlatformChecker';
-import {
-  callMicrotasks,
-  executeOnUIRuntimeSync,
-  runOnJS,
-  setupMicrotasks,
-} from './threads';
+import { executeOnUIRuntimeSync, runOnJS, setupMicrotasks } from './threads';
 import { registerWorkletsError, WorkletsError } from './WorkletsError';
 import type { IWorkletsModule } from './WorkletsModule';
 
@@ -148,44 +143,6 @@ export function setupConsole() {
   }
 }
 
-function setupRequestAnimationFrame() {
-  'worklet';
-
-  // Jest mocks requestAnimationFrame API and it does not like if that mock gets overridden
-  // so we avoid doing requestAnimationFrame batching in Jest environment.
-  const nativeRequestAnimationFrame = global.requestAnimationFrame;
-
-  let animationFrameCallbacks: Array<(timestamp: number) => void> = [];
-  let flushRequested = false;
-
-  global.__flushAnimationFrame = (frameTimestamp: number) => {
-    const currentCallbacks = animationFrameCallbacks;
-    animationFrameCallbacks = [];
-    currentCallbacks.forEach((f) => f(frameTimestamp));
-    callMicrotasks();
-  };
-
-  global.requestAnimationFrame = (
-    callback: (timestamp: number) => void
-  ): number => {
-    animationFrameCallbacks.push(callback);
-    if (!flushRequested) {
-      flushRequested = true;
-      nativeRequestAnimationFrame((timestamp) => {
-        flushRequested = false;
-        global.__frameTimestamp = timestamp;
-        global.__flushAnimationFrame(timestamp);
-        global.__frameTimestamp = undefined;
-      });
-    }
-    // Reanimated currently does not support cancelling callbacks requested with
-    // requestAnimationFrame. We return -1 as identifier which isn't in line
-    // with the spec but it should give users better clue in case they actually
-    // attempt to store the value returned from rAF and use it for cancelling.
-    return -1;
-  };
-}
-
 export function initializeUIRuntime(WorkletsModule: IWorkletsModule) {
   if (isWeb()) {
     return;
@@ -211,7 +168,6 @@ export function initializeUIRuntime(WorkletsModule: IWorkletsModule) {
       setupCallGuard();
       setupConsole();
       setupMicrotasks();
-      setupRequestAnimationFrame();
     })();
   }
 }