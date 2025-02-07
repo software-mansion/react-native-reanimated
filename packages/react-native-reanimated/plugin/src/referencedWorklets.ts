2a029b1ef738d318d91669c42d04bcc0b092a18d
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-02-07 14:20:52 +0100

    feat(plugin): multiple worklet referencing (#6904)
    Some time ago I added a feature of worklet referencing to the Plugin,
    but this was limited to only a single reference:
    
    ```ts
    function foo(){
    	// UI stuff
    };
    
    runOnUI(foo)(); // ok, `foo` will get workletized
    
    function bar(){
    	// UI stuff
    };
    
    const foobar = bar;
    
    runOnUI(foobar); // error, foobar didn't get workletized
    ```
    
    This PR allows for any amount of references in the reference chain.
    Simultaneously I discovered that Gesture Handler callbacks didn't
    benefit from worklet referencing, i.e. the following code would be
    invalid:
    
    ```ts
    function foo(){
    	// do something
    }
    
    const pan = Gesture.Pan().onActive(foo) // `foo` wouldn't be workletized
    ```
    
    This PR adds this QoL feat for Gesture Handler callbacks.
    
    I added new unit tests for these cases.

--- packages/react-native-reanimated/plugin/src/referencedWorklets.ts
+++ packages/react-native-reanimated/plugin/src/referencedWorklets.ts
@@ -59,6 +59,13 @@ function findReferencedWorkletFromVariableDeclarator(
   if (acceptObject && isWorkletizableObjectPath(worklet)) {
     return worklet;
   }
+  if (worklet.isIdentifier() && worklet.isReferencedIdentifier()) {
+    return findReferencedWorklet(
+      worklet,
+      acceptWorkletizableFunction,
+      acceptObject
+    );
+  }
   return undefined;
 }
 
@@ -92,6 +99,16 @@ function findReferencedWorkletFromAssignmentExpression(
   }
   if (acceptObject && isWorkletizableObjectPath(workletDefinition)) {
     return workletDefinition;
+  }
+  if (
+    workletDefinition.isIdentifier() &&
+    workletDefinition.isReferencedIdentifier()
+  ) {
+    return findReferencedWorklet(
+      workletDefinition,
+      acceptWorkletizableFunction,
+      acceptObject
+    );
   }
   return undefined;
 }
