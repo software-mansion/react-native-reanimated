7042ca87b73a11baf878b710eef2acf2e1e9c7ee
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-02-19 16:05:57 +0100

    Update comment

--- packages/react-native-reanimated/src/platform-specific/findHostInstance.ts
+++ packages/react-native-reanimated/src/platform-specific/findHostInstance.ts
@@ -81,7 +81,7 @@ export function findHostInstance(
   resolveFindHostInstance_DEPRECATED();
   /*
     The Fabric implementation of `findHostInstance_DEPRECATED` requires a React ref as an argument
-    rather than a native ref. If a component implements the `hasAnimatedRef` method, it must use 
+    rather than a native ref. If a component implements the `getAnimatableRef` method, it must use 
     the ref provided by this method. It is the component's responsibility to ensure that this is 
     a valid React ref.
   */
