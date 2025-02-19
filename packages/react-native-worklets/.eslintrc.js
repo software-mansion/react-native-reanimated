dbe85698c85191ffee19fc9b27b662774faad721
Patrycja Kalińska <cysiak0412@gmail.com>
2025-02-19 08:54:49 +0100

    too much

--- packages/react-native-worklets/.eslintrc.js
+++ packages/react-native-worklets/.eslintrc.js
@@ -1,14 +1,4 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   extends: ['../../.eslintrc.js'],
-  overrides: [
-    {
-      files: ['./src/**/*.ts', './src/**/*.tsx'],
-      plugins: ['reanimated'],
-      rules: {
-        'reanimated/use-worklets-error': 'error',
-      },
-    },
-  ],
-  ignorePatterns: ['lib'],
 };
