760a5f8b847a4c0affcd45eb601f1acdc27b73b3
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-02-25 19:16:16 +0100

    refactor(worklets): move reanimated-babel-plugin (#7066)
    ## Summary
    
    Moving Reanimated Babel plugin to `react-native-worklets`. For the time
    being we'll store bundled version of plugin in `plugin/index.js` like in
    the good old days. Later on we'll probably reexport that in
    `react-native-reanimated` for our users' convenience.
    
    ## Test plan
    
    CI green 👍

--- packages/react-native-reanimated/.eslintrc.js
+++ packages/react-native-reanimated/.eslintrc.js
@@ -11,5 +11,5 @@ module.exports = {
       },
     },
   ],
-  ignorePatterns: ['lib'],
+  ignorePatterns: ['lib, plugin'],
 };
--- packages/react-native-worklets/babel.config.js
+++ packages/react-native-worklets/babel.config.js
@@ -1,3 +1,17 @@
+/** @type {import('@babel/core').TransformOptions} */
 module.exports = {
-  presets: ['module:react-native-builder-bob/babel-preset'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+    'module:@react-native/babel-preset',
+    'module:react-native-builder-bob/babel-preset',
+  ],
+  plugins: [['./plugin', { disableInlineStylesWarning: true }]],
 };
