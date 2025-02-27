10f32ef75618ad216bf8a8d9920c4de199c30d3b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-02-27 12:40:53 +0100

    Remove Layout Animations and SET from Android on Paper

--- packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/NativeProxy.java
+++ packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/NativeProxy.java
@@ -9,12 +9,9 @@ import com.facebook.react.fabric.FabricUIManager;
 import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 import com.facebook.react.uimanager.UIManagerHelper;
 import com.facebook.react.uimanager.common.UIManagerType;
-import com.swmansion.reanimated.layoutReanimation.LayoutAnimations;
-import com.swmansion.reanimated.layoutReanimation.NativeMethodsHolder;
 import com.swmansion.reanimated.nativeProxy.NativeProxyCommon;
 import com.swmansion.worklets.JSCallInvokerResolver;
 import com.swmansion.worklets.WorkletsModule;
-import java.util.HashMap;
 import java.util.Objects;
 
 /**
@@ -32,19 +29,15 @@ public class NativeProxy extends NativeProxyCommon {
     FabricUIManager fabricUIManager =
         (FabricUIManager) UIManagerHelper.getUIManager(context, UIManagerType.FABRIC);
 
-    LayoutAnimations LayoutAnimations = new LayoutAnimations(context);
-
     CallInvokerHolderImpl callInvokerHolder = JSCallInvokerResolver.getJSCallInvokerHolder(context);
     mHybridData =
         initHybrid(
             workletsModule,
             Objects.requireNonNull(context.getJavaScriptContextHolder()).get(),
             callInvokerHolder,
-            LayoutAnimations,
             context.isBridgeless(),
             fabricUIManager);
 
-    prepareLayoutAnimations(LayoutAnimations);
     installJSIBindings();
     if (BuildConfig.DEBUG) {
       checkCppVersion();
@@ -56,7 +49,6 @@ public class NativeProxy extends NativeProxyCommon {
       WorkletsModule workletsModule,
       long jsContext,
       CallInvokerHolderImpl jsCallInvokerHolder,
-      LayoutAnimations LayoutAnimations,
       boolean isBridgeless,
       FabricUIManager fabricUIManager);
 
@@ -73,60 +65,5 @@ public class NativeProxy extends NativeProxyCommon {
 
   public void invalidate() {
     invalidateCpp();
-  }
-
-  public static NativeMethodsHolder createNativeMethodsHolder(
-      LayoutAnimations ignoredLayoutAnimations) {
-    return new NativeMethodsHolder() {
-      @Override
-      public void startAnimation(int tag, int type, HashMap<String, Object> values) {
-        // NOT IMPLEMENTED
-      }
-
-      @Override
-      public boolean isLayoutAnimationEnabled() {
-        // NOT IMPLEMENTED
-        return false;
-      }
-
-      @Override
-      public int findPrecedingViewTagForTransition(int tag) {
-        // NOT IMPLEMENTED
-        return -1;
-      }
-
-      @Override
-      public boolean shouldAnimateExiting(int tag, boolean shouldAnimate) {
-        // NOT IMPLEMENTED
-        return false;
-      }
-
-      @Override
-      public boolean hasAnimation(int tag, int type) {
-        // NOT IMPLEMENTED
-        return false;
-      }
-
-      @Override
-      public void clearAnimationConfig(int tag) {
-        // NOT IMPLEMENTED
-      }
-
-      @Override
-      public void cancelAnimation(int tag) {
-        // NOT IMPLEMENTED
-      }
-
-      @Override
-      public void checkDuplicateSharedTag(int viewTag, int screenTag) {
-        // NOT IMPLEMENTED
-      }
-
-      @Override
-      public int[] getSharedGroup(int viewTag) {
-        // NOT IMPLEMENTED
-        return new int[] {};
-      }
-    };
   }
 }
