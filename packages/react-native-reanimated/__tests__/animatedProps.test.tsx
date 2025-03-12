69253db845e8655d4129207a7fe4397fc7c2ac24
Patrycja Kalinska <cysiak0412@gmail.com>
2025-03-12 12:01:27 +0100

    add another test

--- packages/react-native-reanimated/__tests__/animatedProps.test.tsx
+++ packages/react-native-reanimated/__tests__/animatedProps.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render } from '@testing-library/react-native';
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 import Animated, {
   useAnimatedProps,
   useSharedValue,
@@ -8,33 +8,50 @@ import Animated, {
 } from 'react-native-reanimated';
 import { Circle, Svg } from 'react-native-svg';
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-// const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+Animated.addWhitelistedNativeProps({ text: true });
 
-export default function AnimatedComponent() {
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+export default function AnimatedComponent({ isCustom }: { isCustom: boolean }) {
   const r = useSharedValue(20);
+  const width = useSharedValue(20);
 
   const handlePress = () => {
     r.value += 10;
+    width.value += 10;
   };
 
   const animatedProps = useAnimatedProps(() => ({
     r: withTiming(r.value, { duration: 100 }),
   }));
 
+  const textAnimatedProps = useAnimatedProps(() => {
+    return {
+      text: `Box width: ${width.value}`,
+      defaultValue: `Box width: ${width.value}`,
+    };
+  });
+
   return (
     <View>
       <Svg>
-        {/* This works - it has jestAnimatedProps and jestAnimatedStyles on component 👇 */}
-        {/* <AnimatedPressable testID={'circle'} animatedProps={animatedProps}/> */}
-        {/* This doesn't - it has no jest styles :/ 👇 */}
-        <AnimatedCircle
-          cx="50%"
-          cy="50%"
-          fill="#b58df1"
-          testID={'circle'}
-          animatedProps={animatedProps}
-        />
+        {!isCustom ? (
+          //  This doesn't work - it has no jest styles :/ 👇
+          <AnimatedCircle
+            cx="50%"
+            cy="50%"
+            fill="#b58df1"
+            testID={'circle'}
+            animatedProps={animatedProps}
+          />
+        ) : (
+          // This works - it has jestAnimatedProps and jestAnimatedStyles on component 👇
+          <AnimatedTextInput
+            testID={'text'}
+            animatedProps={textAnimatedProps}
+          />
+        )}
       </Svg>
 
       <Button testID={'button'} onPress={handlePress} title="Click me" />
@@ -53,7 +70,7 @@ describe('animatedProps', () => {
   });
 
   test('custom prop', () => {
-    const { getByTestId } = render(<AnimatedComponent />);
+    const { getByTestId } = render(<AnimatedComponent isCustom={true} />);
     const circle = getByTestId('circle');
     const button = getByTestId('button');
 
@@ -63,5 +80,17 @@ describe('animatedProps', () => {
     jest.advanceTimersByTime(100);
 
     expect(circle).toHaveAnimatedProps({ r: 30 });
+  });
+  test('normal prop', () => {
+    const { getByTestId } = render(<AnimatedComponent isCustom={false} />);
+    const textInput = getByTestId('text');
+    const button = getByTestId('button');
+
+    expect(textInput).toHaveAnimatedProps({ text: 'Box width: 20' });
+
+    fireEvent.press(button);
+    jest.advanceTimersByTime(100);
+
+    expect(textInput).toHaveAnimatedProps({ text: 'Box width: 30' });
   });
 });
