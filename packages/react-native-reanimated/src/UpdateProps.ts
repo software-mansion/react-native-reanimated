1d4564aab6156e205b8cbeae08c72f4bb2a9fbe8
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-03-13 11:00:21 +0100

    refactor: remove runOnUIImmediately (#7226)
    ## Summary
    
    `runOnUIImmediately` is not conforming with a well defined event loop
    model of the UI Runtime, so we should remove it.
    
    I considered that we can replace it with:
    1. `runOnUI`
    2. `executeOnUIRuntimeSync`
    3. Additional batching mechanism on the side of Reanimated, using
    `runOnUI`.
    
    Of those I went for **1.** for the sake of simplicity and didn't notice
    any regressions. **2.** would be the second choice since it actually
    preserves the logic but could be potentially slowing down the
    initialization process.
    
    Please submit any suggestions on how to test this change more
    thoroughly.
    
    ## Test plan
    
    Run an example and see that all animations run well 🚀

--- packages/react-native-reanimated/src/UpdateProps.ts
+++ packages/react-native-reanimated/src/UpdateProps.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 'use strict';
 import type { MutableRefObject } from 'react';
-import { runOnUIImmediately } from 'react-native-worklets';
+import { runOnUI } from 'react-native-worklets';
 
 import { processColorsInProps } from './Colors';
 import type {
@@ -107,7 +107,7 @@ if (shouldBeUseWeb()) {
     },
   });
 } else {
-  runOnUIImmediately(() => {
+  runOnUI(() => {
     'worklet';
     global.UpdatePropsManager = createUpdatePropsManager();
   })();
