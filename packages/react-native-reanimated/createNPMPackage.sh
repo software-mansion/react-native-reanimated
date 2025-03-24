software-mansion/react-native-reanimated

commit 22b25b92fe3d98f3ff1c8b4226e6fe1528ab3cea

fix: createNPMPackage.sh not working without parameters (#7276)
## Summary

After the changes to the releasing scripts I introduced a regression
where calling `createNPMPackage.sh` without parameters would result in
an error.

#7107 

## Test plan

🚀

packages/react-native-reanimated/createNPMPackage.sh
@@ -2,14 +2,18 @@
 
 yarn install --immutable
 
-if ! CURRENT_VERSION=$(node scripts/set-reanimated-version.js "$@"); then
-  exit 1
+if [ $# -gt 1 ]; then
+  if ! CURRENT_VERSION=$(node scripts/set-reanimated-version.js "$@"); then
+    exit 1
+  fi
 fi
 
 yarn build
 
 npm pack
 
-node scripts/set-reanimated-version.js "$CURRENT_VERSION" >/dev/null
+if [ $# -gt 1 ]; then
+  node scripts/set-reanimated-version.js "$CURRENT_VERSION" >/dev/null
+fi
 
 echo "Done!"

packages/react-native-worklets/createNPMPackage.sh
@@ -2,14 +2,18 @@
 
 yarn install --immutable
 
-if ! CURRENT_VERSION=$(node scripts/set-worklets-version.js "$@"); then
-  exit 1
+if [ $# -gt 1 ]; then
+  if ! CURRENT_VERSION=$(node scripts/set-worklets-version.js "$@"); then
+    exit 1
+  fi
 fi
 
 yarn build
 
 npm pack
 
-node scripts/set-worklets-version.js "$CURRENT_VERSION" >/dev/null
+if [ $# -gt 1 ]; then
+  node scripts/set-worklets-version.js "$CURRENT_VERSION" >/dev/null
+fi
 
 echo "Done!"

commit 23578ac8ae134f93c4f59997eac5ebd980a0ce88

chore: fix nightly release (#7294)
## Summary

I introduced another regression in #7276, should have used `-ge` for
greater-equal instead of `-gt`, greater than.

## Test plan

🚀

packages/react-native-reanimated/createNPMPackage.sh
@@ -2,7 +2,7 @@
 
 yarn install --immutable
 
-if [ $# -gt 1 ]; then
+if [ $# -ge 1 ]; then
   if ! CURRENT_VERSION=$(node scripts/set-reanimated-version.js "$@"); then
     exit 1
   fi
@@ -12,7 +12,7 @@ yarn build
 
 npm pack
 
-if [ $# -gt 1 ]; then
+if [ $# -ge 1 ]; then
   node scripts/set-reanimated-version.js "$CURRENT_VERSION" >/dev/null
 fi
 

packages/react-native-worklets/createNPMPackage.sh
@@ -2,7 +2,7 @@
 
 yarn install --immutable
 
-if [ $# -gt 1 ]; then
+if [ $# -ge 1 ]; then
   if ! CURRENT_VERSION=$(node scripts/set-worklets-version.js "$@"); then
     exit 1
   fi
@@ -12,7 +12,7 @@ yarn build
 
 npm pack
 
-if [ $# -gt 1 ]; then
+if [ $# -ge 1 ]; then
   node scripts/set-worklets-version.js "$CURRENT_VERSION" >/dev/null
 fi
 