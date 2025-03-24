af79f9a4d2c25827af48b3a8b9e462bc3d02bcd6
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2025-03-24 17:01:27 +0100

    format :))

--- packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
@@ -834,9 +834,10 @@ void ReanimatedModuleProxy::dispatchCommand(
   ShadowNode::Shared shadowNode = shadowNodeFromValue(rt, shadowNodeValue);
   std::string commandName = stringFromValue(rt, commandNameValue);
   folly::dynamic args = commandArgsFromValue(rt, argsValue);
-  auto scheduler = static_cast<Scheduler*>(uiManager_->getDelegate());
+  auto scheduler = static_cast<Scheduler *>(uiManager_->getDelegate());
   auto shadowView = ShadowView(*shadowNode);
-  scheduler->getDelegate()->schedulerDidDispatchCommand(shadowView, commandName, args);
+  scheduler->getDelegate()->schedulerDidDispatchCommand(
+      shadowView, commandName, args);
 }
 
 jsi::String ReanimatedModuleProxy::obtainProp(
