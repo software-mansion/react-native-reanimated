4c40a09b60d7ee05babf8b881f79f4be80511aa6
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-04-10 17:28:13 +0200

    Add `RCTInvalidating` protocol in `ReanimatedModule` and `WorkletsModule`

--- packages/react-native-reanimated/apple/reanimated/apple/ReanimatedModule.h
+++ packages/react-native-reanimated/apple/reanimated/apple/ReanimatedModule.h
@@ -1,13 +1,14 @@
 #import <React/RCTCallInvokerModule.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTUIManagerObserverCoordinator.h>
+#import <React/RCTInvalidating.h>
 
 #import <rnreanimated/rnreanimated.h>
 
 #import <reanimated/apple/REANodesManager.h>
 
 @interface ReanimatedModule
-    : RCTEventEmitter <NativeReanimatedModuleSpec, RCTCallInvokerModule, RCTEventDispatcherObserver>
+    : RCTEventEmitter <NativeReanimatedModuleSpec, RCTCallInvokerModule, RCTEventDispatcherObserver, RCTInvalidating>
 
 @property (nonatomic, readonly) REANodesManager *nodesManager;
 
