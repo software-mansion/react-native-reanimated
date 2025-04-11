0bc3cc517ef7ab5109947dc42cc166f365e9606b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-04-11 13:46:38 +0200

    Restore example

--- apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { ReanimatedView } from 'react-native-reanimated';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function EmptyExample() {
-  const [color, setColor] = useState('red');
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
-      <ReanimatedView style={{width: 200, backgroundColor: 'green' }}>
-        <Text>Reanimated View</Text>
-        <View style={{width: 100, height: 100, backgroundColor: color }} />
-        <View style={{width: 100, height: 100, backgroundColor: 'blue' }} />
-        <View style={{width: 100, height: 100, backgroundColor: 'yellow' }} />
-      </ReanimatedView>
-      <Button title='Click' onPress={() => {
-        setColor(color === 'red' ? 'olive' : 'red');
-      }} />
     </View>
   );
 }
