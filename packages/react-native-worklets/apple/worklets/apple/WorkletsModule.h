638343339234349eb255d3f3400d48c58971626c
Tomasz Zawadzki <tomekzawadzki98@gmail.com>
2025-04-11 18:35:50 +0200

    Add `RCTInvalidating` protocol in `ReanimatedModule` and `WorkletsModule` on iOS (#7381)
    ## Summary
    
    This PR adds `RCTInvalidating` protocol in `ReanimatedModule` and
    `WorkletsModule` since they both already implement `invalidate` methods
    with custom cleanup code on iOS according to the suggestion in
    https://reactnative.dev/docs/next/the-new-architecture/native-modules-lifecycle.
    
    I haven't noticed any major changes after this change, here's the stack
    trace before and after:
    
    <img width="514" alt="Screenshot 2025-04-10 at 17 25 35"
    src="https://github.com/user-attachments/assets/726ff4b8-914c-4e70-8c28-99053339064f"
    />
    
    <img width="512" alt="Screenshot 2025-04-10 at 17 24 38"
    src="https://github.com/user-attachments/assets/e5766852-80dc-47e7-bc03-001dea369a52"
    />
    
    ## Test plan
    
    Reload app during bokeh example a few times.

--- packages/react-native-worklets/apple/worklets/apple/WorkletsModule.h
+++ packages/react-native-worklets/apple/worklets/apple/WorkletsModule.h
@@ -1,9 +1,10 @@
 #import <React/RCTCallInvokerModule.h>
 #import <React/RCTEventEmitter.h>
+#import <React/RCTInvalidating.h>
 
 #import <worklets/NativeModules/WorkletsModuleProxy.h>
 
-@interface WorkletsModule : RCTEventEmitter <RCTCallInvokerModule>
+@interface WorkletsModule : RCTEventEmitter <RCTCallInvokerModule, RCTInvalidating>
 
 - (std::shared_ptr<worklets::WorkletsModuleProxy>)getWorkletsModuleProxy;
 
