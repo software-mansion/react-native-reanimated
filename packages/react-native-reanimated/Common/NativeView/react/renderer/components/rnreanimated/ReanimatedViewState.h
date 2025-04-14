1665895decaef721130f1a49a1a2645dfdb3ea52
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-04-14 14:22:52 +0200

    Custom Shadow Nodes (#7380)
    ## Summary
    
    This PR introduces a common implementation of custom shadow nodes for
    both Android and iOS. It's part of the migration towards using `display:
    content`.
    
    ![Screenshot 2025-04-11 at 13 42
    07](https://github.com/user-attachments/assets/f1668675-15e9-4cae-8ed1-8294c4ba0088)
    
    ![Screenshot 2025-04-11 at 13 42
    26](https://github.com/user-attachments/assets/34448928-9258-40bf-8683-65b66c382365)
    
    ## Test plan
    
    <details>
    <summary>Example code</summary>
    
    ```js
    import React, { useState } from 'react';
    import { Button, StyleSheet, Text, View } from 'react-native';
    import { ReanimatedView } from 'react-native-reanimated';
    
    export default function EmptyExample() {
      const [color, setColor] = useState('red');
      return (
        <View style={styles.container}>
          <Text>Hello world!</Text>
          <ReanimatedView style={{width: 200, backgroundColor: 'green' }}>
            <Text>Reanimated View</Text>
            <View style={{width: 100, height: 100, backgroundColor: color }} />
            <View style={{width: 100, height: 100, backgroundColor: 'blue' }} />
            <View style={{width: 100, height: 100, backgroundColor: 'yellow' }} />
          </ReanimatedView>
          <Button title='Click' onPress={() => {
            setColor(color === 'red' ? 'olive' : 'red');
          }} />
        </View>
      );
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'center',
      },
    });
    
    ```
    
    </details>

---
+++ packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/ReanimatedViewState.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifdef ANDROID
+#include <folly/dynamic.h>
+#endif
+
+namespace facebook::react {
+
+class ReanimatedViewState {
+ public:
+  ReanimatedViewState() = default;
+
+#ifdef ANDROID
+  ReanimatedViewState(
+      ReanimatedViewState const &previousState,
+      folly::dynamic data) {};
+  folly::dynamic getDynamic() const {
+    return {};
+  };
+#endif
+};
+
+} // namespace facebook::react
