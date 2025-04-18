a4e2d8baf65bb2fae7a199e77bdbb6fd1027db23
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-04-18 23:56:14 +0200

    Inject reanimated module proxy to the contextContainer

--- packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/ComponentDescriptors.h
+++ packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/ComponentDescriptors.h
@@ -8,8 +8,36 @@
 
 namespace facebook::react {
 
-using ReanimatedViewComponentDescriptor =
-    ConcreteComponentDescriptor<ReanimatedShadowNode>;
+class ReanimatedViewComponentDescriptor
+    : public ConcreteComponentDescriptor<ReanimatedShadowNode> {
+ public:
+  explicit ReanimatedViewComponentDescriptor(
+      const ComponentDescriptorParameters &parameters)
+      : ConcreteComponentDescriptor<ReanimatedShadowNode>(parameters) {
+    ;
+  }
+
+  void adopt(ShadowNode &shadowNode) const override {
+    //       const auto &reanimatedShadowNode =
+    //           static_cast<ReanimatedShadowNode &>(shadowNode);
+    //       reanimatedShadowNode.initialize(reanimatedModuleProxy_);
+
+    LOG(INFO) << "Has? "
+              << getContextContainer()
+                     ->find<std::shared_ptr<ReanimatedModuleProxy>>(
+                         "ReanimatedModuleProxy")
+                     .has_value();
+  }
+
+ private:
+  std::shared_ptr<ReanimatedModuleProxy> reanimatedModuleProxy_;
+
+  static std::shared_ptr<ReanimatedModuleProxy> getReanimatedModuleProxy(
+      const std::shared_ptr<const ContextContainer> &contextContainer) {
+    return contextContainer->at<std::shared_ptr<ReanimatedModuleProxy>>(
+        "ReanimatedModuleProxy");
+  }
+};
 
 void rnreanimated_registerComponentDescriptorsFromCodegen(
     const std::shared_ptr<const ComponentDescriptorProviderRegistry> &registry);
--- packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/ReanimatedShadowNode.h
+++ packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/ReanimatedShadowNode.h
@@ -1,13 +1,18 @@
 #pragma once
 
-#include <jsi/jsi.h>
 #include <react/renderer/components/rnreanimated/EventEmitters.h>
 #include <react/renderer/components/rnreanimated/Props.h>
 #include <react/renderer/components/rnreanimated/ReanimatedViewState.h>
 #include <react/renderer/components/view/ConcreteViewShadowNode.h>
 #include <react/renderer/core/LayoutContext.h>
 
+#include <reanimated/NativeModules/ReanimatedModuleProxy.h>
+
+#include <jsi/jsi.h>
+
 namespace facebook::react {
+
+using namespace reanimated;
 
 JSI_EXPORT extern const char ReanimatedViewComponentName[];
 
@@ -20,6 +25,9 @@ using ReanimatedViewShadowNodeBase = ConcreteViewShadowNode<
 class ReanimatedShadowNode final : public ReanimatedViewShadowNodeBase {
  public:
   using ConcreteViewShadowNode::ConcreteViewShadowNode;
+
+  void initialize(const std::shared_ptr<CSSAnimationsRegistry>
+                      &reanimatedModuleProxy) const;
   void layout(LayoutContext layoutContext) override;
 };
 
