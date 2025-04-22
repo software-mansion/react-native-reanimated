4c43f7db3bd24a73233a008ff200e14e32a7e475
Tomasz Żelawski <tzelawski@gmail.com>
2025-04-22 20:12:16 +0200

    feat(Reanimated): react-native-worklets compatibility assertions

---
+++ packages/react-native-reanimated/src/platform-specific/workletsVersion.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import semverSatisfies from 'semver/functions/satisfies';
+
+import { ReanimatedError } from '../errors';
+import { acceptedWorkletsVerison } from './jsVersion';
+
+export function assertWorkletsVersion() {
+  let workletsVersion: string | undefined;
+
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { version } = require('react-native-worklets/package.json');
+    workletsVersion = version;
+  } catch (e) {
+    throw new ReanimatedError(
+      "react-native-worklets package isn't installed. Please install it to use Reanimated."
+    );
+  }
+
+  workletsVersion = workletsVersion!;
+
+  if (workletsVersion?.includes('nightly')) {
+    // Don't perform any checks for nightly versions, the user knows what they're doing.
+    return;
+  }
+
+  const acceptedRange = `${acceptedWorkletsVerison.min} - ${acceptedWorkletsVerison.max}`;
+
+  if (!semverSatisfies(workletsVersion, acceptedRange)) {
+    throw new ReanimatedError(
+      `Invalid version of react-native-worklets: ${workletsVersion}. Expected the version to be in inclusive range ${acceptedRange}. Please install a compatible version of react-native-worklets.`
+    );
+  }
+}
