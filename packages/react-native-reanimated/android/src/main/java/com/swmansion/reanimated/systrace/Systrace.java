1b593004cae58c1b5809daf5a9126a455eb1f994
Nicola Corti <corti.nico@gmail.com>
2025-04-28 17:52:20 +0100

    spotlessApply

--- packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/systrace/Systrace.java
+++ packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/systrace/Systrace.java
@@ -2,13 +2,11 @@ package com.swmansion.reanimated.systrace;
 
 import androidx.tracing.Trace;
 
-/**
- * A stripped copy of Systrace from React Native to suppor profiling of Reanimated.
- */
+/** A stripped copy of Systrace from React Native to suppor profiling of Reanimated. */
 public class Systrace {
 
   public static final long TRACE_TAG_REACT_JAVA_BRIDGE = 0L;
-  
+
   public static void beginSection(long tag, final String sectionName) {
     Trace.beginSection(sectionName);
   }
