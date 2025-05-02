8e82ef5267455d48334f372696c9f09d7a94c961
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-05-03 01:35:12 +0200

    Remove shadow node from CSSTransition, continue implementing the manager

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/manager/CSSTransitionManager.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/manager/CSSTransitionManager.cpp
@@ -8,11 +8,53 @@ CSSTransitionManager::CSSTransitionManager(
     : operationsLoop_(std::move(operationsLoop)),
       viewStylesRepository_(std::move(viewStylesRepository)) {}
 
-folly::dynamic CSSTransitionManager::getCurrentFrameProps() const {
+CSSTransitionManager::~CSSTransitionManager() {
+  if (transition_) {
+    removeTransition();
+  }
+}
+
+folly::dynamic CSSTransitionManager::getCurrentFrameProps(
+    const ShadowNode::Shared &shadowNode) const {
   if (!transition_) {
     return folly::dynamic();
   }
-  return transition_->getCurrentFrameProps(viewStylesRepository_);
+  return transition_->getCurrentFrameProps(shadowNode, viewStylesRepository_);
 }
+
+void CSSTransitionManager::update(
+    const ReanimatedViewProps &oldProps,
+    const ReanimatedViewProps &newProps) {
+  const auto &newConfig = newProps.cssTransition;
+
+  if (!transition_) {
+    if (!newConfig.empty()) {
+      createTransition(newConfig);
+    }
+  } else if (newConfig.empty()) {
+    removeTransition();
+    return;
+  }
+
+  // TODO - improve not to compare dynamics
+  const auto &oldConfig = oldProps.cssTransition;
+  if (newConfig != oldConfig) {
+    LOG(INFO) << "update transition settings";
+  }
+
+  // TODO - get transition props and check if the transition should be triggered
+}
+
+void CSSTransitionManager::createTransition(const folly::dynamic &config) {
+  transition_ = CSSTransition(parseCSSTransitionConfig(config));
+}
+
+void CSSTransitionManager::removeTransition() {
+  operationsLoop_->remove(operationHandle_);
+  transition_ = std::nullopt;
+}
+
+void CSSTransitionManager::scheduleOrActivateTransition(
+    const CSSTransition &transition) {}
 
 } // namespace reanimated::css
--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/manager/CSSTransitionManager.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/manager/CSSTransitionManager.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <react/renderer/components/rnreanimated/Props.h>
+
 #include <reanimated/CSS/config/CSSTransitionConfig.h>
 #include <reanimated/CSS/core/CSSTransition.h>
+#include <reanimated/CSS/util/DelayedItemsManager.h>
 #include <reanimated/Fabric/OperationsLoop.h>
 
 #include <folly/dynamic.h>
@@ -17,16 +20,27 @@ class CSSTransitionManager {
       std::shared_ptr<OperationsLoop> operationsLoop,
       std::shared_ptr<ViewStylesRepository> viewStylesRepository);
 
-  folly::dynamic getCurrentFrameProps() const;
+  ~CSSTransitionManager();
+
+  folly::dynamic getCurrentFrameProps(
+      const ShadowNode::Shared &shadowNode) const;
+
+  void update(
+      const ReanimatedViewProps &oldProps,
+      const ReanimatedViewProps &newProps);
 
  private:
   std::optional<CSSTransition> transition_;
   OperationsLoop::OperationHandle operationHandle_{0};
+  DelayedItemsManager<Tag> delayedTransitionsManager_;
 
   std::shared_ptr<OperationsLoop> operationsLoop_;
   std::shared_ptr<ViewStylesRepository> viewStylesRepository_;
 
-  void unregisterFromOperationsLoop();
+  void createTransition(const folly::dynamic &config);
+  void removeTransition();
+  // void updateTransition(const CSSTransition &transition);
+  void scheduleOrActivateTransition(const CSSTransition &transition);
 };
 
 } // namespace reanimated::css
