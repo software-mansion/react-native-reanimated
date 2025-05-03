2f60c61583e9bc2e25b7fe9a6129fba8fb34609d
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-05-03 18:02:41 +0200

    Add atomic queue, refine operations loop

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/operations/OperationsLoop.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/operations/OperationsLoop.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <reanimated/Fabric/operations/AtomicQueue.h>
+#include <reanimated/Fabric/operations/DelayedItemsManager.h>
 #include <reanimated/Fabric/operations/Operation.h>
-#include <reanimated/Tools/PlatformDepMethodsHolder.h>
 
-#include <queue>
+#include <atomic>
+#include <memory>
 #include <unordered_map>
 
 namespace reanimated {
+
+using GetAnimationTimestampFunction = std::function<double()>;
 
 class OperationsLoop {
  public:
@@ -15,37 +19,28 @@ class OperationsLoop {
 
   using OperationHandle = uint64_t;
 
-  double getTimestamp() const;
-
-  OperationHandle schedule(Operation &&operation);
-
+  OperationHandle schedule(std::unique_ptr<Operation> operation);
   void remove(OperationHandle handle);
   void update();
 
  private:
-  struct ScheduledOperation {
-    double executeAt;
-    Operation operation;
-    OperationHandle handle;
+  using OperationPair = std::pair<OperationHandle, std::unique_ptr<Operation>>;
+  using OperationsMap =
+      std::unordered_map<OperationHandle, std::unique_ptr<Operation>>;
 
-    bool operator>(const ScheduledOperation &other) const {
-      return executeAt > other.executeAt;
-    }
-  };
-
-  std::unordered_map<OperationHandle, Operation> activeOperations_;
-  std::priority_queue<
-      ScheduledOperation,
-      std::vector<ScheduledOperation>,
-      std::greater<>>
-      scheduledOperations_;
+  AtomicQueue<OperationPair> additionRequests_;
+  AtomicQueue<OperationHandle> removalRequests_;
+  DelayedItemsManager<OperationPair> delayedOperations_;
+  OperationsMap activeOperations_;
 
   double timestamp_{0};
+  GetAnimationTimestampFunction getTimestamp_;
+  std::atomic<OperationHandle> nextHandle_{0};
 
-  GetAnimationTimestampFunction getAnimationTimestamp_;
-  OperationHandle nextHandle_{0};
-
-  void activateScheduledOperations();
+  void processAdditionRequests();
+  void activateDelayedOperations();
+  void processRemovalRequests();
+  void executeActiveOperations();
 };
 
 } // namespace reanimated
