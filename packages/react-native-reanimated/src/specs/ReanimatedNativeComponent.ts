bb40b8d0724527b902456c264771e5b91cc6c061
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-05-12 21:40:38 +0200

    Import `codegenNativeComponent` from react-native

--- packages/react-native-reanimated/src/specs/ReanimatedNativeComponent.ts
+++ packages/react-native-reanimated/src/specs/ReanimatedNativeComponent.ts
@@ -1,6 +1,6 @@
 'use strict';
 import type { ViewProps } from 'react-native';
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+import { codegenNativeComponent } from 'react-native';
 
 interface NativeProps extends ViewProps {}
 
