a42e8632788f9e7b0afd2ac8649e7ebdda541f56
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-05-14 21:52:25 +0200

    Remove unused `localGlobal`

--- packages/react-native-reanimated/src/PlatformChecker.ts
+++ packages/react-native-reanimated/src/PlatformChecker.ts
@@ -1,10 +1,6 @@
 'use strict';
 import { Platform } from 'react-native';
 
-// This type is necessary since some libraries tend to do a lib check
-// and this file causes type errors on `global` access.
-type localGlobal = typeof global & Record<string, unknown>;
-
 export function isJest(): boolean {
   return !!process.env.JEST_WORKER_ID;
 }
