software-mansion/react-native-reanimated

commit 956c20cf4f302f112d2e27972bd1be770ea41ccc

refactor: use arguments instead of closures in Worklets plugin (#7365)
## Summary

Currently Worklets Babel plugin uses implicit closure capturing to
retain identifiers in the original worklet:

```js
// input
const mydlo = 'smiglo';

function foo() {
  'worklet';
  console.log(mydlo);
}
```

```js
// output
const mydlo = 'smiglo';
const _worklet_5876054941198_init_data = {
  // init data
};
const foo = (function () {
  const foo = function () {
    console.log(mydlo);
  };
  foo.__closure = {
    mydlo,
  };
  foo.__workletHash = 5876054941198;
  foo.__initData = _worklet_5876054941198_init_data;
  return foo;
})();
```

This PR changes that, so instead of using a closure we'd pass all
closure variables as arguments to the worklet factory, the IIFE on the
right of `const foo` in the output:

```js
// new output
const mydlo = 'smiglo';
const mydlo = 'smiglo';
const _worklet_5876054941198_init_data = {
  // init data
};
const foo = function foo_fileJs1Factory({
  _worklet_5876054941198_init_data,
  mydlo
}) {
  const foo = function () {
    console.log(mydlo);
  };
  foo.__closure = {
    mydlo
  };
  foo.__workletHash = 5876054941198;
  foo.__initData = _worklet_5876054941198_init_data;
  return foo;
}({
  _worklet_5876054941198_init_data,
  mydlo
});
```

Thanks to that worklet factories no longer have closures, which is a
milestone in the new way of bundling worklets.
As a bonus I'm fixing some bugs and making slight tweaks.

## Test plan

Ran this in our Example app and everything works fine 👍

packages/react-native-worklets/plugin/src/workletFactory.ts
@@ -7,6 +7,7 @@ import type {
   File as BabelFile,
   FunctionExpression,
   Identifier,
+  ObjectExpression,
   ReturnStatement,
   VariableDeclaration,
 } from '@babel/types';
@@ -30,6 +31,7 @@ import {
   newExpression,
   numericLiteral,
   objectExpression,
+  objectPattern,
   objectProperty,
   returnStatement,
   stringLiteral,
@@ -54,7 +56,7 @@ const MOCK_VERSION = 'x.y.z';
 export function makeWorkletFactory(
   fun: NodePath<WorkletizableFunction>,
   state: ReanimatedPluginPass
-): FunctionExpression {
+): { factory: FunctionExpression; factoryCallParamPack: ObjectExpression } {
   // Returns a new FunctionExpression which is a workletized version of provided
   // FunctionDeclaration, FunctionExpression, ArrowFunctionExpression or ObjectMethod.
 
@@ -92,7 +94,7 @@ export function makeWorkletFactory(
   assert(transformed, '[Reanimated] `transformed` is undefined.');
   assert(transformed.ast, '[Reanimated] `transformed.ast` is undefined.');
 
-  const variables = makeArrayFromCapturedBindings(transformed.ast, fun);
+  const closureVariables = makeArrayFromCapturedBindings(transformed.ast, fun);
 
   const clone = cloneNode(fun.node);
   const funExpression = isBlockStatement(clone.body)
@@ -110,7 +112,7 @@ export function makeWorkletFactory(
   let [funString, sourceMapString] = buildWorkletString(
     transformed.ast,
     state,
-    variables,
+    closureVariables,
     workletName,
     transformed.map
   );
@@ -118,13 +120,13 @@ export function makeWorkletFactory(
   const workletHash = hash(funString);
 
   let lineOffset = 1;
-  if (variables.length > 0) {
+  if (closureVariables.length > 0) {
     // When worklet captures some variables, we append closure destructing at
     // the beginning of the function body. This effectively results in line
     // numbers shifting by the number of captured variables (size of the
     // closure) + 2 (for the opening and closing brackets of the destruct
     // statement)
-    lineOffset -= variables.length + 2;
+    lineOffset -= closureVariables.length + 2;
   }
 
   const pathForStringDefinitions = fun.parentPath.isProgram()
@@ -213,7 +215,7 @@ export function makeWorkletFactory(
         '=',
         memberExpression(identifier(reactName), identifier('__closure'), false),
         objectExpression(
-          variables.map((variable) =>
+          closureVariables.map((variable) =>
             variable.name.endsWith(workletClassFactorySuffix)
               ? objectProperty(
                   identifier(variable.name),
@@ -227,7 +229,12 @@ export function makeWorkletFactory(
                     identifier(variable.name)
                   )
                 )
-              : objectProperty(identifier(variable.name), variable, false, true)
+              : objectProperty(
+                  cloneNode(variable, true),
+                  cloneNode(variable, true),
+                  false,
+                  true
+                )
           )
         )
       )
@@ -255,7 +262,7 @@ export function makeWorkletFactory(
             identifier('__initData'),
             false
           ),
-          initDataId
+          cloneNode(initDataId, true)
         )
       )
     );
@@ -294,9 +301,45 @@ export function makeWorkletFactory(
 
   statements.push(returnStatement(identifier(reactName)));
 
-  const newFun = functionExpression(undefined, [], blockStatement(statements));
+  const factoryParams = [
+    cloneNode(initDataId, true),
+    ...closureVariables.map((variableId) => cloneNode(variableId, true)),
+  ];
 
-  return newFun;
+  const factoryParamObjectPattern = objectPattern(
+    factoryParams.map((param) =>
+      objectProperty(
+        cloneNode(param, true),
+        cloneNode(param, true),
+        false,
+        true
+      )
+    )
+  );
+
+  const factory = functionExpression(
+    identifier(workletName + 'Factory'),
+    [factoryParamObjectPattern],
+    blockStatement(statements)
+  );
+
+  const factoryCallArgs = [
+    identifier(initDataId.name),
+    ...closureVariables.map((variableId) => cloneNode(variableId, true)),
+  ];
+
+  const factoryCallParamPack = objectExpression(
+    factoryCallArgs.map((param) =>
+      objectProperty(
+        cloneNode(param, true),
+        cloneNode(param, true),
+        false,
+        true
+      )
+    )
+  );
+
+  return { factory, factoryCallParamPack };
 }
 
 function removeWorkletDirective(fun: NodePath<WorkletizableFunction>): void {
@@ -427,7 +470,7 @@ function makeArrayFromCapturedBindings(
         }
         currentScope = currentScope.parent;
       }
-      closure.set(name, path.node);
+      closure.set(name, cloneNode(path.node, true));
       isLocationAssignedMap.set(name, false);
     },
   });

packages/react-native-worklets/__tests__/plugin.test.ts
@@ -2355,7 +2355,9 @@ describe('babel plugin', () => {
       </script>`;
 
       const { code } = runPlugin(input);
-      expect(code).toContain('var Clazz__classFactory = function ()');
+      expect(code).toContain(
+        'var Clazz__classFactory = function Clazz__classFactory_null6Factory'
+      );
       expect(code).toContainInWorkletString('Clazz__classFactory');
       expect(code).toContain(
         'Clazz.Clazz__classFactory = _Clazz__classFactory'
@@ -2375,7 +2377,9 @@ describe('babel plugin', () => {
 
       const { code } = runPlugin(input);
       expect(code).toContain('var Clazz = exports.Clazz = function ()');
-      expect(code).toContain('var Clazz__classFactory = function ()');
+      expect(code).toContain(
+        'var Clazz__classFactory = function Clazz__classFactory_null6Factory'
+      );
       expect(code).toContainInWorkletString('Clazz__classFactory');
       expect(code).toContain(
         'Clazz.Clazz__classFactory = _Clazz__classFactory'
@@ -2395,7 +2399,9 @@ describe('babel plugin', () => {
 
       const { code } = runPlugin(input);
       expect(code).toContain('var Clazz = exports.default = function ()');
-      expect(code).toContain('var Clazz__classFactory = function ()');
+      expect(code).toContain(
+        'var Clazz__classFactory = function Clazz__classFactory_null6Factory'
+      );
       expect(code).toContainInWorkletString('Clazz__classFactory');
       expect(code).toContain(
         'Clazz.Clazz__classFactory = _Clazz__classFactory'

packages/react-native-reanimated/babel.config.js
@@ -12,5 +12,7 @@ module.exports = {
     '@babel/preset-typescript',
     'module:@react-native/babel-preset',
   ],
-  plugins: [['./plugin', { disableInlineStylesWarning: true }]],
+  plugins: [
+    ['react-native-worklets/plugin', { disableInlineStylesWarning: true }],
+  ],
 };

packages/react-native-reanimated/src/animation/util.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 'use strict';
-import { isWorkletFunction, logger, runOnUI } from 'react-native-worklets';
+import {
+  isWorkletFunction,
+  logger,
+  makeShareableCloneRecursive,
+  runOnUI,
+  shareableMappingCache,
+} from 'react-native-worklets';
 
 import type { ParsedColorArray } from '../Colors';
 import {
@@ -41,7 +47,15 @@ import {
   subtractMatrices,
 } from './transformationMatrix/matrixUtils';
 
-let IN_STYLE_UPDATER = false;
+/**
+ * This variable has to be an object, because it can't be changed for the
+ * worklets if it's a primitive value. We also have to bind it to a separate
+ * object to prevent from freezing it in development.
+ */
+const IN_STYLE_UPDATER = { current: false };
+const IN_STYLE_UPDATER_UI = makeShareableCloneRecursive({ current: false });
+shareableMappingCache.set(IN_STYLE_UPDATER, IN_STYLE_UPDATER_UI);
+
 const SHOULD_BE_USE_WEB = shouldBeUseWeb();
 
 const LAYOUT_ANIMATION_SUPPORTED_PROPS = {
@@ -95,9 +109,9 @@ export function assertEasingIsWorklet(
 }
 
 export function initialUpdaterRun<T>(updater: () => T) {
-  IN_STYLE_UPDATER = true;
+  IN_STYLE_UPDATER.current = true;
   const result = updater();
-  IN_STYLE_UPDATER = false;
+  IN_STYLE_UPDATER.current = false;
   return result;
 }
 
@@ -540,7 +554,7 @@ export function defineAnimation<
   U extends AnimationObject | StyleLayoutAnimation = T, // type that's received
 >(starting: AnimationToDecoration<T, U>, factory: () => T): T {
   'worklet';
-  if (IN_STYLE_UPDATER) {
+  if (!globalThis._WORKLET && IN_STYLE_UPDATER.current) {
     return starting as unknown as T;
   }
   const create = () => {

packages/react-native-worklets/plugin/bundle.js
@@ -10,5 +10,4 @@ esbuild.build({
   logLevel: 'warning',
   platform: 'node',
   sourcemap: 'linked',
-  minify: true,
 });

packages/react-native-worklets/plugin/src/jestMatchers.ts
@@ -13,7 +13,7 @@ declare global {
   }
 }
 
-const WORKLET_REGEX = /var _worklet_[0-9]+_init_data/g;
+const WORKLET_REGEX = /^var _worklet_[0-9]+_init_data/gm;
 const INLINE_STYLE_WARNING_REGEX =
   /console\.warn\(require\("react-native-reanimated"\)\.getUseOfValueInStyleWarning\(\)\)/g;
 

commit 5077571ba5d4bfbc0e7dcb500a2df418609b69d0

fix(Worklets): closure regressions (#7492)
## Summary

I introduced some closure regressions in #7365 with excessive naming,
this PR fixes them. Discovered them accidentally by testing runtime
tests for another PR.

## Test plan

Jest unit tests and Worklets Plugin runtime tests pass.

packages/react-native-worklets/plugin/babel.config.js
@@ -1,7 +1,8 @@
 // This file is needed for manual tests of the plugin.
-const reanimatedPlugin = require('./index.js');
+const workletsPlugin = require('./index.js');
 
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = {
-  plugins: [reanimatedPlugin],
+  presets: ['@babel/preset-typescript'],
+  plugins: [workletsPlugin],
 };

packages/react-native-worklets/plugin/index.js
@@ -740,15 +740,19 @@ var require_workletFactory = __commonJS({
       statements.push((0, types_12.returnStatement)((0, types_12.identifier)(reactName)));
       const factoryParams = [
         (0, types_12.cloneNode)(initDataId, true),
-        ...closureVariables.map((variableId) => (0, types_12.cloneNode)(variableId, true))
+        ...closureVariables.map((variableId) => {
+          const clonedId = (0, types_12.cloneNode)(variableId, true);
+          if (clonedId.name.endsWith(types_2.workletClassFactorySuffix)) {
+            clonedId.name = clonedId.name.slice(0, clonedId.name.length - types_2.workletClassFactorySuffix.length);
+          }
+          return clonedId;
+        })
       ];
       const factoryParamObjectPattern = (0, types_12.objectPattern)(factoryParams.map((param) => (0, types_12.objectProperty)((0, types_12.cloneNode)(param, true), (0, types_12.cloneNode)(param, true), false, true)));
       const factory = (0, types_12.functionExpression)((0, types_12.identifier)(workletName + "Factory"), [factoryParamObjectPattern], (0, types_12.blockStatement)(statements));
-      const factoryCallArgs = [
-        (0, types_12.identifier)(initDataId.name),
-        ...closureVariables.map((variableId) => (0, types_12.cloneNode)(variableId, true))
-      ];
+      const factoryCallArgs = factoryParams.map((param) => (0, types_12.cloneNode)(param, true));
       const factoryCallParamPack = (0, types_12.objectExpression)(factoryCallArgs.map((param) => (0, types_12.objectProperty)((0, types_12.cloneNode)(param, true), (0, types_12.cloneNode)(param, true), false, true)));
+      factory.workletized = true;
       return { factory, factoryCallParamPack };
     }
     exports2.makeWorkletFactory = makeWorkletFactory;
@@ -1110,8 +1114,9 @@ var require_autoworkletization = __commonJS({
     exports2.processCalleesAutoworkletizableCallbacks = processCalleesAutoworkletizableCallbacks;
     function processArgs(args, state, acceptWorkletizableFunction, acceptObject) {
       args.forEach((arg) => {
+        var _a;
         const maybeWorklet = findWorklet(arg, acceptWorkletizableFunction, acceptObject);
-        if (!maybeWorklet) {
+        if (!maybeWorklet || ((_a = maybeWorklet.getFunctionParent()) === null || _a === void 0 ? void 0 : _a.node.workletized)) {
           return;
         }
         if ((0, types_2.isWorkletizableFunctionPath)(maybeWorklet)) {
@@ -1601,16 +1606,16 @@ var inlineStylesWarning_1 = require_inlineStylesWarning();
 var types_1 = require_types();
 var webOptimization_1 = require_webOptimization();
 var workletSubstitution_1 = require_workletSubstitution();
-module.exports = function() {
+module.exports = function WorkletsBabelPlugin() {
   function runWithTaggedExceptions(fun) {
     try {
       fun();
     } catch (e) {
-      throw new Error(`[Reanimated] Babel plugin exception: ${e}`);
+      throw new Error(`[Worklets] Babel plugin exception: ${e}`);
     }
   }
   return {
-    name: "reanimated",
+    name: "worklets",
     pre() {
       runWithTaggedExceptions(() => {
         (0, globals_1.initializeState)(this);

packages/react-native-worklets/plugin/src/autoworkletization.ts
@@ -126,7 +126,9 @@ function processArgs(
       acceptWorkletizableFunction,
       acceptObject
     );
-    if (!maybeWorklet) {
+    // @ts-expect-error There's no need to workletize
+    // inside an already workletized function.
+    if (!maybeWorklet || maybeWorklet.getFunctionParent()?.node.workletized) {
       return;
     }
     if (isWorkletizableFunctionPath(maybeWorklet)) {

packages/react-native-worklets/plugin/src/plugin.ts
@@ -21,17 +21,17 @@ import { WorkletizableFunction } from './types';
 import { substituteWebCallExpression } from './webOptimization';
 import { processIfWithWorkletDirective } from './workletSubstitution';
 
-module.exports = function (): PluginItem {
+module.exports = function WorkletsBabelPlugin(): PluginItem {
   function runWithTaggedExceptions(fun: () => void) {
     try {
       fun();
     } catch (e) {
-      throw new Error(`[Reanimated] Babel plugin exception: ${e as string}`);
+      throw new Error(`[Worklets] Babel plugin exception: ${e as string}`);
     }
   }
 
   return {
-    name: 'reanimated',
+    name: 'worklets',
 
     pre(this: ReanimatedPluginPass) {
       runWithTaggedExceptions(() => {

packages/react-native-worklets/plugin/src/workletFactory.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import type { NodePath } from '@babel/core';
 import { traverse } from '@babel/core';
 import generate from '@babel/generator';
@@ -49,6 +48,7 @@ import { workletClassFactorySuffix } from './types';
 import { isRelease } from './utils';
 import { buildWorkletString } from './workletStringCode';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const REAL_VERSION = require('../../package.json').version;
 
 const MOCK_VERSION = 'x.y.z';
@@ -303,7 +303,16 @@ export function makeWorkletFactory(
 
   const factoryParams = [
     cloneNode(initDataId, true),
-    ...closureVariables.map((variableId) => cloneNode(variableId, true)),
+    ...closureVariables.map((variableId) => {
+      const clonedId = cloneNode(variableId, true);
+      if (clonedId.name.endsWith(workletClassFactorySuffix)) {
+        clonedId.name = clonedId.name.slice(
+          0,
+          clonedId.name.length - workletClassFactorySuffix.length
+        );
+      }
+      return clonedId;
+    }),
   ];
 
   const factoryParamObjectPattern = objectPattern(
@@ -323,10 +332,7 @@ export function makeWorkletFactory(
     blockStatement(statements)
   );
 
-  const factoryCallArgs = [
-    identifier(initDataId.name),
-    ...closureVariables.map((variableId) => cloneNode(variableId, true)),
-  ];
+  const factoryCallArgs = factoryParams.map((param) => cloneNode(param, true));
 
   const factoryCallParamPack = objectExpression(
     factoryCallArgs.map((param) =>
@@ -338,6 +344,10 @@ export function makeWorkletFactory(
       )
     )
   );
+
+  // @ts-expect-error We must mark the factory as workletized
+  // to avoid further workletization inside the factory.
+  factory.workletized = true;
 
   return { factory, factoryCallParamPack };
 }

packages/react-native-worklets/__tests__/__snapshots__/plugin.test.ts.snap
@@ -498,6 +498,7 @@ exports[`babel plugin for class worklets workletizes getter 1`] = `
 "var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+var x = 5;
 var _worklet_13515258128853_init_data = {
   code: "function get_null1(){const{x}=this.__closure;return x+2;}",
   location: "/dev/null",
@@ -3537,6 +3538,35 @@ var firstReference = secondReference;
 var animatedStyle = useAnimatedStyle(firstReference);"
 `;
 
+exports[`babel plugin for referenced worklets workletizes recursion 1`] = `
+"var _worklet_14493084721304_init_data = {
+  code: "function recursiveWorklet_null1(){const recursiveWorklet_null1=this._recur;const{runOnUI}=this.__closure;if(!globalThis._WORKLET){runOnUI(recursiveWorklet_null1)();}}",
+  location: "/dev/null",
+  sourceMap: "\\"mock source map\\"",
+  version: "x.y.z"
+};
+var recursiveWorklet = function recursiveWorklet_null1Factory(_ref) {
+  var _worklet_14493084721304_init_data = _ref._worklet_14493084721304_init_data,
+    runOnUI = _ref.runOnUI;
+  var _e = [new global.Error(), -2, -27];
+  var _recursiveWorklet = function recursiveWorklet() {
+    if (!globalThis._WORKLET) {
+      runOnUI(_recursiveWorklet)();
+    }
+  };
+  _recursiveWorklet.__closure = {
+    runOnUI: runOnUI
+  };
+  _recursiveWorklet.__workletHash = 14493084721304;
+  _recursiveWorklet.__initData = _worklet_14493084721304_init_data;
+  _recursiveWorklet.__stackDetails = _e;
+  return _recursiveWorklet;
+}({
+  _worklet_14493084721304_init_data: _worklet_14493084721304_init_data,
+  runOnUI: runOnUI
+});"
+`;
+
 exports[`babel plugin for sequence expressions supports SequenceExpression 1`] = `
 "function App() {
   (0, fun)({
@@ -4095,7 +4125,7 @@ exports[`babel plugin for worklet classes injects class factory into worklets 1`
 };
 var foo = function foo_null1Factory(_ref) {
   var _worklet_3370991485663_init_data = _ref._worklet_3370991485663_init_data,
-    Clazz__classFactory = _ref.Clazz__classFactory;
+    Clazz = _ref.Clazz;
   var _e = [new global.Error(), -2, -27];
   var foo = function foo() {
     var clazz = new Clazz();
@@ -4109,7 +4139,7 @@ var foo = function foo_null1Factory(_ref) {
   return foo;
 }({
   _worklet_3370991485663_init_data: _worklet_3370991485663_init_data,
-  Clazz__classFactory: Clazz__classFactory
+  Clazz: Clazz
 });"
 `;
 
@@ -4328,7 +4358,7 @@ exports[`babel plugin for worklet classes modifies closures 1`] = `
 };
 var foo = function foo_null1Factory(_ref) {
   var _worklet_3370991485663_init_data = _ref._worklet_3370991485663_init_data,
-    Clazz__classFactory = _ref.Clazz__classFactory;
+    Clazz = _ref.Clazz;
   var _e = [new global.Error(), -2, -27];
   var foo = function foo() {
     var clazz = new Clazz();
@@ -4342,7 +4372,7 @@ var foo = function foo_null1Factory(_ref) {
   return foo;
 }({
   _worklet_3370991485663_init_data: _worklet_3370991485663_init_data,
-  Clazz__classFactory: Clazz__classFactory
+  Clazz: Clazz
 });"
 `;
 

packages/react-native-worklets/__tests__/plugin.test.ts
@@ -537,6 +537,7 @@ describe('babel plugin', () => {
 
     it('workletizes getter', () => {
       const input = html`<script>
+        const x = 5;
         class Foo {
           get bar() {
             'worklet';
@@ -1608,7 +1609,7 @@ describe('babel plugin', () => {
         });
       </script>`;
 
-      expect(() => runPlugin(input)).toThrow('[Reanimated]');
+      expect(() => runPlugin(input)).toThrow('[Worklets]');
     });
   });
 
@@ -2095,6 +2096,20 @@ describe('babel plugin', () => {
         const secondReference = () => ({});
         const firstReference = secondReference;
         const animatedStyle = useAnimatedStyle(firstReference);
+      </script>`;
+
+      const { code } = runPlugin(input);
+      expect(code).toHaveWorkletData(1);
+      expect(code).toMatchSnapshot();
+    });
+
+    it('workletizes recursion', () => {
+      const input = html`<script>
+        function recursiveWorklet() {
+          if (!globalThis._WORKLET) {
+            runOnUI(recursiveWorklet)();
+          }
+        }
       </script>`;
 
       const { code } = runPlugin(input);