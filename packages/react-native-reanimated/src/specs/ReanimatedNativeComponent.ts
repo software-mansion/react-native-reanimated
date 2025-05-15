aa65bfc207f2c767e39cd8b0b8ba36362dce1fe3
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-05-15 13:19:09 +0200

    Restore deep import of `codegenNativeComponent` for backward-compatibility

--- packages/react-native-reanimated/src/specs/ReanimatedNativeComponent.ts
+++ packages/react-native-reanimated/src/specs/ReanimatedNativeComponent.ts
@@ -1,6 +1,6 @@
 'use strict';
 import type { ViewProps } from 'react-native';
-import { codegenNativeComponent } from 'react-native';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
 interface NativeProps extends ViewProps {}
 
