a109a5f4dc4c4d3c8774528338f69e0a8c58e1b8
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-05-22 12:40:34 +0200

    feat: Base implementation of the native CSSAnimationsManager (#7465)
    ## Summary
    
    Base implementation of the CPP `CSSAnimationsManager` class. Will be
    fully implemented in the next PR, similarly as I did with the
    transitions manager.

---
+++ packages/react-native-reanimated/Common/NativeView/react/renderer/components/rnreanimated/managers/CSSAnimationsManager.cpp
@@ -0,0 +1,30 @@
+#include <react/renderer/components/rnreanimated/managers/CSSAnimationsManager.h>
+
+namespace facebook::react {
+
+CSSAnimationsManager::CSSAnimationsManager(
+    std::shared_ptr<OperationsLoop> operationsLoop,
+    std::shared_ptr<ViewStylesRepository> viewStylesRepository)
+    : operationsLoop_(std::move(operationsLoop)),
+      viewStylesRepository_(std::move(viewStylesRepository)) {}
+
+CSSAnimationsManager::~CSSAnimationsManager() {
+  // TODO - cleanup
+}
+
+folly::dynamic CSSAnimationsManager::getCurrentFrameProps(
+    const ShadowNode::Shared &shadowNode) {
+  // TODO - implement
+  return folly::dynamic::object();
+}
+
+void CSSAnimationsManager::update(
+    const ReanimatedViewProps &oldProps,
+    const ReanimatedViewProps &newProps) {
+  // TODO - implement
+  // LOG(INFO) << "CSSAnimationsManager::update: " << oldProps.cssAnimations <<
+  // " "
+  //           << newProps.cssAnimations;
+}
+
+} // namespace facebook::react
