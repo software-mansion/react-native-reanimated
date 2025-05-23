65fc8a66511a028856a98cea6dd403c967b0100a
Tomasz Żelawski <tzelawski@gmail.com>
2025-05-23 16:09:56 +0200

    fix: missing header on iOS

--- packages/react-native-worklets/apple/worklets/apple/WorkletsModule.h
+++ packages/react-native-worklets/apple/worklets/apple/WorkletsModule.h
@@ -1,17 +1,22 @@
 #import <React/RCTCallInvokerModule.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTInvalidating.h>
-#import <ReactCommon/NSBigStringBuffer.h>
 
 #import <rnworklets/rnworklets.h>
 
 #import <worklets/NativeModules/WorkletsModuleProxy.h>
 
+#ifdef WORKLETS_EXPERIMENTAL_BUNDLING
+#import <ReactCommon/NSBigStringBuffer.h>
+#endif // WORKLETS_EXPERIMENTAL_BUNDLING
+
 @interface WorkletsModule : RCTEventEmitter <NativeWorkletsModuleSpec, RCTCallInvokerModule, RCTInvalidating>
 
 - (std::shared_ptr<worklets::WorkletsModuleProxy>)getWorkletsModuleProxy;
 
+#ifdef WORKLETS_EXPERIMENTAL_BUNDLING
 - (void)setScriptBuffer:(NSBigStringBuffer *)script;
+#endif // WORKLETS_EXPERIMENTAL_BUNDLING
 
 - (void)setSourceURL:(const std::string &)sourceURL;
 
