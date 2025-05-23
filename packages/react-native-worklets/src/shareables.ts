software-mansion/react-native-reanimated

commit 27e81781f34874aa4b5986c8d80047763fa30148

refactor(Worklets)(makeShareableClone): add makeShareableInitializer (#7548)
## Summary
This PR adds `makeShareableInitializer` function and is part of a series
of PRs that aim to refactor the makeSharableClone function ->
https://github.com/software-mansion/react-native-reanimated/pull/7481

## Test plan

<video
src="https://github.com/user-attachments/assets/8eae5fa2-ef53-4ff7-b5dd-ee6841f4d669"
/>

packages/react-native-worklets/src/shareables.ts
@@ -165,6 +165,13 @@ function makeShareableCloneRecursiveNative<T>(
   if (isHostObject(value)) {
     return cloneHostObject(value, shouldPersistRemote);
   }
+  if (isPlainJSObject(value) && value.__init) {
+    return cloneInitializer(
+      value,
+      shouldPersistRemote,
+      depth
+    ) as ShareableRef<T>;
+  }
   if (isPlainJSObject(value) && value.__workletContextObjectFactory) {
     return cloneContextObject(value);
   }
@@ -248,6 +255,41 @@ function cloneNull(): ShareableRef<null> {
   return WorkletsModule.makeShareableNull();
 }
 
+function cloneObjectProperties<T extends object>(
+  value: T,
+  shouldPersistRemote: boolean,
+  depth: number
+): Record<string, unknown> {
+  const clonedProps: Record<string, unknown> = {};
+  for (const [key, element] of Object.entries(value)) {
+    // We don't need to clone __initData field as it contains long strings
+    // representing the worklet code, source map, and location, and we will
+    // serialize/deserialize it once.
+    if (key === '__initData' && clonedProps.__initData !== undefined) {
+      continue;
+    }
+    clonedProps[key] = makeShareableCloneRecursive(
+      element,
+      shouldPersistRemote,
+      depth + 1
+    );
+  }
+  return clonedProps;
+}
+
+function cloneInitializer(
+  value: object,
+  shouldPersistRemote = false,
+  depth = 0
+): ShareableRef<object> {
+  const clonedProps: Record<string, unknown> = cloneObjectProperties(
+    value,
+    shouldPersistRemote,
+    depth
+  );
+  return WorkletsModule.makeShareableInitializer(clonedProps);
+}
+
 function cloneArray<T extends unknown[]>(
   value: T,
   shouldPersistRemote: boolean,
@@ -327,28 +369,22 @@ function cloneWorklet<T extends WorkletFunction>(
     // seems more elegant to handle it this way.
     delete (value as WorkletFunction).__stackDetails;
   }
+  const clonedProps: Record<string, unknown> = cloneObjectProperties(
+    value,
+    true,
+    depth
+  );
   // to save on transferring static __initData field of worklet structure
   // we request shareable value to persist its UI counterpart. This means
-  // that the __initData field that contains long strings represeting the
+  // that the __initData field that contains long strings representing the
   // worklet code, source map, and location, will always be
   // serialized/deserialized once.
-  const clonedProps: Record<string, unknown> = {};
   clonedProps.__initData = makeShareableCloneRecursive(
     value.__initData,
     true,
     depth + 1
   );
 
-  for (const [key, element] of Object.entries(value)) {
-    if (key === '__initData' && clonedProps.__initData !== undefined) {
-      continue;
-    }
-    clonedProps[key] = makeShareableCloneRecursive(
-      element,
-      shouldPersistRemote,
-      depth + 1
-    );
-  }
   const clone = WorkletsModule.makeShareableClone(
     clonedProps,
     // retain all worklets
@@ -365,7 +401,7 @@ function cloneWorklet<T extends WorkletFunction>(
 function cloneContextObject<T extends object>(value: T): ShareableRef<T> {
   const workletContextObjectFactory = (value as Record<string, unknown>)
     .__workletContextObjectFactory as () => T;
-  const handle = makeShareableCloneRecursive({
+  const handle = cloneInitializer({
     __init: () => {
       'worklet';
       return workletContextObjectFactory();
@@ -380,17 +416,11 @@ function clonePlainJSObject<T extends object>(
   shouldPersistRemote: boolean,
   depth: number
 ): ShareableRef<T> {
-  const clonedProps: Record<string, unknown> = {};
-  for (const [key, element] of Object.entries(value)) {
-    if (key === '__initData' && clonedProps.__initData !== undefined) {
-      continue;
-    }
-    clonedProps[key] = makeShareableCloneRecursive(
-      element,
-      shouldPersistRemote,
-      depth + 1
-    );
-  }
+  const clonedProps: Record<string, unknown> = cloneObjectProperties(
+    value,
+    shouldPersistRemote,
+    depth
+  );
   const clone = WorkletsModule.makeShareableClone(
     clonedProps,
     shouldPersistRemote,
@@ -406,7 +436,7 @@ function clonePlainJSObject<T extends object>(
 function cloneRegExp<T extends RegExp>(value: T): ShareableRef<T> {
   const pattern = value.source;
   const flags = value.flags;
-  const handle = makeShareableCloneRecursive({
+  const handle = cloneInitializer({
     __init: () => {
       'worklet';
       return new RegExp(pattern, flags);
@@ -419,7 +449,7 @@ function cloneRegExp<T extends RegExp>(value: T): ShareableRef<T> {
 
 function cloneError<T extends Error>(value: T): ShareableRef<T> {
   const { name, message, stack } = value;
-  const handle = makeShareableCloneRecursive({
+  const handle = cloneInitializer({
     __init: () => {
       'worklet';
       // eslint-disable-next-line reanimated/use-worklets-error
@@ -454,7 +484,7 @@ function cloneArrayBufferView<T extends ArrayBufferView>(
 ): ShareableRef<T> {
   const buffer = value.buffer;
   const typeName = value.constructor.name;
-  const handle = makeShareableCloneRecursive({
+  const handle = cloneInitializer({
     __init: () => {
       'worklet';
       if (!VALID_ARRAY_VIEWS_NAMES.includes(typeName)) {
@@ -586,6 +616,9 @@ export function makeShareableCloneOnUIRecursive<T>(
         ) as FlatShareableRef<T>;
       }
       const toAdapt: Record<string, FlatShareableRef<T>> = {};
+      if (isPlainJSObject(value) && value.__init) {
+        return global._makeShareableInitializer(toAdapt);
+      }
       for (const [key, element] of Object.entries(value)) {
         toAdapt[key] = cloneRecursive(element);
       }

apps/common-app/src/apps/reanimated/examples/ShareablesExample.tsx
@@ -39,6 +39,7 @@ export default function ShareablesExample() {
         <NumberDemo />
         <UndefinedDemo />
         <NullDemo />
+        <RegExpDemo />
         <CyclicObjectDemo />
         <InaccessibleObjectDemo />
         <RemoteNamedFunctionSyncCallDemo />
@@ -236,6 +237,43 @@ function NullDemo() {
       'worklet';
       try {
         if (x === null) {
+          runOnJS(isOk)();
+        } else {
+          runOnJS(isNotOk)();
+        }
+      } catch (e) {
+        runOnJS(isError)();
+      }
+    })();
+  };
+  return (
+    <DemoItemRow
+      title={title}
+      onPress={handlePress}
+      status={status}
+      expected={expectedStatus}
+    />
+  );
+}
+
+function RegExpDemo() {
+  const title = 'RegExp';
+  const { status, isOk, isNotOk, isError } = useStatus();
+  const expectedStatus: Status = 'ok';
+
+  const handlePress = () => {
+    const regex1 = /test/;
+    // eslint-disable-next-line prefer-regex-literals
+    const regex2 = new RegExp('test');
+    runOnUI(() => {
+      'worklet';
+      try {
+        const checks = [
+          regex1.test('test'),
+          regex2.test('test'),
+          regex1.toString() === regex2.toString(),
+        ];
+        if (checks.every(Boolean)) {
           runOnJS(isOk)();
         } else {
           runOnJS(isNotOk)();

packages/react-native-worklets/Common/cpp/worklets/NativeModules/JSIWorkletsModuleProxy.cpp
@@ -123,6 +123,8 @@ std::vector<jsi::PropNameID> JSIWorkletsModuleProxy::getPropertyNames(
       jsi::PropNameID::forAscii(rt, "makeShareableString"));
   propertyNames.emplace_back(
       jsi::PropNameID::forAscii(rt, "makeShareableUndefined"));
+  propertyNames.emplace_back(
+      jsi::PropNameID::forAscii(rt, "makeShareableInitializer"));
 
   propertyNames.emplace_back(jsi::PropNameID::forAscii(rt, "scheduleOnUI"));
   propertyNames.emplace_back(
@@ -218,6 +220,18 @@ jsi::Value JSIWorkletsModuleProxy::get(
            const jsi::Value &thisValue,
            const jsi::Value *args,
            size_t count) { return makeShareableUndefined(rt); });
+  }
+  if (name == "makeShareableInitializer") {
+    return jsi::Function::createFromHostFunction(
+        rt,
+        propName,
+        1,
+        [](jsi::Runtime &rt,
+           const jsi::Value &thisValue,
+           const jsi::Value *args,
+           size_t count) {
+          return makeShareableInitializer(rt, args[0].asObject(rt));
+        });
   }
   if (name == "scheduleOnUI") {
     return jsi::Function::createFromHostFunction(

packages/react-native-worklets/Common/cpp/worklets/SharedItems/Shareables.cpp
@@ -54,7 +54,7 @@ jsi::Value makeShareableClone(
         shareable = std::make_shared<ShareableWorklet>(rt, object);
       }
     } else if (!object.getProperty(rt, "__init").isUndefined()) {
-      shareable = std::make_shared<ShareableHandle>(rt, object);
+      return makeShareableInitializer(rt, object);
     } else if (object.isFunction(rt)) {
       auto function = object.asFunction(rt);
       if (function.isHostFunction(rt)) {
@@ -116,32 +116,40 @@ jsi::Value makeShareableClone(
 }
 
 jsi::Value makeShareableString(jsi::Runtime &rt, const jsi::String &string) {
-  auto shareable = std::make_shared<ShareableString>(string.utf8(rt));
+  const auto shareable = std::make_shared<ShareableString>(string.utf8(rt));
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
 jsi::Value makeShareableNumber(jsi::Runtime &rt, double number) {
-  auto shareable = std::make_shared<ShareableScalar>(number);
+  const auto shareable = std::make_shared<ShareableScalar>(number);
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
 jsi::Value makeShareableBoolean(jsi::Runtime &rt, bool boolean) {
-  auto shareable = std::make_shared<ShareableScalar>(boolean);
+  const auto shareable = std::make_shared<ShareableScalar>(boolean);
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
 jsi::Value makeShareableBigInt(jsi::Runtime &rt, const jsi::BigInt &bigint) {
-  auto shareable = std::make_shared<ShareableBigInt>(rt, bigint);
+  const auto shareable = std::make_shared<ShareableBigInt>(rt, bigint);
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
 jsi::Value makeShareableUndefined(jsi::Runtime &rt) {
-  auto shareable = std::make_shared<ShareableScalar>();
+  const auto shareable = std::make_shared<ShareableScalar>();
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
 jsi::Value makeShareableNull(jsi::Runtime &rt) {
-  auto shareable = std::make_shared<ShareableScalar>(nullptr);
+  const auto shareable = std::make_shared<ShareableScalar>(nullptr);
+  return ShareableJSRef::newHostObject(rt, shareable);
+}
+
+jsi::Value makeShareableInitializer(
+    jsi::Runtime &rt,
+    const jsi::Object &initializerObject) {
+  const auto shareable =
+      std::make_shared<ShareableInitializer>(rt, initializerObject);
   return ShareableJSRef::newHostObject(rt, shareable);
 }
 
@@ -300,7 +308,7 @@ jsi::Value ShareableRemoteFunction::toJSValue(jsi::Runtime &rt) {
   }
 }
 
-jsi::Value ShareableHandle::toJSValue(jsi::Runtime &rt) {
+jsi::Value ShareableInitializer::toJSValue(jsi::Runtime &rt) {
   if (remoteValue_ == nullptr) {
     auto initObj = initializer_->toJSValue(rt);
     auto value = std::make_unique<jsi::Value>(getValueUnpacker(rt).call(

packages/react-native-worklets/Common/cpp/worklets/SharedItems/Shareables.h
@@ -157,6 +157,10 @@ jsi::Value makeShareableUndefined(jsi::Runtime &rt);
 
 jsi::Value makeShareableNull(jsi::Runtime &rt);
 
+jsi::Value makeShareableInitializer(
+    jsi::Runtime &rt,
+    const jsi::Object &initializerObject);
+
 std::shared_ptr<Shareable> extractShareableOrThrow(
     jsi::Runtime &rt,
     const jsi::Value &maybeShareableValue,
@@ -285,7 +289,7 @@ class ShareableRemoteFunction
   jsi::Value toJSValue(jsi::Runtime &rt) override;
 };
 
-class ShareableHandle : public Shareable {
+class ShareableInitializer : public Shareable {
  private:
   // We don't release the initializer since the handle can get
   // initialized in parallel on multiple threads. However this is not a problem,
@@ -297,12 +301,12 @@ class ShareableHandle : public Shareable {
   jsi::Runtime *remoteRuntime_;
 
  public:
-  ShareableHandle(jsi::Runtime &rt, const jsi::Object &initializerObject)
+  ShareableInitializer(jsi::Runtime &rt, const jsi::Object &initializerObject)
       : Shareable(HandleType),
         initializer_(std::make_unique<ShareableObject>(rt, initializerObject)) {
   }
 
-  ~ShareableHandle() {
+  ~ShareableInitializer() {
     cleanupIfRuntimeExists(remoteRuntime_, remoteValue_);
   }
 

packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/WorkletRuntimeDecorator.cpp
@@ -142,6 +142,13 @@ void WorkletRuntimeDecorator::decorate(
 
   jsi_utils::installJsiFunction(
       rt,
+      "_makeShareableInitializer",
+      [](jsi::Runtime &rt, const jsi::Value &value) {
+        return makeShareableInitializer(rt, value.asObject(rt));
+      });
+
+  jsi_utils::installJsiFunction(
+      rt,
       "_scheduleRemoteFunctionOnJS",
       [jsScheduler](
           jsi::Runtime &rt,

packages/react-native-worklets/src/WorkletsModule/JSWorklets.ts
@@ -62,6 +62,12 @@ class JSWorklets implements IWorkletsModule {
     );
   }
 
+  makeShareableInitializer(): ShareableRef<object> {
+    throw new WorkletsError(
+      'makeShareableInitializer should never be called in JSWorklets.'
+    );
+  }
+
   scheduleOnUI<TValue>(worklet: ShareableRef<TValue>) {
     // TODO: `requestAnimationFrame` should be used exclusively in Reanimated
 

packages/react-native-worklets/src/WorkletsModule/NativeWorklets.ts
@@ -74,6 +74,10 @@ See https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooti
     return this.#shareableNull;
   }
 
+  makeShareableInitializer(obj: object) {
+    return this.#workletsModuleProxy.makeShareableInitializer(obj);
+  }
+
   scheduleOnUI<TValue>(shareable: ShareableRef<TValue>) {
     return this.#workletsModuleProxy.scheduleOnUI(shareable);
   }

commit 798434513dacfc4a031c936201d2d07cdfb54858

fix: Crashes by adding missing worklet directive to plain object check (#7579)
## Summary

Quick fix for the issue introduced in the #7548 PR.

## Test plan

Open the drawer navigator in the fabric example app and see this crash
when there is no `'worklet'` directive.

![Screenshot 2025-05-23 at 17 08
53](https://github.com/user-attachments/assets/75c33c35-a55b-4fc2-a290-c3710dbac239)

packages/react-native-worklets/src/shareables.ts
@@ -34,6 +34,7 @@ function isHostObject(value: NonNullable<object>) {
 }
 
 function isPlainJSObject(object: object): object is Record<string, unknown> {
+  'worklet';
   return Object.getPrototypeOf(object) === Object.prototype;
 }
 