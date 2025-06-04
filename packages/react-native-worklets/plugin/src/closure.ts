636dd155d831359050c3b04a52d930660fddcad7
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-06-04 20:45:51 +0200

    feat(Worklets): generated worklets (#7513)
    ## Summary
    
    This pull request introduces extracting each worklet to its dedicated
    separate file. This feature is available behind a feature flag
    `experimentalBundling` - it has no purpose now, but it will have it in
    the upcoming PRs.
    
    The reason to extract worklets to separate files is to enable importing
    arbitrary worklets on Worklet Runtimes from the bundle.
    
    The file containing a worklet in user-space most likely has imports
    (side-effects) from React Native, which would break a Worklet Runtime.
    By extracting the worklet to a separate file, we get rid of any unwanted
    side-effects.
    
    To illustrate what kind of change this brings, say we have the following
    file:
    
    ```ts
    // file.ts
    const foo = 5;
    
    const bar = () => {
      'worklet';
      return foo;
    };
    ```
    
    With legacy bundling the file would be transformed to:
    
    ```js
    // file.legacy.out.js
    const foo = 5;
    const _worklet_12379853172447_init_data = {
      code: 'function fileTs1(){const{foo}=this.__closure;return foo;}',
      location: 'location data',
      sourceMap: 'source map data',
      version: '0.4.0',
    };
    const bar = (function fileTs1Factory({
      _worklet_12379853172447_init_data,
      foo,
    }) {
      const _e = [new global.Error(), -2, -27];
      const fileTs1 = function () {
        return foo;
      };
      fileTs1.__closure = {
        foo,
      };
      fileTs1.__workletHash = 12379853172447;
      fileTs1.__initData = _worklet_12379853172447_init_data;
      fileTs1.__stackDetails = _e;
      return fileTs1;
    })({
      _worklet_12379853172447_init_data,
      foo,
    });
    
    ```
    
    With experimental bundling, the file output would be:
    
    ```js
    // file.experimental.out.js
    const foo = 5;
    const bar =
      require('react-native-worklets/__generatedWorklets/12379853172447.js').default(
        {
          foo,
        }
      );
    ```
    
    While also emitting an extra file:
    
    ```js
    // react-native-worklets/__generatedWorklets/12379853172447.js
    const _worklet_12379853172447_init_data = {
      code: 'function fileTs1(){const{foo}=this.__closure;return foo;}',
      location: 'location data',
      sourceMap: 'source map data',
      version: '0.4.0',
    };
    export default (function fileTs1Factory({ foo }) {
      const _e = [new global.Error(), -2, -27];
      const fileTs1 = function () {
        return foo;
      };
      fileTs1.__closure = {
        foo,
      };
      fileTs1.__workletHash = 12379853172447;
      fileTs1.__initData = _worklet_12379853172447_init_data;
      fileTs1.__stackDetails = _e;
      return fileTs1;
    });
    ```
    
    For the sake of atomicity, I'm not stripping unused `initData` in
    experimental bundling just yet.
    
    ## Test plan
    
    No testing is required now, as this feature is not yet used.

--- packages/react-native-worklets/plugin/src/closure.ts
+++ packages/react-native-worklets/plugin/src/closure.ts
@@ -1,16 +1,25 @@
 import type { NodePath } from '@babel/core';
-import type { Identifier } from '@babel/types';
+import type { Binding } from '@babel/traverse';
+import type { Identifier, ImportDeclaration } from '@babel/types';
 import { cloneNode } from '@babel/types';
 
 import { globals } from './globals';
 import type { ReanimatedPluginPass, WorkletizableFunction } from './types';
+import { generatedWorkletsDir } from './types';
 
 export function getClosure(
   funPath: NodePath<WorkletizableFunction>,
   state: ReanimatedPluginPass
-): Identifier[] {
+): {
+  closureVariables: Identifier[];
+  libraryBindingsToImport: Set<Binding>;
+  relativeBindingsToImport: Set<Binding>;
+} {
   const capturedNames = new Set<string>();
   const closureVariables = new Array<Identifier>();
+  const libraryBindingsToImport = new Set<Binding>();
+  const relativeBindingsToImport = new Set<Binding>();
+
   funPath.traverse(
     {
       'TSType|TSTypeAliasDeclaration|TSInterfaceDeclaration'(typePath) {
@@ -31,6 +40,14 @@ export function getClosure(
           return;
         }
 
+        const binding = idPath.scope.getBinding(name);
+        if (!binding) {
+          // Implicitly bound variable from the global scope.
+          capturedNames.add(name);
+          closureVariables.push(cloneNode(idPath.node as Identifier, true));
+          return;
+        }
+
         if ('id' in funPath.node) {
           // We must handle recursion and
           // not capture the function itself.
@@ -48,6 +65,22 @@ export function getClosure(
           scope = scope.parent;
         }
 
+        if (
+          state.opts.experimentalBundling &&
+          isImport(binding) &&
+          isAllowedToImport(state.filename)
+        ) {
+          if (isImportRelative(binding)) {
+            capturedNames.add(name);
+            relativeBindingsToImport.add(binding);
+            return;
+          } else {
+            // Disallow imports from 3rd party libraries for now.
+            // libraryBindingsToImport.add(binding);
+            // return;
+          }
+        }
+
         capturedNames.add(name);
         closureVariables.push(cloneNode(idPath.node as Identifier, true));
       },
@@ -55,5 +88,34 @@ export function getClosure(
     state
   );
 
-  return closureVariables;
+  return {
+    closureVariables,
+    libraryBindingsToImport,
+    relativeBindingsToImport,
+  };
+}
+
+function isImport(binding: Binding): boolean {
+  return (
+    binding.kind === 'module' &&
+    binding.constant &&
+    binding.path.isImportSpecifier() &&
+    binding.path.parentPath.isImportDeclaration()
+  );
+}
+
+function isImportRelative(imported: Binding): boolean {
+  return (
+    imported.path.parentPath as NodePath<ImportDeclaration>
+  ).node.source.value.startsWith('.');
+}
+
+function isAllowedToImport(filename: string | undefined): boolean {
+  // Right now we only allow imports in worklets within
+  // `react-native-worklets` package.
+  return (
+    !!filename &&
+    filename.includes('react-native-worklets') &&
+    !filename.includes(generatedWorkletsDir)
+  );
 }
