b9b83d88af5a5fd9c236538da7977e6677352520
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-04 16:03:09 +0200

    fix: useScrollViewOffset not working after delayed ref initialization (#7612)
    ## Summary
    
    This PR fixes `useScrollViewOffset` hook not updating the returned
    SharedValue if the scrollable to which the ref is attached is rendered
    with some delay.
    
    The previous implementation didn't work because the `animatedRef` was
    uninitialized when the `useEffect` hook in the `useScrollViewOffset` was
    called if the ref wasn't passed to any component. The `useEffect` was
    not triggered after the ref was passed to the component later on, even
    though (at least as I understand) the `animatedRef?.current` in the
    `useEffect` dependencies changed, because this change happened after
    hook dependencies were recalculated.
    
    Since the code od the component executes from the top to the bottom
    during render, the `useEffect` hook dependencies array was created
    before the `animatedRef.current` was set in the function created within
    the `useAnimatedRef` hook (it is set as soon as the ref function is
    called with the exiting component argument, which happened after
    `useEffect` hook dependencies were recalculated).
    
    ## Example recordings
    
    ### Mobile
    
    | Before | After |
    |-|-|
    | <video
    src="https://github.com/user-attachments/assets/16b2d7dd-ec29-45be-ab7e-44007e922204"
    /> | <video
    src="https://github.com/user-attachments/assets/9e6f4475-4f66-4b46-a42b-2322a3cdb301"
    /> |
    
    ### Web
    
    | Before | After |
    |-|-|
    | <video
    src="https://github.com/user-attachments/assets/4e8f02af-5aee-498a-a738-a674db062bbc"
    /> | <video
    src="https://github.com/user-attachments/assets/cd0965da-7e2c-4281-ac3b-f3f58c97e153"
    /> |
    
    ## Test plan
    
    Copy-paste the following code snippet and see how it works.
    
    <details>
    <summary>Code snippet</summary>
    
    ```tsx
    import React, { useState } from 'react';
    import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
    import type Animated from 'react-native-reanimated';
    import {
      useAnimatedReaction,
      useAnimatedRef,
      useScrollViewOffset,
    } from 'react-native-reanimated';
    
    export default function EmptyExample() {
      const [shouldRender, setShouldRender] = useState(false);
      const scrollRef = useAnimatedRef<Animated.ScrollView>();
      const scrollOffset = useScrollViewOffset(scrollRef);
    
      useAnimatedReaction(
        () => scrollOffset.value,
        (value) => {
          console.log('scrollOffset', value);
        }
      );
    
      return (
        <View style={styles.container}>
          <Text style={styles.text}>
            ScrollView is {shouldRender ? 'rendered' : 'not rendered'}
          </Text>
          <Button
            title={shouldRender ? 'Remove ScrollView' : 'Render ScrollView'}
            onPress={() => setShouldRender(!shouldRender)}
          />
    
          {shouldRender && (
            <ScrollView
              ref={scrollRef}
              scrollEventThrottle={1000 / 60}
              style={styles.scrollView}>
              {Array.from({ length: 20 }).map((_, index) => (
                <View key={index} style={styles.item}>
                  <Text style={styles.itemText}>Scroll Item {index + 1}</Text>
                </View>
              ))}
            </ScrollView>
          )}
        </View>
      );
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        backgroundColor: '#fff',
        padding: 20,
      },
      text: {
        fontSize: 16,
        marginBottom: 10,
      },
      scrollView: {
        flex: 1,
        marginTop: 20,
      },
      item: {
        height: 100,
        backgroundColor: '#f0f0f0',
        marginVertical: 8,
        borderRadius: 8,
        justifyContent: 'center',
        alignItems: 'center',
      },
      itemText: {
        fontSize: 16,
        color: '#333',
      },
    });
    ```
    </details>
    
    I also checked if the following examples in the example app still work
    (on mobile and web):
    
    - useAnimatedScrollHandler
    - scrollTo
    - useScrollViewOffset

--- packages/react-native-reanimated/src/hook/useAnimatedRef.ts
+++ packages/react-native-reanimated/src/hook/useAnimatedRef.ts
@@ -12,7 +12,12 @@ import type { ShadowNodeWrapper } from '../commonTypes';
 import { getShadowNodeWrapperFromRef } from '../fabricUtils';
 import { makeMutable } from '../mutables';
 import { findNodeHandle } from '../platformFunctions/findNodeHandle';
-import type { AnimatedRef, AnimatedRefOnUI } from './commonTypes';
+import type {
+  AnimatedRef,
+  AnimatedRefObserver,
+  AnimatedRefOnUI,
+  MaybeObserverCleanup,
+} from './commonTypes';
 
 interface MaybeScrollableComponent extends Component {
   getNativeScrollRef?: FlatList['getNativeScrollRef'];
@@ -36,6 +41,9 @@ function useAnimatedRefNative<
   TComponent extends Component,
 >(): AnimatedRef<TComponent> {
   const [tag] = useState(() => makeMutable<ShadowNodeWrapper | null>(null));
+  const observers = useRef<Map<AnimatedRefObserver, MaybeObserverCleanup>>(
+    new Map()
+  ).current;
   const tagRef = useRef<ShadowNodeWrapper | null>(null);
 
   const ref = useRef<AnimatedRef<TComponent> | null>(null);
@@ -60,11 +68,34 @@ function useAnimatedRefNative<
         // We have to unwrap the tag from the shadow node wrapper.
         fun.getTag = () =>
           findNodeHandle(getComponentOrScrollable(component) as Component)!;
-
         fun.current = component;
       }
+
+      if (observers.size) {
+        const currentTag = fun?.getTag?.() ?? null;
+        observers.forEach((cleanup, observer) => {
+          // Perform the cleanup before calling the observer again.
+          // This ensures that all events that were set up in the observer
+          // are cleaned up before the observer sets up new events during
+          // the next call.
+          cleanup?.();
+          observers.set(observer, observer(currentTag));
+        });
+      }
+
       return tagRef.current;
     });
+
+    fun.observe = (observer: AnimatedRefObserver) => {
+      // Call observer immediately to get the initial value
+      const cleanup = observer(fun?.getTag?.() ?? null);
+      observers.set(observer, cleanup);
+
+      return () => {
+        observers.get(observer)?.();
+        observers.delete(observer);
+      };
+    };
 
     fun.current = null;
 
@@ -85,6 +116,9 @@ function useAnimatedRefWeb<
   TComponent extends Component,
 >(): AnimatedRef<TComponent> {
   const tagRef = useRef<ShadowNodeWrapper | null>(null);
+  const observers = useRef<Map<AnimatedRefObserver, MaybeObserverCleanup>>(
+    new Map()
+  ).current;
 
   const ref = useRef<AnimatedRef<TComponent> | null>(null);
 
@@ -103,11 +137,33 @@ function useAnimatedRefWeb<
         // We have to unwrap the tag from the shadow node wrapper.
         fun.getTag = () =>
           findNodeHandle(getComponentOrScrollable(component) as Component)!;
-
         fun.current = component;
       }
+
+      if (observers.size) {
+        const currentTag = fun?.getTag?.() ?? null;
+        observers.forEach((cleanup, observer) => {
+          // Perform the cleanup before calling the observer again.
+          // This ensures that all events that were set up in the observer
+          // are cleaned up before the observer sets up new events during
+          // the next call.
+          cleanup?.();
+          observers.set(observer, observer(currentTag));
+        });
+      }
+
       return tagRef.current;
     });
+
+    fun.observe = (observer: AnimatedRefObserver) => {
+      const cleanup = observer(fun?.getTag?.() ?? null);
+      observers.set(observer, cleanup);
+
+      return () => {
+        observers.get(observer)?.();
+        observers.delete(observer);
+      };
+    };
 
     fun.current = null;
 
