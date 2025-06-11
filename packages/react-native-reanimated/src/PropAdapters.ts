f957359b5262cfa631123bda7aa4d7526cdfb5dc
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-06-11 08:56:34 +0200

    Remove `addWhitelistedNativeProps` in `createAnimatedPropAdapter`

--- packages/react-native-reanimated/src/PropAdapters.ts
+++ packages/react-native-reanimated/src/PropAdapters.ts
@@ -3,7 +3,6 @@ import type {
   AnimatedPropsAdapterFunction,
   AnimatedPropsAdapterWorklet,
 } from './commonTypes';
-import { addWhitelistedNativeProps } from './ConfigHelper';
 
 // @ts-expect-error This overload is required by our API.
 export function createAnimatedPropAdapter(
@@ -13,12 +12,8 @@ export function createAnimatedPropAdapter(
 
 export function createAnimatedPropAdapter(
   adapter: AnimatedPropsAdapterWorklet,
-  nativeProps?: string[]
+  _nativeProps?: string[]
 ): AnimatedPropsAdapterWorklet {
-  const nativePropsToAdd: { [key: string]: boolean } = {};
-  nativeProps?.forEach((prop) => {
-    nativePropsToAdd[prop] = true;
-  });
-  addWhitelistedNativeProps(nativePropsToAdd);
+  // Do nothing
   return adapter;
 }
