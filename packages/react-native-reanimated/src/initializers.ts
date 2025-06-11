d689e41ac27f8e50c21febf0f2450245bfa2a276
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-11 12:14:28 +0200

    Add duplicate logger in reanimated

---
+++ packages/react-native-reanimated/src/initializers.ts
@@ -0,0 +1,15 @@
+import {
+  DEFAULT_LOGGER_CONFIG,
+  registerLoggerConfig,
+  SHOULD_BE_USE_WEB,
+} from './common';
+import { executeOnUIRuntimeSync } from './core';
+
+// the React runtime global scope
+if (!globalThis._WORKLET) {
+  registerLoggerConfig(DEFAULT_LOGGER_CONFIG);
+
+  if (!SHOULD_BE_USE_WEB) {
+    executeOnUIRuntimeSync(registerLoggerConfig)(DEFAULT_LOGGER_CONFIG);
+  }
+}
