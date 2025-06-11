77798b00f626645d6a4593e10fb9ca6081d15ff1
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-06-11 09:41:37 +0200

    Add warning in `createAnimatedPropAdapter`

--- packages/react-native-reanimated/src/PropAdapters.ts
+++ packages/react-native-reanimated/src/PropAdapters.ts
@@ -14,6 +14,8 @@ export function createAnimatedPropAdapter(
   adapter: AnimatedPropsAdapterWorklet,
   _nativeProps?: string[]
 ): AnimatedPropsAdapterWorklet {
-  // Do nothing
+  console.warn(
+    '[Reanimated] `createAnimatedPropAdapter` is no longer necessary in Reanimated 4 and will be removed in next version. Please remove this call from your code and pass the adapter function directly.'
+  );
   return adapter;
 }
