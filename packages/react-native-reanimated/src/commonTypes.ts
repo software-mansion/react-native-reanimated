60dc7da9a308c25fb41336a09637539f4d51b3dc
skusnierz <sebastiano139@gmail.com>
2025-06-16 16:12:31 +0200

    fix eslint error

--- packages/react-native-reanimated/src/commonTypes.ts
+++ packages/react-native-reanimated/src/commonTypes.ts
@@ -253,7 +253,7 @@ export type SharedRegistryItem = {
   keys: Array<number | string>;
 };
 
-export type SharedRegistry = Mutable<Array<SharedRegistryItem>>;
+type SharedRegistry = Mutable<Array<SharedRegistryItem>>;
 
 export interface SharedRegisterer {
   shareableRegistry: SharedRegistry;
