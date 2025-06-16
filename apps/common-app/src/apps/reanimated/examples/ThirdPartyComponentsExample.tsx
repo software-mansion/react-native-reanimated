d06bef39dba853f54626d1d698992389e5453e53
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-06-16 15:48:44 +0200

    Remove `flex: 1`

--- apps/common-app/src/apps/reanimated/examples/ThirdPartyComponentsExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/ThirdPartyComponentsExample.tsx
@@ -161,7 +161,6 @@ export default function ThirdPartyComponentsExample() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
