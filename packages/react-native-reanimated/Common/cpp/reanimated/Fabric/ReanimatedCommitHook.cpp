8ccef040cfbaad9eaa75e7648866462ea92ee7f4
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-06-17 15:24:25 +0200

    Reformat comment

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.cpp
@@ -87,8 +87,8 @@ RootShadowNode::Unshared ReanimatedCommitHook::shadowTreeWillCommit(
     // props will be applied in ReanimatedCommitHook by UpdatesRegistryManager
     // This is very important, since if we didn't pause Reanimated commits,
     // it could lead to RN commits being delayed until the animation is finished
-    // (very bad). We don't pause Reanimated commits for state updates coming from
-    // React Native as this would break sticky header animations.
+    // (very bad). We don't pause Reanimated commits for state updates coming
+    // from React Native as this would break sticky header animations.
     if (commitOptions.source == ShadowTreeCommitSource::React) {
       updatesRegistryManager_->pauseReanimatedCommits();
     }
