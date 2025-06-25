3f1acee6f634705ce934abfded7355eae2fa99b9
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-06-25 17:37:34 +0200

    Lint

--- packages/react-native-reanimated/Common/cpp/reanimated/Tools/FeatureFlags.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/Tools/FeatureFlags.h
@@ -5,26 +5,26 @@
 namespace reanimated {
 
 class StaticFeatureFlags {
-public:
+ public:
 #ifdef REANIMATED_FEATURE_FLAGS
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #define REANIMATED_FEATURE_FLAGS_STRING TOSTRING(REANIMATED_FEATURE_FLAGS)
 
-  static constexpr bool getFlag(const char* key) {
+  static constexpr bool getFlag(const char *key) {
     const std::string keyStr = key;
     std::string featureFlags = REANIMATED_FEATURE_FLAGS_STRING;
     if (featureFlags.find(key) == std::string::npos) {
       // this will cause compilation error not runtime error
       throw std::logic_error("Unable to recognize flag: " + keyStr);
     }
-    return  featureFlags.find("[" + keyStr + ":true]") != std::string::npos;
+    return featureFlags.find("[" + keyStr + ":true]") != std::string::npos;
   }
 
 #else
 
-  static constexpr bool getFlag(const char*) {
+  static constexpr bool getFlag(const char *) {
     return false;
   }
 
@@ -32,10 +32,11 @@ public:
 };
 
 class DynamicFeatureFlags {
-static std::unordered_map<std::string, bool> flags_;
-public:
-  static bool getFlag(const char* key);
-  static void setFlag(const std::string& key, bool value);
+  static std::unordered_map<std::string, bool> flags_;
+
+ public:
+  static bool getFlag(const char *key);
+  static void setFlag(const std::string &key, bool value);
 };
 
 } // namespace reanimated
