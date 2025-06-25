f3ec67830d85a42b37cecc50d250e426437844cb
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-06-25 17:43:30 +0200

    Lint & format

--- packages/react-native-reanimated/Common/cpp/reanimated/Tools/FeatureFlags.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/Tools/FeatureFlags.cpp
@@ -4,13 +4,12 @@ namespace reanimated {
 
 std::unordered_map<std::string, bool> DynamicFeatureFlags::flags_;
 
-bool DynamicFeatureFlags::getFlag(const char* key) {
+bool DynamicFeatureFlags::getFlag(const char *key) {
   return flags_.find(key) != flags_.end() && flags_[key];
 }
 
-void DynamicFeatureFlags::setFlag(const std::string& key, bool value) {
+void DynamicFeatureFlags::setFlag(const std::string &key, bool value) {
   flags_[key] = value;
 }
 
 } // namespace reanimated
-
--- packages/react-native-reanimated/src/featureFlags/staticFlags.ts
+++ packages/react-native-reanimated/src/featureFlags/staticFlags.ts
@@ -1,3 +1,4 @@
+'use strict';
 // ts-prune-ignore-next
 export const StaticFeatureFlags = {
   TEST_STATIC_FLAG: true,
