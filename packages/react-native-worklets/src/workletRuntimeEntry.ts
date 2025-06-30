e9e8c94c808bf9c09eaf32fcdd8cdd505273ebc4
Tomasz Żelawski <tzelawski@gmail.com>
2025-06-30 15:16:23 +0200

    feat(Worklets): gracefully handle imports from react-native on Worklet Runtimes

--- packages/react-native-worklets/src/workletRuntimeEntry.ts
+++ packages/react-native-worklets/src/workletRuntimeEntry.ts
@@ -22,6 +22,52 @@ export function initializeLibraryOnWorkletRuntime() {
     globalThis.__valueUnpacker = bundleValueUnpacker as ValueUnpacker;
     setupCallGuard();
 
+    if (__DEV__) {
+      const modules = require.getModules();
+      const ReactNativeModuleId = require.resolveWeak('react-native');
+      globalThis._log("overriding 'react-native' module");
+
+      const factory = function (
+        _global,
+        _$$_REQUIRE,
+        _$$_IMPORT_DEFAULT,
+        _$$_IMPORT_ALL,
+        module,
+        _exports,
+        _dependencyMap
+      ) {
+        module.exports = new Proxy(
+          {},
+          {
+            get: function get(target, prop) {
+              globalThis.console.warn(
+                `You tried to import '${String(prop)}' from 'react-native' module on a Worklet Runtime. Using 'react-native' module on a Worklet Runtime is not allowed.`
+              );
+              return {
+                get() {
+                  return undefined;
+                },
+              };
+            },
+          }
+        );
+      };
+
+      const mod = {
+        dependencyMap: [],
+        factory,
+        hasError: false,
+        importedAll: {},
+        importedDefault: {},
+        isInitialized: false,
+        publicModule: {
+          exports: {},
+        },
+      };
+
+      modules.set(ReactNativeModuleId, mod);
+    }
+
     // We have to throw an error here because otherwise
     // the next module to be ran would be the React Native one,
     // and we cannot allow it on a Worklet Runtime.
