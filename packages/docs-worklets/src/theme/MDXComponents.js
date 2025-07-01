59daa95d27b01191683fda73aabc33f69be3166e
skusnierz <sebastiano139@gmail.com>
2025-07-01 09:56:29 +0200

    Remove more unused components

--- packages/docs-worklets/src/theme/MDXComponents.js
+++ packages/docs-worklets/src/theme/MDXComponents.js
@@ -1,11 +1,8 @@
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import InteractiveExample from '@site/src/components/InteractiveExample';
-import CollapsibleCode from '@site/src/components/CollapsibleCode';
 import PlatformCompatibility from '@site/src/components/PlatformCompatibility';
 import { Yes, No, Version, Spacer } from '@site/src/components/Compatibility';
-import Optional from '@site/src/components/Optional';
-import AvailableFrom from '@site/src/components/AvailableFrom';
 import Indent from '@site/src/components/Indent';
 import Grid from '@site/src/components/Grid';
 
@@ -15,14 +12,11 @@ export default {
   // Map the "<Highlight>" tag to our Highlight component
   // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   InteractiveExample,
-  CollapsibleCode,
   PlatformCompatibility,
   Yes,
   No,
   Version,
   Spacer,
-  Optional,
-  AvailableFrom,
   Indent,
   Grid,
 };
