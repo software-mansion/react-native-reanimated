1465aebf64ab3188b183e5e5d0a1abba979013fe
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-03 00:17:49 +0200

    Remove logs

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/core/CSSAnimation.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/core/CSSAnimation.cpp
@@ -17,15 +17,15 @@ CSSAnimation::CSSAnimation(
       styleInterpolator_(cssKeyframesRegistry->getOrCreateInterpolator(
           name_,
           shadowNode_->getComponentName())),
-      progressProvider_(std::make_shared<AnimationProgressProvider>(
-          timestamp,
-          settings.duration,
-          settings.delay,
-          settings.iterationCount,
-          settings.direction,
-          settings.easingFunction,
-          cssKeyframesRegistry->getKeyframeEasingFunctions(name_))) {
-  LOG(INFO) << "CSSAnimation created for animation: " << name_;
+      progressProvider_(
+          std::make_shared<AnimationProgressProvider>(
+              timestamp,
+              settings.duration,
+              settings.delay,
+              settings.iterationCount,
+              settings.direction,
+              settings.easingFunction,
+              cssKeyframesRegistry->getKeyframeEasingFunctions(name_))) {
   if (settings.playState == AnimationPlayState::Paused) {
     progressProvider_->pause(timestamp);
   }
