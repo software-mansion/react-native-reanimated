637b16ebe96b01fc5bb630734d7943e9fc8a6e4f
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-02 15:26:05 +0200

    Add requested changes

--- packages/react-native-worklets/src/specs/NativeWorkletsModule.ts
+++ packages/react-native-worklets/src/specs/NativeWorkletsModule.ts
@@ -2,7 +2,6 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
-/** @knipIgnore False positive. Required for TypeScript compilation. */
 export interface Spec extends TurboModule {
   installTurboModule: () => boolean;
 }
