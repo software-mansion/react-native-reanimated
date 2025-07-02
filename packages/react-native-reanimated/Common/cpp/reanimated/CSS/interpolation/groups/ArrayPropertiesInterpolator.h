d2a2dc819393c3d58c99f640f7c41cd0717b31b5
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-02 18:24:38 +0200

    Change updateKeyframes to work with folly::dynamic instead of jsi::Value

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/groups/ArrayPropertiesInterpolator.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/groups/ArrayPropertiesInterpolator.h
@@ -19,7 +19,7 @@ class ArrayPropertiesInterpolator : public GroupPropertiesInterpolator {
   bool equalsReversingAdjustedStartValue(
       const folly::dynamic &propertyValue) const override;
 
-  void updateKeyframes(jsi::Runtime &rt, const jsi::Value &keyframes) override;
+  void updateKeyframes(const folly::dynamic &keyframes) override;
   void updateKeyframesFromStyleChange(
       const folly::dynamic &oldStyleValue,
       const folly::dynamic &newStyleValue,
