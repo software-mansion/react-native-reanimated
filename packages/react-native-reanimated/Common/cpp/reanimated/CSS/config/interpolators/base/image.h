a04fca69109e6889e42c6c7a6706ca96be4b7f6a
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-03 15:45:55 +0200

    Create image interpolators by extending view interpolators

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/config/interpolators/base/image.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/config/interpolators/base/image.h
@@ -1,32 +1,15 @@
 #pragma once
 
-#include <reanimated/CSS/config/interpolators/base/common.h>
+#include <reanimated/CSS/config/interpolators/base/view.h>
 
 namespace reanimated::css {
 
 const InterpolatorFactoriesRecord IMAGE_INTERPOLATORS = mergeInterpolators(
-    FLEX_INTERPOLATORS,
-    SHADOW_INTERPOLATORS_IOS,
-    TRANSFORMS_INTERPOLATORS,
+    VIEW_INTERPOLATORS,
     InterpolatorFactoriesRecord{
         {"resizeMode", value<CSSKeyword>("cover")},
-        {"backfaceVisibility", value<CSSKeyword>("visible")},
-        {"borderBottomLeftRadius",
-         value<CSSDimension>(RelativeTo::Self, "width", 0)},
-        {"borderBottomRightRadius",
-         value<CSSDimension>(RelativeTo::Self, "width", 0)},
-        {"backgroundColor", value<CSSColor>(TRANSPARENT)},
-        {"borderColor", value<CSSColor>(BLACK)},
-        {"borderRadius", value<CSSDimension>(RelativeTo::Self, "width", 0)},
-        {"borderTopLeftRadius",
-         value<CSSDimension>(RelativeTo::Self, "width", 0)},
-        {"borderTopRightRadius",
-         value<CSSDimension>(RelativeTo::Self, "width", 0)},
-        {"overflow", value<CSSKeyword>("visible")},
         {"overlayColor", value<CSSColor>(BLACK)},
         {"tintColor", value<CSSColor>(BLACK)},
-        {"opacity", value<CSSDouble>(1)},
-        {"cursor", value<CSSKeyword>("auto")},
     });
 
 } // namespace reanimated::css
