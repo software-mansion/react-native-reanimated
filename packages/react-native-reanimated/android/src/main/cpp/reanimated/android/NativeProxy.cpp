software-mansion/react-native-reanimated

commit 7b1b33912237c71120c727c660548866f18d5b24

fix: Memory mismanagement with UI scheduled callbacks (#6900)
## Summary

### What
The pattern we used with scheduling callbacks on the UI thread -
capturing `this` as a reference - is prone to memory issues. Given such
code:

```c++
void WorkletsModuleProxy::scheduleOnUI(
    jsi::Runtime &rt,
    const jsi::Value &worklet) {
  auto shareableWorklet = extractShareableOrThrow<ShareableWorklet>(
      rt, worklet, "[Worklets] Only worklets can be scheduled to run on UI.");
  uiScheduler_->scheduleOnUI(
      [this, shareableWorklet] {
        this->uiWorkletRuntime_->runGuarded(shareableWorklet);
      });
}
```

We are likely to run into accessing invalidated memory during a reload.
This is due to fact that `WorkletsModuleProxy` is managed by some object
held by the instance of React Native. Let's look at the following
scenario.

1. `WorkletsModuleProxy` is created on the JS thread and held by the
`WorkletsModule` Native Module.
2. `WorkletsModuleProxy::scheduleOnUI` is invoked on the JS thread. The
callback is scheduled to be executed on the UI thread.
3. Application's reload gets triggered. A tear down of React Native is
starting on the JS thread.
4. `WorkletsModule` gets destroyed. Therefore, `WorkletsModuleProxy` is
released and also destroyed.
5. The callback is finally executed on the UI thread by the scheduler.
However, `this` has been invalidated. The App crashes.

Keep in mind that this isn't exclusive to thread jumps exclusively.
Calling `scheduleOnUI` on the UI thread could still result in the
callback executing after the memory has been invalidated.

`WorkletsModuleProxy` is only an example here, the problem could
manifest in all the places where we pass lambdas that capture `this` by
reference.

### Fix
To fix this I refactored the code so everytime we pass `this` to a
scheduled callback, it would be done via a _weak pointer_ which would
lock the object and prevent it from being destroyed while the callback
is being executed on the UI thread.

packages/react-native-reanimated/android/src/main/cpp/reanimated/android/NativeProxy.cpp
@@ -50,6 +50,7 @@ NativeProxy::NativeProxy(
           isBridgeless,
           getIsReducedMotion())),
       layoutAnimations_(std::move(layoutAnimations)) {
+  reanimatedModuleProxy_->init(getPlatformDependentMethods());
 #ifdef RCT_NEW_ARCH_ENABLED
   commonInit(fabricUIManager);
 #endif // RCT_NEW_ARCH_ENABLED
@@ -477,8 +478,13 @@ PlatformDepMethodsHolder NativeProxy::getPlatformDependentMethods() {
   auto progressLayoutAnimation =
       bindThis(&NativeProxy::progressLayoutAnimation);
 
-  auto endLayoutAnimation = [this](int tag, bool removeView) {
-    this->layoutAnimations_->cthis()->endLayoutAnimation(tag, removeView);
+  auto endLayoutAnimation = [weakThis = weak_from_this()](
+                                int tag, bool removeView) {
+    auto strongThis = weakThis.lock();
+    if (!strongThis) {
+      return;
+    }
+    strongThis->layoutAnimations_->cthis()->endLayoutAnimation(tag, removeView);
   };
 
   auto maybeFlushUiUpdatesQueueFunction =
@@ -622,12 +628,7 @@ void NativeProxy::setupLayoutAnimations() {
 
 void NativeProxy::invalidateCpp() {
   layoutAnimations_->cthis()->invalidate();
-
   workletsModuleProxy_.reset();
-
-  if (reanimatedModuleProxy_ != nullptr) {
-    reanimatedModuleProxy_->invalidate();
-  }
   reanimatedModuleProxy_.reset();
 }
 

packages/react-native-reanimated/android/src/main/cpp/reanimated/android/NativeProxy.h
@@ -141,7 +141,8 @@ class KeyboardWorkletWrapper : public HybridClass<KeyboardWorkletWrapper> {
   std::function<void(int, int)> callback_;
 };
 
-class NativeProxy : public jni::HybridClass<NativeProxy> {
+class NativeProxy : public jni::HybridClass<NativeProxy>,
+                    std::enable_shared_from_this<NativeProxy> {
  public:
   static auto constexpr kJavaDescriptor =
       "Lcom/swmansion/reanimated/NativeProxy;";
@@ -251,6 +252,7 @@ class NativeProxy : public jni::HybridClass<NativeProxy> {
   template <class TReturn, class... TParams>
   std::function<TReturn(TParams...)> bindThis(
       TReturn (NativeProxy::*methodPtr)(TParams...)) {
+    // It's probably safe to pass `this` as reference here...
     return [this, methodPtr](TParams &&...args) {
       return (this->*methodPtr)(std::forward<TParams>(args)...);
     };

packages/react-native-reanimated/Common/cpp/reanimated/CSS/registry/CSSAnimationsRegistry.h
@@ -15,7 +15,9 @@
 
 namespace reanimated {
 
-class CSSAnimationsRegistry : public UpdatesRegistry {
+class CSSAnimationsRegistry
+    : public UpdatesRegistry,
+      std::enable_shared_from_this<CSSAnimationsRegistry> {
  public:
   using SettingsUpdates =
       std::vector<std::pair<unsigned, PartialCSSAnimationSettings>>;

packages/react-native-reanimated/Common/cpp/reanimated/CSS/registry/CSSTransitionsRegistry.cpp
@@ -118,11 +118,16 @@ void CSSTransitionsRegistry::scheduleOrActivateTransition(
 }
 
 PropsObserver CSSTransitionsRegistry::createPropsObserver(const Tag viewTag) {
-  return [this, viewTag](
+  return [weakThis = weak_from_this(), viewTag](
              jsi::Runtime &rt,
              const jsi::Value &oldProps,
              const jsi::Value &newProps) {
-    const auto &transition = registry_.at(viewTag);
+    auto strongThis = weakThis.lock();
+    if (!strongThis) {
+      return;
+    }
+
+    const auto &transition = strongThis->registry_.at(viewTag);
     const auto changedProps = getChangedProps(
         rt,
         oldProps,
@@ -135,14 +140,14 @@ PropsObserver CSSTransitionsRegistry::createPropsObserver(const Tag viewTag) {
     }
 
     {
-      std::lock_guard<std::mutex> lock{mutex_};
+      std::lock_guard<std::mutex> lock{strongThis->mutex_};
 
       const auto &initialProps =
-          transition->run(rt, changedProps, getCurrentTimestamp_());
+          transition->run(rt, changedProps, strongThis->getCurrentTimestamp_());
       const auto &shadowNode = transition->getShadowNode();
 
-      setInUpdatesRegistry(rt, shadowNode, initialProps);
-      scheduleOrActivateTransition(transition);
+      strongThis->setInUpdatesRegistry(rt, shadowNode, initialProps);
+      strongThis->scheduleOrActivateTransition(transition);
     }
   };
 }

packages/react-native-reanimated/Common/cpp/reanimated/CSS/registry/CSSTransitionsRegistry.h
@@ -17,7 +17,9 @@
 
 namespace reanimated {
 
-class CSSTransitionsRegistry : public UpdatesRegistry {
+class CSSTransitionsRegistry
+    : public UpdatesRegistry,
+      public std::enable_shared_from_this<CSSTransitionsRegistry> {
  public:
   CSSTransitionsRegistry(
       const std::shared_ptr<StaticPropsRegistry> &staticPropsRegistry,

packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.cpp
@@ -34,14 +34,16 @@ void ReanimatedCommitHook::maybeInitializeLayoutAnimations(
     // when a new surfaceId is observed we call setMountingOverrideDelegate
     // for all yet unseen surfaces
     uiManager_->getShadowTreeRegistry().enumerate(
-        [this](const ShadowTree &shadowTree, bool &stop) {
-          if (shadowTree.getSurfaceId() <= currentMaxSurfaceId_) {
+        [strongThis = shared_from_this()](
+            const ShadowTree &shadowTree, bool &stop) {
+          // Executed synchronously.
+          if (shadowTree.getSurfaceId() <= strongThis->currentMaxSurfaceId_) {
             // the set function actually adds our delegate to a list, so we
             // shouldn't invoke it twice for the same surface
             return;
           }
           shadowTree.getMountingCoordinator()->setMountingOverrideDelegate(
-              layoutAnimationsProxy_);
+              strongThis->layoutAnimationsProxy_);
         });
     currentMaxSurfaceId_ = surfaceId;
   }

packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.h
@@ -12,7 +12,9 @@ using namespace facebook::react;
 
 namespace reanimated {
 
-class ReanimatedCommitHook : public UIManagerCommitHook {
+class ReanimatedCommitHook
+    : public UIManagerCommitHook,
+      public std::enable_shared_from_this<ReanimatedCommitHook> {
  public:
   ReanimatedCommitHook(
       const std::shared_ptr<UIManager> &uiManager,

packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy.cpp
@@ -618,30 +618,42 @@ void LayoutAnimationsProxy::startEnteringAnimation(
       static_cast<const ViewProps &>(*mutation.newChildShadowView.props);
   auto opacity = viewProps.opacity;
 
-  uiScheduler_->scheduleOnUI(
-      [finalView, current, parent, mutation, opacity, this, tag]() {
-        Rect window{};
-        {
-          auto lock = std::unique_lock<std::recursive_mutex>(mutex);
-          layoutAnimations_.insert_or_assign(
-              tag, LayoutAnimation{finalView, current, parent, opacity});
-          window =
-              surfaceManager.getWindow(mutation.newChildShadowView.surfaceId);
-        }
+  uiScheduler_->scheduleOnUI([weakThis = weak_from_this(),
+                              finalView,
+                              current,
+                              parent,
+                              mutation,
+                              opacity,
+                              tag]() {
+    auto strongThis = weakThis.lock();
+    if (!strongThis) {
+      return;
+    }
 
-        Snapshot values(mutation.newChildShadowView, window);
-        jsi::Object yogaValues(uiRuntime_);
-        yogaValues.setProperty(uiRuntime_, "targetOriginX", values.x);
-        yogaValues.setProperty(uiRuntime_, "targetGlobalOriginX", values.x);
-        yogaValues.setProperty(uiRuntime_, "targetOriginY", values.y);
-        yogaValues.setProperty(uiRuntime_, "targetGlobalOriginY", values.y);
-        yogaValues.setProperty(uiRuntime_, "targetWidth", values.width);
-        yogaValues.setProperty(uiRuntime_, "targetHeight", values.height);
-        yogaValues.setProperty(uiRuntime_, "windowWidth", values.windowWidth);
-        yogaValues.setProperty(uiRuntime_, "windowHeight", values.windowHeight);
-        layoutAnimationsManager_->startLayoutAnimation(
-            uiRuntime_, tag, LayoutAnimationType::ENTERING, yogaValues);
-      });
+    Rect window{};
+    {
+      auto &mutex = strongThis->mutex;
+      auto lock = std::unique_lock<std::recursive_mutex>(mutex);
+      strongThis->layoutAnimations_.insert_or_assign(
+          tag, LayoutAnimation{finalView, current, parent, opacity});
+      window = strongThis->surfaceManager.getWindow(
+          mutation.newChildShadowView.surfaceId);
+    }
+
+    Snapshot values(mutation.newChildShadowView, window);
+    auto &uiRuntime = strongThis->uiRuntime_;
+    jsi::Object yogaValues(uiRuntime);
+    yogaValues.setProperty(uiRuntime, "targetOriginX", values.x);
+    yogaValues.setProperty(uiRuntime, "targetGlobalOriginX", values.x);
+    yogaValues.setProperty(uiRuntime, "targetOriginY", values.y);
+    yogaValues.setProperty(uiRuntime, "targetGlobalOriginY", values.y);
+    yogaValues.setProperty(uiRuntime, "targetWidth", values.width);
+    yogaValues.setProperty(uiRuntime, "targetHeight", values.height);
+    yogaValues.setProperty(uiRuntime, "windowWidth", values.windowWidth);
+    yogaValues.setProperty(uiRuntime, "windowHeight", values.windowHeight);
+    strongThis->layoutAnimationsManager_->startLayoutAnimation(
+        uiRuntime, tag, LayoutAnimationType::ENTERING, yogaValues);
+  });
 }
 
 void LayoutAnimationsProxy::startExitingAnimation(
@@ -652,30 +664,38 @@ void LayoutAnimationsProxy::startExitingAnimation(
 #endif
   auto surfaceId = mutation.oldChildShadowView.surfaceId;
 
-  uiScheduler_->scheduleOnUI([this, tag, mutation, surfaceId]() {
-    auto oldView = mutation.oldChildShadowView;
-    Rect window{};
-    {
-      auto lock = std::unique_lock<std::recursive_mutex>(mutex);
-      createLayoutAnimation(mutation, oldView, surfaceId, tag);
-      window = surfaceManager.getWindow(surfaceId);
-    }
+  uiScheduler_->scheduleOnUI(
+      [weakThis = weak_from_this(), tag, mutation, surfaceId]() {
+        auto strongThis = weakThis.lock();
+        if (!strongThis) {
+          return;
+        }
 
-    Snapshot values(oldView, window);
+        auto oldView = mutation.oldChildShadowView;
+        Rect window{};
+        {
+          auto &mutex = strongThis->mutex;
+          auto lock = std::unique_lock<std::recursive_mutex>(mutex);
+          strongThis->createLayoutAnimation(mutation, oldView, surfaceId, tag);
+          window = strongThis->surfaceManager.getWindow(surfaceId);
+        }
 
-    jsi::Object yogaValues(uiRuntime_);
-    yogaValues.setProperty(uiRuntime_, "currentOriginX", values.x);
-    yogaValues.setProperty(uiRuntime_, "currentGlobalOriginX", values.x);
-    yogaValues.setProperty(uiRuntime_, "currentOriginY", values.y);
-    yogaValues.setProperty(uiRuntime_, "currentGlobalOriginY", values.y);
-    yogaValues.setProperty(uiRuntime_, "currentWidth", values.width);
-    yogaValues.setProperty(uiRuntime_, "currentHeight", values.height);
-    yogaValues.setProperty(uiRuntime_, "windowWidth", values.windowWidth);
-    yogaValues.setProperty(uiRuntime_, "windowHeight", values.windowHeight);
-    layoutAnimationsManager_->startLayoutAnimation(
-        uiRuntime_, tag, LayoutAnimationType::EXITING, yogaValues);
-    layoutAnimationsManager_->clearLayoutAnimationConfig(tag);
-  });
+        Snapshot values(oldView, window);
+
+        auto &uiRuntime = strongThis->uiRuntime_;
+        jsi::Object yogaValues(uiRuntime);
+        yogaValues.setProperty(uiRuntime, "currentOriginX", values.x);
+        yogaValues.setProperty(uiRuntime, "currentGlobalOriginX", values.x);
+        yogaValues.setProperty(uiRuntime, "currentOriginY", values.y);
+        yogaValues.setProperty(uiRuntime, "currentGlobalOriginY", values.y);
+        yogaValues.setProperty(uiRuntime, "currentWidth", values.width);
+        yogaValues.setProperty(uiRuntime, "currentHeight", values.height);
+        yogaValues.setProperty(uiRuntime, "windowWidth", values.windowWidth);
+        yogaValues.setProperty(uiRuntime, "windowHeight", values.windowHeight);
+        strongThis->layoutAnimationsManager_->startLayoutAnimation(
+            uiRuntime, tag, LayoutAnimationType::EXITING, yogaValues);
+        strongThis->layoutAnimationsManager_->clearLayoutAnimationConfig(tag);
+      });
 }
 
 void LayoutAnimationsProxy::startLayoutAnimation(
@@ -686,36 +706,46 @@ void LayoutAnimationsProxy::startLayoutAnimation(
 #endif
   auto surfaceId = mutation.oldChildShadowView.surfaceId;
 
-  uiScheduler_->scheduleOnUI([this, mutation, surfaceId, tag]() {
+  uiScheduler_->scheduleOnUI([weakThis = weak_from_this(),
+                              mutation,
+                              surfaceId,
+                              tag]() {
+    auto strongThis = weakThis.lock();
+    if (!strongThis) {
+      return;
+    }
+
     auto oldView = mutation.oldChildShadowView;
     Rect window{};
     {
+      auto &mutex = strongThis->mutex;
       auto lock = std::unique_lock<std::recursive_mutex>(mutex);
-      createLayoutAnimation(mutation, oldView, surfaceId, tag);
-      window = surfaceManager.getWindow(surfaceId);
+      strongThis->createLayoutAnimation(mutation, oldView, surfaceId, tag);
+      window = strongThis->surfaceManager.getWindow(surfaceId);
     }
 
     Snapshot currentValues(oldView, window);
     Snapshot targetValues(mutation.newChildShadowView, window);
 
-    jsi::Object yogaValues(uiRuntime_);
-    yogaValues.setProperty(uiRuntime_, "currentOriginX", currentValues.x);
-    yogaValues.setProperty(uiRuntime_, "currentGlobalOriginX", currentValues.x);
-    yogaValues.setProperty(uiRuntime_, "currentOriginY", currentValues.y);
-    yogaValues.setProperty(uiRuntime_, "currentGlobalOriginY", currentValues.y);
-    yogaValues.setProperty(uiRuntime_, "currentWidth", currentValues.width);
-    yogaValues.setProperty(uiRuntime_, "currentHeight", currentValues.height);
-    yogaValues.setProperty(uiRuntime_, "targetOriginX", targetValues.x);
-    yogaValues.setProperty(uiRuntime_, "targetGlobalOriginX", targetValues.x);
-    yogaValues.setProperty(uiRuntime_, "targetOriginY", targetValues.y);
-    yogaValues.setProperty(uiRuntime_, "targetGlobalOriginY", targetValues.y);
-    yogaValues.setProperty(uiRuntime_, "targetWidth", targetValues.width);
-    yogaValues.setProperty(uiRuntime_, "targetHeight", targetValues.height);
-    yogaValues.setProperty(uiRuntime_, "windowWidth", targetValues.windowWidth);
+    auto &uiRuntime = strongThis->uiRuntime_;
+    jsi::Object yogaValues(uiRuntime);
+    yogaValues.setProperty(uiRuntime, "currentOriginX", currentValues.x);
+    yogaValues.setProperty(uiRuntime, "currentGlobalOriginX", currentValues.x);
+    yogaValues.setProperty(uiRuntime, "currentOriginY", currentValues.y);
+    yogaValues.setProperty(uiRuntime, "currentGlobalOriginY", currentValues.y);
+    yogaValues.setProperty(uiRuntime, "currentWidth", currentValues.width);
+    yogaValues.setProperty(uiRuntime, "currentHeight", currentValues.height);
+    yogaValues.setProperty(uiRuntime, "targetOriginX", targetValues.x);
+    yogaValues.setProperty(uiRuntime, "targetGlobalOriginX", targetValues.x);
+    yogaValues.setProperty(uiRuntime, "targetOriginY", targetValues.y);
+    yogaValues.setProperty(uiRuntime, "targetGlobalOriginY", targetValues.y);
+    yogaValues.setProperty(uiRuntime, "targetWidth", targetValues.width);
+    yogaValues.setProperty(uiRuntime, "targetHeight", targetValues.height);
+    yogaValues.setProperty(uiRuntime, "windowWidth", targetValues.windowWidth);
     yogaValues.setProperty(
-        uiRuntime_, "windowHeight", targetValues.windowHeight);
-    layoutAnimationsManager_->startLayoutAnimation(
-        uiRuntime_, tag, LayoutAnimationType::LAYOUT, yogaValues);
+        uiRuntime, "windowHeight", targetValues.windowHeight);
+    strongThis->layoutAnimationsManager_->startLayoutAnimation(
+        uiRuntime, tag, LayoutAnimationType::LAYOUT, yogaValues);
   });
 }
 
@@ -731,8 +761,14 @@ void LayoutAnimationsProxy::maybeCancelAnimation(const int tag) const {
     return;
   }
   layoutAnimations_.erase(tag);
-  uiScheduler_->scheduleOnUI([this, tag]() {
-    layoutAnimationsManager_->cancelLayoutAnimation(uiRuntime_, tag);
+  uiScheduler_->scheduleOnUI([weakThis = weak_from_this(), tag]() {
+    auto strongThis = weakThis.lock();
+    if (!strongThis) {
+      return;
+    }
+
+    auto &uiRuntime = strongThis->uiRuntime_;
+    strongThis->layoutAnimationsManager_->cancelLayoutAnimation(uiRuntime, tag);
   });
 }
 

commit b54491f757b067ade2e4e6f5a72479784a6a8cf6

[3.18] Fix layout animations on Android (#7757)
## Summary

This PR fixes a small regression introduced in
[#6900](https://github.com/software-mansion/react-native-reanimated/pull/6900).
The issue occurred because we called `weak_from_this()` inside the
constructor, but doing so in a constructor always returns null. As a
result, we couldn't end the layout animation.

To fix this, I moved the call to the `installBinding` function, which is
called later. However, this still didn't solve the problem because you
can only call `weak_from_this()` on instances created as shared
pointers, while HybridObjects are managed by JNI and are unique
pointers.

So, I decided to create a weakable reference wrapper that wraps the
desired object into a shared pointer and allows you to obtain a weak
reference to it.

Fixes
https://github.com/software-mansion/react-native-reanimated/issues/7723


https://github.com/user-attachments/assets/75419bb6-ce71-4651-b65a-c7ee286ff0e8

## Test plan

<details>
<summary>code</summary>

```js
import { useCallback, useState } from 'react';
import { Button, View, Text, SafeAreaView, StyleSheet } from 'react-native';
import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';


export default function App() {
  const [isVisible, setVisible] = useState(true);
  const [count, setCount] = useState(0);

  const onToggle = useCallback(() => {
    setVisible((prev) => !prev);
  }, []);

  return (
    <SafeAreaView style={styles.container}>
      <View style={{ padding: 16 }}>
        <Button title="Toggle" onPress={onToggle} />
      </View>
      {isVisible && (
        <Animated.View entering={FadeIn} exiting={FadeOut}>
          <View
            style={{ width: '100%', borderColor: 'black', borderWidth: 1, alignItems: 'center' }}
          >
            <Text>Count: {count}</Text>
            <Button
              title="Clickable"
              onPress={() => {
                setCount((p) => p + 1);
              }}
            />

packages/react-native-reanimated/android/src/main/cpp/reanimated/android/NativeProxy.cpp
@@ -41,14 +41,14 @@ NativeProxy::NativeProxy(
     )
     : javaPart_(jni::make_global(jThis)),
       rnRuntime_(rnRuntime),
+      layoutAnimations_(std::make_shared<jni::global_ref<LayoutAnimations::javaobject>>(std::move(layoutAnimations))),
       reanimatedModuleProxy_(std::make_shared<ReanimatedModuleProxy>(
           workletsModuleProxy,
           *rnRuntime,
           jsCallInvoker,
           getPlatformDependentMethods(),
           isBridgeless,
-          getIsReducedMotion())),
-      layoutAnimations_(std::move(layoutAnimations)) {
+          getIsReducedMotion())) {
   reanimatedModuleProxy_->init(getPlatformDependentMethods());
 #ifdef RCT_NEW_ARCH_ENABLED
   commonInit(fabricUIManager);
@@ -407,7 +407,7 @@ void NativeProxy::progressLayoutAnimation(
     const jsi::Object &newProps,
     bool isSharedTransition) {
   auto newPropsJNI = JNIHelper::ConvertToPropsMap(rt, newProps);
-  layoutAnimations_->cthis()->progressLayoutAnimation(
+  (*layoutAnimations_)->cthis()->progressLayoutAnimation(
       tag, newPropsJNI, isSharedTransition);
 }
 
@@ -450,13 +450,11 @@ PlatformDepMethodsHolder NativeProxy::getPlatformDependentMethods() {
   auto progressLayoutAnimation =
       bindThis(&NativeProxy::progressLayoutAnimation);
 
-  auto endLayoutAnimation = [weakThis = weak_from_this()](
+  auto endLayoutAnimation = [layoutAnimationsWeak = std::weak_ptr<jni::global_ref<LayoutAnimations::javaobject>>(layoutAnimations_)](
                                 int tag, bool removeView) {
-    auto strongThis = weakThis.lock();
-    if (!strongThis) {
-      return;
+    if (auto layoutAnimations = layoutAnimationsWeak.lock()) {
+      (*layoutAnimations)->cthis()->endLayoutAnimation(tag, removeView);
     }
-    strongThis->layoutAnimations_->cthis()->endLayoutAnimation(tag, removeView);
   };
 
   auto maybeFlushUiUpdatesQueueFunction =
@@ -489,8 +487,9 @@ PlatformDepMethodsHolder NativeProxy::getPlatformDependentMethods() {
 void NativeProxy::setupLayoutAnimations() {
   auto weakReanimatedModuleProxy =
       std::weak_ptr<ReanimatedModuleProxy>(reanimatedModuleProxy_);
+  const auto &layoutAnimations = (*layoutAnimations_);
 
-  layoutAnimations_->cthis()->setAnimationStartingBlock(
+  layoutAnimations->cthis()->setAnimationStartingBlock(
       [weakReanimatedModuleProxy](
           int tag, int type, alias_ref<JMap<jstring, jstring>> values) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
@@ -520,7 +519,7 @@ void NativeProxy::setupLayoutAnimations() {
         }
       });
 
-  layoutAnimations_->cthis()->setHasAnimationBlock(
+  layoutAnimations->cthis()->setHasAnimationBlock(
       [weakReanimatedModuleProxy](int tag, int type) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           return reanimatedModuleProxy->layoutAnimationsManager()
@@ -529,7 +528,7 @@ void NativeProxy::setupLayoutAnimations() {
         return false;
       });
 
-  layoutAnimations_->cthis()->setShouldAnimateExitingBlock(
+  layoutAnimations->cthis()->setShouldAnimateExitingBlock(
       [weakReanimatedModuleProxy](int tag, bool shouldAnimate) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           return reanimatedModuleProxy->layoutAnimationsManager()
@@ -539,7 +538,7 @@ void NativeProxy::setupLayoutAnimations() {
       });
 
 #ifndef NDEBUG
-  layoutAnimations_->cthis()->setCheckDuplicateSharedTag(
+  layoutAnimations->cthis()->setCheckDuplicateSharedTag(
       [weakReanimatedModuleProxy](int viewTag, int screenTag) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           reanimatedModuleProxy->layoutAnimationsManager()
@@ -548,7 +547,7 @@ void NativeProxy::setupLayoutAnimations() {
       });
 #endif
 
-  layoutAnimations_->cthis()->setClearAnimationConfigBlock(
+  layoutAnimations->cthis()->setClearAnimationConfigBlock(
       [weakReanimatedModuleProxy](int tag) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           reanimatedModuleProxy->layoutAnimationsManager()
@@ -556,7 +555,7 @@ void NativeProxy::setupLayoutAnimations() {
         }
       });
 
-  layoutAnimations_->cthis()->setCancelAnimationForTag(
+  layoutAnimations->cthis()->setCancelAnimationForTag(
       [weakReanimatedModuleProxy](int tag) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           jsi::Runtime &rt = reanimatedModuleProxy->getUIRuntime();
@@ -565,7 +564,7 @@ void NativeProxy::setupLayoutAnimations() {
         }
       });
 
-  layoutAnimations_->cthis()->setFindPrecedingViewTagForTransition(
+  layoutAnimations->cthis()->setFindPrecedingViewTagForTransition(
       [weakReanimatedModuleProxy](int tag) {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           return reanimatedModuleProxy->layoutAnimationsManager()
@@ -575,7 +574,7 @@ void NativeProxy::setupLayoutAnimations() {
         }
       });
 
-  layoutAnimations_->cthis()->setGetSharedGroupBlock(
+  layoutAnimations->cthis()->setGetSharedGroupBlock(
       [weakReanimatedModuleProxy](int tag) -> std::vector<int> {
         if (auto reanimatedModuleProxy = weakReanimatedModuleProxy.lock()) {
           return reanimatedModuleProxy->layoutAnimationsManager()
@@ -587,7 +586,7 @@ void NativeProxy::setupLayoutAnimations() {
 }
 
 void NativeProxy::invalidateCpp() {
-  layoutAnimations_->cthis()->invalidate();
+  (*layoutAnimations_)->cthis()->invalidate();
   // cleanup all animated sensors here, since the next line resets
   // the pointer and it will be too late after it
   reanimatedModuleProxy_->cleanupSensors();

packages/react-native-reanimated/android/src/main/cpp/reanimated/android/NativeProxy.h
@@ -141,8 +141,7 @@ class KeyboardWorkletWrapper : public HybridClass<KeyboardWorkletWrapper> {
   std::function<void(int, int)> callback_;
 };
 
-class NativeProxy : public jni::HybridClass<NativeProxy>,
-                    std::enable_shared_from_this<NativeProxy> {
+class NativeProxy : public jni::HybridClass<NativeProxy> {
  public:
   static auto constexpr kJavaDescriptor =
       "Lcom/swmansion/reanimated/NativeProxy;";
@@ -169,8 +168,9 @@ class NativeProxy : public jni::HybridClass<NativeProxy>,
   friend HybridBase;
   jni::global_ref<NativeProxy::javaobject> javaPart_;
   jsi::Runtime *rnRuntime_;
+  // We keep a shared_ptr to a jni::global_ref because you can't make weak_ptrs from jni::global_ref.
+  std::shared_ptr<jni::global_ref<LayoutAnimations::javaobject>> layoutAnimations_;
   std::shared_ptr<ReanimatedModuleProxy> reanimatedModuleProxy_;
-  jni::global_ref<LayoutAnimations::javaobject> layoutAnimations_;
 #ifndef NDEBUG
   void checkJavaVersion(jsi::Runtime &);
   void injectCppVersion();