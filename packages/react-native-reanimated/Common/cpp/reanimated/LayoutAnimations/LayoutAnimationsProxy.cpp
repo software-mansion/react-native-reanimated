742161f014705cdeca0ce32c9b87c5fc7cee741d
Bartłomiej Błoniarz <56109050+bartlomiejbloniarz@users.noreply.github.com>
2025-07-10 14:52:02 +0200

    Fix zombie view when parent is moved (#7768)
    ## Summary
    
    This PR fixes how we handle the case when a view is supposed to be
    removed but its parent is being moved due to view flattening, and has an
    exiting animation configured. Currently this scenario would lead to a
    state where the children are not removed (because there is an exiting
    animation configured), but the animation would not start (because the
    view is not deleted- it's only moved). This would lead to the children
    being left on the screen forever.
    
    ## Test plan
    
    Test the `[LA] Move with exiting` example.

--- packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy.cpp
@@ -494,7 +494,8 @@ bool LayoutAnimationsProxy::startAnimationsRecursively(
   bool hasAnimatedChildren = false;
 
   shouldRemoveSubviewsWithoutAnimations =
-      shouldRemoveSubviewsWithoutAnimations && !hasExitAnimation;
+      shouldRemoveSubviewsWithoutAnimations &&
+      (!hasExitAnimation || node->state == MOVED);
   std::vector<std::shared_ptr<MutationNode>> toBeRemoved;
 
   // iterate from the end, so that children
