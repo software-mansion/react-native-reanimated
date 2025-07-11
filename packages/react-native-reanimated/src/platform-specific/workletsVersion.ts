46466e86b131320d6088ac4ba9890b54c03b3e51
Tomasz Żelawski <tzelawski@gmail.com>
2025-07-11 12:39:20 +0200

    fix(Reanimated): Worklets version validation scripts failing in runtime

--- packages/react-native-reanimated/src/platform-specific/workletsVersion.ts
+++ packages/react-native-reanimated/src/platform-specific/workletsVersion.ts
@@ -1,6 +1,15 @@
+/* eslint-disable n/no-missing-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 'use strict';
 
+import { ReanimatedError } from '../common/errors';
+
 export function assertWorkletsVersion() {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, n/no-missing-require
-  require('react-native-reanimated/scripts/validate-worklets-version');
+  const result =
+    require('react-native-reanimated/scripts/validate-worklets-version')();
+
+  if (!result.ok) {
+    throw new ReanimatedError(result.message);
+  }
 }
