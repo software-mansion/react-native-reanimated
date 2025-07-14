0862ec2b79d77ea0efd27941679411db0adfd6c4
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-14 14:02:20 +0200

    Add color interpolation example

--- apps/common-app/src/apps/css/examples/animations/routes/properties/svg.ts
+++ apps/common-app/src/apps/css/examples/animations/routes/properties/svg.ts
@@ -6,4 +6,13 @@ export const svgPropertiesRoutes = {
     name: 'Circle',
     Component: svgAnimatedProperties.Circle,
   },
+  Common: {
+    name: 'Common',
+    routes: {
+      Colors: {
+        name: 'Colors',
+        Component: svgAnimatedProperties.common.Colors,
+      },
+    },
+  },
 } satisfies Routes;
