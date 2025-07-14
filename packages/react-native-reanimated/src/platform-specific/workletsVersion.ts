cdff7a7ac3fdb8a25f59336023aac73f04829697
Tomasz Żelawski <tzelawski@gmail.com>
2025-07-14 14:38:36 +0200

    chore: reveiw changes

--- packages/react-native-reanimated/src/platform-specific/workletsVersion.ts
+++ packages/react-native-reanimated/src/platform-specific/workletsVersion.ts
@@ -1,13 +1,12 @@
-/* eslint-disable n/no-missing-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
 'use strict';
+
+//@ts-expect-error There's no types for this script.
+import validateWorkletsVersion from 'react-native-reanimated/scripts/validate-worklets-version';
 
 import { ReanimatedError } from '../common/errors';
 
 export function assertWorkletsVersion() {
-  const result =
-    require('react-native-reanimated/scripts/validate-worklets-version')();
+  const result = validateWorkletsVersion();
 
   if (!result.ok) {
     throw new ReanimatedError(result.message);
