35c5c5950212e2411eb39d79b671586a0ccf9a10
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-07-22 14:42:51 +0200

    Add more test cases

--- apps/common-app/src/apps/reanimated/examples/OpacityTransformExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/OpacityTransformExample.tsx
@@ -22,11 +22,37 @@ export default function OpacityTransformExample() {
 
   const animatedStyle = useAnimatedStyle(() => ({
     opacity: 0.5 + sv.value / 2,
-    transform: [
-      { scale: 0.5 + sv.value / 2 },
-      { rotate: `${sv.value * 90}deg` },
-    ],
-    backgroundColor: interpolateColor(sv.value, [0, 1], ['red', 'lime']),
+    // borderRadius: sv.value * 25,
+    // backgroundColor: interpolateColor(sv.value, [0, 1], ['red', 'lime']),
+    // borderColor: interpolateColor(sv.value, [0, 1], ['red', 'lime']),
+    // color: interpolateColor(sv.value, [0, 1], ['red', 'lime']),
+    // transform: [],
+    // transform: [
+    //   { perspective: Math.pow(2, sv.value * 3 + 4.5) },
+    //   { rotateY: '45deg' },
+    // ],
+    // transform: [{ rotate: `${sv.value * 90}deg` }],
+    // transform: [{ rotate: `${(sv.value * Math.PI) / 2}rad` }],
+    // transform: [{ rotateX: `${sv.value * 90}deg` }],
+    // transform: [{ rotateX: `${(sv.value * Math.PI) / 2}rad` }],
+    // transform: [{ rotateY: `${sv.value * 90}deg` }],
+    // transform: [{ rotateY: `${(sv.value * Math.PI) / 2}rad` }],
+    // transform: [{ rotateZ: `${sv.value * 90}deg` }],
+    // transform: [{ rotateZ: `${(sv.value * Math.PI) / 2}rad` }],
+    // transform: [{ scale: 0.5 + sv.value / 2 }],
+    // transform: [{ scaleX: 0.5 + sv.value / 2 }],
+    // transform: [{ scaleY: 0.5 + sv.value / 2 }],
+    // transform: [{ translateX: sv.value * 100 }],
+    // transform: [{ translateX: `${sv.value * 100}%` }],
+    // transform: [{ translateY: sv.value * 100 }],
+    // transform: [{ translateY: `${sv.value * 100}%` }],
+    // transform: [{ skewX: `${sv.value * 45}deg` }],
+    // transform: [{ skewX: `${(sv.value * Math.PI) / 4}rad` }],
+    // transform: [{ skewY: `${sv.value * 45}deg` }],
+    // transform: [{ skewY: `${(sv.value * Math.PI) / 4}rad` }],
+    // transform: [
+    //   { matrix: [sv.value * 2, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 10, 1] },
+    // ],
   }));
 
   return (
@@ -54,6 +80,8 @@ const styles = StyleSheet.create({
   },
   box: {
     backgroundColor: 'navy',
+    borderColor: 'pink',
+    borderWidth: 1,
     width: SIZE,
     height: SIZE,
   },
