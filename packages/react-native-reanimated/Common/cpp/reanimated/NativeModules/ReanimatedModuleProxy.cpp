5b9413a687c478f3ee56c14ccec4bc8c70ce48a7
Tomasz Zawadzki <tomekzawadzki98@gmail.com>
2025-07-23 11:19:50 +0200

    Restore fast path with `synchronouslyUpdateViewOnUIThread` on Android (#7823)
    ## Summary
    
    This PR restores a fast path for updating animated styles and props
    using `synchronouslyUpdateViewOnUIThread` method on Android, currently
    hidden behind a static feature flag.
    
    The optimization was removed in
    https://github.com/software-mansion/react-native-reanimated/pull/7014
    due to the fact that it broke view measurement and touch detection
    system e.g. when using `Pressable` from react-native.
    
    ## How to test out this change
    
    If you want to test out the new experimental behavior, please:
    1. Make sure you're using `react-native-reanimated@4.0.0` or newer
    2. Enable `ANDROID_SYNCHRONOUSLY_UPDATE_UI_PROPS` feature flag in your
    app's `package.json`:
    ```json
    "reanimated": {
      "staticFeatureFlags": {
        "ANDROID_SYNCHRONOUSLY_UPDATE_UI_PROPS": true
      }
    }
    ```
    3. If you use `Pressable` component from `react-native`, use the one
    from `react-native-gesture-handler`. That's because it attaches to the
    platform view as opposed to the one from `react-native`.
    
    ## Disclaimers
    
    Keep in mind that the fast path works only on Android for components
    with animated `opacity`, `transform`, `backgroundColor`, `borderColor`
    and `borderRadius`. If there is some other style or prop in
    `useAnimatedStyle` or `useAnimatedProps`, all props of this component
    will be updated using the default path (i.e. via `ShadowTree::commit`).
    In some cases, this can lead to non-consistent animation behavior, e.g.
    when one component will be animated using the fast path and the other
    will be animated using the default path.
    
    ## Performance measurements
    
    Initial benchmarks show 2-4x speedup on Android emulator in debug mode.
    
    ## Further improvements
    
    * Use JNI direct access instead of copying int and float arrays
    * O(1) string lookup (hashes?) in string to command conversion functions
    * Make `intBuffer` and `doubleBuffer` static to avoid allocating them on
    each animation frame
    * Use `intBuffer[i++]` instead of `intIterator.nextInt()`
    * Move serializer and parser logic to separate files
    
    ## Test plan

--- packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
@@ -22,6 +22,7 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 
 namespace reanimated {
@@ -64,6 +65,10 @@ ReanimatedModuleProxy::ReanimatedModuleProxy(
       viewStylesRepository_(std::make_shared<ViewStylesRepository>(
           staticPropsRegistry_,
           animatedPropsRegistry_)),
+#ifdef ANDROID
+      synchronouslyUpdateUIPropsFunction_(
+          platformDepMethodsHolder.synchronouslyUpdateUIPropsFunction),
+#endif // ANDROID
       subscribeForKeyboardEventsFunction_(
           platformDepMethodsHolder.subscribeForKeyboardEvents),
       unsubscribeFromKeyboardEventsFunction_(
@@ -664,6 +669,231 @@ void ReanimatedModuleProxy::performOperations() {
     }
 
     shouldUpdateCssAnimations_ = false;
+
+#ifdef ANDROID
+    if constexpr (StaticFeatureFlags::getFlag(
+                      "ANDROID_SYNCHRONOUSLY_UPDATE_UI_PROPS")) {
+      static const std::unordered_set<std::string> synchronousProps = {
+          "transform",
+          "opacity",
+          "borderRadius",
+          "backgroundColor",
+          "borderColor",
+          // "color", // TODO: fix animating color of Animated.Text
+      };
+
+      // NOTE: Keep in sync with NativeProxy.java
+      static constexpr auto CMD_START_OF_VIEW = 1;
+      static constexpr auto CMD_START_OF_TRANSFORM = 2;
+      static constexpr auto CMD_END_OF_TRANSFORM = 3;
+      static constexpr auto CMD_END_OF_VIEW = 4;
+      static constexpr auto CMD_OPACITY = 10;
+      static constexpr auto CMD_BORDER_RADIUS = 11;
+      static constexpr auto CMD_BACKGROUND_COLOR = 12;
+      static constexpr auto CMD_BORDER_COLOR = 13;
+      static constexpr auto CMD_COLOR = 14;
+      static constexpr auto CMD_TRANSFORM_TRANSLATE_X = 100;
+      static constexpr auto CMD_TRANSFORM_TRANSLATE_Y = 101;
+      static constexpr auto CMD_TRANSFORM_SCALE = 102;
+      static constexpr auto CMD_TRANSFORM_SCALE_X = 103;
+      static constexpr auto CMD_TRANSFORM_SCALE_Y = 104;
+      static constexpr auto CMD_TRANSFORM_ROTATE = 105;
+      static constexpr auto CMD_TRANSFORM_ROTATE_X = 106;
+      static constexpr auto CMD_TRANSFORM_ROTATE_Y = 107;
+      static constexpr auto CMD_TRANSFORM_ROTATE_Z = 108;
+      static constexpr auto CMD_TRANSFORM_SKEW_X = 109;
+      static constexpr auto CMD_TRANSFORM_SKEW_Y = 110;
+      static constexpr auto CMD_TRANSFORM_MATRIX = 111;
+      static constexpr auto CMD_TRANSFORM_PERSPECTIVE = 112;
+      static constexpr auto CMD_UNIT_DEG = 200;
+      static constexpr auto CMD_UNIT_RAD = 201;
+      static constexpr auto CMD_UNIT_PX = 202;
+      static constexpr auto CMD_UNIT_PERCENT = 203;
+
+      const auto propNameToCommand = [](const std::string &name) {
+        if (name == "opacity")
+          return CMD_OPACITY;
+        if (name == "borderRadius")
+          return CMD_BORDER_RADIUS;
+        if (name == "backgroundColor")
+          return CMD_BACKGROUND_COLOR;
+        if (name == "borderColor")
+          return CMD_BORDER_COLOR;
+        if (name == "color")
+          return CMD_COLOR;
+        if (name == "transform")
+          return CMD_START_OF_TRANSFORM; // TODO: use CMD_TRANSFORM?
+        throw std::runtime_error("Unsupported style: " + name);
+      };
+
+      const auto transformNameToCommand = [](const std::string &name) {
+        if (name == "translateX")
+          return CMD_TRANSFORM_TRANSLATE_X;
+        if (name == "translateY")
+          return CMD_TRANSFORM_TRANSLATE_Y;
+        if (name == "scale")
+          return CMD_TRANSFORM_SCALE;
+        if (name == "scaleX")
+          return CMD_TRANSFORM_SCALE_X;
+        if (name == "scaleY")
+          return CMD_TRANSFORM_SCALE_Y;
+        if (name == "rotate")
+          return CMD_TRANSFORM_ROTATE;
+        if (name == "rotateX")
+          return CMD_TRANSFORM_ROTATE_X;
+        if (name == "rotateY")
+          return CMD_TRANSFORM_ROTATE_Y;
+        if (name == "rotateZ")
+          return CMD_TRANSFORM_ROTATE_Z;
+        if (name == "skewX")
+          return CMD_TRANSFORM_SKEW_X;
+        if (name == "skewY")
+          return CMD_TRANSFORM_SKEW_Y;
+        if (name == "matrix")
+          return CMD_TRANSFORM_MATRIX;
+        if (name == "perspective")
+          return CMD_TRANSFORM_PERSPECTIVE;
+        throw std::runtime_error("Unsupported transform: " + name);
+      };
+
+      UpdatesBatch synchronousUpdatesBatch, shadowTreeUpdatesBatch;
+
+      for (const auto &[shadowNode, props] : updatesBatch) {
+        bool hasOnlySynchronousProps = true;
+        for (const auto &key : props.keys()) {
+          const auto keyStr = key.asString();
+          if (!synchronousProps.contains(keyStr)) {
+            hasOnlySynchronousProps = false;
+            break;
+          }
+        }
+        if (hasOnlySynchronousProps) {
+          synchronousUpdatesBatch.emplace_back(shadowNode, props);
+        } else {
+          shadowTreeUpdatesBatch.emplace_back(shadowNode, props);
+        }
+      }
+
+      if (!synchronousUpdatesBatch.empty()) {
+        std::vector<int> intBuffer;
+        std::vector<double> doubleBuffer;
+        intBuffer.reserve(1024);
+        doubleBuffer.reserve(1024);
+
+        for (const auto &[shadowNode, props] : synchronousUpdatesBatch) {
+          intBuffer.push_back(CMD_START_OF_VIEW);
+          intBuffer.push_back(shadowNode->getTag());
+          for (const auto &[key, value] : props.items()) {
+            const auto command = propNameToCommand(key.getString());
+            switch (command) {
+              case CMD_OPACITY:
+              case CMD_BORDER_RADIUS:
+                intBuffer.push_back(command);
+                doubleBuffer.push_back(value.asDouble());
+                break;
+
+              case CMD_BACKGROUND_COLOR:
+              case CMD_BORDER_COLOR:
+              case CMD_COLOR:
+                intBuffer.push_back(command);
+                intBuffer.push_back(value.asInt());
+                break;
+
+              case CMD_START_OF_TRANSFORM:
+                intBuffer.push_back(command);
+                react_native_assert(
+                    value.isArray() && "Transform value must be an array");
+                for (const auto &item : value) {
+                  react_native_assert(
+                      item.isObject() &&
+                      "Transform array item must be an object");
+                  react_native_assert(
+                      item.size() == 1 &&
+                      "Transform array item must have exactly one key-value pair");
+                  const auto transformCommand =
+                      transformNameToCommand(item.keys().begin()->getString());
+                  const auto &transformValue = *item.values().begin();
+                  switch (transformCommand) {
+                    case CMD_TRANSFORM_SCALE:
+                    case CMD_TRANSFORM_SCALE_X:
+                    case CMD_TRANSFORM_SCALE_Y:
+                    case CMD_TRANSFORM_PERSPECTIVE: {
+                      intBuffer.push_back(transformCommand);
+                      doubleBuffer.push_back(transformValue.asDouble());
+                      break;
+                    }
+
+                    case CMD_TRANSFORM_TRANSLATE_X:
+                    case CMD_TRANSFORM_TRANSLATE_Y: {
+                      intBuffer.push_back(transformCommand);
+                      if (transformValue.isDouble()) {
+                        intBuffer.push_back(CMD_UNIT_PX);
+                        doubleBuffer.push_back(transformValue.asDouble());
+                      } else if (transformValue.isString()) {
+                        const auto &transformValueStr =
+                            transformValue.getString();
+                        if (!transformValueStr.ends_with("%")) {
+                          throw std::runtime_error(
+                              "String translate must be a percentage");
+                        }
+                        intBuffer.push_back(CMD_UNIT_PERCENT);
+                        doubleBuffer.push_back(
+                            std::stof(transformValueStr.substr(0, -1)));
+                      } else {
+                        throw std::runtime_error(
+                            "Translate value must be a number or a string");
+                      }
+                      break;
+                    }
+
+                    case CMD_TRANSFORM_ROTATE:
+                    case CMD_TRANSFORM_ROTATE_X:
+                    case CMD_TRANSFORM_ROTATE_Y:
+                    case CMD_TRANSFORM_ROTATE_Z:
+                    case CMD_TRANSFORM_SKEW_X:
+                    case CMD_TRANSFORM_SKEW_Y: {
+                      const auto &transformValueStr =
+                          transformValue.getString();
+                      intBuffer.push_back(transformCommand);
+                      if (transformValueStr.ends_with("deg")) {
+                        intBuffer.push_back(CMD_UNIT_DEG);
+                      } else if (transformValueStr.ends_with("rad")) {
+                        intBuffer.push_back(CMD_UNIT_RAD);
+                      } else {
+                        throw std::runtime_error(
+                            "Unsupported rotation unit: " + transformValueStr);
+                      }
+                      doubleBuffer.push_back(
+                          std::stof(transformValueStr.substr(0, -3)));
+                      break;
+                    }
+
+                    case CMD_TRANSFORM_MATRIX: {
+                      intBuffer.push_back(transformCommand);
+                      react_native_assert(
+                          transformValue.isArray() &&
+                          "Matrix must be an array");
+                      int size = transformValue.size();
+                      intBuffer.push_back(size);
+                      for (int i = 0; i < size; i++) {
+                        doubleBuffer.push_back(transformValue[i].asDouble());
+                      }
+                      break;
+                    }
+                  }
+                }
+                intBuffer.push_back(CMD_END_OF_TRANSFORM);
+                break;
+            }
+          }
+          intBuffer.push_back(CMD_END_OF_VIEW);
+        }
+        synchronouslyUpdateUIPropsFunction_(intBuffer, doubleBuffer);
+      }
+
+      updatesBatch = std::move(shadowTreeUpdatesBatch);
+    }
+#endif // ANDROID
 
     if ((updatesBatch.size() > 0) &&
         updatesRegistryManager_->shouldReanimatedSkipCommit()) {
