cda71480d4d670a4327dfb6286e58c5ea5ba322f
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-24 13:04:10 +0200

    Update packages/react-native-reanimated/Common/cpp/reanimated/CSS/config/interpolators/registry.h
    Co-authored-by: Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/config/interpolators/registry.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/config/interpolators/registry.h
@@ -6,7 +6,6 @@
 #include <reanimated/CSS/config/interpolators/base/image.h>
 #include <reanimated/CSS/config/interpolators/base/text.h>
 #include <reanimated/CSS/config/interpolators/base/view.h>
-// react-native-svg style props interpolators
 #include <reanimated/CSS/config/interpolators/svg/circle.h>
 
 #include <stdexcept>
