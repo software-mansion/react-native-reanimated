be0ef8474250df41b66885c77f8f035b087d3ac9
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-24 16:24:51 +0200

    Revert unnecessary changes

--- apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
@@ -1,48 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Animated, {
-  AnimatableValue,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-} from 'react-native-reanimated';
-import { DefaultStyle } from 'react-native-reanimated/lib/typescript/hook/commonTypes';
 
 export default function EmptyExample() {
-  const boxShadowActiveSV = useSharedValue({
-    blurRadius: 7,
-    color: 'blue',
-    offsetX: 20,
-    offsetY: 20,
-    spreadDistance: 10,
-  });
-
-  const styleActive = useAnimatedStyle(() => {
-    return {
-      boxShadow: [
-        withSpring(boxShadowActiveSV.value as unknown as AnimatableValue, {
-          duration: 700,
-        }),
-      ],
-    } as DefaultStyle;
-  });
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     console.log('set value');
-  //     boxShadowActiveSV.value = {
-  //       blurRadius: 10,
-  //       color: 'red',
-  //       offsetX: 30,
-  //       offsetY: 30,
-  //       spreadDistance: 20,
-  //     };
-  //   }, 1000);
-  // }, []);
-
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.box1, styleActive]} />
+      <Text>Hello world!</Text>
     </View>
   );
 }
@@ -52,11 +14,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box1: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'palevioletred',
-    boxShadow: '20px 20px 5px 0px red',
   },
 });
