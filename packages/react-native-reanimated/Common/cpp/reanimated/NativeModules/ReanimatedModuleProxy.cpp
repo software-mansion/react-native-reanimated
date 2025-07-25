0de3f1493c95bd0e1e1b663a23cf4dbfa37d6559
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-07-25 10:33:52 +0200

    feat(Worklets): make AsyncQueue virtual and public (#7886)
    ## Summary
    
    Implementing requested APIs for libraries like
    `react-native-vision-camera`
    https://github.com/software-mansion/react-native-reanimated/discussions/7831.
    
    Future PRs will tackle `createWorkletRuntime` and the rest of the
    necessary changes.
    
    ## Test plan
    
    CI & WorkletRuntime Example

--- packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
@@ -7,7 +7,6 @@
 
 #include <worklets/Registries/EventHandlerRegistry.h>
 #include <worklets/SharedItems/Shareables.h>
-#include <worklets/Tools/AsyncQueue.h>
 #include <worklets/Tools/WorkletEventHandler.h>
 
 #ifdef __ANDROID__
