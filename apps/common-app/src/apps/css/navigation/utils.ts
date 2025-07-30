5f26f7eb728accffebcd85110421e3db44829c50
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-30 13:28:54 +0200

    feat: Support for SVG circle base props (#7750)
    ## Summary
    
    This PR adds support for base SVG circle properties interpolation and an
    example with just SVG-specific props. More example screens for different
    svg props will be added in separate PRs soon.

--- apps/common-app/src/apps/css/navigation/utils.ts
+++ apps/common-app/src/apps/css/navigation/utils.ts
@@ -4,8 +4,23 @@ export function isRouteWithRoutes(route: Route): route is RouteWithRoutes {
   return route && typeof route === 'object' && 'routes' in route;
 }
 
+const isUpperCase = (char: string) => char && char === char.toUpperCase();
+
 export function getScreenTitle(path: string): string {
   const parts = path.split('/');
   const lastPart = parts[parts.length - 1] ?? '';
-  return lastPart.replace(/([A-Z])/g, ' $1').trim();
+  const words = [];
+
+  let firstWordCharIdx = 0;
+  for (let i = 1; i < lastPart.length; i++) {
+    const char = lastPart[i];
+    if (isUpperCase(char) && !isUpperCase(lastPart[i + 1])) {
+      words.push(lastPart.slice(firstWordCharIdx, i));
+      firstWordCharIdx = i;
+    }
+  }
+
+  words.push(lastPart.slice(firstWordCharIdx));
+
+  return words.join(' ');
 }
