75092dc2ac74c8c9790a70f98f17d51874385119
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-01 18:49:26 +0200

    Remove unused code

--- packages/react-native-reanimated/src/css/platform/native/style/processors/svg/others.ts
+++ packages/react-native-reanimated/src/css/platform/native/style/processors/svg/others.ts
@@ -1,11 +1,6 @@
 'use strict';
 import type { ValueProcessor } from '../../types';
 
-export const convertPercentageToNumber: ValueProcessor<
-  number | string,
-  number
-> = (value) => (typeof value === 'number' ? value : parseFloat(value) / 100);
-
 export const convertStringToNumber =
   <S extends string>(
     conversions: Record<S, number>
