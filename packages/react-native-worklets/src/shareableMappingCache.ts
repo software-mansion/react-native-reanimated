f81c733c15361fc7c477dcfae8ae059e7d771a07
skusnierz <sebastiano139@gmail.com>
2025-08-04 17:17:31 +0200

    Update comment

--- packages/react-native-worklets/src/shareableMappingCache.ts
+++ packages/react-native-worklets/src/shareableMappingCache.ts
@@ -15,7 +15,7 @@ During a fast refresh, React holds the same instance of a Mutable
 (that's guaranteed by `useRef`) but `serializableCache` gets regenerated and thus
 becoming empty. This happens when editing the file that contains the definition of this cache.
 
-Because of it, `makeShareableCloneRecursive` can't find given mapping
+Because of it, `createSerializable` can't find given mapping
 in `serializableCache` for the Mutable and tries to clone it as if it was a regular JS object.
 During cloning we use `Object.entries` to iterate over the keys which throws an error on accessing `_value`.
 For convenience we moved this cache to a separate file so it doesn't scare us with red squiggles.
