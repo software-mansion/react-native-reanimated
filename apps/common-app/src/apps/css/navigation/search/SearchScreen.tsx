884ac5b7efc7e3aafe87097f2ee8889005f73578
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-04 11:16:18 +0200

    Remove log

--- apps/common-app/src/apps/css/navigation/search/SearchScreen.tsx
+++ apps/common-app/src/apps/css/navigation/search/SearchScreen.tsx
@@ -139,7 +139,6 @@ export default function SearchScreen({ children }: SearchScreenProps) {
 
           searchBarShowProgress.value = progress;
           if (progress === 1) {
-            console.log('expand', progress);
             runOnJS(changeIsExpanded)(true);
           } else if (progress === 0) {
             runOnJS(changeIsExpanded)(false);
