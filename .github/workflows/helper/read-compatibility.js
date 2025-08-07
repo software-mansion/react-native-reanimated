87d871a84a8ade0ca456880aff9a52d5122c0ed3
skusnierz <sebastiano139@gmail.com>
2025-08-07 12:50:43 +0200

    run formatter

--- .github/workflows/helper/read-compatibility.js
+++ .github/workflows/helper/read-compatibility.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const compatibilityPath = path.join(__dirname, '..', '..', '..', 'packages', 'react-native-reanimated', 'compatibility.json');
-const compatibilityData = JSON.parse(fs.readFileSync(compatibilityPath, 'utf8'));
+const compatibilityPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'packages',
+  'react-native-reanimated',
+  'compatibility.json'
+);
+const compatibilityData = JSON.parse(
+  fs.readFileSync(compatibilityPath, 'utf8')
+);
 
 const platforms = compatibilityData['platforms'];
 const platformsJson = JSON.stringify(platforms);
@@ -14,5 +24,11 @@ const versionsWithNightly = [...versions, 'nightly'];
 const versionsWithNightlyJson = JSON.stringify(versionsWithNightly);
 
 fs.writeFileSync('/tmp/platforms.json', 'platforms=' + platformsJson);
-fs.writeFileSync('/tmp/react-native-versions.json', 'react-native-versions=' + versionsJson);
-fs.writeFileSync('/tmp/react-native-versions-with-nightly.json', 'react-native-versions-with-nightly=' + versionsWithNightlyJson);
+fs.writeFileSync(
+  '/tmp/react-native-versions.json',
+  'react-native-versions=' + versionsJson
+);
+fs.writeFileSync(
+  '/tmp/react-native-versions-with-nightly.json',
+  'react-native-versions-with-nightly=' + versionsWithNightlyJson
+);
