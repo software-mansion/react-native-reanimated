0ffada6fdeea5e2ef213729170b01cc781ad5678
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-07 15:34:05 +0200

    Fix Keyframe layout animation crashes

--- packages/react-native-reanimated/src/layoutReanimation/web/config.ts
+++ packages/react-native-reanimated/src/layoutReanimation/web/config.ts
@@ -43,7 +43,10 @@ import type { AnimationData, AnimationStyle } from './animationParser';
 
 export type AnimationCallback = ((finished: boolean) => void) | null;
 
-export type KeyframeDefinitions = Record<number, AnimationStyle>;
+export type KeyframeDefinitions = Record<
+  `${number}` | 'from' | 'to',
+  AnimationStyle
+>;
 
 export type InitialValuesStyleProps = Omit<StyleProps, 'opacity'> & {
   opacity?: number;
