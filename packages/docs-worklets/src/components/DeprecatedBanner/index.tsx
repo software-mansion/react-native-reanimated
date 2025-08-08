4ccaceaf4918aaadaf5609c0fc1020b977b5cc33
skusnierz <sebastiano139@gmail.com>
2025-08-08 13:59:52 +0200

    Add isSerializableRef page

--- packages/docs-worklets/src/components/DeprecatedBanner/index.tsx
+++ packages/docs-worklets/src/components/DeprecatedBanner/index.tsx
@@ -1,10 +1,11 @@
 import styles from './styles.module.css';
 
-export default function DeprecatedBanner() {
+export default function DeprecatedBanner({ text }: { text: string }) {
   return (
     <div className={styles.container}>
       <span className={styles.text}>
-        ⚠️ This is deprecated and will be removed in the next major release.
+        {text ||
+          '⚠️ This is deprecated and will be removed in the next major release.'}
       </span>
     </div>
   );
