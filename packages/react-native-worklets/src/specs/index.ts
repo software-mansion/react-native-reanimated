95d4533843b61c9d208a63359ef15987a488844b
Tomasz Żelawski <tzelawski@gmail.com>
2025-08-12 16:59:06 +0200

    chore: review changes

--- packages/react-native-worklets/src/specs/index.ts
+++ packages/react-native-worklets/src/specs/index.ts
@@ -7,4 +7,7 @@ import RNWorkletsTurboModule from './NativeWorkletsModule';
 export const WorkletsTurboModule: Spec | null =
   globalThis.__RUNTIME_KIND === RuntimeKind.ReactNative
     ? RNWorkletsTurboModule
-    : null;
+    : // In Bundle Mode, on Worklet Runtimes `RNWorkletsTurboModule` isn't
+      // available and shouldn't be accessed. We return null here
+      // to keep the same codebase for the Bundle Mode and legacy behavior.
+      null;
