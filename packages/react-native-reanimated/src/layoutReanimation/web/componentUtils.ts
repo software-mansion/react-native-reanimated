e8c28f442338397ac3d56b84f14a4a93ac5ea9cf
Michał Bert <63123542+m-bert@users.noreply.github.com>
2025-08-12 08:32:32 +0200

    [Web LA] Correctly handle `Easing.bezier` (#8049)
    ## Summary
    
    ### Preface
    
    Currently using `Easing.bezier` is not supported in layout animations on
    web. If you try to use it in your animation, eg.:
    
    ```jsx
    <Animated.View
      entering={BounceInLeft.easing(Easing.bezier(0.25, 1, 0.25, 1))}
      ...
    />
    ```
    you will get the following warning in console:
    
    ```
    [Reanimated] Selected easing is not currently supported on web.
    ```
    
    This PR adds correct handling of this easing when creating animation.
    
    
    ### Solution
    
    Bezier easing is defined like so:
    
    
    ```tsx
    function bezier(
      x1: number,
      y1: number,
      x2: number,
      y2: number
    ): EasingFunctionFactory {
      'worklet';
      return {
        factory: () => {
          'worklet';
          return Bezier(x1, y1, x2, y2);
        },
      };
    }
    ```
    
    therefore to obtain points required for bezier curve, we need to access
    `factory` closure. We can do so, by checking `__closure` property:
    
    ```js
    easing.factory.__closure
    ```
    
    Since `factory` returns `Bezier` function, we can check if this exists
    in closure. If so, we are dealing with Bezier easing.
    
    
    ## Test plan
    
    Tested on Basic Layout Animation example with `Easing.bezier` added.

--- packages/react-native-reanimated/src/layoutReanimation/web/componentUtils.ts
+++ packages/react-native-reanimated/src/layoutReanimation/web/componentUtils.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import { LayoutAnimationType, ReduceMotion } from '../../commonTypes';
+import type { EasingFunctionFactory } from '../../Easing';
 import { EasingNameSymbol } from '../../Easing';
 import { logger } from '../../logger';
 import type { ReanimatedHTMLElement } from '../../ReanimatedModule/js-reanimated';
@@ -15,13 +16,18 @@ import type {
   AnimationConfig,
   AnimationNames,
   CustomConfig,
+  EasingType,
   KeyframeDefinitions,
 } from './config';
 import { Animations, TransitionType } from './config';
 import { TransitionGenerator } from './createAnimation';
 import { scheduleAnimationCleanup } from './domUtils';
 import type { WebEasingsNames } from './Easing.web';
-import { getEasingByName, WebEasings } from './Easing.web';
+import {
+  getEasingByName,
+  maybeGetBezierEasing,
+  WebEasings,
+} from './Easing.web';
 import { prepareCurvedTransition } from './transition/Curved.web';
 
 function getEasingFromConfig(config: CustomConfig): string {
@@ -29,15 +35,25 @@ function getEasingFromConfig(config: CustomConfig): string {
     return getEasingByName('linear');
   }
 
-  const easingName = config.easingV[EasingNameSymbol];
+  const easingName = (config.easingV as EasingType)[EasingNameSymbol];
 
-  if (!(easingName in WebEasings)) {
-    logger.warn(`Selected easing is not currently supported on web.`);
+  if (easingName in WebEasings) {
+    return getEasingByName(easingName as WebEasingsNames);
+  }
+
+  const bezierEasing = maybeGetBezierEasing(
+    config.easingV as EasingFunctionFactory
+  );
+
+  if (!bezierEasing) {
+    logger.warn(
+      `Selected easing is not currently supported on web. Using linear easing instead.`
+    );
 
     return getEasingByName('linear');
   }
 
-  return getEasingByName(easingName as WebEasingsNames);
+  return bezierEasing;
 }
 
 function getRandomDelay(maxDelay = 1000) {
