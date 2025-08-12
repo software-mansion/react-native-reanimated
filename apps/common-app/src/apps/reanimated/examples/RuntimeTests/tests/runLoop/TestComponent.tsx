69a6dd91d4dff03ae2bc9963cb9a6421b982e966
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-08-12 15:36:02 +0200

    Component rename

---
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/runLoop/TestComponent.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { View } from 'react-native';
+import { createWorkletRuntime, runOnRuntime, runOnUI } from 'react-native-worklets';
+
+export function DispatchTestComponent({ worklet, runtimeType }: { worklet: () => void; runtimeType: string }) {
+  useEffect(() => {
+    if (runtimeType === 'ui') {
+      runOnUI(() => {
+        'worklet';
+        worklet();
+      })();
+    } else {
+      const rt = createWorkletRuntime({ name: 'testRuntime' });
+      runOnRuntime(rt, () => {
+        'worklet';
+        worklet();
+      })();
+    }
+  });
+
+  return <View />;
+}
