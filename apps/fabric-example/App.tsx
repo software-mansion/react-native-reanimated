9a4b326bea6edc03fe44a0008a1d40ad93352c36
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-21 14:40:32 +0200

    Remove temp test example

--- apps/fabric-example/App.tsx
+++ apps/fabric-example/App.tsx
@@ -1,32 +1,3 @@
-import { Pressable, Text, View } from 'react-native';
-import Animated from 'react-native-reanimated';
+import App from 'common-app';
 
-function LaunchButton() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'black',
-      }}>
-      <Pressable>
-        {({ pressed }) => (
-          <Animated.View
-            style={{
-              transitionDuration: 100,
-              boxShadow: pressed
-                ? ` 0px -1px 1px rgba(255, 255, 255, 0.6)`
-                : ` 0px -3px 3px rgba(255, 255, 255, 0.85)`,
-            }}>
-            <Text style={{ color: 'white' }}>Hello</Text>
-          </Animated.View>
-        )}
-      </Pressable>
-    </View>
-  );
-}
-
-export default function Playground() {
-  return <LaunchButton />;
-}
+export default App;
