5ed03a4b9bdc1e870d54973db82d4af1f33a3e83
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-08-21 17:29:38 +0200

    Add notification timeout

--- apps/common-app/src/apps/reanimated/examples/RuntimeTests/ReJest/TestRunner/NotificationRegistry.ts
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/ReJest/TestRunner/NotificationRegistry.ts
@@ -1,4 +1,4 @@
-import { runOnJS } from 'react-native-worklets';
+import { RuntimeKind, scheduleOnRN } from 'react-native-worklets';
 
 let notificationRegistry: Record<string, boolean> = {};
 function notifyJS(name: string) {
@@ -8,32 +8,30 @@ function notifyJS(name: string) {
 export class NotificationRegistry {
   public notify(name: string) {
     'worklet';
-    if (globalThis._WORKLET) {
-      runOnJS(notifyJS)(name);
+    if (globalThis.__RUNTIME_KIND != RuntimeKind.ReactNative) {
+      scheduleOnRN(notifyJS, name);
     } else {
       notifyJS(name);
     }
   }
 
-  public async waitForNotify(name: string) {
-    const polingRate = 10;
-    return new Promise(resolve => {
-      const interval = setInterval(() => {
-        if (notificationRegistry[name]) {
-          clearInterval(interval);
-          resolve(true);
-        }
-      }, polingRate);
-    });
+  public async waitForNotify(name: string, timeout?: number) {
+    return this.waitForNotifications([name], timeout);
   }
 
-  public async waitForNotifications(names: string[]) {
+  public async waitForNotifications(names: string[], timeout?: number) {
+    const beginTime = performance.now();
     const polingRate = 10;
     return new Promise(resolve => {
       const interval = setInterval(() => {
         if (names.every(name => notificationRegistry[name])) {
           clearInterval(interval);
           resolve(true);
+        }
+        if (timeout != undefined && performance.now() - beginTime > timeout) {
+          names.forEach(name => console.log(`Notification '${name}' timeout exceeded.`));
+          clearInterval(interval);
+          resolve(false);
         }
       }, polingRate);
     });
