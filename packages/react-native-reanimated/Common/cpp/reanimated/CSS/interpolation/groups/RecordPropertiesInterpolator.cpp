afa0a7b7b57852ea0af59e20cf8f43252eba909e
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-21 22:02:45 +0200

    fix: CSS transition crashes on complex prop animation (#8106)
    The issue was reported by @EvanBacon who noticed that animating the
    `boxShadow` property in CSS transitions results in the application
    crash. It turned out that the method responsible for the keyframe
    updates in style interpolators was incorrectly implemented in complex
    (array, record) value interpolators.
    
    This PR fixes the incorrect implementation of the array and record
    interpolators' `updateKeyframesFromStyleChange` method.
    
    | Before | After |
    |-|-|
    | <video
    src="https://github.com/user-attachments/assets/9928ecc2-bc38-4ed6-ac7e-36cbb2cc26b2"
    /> | <video
    src="https://github.com/user-attachments/assets/97eefab5-1dc4-4b16-9377-ce286a814097"
    /> |
    
    <details>
    <summary>Source code</summary>
    
    ```tsx
    import { Pressable, Text, View } from 'react-native';
    import Animated from 'react-native-reanimated';
    
    function LaunchButton() {
      return (
        <View
          style={{
            flex: 1,
            justifyContent: 'center',
            alignItems: 'center',
            backgroundColor: 'black',
          }}>
          <Pressable>
            {({ pressed }) => (
              <Animated.View
                style={{
                  transitionDuration: 100,
                  boxShadow: pressed
                    ? ` 0px -1px 1px rgba(255, 255, 255, 0.6)`
                    : ` 0px -3px 3px rgba(255, 255, 255, 0.85)`,
                }}>
                <Text style={{ color: 'white' }}>Hello</Text>
              </Animated.View>
            )}
          </Pressable>
        </View>
      );
    }
    
    export default function Playground() {
      return <LaunchButton />;
    }
    ```
    </details>

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/groups/RecordPropertiesInterpolator.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/groups/RecordPropertiesInterpolator.cpp
@@ -37,14 +37,16 @@ void RecordPropertiesInterpolator::updateKeyframesFromStyleChange(
     const folly::dynamic &newStyleValue,
     const folly::dynamic &lastUpdateValue) {
   // TODO - maybe add a possibility to remove interpolators that are no longer
-  // used  (for now, for simplicity, we only add new ones)
-  const folly::dynamic empty = folly::dynamic::object();
-  const folly::dynamic &oldStyleObject =
-      !oldStyleValue.empty() ? oldStyleValue : empty;
-  const folly::dynamic &newStyleObject =
-      !newStyleValue.empty() ? newStyleValue : empty;
-  const folly::dynamic &lastUpdateObject =
-      !lastUpdateValue.empty() ? lastUpdateValue : empty;
+  // used (for now, for simplicity, we only add new ones)
+  const folly::dynamic emptyObject = folly::dynamic::object();
+  const auto null = folly::dynamic();
+
+  const auto &oldStyleObject =
+      oldStyleValue.empty() ? emptyObject : oldStyleValue;
+  const auto &newStyleObject =
+      newStyleValue.empty() ? emptyObject : newStyleValue;
+  const auto &lastUpdateObject =
+      lastUpdateValue.empty() ? emptyObject : lastUpdateValue;
 
   std::unordered_set<std::string> propertyNamesSet;
   for (const auto &key : oldStyleObject.keys()) {
@@ -57,9 +59,9 @@ void RecordPropertiesInterpolator::updateKeyframesFromStyleChange(
   for (const auto &propertyName : propertyNamesSet) {
     maybeCreateInterpolator(propertyName);
     interpolators_[propertyName]->updateKeyframesFromStyleChange(
-        oldStyleObject.getDefault(propertyName, empty),
-        newStyleObject.getDefault(propertyName, empty),
-        lastUpdateObject.getDefault(propertyName, empty));
+        oldStyleObject.getDefault(propertyName, null),
+        newStyleObject.getDefault(propertyName, null),
+        lastUpdateObject.getDefault(propertyName, null));
   }
 }
 
