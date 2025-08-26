03d0cf9e7a1e85ce8c7479ed15d2eb05242fe7f3
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-08-26 10:00:31 +0200

    Fix after merge

--- packages/react-native-reanimated/scripts/validate-worklets-version.js
+++ packages/react-native-reanimated/scripts/validate-worklets-version.js
@@ -25,9 +25,7 @@ function validateVersion() {
         expectedVersion.min +
         ' and ' +
         expectedVersion.max +
-        ' to use Reanimated ' +
-        reanimatedVersion +
-        '.',
+        ' to use Reanimated ' + reanimatedVersion + '.',
     };
   }
 
@@ -44,9 +42,7 @@ function validateVersion() {
   for (const key in compatibilityFile) {
     if (semverSatisfies(reanimatedVersion, key)) {
       // @ts-ignore
-      supportedWorkletsVersions.push(
-        ...compatibilityFile[key]['react-native-worklets']
-      );
+      supportedWorkletsVersions.push(...compatibilityFile[key]['react-native-worklets']);
     }
   }
 
