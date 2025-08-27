f97e852e87c2c1e8f0e73f25eb754cd315a71046
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-08-27 17:57:31 +0200

    Linter

--- packages/react-native-reanimated/src/layoutReanimation/animationBuilder/ComplexAnimationBuilder.ts
+++ packages/react-native-reanimated/src/layoutReanimation/animationBuilder/ComplexAnimationBuilder.ts
@@ -232,8 +232,8 @@ export class ComplexAnimationBuilder extends BaseAnimationBuilder {
   }
 
   /**
-   * Lets you adjust the energy threshold level to stop the spring
-   * animation. Can be chained alongside other [layout animation
+   * Lets you adjust the energy threshold level to stop the spring animation.
+   * Can be chained alongside other [layout animation
    * modifiers](https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/glossary#layout-animation-modifier).
    *
    * @param energyThreshold - Relative energy threshold below which the spring
