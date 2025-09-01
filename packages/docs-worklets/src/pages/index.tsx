50aa1a192ae4f2647224beaf78f4427ee5d0e629
skusnierz <sebastiano139@gmail.com>
2025-09-01 16:52:18 +0200

    adjust background position

--- packages/docs-worklets/src/pages/index.tsx
+++ packages/docs-worklets/src/pages/index.tsx
@@ -4,7 +4,6 @@ import HomepageMainSection from '@site/src/components/HomepageMainSection';
 import HomepageTryOutSection from '@site/src/components/HomepageTryOutSection';
 import HomepageUseCasesSection from '@site/src/components/HomepageUseCasesSection';
 import Layout from '@theme/Layout';
-import Background from '@site/static/img/background.svg';
 
 import HomepageFooter from '../components/HomepageFooter';
 import styles from './styles.module.css';
@@ -16,7 +15,6 @@ export default function Homepage() {
         className={styles.layout}
         title="React Native Worklets: Multithreading engine for your apps and libraries"
         description="Run concurrent processes to boost your app’s performance. Used by Reanimated, Gesture Handler, Skia and more.">
-        <Background className={styles.background} />
         <div className={styles.content}>
           <HomepageMainSection />
           <HomepageAboutSection />
