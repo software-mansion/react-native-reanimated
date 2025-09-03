22cb743e9224e93695be337070cf0d2e31721641
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-03 15:09:26 +0200

    Bump RN patch to 0.82

--- apps/web-example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-/** This file is required to properly resolve native dependencies */
-import { getDependencies } from '../common-app/scripts/dependencies';
-
-const dependencies = getDependencies(__dirname);
-
-module.exports = {
-  dependencies,
-  assets: ['./assets/fonts/'],
-};
