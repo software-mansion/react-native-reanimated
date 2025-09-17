ccdad722febb78ccb563227919fa5b4108b97f1a
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-17 18:30:30 +0200

    Revert vector changes

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/common/transforms/vectors.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/common/transforms/vectors.cpp
@@ -4,6 +4,20 @@ namespace reanimated::css {
 
 // Vector2D
 
+double &Vector2D::operator[](const size_t idx) {
+  return vec[idx];
+}
+
+const double &Vector2D::operator[](const size_t idx) const {
+  return vec[idx];
+}
+
+Vector2D &Vector2D::operator*=(const double scalar) {
+  vec[0] *= scalar;
+  vec[1] *= scalar;
+  return *this;
+}
+
 #ifndef NDEBUG
 
 std::ostream &operator<<(std::ostream &os, const Vector2D &vector) {
@@ -12,6 +26,10 @@ std::ostream &operator<<(std::ostream &os, const Vector2D &vector) {
 }
 
 #endif // NDEBUG
+
+double Vector2D::length() const {
+  return std::hypot(vec[0], vec[1]);
+}
 
 void Vector2D::scaleToLength(double targetLength) {
   double currentLength = length();
@@ -26,7 +44,41 @@ void Vector2D::normalize() {
   scaleToLength(1);
 }
 
+double Vector2D::dot(const Vector2D &other) const {
+  return vec[0] * other.vec[0] + vec[1] * other.vec[1];
+}
+
+double Vector2D::cross(const Vector2D &other) const {
+  return vec[0] * other.vec[1] - vec[1] * other.vec[0];
+}
+
+Vector2D Vector2D::addScaled(const Vector2D &other, const double scale) const {
+  return Vector2D{vec[0] + scale * other.vec[0], vec[1] + scale * other.vec[1]};
+}
+
+Vector2D Vector2D::interpolate(const double progress, const Vector2D &other)
+    const {
+  return Vector2D{
+      vec[0] + progress * (other.vec[0] - vec[0]),
+      vec[1] + progress * (other.vec[1] - vec[1])};
+}
+
 // Vector3D
+
+double &Vector3D::operator[](const size_t idx) {
+  return vec[idx];
+}
+
+const double &Vector3D::operator[](const size_t idx) const {
+  return vec[idx];
+}
+
+Vector3D &Vector3D::operator*=(const double scalar) {
+  vec[0] *= scalar;
+  vec[1] *= scalar;
+  vec[2] *= scalar;
+  return *this;
+}
 
 #ifndef NDEBUG
 
@@ -37,6 +89,10 @@ std::ostream &operator<<(std::ostream &os, const Vector3D &vector) {
 }
 
 #endif // NDEBUG
+
+double Vector3D::length() const {
+  return std::hypot(vec[0], vec[1], vec[2]);
+}
 
 void Vector3D::scaleToLength(double targetLength) {
   double currentLength = length();
@@ -52,7 +108,50 @@ void Vector3D::normalize() {
   scaleToLength(1);
 }
 
+double Vector3D::dot(const Vector3D &other) const {
+  return vec[0] * other.vec[0] + vec[1] * other.vec[1] + vec[2] * other.vec[2];
+}
+
+Vector3D Vector3D::cross(const Vector3D &other) const {
+  return Vector3D{
+      vec[1] * other.vec[2] - vec[2] * other.vec[1],
+      vec[2] * other.vec[0] - vec[0] * other.vec[2],
+      vec[0] * other.vec[1] - vec[1] * other.vec[0]};
+}
+
+Vector3D Vector3D::addScaled(const Vector3D &other, const double scale) const {
+  return Vector3D{
+      vec[0] + scale * other.vec[0],
+      vec[1] + scale * other.vec[1],
+      vec[2] + scale * other.vec[2]};
+}
+
+Vector3D Vector3D::interpolate(const double progress, const Vector3D &other)
+    const {
+  return Vector3D{
+      vec[0] + progress * (other.vec[0] - vec[0]),
+      vec[1] + progress * (other.vec[1] - vec[1]),
+      vec[2] + progress * (other.vec[2] - vec[2])};
+}
+
 // Vector4D
+
+double &Vector4D::operator[](const size_t idx) {
+  return vec[idx];
+}
+
+const double &Vector4D::operator[](const size_t idx) const {
+  return vec[idx];
+}
+
+Vector4D Vector4D::interpolate(const double progress, const Vector4D &other)
+    const {
+  return Vector4D{
+      vec[0] + progress * (other.vec[0] - vec[0]),
+      vec[1] + progress * (other.vec[1] - vec[1]),
+      vec[2] + progress * (other.vec[2] - vec[2]),
+      vec[3] + progress * (other.vec[3] - vec[3])};
+}
 
 #ifndef NDEBUG
 
