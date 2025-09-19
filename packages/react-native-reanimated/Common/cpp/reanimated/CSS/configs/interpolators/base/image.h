5c53af875736024a56b97b2dfcb053d7acc0f0ac
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-19 18:16:30 +0200

    Merge changes from @matipl01/some-css-improvements

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/configs/interpolators/base/image.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <reanimated/CSS/interpolation/PropertyInterpolator.h>
-
-namespace reanimated::css {
-
-const InterpolatorFactoriesRecord &getImageInterpolators();
-
-} // namespace reanimated::css
