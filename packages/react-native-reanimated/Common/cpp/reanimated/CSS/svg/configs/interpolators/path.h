76478fc332d4a98e9f423183f02a2df0251c1ec7
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-19 16:34:48 +0200

    cherry-pick: Move interpolator configs to a single implementation file

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/svg/configs/interpolators/path.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <reanimated/CSS/interpolation/PropertyInterpolator.h>
-
-namespace reanimated::css {
-
-const InterpolatorFactoriesRecord &getSvgPathInterpolators();
-
-} // namespace reanimated::css
--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/svg/configs/interpolators/rect.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <reanimated/CSS/interpolation/PropertyInterpolator.h>
-
-namespace reanimated::css {
-
-const InterpolatorFactoriesRecord &getSvgRectInterpolators();
-
-} // namespace reanimated::css
