ef77854aa8ea177097e2622391e5e863c884a374
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2025-09-26 14:55:58 +0200

    Mark examples

--- apps/common-app/src/apps/reanimated/examples/index.ts
+++ apps/common-app/src/apps/reanimated/examples/index.ts
@@ -864,7 +864,7 @@ export const EXAMPLES: Record<string, Example> = {
     title: '[SET] Many screens',
     screen: ManyScreensExample,
     shouldWork: {
-      ios: true,
+      ios: false, // header height issues on iOS 26
       android: true,
     },
   },
