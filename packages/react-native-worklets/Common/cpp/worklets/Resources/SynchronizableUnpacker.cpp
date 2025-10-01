8168799e95e242e7931d849d369ceca67e1aacb5
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-10-01 08:12:38 +0200

    Tried to synchrounously call had old link to Reanimated

--- packages/react-native-worklets/Common/cpp/worklets/Resources/SynchronizableUnpacker.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/Resources/SynchronizableUnpacker.cpp
@@ -9,7 +9,7 @@ namespace worklets {
 const char SynchronizableUnpackerCode[] =
     R"DELIMITER__((function () {
   var serializer = !globalThis._WORKLET || globalThis._WORKLETS_BUNDLE_MODE ? function (value, _) {
-    return (0, _serializable.createSerializable)(value);
+    return createSerializable(value);
   } : globalThis._createSerializable;
   function synchronizableUnpacker(synchronizableRef) {
     var synchronizable = synchronizableRef;
