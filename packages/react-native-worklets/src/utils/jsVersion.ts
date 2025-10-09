2ecd9005fa164123a3ccf5239f7ea3a49a4d1339
Tomasz Żelawski <tzelawski@gmail.com>
2025-10-09 15:40:27 +0200

    release(Worklets): 0.6.1

--- packages/react-native-worklets/src/utils/jsVersion.ts
+++ packages/react-native-worklets/src/utils/jsVersion.ts
@@ -4,4 +4,4 @@
  * version used to build the native part of the library in runtime. Remember to
  * keep this in sync with the version declared in `package.json`
  */
-export const jsVersion = '0.6.0';
+export const jsVersion = '0.6.1';
