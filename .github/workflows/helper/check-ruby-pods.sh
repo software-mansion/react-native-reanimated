4073a016a5f138671afb67c97af580e4f05e4f9e
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-10-10 14:28:03 +0200

    its a good thing to hardcode things

--- .github/workflows/helper/check-ruby-pods.sh
+++ .github/workflows/helper/check-ruby-pods.sh
@@ -2,13 +2,15 @@
 set -e
 
 WORKING_DIR="$1"
-BASE_BRANCH="$2"
 
 echo "Checking Ruby and cocoapods versions in $WORKING_DIR"
 cd "$WORKING_DIR"
 
 GEM_LOCK="Gemfile.lock"
 POD_LOCK="Podfile.lock"
+
+EXPECTED_RUBY="3.3.6p108"
+EXPECTED_PODS="1.15.2"
 
 if [[ "$WORKING_DIR" == *macos-example ]]; then
   POD_LOCK="macos/Podfile.lock"
@@ -18,38 +20,25 @@ fi
 
 HAS_DIFF=false
 
-git fetch origin "$BASE_BRANCH"
-
 if [ -f "$GEM_LOCK" ]; then
-  if git diff --name-only origin/"$BASE_BRANCH" -- "$GEM_LOCK" | grep -q "$GEM_LOCK"; then
-    CURRENT_RUBY=$(grep -A1 "RUBY VERSION" "$GEM_LOCK" | tail -n1 | awk '{print $2}')
-    PREV_RUBY=$(git show origin/"$BASE_BRANCH":"$GEM_LOCK" | grep -A1 "RUBY VERSION" | tail -n1 | awk '{print $2}')
-
-    if [ "$CURRENT_RUBY" != "$PREV_RUBY" ]; then
-      echo "$WORKING_DIR | Ruby version changed: $PREV_RUBY → $CURRENT_RUBY"
-      HAS_DIFF=true
-    else
-      echo "$WORKING_DIR | Ruby version unchanged: ($CURRENT_RUBY)"
-    fi
+  CURRENT_RUBY=$(grep -A1 "RUBY VERSION" "$GEM_LOCK" | tail -n1 | awk '{print $2}')
+  if [ "$CURRENT_RUBY" != "$EXPECTED_RUBY" ]; then
+    echo "$WORKING_DIR | Ruby version changed: $PREV_RUBY → $CURRENT_RUBY"
+    HAS_DIFF=true
   else
-    echo "$GEM_LOCK did not change - skipping ruby version check"
+    echo "$WORKING_DIR | Ruby version is ok: ($CURRENT_RUBY)"
   fi
 else
   echo "No Gemfile.lock found in $WORKING_DIR"
 fi
 
 if [ -f "$POD_LOCK" ]; then
-  if git diff --name-only origin/"$BASE_BRANCH" -- "$POD_LOCK" | grep -q "$POD_LOCK"; then
-    CURRENT_PODS=$(grep "COCOAPODS:" "$POD_LOCK" | awk '{print $2}')
-    PREV_PODS=$(git show origin/"$BASE_BRANCH":"$POD_LOCK" | grep "COCOAPODS:" | awk '{print $2}')
-
-    if [ "$CURRENT_PODS" != "$PREV_PODS" ]; then
-      echo "$WORKING_DIR | cocoapods version changed: $PREV_PODS → $CURRENT_PODS"
-      HAS_DIFF=true
-    else
-      echo "$WORKING_DIR | cocoapods version unchanged: ($CURRENT_PODS)"
+  CURRENT_PODS=$(grep "COCOAPODS:" "$POD_LOCK" | awk '{print $2}')
+  if [ "$CURRENT_PODS" != "$EXPECTED_PODS" ]; then
+    echo "$WORKING_DIR | cocoapods version changed: $PREV_PODS → $CURRENT_PODS"
+    HAS_DIFF=true
   else
-    echo "$POD_LOCK did not change - skipping cocoapods version check"
+    echo "$WORKING_DIR | cocoapods version is ok: ($CURRENT_PODS)"
   fi
 else
   echo "No Podfile.lock found at $POD_LOCK"
@@ -58,6 +47,9 @@ fi
 if [ "$HAS_DIFF" = true ]; then
   echo ""
   echo "Ruby or cocoapods version change detected in $WORKING_DIR."
+  echo "Please make sure that you use following versions:"
+  echo "- ruby: $EXPECTED_RUBY"
+  echo "- cocoapods: $EXPECTED_PODS"
   exit 1
 fi
 
