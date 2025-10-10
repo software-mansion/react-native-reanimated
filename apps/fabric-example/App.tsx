26f92cf590649827ceb8eae5b8ae7059256b9777
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-10-10 12:55:56 +0200

    Update example

--- apps/fabric-example/App.tsx
+++ apps/fabric-example/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -7,53 +7,56 @@ import Animated, {
   withTiming,
 } from 'react-native-reanimated';
 
-export default function HomeScreen() {
+export default function ScrollPerformanceExample() {
   return (
-    <ScrollView contentContainerStyle={{ gap: 10 }}>
+    <ScrollView contentContainerStyle={styles.container}>
       {Array.from({ length: 500 }).map((_, index) => (
-        <SomeAnimatedRow key={index} />
+        <Item key={index} />
       ))}
     </ScrollView>
   );
 }
 
-const SomeAnimatedRow = () => {
-  const opacity = useSharedValue(0);
+function Item() {
+  const sv = useSharedValue(0);
 
   const animatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: opacity.value,
-    };
+    return { opacity: sv.value };
   });
 
   useEffect(() => {
-    opacity.value = withRepeat(
-      withTiming(1, {
-        duration: Math.random() * 1000,
-      }),
+    sv.value = withRepeat(
+      withTiming(1, { duration: Math.random() * 1000 }),
       -1,
       true
     );
-  }, [opacity]);
+  }, [sv]);
 
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.item}>
       <Animated.View
         style={[
           animatedStyle,
-          {
-            width: 100,
-            height: 100,
-            backgroundColor: Math.random() > 0.5 ? 'red' : 'blue',
-          },
+          styles.box,
+          { backgroundColor: Math.random() > 0.5 ? 'red' : 'blue' },
         ]}
       />
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+  },
+  item: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+});
