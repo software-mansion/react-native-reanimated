2c4614d24988b181c632998d104ef5bbe7723771
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-10-10 09:17:24 +0200

    review changes

--- apps/macos-example/macos/MacOSExample-macOS/AppDelegate.mm
+++ apps/macos-example/macos/MacOSExample-macOS/AppDelegate.mm
@@ -10,7 +10,6 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
   return [super applicationDidFinishLaunching:notification];
 }
 
