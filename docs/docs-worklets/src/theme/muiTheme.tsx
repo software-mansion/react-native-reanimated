182fd01fb7e93e718a97c752ace921df851dba6b
Tomasz Żelawski <tzelawski@gmail.com>
2025-10-16 12:19:53 +0200

    chore: docs, prettier fixes

--- docs/docs-worklets/src/theme/muiTheme.tsx
+++ docs/docs-worklets/src/theme/muiTheme.tsx
@@ -1,4 +1,4 @@
-import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import { extendTheme } from '@mui/material/styles';
 
 import Aeonik from '/static/font/Aeonik-Regular.otf';
 
