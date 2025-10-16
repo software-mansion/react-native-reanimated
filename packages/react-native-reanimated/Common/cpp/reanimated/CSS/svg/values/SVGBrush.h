fd11a4af70c410d6192f57ebb68e0e3edb42aea6
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-10-16 23:36:42 +0200

    Move SVG color to a separate SVGBrush struct

---
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/svg/values/SVGBrush.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <reanimated/CSS/common/values/CSSColor.h>
+
+#include <string>
+
+namespace reanimated::css {
+
+enum class SVGBrushType {
+  Rgba,
+  Transparent,
+  CurrentColor,
+};
+
+struct SVGBrush : public CSSColorBase<SVGBrushType, SVGBrush> {
+  using CSSColorBase<SVGBrushType, SVGBrush>::CSSColorBase;
+
+  explicit SVGBrush(jsi::Runtime &rt, const jsi::Value &jsiValue);
+  explicit SVGBrush(const folly::dynamic &value);
+
+  static bool canConstruct(jsi::Runtime &rt, const jsi::Value &jsiValue);
+  static bool canConstruct(const folly::dynamic &value);
+
+  folly::dynamic toDynamic() const override;
+  std::string toString() const override;
+
+  SVGBrush interpolate(double progress, const SVGBrush &to) const override;
+  bool isInterpolatable() const;
+
+#ifndef NDEBUG
+  friend std::ostream &operator<<(std::ostream &os, const SVGBrush &colorValue);
+#endif // NDEBUG
+
+ private:
+  static bool isValidColorString(const std::string &value);
+};
+
+} // namespace reanimated::css
