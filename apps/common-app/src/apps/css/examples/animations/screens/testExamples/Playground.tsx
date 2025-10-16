639c6611dd3317a222e288a9d2fa6bbe747a00c4
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-10-16 12:12:39 +0200

    change name

--- apps/common-app/src/apps/css/examples/animations/screens/testExamples/Playground.tsx
+++ apps/common-app/src/apps/css/examples/animations/screens/testExamples/Playground.tsx
@@ -5,12 +5,12 @@
 
 import React from 'react';
 import { Text, View } from 'react-native';
-import { createAnimatedComponent, css } from 'react-native-reanimated';
+import { createCSSAnimatedComponent, css } from 'react-native-reanimated';
 
 import { Screen } from '@/apps/css/components';
 import { flex } from '@/theme';
 
-const AnimatedView = createAnimatedComponent(View);
+const AnimatedView = createCSSAnimatedComponent(View);
 
 export default function Playground() {
   return (
@@ -37,42 +37,18 @@ const styles = css.create({
     animationIterationCount: 'infinite',
     animationName: [
       css.keyframes({
-        0.1: {
-          width: '75%',
-        },
-        0.2: {
-          width: 20,
-        },
-        0.3: {
-          width: '50%',
-        },
-        0.4: {
-          width: 20,
-        },
-        0.5: {
-          width: '75%',
-        },
-        0.6: {
-          width: 0,
-        },
-        0.7: {
-          width: '100%',
-        },
-        0.8: {
-          width: '25%',
-        },
-        0.9: {
-          width: '75%',
-        },
-        from: {
-          width: 20,
-        },
+        0.1: { width: '75%' },
+        0.2: { width: 20 },
+        0.3: { width: '50%' },
+        0.4: { width: 20 },
+        0.5: { width: '75%' },
+        0.6: { width: 0 },
+        0.7: { width: '100%' },
+        0.8: { width: '25%' },
+        0.9: { width: '75%' },
+        from: { width: 20 },
       }),
-      {
-        to: {
-          backgroundColor: 'red',
-        },
-      },
+      { to: { backgroundColor: 'red' } },
     ],
     animationTimingFunction: 'linear',
     backgroundColor: 'gold',
@@ -85,26 +61,14 @@ const styles = css.create({
     animationIterationCount: 'infinite',
     animationName: [
       css.keyframes({
-        0: {
-          width: 200,
-        },
-        0.5: {
-          width: 350,
-        },
-        1: {
-          width: 200,
-        },
+        0: { width: 200 },
+        0.5: { width: 350 },
+        1: { width: 200 },
       }),
+      css.keyframes({ '50%': { height: 200 } }),
       css.keyframes({
-        '50%': { height: 200 },
-      }),
-      css.keyframes({
-        from: {
-          transform: [{ rotate: '0deg' }],
-        },
-        to: {
-          transform: [{ rotate: '360deg' }],
-        },
+        from: { transform: [{ rotate: '0deg' }] },
+        to: { transform: [{ rotate: '360deg' }] },
       }),
     ],
     animationTimingFunction: 'linear',
