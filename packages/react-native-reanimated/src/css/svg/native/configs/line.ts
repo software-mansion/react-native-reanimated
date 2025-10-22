f89ffd2e6f0767d5c748d870b59f408ca46c74f8
Tomasz Żelawski <tzelawski@gmail.com>
2025-10-22 10:41:56 +0200

    chore: cleanup

--- packages/react-native-reanimated/src/css/svg/native/configs/line.ts
+++ packages/react-native-reanimated/src/css/svg/native/configs/line.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+// TODO: Fix me
 // @ts-ignore RNSVG doesn't export types for web, see https://github.com/software-mansion/react-native-svg/pull/2801
 import type { LineProps } from 'react-native-svg';
 
