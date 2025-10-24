b84ce20526ae41ec09cb40d8275e4edc76147da3
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-10-24 11:13:52 +0200

    chore(Worklets): remove runtime-string association from RuntimeManager (#8443)
    ## Summary
    
    I'm removing the association of runtime name - worklet runtime from
    runtime manager. It was added as a possibility for future extension but
    since the names of the runtimes don't have to be unique and there are
    already unique IDs for the runtimes, the association is obsolete.
    
    It's safe to delete it since it's not used and it's a private API.
    
    ## Test plan
    
    CI

--- packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/RuntimeData.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/RuntimeData.cpp
@@ -1,3 +1,9 @@
 #include <worklets/WorkletRuntime/RuntimeData.h>
 
+namespace worklets {
+namespace RuntimeData {
+
 const std::string uiRuntimeName{"UI"};
+
+} // namespace RuntimeData
+} // namespace worklets
