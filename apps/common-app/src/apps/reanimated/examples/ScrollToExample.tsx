software-mansion/react-native-reanimated

commit e517d5b18fd40ac6ef30902beb32930e8f8cdac4

cherry-pick(Reanimated): useAnimatedRef breaking on SectionList (#8301) (#8478)
## Summary

Cherry-pick of the #8301

apps/common-app/src/apps/reanimated/examples/ScrollToExample.tsx
@@ -5,10 +5,22 @@ import {
 } from '@shopify/flash-list';
 import type { Ref } from 'react';
 import React, { useCallback, useImperativeHandle, useRef } from 'react';
-import type { ListRenderItem as FlatListRenderItem } from 'react-native';
-import { Button, StyleSheet, Switch, Text, View } from 'react-native';
+import type {
+  ListRenderItem as FlatListRenderItem,
+  SectionListRenderItem,
+} from 'react-native';
+import {
+  Button,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from 'react-native';
 import Animated, { scrollTo, useAnimatedRef } from 'react-native-reanimated';
-import { runOnUI } from 'react-native-worklets';
+import { ScrollView as RNGHScrollView } from 'react-native-gesture-handler';
+import { scheduleOnUI, getRuntimeKind } from 'react-native-worklets';
 
 const DATA = [...Array(100).keys()];
 
@@ -40,8 +52,16 @@ export default function ScrollToExample() {
       component: FlatListExample,
     },
     {
+      title: 'SectionList',
+      component: SectionListExample,
+    },
+    {
       title: 'FlashList',
       component: FlashListExample,
+    },
+    {
+      title: 'RNGHScrollView',
+      component: RNGHScrollViewExample,
     },
   ];
 
@@ -49,7 +69,10 @@ export default function ScrollToExample() {
 
   return (
     <>
-      <View style={styles.optionsRow}>
+      <ScrollView
+        contentContainerStyle={styles.optionsRow}
+        horizontal
+        style={styles.optionsContainer}>
         {examples.map(({ title }, index) => (
           <Button
             disabled={index === currentExample}
@@ -58,7 +81,7 @@ export default function ScrollToExample() {
             onPress={() => setCurrentExample(index)}
           />
         ))}
-      </View>
+      </ScrollView>
       <View style={styles.buttons}>
         <View style={styles.optionsRow}>
           <Text style={styles.switchLabel}>Animated</Text>
@@ -88,14 +111,15 @@ const ScrollViewExample = ({ animated, ref }: ExampleProps) => {
 
   useImperativeHandle(ref, () => ({
     scrollFromJS() {
-      console.log(_WORKLET);
+      console.log(getRuntimeKind());
       aref.current?.scrollTo({ y: getRandomOffset(), animated });
     },
     scrollFromUI() {
-      runOnUI(() => {
-        console.log(_WORKLET);
+      scheduleOnUI(() => {
+        'worklet';
+        console.log(getRuntimeKind());
         scrollTo(aref, 0, getRandomOffset(), animated);
-      })();
+      });
     },
   }));
 
@@ -115,14 +139,15 @@ const FlatListExample = ({ animated, ref }: ExampleProps) => {
 
   useImperativeHandle(ref, () => ({
     scrollFromJS() {
-      console.log(_WORKLET);
+      console.log(getRuntimeKind());
       aref.current?.scrollToOffset({ offset: getRandomOffset(), animated });
     },
     scrollFromUI() {
-      runOnUI(() => {
-        console.log(_WORKLET);
+      scheduleOnUI(() => {
+        'worklet';
+        console.log(getRuntimeKind());
         scrollTo(aref, 0, getRandomOffset(), animated);
-      })();
+      });
     },
   }));
 
@@ -146,14 +171,15 @@ const FlashListExample = ({ animated, ref }: ExampleProps) => {
 
   useImperativeHandle(ref, () => ({
     scrollFromJS() {
-      console.log(_WORKLET);
+      console.log(getRuntimeKind());
       aref.current?.scrollToOffset({ offset: getRandomOffset(), animated });
     },
     scrollFromUI() {
-      runOnUI(() => {
-        console.log(_WORKLET);
+      scheduleOnUI(() => {
+        'worklet';
+        console.log(getRuntimeKind());
         scrollTo(aref, 0, getRandomOffset(), animated);
-      })();
+      });
     },
   }));
 
@@ -165,13 +191,99 @@ const FlashListExample = ({ animated, ref }: ExampleProps) => {
   return <AnimatedFlashList ref={aref} renderItem={renderItem} data={DATA} />;
 };
 
+const AnimatedSectionList = Animated.createAnimatedComponent(
+  SectionList<number>
+);
+
+const SectionListExample = ({ animated, ref }: ExampleProps) => {
+  const aref = useAnimatedRef<typeof AnimatedSectionList>();
+
+  useImperativeHandle(ref, () => ({
+    scrollFromJS() {
+      console.log(getRuntimeKind());
+      aref.current?.scrollToLocation({
+        sectionIndex: Math.floor((Math.random() * DATA.length) / 10),
+        itemIndex: Math.floor((Math.random() * DATA.length) / 10),
+      });
+    },
+    scrollFromUI() {
+      scheduleOnUI(() => {
+        'worklet';
+        console.log(getRuntimeKind());
+        scrollTo(aref, 0, getRandomOffset(), animated);
+      });
+    },
+  }));
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderItem = useCallback<SectionListRenderItem<any>>(
+    ({ item }) => <Text style={styles.text}>{item}</Text>,
+    []
+  );
+
+  const sections = React.useMemo(
+    () =>
+      Array.from({ length: 10 }, (_, i) => ({
+        title: `Section ${i + 1}`,
+        data: DATA.slice(i * 10, (i + 1) * 10),
+      })),
+    []
+  );
+
+  return (
+    <AnimatedSectionList
+      ref={aref}
+      renderItem={renderItem}
+      style={styles.fill}
+      sections={sections}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+      stickySectionHeadersEnabled={false}
+    />
+  );
+};
+
+const AnimatedRNGHScrollView = Animated.createAnimatedComponent(RNGHScrollView);
+
+const RNGHScrollViewExample = ({ animated, ref }: ExampleProps) => {
+  const aref = useAnimatedRef<typeof AnimatedRNGHScrollView>();
+
+  useImperativeHandle(ref, () => ({
+    scrollFromJS() {
+      console.log(getRuntimeKind());
+      // @ts-ignore This is broken with react-native-strict-api types.
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      aref.current?.scrollTo({ y: getRandomOffset(), animated });
+    },
+    scrollFromUI() {
+      scheduleOnUI(() => {
+        'worklet';
+        console.log(getRuntimeKind());
+        scrollTo(aref, 0, getRandomOffset(), animated);
+      });
+    },
+  }));
+
+  return (
+    <AnimatedRNGHScrollView ref={aref} style={styles.fill}>
+      {DATA.map((_, i) => (
+        <Text key={i} style={styles.text}>
+          {i}
+        </Text>
+      ))}
+    </AnimatedRNGHScrollView>
+  );
+};
+
 const styles = StyleSheet.create({
+  optionsContainer: {
+    flexGrow: 0,
+  },
   optionsRow: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10,
     marginVertical: 10,
+    fontSize: 10,
+    gap: 3,
   },
   switchLabel: {
     fontSize: 20,
@@ -187,6 +299,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 50,
+    textAlign: 'center',
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: 'bold',
     textAlign: 'center',
   },
 });

apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/core/useAnimatedRef.test.tsx
@@ -1,0 +1,306 @@
+import React from 'react';
+import { FlashList } from '@shopify/flash-list';
+import {
+  ActivityIndicator,
+  Button,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Modal,
+  Pressable,
+  RefreshControl,
+  Text,
+  Switch,
+  SectionList,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  VirtualizedList,
+} from 'react-native';
+import { ScrollView as RNGHScrollView } from 'react-native-gesture-handler';
+import { Path } from 'react-native-svg';
+import Animated, { useAnimatedRef } from 'react-native-reanimated';
+
+import { describe, render, test } from '../../ReJest/RuntimeTestsApi';
+
+const AnimatedFlashList = Animated.createAnimatedComponent(FlashList);
+const AnimatedActivityIndicator = Animated.createAnimatedComponent(ActivityIndicator);
+const AnimatedButton = Animated.createAnimatedComponent(Button);
+const AnimatedImageBackground = Animated.createAnimatedComponent(ImageBackground);
+const AnimatedKeyboardAvoidingView = Animated.createAnimatedComponent(KeyboardAvoidingView);
+const AnimatedModal = Animated.createAnimatedComponent(Modal);
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+const AnimatedRefreshControl = Animated.createAnimatedComponent(RefreshControl);
+const AnimatedSectionList = Animated.createAnimatedComponent(SectionList);
+const AnimatedSwitch = Animated.createAnimatedComponent(Switch);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+const AnimatedTouchableHighlight = Animated.createAnimatedComponent(TouchableHighlight);
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+const AnimatedTouchableWithoutFeedback = Animated.createAnimatedComponent(TouchableWithoutFeedback);
+// @ts-ignore This is broken with react-native-strict-api types.
+const AnimatedVirtualizedList = Animated.createAnimatedComponent(VirtualizedList);
+const AnimatedRNGHScrollView = Animated.createAnimatedComponent(RNGHScrollView);
+const AnimatedPath = Animated.createAnimatedComponent(Path);
+
+describe('Test *****useAnimatedRef*****', () => {
+  describe('Animated.View', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<Animated.View>();
+
+      return <Animated.View ref={animatedRef} />;
+    };
+
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Text', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<Animated.Text>();
+
+      return <Animated.Text ref={animatedRef}>Hello</Animated.Text>;
+    };
+
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Image', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<Animated.Image>();
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      return <Animated.Image ref={animatedRef} source={{ uri: require('../../../assets/doge.png') }} />;
+    };
+
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.ScrollView', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<Animated.ScrollView>();
+      return <Animated.ScrollView ref={animatedRef} />;
+    };
+
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.FlatList', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<Animated.FlatList<number>>();
+      return <Animated.FlatList ref={animatedRef} data={[]} renderItem={() => null} />;
+    };
+
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.FlashList', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedFlashList>();
+      return <AnimatedFlashList ref={animatedRef} data={[]} renderItem={() => null} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.ActivityIndicator', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedActivityIndicator>();
+      return <AnimatedActivityIndicator ref={animatedRef} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Button', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedButton>();
+      return <AnimatedButton ref={animatedRef} title="Press me" onPress={() => {}} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.ImageBackground', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedImageBackground>();
+      return (
+        <AnimatedImageBackground
+          ref={animatedRef}
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          source={{ uri: require('../../../assets/doge.png') }}
+          style={{ width: 100, height: 100 }}
+        />
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.KeyboardAvoidingView', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedKeyboardAvoidingView>();
+      return <AnimatedKeyboardAvoidingView ref={animatedRef} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Modal', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedModal>();
+      return <AnimatedModal ref={animatedRef} visible />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Pressable', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedPressable>();
+      return (
+        <AnimatedPressable ref={animatedRef} onPress={() => {}}>
+          <Text>Press me</Text>
+        </AnimatedPressable>
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.RefreshControl', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedRefreshControl>();
+      return <AnimatedRefreshControl ref={animatedRef} refreshing={false} onRefresh={() => {}} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.SectionList', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedSectionList>();
+      return <AnimatedSectionList ref={animatedRef} sections={[]} renderItem={() => null} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Switch', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedSwitch>();
+      return <AnimatedSwitch ref={animatedRef} value={false} onValueChange={() => {}} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.TextInput', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedTextInput>();
+      return <AnimatedTextInput ref={animatedRef} value="Hello" onChangeText={() => {}} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.TouchableHighlight', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedTouchableHighlight>();
+      return (
+        <AnimatedTouchableHighlight ref={animatedRef} onPress={() => {}}>
+          <Text>Highlight me</Text>
+        </AnimatedTouchableHighlight>
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.TouchableOpacity', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedTouchableOpacity>();
+      return (
+        <AnimatedTouchableOpacity ref={animatedRef} onPress={() => {}}>
+          <Text>Opacitize me</Text>
+        </AnimatedTouchableOpacity>
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.TouchableWithoutFeedback', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedTouchableWithoutFeedback>();
+      return (
+        <AnimatedTouchableWithoutFeedback ref={animatedRef} onPress={() => {}}>
+          <Text>Dont feedback me</Text>
+        </AnimatedTouchableWithoutFeedback>
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.VirtualizedList', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedVirtualizedList>();
+      return (
+        <AnimatedVirtualizedList
+          ref={animatedRef}
+          data={[]}
+          renderItem={() => null}
+          // @ts-ignore This is broken with react-native-strict-api types.
+          getItemCount={() => 0}
+          getItem={() => undefined}
+        />
+      );
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.ScrollView from RNGH', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedRNGHScrollView>();
+      return <AnimatedRNGHScrollView ref={animatedRef} />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+
+  describe('Animated.Path from react-native-svg', () => {
+    const Component = () => {
+      const animatedRef = useAnimatedRef<typeof AnimatedPath>();
+      return <AnimatedPath ref={animatedRef} d="M10 10 H 90 V 90 H 10 L 10 10" stroke="black" fill="transparent" />;
+    };
+    test('mounts without crashing', async () => {
+      await render(<Component />);
+    });
+  });
+});

commit 90a6d111c51a2582748b5574c50ae2a258eefdf9

fix: Type issues after merging cherry-picks (#8489)
## Summary

These type issues were introduced in the #8478. I merged it without
checking why CIs fail.

apps/common-app/src/apps/reanimated/examples/ScrollToExample.tsx
@@ -196,7 +196,7 @@ const AnimatedSectionList = Animated.createAnimatedComponent(
 );
 
 const SectionListExample = ({ animated, ref }: ExampleProps) => {
-  const aref = useAnimatedRef<typeof AnimatedSectionList>();
+  const aref = useAnimatedRef<SectionList<number>>();
 
   useImperativeHandle(ref, () => ({
     scrollFromJS() {
@@ -247,13 +247,11 @@ const SectionListExample = ({ animated, ref }: ExampleProps) => {
 const AnimatedRNGHScrollView = Animated.createAnimatedComponent(RNGHScrollView);
 
 const RNGHScrollViewExample = ({ animated, ref }: ExampleProps) => {
-  const aref = useAnimatedRef<typeof AnimatedRNGHScrollView>();
+  const aref = useAnimatedRef<RNGHScrollView>();
 
   useImperativeHandle(ref, () => ({
     scrollFromJS() {
       console.log(getRuntimeKind());
-      // @ts-ignore This is broken with react-native-strict-api types.
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       aref.current?.scrollTo({ y: getRandomOffset(), animated });
     },
     scrollFromUI() {

apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/core/useAnimatedRef.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlashList } from '@shopify/flash-list';
+import { FlashList, FlashListRef } from '@shopify/flash-list';
 import {
   ActivityIndicator,
   Button,
@@ -16,6 +16,7 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   VirtualizedList,
+  View,
 } from 'react-native';
 import { ScrollView as RNGHScrollView } from 'react-native-gesture-handler';
 import { Path } from 'react-native-svg';
@@ -104,7 +105,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.FlashList', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedFlashList>();
+      const animatedRef = useAnimatedRef<FlashListRef<unknown>>();
       return <AnimatedFlashList ref={animatedRef} data={[]} renderItem={() => null} />;
     };
     test('mounts without crashing', async () => {
@@ -114,7 +115,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.ActivityIndicator', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedActivityIndicator>();
+      const animatedRef = useAnimatedRef<ActivityIndicator>();
       return <AnimatedActivityIndicator ref={animatedRef} />;
     };
     test('mounts without crashing', async () => {
@@ -124,7 +125,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.Button', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedButton>();
+      const animatedRef = useAnimatedRef<Button>();
       return <AnimatedButton ref={animatedRef} title="Press me" onPress={() => {}} />;
     };
     test('mounts without crashing', async () => {
@@ -134,7 +135,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.ImageBackground', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedImageBackground>();
+      const animatedRef = useAnimatedRef<ImageBackground>();
       return (
         <AnimatedImageBackground
           ref={animatedRef}
@@ -151,7 +152,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.KeyboardAvoidingView', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedKeyboardAvoidingView>();
+      const animatedRef = useAnimatedRef<KeyboardAvoidingView>();
       return <AnimatedKeyboardAvoidingView ref={animatedRef} />;
     };
     test('mounts without crashing', async () => {
@@ -161,7 +162,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.Modal', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedModal>();
+      const animatedRef = useAnimatedRef<Modal>();
       return <AnimatedModal ref={animatedRef} visible />;
     };
     test('mounts without crashing', async () => {
@@ -171,7 +172,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.Pressable', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedPressable>();
+      const animatedRef = useAnimatedRef<View>();
       return (
         <AnimatedPressable ref={animatedRef} onPress={() => {}}>
           <Text>Press me</Text>
@@ -185,7 +186,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.RefreshControl', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedRefreshControl>();
+      const animatedRef = useAnimatedRef<RefreshControl>();
       return <AnimatedRefreshControl ref={animatedRef} refreshing={false} onRefresh={() => {}} />;
     };
     test('mounts without crashing', async () => {
@@ -195,7 +196,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.SectionList', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedSectionList>();
+      const animatedRef = useAnimatedRef<SectionList<unknown, unknown>>();
       return <AnimatedSectionList ref={animatedRef} sections={[]} renderItem={() => null} />;
     };
     test('mounts without crashing', async () => {
@@ -205,7 +206,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.Switch', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedSwitch>();
+      const animatedRef = useAnimatedRef<Switch>();
       return <AnimatedSwitch ref={animatedRef} value={false} onValueChange={() => {}} />;
     };
     test('mounts without crashing', async () => {
@@ -215,7 +216,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.TextInput', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedTextInput>();
+      const animatedRef = useAnimatedRef<TextInput>();
       return <AnimatedTextInput ref={animatedRef} value="Hello" onChangeText={() => {}} />;
     };
     test('mounts without crashing', async () => {
@@ -225,7 +226,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.TouchableHighlight', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedTouchableHighlight>();
+      const animatedRef = useAnimatedRef<View>();
       return (
         <AnimatedTouchableHighlight ref={animatedRef} onPress={() => {}}>
           <Text>Highlight me</Text>
@@ -239,7 +240,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.TouchableOpacity', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedTouchableOpacity>();
+      const animatedRef = useAnimatedRef<View>();
       return (
         <AnimatedTouchableOpacity ref={animatedRef} onPress={() => {}}>
           <Text>Opacitize me</Text>
@@ -253,7 +254,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.TouchableWithoutFeedback', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedTouchableWithoutFeedback>();
+      const animatedRef = useAnimatedRef<TouchableWithoutFeedback>();
       return (
         <AnimatedTouchableWithoutFeedback ref={animatedRef} onPress={() => {}}>
           <Text>Dont feedback me</Text>
@@ -267,7 +268,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.VirtualizedList', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedVirtualizedList>();
+      const animatedRef = useAnimatedRef<VirtualizedList<unknown>>();
       return (
         <AnimatedVirtualizedList
           ref={animatedRef}
@@ -286,7 +287,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.ScrollView from RNGH', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedRNGHScrollView>();
+      const animatedRef = useAnimatedRef<RNGHScrollView>();
       return <AnimatedRNGHScrollView ref={animatedRef} />;
     };
     test('mounts without crashing', async () => {
@@ -296,7 +297,7 @@ describe('Test *****useAnimatedRef*****', () => {
 
   describe('Animated.Path from react-native-svg', () => {
     const Component = () => {
-      const animatedRef = useAnimatedRef<typeof AnimatedPath>();
+      const animatedRef = useAnimatedRef<Path>();
       return <AnimatedPath ref={animatedRef} d="M10 10 H 90 V 90 H 10 L 10 10" stroke="black" fill="transparent" />;
     };
     test('mounts without crashing', async () => {