47afc532940aa4b3be1366ad3ebfa16b8c5ef4a3
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-10-29 15:43:20 +0100

    Revert temp changes

--- apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function EmptyExample() {
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      shadowColor: 'black',
-      shadowOffset: { width: 0, height: 10 },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-    };
-  });
-
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.box, animatedStyle]} />
+      <Text>Hello world!</Text>
     </View>
   );
 }
@@ -24,10 +14,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
   },
 });
