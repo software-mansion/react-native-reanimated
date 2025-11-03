14b72bc41cd01287d4ea1e3847341880c42c84a3
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2025-11-03 13:37:55 +0100

    Bigger container tags 🙈

--- packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy_Experimental.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy_Experimental.h
@@ -36,9 +36,8 @@ struct LayoutAnimation {
   LayoutAnimation &operator=(const LayoutAnimation &other) = default;
 };
 
-struct LayoutAnimationsProxy_Experimental
-    : public MountingOverrideDelegate,
-      public std::enable_shared_from_this<LayoutAnimationsProxy_Experimental> {
+struct LayoutAnimationsProxy_Experimental : public MountingOverrideDelegate,
+                                            public std::enable_shared_from_this<LayoutAnimationsProxy_Experimental> {
   mutable std::unordered_map<Tag, std::shared_ptr<Node>> nodeForTag_;
   mutable std::unordered_map<Tag, LayoutAnimation> layoutAnimations_;
   mutable std::recursive_mutex mutex;
@@ -51,7 +50,7 @@ struct LayoutAnimationsProxy_Experimental
   mutable bool transitionUpdated_;
   mutable TransitionState transitionState_ = NONE;
   mutable std::unordered_map<SurfaceId, std::shared_ptr<LightNode>> topScreen;
-  mutable int myTag = 10002;
+  mutable int myTag = 10000002;
   mutable std::vector<Tag> sharedContainersToRemove_;
   mutable std::unordered_map<Tag, Tag[2]> restoreMap_;
   mutable std::vector<Tag> tagsToRestore_;
@@ -92,8 +91,7 @@ struct LayoutAnimationsProxy_Experimental
       std::shared_ptr<CallInvoker> jsInvoker
 #endif
       )
-      : sharedTransitionManager_(
-            layoutAnimationsManager->sharedTransitionManager_),
+      : sharedTransitionManager_(layoutAnimationsManager->sharedTransitionManager_),
         layoutAnimationsManager_(layoutAnimationsManager),
         contextContainer_(contextContainer),
         componentDescriptorRegistry_(componentDescriptorRegistry),
@@ -113,16 +111,10 @@ struct LayoutAnimationsProxy_Experimental
   void startEnteringAnimation(const LightNode::Unshared &node) const;
   void startExitingAnimation(const LightNode::Unshared &node) const;
   void startLayoutAnimation(const LightNode::Unshared &node) const;
-  void startSharedTransition(
-      const int tag,
-      const ShadowView &before,
-      const ShadowView &after,
-      SurfaceId surfaceId) const;
-  void startProgressTransition(
-      const int tag,
-      const ShadowView &before,
-      const ShadowView &after,
-      SurfaceId surfaceId) const;
+  void startSharedTransition(const int tag, const ShadowView &before, const ShadowView &after, SurfaceId surfaceId)
+      const;
+  void startProgressTransition(const int tag, const ShadowView &before, const ShadowView &after, SurfaceId surfaceId)
+      const;
   void handleProgressTransition(
       ShadowViewMutationList &filteredMutations,
       const ShadowViewMutationList &mutations,
@@ -158,18 +150,11 @@ struct LayoutAnimationsProxy_Experimental
       ShadowViewMutationList &filteredMutations,
       const PropsParserContext &propsParserContext) const;
 
-  void transferConfigFromNativeID(const std::string nativeId, const int tag)
-      const;
-  std::optional<SurfaceId> progressLayoutAnimation(
-      int tag,
-      const jsi::Object &newStyle);
+  void transferConfigFromNativeID(const std::string nativeId, const int tag) const;
+  std::optional<SurfaceId> progressLayoutAnimation(int tag, const jsi::Object &newStyle);
   std::optional<SurfaceId> endLayoutAnimation(int tag, bool shouldRemove);
-  std::optional<SurfaceId> onTransitionProgress(
-      int tag,
-      double progress,
-      bool isClosing,
-      bool isGoingForward,
-      bool isSwiping);
+  std::optional<SurfaceId>
+  onTransitionProgress(int tag, double progress, bool isClosing, bool isGoingForward, bool isSwiping);
   std::optional<SurfaceId> onGestureCancel();
 
   void maybeCancelAnimation(const int tag) const;
@@ -181,13 +166,9 @@ struct LayoutAnimationsProxy_Experimental
       int index,
       const PropsParserContext &propsParserContext) const;
 
-  void insertContainers(
-      ShadowViewMutationList &filteredMutations,
-      int &rootChildCount,
-      SurfaceId surfaceId) const;
+  void insertContainers(ShadowViewMutationList &filteredMutations, int &rootChildCount, SurfaceId surfaceId) const;
 
-  std::vector<react::Point> getAbsolutePositionsForRootPathView(
-      const LightNode::Unshared &node) const;
+  std::vector<react::Point> getAbsolutePositionsForRootPathView(const LightNode::Unshared &node) const;
 
   void transferConfigToContainer(Tag containerTag, Tag beforeTag) const;
 
@@ -209,37 +190,19 @@ struct LayoutAnimationsProxy_Experimental
       const LayoutMetrics &layoutMetrics,
       const Transform &transform,
       const TransformOrigin &transformOrigin) const;
-  std::array<float, 3> getTranslateForTransformOrigin(
-      float viewWidth,
-      float viewHeight,
-      const TransformOrigin &transformOrigin) const;
+  std::array<float, 3>
+  getTranslateForTransformOrigin(float viewWidth, float viewHeight, const TransformOrigin &transformOrigin) const;
 
-  void handleRemovals(
-      ShadowViewMutationList &filteredMutations,
-      std::vector<std::shared_ptr<LightNode>> &roots) const;
+  void handleRemovals(ShadowViewMutationList &filteredMutations, std::vector<std::shared_ptr<LightNode>> &roots) const;
 
-  void addOngoingAnimations(
-      SurfaceId surfaceId,
-      ShadowViewMutationList &mutations) const;
-  void updateOngoingAnimationTarget(
-      const int tag,
-      const ShadowViewMutation &mutation) const;
-  ShadowView cloneViewWithoutOpacity(
-      const ShadowView &shadowView,
-      const PropsParserContext &propsParserContext) const;
+  void addOngoingAnimations(SurfaceId surfaceId, ShadowViewMutationList &mutations) const;
+  void updateOngoingAnimationTarget(const int tag, const ShadowViewMutation &mutation) const;
+  ShadowView cloneViewWithoutOpacity(const ShadowView &shadowView, const PropsParserContext &propsParserContext) const;
 
-  ShadowView cloneViewWithOpacity(
-      const ShadowView &shadowView,
-      const PropsParserContext &propsParserContext) const;
-  void maybeRestoreOpacity(
-      LayoutAnimation &layoutAnimation,
-      const jsi::Object &newStyle) const;
-  void maybeUpdateWindowDimensions(
-      const facebook::react::ShadowViewMutation &mutation) const;
-  ShadowView createLayoutAnimation(
-      ShadowView &before,
-      const ShadowView &after,
-      const Tag parentTag) const;
+  ShadowView cloneViewWithOpacity(const ShadowView &shadowView, const PropsParserContext &propsParserContext) const;
+  void maybeRestoreOpacity(LayoutAnimation &layoutAnimation, const jsi::Object &newStyle) const;
+  void maybeUpdateWindowDimensions(const facebook::react::ShadowViewMutation &mutation) const;
+  ShadowView createLayoutAnimation(ShadowView &before, const ShadowView &after, const Tag parentTag) const;
 
   bool startAnimationsRecursively(
       std::shared_ptr<LightNode> node,
@@ -247,21 +210,16 @@ struct LayoutAnimationsProxy_Experimental
       const bool shouldAnimate,
       const bool isScreenPop,
       ShadowViewMutationList &mutations) const;
-  void endAnimationsRecursively(
-      std::shared_ptr<LightNode> node,
-      int index,
-      ShadowViewMutationList &mutations) const;
+  void endAnimationsRecursively(std::shared_ptr<LightNode> node, int index, ShadowViewMutationList &mutations) const;
   void maybeDropAncestors(
       std::shared_ptr<LightNode> node,
       std::shared_ptr<LightNode> child,
       ShadowViewMutationList &cleanupMutations) const;
 
-  const ComponentDescriptor &getComponentDescriptorForShadowView(
-      const ShadowView &shadowView) const;
+  const ComponentDescriptor &getComponentDescriptorForShadowView(const ShadowView &shadowView) const;
 #ifdef ANDROID
   void restoreOpacityInCaseOfFlakyEnteringAnimation(SurfaceId surfaceId) const;
-  const ShadowNode *findInShadowTreeByTag(const ShadowNode &node, Tag tag)
-      const;
+  const ShadowNode *findInShadowTreeByTag(const ShadowNode &node, Tag tag) const;
 #endif // ANDROID
   // MountingOverrideDelegate
 
