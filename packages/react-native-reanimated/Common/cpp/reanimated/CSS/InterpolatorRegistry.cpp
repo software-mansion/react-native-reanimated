a4b4056a95a4da3e91ad30059b45792004bcf10e
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-11-05 14:03:08 +0100

    to upper letter

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/InterpolatorRegistry.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/InterpolatorRegistry.cpp
@@ -29,7 +29,7 @@
 #include <reanimated/CSS/interpolation/filters/operations/blur.h>
 #include <reanimated/CSS/interpolation/filters/operations/brightness.h>
 #include <reanimated/CSS/interpolation/filters/operations/contrast.h>
-#include <reanimated/CSS/interpolation/filters/operations/dropshadow.h>
+#include <reanimated/CSS/interpolation/filters/operations/dropShadow.h>
 #include <reanimated/CSS/interpolation/filters/operations/grayscale.h>
 #include <reanimated/CSS/interpolation/filters/operations/huerotate.h>
 #include <reanimated/CSS/interpolation/filters/operations/invert.h>
