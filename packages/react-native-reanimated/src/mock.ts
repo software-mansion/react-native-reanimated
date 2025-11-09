951e5a6958c9a91dcf875d21f33d0bc925874277
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-11-09 23:55:19 +0100

    cherry-pick(Reanimated): Fix `Keyframe` mock (#6961) (#8570)
    Cherry-pick of the #6961
    
    Co-authored-by: Bartłomiej Błoniarz <56109050+bartlomiejbloniarz@users.noreply.github.com>

--- packages/react-native-reanimated/src/mock.ts
+++ packages/react-native-reanimated/src/mock.ts
@@ -321,7 +321,7 @@ const core = {
 const layoutReanimation = {
   BaseAnimationBuilder: new BaseAnimationMock(),
   ComplexAnimationBuilder: new BaseAnimationMock(),
-  Keyframe: new BaseAnimationMock(),
+  Keyframe: BaseAnimationMock,
   // Flip
   FlipInXUp: new BaseAnimationMock(),
   FlipInYLeft: new BaseAnimationMock(),
