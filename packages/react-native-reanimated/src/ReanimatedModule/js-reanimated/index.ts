efcfad42c1ec1f7909bd55eae308a5b7b19bf2ad
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-11-10 00:00:10 +0100

    cherry-pick(Reanimated): Fix disappearing views when exiting LA is used on web with strict mode (#8569)
    Cherry-pick of the #8567

--- packages/react-native-reanimated/src/ReanimatedModule/js-reanimated/index.ts
+++ packages/react-native-reanimated/src/ReanimatedModule/js-reanimated/index.ts
@@ -27,7 +27,8 @@ export interface ReanimatedHTMLElement extends HTMLElement {
   _touchableNode: {
     setAttribute: (key: string, props: unknown) => void;
   };
-  reanimatedDummy?: boolean;
+  isDummy?: boolean;
+  dummyClone?: ReanimatedHTMLElement;
   removedAfterAnimation?: boolean;
 }
 
