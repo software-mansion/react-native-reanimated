software-mansion/react-native-reanimated

commit 029cf19a5db7b36425d091f1193791313f929208

fix: Web CSS animation restarting on navigation between screens (#8472)
## Summary

This PR fixes CSS animations behavior on web when we navigate between
different screens.

Since in CSS the animation is always triggered when the view display
mode changes (e.g. the DOM subtree changes from `display: 'none'` to
`display: 'flex'`, the new animation was always started when we
navigated back to the previous screens, which `display` prop changed
back from `none` to `flex`.

I changed this behavior by tracking when the animation was attached for
the first time and adjusting its delay when the view is displayed again.
Thanks to this, I can pretend that the animation is still running when
the user navigates back to the screen left before by starting it with
the adjusted (usually negative) delay.

## Example recordings

| Before | After |
|-|-|
| <video
src="https://github.com/user-attachments/assets/a8af25ca-e8e1-43e6-9372-0933c1266e52"
/> | <video
src="https://github.com/user-attachments/assets/c93971b3-5d66-4ad2-a851-e7773a37ea41"
/> |

## Test plan

Open the `FreezeExample` in the `Reanimated` tab in the example app and
navigate between screens.

packages/react-native-reanimated/src/css/web/managers/CSSAnimationsManager.ts
@@ -3,6 +3,7 @@ import {
   convertPropertiesToArrays,
   type ConvertValuesToArrays,
   kebabizeCamelCase,
+  maybeAddSuffix,
 } from '../../../common';
 import type { ReanimatedHTMLElement } from '../../../ReanimatedModule/js-reanimated';
 import type {
@@ -11,6 +12,7 @@ import type {
   ExistingCSSAnimationProperties,
   ICSSAnimationsManager,
 } from '../../types';
+import { normalizeTimeUnit } from '../../utils';
 import { processKeyframeDefinitions } from '../animationParser';
 import {
   configureWebCSSAnimations,
@@ -28,6 +30,8 @@ const isCSSKeyframesRuleImpl = (
 type ProcessedAnimation = {
   keyframesRule: CSSKeyframesRuleImpl;
   removable: boolean;
+  creationTimestamp: number;
+  elapsedTime?: number;
 };
 
 type ProcessedSettings = ConvertValuesToArrays<CSSAnimationSettings>;
@@ -37,6 +41,7 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
 
   // Keys are processed keyframes
   private attachedAnimations: Record<string, ProcessedAnimation> = {};
+  private unmountCleanupCalled = false;
 
   constructor(element: ReanimatedHTMLElement) {
     configureWebCSSAnimations();
@@ -58,53 +63,66 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
       return;
     }
 
+    const timestamp = Date.now();
     const processedAnimations = definitions.map((definition) => {
+      let processedAnimation: ProcessedAnimation;
+
       // If the CSSKeyframesRule instance was provided, we can just use it
       if (isCSSKeyframesRuleImpl(definition)) {
-        return { keyframesRule: definition, removable: false };
-      }
+        processedAnimation = this.attachedAnimations[
+          definition.processedKeyframes
+        ] ?? {
+          keyframesRule: definition,
+          removable: false,
+          creationTimestamp: timestamp,
+        };
+      } else {
+        // If keyframes was defined as an object, the additional processing is needed
+        const keyframes = definition as CSSAnimationKeyframes;
+        const processedKeyframes = processKeyframeDefinitions(keyframes);
 
-      // If keyframes was defined as an object, the additional processing is needed
-      const keyframes = definition as CSSAnimationKeyframes;
-      const processedKeyframes = processKeyframeDefinitions(keyframes);
-
-      // If the animation with the same keyframes was already attached, we can reuse it
-      if (this.attachedAnimations[processedKeyframes]) {
-        return {
-          keyframesRule:
-            this.attachedAnimations[processedKeyframes].keyframesRule,
+        // If the animation with the same keyframes was already attached, we can reuse it
+        // Otherwise, we need to create a new CSSKeyframesRule object
+        processedAnimation = this.attachedAnimations[processedKeyframes] ?? {
+          keyframesRule: new CSSKeyframesRuleImpl(
+            keyframes,
+            processedKeyframes
+          ),
           removable: true,
+          creationTimestamp: timestamp,
         };
       }
 
-      // Otherwise, we need to create a new CSSKeyframesRule object
-      return {
-        keyframesRule: new CSSKeyframesRuleImpl(keyframes, processedKeyframes),
-        removable: true,
-      };
+      if (this.unmountCleanupCalled) {
+        // unmountCleanup is called not only when the component truly unmounts, but also
+        // when display property is set to 'none' (e.g. during navigation between screens)
+        // In such a case, we don't want to restart the animation after re-entering the
+        // screen so we have to shift its delay by the time elapsed since the animation
+        // was started for the first time.
+        processedAnimation.elapsedTime =
+          timestamp - processedAnimation.creationTimestamp;
+      }
+
+      return processedAnimation;
     });
 
-    const animationNames = processedAnimations.map(
-      ({ keyframesRule: { name } }) => name
-    );
-
+    this.unmountCleanupCalled = false;
     this.updateAttachedAnimations(processedAnimations);
-    this.setElementAnimations(animationNames, animationSettings);
+    this.setElementAnimations(processedAnimations, animationSettings);
   }
 
   unmountCleanup(): void {
-    // We use setTimeout to ensure that the animation is removed after the
-    // component is unmounted (it puts the detach call at the end of the event loop)
-    setTimeout(this.detach.bind(this));
+    if (!this.unmountCleanupCalled) {
+      this.unmountCleanupCalled = true;
+      // We use setTimeout to ensure that the animation is removed after the
+      // component is unmounted (it puts the detach call at the end of the event loop)
+      // We just remove the animation definition from the style sheet as there is no
+      // need to clean up view props if it is removed from the DOM.
+      setTimeout(this.removeAnimationsFromStyleSheet.bind(this));
+    }
   }
 
   private detach() {
-    const attachedAnimations = Object.values(this.attachedAnimations);
-
-    if (attachedAnimations.length === 0) {
-      return;
-    }
-
     this.element.style.animationDuration = '';
     this.element.style.animationDelay = '';
     this.element.style.animationDirection = '';
@@ -112,13 +130,8 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
     this.element.style.animationPlayState = '';
     this.element.style.animationTimingFunction = '';
 
-    attachedAnimations.forEach(
-      ({ keyframesRule: { name, processedKeyframes }, removable }) => {
-        if (removable && processedKeyframes) {
-          removeCSSAnimation(name);
-        }
-      }
-    );
+    this.removeAnimationsFromStyleSheet();
+    this.unmountCleanupCalled = false;
     this.attachedAnimations = {};
   }
 
@@ -150,10 +163,12 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
   }
 
   private setElementAnimations(
-    animationNames: string[],
+    processedAnimations: ProcessedAnimation[],
     animationSettings: ProcessedSettings
   ) {
-    this.element.style.animationName = animationNames.join(',');
+    this.element.style.animationName = processedAnimations
+      .map(({ keyframesRule: { name } }) => name)
+      .join(',');
 
     this.element.style.animationDuration = maybeAddSuffixes(
       animationSettings,
@@ -161,11 +176,18 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
       'ms'
     ).join(',');
 
-    this.element.style.animationDelay = maybeAddSuffixes(
-      animationSettings,
-      'animationDelay',
-      'ms'
-    ).join(',');
+    const animationDelays = animationSettings.animationDelay ?? [];
+    this.element.style.animationDelay = processedAnimations
+      .map(({ elapsedTime }, i) => {
+        const providedDelay = animationDelays[i] ?? 0;
+        return maybeAddSuffix(
+          elapsedTime
+            ? (normalizeTimeUnit(providedDelay) ?? 0) - elapsedTime
+            : providedDelay,
+          'ms'
+        );
+      })
+      .join(',');
 
     if (animationSettings.animationIterationCount) {
       this.element.style.animationIterationCount =
@@ -192,5 +214,15 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
         animationSettings.animationTimingFunction
       );
     }
+  }
+
+  private removeAnimationsFromStyleSheet() {
+    Object.values(this.attachedAnimations).forEach(
+      ({ keyframesRule: { name, processedKeyframes }, removable }) => {
+        if (removable && processedKeyframes) {
+          removeCSSAnimation(name);
+        }
+      }
+    );
   }
 }

packages/react-native-reanimated/src/css/native/normalization/common/settings.ts
@@ -1,17 +1,13 @@
 'use strict';
 import { ReanimatedError } from '../../../../common';
-import {
-  MILLISECONDS_REGEX,
-  SECONDS_REGEX,
-  VALID_PREDEFINED_TIMING_FUNCTIONS,
-} from '../../../constants';
+import { VALID_PREDEFINED_TIMING_FUNCTIONS } from '../../../constants';
 import type {
   CSSTimingFunction,
   NormalizedCSSTimingFunction,
   PredefinedTimingFunction,
 } from '../../../easing';
 import type { TimeUnit } from '../../../types';
-import { isPredefinedTimingFunction } from '../../../utils';
+import { isPredefinedTimingFunction, normalizeTimeUnit } from '../../../utils';
 
 export const ERROR_MESSAGES = {
   invalidDelay: (timeUnit: TimeUnit) =>
@@ -25,17 +21,6 @@ export const ERROR_MESSAGES = {
   invalidParametrizedTimingFunction: (timingFunction: CSSTimingFunction) =>
     `Invalid parametrized timing function "${timingFunction?.toString()}".`,
 };
-
-function normalizeTimeUnit(timeUnit: TimeUnit): number | null {
-  if (typeof timeUnit === 'number') {
-    return timeUnit;
-  } else if (MILLISECONDS_REGEX.test(timeUnit)) {
-    return parseInt(timeUnit, 10);
-  } else if (SECONDS_REGEX.test(timeUnit)) {
-    return parseFloat(timeUnit) * 1000;
-  }
-  return null;
-}
 
 export function normalizeDelay(delay: TimeUnit = 0): number {
   const delayMs = normalizeTimeUnit(delay);

packages/react-native-reanimated/src/css/utils/parsers.ts
@@ -1,7 +1,8 @@
 'use strict';
 
 import { ReanimatedError } from '../../common';
-import type { SingleCSSTransitionConfig } from '../types';
+import { MILLISECONDS_REGEX, SECONDS_REGEX } from '../constants';
+import type { SingleCSSTransitionConfig, TimeUnit } from '../types';
 import { isTimeUnit, smellsLikeTimingFunction } from './guards';
 
 export function splitByComma(str: string) {
@@ -79,4 +80,15 @@ export function parseSingleTransitionShorthand(
   }
 
   return result;
+}
+
+export function normalizeTimeUnit(timeUnit: TimeUnit): number | null {
+  if (typeof timeUnit === 'number') {
+    return timeUnit;
+  } else if (MILLISECONDS_REGEX.test(timeUnit)) {
+    return parseInt(timeUnit, 10);
+  } else if (SECONDS_REGEX.test(timeUnit)) {
+    return parseFloat(timeUnit) * 1000;
+  }
+  return null;
 }

commit 44334f9a7106f21048aedcc1db4f65883bf9735f

fix: Incorrect detach call in web `CSSAnimationsManager` (#8573)
## Summary

Fixes crash caused by changes mistakenly introduced in #8472

Was getting this crash on the web:

<img width="530" height="99" alt="Screenshot 2025-11-10 at 03 06 15"
src="https://github.com/user-attachments/assets/2e854df8-4ab3-4cfe-bceb-ef01b7416d0a"
/>

packages/react-native-reanimated/src/css/web/managers/CSSAnimationsManager.ts
@@ -118,11 +118,21 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
       // component is unmounted (it puts the detach call at the end of the event loop)
       // We just remove the animation definition from the style sheet as there is no
       // need to clean up view props if it is removed from the DOM.
-      setTimeout(this.removeAnimationsFromStyleSheet.bind(this));
+      setTimeout(() => {
+        this.removeAnimationsFromStyleSheet(
+          Object.values(this.attachedAnimations)
+        );
+      });
     }
   }
 
   private detach() {
+    const attachedAnimations = Object.values(this.attachedAnimations);
+
+    if (attachedAnimations.length === 0) {
+      return;
+    }
+
     this.element.style.animationDuration = '';
     this.element.style.animationDelay = '';
     this.element.style.animationDirection = '';
@@ -130,7 +140,7 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
     this.element.style.animationPlayState = '';
     this.element.style.animationTimingFunction = '';
 
-    this.removeAnimationsFromStyleSheet();
+    this.removeAnimationsFromStyleSheet(attachedAnimations);
     this.unmountCleanupCalled = false;
     this.attachedAnimations = {};
   }
@@ -216,8 +226,8 @@ export default class CSSAnimationsManager implements ICSSAnimationsManager {
     }
   }
 
-  private removeAnimationsFromStyleSheet() {
-    Object.values(this.attachedAnimations).forEach(
+  private removeAnimationsFromStyleSheet(animations: ProcessedAnimation[]) {
+    animations.forEach(
       ({ keyframesRule: { name, processedKeyframes }, removable }) => {
         if (removable && processedKeyframes) {
           removeCSSAnimation(name);