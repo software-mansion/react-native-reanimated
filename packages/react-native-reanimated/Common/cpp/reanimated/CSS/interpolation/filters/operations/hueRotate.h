96ee63bf1bc2b25ff8153f66cb9ccdd41ad73c78
Patrycja Kalinska <patrycja.kalinska@swmansion.com>
2025-11-10 13:20:26 +0100

    everything works :)

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/filters/operations/hueRotate.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/interpolation/filters/operations/hueRotate.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <reanimated/CSS/common/values/CSSAngle.h>
+#include <reanimated/CSS/common/values/CSSNumber.h>
 #include <reanimated/CSS/interpolation/filters/FilterOperation.h>
 
 namespace reanimated::css {
 
-struct HueRotateOperation final : public FilterOperationBase<FilterOp::HueRotate, CSSAngle> {
-  using FilterOperationBase<FilterOp::HueRotate, CSSAngle>::FilterOperationBase;
+struct HueRotateOperation final : public FilterOperationBase<FilterOp::HueRotate, CSSDouble> {
+  using FilterOperationBase<FilterOp::HueRotate, CSSDouble>::FilterOperationBase;
 
-  explicit HueRotateOperation(double value) : FilterOperationBase<FilterOp::HueRotate, CSSAngle>(CSSAngle(value)) {}
+  explicit HueRotateOperation(double value) : FilterOperationBase<FilterOp::HueRotate, CSSDouble>(CSSDouble(value)) {}
 };
 
 } // namespace reanimated::css
