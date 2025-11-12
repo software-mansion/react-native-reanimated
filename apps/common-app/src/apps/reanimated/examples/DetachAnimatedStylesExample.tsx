e19c6bfc5bde5eaeea8ea0d07d4df8a820601026
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-11-12 10:02:05 +0100

    Add missing import

--- apps/common-app/src/apps/reanimated/examples/DetachAnimatedStylesExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/DetachAnimatedStylesExample.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 import Animated, {
   interpolateColor,
