501e888e9aff1535b43e008866319d9c27978bf6
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-11-17 15:41:15 +0100

    chore(Worklets): replace deprecated BigStringBuffer with JSBigString (#8601)
    ## Summary
    
    BigStringBuffer was deprecated and replaced with JSBigString here,
    https://github.com/facebook/react-native/pull/54206. The change will be
    available from RN 0.84.
    
    ## Test plan
    
    Add nightly RN version and try to compile it
    
    ```patch
    diff --git a/package.json b/package.json
    index 141335c036..7efe27344e 100644
    --- a/package.json
    +++ b/package.json
    @@ -70,5 +70,8 @@
         "react-native-builder-bob": "0.40.13",
         "shelljs": "0.10.0",
         "typescript": "5.8.3"
    +  },
    +  "resolutions": {
    +    "react-native": "0.84.0-nightly-20251114-dd6805dec"
       }
     }
    ```

--- packages/react-native-worklets/Common/cpp/worklets/NativeModules/WorkletsModuleProxy.h
+++ packages/react-native-worklets/Common/cpp/worklets/NativeModules/WorkletsModuleProxy.h
@@ -5,6 +5,7 @@
 #include <jsireact/JSIExecutor.h>
 #include <worklets/AnimationFrameQueue/AnimationFrameBatchinator.h>
 #include <worklets/NativeModules/JSIWorkletsModuleProxy.h>
+#include <worklets/Tools/Defs.h>
 #include <worklets/Tools/JSLogger.h>
 #include <worklets/Tools/JSScheduler.h>
 #include <worklets/Tools/SingleInstanceChecker.h>
@@ -26,7 +27,7 @@ class WorkletsModuleProxy : public std::enable_shared_from_this<WorkletsModulePr
       const std::shared_ptr<UIScheduler> &uiScheduler,
       std::function<bool()> &&isJavaScriptQueue,
       RuntimeBindings runtimeBindings,
-      const std::shared_ptr<const BigStringBuffer> &script,
+      const std::shared_ptr<const JSBigStringBuffer> &script,
       const std::string &sourceUrl);
 
   ~WorkletsModuleProxy();
@@ -64,7 +65,7 @@ class WorkletsModuleProxy : public std::enable_shared_from_this<WorkletsModulePr
   const std::shared_ptr<UIScheduler> uiScheduler_;
   const std::shared_ptr<JSLogger> jsLogger_;
   const RuntimeBindings runtimeBindings_;
-  const std::shared_ptr<const BigStringBuffer> script_;
+  const std::shared_ptr<const JSBigStringBuffer> script_;
   const std::string sourceUrl_;
   const std::shared_ptr<RuntimeManager> runtimeManager_;
   std::shared_ptr<WorkletRuntime> uiWorkletRuntime_;
--- packages/react-native-worklets/Common/cpp/worklets/Tools/Defs.h
+++ packages/react-native-worklets/Common/cpp/worklets/Tools/Defs.h
@@ -8,3 +8,15 @@ so we have to check if headers are available.
 #if __APPLE__ && __has_include(<hermes/hermes.h>)
 #define JS_RUNTIME_HERMES 1
 #endif
+
+#if REACT_NATIVE_MINOR_VERSION >= 84
+#include <cxxreact/JSBigString.h>
+namespace worklets {
+using JSBigStringBuffer = facebook::react::JSBigString;
+}
+#else
+#include <jsireact/JSIExecutor.h>
+namespace worklets {
+using JSBigStringBuffer = facebook::react::BigStringBuffer;
+}
+#endif // REACT_NATIVE_MINOR_VERSION >= 84
