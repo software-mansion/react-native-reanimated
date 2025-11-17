b714cb805aa0bc3b74981f5309d58a1eae5d418a
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-11-17 14:15:27 +0100

    Update TODOs

--- apps/common-app/src/apps/reanimated/examples/AnimatedTextExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/AnimatedTextExample.tsx
@@ -8,17 +8,15 @@ import Animated, {
   withTiming,
 } from 'react-native-reanimated';
 
-// TODO: make sure that React render doesn't overwrite the text
 // TODO: add support for the following syntax: <Animated.Text>{sharedValue}</Animated.Text>
 // TODO: add support for the following syntax: <Animated.Text>Before {sharedValue} After</Animated.Text>
 // TODO: add TypeScript validation that allows <Animated.Text> to either have children or have `text` animated prop
 // TODO: add error handling for unsupported nested text
 // TODO: add gauges example
-// TODO: convert existing examples from AnimatedTextInput to Animated.Text
-// TODO: convert PerformanceMonitor from AnimatedTextInput to Animated.Text
 // TODO: sync `text` prop updates back to React as `children` prop
 // TODO: fix crash when returning empty string as text animated prop
 // TODO: support both string and number as text animated prop
+// TODO: convert docs examples from AnimatedTextInput to Animated.Text
 
 export default function AnimatedTextExample() {
   const sv = useSharedValue(0);
