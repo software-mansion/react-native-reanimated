a3073b2008e175b387b626d958fb5fb5835482f6
Tomasz Żelawski <tzelawski@gmail.com>
2025-11-19 22:26:56 +0100

    chore: cleanup

--- apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
@@ -1,75 +1,18 @@
-// import App from 'common-app';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-// export default App;
+export default function EmptyExample() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
 
-import { createMMKV } from 'react-native-mmkv';
-import { type HybridObject, NitroModules } from 'react-native-nitro-modules';
-import { registerSerializable, scheduleOnUI } from 'react-native-worklets';
-
-const storage = createMMKV();
-
-storage.set('key', 2137);
-
-// globalThis.__ENABLE_LOGGING = true;
-
-NitroModules.box(storage);
-
-const boxedNitroModules = NitroModules.box(NitroModules);
-
-const determinant: any = (value: Record<string, unknown>) => {
-  'worklet';
-  if (value.__type != undefined) {
-    console.log('determined');
-  }
-  return value.__type != undefined;
-};
-
-const serializer: any = (value: HybridObject<any>) => {
-  'worklet';
-  console.log('serialized');
-  return boxedNitroModules.unbox().box(value);
-};
-
-const deserializer: any = (
-  value: /* TODO: not exposed BoxedHybridObject<any> */ typeof boxedNitroModules
-) => {
-  'worklet';
-  return value.unbox();
-};
-
-registerSerializable(determinant, serializer, deserializer);
-// const SerializedNitroModules = createSerializable(
-//   NitroModules
-//   //   NitroModules.box(NitroModules)
-// );
-// globalThis.__ENABLE_LOGGING = false;
-
-scheduleOnUI(() => {
-  'worklet';
-  console.log(storage.__type);
-  console.log(storage.getNumber('key'));
-  // console.log(storage.unbox);
-  // console.log((storage as any).unbox() as typeof storage);
-  // console.log(SerializedNitroModules);
-  // console.log(SerializedNitroModules.getAllHybridObjectNames());
-  // console.log(SerializedNitroModules.version);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-export default function App() {
-  return null;
-}
-
-declare global {
-  var __ENABLE_LOGGING: boolean;
-}
-
-// const mathObj = NitroModules.createHybridObject('Math');
-
-// const SerializedNitroModules = createSerializable()
-
-const deserializer: any = (
-  value: /* TODO: not exposed BoxedHybridObject<any> */ typeof boxedNitroModules
-) => {
-  'worklet';
-  return value.unbox();
-};
