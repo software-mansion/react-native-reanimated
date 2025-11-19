software-mansion/react-native-reanimated

commit d70002164bd3acc314d9e13ce51cd03605352ac8

chore: Relocate style processors to the common dir (#8391)
## Summary

This PR relocates style prop value preprocessors from the `css` dir to
the `common` dir. It also moves all dependent code, like some utils and
types.

packages/react-native-reanimated/src/common/style/config.ts
@@ -1,0 +1,227 @@
+'use strict';
+import { IS_ANDROID } from '../constants';
+import type { PlainStyle } from '../types';
+import { processTransformOrigin } from '../web';
+import {
+  processAspectRatio,
+  processBoxShadowNative,
+  processColor,
+  processFontWeight,
+  processGap,
+  processInset,
+  processInsetBlock,
+  processInsetInline,
+  processTransform,
+} from './processors';
+import type { StyleBuilderConfig } from './types';
+
+const colorAttributes = { process: processColor };
+
+export const BASE_PROPERTIES_CONFIG: StyleBuilderConfig<PlainStyle> = {
+  /** Layout and Positioning */
+  // FLEXBOX
+  flex: true,
+  flexBasis: true,
+  flexDirection: true,
+  justifyContent: true,
+  alignItems: true,
+  alignSelf: true,
+  alignContent: true,
+  flexGrow: true,
+  flexShrink: true,
+  flexWrap: true,
+  gap: { process: processGap },
+  rowGap: true,
+  columnGap: true,
+  start: true,
+  end: true,
+  direction: true,
+
+  // DIMENSIONS
+  height: true,
+  width: true,
+  maxHeight: true,
+  maxWidth: true,
+  minHeight: true,
+  minWidth: true,
+
+  // MARGINS
+  margin: true,
+  marginTop: true,
+  marginRight: true,
+  marginBottom: true,
+  marginLeft: true,
+  marginStart: true,
+  marginEnd: true,
+  // TODO - check if these props should depend on layout direction
+  marginBlock: { as: 'marginVertical' },
+  marginBlockEnd: { as: 'marginBottom' },
+  marginBlockStart: { as: 'marginTop' },
+  marginInline: { as: 'marginHorizontal' },
+  marginInlineEnd: { as: 'marginRight' },
+  marginInlineStart: { as: 'marginLeft' },
+  marginHorizontal: true,
+  marginVertical: true,
+
+  // PADDINGS
+  padding: true,
+  paddingTop: true,
+  paddingRight: true,
+  paddingBottom: true,
+  paddingLeft: true,
+  paddingStart: true,
+  paddingEnd: true,
+  // TODO - check if these props should depend on layout direction
+  paddingBlock: { as: 'paddingVertical' },
+  paddingBlockEnd: { as: 'paddingBottom' },
+  paddingBlockStart: { as: 'paddingTop' },
+  paddingInline: { as: 'paddingHorizontal' },
+  paddingInlineEnd: { as: 'paddingRight' },
+  paddingInlineStart: { as: 'paddingLeft' },
+  paddingHorizontal: true,
+  paddingVertical: true,
+
+  // INSETS
+  top: true,
+  left: true,
+  bottom: true,
+  right: true,
+
+  inset: { process: processInset },
+  insetBlock: { process: processInsetBlock },
+  insetInline: { process: processInsetInline },
+  // TODO - check if these props should depend on layout direction
+  insetBlockStart: { as: 'top' },
+  insetBlockEnd: { as: 'bottom' },
+  insetInlineStart: { as: 'left' },
+  insetInlineEnd: { as: 'right' },
+
+  // OTHERS
+  position: true,
+  display: true,
+  overflow: true,
+  zIndex: true,
+  aspectRatio: { process: processAspectRatio },
+  boxSizing: false, // web only
+
+  /** Appearance */
+  // COLORS
+  // Background
+  backgroundColor: colorAttributes,
+  // Text
+  color: colorAttributes,
+  textDecorationColor: colorAttributes,
+  textShadowColor: colorAttributes,
+  // Border
+  borderColor: colorAttributes,
+  borderTopColor: colorAttributes,
+  borderBlockStartColor: colorAttributes,
+  borderRightColor: colorAttributes,
+  borderEndColor: colorAttributes,
+  borderBottomColor: colorAttributes,
+  borderBlockEndColor: colorAttributes,
+  borderLeftColor: colorAttributes,
+  borderStartColor: colorAttributes,
+  borderBlockColor: colorAttributes,
+  // Other
+  outlineColor: colorAttributes,
+  shadowColor: colorAttributes,
+  overlayColor: IS_ANDROID ? colorAttributes : false,
+  tintColor: colorAttributes,
+
+  // SHADOWS
+  // View
+  shadowOffset: !IS_ANDROID,
+  shadowOpacity: !IS_ANDROID,
+  shadowRadius: !IS_ANDROID,
+  elevation: IS_ANDROID,
+  textShadowOffset: true,
+  textShadowRadius: true,
+  boxShadow: { process: processBoxShadowNative },
+
+  // BORDERS
+  // Radius
+  borderRadius: true,
+  // top-left
+  borderTopLeftRadius: true,
+  borderTopStartRadius: true,
+  borderStartStartRadius: true,
+  // top-right
+  borderTopRightRadius: true,
+  borderTopEndRadius: true,
+  borderStartEndRadius: true,
+  // bottom-left
+  borderBottomLeftRadius: true,
+  borderBottomStartRadius: true,
+  borderEndStartRadius: true,
+  // bottom-right
+  borderBottomRightRadius: true,
+  borderBottomEndRadius: true,
+  borderEndEndRadius: true,
+
+  // Width
+  borderWidth: true,
+  // top
+  borderTopWidth: true,
+  borderStartWidth: true,
+  // bottom
+  borderBottomWidth: true,
+  borderEndWidth: true,
+  // left
+  borderLeftWidth: true,
+  // right
+  borderRightWidth: true,
+
+  // Decoration
+  borderCurve: false,
+  borderStyle: true,
+
+  // OUTLINES
+  outlineOffset: true,
+  outlineStyle: true,
+  outlineWidth: true,
+
+  // TRANSFORMS
+  transformOrigin: { process: processTransformOrigin },
+  transform: { process: processTransform },
+
+  // OTHERS
+  backfaceVisibility: true,
+  opacity: true,
+  mixBlendMode: true,
+  // eslint-disable-next-line camelcase
+  experimental_backgroundImage: false, // TODO
+
+  /** Typography */
+  // Font
+  fontFamily: true,
+  fontSize: true,
+  fontStyle: true,
+  fontVariant: true,
+  fontWeight: { process: processFontWeight },
+  // Alignment
+  textAlign: true,
+  textAlignVertical: true,
+  verticalAlign: false,
+  // Decoration
+  letterSpacing: true,
+  lineHeight: true,
+  textTransform: true,
+  textDecorationLine: true,
+  textDecorationStyle: true,
+  // Others
+  userSelect: true,
+  writingDirection: false,
+  includeFontPadding: true,
+
+  /** Others */
+  // Image
+  resizeMode: true,
+  objectFit: false,
+  // Cursor
+  cursor: true,
+  pointerEvents: true,
+  // Others
+  filter: false, // web only
+  isolation: true,
+};

packages/react-native-reanimated/src/common/style/processors/index.ts
@@ -1,0 +1,12 @@
+'use strict';
+export {
+  DynamicColorIOS,
+  PlatformColor,
+  processColor,
+  processColorsInProps,
+} from './colors';
+export { processFontWeight } from './font';
+export { processInset, processInsetBlock, processInsetInline } from './insets';
+export { processAspectRatio, processGap } from './others';
+export { processBoxShadowNative } from './shadows';
+export { processTransform } from './transform';

packages/react-native-reanimated/src/common/constants/index.ts
@@ -1,0 +1,3 @@
+'use strict';
+export * from './font';
+export * from './platform';

packages/react-native-reanimated/src/common/index.ts
@@ -2,6 +2,6 @@
 export * from './constants';
 export * from './errors';
 export * from './logger';
-export * from './processors';
+export * from './style';
 export type * from './types';
 export * from './utils';

packages/react-native-reanimated/src/common/processors/index.ts
@@ -1,9 +1,0 @@
-'use strict';
-export {
-  DynamicColorIOS,
-  PlatformColor,
-  processColor,
-  processColorsInProps,
-} from './colors';
-export { processBoxShadowNative, processBoxShadowWeb } from './shadows';
-export { processTransformOrigin } from './transformOrigin';

packages/react-native-reanimated/src/common/types/config.ts
@@ -1,0 +1,11 @@
+'use strict';
+
+import type { AnyRecord, Maybe, NonMutable } from './helpers';
+
+export type ValueProcessor<V, R = V> = (
+  value: NonMutable<V>
+) => Maybe<R> | Record<string, R>;
+
+export type ConfigPropertyAlias<P extends AnyRecord> = {
+  as: keyof P;
+};

packages/react-native-reanimated/src/common/types/index.ts
@@ -1,0 +1,5 @@
+'use strict';
+
+export type * from './config';
+export type * from './helpers';
+export type * from './style';

commit 3288a48ba7066a28ff930fbe296a2fedfd4235fb

fix: Use correct `transformOrigin` processor for native CSS style processing (#8623)
## Summary

This PR fixes the issue mistakenly introduced by me in the #8391 PR. The
issue was caused by the fact that the `transformOrigin` processor was
imported from the `web` style processors dir instead of the native ones.

## Examples

| Before | After |
|-|-|
| <img width="1206" height="2622" alt="Simulator Screenshot - iPhone 17
Pro - 2025-11-18 at 23 27 16"
src="https://github.com/user-attachments/assets/f55af6cb-1822-4355-aac6-e8c1b62224ad"
/> | <video
src="https://github.com/user-attachments/assets/ce8569b0-2804-4ea1-9983-46aa2a67823f"
/> |

packages/react-native-reanimated/src/common/style/config.ts
@@ -1,7 +1,6 @@
 'use strict';
 import { IS_ANDROID } from '../constants';
 import type { PlainStyle } from '../types';
-import { processTransformOrigin } from '../web';
 import {
   processAspectRatio,
   processBoxShadowNative,
@@ -13,6 +12,7 @@ import {
   processInsetBlock,
   processInsetInline,
   processTransform,
+  processTransformOrigin,
 } from './processors';
 import type { StyleBuilderConfig } from './types';
 

packages/react-native-reanimated/src/common/style/processors/index.ts
@@ -11,3 +11,4 @@ export { processInset, processInsetBlock, processInsetInline } from './insets';
 export { processAspectRatio, processGap } from './others';
 export { processBoxShadowNative } from './shadows';
 export { processTransform } from './transform';
+export { processTransformOrigin } from './transformOrigin';