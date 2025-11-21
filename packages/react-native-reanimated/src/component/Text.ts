02210b33c78a858ebb03657d96ee58f1b0b1f77d
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-11-21 10:27:33 +0000

    Revert unnecessary changes

--- packages/react-native-reanimated/src/component/Text.ts
+++ packages/react-native-reanimated/src/component/Text.ts
@@ -1,13 +1,7 @@
 'use strict';
-import { Text, TextProps } from 'react-native';
+import { Text } from 'react-native';
 
 import { createAnimatedComponent } from '../createAnimatedComponent';
-import type { SharedValue } from '../commonTypes';
-import { ComponentType } from 'react';
-
-type AnimatedTextProps = TextProps & {
-  text?: SharedValue<string>;
-};
 
 // Since createAnimatedComponent return type is ComponentClass that has the props of the argument,
 // but not things like NativeMethods, etc. we need to add them manually by extending the type.
@@ -15,7 +9,6 @@ interface AnimatedTextComplement extends Text {
   getNode(): Text;
 }
 
-export const AnimatedText =
-  createAnimatedComponent<ComponentType<AnimatedTextProps>>(Text);
+export const AnimatedText = createAnimatedComponent(Text);
 
 export type AnimatedText = typeof AnimatedText & AnimatedTextComplement;
