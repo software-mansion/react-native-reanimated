45bbfef2ce41838ee8a4685b3875dabdb5c78474
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2025-11-21 13:50:58 +0100

    feat(Worklets): custom serialization support (#8600)
    ## Summary
    
    Adding a feature to register Custom Serializables - a way to transfer
    objects between runtimes that can't be trivially serialized or
    deserialized - like primitives, or objects without circular references,
    containing only primitives. The user can register that certain objects
    need "packing" before serializing and "unpacking" after deserialization.
    
    For the time being the API is going to be JS only. For an object to be
    considered a potential Custom Serializable it __must have a custom
    prototype, different than just `Object`__.
    
    ### API
    
    The user registers a Custom Serializable with the
    `registerCustomSerializable` function:
    
    ```ts
    function registerCustomSerializable<
      TValue extends object,
      TSerialized extends object,
    >(registrationData: RegistrationData<TValue, TSerialized>);
    
    type RegistrationData<
      TValue extends object,
      TSerialized extends object,
    > = {
      name: string;
      determine: (value: object) => value is TValue;
      pack: (value: TValue) => TSerialized;
      unpack: (value: TSerialized) => TValue;
    };
    ```
    
    where:
    
    - `name` is the user-specified name for the Custom Serializable. It's
    used to prevent registering the same Custom Serializable twice, i.e.
    during hot-reload.
    - `determine` is a __worklet__ that receives an `object` type and
    returns a boolean that tells if the object is an instance of `TValue`,
    that is a known type for packing and unpacking.
    - `pack` is a __worklet__ that receives a `TValue` that passed the
    `determine` checks and returns an object that can be trivially
    serialized, `TPacked`.
    - `unpack` is a __worklet__ that receives a `TPacked` objects and
    unpacks it to the `TValue` object for future use.
    
    All these functions are called by Worklets, so the packing and unpacking
    process is hidden from them.
    
    ### Example use (with Nitro Hybrid Objects)
    
    ```ts
    import { createMMKV, type MMKV } from 'react-native-mmkv';
    import { type HybridObject, NitroModules } from 'react-native-nitro-modules';
    import type { BoxedHybridObject } from 'react-native-nitro-modules';
    import { registerCustomSerializable, scheduleOnUI } from 'react-native-worklets';
    
    const storage: createMMKV(); // MMKV instance can't be trivially serialized, it has a custom prototype.
    storage.set('key', 42);
    
    const determine = (value: object) => {
      'worklet';
      return NitroModules.isHybridObject(value); // `NitroModules.isHybridObject` is the source of truth.
    };
    
    const pack = (value: HybridObject<never>) => {
      'worklet';
      return NitroModules.box(value); // `.box()` method creates a HostObject, which is trivially serializable.
    };
    
    const unpack = (value: BoxedHybridObject<HybridObject<never>>) => {
      'worklet';
      return value.unbox(); // `.unbox()` method extracts the HostObject to an object with the customn prototype.
    };
    
    registerCustomSerializable({
      name: 'nitro::HybridObject',
      determine,
      pack,
      unpack,
    });
    
    scheduleOnUI(() => {
      'worklet';
      const value = storage.getNumber('key'); // `storage` was implicitly packed on the RN Runtime and unpacked on the UI Runtime.
      console.log(value); // 42
    });
    ```
    
    ### Implementation details
    
    React Native runtime is the source of truth here. When a new type of a
    Serializable is registered, the registration data is __forwarded
    synchronously to all active runtimes__ and registered there. This way
    all runtimes are consistent and all of them can accept/send the new type
    of an object. The registrations are supposed to be rare so I don't think
    we should consider it a bottleneck.
    
    Registrations are also saved in new `MemoryManager` class. When a new
    Worklet Runtime is created `MemoryManager` loads all Custom Serializable
    info on it.
    
    ## Test plan
    
    Added runtime test suite.
    
    ---
    
    Docs will land after we decide if the current form of the API is
    sufficient.

--- packages/react-native-worklets/src/memory/serializable.ts
+++ packages/react-native-worklets/src/memory/serializable.ts
@@ -1,6 +1,10 @@
 'use strict';
 
-import type { FlatSerializableRef, SerializableRef } from './types';
+import type {
+  FlatSerializableRef,
+  RegistrationData,
+  SerializableRef,
+} from './types';
 
 export function isSerializableRef(value: unknown): value is SerializableRef {
   return true;
@@ -20,4 +24,11 @@ export function makeShareableCloneOnUIRecursive<TValue>(
 
 export function makeShareable<TValue>(value: TValue): TValue {
   return value;
+}
+
+export function registerCustomSerializable<
+  TValue extends object,
+  TPacked extends object,
+>(_registrationData: RegistrationData<TValue, TPacked>) {
+  // noop
 }
