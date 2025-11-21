e08fe0efa4e42712adf7abad8b7d0e29cd7e2640
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-11-21 10:24:29 +0000

    Add we transition properties normalization tests

--- packages/react-native-reanimated/src/component/Text.ts
+++ packages/react-native-reanimated/src/component/Text.ts
@@ -1,7 +1,13 @@
 'use strict';
-import { Text } from 'react-native';
+import { Text, TextProps } from 'react-native';
 
 import { createAnimatedComponent } from '../createAnimatedComponent';
+import type { SharedValue } from '../commonTypes';
+import { ComponentType } from 'react';
+
+type AnimatedTextProps = TextProps & {
+  text?: SharedValue<string>;
+};
 
 // Since createAnimatedComponent return type is ComponentClass that has the props of the argument,
 // but not things like NativeMethods, etc. we need to add them manually by extending the type.
@@ -9,6 +15,7 @@ interface AnimatedTextComplement extends Text {
   getNode(): Text;
 }
 
-export const AnimatedText = createAnimatedComponent(Text);
+export const AnimatedText =
+  createAnimatedComponent<ComponentType<AnimatedTextProps>>(Text);
 
 export type AnimatedText = typeof AnimatedText & AnimatedTextComplement;
