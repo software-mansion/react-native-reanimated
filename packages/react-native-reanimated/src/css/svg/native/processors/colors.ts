2652d7113cf507b911f3336bd6e3e6481f0c2b1c
Jakub Wiśniewski <wisniewskij514@gmail.com>
2025-11-24 18:02:17 +0100

    Introduce SVGPath

--- packages/react-native-reanimated/src/css/svg/native/processors/colors.ts
+++ packages/react-native-reanimated/src/css/svg/native/processors/colors.ts
@@ -7,6 +7,9 @@ export const processColorSVG: ValueProcessor<
   ColorValue | number,
   number | string
 > = (value) => {
+  if (value === 'none') {
+    return 'none';
+  }
   if (value === 'currentColor') {
     return 'currentColor';
   }
