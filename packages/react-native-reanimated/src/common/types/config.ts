2dedc13ad495960472f393c1918a4f0ea1ba5b9a
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-12-02 18:52:32 +0100

    Start working on target-dependent value processing

--- packages/react-native-reanimated/src/common/types/config.ts
+++ packages/react-native-reanimated/src/common/types/config.ts
@@ -2,6 +2,15 @@
 
 import type { AnyRecord, Maybe, NonMutable } from './helpers';
 
+export enum ValueProcessorTarget {
+  CSS = 'css',
+  Native = 'native',
+}
+
+export type ValueProcessorContext = {
+  target: ValueProcessorTarget;
+};
+
 export type ValueProcessor<V, R = V> = (
   value: NonMutable<V>
 ) => Maybe<R> | Record<string, R>;
