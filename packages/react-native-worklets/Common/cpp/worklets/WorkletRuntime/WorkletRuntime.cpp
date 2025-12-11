fffcded1e6e394c51c34b72f79a323dd7fee0875
Tomasz Żelawski <tzelawski@gmail.com>
2025-12-11 15:06:52 +0100

    feat: patches for 0.83.0

--- packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/WorkletRuntime.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/WorkletRuntime.cpp
@@ -116,7 +116,8 @@ void WorkletRuntime::init(std::shared_ptr<JSIWorkletsModuleProxy> jsiWorkletsMod
   }
 
   try {
-    rt.evaluateJavaScript(script, sourceUrl);
+    auto buffer = std::make_shared<const BigStringBuffer>(script);
+    rt.evaluateJavaScript(buffer, sourceUrl);
   } catch (facebook::jsi::JSError error) {
     const auto &message = error.getMessage();
     const auto &stack = error.getStack();
