2b63f3a8cfead49faa6769664f820935e073dc66
Tomasz Żelawski <tzelawski@gmail.com>
2025-12-12 14:45:47 +0100

    feat: support SDK lower than 33

--- packages/react-native-worklets/android/src/main/java/com/swmansion/worklets/ScriptWrapper.java
+++ packages/react-native-worklets/android/src/main/java/com/swmansion/worklets/ScriptWrapper.java
@@ -7,6 +7,10 @@ import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.proguard.annotations.DoNotStripAny;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /** A wrapper around a JavaScript bundle that is backed by a native C++ object. */
 @SuppressWarnings("JavaJniMissingFunction")
@@ -35,12 +39,36 @@ public class ScriptWrapper {
   }
 
   private static String downloadScript(String url) throws IOException {
-    var connection = (java.net.HttpURLConnection) new java.net.URL(url).openConnection();
-    byte[] content = null;
+    var scriptUrl = new URL(url);
+    HttpURLConnection connection = (HttpURLConnection) scriptUrl.openConnection();
+
+    byte[] content;
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
       content = connection.getInputStream().readAllBytes();
+    } else {
+      content = readBytes(connection.getInputStream());
     }
-    return new String(content, java.nio.charset.StandardCharsets.UTF_8);
+
+    return new String(content, StandardCharsets.UTF_8);
+  }
+
+  /** Reads all bytes from an InputStream into a byte array for SDKs below 33. */
+  private static byte[] readBytes(InputStream inputStream) throws IOException {
+    final int BUFFER_SIZE = 4096;
+    try {
+      java.io.ByteArrayOutputStream byteBuffer = new java.io.ByteArrayOutputStream();
+      byte[] buffer = new byte[BUFFER_SIZE];
+      int len;
+      while ((len = inputStream.read(buffer)) != -1) {
+        byteBuffer.write(buffer, 0, len);
+      }
+      return byteBuffer.toByteArray();
+    } finally {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+    }
   }
 
   private native HybridData initHybridFromAssets(AssetManager assetManager, String assetURL);
