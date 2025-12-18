687cb10e8650d25200e32939ef47e4ec5d056fdf
Jacek Pudysz <jacekpudysz@gmail.com>
2025-12-18 13:12:51 +0100

    fix: one more for tree cloner

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.cpp
@@ -48,8 +48,7 @@ std::shared_ptr<ShadowNode> cloneShadowTreeWithNewPropsRecursive(
 
   if (affectedChildrenIt != childrenMap.end()) {
     for (const auto index : affectedChildrenIt->second) {
-      children[index] = cloneShadowTreeWithNewPropsRecursive(
-          *children[index], childrenMap, propsMap, enableRuntimeReferenceUpdates);
+      children[index] = cloneShadowTreeWithNewPropsRecursive(*children[index], childrenMap, propsMap, enableRuntimeReferenceUpdates);
     }
   }
 
