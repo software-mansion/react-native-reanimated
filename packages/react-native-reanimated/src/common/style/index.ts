29ed9b2c6fa9dfb26bf4a64d6298455d35ecaf8e
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2026-01-21 18:39:58 +0100

    feat: Web props and rule builders functional reimplementation (#8764)
    ## Summary
    
    This is a follow-up PR after the props builder changes in the
    implementation used by native platforms added in #8693. This PR changes
    the web's style/rule builders to the functional props/rule builders.

--- packages/react-native-reanimated/src/common/style/index.ts
+++ packages/react-native-reanimated/src/common/style/index.ts
@@ -1,5 +1,6 @@
 'use strict';
 export * from './config';
+export { default as createPropsBuilder } from './createPropsBuilder';
 export * from './processors';
 export { type NativePropsBuilder, stylePropsBuilder } from './propsBuilder';
 export * from './registry';
