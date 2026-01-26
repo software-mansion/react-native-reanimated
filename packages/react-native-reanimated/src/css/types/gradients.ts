2c7ae24e40a00dc7a0a241ed1ef4bfbe88d04974
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-01-26 15:56:59 +0100

    Finish basic props from radial gradient

--- packages/react-native-reanimated/src/css/types/gradients.ts
+++ packages/react-native-reanimated/src/css/types/gradients.ts
@@ -1,12 +1,15 @@
+'use strict';
+
 import 'react-native-svg';
 
-import type { NumberProp } from 'react-native-svg';
 import type { ColorValue } from 'react-native';
+import type { NumberProp } from 'react-native-svg';
 
 declare module 'react-native-svg' {
   export interface GradientStop {
     offset: NumberProp;
     color?: ColorValue | number;
+    opacity?: NumberProp;
   }
 
   export interface RadialGradientProps {
