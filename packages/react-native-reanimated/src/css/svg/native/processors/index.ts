efecc0487a89ca9f29d45be317c732a78d94bf1b
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-01-30 10:02:14 +0100

    Inline some processors

--- packages/react-native-reanimated/src/css/svg/native/processors/index.ts
+++ packages/react-native-reanimated/src/css/svg/native/processors/index.ts
@@ -1,13 +1,7 @@
 'use strict';
 export * from './colors';
-export { processSVGGradientUnits } from './gradientUnits';
 export * from './others';
-export { processSVGPath } from './path';
 export * from './percentage';
-export {
-  processSVGRadialGradientFocalX,
-  processSVGRadialGradientFocalY,
-  processSVGRadialGradientRadius,
-} from './radialGradientCoords';
+export { processSVGPath } from './path';
 export { processSVGGradientStops } from './stops';
 export { processStrokeDashArray } from './stroke';
