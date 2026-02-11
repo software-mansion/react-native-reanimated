e9706e66a3ff280a2cdd5cbc4cc383ab715e9931
Jakub Wiśniewski <119816982+wisniewskij@users.noreply.github.com>
2026-02-11 10:17:56 +0100

    refactor: Rename processOpacity to processPercentage (#8947)
    ## Summary
    `processOpacity` basically just works like a fraction processor: if it's
    a string with a percentage sign -> turn into fraction, then clamp the
    number between 0 and 1. I'd like to use this processor with some
    properties that don't relate to opacity, therefore name change.

--- packages/react-native-reanimated/src/css/svg/native/processors/index.ts
+++ packages/react-native-reanimated/src/css/svg/native/processors/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 export * from './colors';
-export * from './opacity';
 export * from './others';
 export { processSVGPath } from './path';
+export * from './percentage';
 export { processStrokeDashArray } from './stroke';
