a4ac32c92f4aea393ad57edb029ee8b444669648
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-02-11 07:15:08 +0100

    Rename processOpacity to processPercentage

--- packages/react-native-reanimated/src/css/svg/native/configs/path.ts
+++ packages/react-native-reanimated/src/css/svg/native/configs/path.ts
@@ -4,7 +4,7 @@
 // @ts-ignore RNSVG doesn't export types for web, see https://github.com/software-mansion/react-native-svg/pull/2801
 import type { PathProps } from 'react-native-svg';
 
-import { processOpacity, processSVGPath } from '../processors';
+import { processPercentage, processSVGPath } from '../processors';
 import type { SvgStyleBuilderConfig } from './common';
 import { commonSvgProps } from './common';
 
@@ -13,5 +13,5 @@ import { commonSvgProps } from './common';
 export const SVG_PATH_PROPERTIES_CONFIG: SvgStyleBuilderConfig<PathProps> = {
   ...commonSvgProps,
   d: { process: processSVGPath },
-  opacity: { process: processOpacity },
+  opacity: { process: processPercentage },
 };
