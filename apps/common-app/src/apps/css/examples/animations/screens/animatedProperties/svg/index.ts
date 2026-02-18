dbed84f05250c8aabdd49883d2de12a25278bf08
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2026-02-18 15:21:15 +0100

    chore: eslint fixes in repo (#8944)
    ## Summary
    
    I was annoyed that `yarn lint` in `packages/react-native-reanimated`
    lists 70+ warnings about `any` uses etc. that realistically we're not
    going to fix anytime soon.
    
    I made `any` ignored in `packages/react-native-reanimated`, changed the
    rules a bit in eslint configs and run `yarn lint --fix` in relevant
    directories.
    
    ## Test plan
    
    CI

--- apps/common-app/src/apps/css/examples/animations/screens/animatedProperties/svg/index.ts
+++ apps/common-app/src/apps/css/examples/animations/screens/animatedProperties/svg/index.ts
@@ -4,18 +4,18 @@ import Ellipse from './Ellipse';
 import Image from './Image';
 import Line from './Line';
 import LinearGradient from './LinearGradient';
-import Rect from './Rect';
-import RadialGradient from './RadialGradient';
 import Path from './Path';
+import RadialGradient from './RadialGradient';
+import Rect from './Rect';
 
 export default {
   Circle,
+  common,
   Ellipse,
   Image,
   Line,
   LinearGradient,
   Path,
-  common,
-  Rect,
   RadialGradient,
+  Rect,
 };
