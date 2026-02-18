76d041a082866573f30a8c3c3fd138a503926cb1
Artur Bilski <admin@arturbilski.pl>
2026-02-18 11:10:17 +0100

    Update TSDocs with parameter default values. (#7438)

--- packages/react-native-reanimated/src/animation/decay/decay.ts
+++ packages/react-native-reanimated/src/animation/decay/decay.ts
@@ -56,6 +56,7 @@ function validateConfig(config: DefaultDecayConfig): void {
  * Lets you create animations that mimic objects in motion with friction.
  *
  * @param config - The decay animation configuration - {@link DecayConfig}.
+ *   Defaults to {@link DecayConfig} default values.
  * @param callback - A function called upon animation completion -
  *   {@link AnimationCallback}.
  * @returns An [animation
