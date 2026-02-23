51fa717896dd8e1b0b6acae484b4c143be3b5132
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-02-23 13:35:46 +0100

    Fix the processor

--- packages/react-native-reanimated/src/css/svg/native/processors/polypoints.ts
+++ packages/react-native-reanimated/src/css/svg/native/processors/polypoints.ts
@@ -2,12 +2,19 @@
 import type { NumberProp } from 'react-native-svg';
 
 import type { ValueProcessor } from '../../../../common';
+import { processSVGPath } from './path';
 
-export const processPolylinePoints: ValueProcessor<string | ReadonlyArray<NumberProp>> = (points) => {
-    const polyPoints = Array.isArray(points) ? points.join(',') : points;
+export const processPolylinePoints: ValueProcessor<
+  string | ReadonlyArray<NumberProp>
+> = (points) => {
+  const polyPoints = Array.isArray(points) ? points.join(',') : points;
 
-    return {d: `M${(polyPoints as string)
-        .replace(/[^eE]-/, ' -')
+  return {
+    d: processSVGPath(
+      `M${(polyPoints as string)
+        .replace(/([^eE])-/g, '$1 -')
         .split(/(?:\s+|\s*,\s*)/g)
-        .join(' ')}`};
+        .join(' ')}`
+    ) as string,
+  };
 };
