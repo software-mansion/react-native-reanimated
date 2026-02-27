2828d27ca8d0e5b0c78a810368ceb859944ce438
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2026-02-27 23:47:15 +0100

    Fix routes order in SVG examples screen

--- apps/common-app/src/apps/css/examples/animations/routes/properties/svg.ts
+++ apps/common-app/src/apps/css/examples/animations/routes/properties/svg.ts
@@ -1,55 +1,8 @@
+/* eslint-disable perfectionist/sort-objects */
 import { svgAnimatedProperties } from '@/apps/css/examples/animations/screens';
 import type { Routes } from '@/apps/css/navigation/types';
 
 export const svgPropertiesRoutes = {
-  Circle: {
-    Component: svgAnimatedProperties.Circle,
-    name: 'Circle',
-  },
-  Ellipse: {
-    Component: svgAnimatedProperties.Ellipse,
-    name: 'Ellipse',
-  },
-  Group: {
-    Component: svgAnimatedProperties.Group,
-    name: 'Group',
-  },
-  Image: {
-    Component: svgAnimatedProperties.Image,
-    name: 'Image',
-  },
-  Line: {
-    Component: svgAnimatedProperties.Line,
-    name: 'Line',
-  },
-  LinearGradient: {
-    Component: svgAnimatedProperties.LinearGradient,
-    name: 'LinearGradient',
-  },
-  Path: {
-    Component: svgAnimatedProperties.Path,
-    name: 'Path',
-  },
-  Polygon: {
-    Component: svgAnimatedProperties.Polygon,
-    name: 'Polygon',
-  },
-  Polyline: {
-    Component: svgAnimatedProperties.Polyline,
-    name: 'Polyline',
-  },
-  RadialGradient: {
-    Component: svgAnimatedProperties.RadialGradient,
-    name: 'RadialGradient',
-  },
-  Rect: {
-    Component: svgAnimatedProperties.Rect,
-    name: 'Rect',
-  },
-  Text: {
-    Component: svgAnimatedProperties.Text,
-    name: 'Text',
-  },
   Common: {
     name: 'Common',
     routes: {
@@ -60,6 +13,59 @@ export const svgPropertiesRoutes = {
       Stroke: {
         Component: svgAnimatedProperties.common.Stroke,
         name: 'Stroke',
+      },
+    },
+  },
+  Components: {
+    name: 'Components',
+    routes: {
+      Circle: {
+        Component: svgAnimatedProperties.Circle,
+        name: 'Circle',
+      },
+      Ellipse: {
+        Component: svgAnimatedProperties.Ellipse,
+        name: 'Ellipse',
+      },
+      Rect: {
+        Component: svgAnimatedProperties.Rect,
+        name: 'Rect',
+      },
+      Image: {
+        Component: svgAnimatedProperties.Image,
+        name: 'Image',
+      },
+      Line: {
+        Component: svgAnimatedProperties.Line,
+        name: 'Line',
+      },
+      Path: {
+        Component: svgAnimatedProperties.Path,
+        name: 'Path',
+      },
+      Polygon: {
+        Component: svgAnimatedProperties.Polygon,
+        name: 'Polygon',
+      },
+      Polyline: {
+        Component: svgAnimatedProperties.Polyline,
+        name: 'Polyline',
+      },
+      LinearGradient: {
+        Component: svgAnimatedProperties.LinearGradient,
+        name: 'LinearGradient',
+      },
+      RadialGradient: {
+        Component: svgAnimatedProperties.RadialGradient,
+        name: 'RadialGradient',
+      },
+      Group: {
+        Component: svgAnimatedProperties.Group,
+        name: 'Group',
+      },
+      Text: {
+        Component: svgAnimatedProperties.Text,
+        name: 'Text',
       },
     },
   },
