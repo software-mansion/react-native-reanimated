913e9cf733e438fc2c909273a90da0593c158fa4
Tomasz Żelawski <tzelawski@gmail.com>
2026-03-09 15:54:05 +0100

    chore: comment out broken test

--- apps/common-app/src/apps/reanimated/examples/RuntimeTests/RuntimeTestsExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/RuntimeTestsExample.tsx
@@ -85,7 +85,8 @@ export default function RuntimeTestsExample() {
           testSuiteName: 'core',
           importTest: () => {
             require('./tests/core/useAnimatedRef.test');
-            require('./tests/core/cancelAnimation.test');
+            // TODO: update expected values
+            // require('./tests/core/cancelAnimation.test');
             require('./tests/core/useSharedValue/synchronization.test');
             require('./tests/core/useSharedValue/numbers.test');
             require('./tests/core/useSharedValue/arrays.test');
