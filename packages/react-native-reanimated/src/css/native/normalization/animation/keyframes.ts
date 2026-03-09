software-mansion/react-native-reanimated

commit 1f212d04892cfd6df36bad2e936245a113687562

chore: Rename style builder to props builder (#8757)
## Summary

Since we have builders for props other than style properties (e.g. props
from SVGs), I renamed the builder and other related types to use more
appropriate name.

packages/react-native-reanimated/src/css/native/normalization/animation/keyframes.ts
@@ -1,7 +1,15 @@
 'use strict';
-import type { AnyRecord, StyleBuilder } from '../../../../common';
-import { isDefined, isNumber, ReanimatedError } from '../../../../common';
-import type { StyleProps } from '../../../../commonTypes';
+import type {
+  AnyRecord,
+  PropsBuilder,
+  UnknownRecord,
+} from '../../../../common';
+import {
+  isDefined,
+  isNumber,
+  isRecord,
+  ReanimatedError,
+} from '../../../../common';
 import { PERCENTAGE_REGEX } from '../../../constants';
 import type {
   CSSAnimationKeyframes,
@@ -10,8 +18,8 @@ import type {
 } from '../../../types';
 import type {
   NormalizedCSSAnimationKeyframesConfig,
-  NormalizedCSSKeyframesStyle,
   NormalizedCSSKeyframeTimingFunctions,
+  PropsWithKeyframes,
 } from '../../types';
 import { normalizeTimingFunction } from '../common';
 
@@ -61,24 +69,24 @@ export function normalizeKeyframeSelector(
 
 type ProcessedKeyframes = Array<{
   offset: number;
-  style: StyleProps;
+  props: UnknownRecord;
   timingFunction?: CSSAnimationTimingFunction;
 }>;
 
 export function processKeyframes(
   keyframes: CSSAnimationKeyframes,
-  styleBuilder: StyleBuilder<AnyRecord>
+  propsBuilder: PropsBuilder<AnyRecord>
 ): ProcessedKeyframes {
   return Object.entries(keyframes)
     .flatMap(
-      ([selector, { animationTimingFunction = undefined, ...style } = {}]) => {
-        const normalizedStyle = styleBuilder.buildFrom(style);
-        if (!normalizedStyle) {
+      ([selector, { animationTimingFunction = undefined, ...props } = {}]) => {
+        const normalizedProps = propsBuilder.buildFrom(props);
+        if (!normalizedProps) {
           return [];
         }
         return normalizeKeyframeSelector(selector).map((offset) => ({
           offset,
-          style: normalizedStyle,
+          props: normalizedProps,
           ...(animationTimingFunction && {
             timingFunction: animationTimingFunction,
           }),
@@ -89,7 +97,7 @@ export function processKeyframes(
     .reduce<ProcessedKeyframes>((acc, keyframe) => {
       const lastKeyframe = acc[acc.length - 1];
       if (lastKeyframe && lastKeyframe.offset === keyframe.offset) {
-        lastKeyframe.style = { ...lastKeyframe.style, ...keyframe.style };
+        lastKeyframe.props = { ...lastKeyframe.props, ...keyframe.props };
         lastKeyframe.timingFunction = keyframe.timingFunction;
       } else {
         acc.push(keyframe);
@@ -98,54 +106,50 @@ export function processKeyframes(
     }, []);
 }
 
-function processStyleProperties<S extends AnyRecord>(
+function processProps(
   offset: number,
-  style: S,
-  keyframeStyle: AnyRecord,
-  styleBuilder: StyleBuilder<AnyRecord>
+  props: UnknownRecord,
+  keyframeProps: AnyRecord,
+  propsBuilder: PropsBuilder<AnyRecord>
 ) {
-  Object.entries(style).forEach(([property, value]) => {
+  Object.entries(props).forEach(([property, value]) => {
     if (!isDefined(value)) {
       return;
     }
 
-    if (typeof value === 'object') {
-      if (styleBuilder.isSeparatelyInterpolatedNestedProperty(property)) {
-        if (!keyframeStyle[property]) {
-          keyframeStyle[property] = Array.isArray(value) ? [] : {};
-        }
-        processStyleProperties(
-          offset,
-          value,
-          keyframeStyle[property],
-          styleBuilder
-        );
-        return;
+    if (
+      isRecord(value) &&
+      propsBuilder.isSeparatelyInterpolatedNestedProperty(property)
+    ) {
+      if (!keyframeProps[property]) {
+        keyframeProps[property] = Array.isArray(value) ? [] : {};
       }
+      processProps(offset, value, keyframeProps[property], propsBuilder);
+      return;
     }
 
-    if (!keyframeStyle[property]) {
-      keyframeStyle[property] = [];
+    if (!keyframeProps[property]) {
+      keyframeProps[property] = [];
     }
-    keyframeStyle[property].push({ offset, value });
+    keyframeProps[property].push({ offset, value });
   });
 }
 
 export function normalizeAnimationKeyframes(
   keyframes: CSSAnimationKeyframes,
-  styleBuilder: StyleBuilder<AnyRecord>
+  propsBuilder: PropsBuilder<AnyRecord>
 ): NormalizedCSSAnimationKeyframesConfig {
-  const keyframesStyle: NormalizedCSSKeyframesStyle = {};
+  const propKeyframes: PropsWithKeyframes = {};
   const timingFunctions: NormalizedCSSKeyframeTimingFunctions = {};
 
-  processKeyframes(keyframes, styleBuilder).forEach(
-    ({ offset, style, timingFunction }) => {
-      processStyleProperties(offset, style, keyframesStyle, styleBuilder);
+  processKeyframes(keyframes, propsBuilder).forEach(
+    ({ offset, props, timingFunction }) => {
+      processProps(offset, props, propKeyframes, propsBuilder);
       if (timingFunction && offset < 1) {
         timingFunctions[offset] = normalizeTimingFunction(timingFunction);
       }
     }
   );
 
-  return { keyframesStyle, keyframeTimingFunctions: timingFunctions };
+  return { propKeyframes, keyframeTimingFunctions: timingFunctions };
 }

packages/react-native-reanimated/Common/cpp/reanimated/CSS/configs/CSSKeyframesConfig.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<AnimationStyleInterpolator> createStyleInterpolator(
     const std::string &componentName,
     const std::shared_ptr<ViewStylesRepository> &viewStylesRepository) {
   return std::make_shared<AnimationStyleInterpolator>(
-      rt, config.getProperty(rt, "keyframesStyle"), componentName, viewStylesRepository);
+      rt, config.getProperty(rt, "propKeyframes"), componentName, viewStylesRepository);
 }
 
 std::shared_ptr<KeyframeEasingFunctions> getKeyframeTimingFunctions(jsi::Runtime &rt, const jsi::Object &config) {

packages/react-native-reanimated/src/common/style/__tests__/createPropsBuilder.test.ts
@@ -1,11 +1,11 @@
 'use strict';
 import type { PlainStyle } from '../../types';
 import { ValueProcessorTarget } from '../../types';
-import createStyleBuilder from '../createStyleBuilder';
+import createPropsBuilder from '../createPropsBuilder';
 // TODO - add more tests
 
-describe(createStyleBuilder, () => {
-  const styleBuilder = createStyleBuilder({
+describe(createPropsBuilder, () => {
+  const styleBuilder = createPropsBuilder({
     width: true,
     margin: true,
     borderRadius: true,
@@ -40,7 +40,7 @@ describe(createStyleBuilder, () => {
   test('passes context to processors', () => {
     const processor = jest.fn();
 
-    const builder = createStyleBuilder(
+    const builder = createPropsBuilder(
       {
         borderRadius: {
           process: processor,
@@ -61,7 +61,7 @@ describe(createStyleBuilder, () => {
   test('uses default target when none provided', () => {
     const processor = jest.fn();
 
-    const builder = createStyleBuilder({ padding: { process: processor } });
+    const builder = createPropsBuilder({ padding: { process: processor } });
 
     builder.buildFrom({ padding: 8 });
 

packages/react-native-reanimated/src/common/style/config.ts
@@ -15,11 +15,11 @@ import {
   processTransform,
   processTransformOrigin,
 } from './processors';
-import type { StyleBuilderConfig } from './types';
+import type { PropsBuilderConfig } from './types';
 
 const colorAttributes = { process: processColor };
 
-export const BASE_PROPERTIES_CONFIG: StyleBuilderConfig<PlainStyle> = {
+export const BASE_PROPERTIES_CONFIG: PropsBuilderConfig<PlainStyle> = {
   /** Layout and Positioning */
   // FLEXBOX
   flex: true,

packages/react-native-reanimated/src/common/style/createPropsBuilder.ts
@@ -4,8 +4,8 @@ import type { AnyRecord, ValueProcessorContext } from '../types';
 import { ValueProcessorTarget } from '../types';
 import { isConfigPropertyAlias, isDefined, isRecord } from '../utils';
 import type {
-  StyleBuilder,
-  StyleBuilderConfig,
+  PropsBuilder,
+  PropsBuilderConfig,
   StyleBuildMiddleware,
 } from './types';
 
@@ -15,15 +15,15 @@ type StyleBuilderOptions<P extends AnyRecord> = {
   target?: ValueProcessorTarget;
 };
 
-class StyleBuilderImpl<P extends AnyRecord> implements StyleBuilder<P> {
+class StyleBuilderImpl<P extends AnyRecord> implements PropsBuilder<P> {
   private readonly buildMiddleware: StyleBuildMiddleware<P>;
-  private readonly config: StyleBuilderConfig<P>;
+  private readonly config: PropsBuilderConfig<P>;
   private readonly separatelyInterpolatedNestedProperties_: (keyof P)[];
   private readonly context: ValueProcessorContext;
 
   private processedProps = {} as P;
 
-  constructor(config: StyleBuilderConfig<P>, options?: StyleBuilderOptions<P>) {
+  constructor(config: PropsBuilderConfig<P>, options?: StyleBuilderOptions<P>) {
     this.config = config;
     this.buildMiddleware = options?.buildMiddleware ?? ((props) => props);
     this.separatelyInterpolatedNestedProperties_ =
@@ -95,9 +95,9 @@ class StyleBuilderImpl<P extends AnyRecord> implements StyleBuilder<P> {
   }
 }
 
-export default function createStyleBuilder<P extends AnyRecord>(
-  config: StyleBuilderConfig<P>,
+export default function createPropsBuilder<P extends AnyRecord>(
+  config: PropsBuilderConfig<P>,
   options?: StyleBuilderOptions<P>
-): StyleBuilder<Partial<P>> {
+): PropsBuilder<Partial<P>> {
   return new StyleBuilderImpl(config, options);
 }

packages/react-native-reanimated/src/common/style/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 export * from './config';
-export { default as createStyleBuilder } from './createStyleBuilder';
+export { default as createPropsBuilder } from './createPropsBuilder';
 export * from './processors';
-export type { StyleBuilder } from './types';
+export type { PropsBuilder } from './types';
 export type * from './types';

packages/react-native-reanimated/src/common/style/types.ts
@@ -3,7 +3,7 @@ import type { AnyRecord, ConfigPropertyAlias, ValueProcessor } from '../types';
 
 export type StyleBuildMiddleware<P extends AnyRecord> = (props: P) => P;
 
-export type StyleBuilder<P extends AnyRecord = AnyRecord> = {
+export type PropsBuilder<P extends AnyRecord = AnyRecord> = {
   isSeparatelyInterpolatedNestedProperty(property: keyof P): boolean;
   add(property: keyof P, value: P[keyof P]): void;
   buildFrom(props: P): P | null;
@@ -13,7 +13,7 @@ type PropertyValueConfigBase<P extends AnyRecord> =
   | boolean // true - included, false - excluded
   | ConfigPropertyAlias<P>; // alias for another property
 
-type StyleBuilderPropertyConfig<
+type PropsBuilderPropertyConfig<
   P extends AnyRecord,
   K extends keyof P = keyof P,
 > =
@@ -25,6 +25,6 @@ type StyleBuilderPropertyConfig<
       process: ValueProcessor<Required<P>[K], any>; // for custom value processing
     };
 
-export type StyleBuilderConfig<P extends AnyRecord = AnyRecord> = {
-  [K in keyof Required<P>]: StyleBuilderPropertyConfig<P, K>;
+export type PropsBuilderConfig<P extends AnyRecord = AnyRecord> = {
+  [K in keyof Required<P>]: PropsBuilderPropertyConfig<P, K>;
 };

packages/react-native-reanimated/src/common/types/helpers.ts
@@ -13,6 +13,7 @@ export type Maybe<T> = T | null | undefined;
 export type NonMutable<T> = T extends object ? Readonly<T> : T;
 
 export type AnyRecord = Record<string, any>;
+export type UnknownRecord = Record<string, unknown>;
 
 export type AnyComponent = ComponentType<any>;
 

commit b8d50c34acb2d172171e8c7ee01ace6c541f40d5

fix: CSS animation box-shadow crashes (#9065)
## Summary

This PR fixes the issue introduced in the #8757 PR where I changed the
`typeof value === 'object'` check to `isRecord(value)`. We need to keep
the object type check to ensure the if statement is entered also when an
array value is passed (not only for records).

## Test plan

Open the **Box Shadow** CSS example (under **Animated Properties**) and
see that it works now.

packages/react-native-reanimated/src/css/native/normalization/animation/keyframes.ts
@@ -9,7 +9,6 @@ import {
   getSeparatelyInterpolatedNestedProperties,
   isDefined,
   isNumber,
-  isRecord,
   ReanimatedError,
 } from '../../../../common';
 import { PERCENTAGE_REGEX } from '../../../constants';
@@ -110,7 +109,7 @@ export function processKeyframes(
 
 function processProps(
   offset: number,
-  props: UnknownRecord,
+  props: object,
   keyframeProps: AnyRecord,
   separatelyInterpolatedNestedProperties: ReadonlySet<string>
 ) {
@@ -120,7 +119,8 @@ function processProps(
     }
 
     if (
-      isRecord(value) &&
+      /* this object type check is correct as it accepts records and arrays */
+      typeof value === 'object' &&
       separatelyInterpolatedNestedProperties.has(property)
     ) {
       if (!keyframeProps[property]) {