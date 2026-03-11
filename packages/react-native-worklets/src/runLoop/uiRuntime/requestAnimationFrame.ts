software-mansion/react-native-reanimated

commit 5eb5c17990f02e176eac05b1b3515e0078a2bf59

fix: Reanimated synchronous events (#9057)
## Summary

I introduced a regression in
- #8900
where Reanimated wouldn't be able to imperatively flush the animation
frame queue anymore, resulting in using "wrong" timestamps for animation
updates coming from events.

This is more of a band-aid fix as imperative flushes have always had
other issues...

## Test plan

Stinky header example works again

packages/react-native-worklets/src/runLoop/uiRuntime/requestAnimationFrame.ts
@@ -62,8 +62,10 @@ export function setupRequestAnimationFrame() {
   globalThis.requestAnimationFrame = requestAnimationFrame;
   globalThis.cancelAnimationFrame =
     cancelAnimationFrame as typeof globalThis.cancelAnimationFrame;
-  globalThis.__flushAnimationFrame = () => {
-    // NOOP for backwards compatibility
+  globalThis.__flushAnimationFrame = (eventTimestamp: number) => {
+    // TODO: Remove this in the future.
+    // Reanimated uses this method to trigger event synchronously.
+    flushQueue(eventTimestamp);
   };
 
   /* Start the loop */

packages/react-native-worklets/src/privateGlobals.d.ts
@@ -67,6 +67,7 @@ declare global {
   var __customSerializationRegistry: CustomSerializationRegistry;
   var __customSerializableUnpacker: CustomSerializableUnpacker;
   var __callGuardDEV: typeof callGuardDEV | undefined;
+  /** @deprecated Don't flush animation frames imperatively. Don't use. */
   var __flushAnimationFrame: (timestamp: number) => void;
   var __frameTimestamp: number | undefined;
   var _log: (value: unknown) => void;

commit 63b87dbd5d62c94e3c01ef49262b5ae189d80e6d

fix(Worklets): cascade raf callbacks with reanimated (#9080)
## Summary

Fixing regression introduced in #9057. Reanimated imperative calls to
`__flushAnimationFrame` would result in exponential scheduling of more
native animation frame queue flushes.

With this change we make sure only one native flush is scheduled at
most.

## Test plan

Stinky Header -> Bokeh Example no longer lags like crazy.

packages/react-native-worklets/src/runLoop/uiRuntime/requestAnimationFrame.ts
@@ -50,13 +50,17 @@ export function setupRequestAnimationFrame() {
     }
   }
 
+  function nativeFlushQueue(timestamp: number) {
+    flushQueue(timestamp);
+
+    /* Schedule next frame */
+    nativeRequestAnimationFrame(nativeFlushQueue);
+  }
+
   function flushQueue(timestamp: number) {
     globalThis.__frameTimestamp = timestamp;
     executeQueue(timestamp);
     globalThis.__frameTimestamp = undefined;
-
-    /* Schedule next frame */
-    nativeRequestAnimationFrame(flushQueue);
   }
 
   globalThis.requestAnimationFrame = requestAnimationFrame;
@@ -69,5 +73,5 @@ export function setupRequestAnimationFrame() {
   };
 
   /* Start the loop */
-  nativeRequestAnimationFrame(flushQueue);
+  nativeRequestAnimationFrame(nativeFlushQueue);
 }