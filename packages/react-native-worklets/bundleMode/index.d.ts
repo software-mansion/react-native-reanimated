158a4c696d9510ff627eebeb195f41b8a56721a1
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2026-03-11 15:22:47 +0100

    chore(Worklets): Bundle Mode setup improvements (#9082)
    Fixing a bunch of setup issues in Bundle Mode
    - Better expo integration with separate instructions for the Metro
    Config
    - Adding dummy file `.worklets/dummy.md` for environments where
    `.worklets` directory couldn't be created ad-hoc. Thanks to that the
    directory always exists now.
    - Removed code responsible for creating `.worklets` directory from the
    plugin.
    
    I tested it both in our app and in an expo app and the Bundle Mode
    worked 👍

--- packages/react-native-worklets/bundleMode/index.d.ts
+++ packages/react-native-worklets/bundleMode/index.d.ts
@@ -1,3 +1,6 @@
 declare module 'react-native-worklets/bundleMode' {
+  /** Use in React Native Community projects. */
   export const bundleModeMetroConfig: any;
+  /** Use in Expo projects. */
+  export function getBundleModeMetroConfig(): any;
 }
