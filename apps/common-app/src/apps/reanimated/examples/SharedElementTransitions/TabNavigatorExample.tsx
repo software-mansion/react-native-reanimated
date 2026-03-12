ef38baca3836757039bbe8668a88a2f33ac76056
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-03-12 15:37:26 +0100

    [SET] Update react navigation for correct useIsFocused timing (#9053)
    This updates React Navigation with fixes to `useIsFocused` behavior to
    avoid workarounds for SET.
    
    It also disables `detachInactiveScreens`, which seems to conflict with
    SETs. Note that it'll be removed in React Navigation 8, so this is
    temporary.
    
    
    https://github.com/user-attachments/assets/e1ba5aca-1fe2-44c3-9a50-7088079c0da2
    
    ---------
    
    Co-authored-by: Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>

--- apps/common-app/src/apps/reanimated/examples/SharedElementTransitions/TabNavigatorExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/SharedElementTransitions/TabNavigatorExample.tsx
@@ -1,11 +1,11 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useIsFocused } from '@react-navigation/native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { memo } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Animated, { SharedTransitionBoundary } from 'react-native-reanimated';
-import EmptyExample from '../EmptyExample';
+import { withSharedTransitionBoundary } from './withSharedTransitionBoundary';
+import { useIsFocused } from '@react-navigation/native';
 
 function getStyle(index: number) {
   switch (index) {
@@ -16,17 +16,6 @@ function getStyle(index: number) {
     default:
       return styles.box3;
   }
-}
-
-function RenderLogger({ id }: { id: number }) {
-  console.log('RenderLogger rendered', id);
-  return null;
-}
-
-function Clicker() {
-  console.log('Clicker clicked');
-  const [count, setCount] = React.useState(0);
-  return <Button title="Click me" onPress={() => setCount(count + 1)} />;
 }
 
 type ScreenProps = {
@@ -43,37 +32,26 @@ const ScreenContent = memo(
 
     return (
       <View style={styles.container}>
-        <RenderLogger id={id} />
-        <Clicker />
         <Text>Current id: {id}</Text>
-        {id === 1 && <EmptyExample />}
         {showButtons && id < 2 && (
           <Button
             title={`Go next ${id + 1}`}
             onPress={() => navigation.push('Details', { id: id + 1 })}
           />
         )}
-        {/* {showButtons && id > 0 && (
+        {showButtons && id > 0 && (
           <Button
             onPress={() => navigation.push('Details', { id: id - 1 })}
             title={`Go next ${id - 1}`}
           />
-        )} */}
+        )}
         <Animated.View style={getStyle(id)} sharedTransitionTag="tag" />
       </View>
     );
   }
 );
 
-function Screen({ navigation, route }: NativeStackScreenProps<ScreenProps>) {
-  const id = route.params?.id ?? 0;
-  const isActive = useIsFocused();
-  return (
-    <SharedTransitionBoundary isActive={isActive && id === 0}>
-      <ScreenContent navigation={navigation} route={route} />
-    </SharedTransitionBoundary>
-  );
-}
+const Screen = withSharedTransitionBoundary(ScreenContent);
 
 function createStack() {
   const Stack = createNativeStackNavigator();
@@ -92,9 +70,51 @@ const Tab = createBottomTabNavigator();
 const StackA = createStack();
 const StackB = createStack();
 
+function FakeScreen({
+  isActive,
+  children,
+}: {
+  isActive: boolean;
+  children: React.ReactNode;
+}) {
+  const isFocused = useIsFocused();
+  return (
+    <SharedTransitionBoundary isActive={isFocused && isActive}>
+      <View style={styles.fakeScreen}>{children}</View>
+    </SharedTransitionBoundary>
+  );
+}
+
+function ScreenWithMultipleBoundaries() {
+  const [activeBoundary, setActiveBoundary] = React.useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Switch boundary"
+        onPress={() => setActiveBoundary((activeBoundary + 1) % 3)}
+      />
+      <FakeScreen isActive={activeBoundary === 0}>
+        <Animated.View style={styles.minibox1} sharedTransitionTag="tag" />
+      </FakeScreen>
+      <FakeScreen isActive={activeBoundary === 1}>
+        <Animated.View style={styles.minibox2} sharedTransitionTag="tag" />
+      </FakeScreen>
+      <FakeScreen isActive={activeBoundary === 2}>
+        <Animated.View style={styles.minibox3} sharedTransitionTag="tag" />
+      </FakeScreen>
+    </View>
+  );
+}
+
 function TabNavigatorExample() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
+    <Tab.Navigator
+      // `react-native-screens` detaches native views for inactive screens
+      // this conflicts with shared element transitions as results in view disappearing
+      // so we disable this behavior
+      detachInactiveScreens={false}
+      screenOptions={{ headerShown: false, animation: 'none' }}>
       <Tab.Screen
         name="A"
         initialParams={{ id: 0 }}
@@ -107,6 +127,8 @@ function TabNavigatorExample() {
       />
       <Tab.Screen name="C" component={StackA} />
       <Tab.Screen name="D" component={StackB} />
+      <Tab.Screen name="E" component={ScreenWithMultipleBoundaries} />
+      <Tab.Screen name="F" component={ScreenWithMultipleBoundaries} />
     </Tab.Navigator>
   );
 }
@@ -115,6 +137,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 10,
+    alignItems: 'center',
   },
   box1: {
     width: 100,
@@ -125,13 +148,36 @@ const styles = StyleSheet.create({
     width: 200,
     height: 100,
     backgroundColor: 'blue',
-    opacity: 0,
   },
   box3: {
     width: 150,
     height: 250,
     backgroundColor: 'red',
     marginLeft: 50,
+  },
+  minibox1: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green',
+  },
+  minibox2: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'blue',
+  },
+  minibox3: {
+    width: 75,
+    height: 125,
+    backgroundColor: 'red',
+    transform: [{ rotate: '45deg' }],
+  },
+  fakeScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+    marginVertical: 10,
+    width: 200,
   },
 });
 
