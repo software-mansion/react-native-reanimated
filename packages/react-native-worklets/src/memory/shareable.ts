c3c72526a3eec84228a0eaf0702173cf69e3f320
Tomasz Żelawski <tzelawski@gmail.com>
2026-03-18 17:50:17 +0100

    feat(Worklets): shareable docs

--- packages/react-native-worklets/src/memory/shareable.ts
+++ packages/react-native-worklets/src/memory/shareable.ts
@@ -4,6 +4,20 @@ import { WorkletsError } from '../debug/WorkletsError';
 import type { UIRuntimeId } from '../runtimes';
 import type { SerializableRef, Shareable, ShareableConfig } from './types';
 
+/**
+ * Creates a new {@link Shareable} holding the provided initial value. You must
+ * explicitly declare which Worklet Runtime will host the Shareable by passing
+ * its `runtimeId`.
+ *
+ * Currently only hosting a Shareable on the UI Runtime is supported.
+ *
+ * @param hostRuntimeId - The `runtimeId` of the Worklet Runtime that will host
+ *   the Shareable. Use {@link UIRuntimeId}.
+ * @param initial - The initial value of the Shareable.
+ * @param config - Optional advanced configuration.
+ * @returns The created {@link Shareable}.
+ * @see {@link https://docs.swmansion.com/react-native-worklets/docs/memory/createShareable | createShareable docs}
+ */
 export function createShareable<
   TValue = unknown,
   THostDecorated = unknown,
