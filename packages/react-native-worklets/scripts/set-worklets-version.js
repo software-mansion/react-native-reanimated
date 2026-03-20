c3c5877aebbdc9b654afc998fbf80801a994bf2f
Tomasz Żelawski <tzelawski@gmail.com>
2026-03-20 16:44:42 +0100

    chore: test OIDC

--- packages/react-native-worklets/scripts/set-worklets-version.js
+++ packages/react-native-worklets/scripts/set-worklets-version.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const getVersion = require('../../../scripts/releasing').getVersion;
+const { getVersion } = require('../../../scripts/releasing');
 const { updateVersion } = require('../../../scripts/version-utils');
 
 const packageJsonPath = path.resolve(__dirname, '../package.json');
 const jsVersionPath = path.resolve(__dirname, '../src/debug/jsVersion.ts');
 
-const { currentVersion, newVersion } = getVersion(
-  process.argv,
-  packageJsonPath
-);
+if (!process.argv.includes('--package-json-path')) {
+  process.argv.push('--package-json-path', packageJsonPath);
+}
+
+const { currentVersion, newVersion } = getVersion(process.argv);
 
 updateVersion(packageJsonPath, jsVersionPath, newVersion);
 
