8a1ce1cd657b68b84958b302c1ac999f6174a995
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2026-03-25 16:55:32 +0100

    fix(Reanimated): RCTModernEventEmmitter for older RN (#9181)
    ## Summary
    
    Turns out that React Native 0.85 doesn't require these methods
    implemented but React Native 0.84 and less does, we have to add them
    back for backwards compatibility.
    
    ## Test plan
    
    Android compilation now passes on RN 0.84

--- packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/nativeProxy/NoopEventHandler.java
+++ packages/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/nativeProxy/NoopEventHandler.java
@@ -2,6 +2,7 @@ package com.swmansion.reanimated.nativeProxy;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTModernEventEmitter;
 
@@ -15,6 +16,25 @@ public class NoopEventHandler implements RCTModernEventEmitter {
       int customCoalesceKey,
       @Nullable WritableMap params,
       int category) {
-    // NOOP
+    // noop
+  }
+
+  @Override
+  public void receiveEvent(
+      int surfaceId, int targetTag, @NonNull String eventName, @Nullable WritableMap params) {
+    // noop
+  }
+
+  @Override
+  public void receiveEvent(int targetTag, @NonNull String eventName, @Nullable WritableMap params) {
+    // noop
+  }
+
+  @Override
+  public void receiveTouches(
+      @NonNull String eventName,
+      @NonNull WritableArray touches,
+      @NonNull WritableArray changedIndices) {
+    // noop
   }
 }
