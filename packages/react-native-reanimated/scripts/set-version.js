119706299208ea95824b54a7c6fe579c8c14cb8a
Tomasz Żelawski <tzelawski@gmail.com>
2026-04-02 15:30:00 +0200

    fix: stale scripts

--- packages/react-native-reanimated/scripts/set-version.js
+++ packages/react-native-reanimated/scripts/set-version.js
@@ -2,6 +2,9 @@ const path = require('path');
 const { setVersion } = require('../../../scripts/set-version');
 
 const packageJsonPath = path.resolve(__dirname, '../package.json');
-const jsVersionPath = path.resolve(__dirname, '../src/debug/jsVersion.ts');
+const jsVersionPath = path.resolve(
+  __dirname,
+  '../src/platform-specific/jsVersion.ts'
+);
 
 setVersion(packageJsonPath, jsVersionPath);
---
+++ scripts/set-version.js
@@ -0,0 +1,18 @@
+const { getVersion } = require('./releasing');
+const { updateVersion } = require('./version-utils');
+
+/**
+ * @param {string} packageJsonPath
+ * @param {string} jsVersionPath
+ */
+function setVersion(packageJsonPath, jsVersionPath) {
+  if (!process.argv.includes('--package-json-path')) {
+    process.argv.push('--package-json-path', packageJsonPath);
+  }
+  const { currentVersion, newVersion } = getVersion(process.argv);
+  updateVersion(packageJsonPath, jsVersionPath, newVersion);
+  // Log the current version so it can be restored if needed.
+  process.stdout.write(currentVersion);
+}
+
+export { setVersion };
