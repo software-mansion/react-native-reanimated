6a4643d3edbbb263bbe42431dd100aa153da670b
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-04-13 11:17:02 +0200

    Fix lint

--- apps/common-app/src/apps/reanimated/examples/RuntimeTests/ReJest/TestRunner/UpdatesContainer.ts
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/ReJest/TestRunner/UpdatesContainer.ts
@@ -111,10 +111,7 @@ export function createUpdatesContainer() {
         update = 'update' in updateRequest ? updateRequest.update : updateRequest.snapshot;
       } else {
         for (const prop of propsNames) {
-          update[prop] =
-            'update' in updateRequest
-              ? updateRequest.update[prop as keyof OperationUpdate]
-              : updateRequest.snapshot[prop as keyof OperationUpdate];
+          update[prop] = 'update' in updateRequest ? updateRequest.update[prop] : updateRequest.snapshot[prop];
         }
       }
       updatesForTag[tag].push(update);
