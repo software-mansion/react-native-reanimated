f73ed604edcdc460120622938035b0287d1af5d1
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2026-04-20 10:20:21 +0200

    Convert easing functions to easing configs

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/configs/CSSAnimationConfig.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/configs/CSSAnimationConfig.cpp
@@ -59,7 +59,7 @@ CSSAnimationSettings parseCSSAnimationSettings(jsi::Runtime &rt, const jsi::Valu
 
   return {
       getDuration(rt, settingsObj),
-      getTimingFunction(rt, settingsObj),
+      getEasingConfig(rt, settingsObj),
       getDelay(rt, settingsObj),
       getIterationCount(rt, settingsObj),
       getDirection(rt, settingsObj),
@@ -76,7 +76,7 @@ PartialCSSAnimationSettings parsePartialCSSAnimationSettings(jsi::Runtime &rt, c
     result.duration = getDuration(rt, partialObj);
   }
   if (partialObj.hasProperty(rt, "timingFunction")) {
-    result.easingFunction = getTimingFunction(rt, partialObj);
+    result.easingConfig = getEasingConfig(rt, partialObj);
   }
   if (partialObj.hasProperty(rt, "delay")) {
     result.delay = getDelay(rt, partialObj);
