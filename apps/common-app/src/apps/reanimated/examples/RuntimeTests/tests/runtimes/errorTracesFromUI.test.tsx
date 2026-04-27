1420579d08b304edbfc29ce5353fc12e37b20210
tshmieldev <wojtekrok88@gmail.com>
2026-04-27 10:13:48 +0200

    added tests

---
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/runtimes/errorTracesFromUI.test.tsx
@@ -0,0 +1,117 @@
+import {
+  describe,
+  test,
+  beforeEach,
+  afterEach,
+  expect,
+  notify,
+  waitForNotification,
+} from '../../ReJest/RuntimeTestsApi';
+import {
+  runOnUISync,
+  scheduleOnUI,
+  scheduleOnRuntime,
+  createWorkletRuntime,
+  runOnRuntimeSync,
+} from 'react-native-worklets';
+
+const originalReportFatalRemoteError = globalThis.__reportFatalRemoteError;
+
+describe('Error traces from UI', () => {
+  let errorData = null;
+  const testRuntime = createWorkletRuntime({
+    name: 'testRuntime',
+  });
+
+  test('setup beforeEach and afterEach', () => {
+    // TODO: there's a bug in ReJest and beforeEach/afterEach have to be
+    // registered inside a test case.
+
+    beforeEach(() => {
+      globalThis.__reportFatalRemoteError = (a, b) => {
+        errorData = a;
+        notify('errorReported');
+      };
+    });
+
+    afterEach(() => {
+      globalThis.__reportFatalRemoteError = originalReportFatalRemoteError;
+    });
+  });
+
+  test('[UI] tag gets added to stack trace', () => {
+    const badFn = (callback: () => void) => {
+      'worklet';
+      callback();
+    };
+    runOnUISync(badFn, () => {
+      'worklet';
+      throw new Error();
+    });
+
+    expect(errorData.stack.includes('at [UI]:')).toBe(true);
+  });
+
+  test('scheduleOnUI has good stack trace added', async () => {
+    const badFn = (callback: () => void) => {
+      'worklet';
+      callback();
+    };
+    scheduleOnUI(badFn, function functionNameA() {
+      'worklet';
+      throw new Error();
+    });
+
+    await waitForNotification('errorReported');
+    expect(errorData.stack.includes('at [UI]:')).toBe(true);
+    expect(errorData.stack.includes('at [UI]: functionNameA')).toBe(true);
+  });
+
+  test('scheduleOnRuntime has good stack trace added', async () => {
+    const badFn = (callback: () => void) => {
+      'worklet';
+      callback();
+    };
+
+    scheduleOnRuntime(testRuntime, badFn, function functionNameB() {
+      'worklet';
+      throw new Error();
+    });
+
+    await waitForNotification('errorReported');
+    expect(errorData.stack.includes('at [testRuntime]:')).toBe(true);
+    expect(errorData.stack.includes('at [testRuntime]: functionNameB')).toBe(true);
+  });
+
+  test('runOnUISync has good stack trace added', async () => {
+    const badFn = (callback: () => void) => {
+      'worklet';
+      callback();
+    };
+
+    runOnUISync(badFn, function functionNameC() {
+      'worklet';
+      throw new Error();
+    });
+
+    await waitForNotification('errorReported');
+    expect(errorData.stack.includes('at [UI]:')).toBe(true);
+    expect(errorData.stack.includes('at [UI]: functionNameC')).toBe(true);
+  });
+
+  test('runOnRuntimeSync has good stack trace added', async () => {
+    const badFn = (callback: () => void) => {
+      'worklet';
+      callback();
+    };
+
+    runOnRuntimeSync(testRuntime, badFn, function functionNameD() {
+      'worklet';
+      throw new Error();
+    });
+
+    await waitForNotification('errorReported');
+    expect(errorData.stack.includes('at [testRuntime]:')).toBe(true);
+    expect(errorData.stack.includes('at [testRuntime]: functionNameD')).toBe(true);
+  });
+});
