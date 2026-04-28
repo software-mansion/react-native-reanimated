2f0579a7389bef217867b079f4464e723051618f
tshmieldev <wojtekrok88@gmail.com>
2026-04-28 11:18:43 +0200

    removed prettier dep, moved root config to docs as only they need it

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-  bracketSameLine: true,
-  printWidth: 80,
-  singleQuote: true,
-  trailingComma: 'es5',
-  tabWidth: 2,
-  arrowParens: 'always',
-};
