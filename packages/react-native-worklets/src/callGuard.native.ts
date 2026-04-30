d256aa575f34177894036bdb13ee5922de90a9d6
Wojciech Rok <58606210+tshmieldev@users.noreply.github.com>
2026-04-30 12:41:48 +0200

    Add RN runtime error traces to errors from UI runtime (#9280)
    ## Summary
    Added RN runtime error traces to errors from UI runtime by passing new
    Error.stack to scheduling functions, to JSI and back to JS
    
    ## Test plan
    
    see the modified EmptyExample, see if the RN frames are in the error
    output

--- packages/react-native-worklets/src/callGuard.native.ts
+++ packages/react-native-worklets/src/callGuard.native.ts
@@ -3,6 +3,7 @@
 /** Used only with debug builds. */
 export function callGuardDEV<Args extends unknown[], ReturnValue>(
   fn: (...args: Args) => ReturnValue,
+  scheduleStack: string | undefined,
   ...args: Args
 ): ReturnValue | void {
   'worklet';
@@ -11,9 +12,23 @@ export function callGuardDEV<Args extends unknown[], ReturnValue>(
   } catch (error) {
     if (globalThis.__workletsModuleProxy) {
       const { message, stack, name } = error as Error;
+
+      const label = (globalThis as unknown as Record<string, string>)._LABEL;
+      const labeledWorkletFrames = (stack ?? '')
+        .split('\n    at')
+        .map((line) => `\n    at [${label}]:` + line)
+        .slice(1) // remove error message
+        .join('');
+
+      const scheduleFrames = scheduleStack
+        ? scheduleStack.substring(scheduleStack.indexOf('\n    at'))
+        : '';
+
+      const combinedStack = message + labeledWorkletFrames + scheduleFrames;
+
       globalThis.__workletsModuleProxy.reportFatalErrorOnJS(
         message,
-        stack ?? '',
+        combinedStack,
         name ?? 'WorkletsError'
       );
     } else {
