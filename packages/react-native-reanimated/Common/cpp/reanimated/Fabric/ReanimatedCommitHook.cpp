5d1bcf9e63d15c4cf6a31ba08c0782799df41501
Bartłomiej Błoniarz <bartlomiej.bloniarz@gmail.com>
2026-05-06 11:04:21 +0200

    No more RN 0.80

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.cpp
@@ -70,7 +70,6 @@ RootShadowNode::Unshared ReanimatedCommitHook::shadowTreeWillCommit(
       return newRootShadowNode;
     }
 
-#if REACT_NATIVE_MINOR_VERSION >= 80
     if constexpr (StaticFeatureFlags::getFlag("USE_COMMIT_HOOK_ONLY_FOR_REACT_COMMITS")) {
       // State updates are based on the currently committed ShadowTree,
       // which means that all animation changes are already included.
@@ -79,7 +78,6 @@ RootShadowNode::Unshared ReanimatedCommitHook::shadowTreeWillCommit(
         return newRootShadowNode;
       }
     }
-#endif
 
     // ShadowTree not commited by Reanimated, apply updates from the updates
     // registry manager
