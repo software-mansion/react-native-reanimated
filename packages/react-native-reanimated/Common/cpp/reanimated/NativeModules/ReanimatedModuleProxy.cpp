6dd67293b8c394aedc2512e8d1e696c4ae2cc963
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2026-05-06 15:05:24 +0200

    fix(Reanimated): registries race conditions (#9323)
    Supersedes #9306
    
    Fixes #9293
    
    ---
    
    I'm fixing race-conditions that occur within UpdateRegistry and its
    derivatives.
    
    Now:
    
    - You can no longer lock a registry imperatively.
    - Each registry public method obtains the lock exclusively.
    - Each registry protected and private method doesn't lock the registry.
    It's the responsibility of the public method - this way we don't have to
    use `std::recursive_mutex`.
    
    The following code would throw after some time:
    
    <details><summary>Code</summary>
    <p>
    
    ```tsx
    import React from 'react';
    import { StyleSheet } from 'react-native';
    import { ScrollView } from 'react-native-gesture-handler';
    import Animated, {
      useAnimatedStyle,
      useSharedValue,
      withTiming,
    } from 'react-native-reanimated';
    
    export default function Repro() {
      return (
        <ScrollView contentContainerStyle={styles.container}>
          {new Array(1024).fill(0).map((_, i) => (
            <AnimatorWrapper key={i} />
          ))}
        </ScrollView>
      );
    }
    
    function AnimatorWrapper() {
      const [display, setDisplay] = React.useState(true);
    
      React.useEffect(() => {
        const timeout = setInterval(
          () => {
            setDisplay((display) => !display);
          },
          Math.random() * 500 + 300
        );
        return () => clearInterval(timeout);
      }, []);
    
      if (!display) {
        return null;
      }
    
      return <Animator />;
    }
    
    function Animator() {
      const opacitySv = useSharedValue(0);
      const widthSv = useSharedValue(10);
      const heightSv = useSharedValue(10);
    
      React.useEffect(() => {
        opacitySv.value = withTiming(1, { duration: 100 });
        widthSv.value = withTiming(100, { duration: 100 });
        heightSv.value = withTiming(20, { duration: 100 });
      }, [opacitySv, widthSv, heightSv]);
    
      const animatedStyle = useAnimatedStyle(() => {
        return {
          opacity: opacitySv.value,
          width: widthSv.value,
          height: heightSv.value,
        };
      });
    
      return <Animated.View style={[styles.box, animatedStyle]} />;
    }
    
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'center',
        gap: 2,
      },
      box: {
        backgroundColor: 'blue',
      },
    });
    ```
    
    </p>
    </details>
    
    ---------
    
    Co-authored-by: Mateusz Łopaciński <lop.mateusz.2001@gmail.com>

--- packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/NativeModules/ReanimatedModuleProxy.cpp
@@ -493,17 +493,13 @@ void ReanimatedModuleProxy::applyCSSAnimations(
     }
   }
 
-  {
-    auto lock = cssAnimationsRegistry_->lock();
-    cssAnimationsRegistry_->apply(
-        rt, shadowNode, updates.animationNames, newAnimations, updates.settingsUpdates, timestamp);
-  }
+  cssAnimationsRegistry_->apply(
+      rt, shadowNode, updates.animationNames, newAnimations, updates.settingsUpdates, timestamp);
 
   maybeRunCSSLoop();
 }
 
 void ReanimatedModuleProxy::unregisterCSSAnimations(const jsi::Value &viewTag) {
-  auto lock = cssAnimationsRegistry_->lock();
   cssAnimationsRegistry_->remove(viewTag.asNumber());
 }
 
@@ -514,16 +510,12 @@ void ReanimatedModuleProxy::runCSSTransition(
   auto shadowNode = shadowNodeFromValue(rt, shadowNodeWrapper);
   const auto config = parseCSSTransitionConfig(rt, transitionConfig);
 
-  {
-    auto lock = cssTransitionsRegistry_->lock();
-    cssTransitionsRegistry_->run(rt, shadowNode, config);
-  }
+  cssTransitionsRegistry_->run(rt, shadowNode, config);
 
   maybeRunCSSLoop();
 }
 
 void ReanimatedModuleProxy::unregisterCSSTransition(jsi::Runtime &rt, const jsi::Value &viewTag) {
-  auto lock = cssTransitionsRegistry_->lock();
   cssTransitionsRegistry_->remove(viewTag.asNumber());
 }
 
@@ -535,15 +527,12 @@ jsi::Value ReanimatedModuleProxy::getSettledUpdates(jsi::Runtime &rt) {
   // TODO(future): use unified timestamp
   const auto currentTimestamp = getAnimationTimestamp_();
 
-  const auto lock = animatedPropsRegistry_->lock();
-
   // TODO: fix bug when threshold difference is smaller than 1 second
   // TODO(future): flush updates from CSS animations and CSS transitions registries
-  animatedPropsRegistry_->removeUpdatesOlderThanTimestamp(currentTimestamp - 2000); // 2 seconds
-
   // TODO(future): find a better way to obtain timestamp for removing updates
   // TODO(future): move removing old updates to separate method
-  return animatedPropsRegistry_->getUpdatesOlderThanTimestamp(rt, currentTimestamp - 1000); // 1 second
+  return animatedPropsRegistry_->getUpdatesOlderThanTimestamp(
+      rt, currentTimestamp - 1000 /* 1 second */, currentTimestamp - 2000 /* 2 seconds */);
 }
 
 bool ReanimatedModuleProxy::handleEvent(
@@ -694,24 +683,17 @@ void ReanimatedModuleProxy::performOperations() {
     auto lock = updatesRegistryManager_->lock();
 
     if (shouldUpdateCssAnimations_) {
-      currentCssTimestamp_ = getAnimationTimestamp_();
-      auto lock = cssTransitionsRegistry_->lock();
       // Update CSS transitions and flush updates
-      cssTransitionsRegistry_->update(currentCssTimestamp_);
-      cssTransitionsRegistry_->flushUpdates(updatesBatch);
+      currentCssTimestamp_ = getAnimationTimestamp_();
+      cssTransitionsRegistry_->updateAndFlush(currentCssTimestamp_, updatesBatch);
     }
 
-    {
-      auto lock = animatedPropsRegistry_->lock();
-      // Flush all animated props updates
-      animatedPropsRegistry_->flushUpdates(updatesBatch);
-    }
+    // Flush all animated props updates
+    animatedPropsRegistry_->flushUpdates(updatesBatch);
 
     if (shouldUpdateCssAnimations_) {
-      auto lock = cssAnimationsRegistry_->lock();
       // Update CSS animations and flush updates
-      cssAnimationsRegistry_->update(currentCssTimestamp_);
-      cssAnimationsRegistry_->flushUpdates(updatesBatch);
+      cssAnimationsRegistry_->updateAndFlush(currentCssTimestamp_, updatesBatch);
     }
 
     shouldUpdateCssAnimations_ = false;
