1ec052d1396326acb225b3b03e633208977c0236
tshmieldev <wojtekrok88@gmail.com>
2026-05-06 17:08:16 +0200

    indent fix

--- packages/react-native-worklets/Common/cpp/worklets/NativeModules/JSIWorkletsModuleProxy.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/NativeModules/JSIWorkletsModuleProxy.cpp
@@ -76,16 +76,16 @@ inline void scheduleOnUI(
       return;
     }
 
-    for (size_t i = 0; i < worklets.size(); i++) {
 #if JS_RUNTIME_HERMES
-      // JSI's scope defined here allows for JSI-objects to be cleared up
-      // after each runtime loop. Within these loops we typically create
-      // some temporary JSI objects and hence it allows for such objects to
-      // be garbage collected much sooner. Apparently the scope API is only
-      // supported on Hermes at the moment.
-      const auto scope = jsi::Scope(uiWorkletRuntime->getJSIRuntime());
+    // JSI's scope defined here allows for JSI-objects to be cleared up
+    // after each runtime loop. Within these loops we typically create
+    // some temporary JSI objects and hence it allows for such objects to
+    // be garbage collected much sooner. Apparently the scope API is only
+    // supported on Hermes at the moment.
+    const auto scope = jsi::Scope(uiWorkletRuntime->getJSIRuntime());
 #endif // JS_RUNTIME_HERMES
 
+    for (size_t i = 0; i < worklets.size(); i++) {
 #ifndef NDEBUG
       uiWorkletRuntime->runSyncWithStack(worklets[i], scheduleStacks[i]);
 #else
