c879a8dcd5ce24c8c5a387f2f80bfa55eb3cf096
tshmieldev <wojtekrok88@gmail.com>
2026-05-08 14:22:50 +0200

    nits

--- docs/docs-reanimated/sidebars.js
+++ docs/docs-reanimated/sidebars.js
@@ -1,6 +1,9 @@
 /**
- * Creating a sidebar enables you to: - create an ordered group of docs - render
- * a sidebar for each doc of that group - provide next/previous navigation
+ * Creating a sidebar enables you to:
+ *
+ * - create an ordered group of docs
+ * - render a sidebar for each doc of that group
+ * - provide next/previous navigation
  *
  * The sidebars can be generated from the filesystem, or explicitly defined
  * here.
