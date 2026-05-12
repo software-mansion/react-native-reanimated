17e0eee7278291a6e09c134e0537c3d11d8915ce
Tomasz Żelawski <tzelawski@gmail.com>
2026-05-12 14:27:44 +0200

    feat: tests

--- apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/runtimes/scheduleOnRN.test.tsx
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/runtimes/scheduleOnRN.test.tsx
@@ -16,12 +16,19 @@ import {
 describe('scheduleOnRN', () => {
   const PASS_NOTIFICATION = 'PASS';
   let value = 0;
+  const FAIL_NOTIFICATION = 'FAIL';
+  let errorMessage = '';
 
   const workletRuntime = createWorkletRuntime({ name: 'test' });
 
   const callbackPass = (num: number) => {
     value = num;
     notify(PASS_NOTIFICATION);
+  };
+
+  const callbackFail = (message: string) => {
+    errorMessage = message;
+    notify(FAIL_NOTIFICATION);
   };
 
   beforeEach(() => {
@@ -54,5 +61,71 @@ describe('scheduleOnRN', () => {
 
     await waitForNotification(PASS_NOTIFICATION);
     expect(value).toBe(42);
+  });
+
+  test('throws actionable error when a locally defined anonymous function is passed to scheduleOnRN on UI Runtime', async () => {
+    scheduleOnUI(() => {
+      'worklet';
+      try {
+        scheduleOnRN(() => {});
+      } catch (error) {
+        scheduleOnRN(callbackFail, (error as Error).message);
+      }
+    });
+
+    await waitForNotification(FAIL_NOTIFICATION);
+    expect(errorMessage).toInclude(
+      'Locally defined function passed to scheduleOnRN'
+    );
+  });
+
+  test('throws actionable error when a locally defined named function is passed to scheduleOnRN on UI Runtime', async () => {
+    scheduleOnUI(() => {
+      'worklet';
+      try {
+        scheduleOnRN(function fooFunction() {});
+      } catch (error) {
+        scheduleOnRN(callbackFail, (error as Error).message);
+      }
+    });
+
+    await waitForNotification(FAIL_NOTIFICATION);
+    expect(errorMessage).toInclude(
+      'Locally defined function passed to scheduleOnRN'
+    );
+    expect(errorMessage).toInclude('fooFunction');
+  });
+
+  test('throws actionable error when a locally defined function is passed to scheduleOnRN on Worker Runtime', async () => {
+    scheduleOnRuntime(workletRuntime, () => {
+      'worklet';
+      try {
+        scheduleOnRN(() => {});
+      } catch (error) {
+        scheduleOnRN(callbackFail, (error as Error).message);
+      }
+    });
+
+    await waitForNotification(FAIL_NOTIFICATION);
+    expect(errorMessage).toInclude(
+      'Locally defined function passed to scheduleOnRN'
+    );
+  });
+
+  test('throws actionable error when a locally defined named function is passed to scheduleOnRN on Worker Runtime', async () => {
+    scheduleOnRuntime(workletRuntime, () => {
+      'worklet';
+      try {
+        scheduleOnRN(function fooFunction() {});
+      } catch (error) {
+        scheduleOnRN(callbackFail, (error as Error).message);
+      }
+    });
+
+    await waitForNotification(FAIL_NOTIFICATION);
+    expect(errorMessage).toInclude(
+      'Locally defined function passed to scheduleOnRN'
+    );
+    expect(errorMessage).toInclude('fooFunction');
   });
 });
