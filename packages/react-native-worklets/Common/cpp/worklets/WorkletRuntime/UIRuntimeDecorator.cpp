da846be128d9f10e2efc5497ada1906bf56b9517
Bartłomiej Błoniarz <bartlomiej.bloniarz@gmail.com>
2026-05-14 13:01:47 +0200

    huh

--- packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/UIRuntimeDecorator.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/UIRuntimeDecorator.cpp
@@ -8,11 +8,6 @@ void UIRuntimeDecorator::decorate(
     facebook::jsi::Runtime &uiRuntime,
     const std::function<void(facebook::jsi::Runtime &rt, const facebook::jsi::Value &callback)>
         &requestAnimationFrame) {
-  uiRuntime.global().setProperty(
-      uiRuntime, RuntimeData::runtimeKindBindingName, static_cast<int>(RuntimeData::RuntimeKind::UI));
-
-  uiRuntime.global().setProperty(uiRuntime, "_UI", true);
-
   installRequestAnimationFrame(uiRuntime, requestAnimationFrame);
 }
 
