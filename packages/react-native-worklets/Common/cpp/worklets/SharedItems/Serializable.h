software-mansion/react-native-reanimated

commit fc9fad2739e777b3f0b54f49452ec976097e2ac1

feat(Worklets): actionable error when passing local function to `scheduleOnRN` (#9386)
## Summary

Addressing a long overdue issue, where passing a locally defined
function to `scheduleOnRN` would crash the app with a vague stack trace.

This PR adds an actionable error in such cases. I had to refactor a
bunch of code for that but even more changes are coming in #9272.

<table>
	<th>Before (straight crash)</th>
	<th>After (scrumptious LogBox)</th>
	<tr>
		<td>
		
<img width="1247" height="1030" alt="Screenshot 2026-05-12 at 13 14 04"
src="https://github.com/user-attachments/assets/3a6c9545-614c-4499-bbf7-e43f4b512147"
/>
	 
		</td>
		<td>
<img width="1206" height="2622" alt="Simulator Screenshot - iPhone 17 -
2026-05-12 at 14 06 25"
src="https://github.com/user-attachments/assets/c9cebab9-d0fd-48d9-a2e9-f9dfcb67033e"
/>
		</td>
	</tr>
</table>

## Test plan

Added new runtime tests to cover this case.

packages/react-native-worklets/Common/cpp/worklets/SharedItems/Serializable.h
@@ -92,23 +92,45 @@ class SerializableJSRef : public jsi::NativeState {
   }
 };
 
+[[nodiscard]]
 jsi::Value makeSerializableClone(
     jsi::Runtime &rt,
     const jsi::Value &value,
     const jsi::Value &shouldRetainRemote,
     const jsi::Value &nativeStateSource);
 
+[[nodiscard]]
 std::shared_ptr<Serializable> extractSerializableOrThrow(
     jsi::Runtime &rt,
     const jsi::Value &maybeSerializableValue,
     const std::string &errorMessage = "[Worklets] Expecting the object to be of type SerializableJSRef.");
 
-template <typename T>
-std::shared_ptr<T> extractSerializableOrThrow(
+[[nodiscard]]
+std::shared_ptr<Serializable> extractSerializableOrThrow(
+    jsi::Runtime &rt,
+    const jsi::Object &maybeSerializableValue,
+    const std::string &errorMessage = "[Worklets] Expecting the object to be of type SerializableJSRef.");
+
+template <typename TSerializable>
+[[nodiscard]]
+std::shared_ptr<TSerializable> extractSerializableOrThrow(
     jsi::Runtime &rt,
     const jsi::Value &serializableRef,
     const std::string &errorMessage = "[Worklets] Provided serializable object is of an incompatible type.") {
-  auto res = std::dynamic_pointer_cast<T>(extractSerializableOrThrow(rt, serializableRef, errorMessage));
+  auto res = std::dynamic_pointer_cast<TSerializable>(extractSerializableOrThrow(rt, serializableRef, errorMessage));
+  if (!res) {
+    throw std::runtime_error(errorMessage);
+  }
+  return res;
+}
+
+template <typename TSerializable>
+[[nodiscard]]
+std::shared_ptr<TSerializable> extractSerializableOrThrow(
+    jsi::Runtime &rt,
+    const jsi::Object &serializableRef,
+    const std::string &errorMessage = "[Worklets] Provided serializable object is of an incompatible type.") {
+  auto res = std::dynamic_pointer_cast<TSerializable>(extractSerializableOrThrow(rt, serializableRef, errorMessage));
   if (!res) {
     throw std::runtime_error(errorMessage);
   }
@@ -120,6 +142,15 @@ class SerializableArray : public Serializable {
   SerializableArray(jsi::Runtime &rt, const jsi::Array &array);
 
   jsi::Value toJSValue(jsi::Runtime &rt) override;
+
+  std::vector<jsi::Value> getJSIValueArr(jsi::Runtime &rt) {
+    std::vector<jsi::Value> args;
+    args.reserve(data_.size());
+    for (const auto &item : data_) {
+      args.push_back(item->toJSValue(rt));
+    }
+    return args;
+  }
 
   [[nodiscard]] const std::vector<std::shared_ptr<Serializable>> &getList() const {
     return data_;
@@ -175,11 +206,8 @@ class SerializableHostObject : public Serializable {
 
 class SerializableHostFunction : public Serializable {
  public:
-  SerializableHostFunction(jsi::Runtime &rt, jsi::Function function)
-      : Serializable(ValueType::HostFunctionType),
-        hostFunction_(function.getHostFunction(rt)),
-        name_(function.getProperty(rt, "name").asString(rt).utf8(rt)),
-        paramCount_(function.getProperty(rt, "length").asNumber()) {}
+  SerializableHostFunction(const jsi::HostFunctionType &function, const std::string &name, unsigned int paramCount)
+      : Serializable(ValueType::HostFunctionType), hostFunction_(function), name_(name), paramCount_(paramCount) {}
 
   jsi::Value toJSValue(jsi::Runtime &rt) override;
 
@@ -232,11 +260,17 @@ class SerializableRemoteFunction : public Serializable,
   std::unique_ptr<jsi::Value> function_;
 
  public:
-  SerializableRemoteFunction(jsi::Runtime &rt, jsi::Function &&function)
+  SerializableRemoteFunction(
+      jsi::Runtime &rt,
+      jsi::Function &&function,
+#ifndef NDEBUG
+      const std::string &name
+#endif
+      )
       : Serializable(ValueType::RemoteFunctionType),
         runtime_(&rt),
 #ifndef NDEBUG
-        name_(function.getProperty(rt, "name").asString(rt).utf8(rt)),
+        name_(name),
 #endif
         function_(std::make_unique<jsi::Value>(rt, std::move(function))) {
   }

packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableFactory.cpp
@@ -51,13 +51,29 @@ jsi::Value makeSerializableInitializer(jsi::Runtime &rt, const jsi::Object &init
   return SerializableJSRef::newNativeStateObject(rt, serializable);
 }
 
-jsi::Value makeSerializableFunction(jsi::Runtime &rt, jsi::Function function) {
-  std::shared_ptr<Serializable> serializable;
-  if (function.isHostFunction(rt)) {
-    serializable = std::make_shared<SerializableHostFunction>(rt, std::move(function));
-  } else {
-    serializable = std::make_shared<SerializableRemoteFunction>(rt, std::move(function));
-  }
+jsi::Value makeSerializableHostFunction(
+    jsi::Runtime &rt,
+    const jsi::HostFunctionType &function,
+    const std::string &name,
+    unsigned int paramCount) {
+  auto serializable = std::make_shared<SerializableHostFunction>(function, name, paramCount);
+  return SerializableJSRef::newNativeStateObject(rt, serializable);
+}
+
+jsi::Value makeSerializableRemoteFunction(
+    jsi::Runtime &rt,
+    jsi::Function function,
+#ifndef NDEBUG
+    const std::string &name
+#endif
+) {
+  auto serializable = std::make_shared<SerializableRemoteFunction>(
+      rt,
+      std::move(function),
+#ifndef NDEBUG
+      name
+#endif
+  );
   return SerializableJSRef::newNativeStateObject(rt, serializable);
 }
 

packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableFactory.h
@@ -43,7 +43,19 @@ jsi::Value makeSerializableSet(jsi::Runtime &rt, const jsi::Array &values);
 
 jsi::Value makeSerializableInitializer(jsi::Runtime &rt, const jsi::Object &initializerObject);
 
-jsi::Value makeSerializableFunction(jsi::Runtime &rt, jsi::Function function);
+jsi::Value makeSerializableHostFunction(
+    jsi::Runtime &rt,
+    const jsi::HostFunctionType &function,
+    const std::string &name,
+    unsigned int paramCount);
+
+jsi::Value makeSerializableRemoteFunction(
+    jsi::Runtime &rt,
+    jsi::Function function,
+#ifndef NDEBUG
+    const std::string &name
+#endif
+);
 
 jsi::Value makeSerializableWorklet(jsi::Runtime &rt, const jsi::Object &object, const bool &shouldRetainRemote);
 

apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/runtimes/scheduleOnRN.test.tsx
@@ -16,6 +16,8 @@ import {
 describe('scheduleOnRN', () => {
   const PASS_NOTIFICATION = 'PASS';
   let value = 0;
+  const FAIL_NOTIFICATION = 'FAIL';
+  let errorMessage = '';
 
   const workletRuntime = getWorkletRuntimeFromPool('test');
 
@@ -24,35 +26,108 @@ describe('scheduleOnRN', () => {
     notify(PASS_NOTIFICATION);
   };
 
+  const callbackFail = (message: string) => {
+    errorMessage = message;
+    notify(FAIL_NOTIFICATION);
+  };
+
+  const rnSource = {
+    name: 'RN',
+    scheduleOnTarget: (fn: (...args: unknown[]) => void, ...args: unknown[]) =>
+      fn(...args),
+  };
+
+  const workletSources = [
+    {
+      name: 'UI',
+      scheduleOnTarget: scheduleOnUI,
+    },
+    {
+      name: 'Worker',
+      scheduleOnTarget: (
+        fn: (...args: unknown[]) => void,
+        ...args: unknown[]
+      ) => scheduleOnRuntime(workletRuntime, fn, ...args),
+    },
+  ];
+
+  const allSources = [rnSource, ...workletSources];
+
   beforeEach(() => {
     value = 0;
+    errorMessage = '';
   });
 
-  test('schedules on RN Runtime to RN Runtime', async () => {
-    scheduleOnRN(callbackPass, 42);
+  allSources.forEach(({ name, scheduleOnTarget }) => {
+    test(`schedules remoteFunction from ${name} Runtime to RN Runtime`, async () => {
+      scheduleOnTarget(() => {
+        'worklet';
+        scheduleOnRN(callbackPass, 42);
+      });
 
-    await waitForNotification(PASS_NOTIFICATION);
-    expect(value).toBe(42);
+      await waitForNotification(PASS_NOTIFICATION);
+      expect(value).toBe(42);
+    });
   });
 
-  test('schedules on UI Runtime to RN Runtime', async () => {
-    scheduleOnUI(() => {
-      'worklet';
-      scheduleOnRN(callbackPass, 42);
+  allSources.forEach(({ name, scheduleOnTarget }) =>
+    test(`schedules hostFunction from ${name} Runtime to RN Runtime`, async () => {
+      scheduleOnTarget(() => {
+        'worklet';
+        const hostFunction = globalThis.__workletsModuleProxy
+          .createSerializableBoolean as (value: boolean) => void;
+        scheduleOnRN(hostFunction, true);
+        scheduleOnRN(callbackPass, 42);
+      });
+
+      await waitForNotification(PASS_NOTIFICATION);
+      expect(value).toBe(42);
+    })
+  );
+
+  test('schedules locally defined function from RN Runtime to RN Runtime without errors', async () => {
+    scheduleOnRN(() => {
+      scheduleOnRN(() => callbackPass(42));
     });
 
     await waitForNotification(PASS_NOTIFICATION);
     expect(value).toBe(42);
   });
 
-  test('schedules on Worker Runtime to RN Runtime', async () => {
-    scheduleOnRuntime(workletRuntime, () => {
-      'worklet';
+  workletSources.forEach(({ name, scheduleOnTarget }) =>
+    test(`throws actionable error when a locally defined anonymous function is passed to scheduleOnRN on ${name} Runtime`, async () => {
+      scheduleOnTarget(() => {
+        'worklet';
+        try {
+          scheduleOnRN(() => {});
+        } catch (error) {
+          scheduleOnRN(callbackFail, (error as Error).message);
+        }
+      });
 
-      scheduleOnRN(callbackPass, 42);
-    });
+      await waitForNotification(FAIL_NOTIFICATION);
+      expect(errorMessage).toInclude(
+        'Locally defined function passed to scheduleOnRN'
+      );
+    })
+  );
 
-    await waitForNotification(PASS_NOTIFICATION);
-    expect(value).toBe(42);
-  });
+  workletSources.forEach(({ name, scheduleOnTarget }) =>
+    test(`throws actionable error when a locally defined named function is passed to scheduleOnRN on ${name} Runtime`, async () => {
+      scheduleOnTarget(() => {
+        'worklet';
+        try {
+          scheduleOnRN(function fooFunction() {});
+        } catch (error) {
+          scheduleOnRN(callbackFail, (error as Error).message);
+        }
+      });
+
+      await waitForNotification(FAIL_NOTIFICATION);
+      expect(errorMessage).toInclude(
+        'Locally defined function passed to scheduleOnRN'
+      );
+      expect(errorMessage).toInclude('fooFunction');
+    })
+  );
 });

docs/docs-worklets/docs/guides/troubleshooting.mdx
@@ -189,3 +189,7 @@ Check [this page](/docs/fundamentals/glossary#worklet) to learn more about workl
 ### Tried to synchronously call a non-worklet function on the UI thread
 
 See [Tried to synchronously call a Remote Function](#tried-to-synchronously-call-a-remote-function).
+
+### Locally defined function passed to scheduleOnRN
+
+See [this page](/docs/threading/scheduleOnRN#what-functions-can-be-passed-to-scheduleonrn) to learn more about functions that can be passed to `scheduleOnRN`.

docs/docs-worklets/docs/threading/scheduleOnRN.mdx
@@ -54,21 +54,45 @@ import ScheduleOnRNsrc from '!!raw-loader!@site/src/examples/ScheduleOnRN';
 
 ## Remarks
 
-- Functions passed to `scheduleOnRN` must be defined in the [RN Runtime](/docs/fundamentals/runtimeKinds#rn-runtime) scope, i.e. in the component body or the global scope. The code below won't work because `myFunction` is defined in the `runOnUI` callback, which is only executed in the [UI Runtime](/docs/fundamentals/runtimeKinds#ui-runtime):
+### What functions can be passed to `scheduleOnRN`?
 
-<Indent>
-  ```jsx
-  import { runOnUI, scheduleOnRN } from 'react-native-worklets';
+Functions passed to `scheduleOnRN` must be defined on the [RN Runtime](/docs/fundamentals/runtimeKinds#rn-runtime) scope, i.e. in the component body or the global scope. The code below won't work because `myFunction` is defined in the `scheduleOnUI` callback, which is only executed on the [UI Runtime](/docs/fundamentals/runtimeKinds#ui-runtime).
 
-  runOnUI(() => {
-    // myFunction is defined on the UI thread 🚨
-    const myFunction = () => {
-      // ...
-    };
-    scheduleOnRN(myFunction); // 💥
-  })();
-  ```
-</Indent>
+```jsx
+import { scheduleOnRN, scheduleOnUI } from 'react-native-worklets';
+
+function App() {
+  //... component code
+
+  scheduleOnUI(() => {
+    // callback is defined on the UI Runtime 🚨
+    // highlight-next-line
+    scheduleOnRN(() => {}); // 💥
+  });
+
+  //... other component code
+}
+```
+
+To fix it, you have to move the function definition to the [RN Runtime](/docs/fundamentals/runtimeKinds#rn-runtime) scope:
+
+```jsx
+import { scheduleOnRN, scheduleOnUI } from 'react-native-worklets';
+
+function App() {
+  //... component code
+
+  // myFunction is defined on the RN Runtime ✅
+  // highlight-next-line
+  const myFunction = () => {};
+
+  scheduleOnUI(() => {
+    scheduleOnRN(myFunction); // 🥳
+  });
+
+  //... other component code
+}
+```
 
 ## Implementation Details
 

packages/react-native-worklets/Common/cpp/worklets/NativeModules/JSIWorkletsModuleProxy.cpp
@@ -16,6 +16,7 @@
 #endif // NDEBUG
 #include <memory>
 #include <optional>
+#include <stdexcept>
 #include <string>
 #include <utility>
 #include <vector>
@@ -345,8 +346,35 @@ jsi::Object JSIWorkletsModuleProxy::toOptimizedObject(jsi::Runtime &rt) const {
       });
 
   jsi_utils::addMethod<1>(
-      rt, obj, "createSerializableFunction", [](jsi::Runtime &rt, const jsi::Value &, const jsi::Value(&args)[1]) {
-        return makeSerializableFunction(rt, at<0>(args).asObject(rt).asFunction(rt));
+      rt,
+      obj,
+      "createSerializableNonWorkletFunction",
+      [](jsi::Runtime &rt, const jsi::Value &, const jsi::Value(&args)[1]) {
+        auto fun = at<0>(args).getObject(rt).getFunction(rt);
+#ifndef NDEBUG
+        const auto funName = fun.getProperty(rt, "name").getString(rt).utf8(rt);
+#endif // NDEBUG
+        if (fun.isHostFunction(rt)) {
+          return makeSerializableHostFunction(
+              rt,
+              fun.getHostFunction(rt),
+#ifndef NDEBUG
+              funName,
+#else
+              fun.getProperty(rt, "name").getString(rt).utf8(rt),
+#endif // NDEBUG
+              fun.getProperty(rt, "length").asNumber());
+        } else {
+          return makeSerializableRemoteFunction(
+              rt,
+              std::move(fun)
+
+#ifndef NDEBUG
+                  ,
+              funName
+#endif
+          );
+        }
       });
 
   jsi_utils::addMethod<2>(
@@ -396,6 +424,54 @@ jsi::Object JSIWorkletsModuleProxy::toOptimizedObject(jsi::Runtime &rt) const {
             rt, at<2>(args), "[Worklets] Unpack function must be a worklet.");
         const auto typeId = at<3>(args).asNumber();
         registerCustomSerializable(runtimeManager, memoryManager, determine, pack, unpack, typeId);
+      });
+
+  jsi_utils::addMethod<2>(
+      rt,
+      obj,
+      "scheduleOnRN",
+      [jsScheduler = jsScheduler_](jsi::Runtime &rt, const jsi::Value &, const jsi::Value(&args)[2]) {
+        const auto &funOrSerializable = at<0>(args).getObject(rt);
+        const auto &remoteArgs = at<1>(args);
+
+        auto serializableArgs = remoteArgs.isUndefined()
+            ? nullptr
+            : extractSerializableOrThrow<SerializableArray>(rt, remoteArgs, "[Worklets] Args must be an array.");
+
+        if (funOrSerializable.isFunction(rt)) {
+          auto fun = funOrSerializable.getFunction(rt);
+          if (fun.isHostFunction(rt)) {
+            auto hostFun = fun.getHostFunction(rt);
+            jsScheduler->scheduleOnJS([hostFun = std::move(hostFun), serializableArgs](jsi::Runtime &rnRuntime) {
+              if (serializableArgs == nullptr) {
+                // fast path for host function w/o arguments
+                hostFun(rnRuntime, jsi::Value::undefined(), nullptr, 0);
+              } else {
+                const auto args = serializableArgs->getJSIValueArr(rnRuntime);
+                hostFun(rnRuntime, jsi::Value::undefined(), args.data(), args.size());
+              }
+            });
+          } else [[unlikely]] { // NOLINT(readability/braces)
+            const auto fnName = fun.getProperty(rt, "name").asString(rt).utf8(rt);
+            const auto nameInError = fnName.empty() ? "" : " (" + fnName + ")";
+            throw std::runtime_error(
+                "[Worklets] Locally defined function passed to scheduleOnRN" + nameInError +
+                ". Only functions defined on the RN Runtime or host functions can be scheduled on the RN Runtime. Define the function on the RN Runtime and pass it as a reference. See https://docs.swmansion.com/react-native-worklets/docs/guides/troubleshooting#locally-defined-function-passed-to-scheduleonrn for more details.");
+          }
+        } else {
+          auto remoteFunction = extractSerializableOrThrow<SerializableRemoteFunction>(rt, funOrSerializable);
+          jsScheduler->scheduleOnJS([remoteFunction, serializableArgs](jsi::Runtime &rnRuntime) {
+            auto fun = remoteFunction->toJSValue(rnRuntime).getObject(rnRuntime).getFunction(rnRuntime);
+
+            if (serializableArgs == nullptr) {
+              // fast path for remote function w/o arguments
+              fun.call(rnRuntime);
+            } else {
+              const auto args = serializableArgs->getJSIValueArr(rnRuntime);
+              fun.call(rnRuntime, args.data(), args.size());
+            }
+          });
+        }
       });
 
   jsi_utils::addMethod<2>(

commit 5ba3f9d2d64c8db6f86c02794aa50e63f4b8556e

fix(Worklets): repair NDEBUG-only build error in SerializableRemoteFunction
In Release builds (NDEBUG defined), the #ifndef NDEBUG blocks elided the
`name` argument but left a dangling comma before it, producing `function, )`
— a syntax error. Move the comma inside the #ifndef NDEBUG blocks so the
last always-present argument never has a trailing comma. Pattern matches
the already-correct site in JSIWorkletsModuleProxy.cpp.

Regression introduced in #9386.

Co-Authored-By: Claude Opus 4.7 <noreply@anthropic.com>

packages/react-native-worklets/Common/cpp/worklets/SharedItems/Serializable.h
@@ -262,8 +262,9 @@ class SerializableRemoteFunction : public Serializable,
  public:
   SerializableRemoteFunction(
       jsi::Runtime &rt,
-      jsi::Function &&function,
+      jsi::Function &&function
 #ifndef NDEBUG
+      ,
       const std::string &name
 #endif
       )

packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableFactory.cpp
@@ -62,15 +62,17 @@ jsi::Value makeSerializableHostFunction(
 
 jsi::Value makeSerializableRemoteFunction(
     jsi::Runtime &rt,
-    jsi::Function function,
+    jsi::Function function
 #ifndef NDEBUG
+    ,
     const std::string &name
 #endif
 ) {
   auto serializable = std::make_shared<SerializableRemoteFunction>(
       rt,
-      std::move(function),
+      std::move(function)
 #ifndef NDEBUG
+          ,
       name
 #endif
   );

packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableFactory.h
@@ -51,8 +51,9 @@ jsi::Value makeSerializableHostFunction(
 
 jsi::Value makeSerializableRemoteFunction(
     jsi::Runtime &rt,
-    jsi::Function function,
+    jsi::Function function
 #ifndef NDEBUG
+    ,
     const std::string &name
 #endif
 );