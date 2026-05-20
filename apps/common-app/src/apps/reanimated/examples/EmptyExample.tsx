203e2dce73f9a6338207bbb3c11eb25e8ce4d1eb
Tomasz Żelawski <tzelawski@gmail.com>
2026-05-20 20:41:33 +0200

    hover

--- apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
+++ apps/common-app/src/apps/reanimated/examples/EmptyExample.tsx
@@ -1,18 +1,230 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, { cubicBezier } from 'react-native-reanimated';
+import { Circle, LinearGradient, Rect, Stop, Svg } from 'react-native-svg';
 
-export default function EmptyExample() {
+import { Screen } from '@/apps/css/components';
+
+const fontFamily = 'DM Sans';
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-    </View>
+    <Animated.Text
+      style={[
+        styles.word,
+        {
+          fontSize: {
+            default: 128,
+            ':hover': 192,
+          },
+          color: {
+            default: '#000',
+            ':hover': '#EBFCF7',
+          },
+          shadowColor: {
+            default: 'transparent',
+            ':hover': '#57B495',
+          },
+          shadowRadius: {
+            default: 5,
+            ':hover': 15,
+          },
+          shadowOffset: {
+            default: { width: 0, height: 0 },
+            ':hover': { width: 5, height: 5 },
+          },
+          paddingHorizontal: 10,
+          marginHorizontal: -10,
+          shadowOpacity: 1,
+          overflow: 'visible',
+          transitionDuration: '250ms',
+          transitionTimingFunction: 'ease-in-out',
+        },
+      ]}>
+      {'App'.split('').map((char, index) => (
+        <Text key={index}>{char}</Text>
+      ))}
+    </Animated.Text>
   );
 }
 
+function Js() {
+  return (
+    <Animated.Text
+      style={[
+        styles.word,
+        {
+          fontSize: {
+            default: 128,
+            ':hover': 192,
+          },
+          color: {
+            default: '#000',
+            ':hover': '#57B495',
+          },
+          shadowColor: {
+            default: 'transparent',
+            ':hover': '#B1DFD0',
+          },
+          shadowRadius: {
+            default: 5,
+            ':hover': 15,
+          },
+          shadowOffset: {
+            default: { width: 0, height: 0 },
+            ':hover': { width: 5, height: 5 },
+          },
+          paddingHorizontal: 10,
+          marginHorizontal: -10,
+          shadowOpacity: 1,
+          overflow: 'visible',
+          transitionDuration: '250ms',
+          transitionTimingFunction: 'ease-in-out',
+        },
+      ]}>
+      {'js'.split('').map((char, index) => (
+        <Text key={index}>{char}</Text>
+      ))}
+    </Animated.Text>
+  );
+}
+
+function Dot() {
+  return <Text style={styles.word}>.</Text>;
+}
+
+function Year2026() {
+  return (
+    <Animated.Text
+      style={{
+        textAlign: 'center',
+        minWidth: 256,
+        minHeight: 48,
+        fontSize: {
+          default: 1,
+          ':hover': 128,
+        },
+        fontFamily,
+        color: {
+          default: '#fff0',
+          ':hover': '#EBFCF7',
+        },
+        shadowColor: {
+          default: '#fff0',
+          ':hover': '#57B495',
+        },
+        shadowRadius: {
+          default: 0,
+          ':hover': 5,
+        },
+        shadowOffset: {
+          default: { width: 0, height: 0 },
+          ':hover': { width: 2, height: 2 },
+        },
+        overflow: 'visible',
+        shadowOpacity: 1,
+        transitionDuration: '250ms',
+        transitionTimingFunction: 'ease-in-out',
+      }}>
+      2026
+    </Animated.Text>
+  );
+}
+
+const AnimatedGradient = Animated.createAnimatedComponent(LinearGradient);
+
+export default function Hover() {
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <View style={[styles.backgroundCircle, { overflow: 'hidden' }]}>
+          <Svg
+            // width="100%"
+            // height
+            style={{
+              position: 'absolute',
+              width: 1000,
+              height: 600,
+              top: 0,
+              left: 0,
+              overflow: 'hidden',
+              backgroundColor: 'yellow',
+            }}>
+            <AnimatedGradient
+              id="gradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="50%"
+              animatedProps={{
+                animationName: {
+                  from: {
+                    gradient: [
+                      { color: brandDark, offset: '0%', opacity: 1 },
+                      { color: brandLight, offset: '20%', opacity: 1 },
+                      { color: brandLight, offset: '30%', opacity: 1 },
+                      { color: brandBlue, offset: '100%', opacity: 1 },
+                    ],
+                  },
+                  to: {
+                    gradient: [
+                      { color: brandDark, offset: '0%', opacity: 1 },
+                      { color: brandLight, offset: '35%', opacity: 1 },
+                      { color: brandLight, offset: '60%', opacity: 1 },
+                      { color: brandBlue, offset: '100%', opacity: 1 },
+                    ],
+                  },
+                },
+                animationTimingFunction: 'ease-in-out',
+                animationDuration: '1500ms',
+                animationIterationCount: 'infinite',
+                animationDirection: 'alternate',
+              }}>
+              <Stop offset="0%" stopColor={brandDark} />
+              <Stop offset="50%" stopColor={brandBlue} />
+              <Stop offset="70%" stopColor={brandLight} />
+            </AnimatedGradient>
+            <Rect x={0} y={0} width={1000} height={600} fill="url(#gradient)" />
+          </Svg>
+          <View style={styles.column}>
+            <View style={styles.row}>
+              <App />
+              <Dot />
+              <Js />
+            </View>
+            <Year2026 />
+          </View>
+        </View>
+      </View>
+    </Screen>
+  );
+}
+
+const brandDark = '#001A72';
+const brandBlue = '#38ACDD';
+const brandLight = '#E1F3FA';
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+  },
+  column: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  word: {
+    fontSize: 128,
+    fontWeight: '700',
+    fontFamily,
+  },
+  backgroundCircle: {
+    borderRadius: 9999,
+    padding: 40,
+    experimental_backgroundImage: `linear-gradient(30deg, ${brandDark}, ${brandBlue}, ${brandLight} 70%)`,
   },
 });
---
+++ apps/fabric-example/assets/fonts/DM_Sans/README.txt
@@ -0,0 +1,136 @@
+DM Sans Variable Font
+=====================
+
+This download contains DM Sans as both variable fonts and static fonts.
+
+DM Sans is a variable font with these axes:
+  opsz
+  wght
+
+This means all the styles are contained in these files:
+  DMSans-VariableFont_opsz,wght.ttf
+  DMSans-Italic-VariableFont_opsz,wght.ttf
+
+If your app fully supports variable fonts, you can now pick intermediate styles
+that aren’t available as static fonts. Not all apps support variable fonts, and
+in those cases you can use the static font files for DM Sans:
+  static/DMSans-Thin.ttf
+  static/DMSans-ExtraLight.ttf
+  static/DMSans-Light.ttf
+  static/DMSans-Regular.ttf
+  static/DMSans-Medium.ttf
+  static/DMSans-SemiBold.ttf
+  static/DMSans-Bold.ttf
+  static/DMSans-ExtraBold.ttf
+  static/DMSans-Black.ttf
+  static/DMSans_18pt-Thin.ttf
+  static/DMSans_18pt-ExtraLight.ttf
+  static/DMSans_18pt-Light.ttf
+  static/DMSans_18pt-Regular.ttf
+  static/DMSans_18pt-Medium.ttf
+  static/DMSans_18pt-SemiBold.ttf
+  static/DMSans_18pt-Bold.ttf
+  static/DMSans_18pt-ExtraBold.ttf
+  static/DMSans_18pt-Black.ttf
+  static/DMSans_24pt-Thin.ttf
+  static/DMSans_24pt-ExtraLight.ttf
+  static/DMSans_24pt-Light.ttf
+  static/DMSans_24pt-Regular.ttf
+  static/DMSans_24pt-Medium.ttf
+  static/DMSans_24pt-SemiBold.ttf
+  static/DMSans_24pt-Bold.ttf
+  static/DMSans_24pt-ExtraBold.ttf
+  static/DMSans_24pt-Black.ttf
+  static/DMSans_36pt-Thin.ttf
+  static/DMSans_36pt-ExtraLight.ttf
+  static/DMSans_36pt-Light.ttf
+  static/DMSans_36pt-Regular.ttf
+  static/DMSans_36pt-Medium.ttf
+  static/DMSans_36pt-SemiBold.ttf
+  static/DMSans_36pt-Bold.ttf
+  static/DMSans_36pt-ExtraBold.ttf
+  static/DMSans_36pt-Black.ttf
+  static/DMSans-ThinItalic.ttf
+  static/DMSans-ExtraLightItalic.ttf
+  static/DMSans-LightItalic.ttf
+  static/DMSans-Italic.ttf
+  static/DMSans-MediumItalic.ttf
+  static/DMSans-SemiBoldItalic.ttf
+  static/DMSans-BoldItalic.ttf
+  static/DMSans-ExtraBoldItalic.ttf
+  static/DMSans-BlackItalic.ttf
+  static/DMSans_18pt-ThinItalic.ttf
+  static/DMSans_18pt-ExtraLightItalic.ttf
+  static/DMSans_18pt-LightItalic.ttf
+  static/DMSans_18pt-Italic.ttf
+  static/DMSans_18pt-MediumItalic.ttf
+  static/DMSans_18pt-SemiBoldItalic.ttf
+  static/DMSans_18pt-BoldItalic.ttf
+  static/DMSans_18pt-ExtraBoldItalic.ttf
+  static/DMSans_18pt-BlackItalic.ttf
+  static/DMSans_24pt-ThinItalic.ttf
+  static/DMSans_24pt-ExtraLightItalic.ttf
+  static/DMSans_24pt-LightItalic.ttf
+  static/DMSans_24pt-Italic.ttf
+  static/DMSans_24pt-MediumItalic.ttf
+  static/DMSans_24pt-SemiBoldItalic.ttf
+  static/DMSans_24pt-BoldItalic.ttf
+  static/DMSans_24pt-ExtraBoldItalic.ttf
+  static/DMSans_24pt-BlackItalic.ttf
+  static/DMSans_36pt-ThinItalic.ttf
+  static/DMSans_36pt-ExtraLightItalic.ttf
+  static/DMSans_36pt-LightItalic.ttf
+  static/DMSans_36pt-Italic.ttf
+  static/DMSans_36pt-MediumItalic.ttf
+  static/DMSans_36pt-SemiBoldItalic.ttf
+  static/DMSans_36pt-BoldItalic.ttf
+  static/DMSans_36pt-ExtraBoldItalic.ttf
+  static/DMSans_36pt-BlackItalic.ttf
+
+Get started
+-----------
+
+1. Install the font files you want to use
+
+2. Use your app's font picker to view the font family and all the
+available styles
+
+Learn more about variable fonts
+-------------------------------
+
+  https://developers.google.com/web/fundamentals/design-and-ux/typography/variable-fonts
+  https://variablefonts.typenetwork.com
+  https://medium.com/variable-fonts
+
+In desktop apps
+
+  https://theblog.adobe.com/can-variable-fonts-illustrator-cc
+  https://helpx.adobe.com/nz/photoshop/using/fonts.html#variable_fonts
+
+Online
+
+  https://developers.google.com/fonts/docs/getting_started
+  https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Fonts/Variable_Fonts_Guide
+  https://developer.microsoft.com/en-us/microsoft-edge/testdrive/demos/variable-fonts
+
+Installing fonts
+
+  MacOS: https://support.apple.com/en-us/HT201749
+  Linux: https://www.google.com/search?q=how+to+install+a+font+on+gnu%2Blinux
+  Windows: https://support.microsoft.com/en-us/help/314960/how-to-install-or-remove-a-font-in-windows
+
+Android Apps
+
+  https://developers.google.com/fonts/docs/android
+  https://developer.android.com/guide/topics/ui/look-and-feel/downloadable-fonts
+
+License
+-------
+Please read the full license text (OFL.txt) to understand the permissions,
+restrictions and requirements for usage, redistribution, and modification.
+
+You can use them in your products & projects – print or digital,
+commercial or otherwise.
+
+This isn't legal advice, please consider consulting a lawyer and see the full
+license for all details.
