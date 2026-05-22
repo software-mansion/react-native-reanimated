3cba4d40f7e5c35d4312b5f333ee979672f88320
Tomasz Żelawski <tzelawski@gmail.com>
2026-05-22 17:17:11 +0200

    chore: remove dead code

--- apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/advancedAPI/useFrameCallback.test.tsx
+++ apps/common-app/src/apps/reanimated/examples/RuntimeTests/tests/advancedAPI/useFrameCallback.test.tsx
@@ -18,23 +18,19 @@ describe('Test *****useFrameCallback*****', () => {
   //   const CancelAfterDelayComponent = ({ timeToStop }: { timeToStop: number }) => {
   //     const width = useSharedValue(0);
   //     const ref = useTestRef(STOP_AFTER_DELAY_REF);
-
   //     const frameCallback = useFrameCallback(({ timeSincePreviousFrame }) => {
   //       if (timeSincePreviousFrame) {
   //         width.value += timeSincePreviousFrame / 8;
   //       }
   //     });
-
   //     const animatedStyle = useAnimatedStyle(() => {
   //       return { width: width.value };
   //     });
-
   //     useEffect(() => {
   //       setTimeout(() => {
   //         frameCallback.setActive(false);
   //       }, timeToStop);
   //     }, [timeToStop, frameCallback]);
-
   //     return (
   //       <View style={styles.container}>
   //         <Animated.View ref={ref} style={[styles.animatedBox, animatedStyle]} />
@@ -44,37 +40,9 @@ describe('Test *****useFrameCallback*****', () => {
   //   test.each([500, 1000])('Run frameCallback for **%p**ms', async timeToStop => {
   //     await render(<CancelAfterDelayComponent timeToStop={timeToStop} />);
   //     const animatedComponent = getTestComponent(STOP_AFTER_DELAY_REF);
-
   //     await wait(timeToStop + 200);
-
   //     const expectedWidth = timeToStop / 8;
   //     expect(await animatedComponent.getAnimatedStyle('width')).toBeWithinRange(expectedWidth - 1, expectedWidth + 2);
   //   });
   // });
-
-  describe('Test _canceling frameCallback_ after predefined time', () => {
-    const InvalidFrameCallback = () => {
-      const imNotAWorklet = () => {
-        console.log('I am not a worklet');
-      };
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const frameCallback = useFrameCallback((_frameInfo) => {
-        imNotAWorklet();
-      });
-
-      return <View />;
-    };
-  });
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column',
-//   },
-//   animatedBox: {
-//     backgroundColor: 'seagreen',
-//     height: 80,
-//     margin: 30,
-//   },
-// });
