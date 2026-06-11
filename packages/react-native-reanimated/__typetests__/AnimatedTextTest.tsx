f9b53fe2c8092dedf8c00bc7d75e56215b8d39cd
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2026-06-11 17:16:25 +0200

    Fix AnimatedTextTest.tsx

--- packages/react-native-reanimated/__typetests__/AnimatedTextTest.tsx
+++ packages/react-native-reanimated/__typetests__/AnimatedTextTest.tsx
@@ -2,7 +2,7 @@
 
 import type React from 'react';
 
-import Animated, { useAnimatedProps, useSharedValue } from '../..';
+import Animated, { useAnimatedProps, useSharedValue } from '..';
 
 function AnimatedTextTest() {
   function AnimatedPropsStringTest() {
