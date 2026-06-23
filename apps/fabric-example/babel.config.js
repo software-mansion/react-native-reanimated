007b3268b780966291d68d18c9b6671b5acf9222
Tomasz Żelawski <tzelawski@gmail.com>
2026-06-23 17:49:33 +0200

    chore: patch issues

--- apps/fabric-example/babel.config.js
+++ apps/fabric-example/babel.config.js
@@ -1,20 +1,3 @@
-const path = require('path');
-
-function getHBCBinary() {
-  const hermescDir = path.join(
-    path.dirname(require.resolve('hermes-compiler/package.json')),
-    'hermesc'
-  );
-  const binDir =
-    process.platform === 'darwin'
-      ? 'osx-bin'
-      : process.platform === 'win32'
-        ? 'win64-bin'
-        : 'linux64-bin';
-  const binName = process.platform === 'win32' ? 'hermesc.exe' : 'hermesc';
-  return path.join(hermescDir, binDir, binName);
-}
-
 /** @type {import('react-native-worklets/plugin').PluginOptions} */
 const workletsPluginOptions = {
   strictGlobal: true,
@@ -37,3 +20,20 @@ module.exports = {
     ],
   ],
 };
+
+const path = require('path');
+
+function getHBCBinary() {
+  const hermescDir = path.join(
+    path.dirname(require.resolve('hermes-compiler/package.json')),
+    'hermesc'
+  );
+  const binDir =
+    process.platform === 'darwin'
+      ? 'osx-bin'
+      : process.platform === 'win32'
+        ? 'win64-bin'
+        : 'linux64-bin';
+  const binName = process.platform === 'win32' ? 'hermesc.exe' : 'hermesc';
+  return path.join(hermescDir, binDir, binName);
+}
