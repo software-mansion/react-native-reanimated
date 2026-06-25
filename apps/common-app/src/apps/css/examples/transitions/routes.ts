4b7e8477a77b018c67e1d8ab08cc75b2168baabb
Jakub Wiśniewski <wisniewskij514@gmail.com>
2026-06-25 15:32:18 +0200

    Add example

--- apps/common-app/src/apps/css/examples/transitions/routes.ts
+++ apps/common-app/src/apps/css/examples/transitions/routes.ts
@@ -100,6 +100,10 @@ const routes = {
         name: ':active',
         Component: pseudoSelectors.Active,
       },
+      PseudoActiveBlocksRender: {
+        name: ':active blocks render transition',
+        Component: pseudoSelectors.ActiveBlocksRender,
+      },
       PseudoActiveDeepest: {
         name: ':active-deepest',
         Component: pseudoSelectors.ActiveDeepest,
