9fd3d3ec3cec6a41622e3e6f5abbb3debb7f9240
Tomasz Żelawski <tzelawski@gmail.com>
2026-06-30 20:25:56 +0200

    refactor: use shared_mutex

--- packages/react-native-worklets/Common/cpp/worklets/Registries/WorkletRuntimeRegistry.h
+++ packages/react-native-worklets/Common/cpp/worklets/Registries/WorkletRuntimeRegistry.h
@@ -3,8 +3,8 @@
 #include <jsi/jsi.h>
 #include <react/debug/react_native_assert.h>
 
-#include <mutex>
 #include <set>
+#include <shared_mutex>
 #include <utility>
 
 using namespace facebook;
@@ -14,17 +14,17 @@ namespace worklets {
 class WorkletRuntimeRegistry {
  private:
   static std::set<jsi::Runtime *> registry_;
-  static std::mutex mutex_; // Protects `registry_`.
+  static std::shared_mutex mutex_;
 
-  WorkletRuntimeRegistry() {} // private ctor
+  WorkletRuntimeRegistry() {}
 
   static void registerRuntime(jsi::Runtime &runtime) {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::lock_guard<std::shared_mutex> lock(mutex_);
     registry_.insert(&runtime);
   }
 
   static void unregisterRuntime(jsi::Runtime &runtime) {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::lock_guard<std::shared_mutex> lock(mutex_);
     registry_.erase(&runtime);
   }
 
@@ -34,7 +34,7 @@ class WorkletRuntimeRegistry {
   template <typename TFn>
   static void runWhileLocked(jsi::Runtime *runtime, TFn &&fn) {
     react_native_assert(runtime != nullptr && "runtime is nullptr");
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::shared_lock<std::shared_mutex> lock(mutex_);
     const bool isAlive = registry_.find(runtime) != registry_.end();
     std::forward<TFn>(fn)(isAlive);
   }
--- packages/react-native-worklets/Common/cpp/worklets/Tools/RNRuntimeStatus.h
+++ packages/react-native-worklets/Common/cpp/worklets/Tools/RNRuntimeStatus.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <mutex>
+#include <shared_mutex>
 #include <utility>
 
 namespace worklets {
@@ -8,20 +8,20 @@ namespace worklets {
 class RNRuntimeStatus {
  public:
   void setDead() {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::lock_guard<std::shared_mutex> lock(mutex_);
     isDead_ = true;
   }
 
   template <typename TFn>
   void runWhileLocked(TFn &&fn) {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::shared_lock<std::shared_mutex> lock(mutex_);
     const bool isDead = isDead_;
     std::forward<TFn>(fn)(isDead);
   }
 
  private:
   bool isDead_ = false;
-  std::mutex mutex_;
+  std::shared_mutex mutex_;
 };
 
 } // namespace worklets
