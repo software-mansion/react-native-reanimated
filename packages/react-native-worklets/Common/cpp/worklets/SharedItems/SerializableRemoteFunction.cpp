c2b6116b1cce419c7051d77f9d0b271efd64d6be
Tomasz Żelawski <tzelawski@gmail.com>
2026-07-01 10:51:29 +0200

    fix(Worklets): tie RNR remote function lifetime to TurboModule invalidate status (#9789)
    ## Summary
    
    Fixes #9751
    
    Changing (again) how RN Runtime remote functions (RFs) are managed,
    since changes from #9673 and #9272 were actually insufficient and the
    map approach won't work.
    
    It's impossible to keep both the actual callback and cached serialized
    callback value alive without a memory cycle, so I'm dropping cache
    consistency. This might result in multiple serializations of a given RF
    the GC triggered and the serialized value wasn't saved, but the
    serialization of a RF is actually cheap.
    
    I'm dropping the map approach entirely in favor of the behavior where
    destructor invocation is based upon the information if the runtime is
    still alive. We can't directly check it, so I'm depending on the
    `invalidate()` method of Turbo Modules - which is supposed to be called
    when the runtime is still alive.
    
    ## Test plan
    
    You can test it on
    [df3e344](https://github.com/software-mansion/react-native-reanimated/pull/9789/commits/df3e344c98b716f2dfb13edae251ae97a365574e)
    commit - there's a special registry there that artificially extends the
    lifetime of Remote Functions - click some buttons on the provided
    example, then reload the app with R button. The callbacks with persist
    and will crash on destructor invocation without the check for `isDead`.
    
    (cherry picked from commit 325a644e3ed00ceb316ce72bdc0fd05f4409465f)

--- packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableRemoteFunction.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/SharedItems/SerializableRemoteFunction.cpp
@@ -6,7 +6,6 @@
 #include <worklets/WorkletRuntime/WorkletRuntime.h>
 
 #include <memory>
-#include <utility>
 
 using namespace facebook;
 
@@ -20,94 +19,51 @@ jsi::Function getRemoteFunctionUnpacker(jsi::Runtime &rt) {
   return remoteFunctionUnpacker.asObject(rt).asFunction(rt);
 }
 
-jsi::Object getRemoteFunctionRegistry(jsi::Runtime &rt) {
-  auto registry = rt.global().getProperty(rt, "__remoteFunctionRegistry");
-  react_native_assert(registry.isObject() && "remoteFunctionRegistry not found");
-  return registry.getObject(rt);
-}
-
 } // namespace
 
-SerializableRemoteFunction::~SerializableRemoteFunction() = default;
-
-jsi::Value SerializableRemoteFunction::unpackSelf(jsi::Runtime &rt) {
-  const auto nameValue = name_.empty() ? jsi::Value::undefined() : jsi::String::createFromUtf8(rt, name_);
-  auto holderFunction = getRemoteFunctionUnpacker(rt).call(rt, nameValue).getObject(rt);
-  holderFunction.setNativeState(rt, std::make_shared<SerializableJSRef>(shared_from_this()));
-  return holderFunction;
-}
-
-jsi::Value SerializableRemoteFunction::RNOrigin::toJSValue(jsi::Runtime &rt) {
-  if (&rt == hostRuntime_) {
-    const auto registry = getRemoteFunctionRegistry(rt);
-    return registry.getPropertyAsFunction(rt, "get").callWithThis(rt, registry, jsi::Value(remoteId_));
-  }
-  std::shared_ptr<SerializableRemoteFunction> proxy;
-  {
-    std::lock_guard<std::mutex> lock(proxyMutex_);
-    proxy = proxy_.lock();
-    if (!proxy) {
-      auto self = std::static_pointer_cast<RNOrigin>(shared_from_this());
-      proxy = std::make_shared<RNOriginProxy>(self);
-      proxy_ = proxy;
+SerializableRemoteFunction::~SerializableRemoteFunction() {
+  if (isHostedOnRNRuntime()) {
+    if (rnRuntimeStatus_->isDead()) {
+      freeWithoutCallingDestructor(function_);
+    } else {
+      function_.reset();
     }
+  } else {
+    cleanupRuntimeAware(hostRuntime_, function_);
   }
-  return proxy->toJSValue(rt);
 }
 
-void SerializableRemoteFunction::RNOrigin::resolveOrRejectPromise(
-    const std::shared_ptr<Serializable> &resolveValue,
-    const std::shared_ptr<RuntimeManager> & /*runtimeManager*/) {
-  jsScheduler_->scheduleOnJS([resolver = shared_from_this(), resolveValue](jsi::Runtime &rt) {
-    resolver->toJSValue(rt).getObject(rt).getFunction(rt).call(rt, resolveValue->toJSValue(rt));
-  });
-}
-
-SerializableRemoteFunction::WorkletOrigin::~WorkletOrigin() {
-  cleanupRuntimeAware(hostRuntime_, function_);
-}
-
-jsi::Value SerializableRemoteFunction::WorkletOrigin::toJSValue(jsi::Runtime &rt) {
+jsi::Value SerializableRemoteFunction::toJSValue(jsi::Runtime &rt) {
   if (&rt == hostRuntime_) {
     return jsi::Value(rt, *function_);
+  } else {
+    const auto name = name_.empty() ? jsi::Value::undefined() : jsi::String::createFromUtf8(rt, name_);
+    auto holderFunction = getRemoteFunctionUnpacker(rt).call(rt, name).getObject(rt);
+    holderFunction.setNativeState(rt, std::make_shared<SerializableJSRef>(shared_from_this()));
+    return holderFunction;
   }
-  return unpackSelf(rt);
 }
 
-void SerializableRemoteFunction::WorkletOrigin::resolveOrRejectPromise(
+// TODO: generalize it and merge with other scheduling methods
+void SerializableRemoteFunction::resolveOrRejectPromise(
     const std::shared_ptr<Serializable> &resolveValue,
     const std::shared_ptr<RuntimeManager> &runtimeManager) {
-  const auto workletRuntime = runtimeManager->getRuntime(hostRuntimeId_);
-  // NOLINTNEXTLINE(readability/braces)
-  if (!workletRuntime) [[unlikely]] {
-    return;
+  if (isHostedOnRNRuntime()) {
+    jsScheduler_->scheduleOnJS([resolver = shared_from_this(), resolveValue](jsi::Runtime &rt) {
+      resolver->toJSValue(rt).getObject(rt).getFunction(rt).call(rt, resolveValue->toJSValue(rt));
+    });
+  } else {
+    const auto workletRuntime = runtimeManager->getRuntime(hostRuntimeId_);
+    // NOLINTNEXTLINE(readability/braces)
+    if (!workletRuntime) [[unlikely]] {
+      // Host runtime is dead, most likely we're the last owner of the Remote Function.
+      // Do nothing.
+    } else {
+      workletRuntime->schedule([resolver = shared_from_this(), resolveValue](jsi::Runtime &rt) {
+        resolver->toJSValue(rt).getObject(rt).getFunction(rt).call(rt, resolveValue->toJSValue(rt));
+      });
+    }
   }
-  workletRuntime->schedule([resolver = shared_from_this(), resolveValue](jsi::Runtime &rt) {
-    resolver->toJSValue(rt).getObject(rt).getFunction(rt).call(rt, resolveValue->toJSValue(rt));
-  });
-}
-
-SerializableRemoteFunction::RNOrigin::RNOriginProxy::RNOriginProxy(const std::shared_ptr<RNOrigin> &origin)
-    : SerializableRemoteFunction(nullptr, origin->getHostRuntimeId(), origin->getName()), origin_(origin) {}
-
-SerializableRemoteFunction::RNOrigin::RNOriginProxy::~RNOriginProxy() {
-  origin_->getJSScheduler()->scheduleOnJS([id = origin_->getRemoteId()](jsi::Runtime &rt) {
-    const auto registry = getRemoteFunctionRegistry(rt);
-    registry.getPropertyAsFunction(rt, "delete").callWithThis(rt, registry, jsi::Value(id));
-  });
-}
-
-jsi::Value SerializableRemoteFunction::RNOrigin::RNOriginProxy::toJSValue(jsi::Runtime &rt) {
-  if (&rt == origin_->getHostRuntime()) {
-    return origin_->toJSValue(rt);
-  }
-  return unpackSelf(rt);
-}
-
-void SerializableRemoteFunction::RNOrigin::RNOriginProxy::resolveOrRejectPromise(
-    const std::shared_ptr<Serializable> &resolveValue,
-    const std::shared_ptr<RuntimeManager> &runtimeManager) {
-  origin_->resolveOrRejectPromise(resolveValue, runtimeManager);
 }
 
 } // namespace worklets
