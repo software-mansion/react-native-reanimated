3640767eacd3b00410998ec41045409cd0a49352
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2026-07-04 11:47:37 +0200

    Release 4.3.2 (#9874)
    ## Summary
    
    Reanimated 4.3.2 patch release. Includes all fixes from the cherry-pick
    thread #9596.

--- packages/react-native-reanimated/Common/cpp/reanimated/CSS/registries/CSSAnimationsRegistry.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/CSS/registries/CSSAnimationsRegistry.cpp
@@ -293,6 +293,10 @@ void CSSAnimationsRegistry::applyViewAnimationsStyle(const Tag viewTag, const do
   }
 
   setInUpdatesRegistry(shadowNode, updatedStyle);
+
+  if (shadowNode && !updatedStyle.empty()) {
+    addUpdatesToBatch(shadowNode, updatedStyle);
+  }
 }
 
 void CSSAnimationsRegistry::activateDelayedAnimations(const double timestamp) {
