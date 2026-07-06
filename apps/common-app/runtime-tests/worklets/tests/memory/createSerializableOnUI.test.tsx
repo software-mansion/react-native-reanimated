338b87c543fa6b62b01e0cc9ec6e567223cf01e4
tshmieldev <wojtekrok88@gmail.com>
2026-07-06 15:14:21 +0200

    Changed error to warning in createSerializable unserializable code branch
    (cherry picked from commit 10a562508f4062c06758f339f49fbcacbd7971cc)
    (cherry picked from commit 1d2942f3ee83178c17a6efd60a21c268044a0eed)
    (cherry picked from commit 2f00765ebeed567c2f78f0995ed35afa7c0acaaf)
    Co-authored-by: Tomasz Żelawski <tzelawski@gmail.com>

--- apps/common-app/runtime-tests/worklets/tests/memory/createSerializableOnUI.test.tsx
+++ apps/common-app/runtime-tests/worklets/tests/memory/createSerializableOnUI.test.tsx
@@ -522,7 +522,7 @@ describe('Test createSerializableOnUI', () => {
   //   expect('magicKey' in Object.getPrototypeOf(obj)).toBe(true);
   // });
 
-  test('createSerializableOnUIInaccessibleObject', async () => {
+  test('createSerializableOnUIInaccessibleObject', () => {
     const clazz = runOnUISync(() => {
       'worklet';
       class Clazz {
@@ -532,9 +532,7 @@ describe('Test createSerializableOnUI', () => {
       return new Clazz();
     });
 
-    await expect(() => {
-      clazz.method();
-    }).toThrow();
+    expect(clazz).toBe(undefined);
   });
 
   test('createSerializableOnUIRemoteNamedFunctionSyncCall', async () => {
@@ -564,13 +562,12 @@ describe('Test createSerializableOnUI', () => {
   });
 
   if (__DEV__) {
-    test('throws when trying to serialize a Promise', async () => {
-      await expect(() =>
-        runOnUISync(() => {
-          'worklet';
-          return Promise.resolve();
-        })
-      ).toThrow('Cannot copy value of type `Promise`');
+    test('replaces an unsupported Promise with undefined', () => {
+      const value = runOnUISync(() => {
+        'worklet';
+        return Promise.resolve();
+      });
+      expect(value).toBe(undefined);
     });
   }
 });
