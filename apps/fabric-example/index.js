a9d43c48ec888df0a1362c6c844921075d19ccaa
tshmieldev <wojtekrok88@gmail.com>
2026-07-30 17:08:20 +0200

    strict mode for fabric-example

--- apps/fabric-example/index.js
+++ apps/fabric-example/index.js
@@ -1,5 +1,19 @@
-import { AppRegistry } from 'react-native';
+import React from 'react';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+LogBox.ignoreLogs([
+  /findNodeHandle is deprecated in StrictMode\. findNodeHandle was passed an instance of/,
+  /findHostInstance_DEPRECATED is deprecated in StrictMode\. findHostInstance_DEPRECATED was passed an instance of/,
+]);
+
+function StrictApp() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => StrictApp);
