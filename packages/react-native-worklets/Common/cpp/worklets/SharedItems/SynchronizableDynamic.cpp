38e9e694f4b95ef127c01e21cab14bcd34f245c5
Tomasz Żelawski <tzelawski@gmail.com>
2026-08-13 17:20:49 +0200

    restore the commented-out setDirty block

--- packages/react-native-worklets/Common/cpp/worklets/SharedItems/SynchronizableDynamic.cpp
+++ packages/react-native-worklets/Common/cpp/worklets/SharedItems/SynchronizableDynamic.cpp
@@ -15,6 +15,14 @@ std::shared_ptr<Serializable> SynchronizableDynamic::getBlocking() {
   return value;
 }
 
+// TODO: Shared pointer members (unless they're atomic) can't be assigned
+// in a non thread-safe manner, therefore `setDirty` has little sense now.
+// void SynchronizableDynamic::setDirty(const std::shared_ptr<Serializable> &value) {
+//   setDirtyBefore();
+//   value_ = value;
+//   setDirtyAfter();
+// }
+
 void SynchronizableDynamic::setBlocking(const std::shared_ptr<Serializable> &value) {
   setBlockingBefore();
   value_ = value;
