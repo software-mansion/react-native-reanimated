8e680030f8d119f8cc0ff3c8adee88850db2434a
Tomasz Żelawski <tzelawski@gmail.com>
2026-08-13 14:50:16 +0200

    restore main's inline docs

--- packages/react-native-worklets/Common/cpp/worklets/SharedItems/Synchronizable.h
+++ packages/react-native-worklets/Common/cpp/worklets/SharedItems/Synchronizable.h
@@ -16,19 +16,27 @@ class Synchronizable : public SynchronizableAccess,
                        public std::enable_shared_from_this<Synchronizable> {
  public:
   /**
-   * Can run concurrently with getDirty, getBlocking, setBlocking.
+   * Can run concurrently with getDirty, setDirty, getBlocking, setBlocking.
    */
   virtual std::shared_ptr<Serializable> getDirty() = 0;
 
   /**
    * Can run concurrently with getDirty, getBlocking.
-   * Can't run concurrently with setBlocking.
+   * Can't run concurrently with setDirty, setBlocking.
    */
   virtual std::shared_ptr<Serializable> getBlocking() = 0;
 
+  // TODO: Shared pointer members (unless they're atomic) can't be assigned
+  // in a non thread-safe manner, therefore `setDirty` has little sense now.
+  /**
+   * Can run concurrently with getDirty, getBlocking.
+   * Can't run concurrently with setDirty, setBlocking.
+   */
+  // void setDirty(const std::shared_ptr<Serializable> &value);
+
   /**
    * Can run concurrently with getDirty.
-   * Can't run concurrently with getBlocking, setBlocking.
+   * Can't run concurrently with getBlocking, setDirty, setBlocking.
    */
   virtual void setBlocking(const std::shared_ptr<Serializable> &value) = 0;
 
