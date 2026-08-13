24c53ef11d74cc52a9e74344e574abc0ef4c50d3
tshmieldev <wojtekrok88@gmail.com>
2026-08-13 14:56:54 +0200

    new impl

--- packages/react-native-worklets/Common/cpp/worklets/SharedItems/RetainingSerializable.h
+++ packages/react-native-worklets/Common/cpp/worklets/SharedItems/RetainingSerializable.h
@@ -5,48 +5,101 @@
 #include <worklets/Registries/WorkletRuntimeRegistry.h>
 #include <worklets/SharedItems/Serializable.h>
 #include <worklets/Tools/JSScheduler.h>
+#include <worklets/Tools/WorkletsSystraceSection.h>
 #include <worklets/WorkletRuntime/RuntimeData.h>
 
+#include <array>
 #include <atomic>
+#include <cstddef>
 #include <memory>
-#include <mutex>
+#include <type_traits>
 #include <utility>
 
 namespace worklets {
 
-template <typename BaseClass>
-class RetainingSerializable : virtual public BaseClass {
+template <typename TSerializable>
+  requires std::is_base_of_v<Serializable, TSerializable>
+class RetainingSerializableStore {
  private:
-  jsi::Runtime *primaryRuntime_;
-  std::unique_ptr<jsi::Value> secondaryValue_;
-  std::atomic<jsi::Runtime *> secondaryRuntime_{nullptr};
-  std::mutex secondaryCacheMutex_;
+  static constexpr std::size_t kSlotsPerChunk = 4;
+
+  struct Slot {
+    std::atomic<jsi::Runtime *> runtime{nullptr};
+    jsi::Value value{jsi::Value::undefined()};
+  };
+
+  std::array<Slot, kSlotsPerChunk> slots_{};
+
+  std::atomic<RetainingSerializableStore *> nextChunk_{nullptr};
+  std::unique_ptr<RetainingSerializableStore> nextChunkOwner_{};
+
+  std::pair<bool, const jsi::Value &> find(jsi::Runtime *rt) {
+    for (auto &slot : slots_) {
+      if (slot.runtime.load(std::memory_order_acquire) == rt) {
+        return {true, slot.value};
+      }
+    }
+    if (auto *next = nextChunk_.load(std::memory_order_acquire)) {
+      return next->find(rt);
+    }
+    return {false, jsi::Value::undefined()};
+  }
+
+  void store(jsi::Runtime *rt, jsi::Value value) {
+    for (auto &slot : slots_) {
+      if (slot.runtime.load(std::memory_order_relaxed) != nullptr) {
+        continue;
+      }
+      jsi::Runtime *expected = nullptr;
+      if (slot.runtime.compare_exchange_strong(expected, rt, std::memory_order_acq_rel)) {
+        slot.value = std::move(value);
+        return;
+      }
+    }
+
+    auto *next = nextChunk_.load(std::memory_order_acquire);
+    if (next == nullptr) {
+      auto fresh = std::make_unique<RetainingSerializableStore>();
+      auto *freshChunk = fresh.get();
+      if (nextChunk_.compare_exchange_strong(next, freshChunk, std::memory_order_acq_rel)) {
+        nextChunkOwner_ = std::move(fresh);
+        next = freshChunk;
+      }
+    }
+    next->store(rt, std::move(value));
+  }
+
+ public:
+  RetainingSerializableStore() = default;
+
+  jsi::Value getOrStore(jsi::Runtime &rt, TSerializable &serializable) {
+    auto [isCached, cachedValue] = find(&rt);
+    if (isCached) {
+      return jsi::Value(rt, cachedValue);
+    }
+
+    auto jsValue = serializable.TSerializable::toJSValue(rt);
+    store(&rt, jsi::Value(rt, jsValue));
+
+    return jsValue;
+  }
+};
+
+template <typename TSerializable>
+  requires std::is_base_of_v<Serializable, TSerializable>
+class RetainingSerializable : virtual public TSerializable {
+ private:
+  std::unique_ptr<RetainingSerializableStore<TSerializable>> store_{
+      std::make_unique<RetainingSerializableStore<TSerializable>>()};
 
  public:
   template <typename... Args>
-  explicit RetainingSerializable(jsi::Runtime &rt, Args &&...args)
-      : BaseClass(rt, std::forward<Args>(args)...), primaryRuntime_(&rt) {}
+  explicit RetainingSerializable(jsi::Runtime &rt, Args &&...args) : TSerializable(rt, std::forward<Args>(args)...) {}
+
+  ~RetainingSerializable() override = default;
 
   jsi::Value toJSValue(jsi::Runtime &rt) override {
-    if (&rt == primaryRuntime_) {
-      return BaseClass::toJSValue(rt);
-    }
-    if (secondaryRuntime_.load(std::memory_order_acquire) == &rt) {
-      return jsi::Value(rt, *secondaryValue_);
-    }
-    auto value = BaseClass::toJSValue(rt);
-    {
-      std::lock_guard<std::mutex> lock(secondaryCacheMutex_);
-      if (secondaryRuntime_.load(std::memory_order_relaxed) == nullptr) {
-        secondaryValue_ = std::make_unique<jsi::Value>(rt, value);
-        secondaryRuntime_.store(&rt, std::memory_order_release);
-      }
-    }
-    return value;
-  }
-
-  ~RetainingSerializable() override {
-    cleanupRuntimeAware(secondaryRuntime_.load(std::memory_order_relaxed), secondaryValue_);
+    return store_->getOrStore(rt, *this);
   }
 };
 
