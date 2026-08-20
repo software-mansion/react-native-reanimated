9fae2e0edc369cc5652645be53d5d44407c88f08
Sullyvahnn-v2 <artur.wojnar@samorzad.agh.edu.pl>
2026-08-20 15:12:54 +0200

    docs: remove noreferrer from privacy policy link
    Co-authored-by: Paweł Małecki <92953623+p-malecki@users.noreply.github.com>

--- docs/docs-reanimated/src/theme/Footer/index.js
+++ docs/docs-reanimated/src/theme/Footer/index.js
@@ -14,7 +14,7 @@ export default function Footer(props) {
     const link = document.createElement('a');
     link.href = PRIVACY_POLICY_URL;
     link.target = '_blank';
-    link.rel = 'noopener noreferrer';
+    link.rel = 'noopener';
     link.dataset.privacyPolicy = '';
     link.textContent = 'Privacy Policy';
     paragraph.appendChild(link);
