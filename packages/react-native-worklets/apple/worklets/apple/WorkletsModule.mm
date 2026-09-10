0ff8f9f76aac0b15a45edfc727145da8a5a68f74
Tomasz Żelawski <tzelawski@gmail.com>
2026-09-10 15:35:56 +0200

    refactor(Worklets): build nativeLoggingHook natively (#10488)
    ## Summary
    
    Read #10490 for more context.
    
    I'm making `nativeLoggingHook` to be re-done in the native code rather
    than acquired from the RN Runtime. This way extra Worklet Runtimes can
    be initialized in parallel to the RN Runtime, instead of waiting for its
    initialization to finish and only then acquire the logger.
    
    ## Test plan
    
    Logging runtime tests work.
    
    ## Changelog
    
    - [x] I added an entry to the `Unpublished` section of each changed
    package's `CHANGELOG.md`, or this PR does not change
    `react-native-reanimated` or `react-native-worklets`.

--- packages/react-native-worklets/apple/worklets/apple/WorkletsModule.mm
+++ packages/react-native-worklets/apple/worklets/apple/WorkletsModule.mm
@@ -83,7 +83,7 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(installTurboModule : (BOOL)bundleModeEnab
     return IsJavaScriptQueue();
   };
   animationFrameQueue_ = [AnimationFrameQueue new];
-  auto runtimeBindings = [self getRuntimeBindings:rnRuntime bundleModeEnabled:bundleModeEnabled];
+  auto runtimeBindings = [self getRuntimeBindings:bundleModeEnabled];
   rnRuntimeStatus_ = std::make_shared<RNRuntimeStatus>();
 
   workletsModuleProxy_ = std::make_shared<WorkletsModuleProxy>(
@@ -129,16 +129,14 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(toggleSlowAnimationsOnUIRuntime)
   return std::make_shared<facebook::react::NativeWorkletsModuleSpecJSI>(params);
 }
 
-- (std::shared_ptr<RuntimeBindings>)getRuntimeBindings:(jsi::Runtime &)rnRuntime
-                                     bundleModeEnabled:(BOOL)bundleModeEnabled
+- (std::shared_ptr<RuntimeBindings>)getRuntimeBindings:(BOOL)bundleModeEnabled
 {
   return std::make_shared<RuntimeBindings>(RuntimeBindings{
       .requestAnimationFrame = [animationFrameQueue =
                                     animationFrameQueue_](std::function<void(const double)> &&callback) -> void {
         [animationFrameQueue requestAnimationFrame:callback];
       },
-      .nativeLoggingHook =
-          bundleModeEnabled ? extractNativeLoggingHookFromRNRuntime(rnRuntime) : RuntimeBindings::NativeLoggingHook{}
+      .nativeLoggingHook = bundleModeEnabled ? makeNativeLoggingHook() : RuntimeBindings::NativeLoggingHook{}
 #ifdef WORKLETS_FETCH_PREVIEW_ENABLED
       ,
       .abortRequest =
