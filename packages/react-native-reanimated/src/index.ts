d450812464f5c58964b27d096c9dec6c50c21edc
Tomasz Żelawski <tzelawski@gmail.com>
2026-09-14 16:55:14 +0200

    chore: docs notes

--- packages/react-native-reanimated/src/index.ts
+++ packages/react-native-reanimated/src/index.ts
@@ -49,7 +49,7 @@ export {
 } from './animation';
 export type { ParsedColorArray } from './Colors';
 export { contrastColor, convertToRGBA, isColor } from './Colors';
-export type { LogData, LogFunction } from './common';
+export type { LogData, LogFunction, LoggerConfig } from './common';
 export { ReanimatedLogLevel } from './common';
 export { DynamicColorIOS, PlatformColor, processColor } from './common';
 export type {
