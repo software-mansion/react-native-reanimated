390dfc4442be74a3f8076572e69524678a08c18b
Oskar Pawica <pawicao@gmail.com>
2026-09-15 22:30:08 +0200

    fix(SET): start a transition from the frame of a running layout animation

--- packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/LayoutAnimations/LayoutAnimationsProxy.h
@@ -199,7 +199,10 @@ struct LayoutAnimationsProxy : public LayoutAnimationsProxyCommon {
 
   void removeSharedContainer(Tag containerTag, TransactionMeta &transaction) const;
 
-  std::vector<react::Point> getAbsolutePositionsForRootPathView(const std::shared_ptr<LightNode> &node) const;
+  std::vector<react::Point> getAbsolutePositionsForRootPathView(
+      const std::shared_ptr<LightNode> &node,
+      bool useViewsOnScreen) const;
+  const ShadowView &viewOnScreen(const LightNode &node) const;
 
   Tag getOrCreateContainer(
       const ShadowView &before,
@@ -214,7 +217,8 @@ struct LayoutAnimationsProxy : public LayoutAnimationsProxyCommon {
 
   std::optional<Transform> parseParentTransforms(
       const std::shared_ptr<LightNode> &node,
-      const std::vector<react::Point> &absolutePositions) const;
+      const std::vector<react::Point> &absolutePositions,
+      bool useViewsOnScreen) const;
   react::Transform resolveTransform(
       const LayoutMetrics &layoutMetrics,
       const Transform &transform,
