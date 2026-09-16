9a3d30ce341f53373e8d6dd25ccdb59169a2f3e0
Tomasz Żelawski <tzelawski@gmail.com>
2026-09-16 10:55:25 +0200

    fix(Worklets): destroy discarded sync call results under the runtime lock (#10552)

--- packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/WorkletRuntime.h
+++ packages/react-native-worklets/Common/cpp/worklets/WorkletRuntime/WorkletRuntime.h
@@ -124,9 +124,15 @@ class WorkletRuntime : public jsi::HostObject, public std::enable_shared_from_th
    * Does not run a microtask checkpoint.
    */
   template <RuntimeCallable TCallable, typename... TArgs>
-  auto runSync(const TCallable &callable, TArgs &&...args) const -> jsi::Value {
+  [[nodiscard]] auto runSync(const TCallable &callable, TArgs &&...args) const -> jsi::Value {
     auto lock = acquireRuntimeLock();
     return runSyncImpl(callable, std::forward<TArgs>(args)...);
+  }
+
+  template <RuntimeCallable TCallable, typename... TArgs>
+  void runSyncAndDiscard(const TCallable &callable, TArgs &&...args) const {
+    auto lock = acquireRuntimeLock();
+    static_cast<void>(runSyncImpl(callable, std::forward<TArgs>(args)...));
   }
 
   /**
@@ -166,7 +172,7 @@ class WorkletRuntime : public jsi::HostObject, public std::enable_shared_from_th
    * Runs a single microtask checkpoint on completion.
    */
   template <RuntimeCallable TCallable, typename... TArgs>
-  auto runSyncAndDrainMicrotasks(const TCallable &callable, TArgs &&...args) const -> jsi::Value {
+  [[nodiscard]] auto runSyncAndDrainMicrotasks(const TCallable &callable, TArgs &&...args) const -> jsi::Value {
     auto lock = acquireRuntimeLock();
     return runSyncImpl<MicrotaskCheckpoint::Run>(callable, std::forward<TArgs>(args)...);
   }
