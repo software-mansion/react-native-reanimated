bb8942cdaeeeb9040c6782e7bf87b5875095c46b
tshmieldev <wojtekrok88@gmail.com>
2026-09-18 15:58:14 +0200

    fix

--- packages/react-native-worklets/src/memory/valueUnpacker.native.ts
+++ packages/react-native-worklets/src/memory/valueUnpacker.native.ts
@@ -15,8 +15,6 @@ declare global {
 export function installValueUnpacker() {
   'worklet';
   'no-worklet-closure';
-  // @ts-expect-error TS thinks we're on the RN runtime where globalThis.__DEV__ is a const
-  globalThis.__DEV__ = typeof __DEV__ !== 'undefined' ? __DEV__ : false;
   const workletsCache = new Map<number, () => unknown>();
 
   function valueUnpacker(objectToUnpack: ObjectToUnpack): unknown {
